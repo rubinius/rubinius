6f01ee536beb97c4158d332640bcbb8384ce8512
Brian Ford <brixen@gmail.com>
2006-12-14 18:00:29 +0000

    Added to sydparse gem extconf.rb that if ENV['DEBUG'] == '1', the gem will compile with debug flags. Also, incremented version to 1.2.1 to distinguish included gem from rubyforge gem.

--- externals/syd-parser/extconf.rb
+++ externals/syd-parser/extconf.rb
@@ -1,8 +1,13 @@
 require 'rbconfig'
 require 'mkmf'
 
-create_makefile('sydney_parser')
-
+if ENV['DEBUG'] == '1'
+  with_cflags("-g") do
+    create_makefile('sydney_parser')
+  end 
+else
+  create_makefile('sydney_parser')
+end
 
 File.open("Makefile","a+") do |f|
     f << <<END
