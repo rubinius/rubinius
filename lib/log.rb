1152ae241441519669b74a29e0a9f9dbc2cdd224
Alexander Kellett <lypanov>
2006-12-15 23:40:05 +0000

    work with ruby 1.8.2

--- lib/log.rb
+++ lib/log.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'date'
 
 class Rubinius::Logger < Logger
   def format_message(severity, time, name, msg)
@@ -6,9 +7,13 @@ class Rubinius::Logger < Logger
   end
 end
 
-Log = Rubinius::Logger.new(STDOUT)
+class Rubinius::Logger182 < Logger
+  def format_message(severity, time, name, msg)
+    "[#{Date.parse(time).strftime("%H:%M:%S")} #{severity[0,1].upcase}] #{msg}\n"
+  end
+end
 
-
+Log = (RUBY_VERSION == "1.8.2") ? Rubinius::Logger182.new(STDOUT) : Rubinius::Logger.new(STDOUT)
 
 if $DEBUG or ENV['RDEBUG']
   Log.level = Logger::DEBUG
