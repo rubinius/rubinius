90ca75adc2959739ddd1b15ba72f9dde0f2c98a2
Pat Eyler <pate>
2006-12-16 04:25:25 +0000

    variable name refactoring to make continuation.rb more clear

--- kernel/continuation.rb
+++ kernel/continuation.rb
@@ -1,44 +1,44 @@
 class Continuation
-  def initialize(ctx)
-    @method = ctx
+  def initialize(method_context)
+    @method = method_context
     @method = copy_method(true)
   end
   
   def copy_method(locals=false)
     top = @method.copy(locals)
-    ctx = top
-    sender = ctx.sender
+    method_context = top
+    sender = method_context.sender
     while sender
-      # p ctx.locals
+      # p method_context.locals
       dp = sender.copy(locals)
       # dp.put 10, dp.locals.dup
       # p dp.locals
-      ctx.put 1, dp
-      ctx = dp
-      sender = ctx.sender
+      method_context.put 1, dp
+      method_context = dp
+      sender = method_context.sender
     end
     
     return top
   end
   
-  def extract_start(blk)
-    @start = blk.block.post_send
+  def extract_start(block)
+    @start = block.block.post_send
   end
   
   def call
-    ctx = copy_method()
-    ctx.put 2, @start
-    ctx.activate(nil)
+    method_context = copy_method()
+    method_context.put 2, @start
+    method_context.activate(nil)
   end
 end
 
 module Kernel
-  def callcc(&blk)
-    cur = MethodContext.current
-    ctx = cur.sender
-    cont = Continuation.new(ctx)
-    cont.extract_start(blk)
-    blk.call(cont)
+  def callcc(&block)
+    current_context = MethodContext.current_contextrent
+    method_context = current_context.sender
+    cont = Continuation.new(method_context)
+    cont.extract_start(block)
+    block.call(cont)
     return nil
   end
 end
