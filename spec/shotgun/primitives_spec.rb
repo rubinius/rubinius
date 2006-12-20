a243a70bd17ec7e9839b69bb18e63d5d943b6095
Brian Ford <brixen@gmail.com>
2006-12-20 19:02:01 +0000

    updated primitives_spec to use example method.

--- spec/shotgun/primitives_spec.rb
+++ spec/shotgun/primitives_spec.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 context "Fixnum" do
   
-  specify "'%' (modulo) should return self modulo other" do
-    rubinius(<<-CODE
-        puts [451 % 2, 93 % 3].inspect
+  specify "% should return self modulo other" do
+    example(<<-CODE
+        p [451 % 2, 93 % 3]
       CODE
     ).should == '[1, 0]'
   end
