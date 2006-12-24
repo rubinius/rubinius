4d137447240f2a10022aa23e09b12d0fe214d8f8
Alexander Kellett <lypanov>
2006-12-24 02:10:17 +0000

    nativebfts now running all bfts test case classes

--- bfts/overlay/test_stuff.rb
+++ bfts/overlay/test_stuff.rb
@@ -1,8 +1,12 @@
 $base_dir = File.dirname(File.expand_path(__FILE__)) + "/"
 $rubinius_base = ENV["RUBINIUS_PATH"] || ($base_dir + "../..")
+
 require $rubinius_base + "/shotgun-tests/helper.rb"
 
-def work_around_bug
+system("#{$rubinius_base}/bin/rcompile #{$base_dir}/foounit.rb")
+system("#{$rubinius_base}/bin/rcompile #{$base_dir}/rubicon_testcase.rb")
+
+def run_single_bfts_testclass filename
   include RubiniusHelper
     system("#{$rubinius_base}/bin/rcompile #{$base_dir}/foounit.rb")
     system("#{$rubinius_base}/bin/rcompile #{$base_dir}/rubicon_testcase.rb")
@@ -12,4 +16,11 @@ def work_around_bug
     puts rp(code)
 end
 
-work_around_bug
+Dir[$base_dir + "/../test_*.rb"].each {
+  |filename|
+  begin
+    run_single_bfts_testclass filename
+  rescue => e
+    puts e.inspect
+  end
+}
