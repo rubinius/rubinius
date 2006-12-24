785ae6158a5f5b3386e0caf7945ad02262711c92
Alexander Kellett <lypanov>
2006-12-24 01:24:27 +0000

    allow for testing any given test set against any given rubinius build, example: RUBINIUS_PATH=~/random-rubinius-checkout ruby ./overlay/test_stuff.rb

--- bfts/overlay/test_stuff.rb
+++ bfts/overlay/test_stuff.rb
@@ -1,10 +1,11 @@
-$base_dir = File.dirname(File.expand_path(__FILE__))
-require $base_dir + "/../../shotgun-tests/helper.rb"
+$base_dir = File.dirname(File.expand_path(__FILE__)) + "/"
+$rubinius_base = ENV["RUBINIUS_PATH"] || ($base_dir + "../..")
+require $rubinius_base + "/shotgun-tests/helper.rb"
 
 def work_around_bug
   include RubiniusHelper
-    system("#{$base_dir}/../../bin/rcompile #{$base_dir}/foounit.rb")
-    system("#{$base_dir}/../../bin/rcompile #{$base_dir}/rubicon_testcase.rb")
+    system("#{$rubinius_base}/bin/rcompile #{$base_dir}/foounit.rb")
+    system("#{$rubinius_base}/bin/rcompile #{$base_dir}/rubicon_testcase.rb")
     code = "$:.unshift(\"#{$base_dir}/\")\n"
     code << "require \"foounit\"\n"
     code << IO.read($base_dir + "/../test_true_class.rb")
