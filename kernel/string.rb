bf83bbb1d223a50676f17d151a9a1e7173b46534
Mae <mae@unknown>
2006-12-28 07:46:51 +0000

    Patch from "kyle" <consalus@gmail.com> Added include? and index to string, some minor fixes to slice and each_byte, plus tests

--- kernel/string.rb
+++ kernel/string.rb
@@ -60,7 +60,7 @@ class String
   end
 
   def prefix?(pre)
-    return false if pre.size >= @size
+    return false if pre.size > self.size
     sub = substring(0, pre.size)
     pre == sub
   end
@@ -101,21 +101,56 @@ class String
     m ? m.full.at(0) : nil 
   end
 
+  def include?(arg)
+    if arg.is_a? Fixnum
+      @data.each { |b|  return true if b == arg }
+      return false
+    elsif arg.is_a? String
+      return (self.index(arg) != nil) 
+    else
+      raise ArgumentError.new("String#include? cannot accept #{arg.class} objects")
+    end
+  end
+
+  def index(arg, offset = nil )
+    if arg.is_a? Fixnum
+      i = 0
+      @data.each { |b| return i if b == arg; i += 1 }
+    elsif arg.is_a? String
+      argsize = arg.size
+      max = self.size - argsize
+      if max >= 0 and argsize > 0
+        0.upto(max) do |i|
+          if @data.get_byte(i) == arg.data.get_byte(0)
+            return i if substring(i,argsize) == arg
+          end
+        end
+      end
+    else
+      raise ArgumentError.new("String#index cannot accept #{arg.class} objects")
+    end
+    return nil 
+  end
+
   def [](arg, len = nil)
     if len
       len = len.to_i
       return nil if len < 0
     end
-    arg = Regexp.new(Regexp.escape(arg)) if arg.is_a?(String)
-    size = self.size
-    
-    if arg.respond_to? :match
+
+    if arg.is_a? String
+      unless len.nil?
+        raise ArgumentError.new("String#[] got incorrect arguments.") # TODO: Make this helpful.
+      end
+      return (self.include?(arg) ? arg.dup : nil)
+    elsif arg.respond_to? :match
       m = arg.match(self)
       m[len.to_i] if m
     elsif arg.respond_to?(:first) && arg.respond_to?(:last)
       from = arg.first
       to = arg.last
       to = to - 1 if arg.respond_to?(:exclude_end?) && arg.exclude_end?
+      size = self.size
       from = from + size if from < 0
       to = to + size if to < 0
       len = to - from + 1
@@ -123,6 +158,7 @@ class String
       
     elsif arg && arg.respond_to?(:to_i)
       arg = arg.to_i
+      size = self.size
       arg = arg + size if arg < 0
       if 0 <= arg && arg < size
         if len
@@ -188,7 +224,7 @@ class ByteArray
   end
 
   def each
-    0.upto(self.size - 1) do
+    0.upto(self.size - 1) do |i|
       yield get_byte(i)
     end
   end
--- shotgun-tests/test_string.rb
+++ shotgun-tests/test_string.rb
@@ -41,6 +41,40 @@ class TestString < RubiniusTestCase
     
     assert_equal "evan is 8", out.first
   end
+
+  def test_string_include
+    out = rp <<-CODE
+    str = 'hello'
+    puts str.include?(?e)
+    puts str.include?('lo')
+    puts str.include?('eld')
+    CODE
+    puts out
+    assert_equal ['true', 'true', 'false'], out 
+  end
+
+  def test_string_index_fixnum
+    out = rp <<-CODE
+    str = 'hello'
+    puts str.index(?e)
+    p str.index(?w)
+    p str.index(?l)
+    CODE
+    puts out
+    assert_equal ['1', 'nil', '2'], out 
+  end
+
+  def test_string_index_string
+    out = rp <<-CODE
+    str = 'hello'
+    puts str.index('e')
+    p str.index('w')
+    p str.index('llo')
+    p str.index('lo')
+    CODE
+    puts out
+    assert_equal ['1', 'nil', '2', '3'], out 
+  end
   
   def test_string_slice
     out = rp <<-CODE
