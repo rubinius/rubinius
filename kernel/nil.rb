cf66b97d370a5668c9973521631f8688feb37541
Brian Ford <brixen@gmail.com>
2007-01-01 20:37:22 +0000

    implemented methods on NilClass.

--- kernel/nil.rb
+++ kernel/nil.rb
@@ -1,12 +1,33 @@
 class NilClass
+  def &(other)
+    false
+  end
+  
+  def ^(other)
+    (other == false or other.nil?) ? false : true
+  end
+  
+  alias :| :^
+  
   def to_s
+    ""
+  end
+  
+  def inspect
     "nil"
   end
   
-  alias :inspect :to_s
-  
   def nil?
     true
+  end
+  
+  def to_a
+    []
+  end
+  
+  def to_f
+    # FIXME: couldn't handle float literals
+    '0.0'.to_f
   end
 
   def to_i
