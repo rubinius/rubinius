ea25c6b17533c280e640dc97e3fec1207fb4be7b
Mae <mae@unknown>
2007-01-03 17:47:09 +0000

    almost done with defined? spec -- still need 'yield' and 'zsuper' test cases from project dir: SPEC_TARGET=mri spec spec/language/defined_spec.rb change SPEC_TARGET to rubinius to test on rubinius

--- spec/language/defined_spec.rb
+++ spec/language/defined_spec.rb
@@ -25,6 +25,27 @@ context "A ruby environment" do
     end.should == 'constant'
   end
 
+  specify "should return 'class variable' when @@x = 1; defined?(@@x) is sent" do
+    example do
+      @@x = 1
+      puts defined?(@@x)
+    end.should == 'class variable'
+  end
+
+  specify "should return 'class variable' when @x = 1; defined?(@x) is sent" do
+    example do
+      @x = 1
+      puts defined?(@x)
+    end.should == 'instance-variable'
+  end
+
+  specify "should return 'global-variable' when $x = 1; defined?($x) is sent" do
+    example do
+      $x = 1
+      puts defined?($x)
+    end.should == 'global-variable'
+  end
+
   specify "should return 'local-variable' when x = 1; defined?(x) is sent" do
     example do
       x = 1
@@ -32,10 +53,41 @@ context "A ruby environment" do
     end.should == 'local-variable'
   end
 
+  specify "should return 'local-variable(in-block)' when defined? is called on a block var" do
+    example do
+      block = Proc.new {|x| defined?(x) }
+      puts block.call(1)
+    end.should == 'local-variable(in-block)'
+  end
+
   specify "should return 'expression' when defined?('foo = bar') is sent" do
     example do
       puts defined?('foo = bar')
     end.should == 'expression'
+  end
+
+  specify "should return 'self' when defined?(self) is sent" do
+    example do
+      puts defined?(self)
+    end.should == 'self'
+  end
+
+  specify "should return 'nil' when defined?(nil) is sent" do
+    example do
+      puts defined?(nil)
+    end.should == 'nil'
+  end
+
+  specify "should return 'true' when defined?(true) is sent" do
+    example do
+      puts defined?(true)
+    end.should == 'true'
+  end
+
+  specify "should return 'false' when defined?(nil) is sent" do
+    example do
+      puts defined?(false)
+    end.should == 'false'
   end
 
   specify "should return 'super' when defined?(Bar#no_args) is sent" do
@@ -46,7 +98,7 @@ context "A ruby environment" do
     end.should == 'super'
   end
 
-  specify "should return 'super' when defined?(Bar#no_args) is sent" do
+  specify "should return 'zsuper' when defined?(Bar#no_args) is sent" do
     example do
       require File.dirname(__FILE__) + '/../defined_fixtures'
       b = Bar.new
