c511d4c7a75001f2597b13f9fc2e910d2dd4d9a2
Mae <mae@unknown>
2007-01-04 06:54:07 +0000

    changed defined spec to be more dumb and just figure out whether its a true/false evaluation

--- spec/language/defined_spec.rb
+++ spec/language/defined_spec.rb
@@ -1,109 +1,116 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 context "A ruby environment" do
-  specify "should return 'method' when defined?(puts) is sent" do
+  specify "should return true when defined?(puts) is sent" do
     example do
-      puts defined?(puts)
-    end.should == 'method'
+      puts !!defined?(puts)
+    end.should == 'true'
   end
 
-  specify "should return 'method' when defined?(Kernel.puts) is sent (attribute)" do
+  specify "should return true when defined?(Kernel.puts) is sent (attribute)" do
     example do
-      puts defined?(Kernel.puts)
-    end.should == 'method'
+      puts !!defined?(Kernel.puts)
+    end.should == 'true'
   end
 
-  specify "should return 'assignment' when defined?(x = 2) is sent" do
+  specify "should return true when defined?(x = 2) is sent" do
     example do
-      puts defined?(x = 2)
-    end.should == 'assignment'
+      puts !!defined?(x = 2)
+    end.should == 'true'
   end
 
-  specify "should return 'constant' when defined?(Object) is sent" do
+  specify "should return true when defined?(Object) is sent" do
     example do
-      puts defined?(Object)
-    end.should == 'constant'
+      puts !!defined?(Object)
+    end.should == 'true'
   end
 
-  specify "should return 'class variable' when @@x = 1; defined?(@@x) is sent" do
+  specify "should return true when @@x = 1; defined?(@@x) is sent" do
     example do
       @@x = 1
-      puts defined?(@@x)
-    end.should == 'class variable'
+      puts !!defined?(@@x)
+    end.should == 'true'
   end
 
-  specify "should return 'class variable' when @x = 1; defined?(@x) is sent" do
+  specify "should return true when @x = 1; defined?(@x) is sent" do
     example do
       @x = 1
-      puts defined?(@x)
-    end.should == 'instance-variable'
+      puts !!defined?(@x)
+    end.should == 'true'
   end
 
-  specify "should return 'global-variable' when $x = 1; defined?($x) is sent" do
+  specify "should return true when $x = 1; defined?($x) is sent" do
     example do
       $x = 1
-      puts defined?($x)
-    end.should == 'global-variable'
+      puts !!defined?($x)
+    end.should == 'true'
   end
 
-  specify "should return 'local-variable' when x = 1; defined?(x) is sent" do
+  specify "should return true when x = 1; defined?(x) is sent" do
     example do
       x = 1
-      puts defined?(x)
-    end.should == 'local-variable'
+      puts !!defined?(x)
+    end.should == 'true'
   end
 
-  specify "should return 'local-variable(in-block)' when defined? is called on a block var" do
+  specify "should return true when defined? is called on a block var" do
     example do
       block = Proc.new {|x| defined?(x) }
-      puts block.call(1)
-    end.should == 'local-variable(in-block)'
+      puts !!block.call(1)
+    end.should == 'true'
   end
 
-  specify "should return 'expression' when defined?('foo = bar') is sent" do
+  specify "should return true when defined?('foo = bar') is sent" do
     example do
-      puts defined?('foo = bar')
-    end.should == 'expression'
+      puts !!defined?('foo = bar')
+    end.should == 'true'
   end
 
-  specify "should return 'self' when defined?(self) is sent" do
+  specify "should return true when defined?(self) is sent" do
     example do
-      puts defined?(self)
-    end.should == 'self'
+      puts !!defined?(self)
+    end.should == 'true'
   end
 
-  specify "should return 'nil' when defined?(nil) is sent" do
+  specify "should return true when defined?(nil) is sent" do
     example do
-      puts defined?(nil)
-    end.should == 'nil'
+      puts !!defined?(nil)
+    end.should == 'true'
   end
 
-  specify "should return 'true' when defined?(true) is sent" do
+  specify "should return true when defined?(true) is sent" do
     example do
       puts defined?(true)
     end.should == 'true'
   end
 
-  specify "should return 'false' when defined?(nil) is sent" do
+  specify "should return true when defined?(nil) is sent" do
     example do
       puts defined?(false)
     end.should == 'false'
   end
 
-  specify "should return 'super' when defined?(Bar#no_args) is sent" do
+  specify "should return false when defined?(x) is sent" do
     example do
-      require File.dirname(__FILE__) + '/../defined_fixtures'
-      b = Bar.new
-      puts b.no_args
-    end.should == 'super'
+      puts !!defined?(x)
+    end.should == 'false'
   end
 
-  specify "should return 'zsuper' when defined?(Bar#no_args) is sent" do
+  specify "should return true when Bar#no_args uses defined?" do
     example do
       require File.dirname(__FILE__) + '/../defined_fixtures'
       b = Bar.new
-      puts b.args
-    end.should == 'zsuper'
+      puts !!b.no_args
+    end.should == 'true'
   end
+
+  specify "should return true when Bar#args uses defined?" do
+    example do
+      require File.dirname(__FILE__) + '/../defined_fixtures'
+      b = Bar.new
+      puts !!b.args
+    end.should == 'true'
+  end
+
 end
 
