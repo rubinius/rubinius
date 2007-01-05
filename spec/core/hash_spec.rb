332378a8900f09009626cb7c4dbf0c8740a657c7
Brian Ford <brixen@gmail.com>
2007-01-05 07:31:56 +0000

    added specs for aliases of Hash#key?

--- spec/core/hash_spec.rb
+++ spec/core/hash_spec.rb
@@ -15,4 +15,25 @@ context "Hash instance method" do
       p [@h.key?(:a), @h.key?(:b), @h.key?('b'), @h.key?(2)]
     end.should == '[true, true, false, false]'
   end
+  
+  specify "has_key? should be a synonym for key?" do
+    example do
+      @h = {:a => 1, :b => 2, :c => 3}
+      p [@h.has_key?(:a), @h.has_key?(:b), @h.has_key?('b'), @h.has_key?(2)]
+    end.should == '[true, true, false, false]'
+  end
+  
+  specify "member? should be a synonym for key?" do
+    example do
+      @h = {:a => 1, :b => 2, :c => 3}
+      p [@h.member?(:a), @h.member?(:b), @h.member?('b'), @h.member?(2)]
+    end.should == '[true, true, false, false]'
+  end
+  
+  specify "include? should be a synonym for key?" do
+    example do
+      @h = {:a => 1, :b => 2, :c => 3}
+      p [@h.include?(:a), @h.include?(:b), @h.include?('b'), @h.include?(2)]
+    end.should == '[true, true, false, false]'
+  end
 end
