2e88d941bc1b1ea506396a915e3c3c4e3dfd1601
Brian Ford <brixen@gmail.com>
2007-01-05 06:35:09 +0000

    updated and simplified float and fixnum specs.

--- spec/core/fixnum_spec.rb
+++ spec/core/fixnum_spec.rb
@@ -1,45 +1,27 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 # %, &, *, **, +, -, -@, /, <, <<, <=, <=>, ==, >, >=, >>, [], ^,
-# abs, div, divmod, id2name, modulo, power!, quo, quo, rdiv, rpower,
+# abs, div, divmod, id2name, modulo, power!, quo, rdiv, rpower,
 # size, to_f, to_s, to_sym, zero?, |, ~
 
 context "Fixnum" do
   
   specify "% should return self modulo other" do
     example do
-      p [451 % 2, 93 % 3]
-    end.should == '[1, 0]'
+      p [451 % 2, 93 % 3.2, 120 % -4.5]
+    end.should == '[1, 0.199999999999995, -1.5]'
   end
 
-  specify "& should return self bitwise AND other fixnum" do
+  specify "& should return self bitwise AND other" do
     example do
-      p [2010 & 5, 65535 & 1, 256 & 16]
-    end.should == '[0, 1, 0]'
+      p [2010 & 5, 65535 & 1, 256 & 16, 0xffff & 0xffffffff]
+    end.should == '[0, 1, 0, 65535]'
   end
   
-  specify "& should return self bitwise AND other bignum" do
+  specify "* should return self multiplied by other" do
     example do
-      p [0xffff & 0xffffffff]
-    end.should == '[65535]'
-  end
-  
-  specify "* should return self multiplied by other fixnum" do
-    example do
-      p [4923 * 2, 5_345_291 * 800]
-    end.should == '[9846, 4276232800]'
-  end
-  
-  specify "* should return self multiplied by other bignum" do
-    example do
-      p [256 * 0xffffffff]
-    end.should == '[1099511627520]'
-  end
-  
-  specify "* should return self multiplied by other float" do
-    example do
-      p [6712 * 0.25]
-    end.should == '[1678.0]'
+      p [4923 * 2, 5_345_291 * 800, 256 * 0xffffffff, 6712 * 0.25]
+    end.should == '[9846, 4276232800, 1099511627520, 1678.0]'
   end
   
   specify "** should return self raise to the other power" do
@@ -48,40 +30,16 @@ context "Fixnum" do
     end.should == '[8, 0.2, 3.0]'
   end
   
-  specify "+ should return self plus other fixnum" do
+  specify "+ should return self plus other" do
     example do
-      p [491 + 2, 90210 + 10, 6000 + 144000]
-    end.should == '[493, 90220, 150000]'
-  end
-  
-  specify "+ should return self plus other bignum" do
-    example do
-      p [9 + 0xffffffff]
-    end.should == '[4294967304]'
-  end
-  
-  specify "+ should return self plus other float" do
-    example do
-      p [1001 + 5.219]
-    end.should == '[1006.219]'
+      p [491 + 2, 90210 + 10, 9 + 0xffffffff, 1001 + 5.219]
+    end.should == '[493, 90220, 4294967304, 1006.219]'
   end
   
   specify "- should return self minus other fixnum" do
     example do
-      p [5 - 10, 259 - 3, 9_237_212 - 5_280]
-    end.should == '[-5, 256, 9231932]'
-  end
-  
-  specify "- should return self minus other bignum" do
-    example do
-      p [4 - 0xffffffff, 2_560_496 - 0xfffffffff]
-    end.should == '[-4294967291, -68716916239]'
-  end
-  
-  specify "- should return self minus other float" do
-    example do
-      p [5 - 5.0, 781 - 0.5, 9 - 200.25]
-    end.should == '[0.0, 780.5, -191.25]'
+      p [5 - 10, 9_237_212 - 5_280, 2_560_496 - 0xfffffffff, 781 - 0.5]
+    end.should == '[-5, 9231932, -68716916239, 780.5]'
   end
   
   specify "-@ should negate self" do
--- spec/core/float_spec.rb
+++ spec/core/float_spec.rb
@@ -5,21 +5,40 @@ require File.dirname(__FILE__) + '/../spec_helper'
 # to_f, to_i, to_int, to_s, truncate, zero?
 
 context "Float" do
-  specify "% should return self modulo other fixnum" do
+  specify "% should return self modulo other" do
     example do
-      p [6543.21 % 137]
-    end.should == '[104.21]'
+      p [6543.21 % 137, 5667.19 % 0xffffffff, 6543.21 % 137.24]
+    end.should == '[104.21, 5667.19, 92.9299999999996]'
   end
   
-  specify "% should return self modulo other bignum" do
+  specify "* should return self multiplied by other" do
     example do
-      p [5667.19 % 0xffffffff]
-    end.should == '[5667.19]'
+      p [4923.98221 * 2, 256.4096 * 0xffffffff, 6712.5 * 0.25]
+    end.should == '[9847.96442, 1101270846124.03, 1678.125]'
   end
   
-  specify "% should return self modulo other float" do
+  specify "** should return self raise to the other power" do
     example do
-      p [6543.21 % 137.24]
-    end.should == '[92.9299999999996]'
+      p [2.3 ** 3, 5.2 ** -1, 9.5 ** 0.5, 9.5 ** 0xffffffff]
+    end.should == '[12.167, 0.192307692307692, 3.08220700148449, Infinity]'
   end
+  
+  specify "+ should return self plus other" do
+    example do
+      p [491.213 + 2, 9.99 + 0xffffffff, 1001.99 + 5.219]
+    end.should == '[493.213, 4294967304.99, 1007.209]'
+  end
+
+  specify "- should return self minus other fixnum" do
+    example do
+      p [9_237_212.5280 - 5_280, 2_560_496.1691 - 0xfffffffff, 5.5 - 5.5]
+    end.should == '[9231932.528, -68716916238.8309, 0.0]'
+  end
+  
+  specify "-@ should negate self" do
+    example do
+      p [2.221.send(:-@), -2.01, -2_455_999_221.5512, --5.5, -8.551.send(:-@)]
+    end.should == '[-2.221, -2.01, -2455999221.5512, 5.5, 8.551]'
+  end
+  
 end
