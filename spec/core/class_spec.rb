27660379c09e561590cf1bc48a9459e29fc00e9c
Mae <mae@unknown>
2007-01-05 06:10:11 +0000

    Added spec for cvar declaration in class bodies

--- spec/core/class_spec.rb
+++ spec/core/class_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-context "class" do
+context "a class definition" do
   specify "should create a new class" do
     example(<<-CODE
     class Foo; end
@@ -10,7 +10,32 @@ context "class" do
     end.should == 'Foo'
   end
 
+  specify "should allow the declaration of cvars in the body" do
+    example(<<-CODE
+    class Foo
+      @@bar = 'foo'
+    end
+    CODE
+    ) do
+      puts Foo.class_variables
+    end.should == '@@bar'
+  end
 
+  specify "should allow the declaration of cvars in a method" do
+    example(<<-CODE
+    class Foo
+      def initialize
+        @@bar = 'foo'
+      end
+    end
+    CODE
+    ) do
+      before = Foo.class_variables
+      Foo.new
+      after = Foo.class_variables 
+      p [before, after]
+    end.should == '[[], ["@@bar"]]'
+  end
 end
 
 context "def" do
@@ -27,3 +52,4 @@ context "def" do
     end.should == 'foo'
   end
 end
+
