a4c621e8319349eda766f73ed9ca55f2a9323ac2
Brian Ford <brixen@gmail.com>
2007-01-15 19:08:22 +0000

    checked in nicksieger's patch to spec_helper that enables specs to run on jruby, woohoo!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 $:.unshift File.dirname(__FILE__)
-require 'mri_target'
-require 'rubinius_target'
-require 'jruby_target'
 
 # Example courtesy of nicksieger, many thanks!
 class Spec::Runner::Context
   def before_context_eval
     case ENV['SPEC_TARGET']
     when /mri/
+      require 'mri_target'
       @context_eval_module.include MRITarget
     when /jruby/
+      require 'jruby_target'
       @context_eval_module.include JRubyTarget
     else
+      require 'rubinius_target'
       @context_eval_module.include RubiniusTarget
     end
   end
