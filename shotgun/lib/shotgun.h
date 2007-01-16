5db681808a3b04721eb6869cacba7030a212b63c
Mental <mental@unknown>
2007-01-16 03:13:30 +0000

    unpack bytes in the right order to begin with, rather than swapping them around later

--- shotgun/lib/shotgun.h
+++ shotgun/lib/shotgun.h
@@ -3,13 +3,6 @@
 #define INTERNAL_MACROS 1
 #define INTERNAL_DEBUG 1
 
-#ifndef swap32
-#define swap32(x)       ((((x)&0xFF)<<24)       \
-                        |(((x)>>24)&0xFF)       \
-                        |(((x)&0x0000FF00)<<8)  \
-                        |(((x)&0x00FF0000)>>8)  )
-#endif
-
 #ifndef MAIN
 extern void *__main_address;
 #endif
