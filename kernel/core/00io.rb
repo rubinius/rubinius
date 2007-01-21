eafeccb64e4ef08cadf3e6467a49a3adf395bfe4
Brian Ford <brixen@gmail.com>
2007-01-21 07:26:36 +0000

    Removed duplicate IO#write method in kernel/core/00io.rb.

--- kernel/core/00io.rb
+++ kernel/core/00io.rb
@@ -7,12 +7,6 @@ class IO
     write "#{str}\n"
   end
   
-  def write(str)
-    Ruby.primitive :io_write
-    exc = IOError.new("Unable to write '#{str}' via #{self}")
-    raise exc
-  end  
-  
   def <<(obj)
     write(obj.to_s)
   end
