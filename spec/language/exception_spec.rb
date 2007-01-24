0d9f9e21c2a268e0710c963c745f07d494e2ab1f
Brian Ford <brixen@gmail.com>
2007-01-24 21:43:17 +0000

    Commiting zimbatm's update to exception_spec.

--- spec/language/exception_spec.rb
+++ spec/language/exception_spec.rb
@@ -80,50 +80,130 @@ context "Exceptions" do
     end.should == [1, 2, 3, 4, 7, 4]
   end
 
-  # TODO : Add "x = something() rescue nil" construct
-  # TODO : Check standard exceptions availability
-  #
-  # * Exception
-  #   o NoMemoryError
-  #   o ScriptError
-  #     + LoadError
-  #     + NotImplementedError
-  #     + SyntaxError
-  #   o SignalExceptio
-  #     + Interrupt
-  #   o StandardError (default for rescue) 
-  #     + ArgumentError
-  #     + IOError
-  #       * EOFError
-  #     + IndexError
-  #     + LocalJumpError
-  #     + NameError
-  #       * NoMethodError
-  #     + RangeError
-  #       * FloatDomainError
-  #     + RegexpError
-  #     + RuntimeError (default for raise)
-  #     + SecurityError
-  #     + SystemCallError
-  #       * Errno::*
-  #     + SystemStackError
-  #     + ThreadError
-  #     + TypeError
-  #     + ZeroDivisionError
-  #   o SystemExit
-  #   o fatal
-  #
-  # Exception construct :
-  #
-  #   begin
-  #     expr..
-  #   [rescue [error_type [=> var],..]
-  #     expr..]..
-  #   [else
-  #     expr..]
-  #   [ensure
-  #     expr..]
-  #   end
-  #
+  specify "on a single line, a default can be assigned on exception" do
+    example do
+      variable = [1,2,3].frist rescue 'exception'
+    end.should == 'exception'
+  end
+
+  specify "that StandardError is the default rescue class" do
+    example do
+      @ret = ''
+      begin
+        begin
+          raise Exception, 'hey hey hey !'
+        rescue => ex
+          @ret = 'intercepted'
+        end
+      rescue Exception => ex
+        @ret = 'not intercepted'
+      end
+    end.should == 'not intercepted'
+    example do
+      @ret = ''
+      begin
+        begin
+          raise StandardError, 'hey hey hey !'
+        rescue => ex
+          @ret = 'intercepted'
+        end
+      rescue Exception => ex
+        @ret = 'not intercepted'
+      end
+    end.should == 'intercepted'
+  end
+
+  specify "that RuntimeError is the default raise class" do
+    example do
+      @ret = ''
+      begin
+        raise
+      rescue => ex
+        @ret = ex.class.to_s
+      end
+      @ret
+    end.should == 'RuntimeError'
+  end
+
+  EXCEPTION_TREE = [
+    :Exception, [
+      :ScriptError, [
+        :LoadError,
+        :NotImplementedError,
+        :SyntaxError
+      ],
+      :SignalException, [
+        :Interrupt
+      ],
+      :StandardError, [ # default for rescue
+        :ArgumentError,
+        :IOError, [
+          :EOFError
+        ],
+        :IndexError,
+        :LocalJumpError,
+        :NameError, [
+          :NoMethodError
+        ],
+        :RangeError, [
+          :FloatDomainError
+        ],
+        :RegexpError,
+        :RuntimeError, # default for raise
+        :SecurityError,
+        :SystemCallError, # FIXME : Errno::*  missing
+        :SystemStackError,
+        :ThreadError,
+        :TypeError,
+        :ZeroDivisionError
+      ],
+      :SystemExit
+    ]
+  ]
+
+  @exception_stack = []
+  @last_exception  = nil
+
+  generate_exception_existance_spec = lambda do |exception_name|
+    specify "exception #{exception_name} is in the core" do
+      example do
+        Object.const_defined?(exception_name)
+      end.should === true
+    end
+  end
+
+  generate_exception_ancestor_spec = lambda do |exception_name, parent_name|
+    specify "#{exception_name} has #{parent_name} as ancestor" do
+      example do
+        exception = Object.const_get(exception_name)
+        exception.ancestors.map{|x| x.to_s}.include?(parent_name.to_s)
+      end.should === true
+    end
+  end
+
+  build_spec_tree = lambda do |tree|
+    tree.each do |element|
+      case element
+      when Array
+        if @exception_stack
+          @exception_stack.push(@last_exception)
+          build_spec_tree.call(element)
+          @exception_stack.pop()
+          @last_exception = nil
+        else
+          raise 'Spec generation error, this case should never occur'
+        end
+      else
+        generate_exception_existance_spec.call(element)
+        @exception_stack.each do |parent_name|
+          generate_exception_ancestor_spec.call(element, parent_name)
+        end
+        @last_exception = element
+      end
+    end
+  end
+
+  build_spec_tree.call(EXCEPTION_TREE)
+
 end
 
