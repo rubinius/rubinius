d0ce7bb2e6115affb0bb41ad512e75c19297ba05
Mae <mae@unknown>
2007-01-26 00:06:39 +0000

    fixed report -- much more accurate now -- and new statistics: rake build_reports

--- bin/compare_tools.rb
+++ bin/compare_tools.rb
@@ -55,21 +55,6 @@ module CompareTools
     nil
   end
 
-  def class_methods_left(klass)
-    if mri_class = mri_class(klass)
-      mri_class.methods - rubinius_class(klass).methods
-    else
-      []
-    end
-  end
-
-  def instance_methods_left(klass)
-    if mri_class = mri_class(klass)
-      mri_class.instance_methods - rubinius_class(klass).instance_methods
-    else
-      []
-    end
-  end
   def rubinius_classes
     constants = Rubinius.constants.map {|const| Rubinius.module_eval(const) }
     constants.select {|const| Class === const }
@@ -80,73 +65,90 @@ module CompareTools
     constants.select {|const| Class === const }
   end
 
-  # high level function for output
-  def complete_classes
-    diffs.select {|klass, info| info[:complete] }.
-      map {|klass, info| klass.to_s }.sort
-  end
-
-  # high level function for output
-  def partially_complete_classes
-    x = diffs.select {|klass, info| !info[:complete] }.
-      map {|klass, info| [klass.to_s, info] }.
-      sort {|x, y| x.first <=> y.first }
-  end
-
-  # high level function for output
-  def incomplete_classes
-    (mri_classes - rubinius_classes).
-      map {|klass| klass.to_s }.sort
-  end
-
-  def total_mri_instance_methods
-    mri_classes.map {|klass| klass.instance_methods.length }.
+  def mri_instance_methods_total
+    mri_classes.map {|klass| x = klass.instance_methods.length }.
       inject {|x, y| x + y }
   end
 
-  def total_mri_class_methods
+  def mri_class_methods_total
     mri_classes.map {|klass| klass.methods.length }.
       inject {|x, y| x + y }
   end
 
-  def total_mri_methods
-    total_mri_class_methods + total_mri_instance_methods
-  end
-
-  def total_rubinius_instance_methods
-    rubinius_classes.map {|klass| klass.instance_methods.length }.
-      inject {|x, y| x + y }
-  end
-
-  def total_rubinius_class_methods
-    rubinius_classes.map {|klass| klass.methods.length }.
-      inject {|x, y| x + y }
-  end
-
-  def total_rubinius_methods
-    total_rubinius_class_methods + total_rubinius_instance_methods
-  end
-
-  def diffs
-    @diffs ||= mri_classes.inject({}) do |info, klass|
-      # if the rubinius class exists, add this info
-      if rklass = rubinius_class(klass)
-        k_info = {}
-        k_info[:method_diff] = (klass.methods - rklass.methods).sort
-        k_info[:instance_method_diff] =\
-          (klass.instance_methods - rklass.instance_methods).sort
-
-        if k_info[:method_diff].empty? and k_info[:instance_method_diff].empty?
-          k_info[:complete] = true
-        else
-          k_info[:complete] = false
-        end
-        info.merge(klass => k_info)
-      # otherwise don't add this to diff info (not implemented)
+  def instance_methods_left
+    mri_classes.map do |klass|
+      if rb_klass = rubinius_class(klass)
+        # diff the methods and count whats left
+        (klass.instance_methods - rb_klass.instance_methods).length
       else
-        info
+        # no rb_klass so just add the mri classes' count
+        klass.instance_methods.length
+      end
+    end.inject {|x, y| x + y }
+  end
+
+  def class_methods_left
+    mri_classes.map do |klass|
+      if rb_klass = rubinius_class(klass)
+        # diff the methods and count whats left
+        (klass.methods - rb_klass.methods).length
+      else
+        # no rb_klass so just add the mri classes' count
+        klass.methods.length
+      end
+    end.inject {|x, y| x + y }
+  end
+
+  # high level function for output
+  # returns a hash of classes and their diffs
+  def partially_complete_classes
+    class_diffs.inject({}) do |classes, segment|
+      klass, info = segment
+      if info[:instance_methods].empty? and info[:class_methods].empty?
+        classes
+      else
+        classes.merge(klass => info) # incomplete
       end
     end
+  end
+
+  # a classes that are finished (all methods implemented)
+  # returns a list
+  def complete_classes
+    class_diffs.map do |klass, info|
+      if info[:instance_methods].empty? and info[:class_methods].empty?
+        klass # complete
+      else
+        nil
+      end
+    end.compact
+  end
+
+  # classes that are not even in objectspace
+  # returns a list
+  def incomplete_classes
+    all = mri_classes.map {|c| c.to_s }
+    present = class_diffs.keys
+    all - present
+  end
+
+  # diff present classes in rubinius core
+  def class_diffs
+    @class_diffs ||= mri_classes.inject({}) do |classes, klass|
+      if rbn_klass = rubinius_class(klass)
+        classes.merge( klass.to_s => class_diff(klass, rbn_klass) )
+      else
+        classes
+      end
+    end
+  end
+
+  # generic class diffing algorithm
+  def class_diff(klass1, klass2)
+    {
+      :instance_methods => klass1.instance_methods - klass2.instance_methods,
+      :class_methods => klass1.methods - klass2.methods
+    }
   end
 end
 
