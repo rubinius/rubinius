80698dc9045ce3232f0d12d48021092ce4efb03d
Cabo <cabo@unknown>
2007-01-27 17:35:14 +0000

    Simple change slashes compile time by 37 %. This would be a very simple change, but some VM breakage (possibly related to the block_call issue) requires leaving in some junk code.  Try deleting that and hunting down the VM issue.  With the junk code left in, the change yields an identical result when compiling lib/bytecode/compiler.rb with rubinius (which goes from 510 to 320 s CPU).

--- kernel/bootstrap/class.rb
+++ kernel/bootstrap/class.rb
@@ -18,9 +18,26 @@ class Class
     end
     return out
   end
+
+  def junk_code
+    out = []
+    sup = direct_superclass()
+    while     sup
+      out[out.size] = sup
+      sup = sup.direct_superclass()
+    end
+    out
+  end
   
   def <(other)
-    ancestors.include?(other)
+    return true if object_id == other.object_id
+    sup = direct_superclass()
+    while sup
+      return true if sup.object_id == other.object_id
+      sup = sup.direct_superclass()
+    end
+    x = junk_code.include?(nil) # Junk code to appease VM -- don't delete
+    false
   end
   
   def ===(inst)
