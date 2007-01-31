a282a3b522eb7abd54116b3e61b7778cf56f7a5e
Brian Ford <brixen@gmail.com>
2007-01-31 17:08:36 +0000

    Moved Integer and Float methods from Object to Kernel. Thanks zimbatm for pointing this out.

--- kernel/core/object.rb
+++ kernel/core/object.rb
@@ -62,14 +62,4 @@ class Object
     end
     return res
   end
-  
-  def Float(o)
-    return o.to_f if o.respond_to?(:to_f)
-    raise TypeError, "can't convert #{o.class} into Float"
-  end
-  
-  def Integer(o)
-    return o.to_i if o.respond_to?(:to_i)
-    raise TypeError, "can't convert #{o.class} into Integer"
-  end
 end
