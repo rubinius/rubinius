af04aa917dda0faaed8ede2a0b4615e1ae6456c6
Brian Ford <brixen@gmail.com>
2007-01-31 11:45:55 +0000

    Added Object.Integer.

--- kernel/core/object.rb
+++ kernel/core/object.rb
@@ -63,8 +63,13 @@ class Object
     return res
   end
   
-  def Float(obj)
-    return obj.to_f if obj.respond_to?(:to_f)
+  def Float(o)
+    return o.to_f if o.respond_to?(:to_f)
     raise TypeError, "can't convert #{obj.class} into Float"
+  end
+  
+  def Integer(o)
+    return o.to_i if o.respond_to?(:to_i)
+    raise TypeError, "can't convert #{o.class} into Integer"
   end
 end
