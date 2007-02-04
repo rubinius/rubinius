8d6897551f23373a5cad2aa7a93134525025816a
Mae <mae@unknown>
2007-02-04 18:16:32 +0000

    BAH -- I forgot to upload the actual report files

---
+++ bin/reports/self_hosting_error_info.rb
@@ -0,0 +1,22 @@
+require 'timeout'
+
+BYTECODE = File.expand_path(File.dirname(__FILE__) + '/../../lib/bytecode')
+CORE = File.expand_path(File.dirname(__FILE__) + '/../../kernel/core') +
+BOOTSTRAP = File.expand_path(File.dirname(__FILE__) + '/../../kernel/bootstrap')
+files  = Dir[BYTECODE + '/*.rb']
+files += Dir[BOOTSTRAP + '/*.rb']
+files += Dir[CORE + '/*.rb']
+
+results = files.map do |path|
+  begin
+    command = "shotgun/rubinius #{path}"
+    output = Timeout::timeout(5) do
+      `(#{command}) 2>&1`
+    end
+    [path, output]
+  rescue Timeout::Error
+    [path, 'Timed out (infinite loop?)']
+  end
+end
+
+puts Marshal.dump(results)
