d4b07b06ca85543423a308f12b82ae4671bdd0c2
Mae <mae@unknown>
2007-02-05 21:01:59 +0000

    -split out common primitive testing functionality to primitives_spec_helper.rb -added spec for primitive "equal" -again primitive specs are rake spec:primitives

--- spec/primitives/compare_spec.rb
+++ spec/primitives/compare_spec.rb
@@ -1,39 +1,4 @@
-require 'rubygems'
-require 'spec'
-
-module PrimitiveSpecHelper
-  def run_code(code)
-  `shotgun/rubinius -e '#{code}'`
-  end
-
-  # have to do this the long way in case literals need to be pushed
-  def run_primitive(name, *args)
-    declarations = ""
-    instructions = ""
-    num_args = args.length
-
-    ('a'..'zzz').each do |id|
-      break unless _value = args.pop
-      declarations << "#{id} = #{_value.inspect}; "
-      instructions << "push #{id}\n"
-    end
-
-    code = "begin; #{declarations} puts Ruby.asm(\"" +
-           "#{instructions}send_primitive #{name} #{num_args}" +
-           "\"); " +
-           "rescue PrimitiveFailure; puts \"PrimitiveFailure\".inspect; end"
-    eval run_code(code)
-  end
-end
-
-class PrimitiveFailure < Exception; end
-
-class Object
-  # all this does is assumes that the string rep of self _is_ the exception name
-  def should_raise(exc)
-    self.to_s.should == exc.to_s
-  end
-end
+require File.dirname(__FILE__) + '/../primitives_spec_helper'
 
 context "The compare primitive sent two integers" do
   include PrimitiveSpecHelper
---
+++ spec/primitives/equal_spec.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + '/../primitives_spec_helper'
+
+context "The equal primitive sent two integers" do
+  include PrimitiveSpecHelper
+
+  specify "should return true if x == y" do
+    run_primitive(:equal, 500, 500).should == true
+    run_primitive(:equal, 0, 0).should == true
+    run_primitive(:equal, -11, -11).should == true
+  end
+
+  specify "should return false if x != y" do
+    run_primitive(:equal, 3, 500).should == false
+    run_primitive(:equal, -8, 3).should == false
+    run_primitive(:equal, -1000, -20).should == false
+  end
+end
+
+context "The equal primitive sent non-integers" do
+  include PrimitiveSpecHelper
+
+  specify "should raise PrimitiveFailure with int, non-int" do
+    run_primitive(:equal, 3, "foo").should_raise(PrimitiveFailure)
+    run_primitive(:equal, 3, 7.9).should_raise(PrimitiveFailure)
+  end
+
+  specify "should raise PrimitiveFailure with non-int, int" do
+    run_primitive(:equal, "foo", 80).should_raise(PrimitiveFailure)
+    run_primitive(:equal, 100.0, 80).should_raise(PrimitiveFailure)
+  end
+
+  specify "should raise PrimitiveFailure with non-int, non-int" do
+    run_primitive(:equal, "foo", "bar").should_raise(PrimitiveFailure)
+    run_primitive(:equal, "foo", 38.8).should_raise(PrimitiveFailure)
+    run_primitive(:equal, 8.8, 38.8).should_raise(PrimitiveFailure)
+  end
+end
+
+context "The equal primitive sent the wrong number of args" do
+  include PrimitiveSpecHelper
+
+  specify "should raise PrimitiveFailure with more than 2 args" do
+    run_primitive(:equal, 1, 2, 3).should_raise(PrimitiveFailure)
+    run_primitive(:equal, 1, 2, 3, 4).should_raise(PrimitiveFailure)
+  end
+
+  specify "should raise PrimitiveFailure with less than 2 args" do
+    run_primitive(:equal, 1).should_raise(PrimitiveFailure)
+    run_primitive(:equal).should_raise(PrimitiveFailure)
+  end
+end
+
