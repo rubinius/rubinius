5f1f51d98999d5c30fa829545740af0822b47226
Mental <mental@unknown>
2007-04-24 06:46:04 +0000

    Task-based Continuation

--- kernel/core/continuation.rb
+++ kernel/core/continuation.rb
@@ -1,34 +1,12 @@
 class Continuation
-  def initialize(method_context)
-    @method = method_context
-    @method = copy_method(true)
+  def initialize(task)
+    @task = task
   end
   
-  def copy_method(locals=false)
-    top = @method.copy(locals)
-    method_context = top
-    sender = method_context.sender
-    while sender
-      # p method_context.locals
-      dp = sender.copy(locals)
-      # dp.put 10, dp.locals.dup
-      # p dp.locals
-      method_context.put 1, dp
-      method_context = dp
-      sender = method_context.sender
-    end
-    
-    return top
-  end
-  
-  def extract_start(block)
-    @start = block.block.post_send
-  end
-  
-  def call
-    method_context = copy_method()
-    method_context.put 2, @start
-    method_context.activate(nil)
+  def call(value=nil)
+    task = @task.dup
+    task.instance_variable_set :@__value__, value
+    Task.current = task
   end
 
   alias :[] :call
@@ -36,11 +14,15 @@ end
 
 module Kernel
   def callcc(&block)
-    current_context = MethodContext.current
-    method_context = current_context.sender
-    cont = Continuation.new(method_context)
-    cont.extract_start(block)
-    block.call(cont)
-    return nil
+    first = true
+    task = Task.current.dup
+    if first
+      first = false
+      cont = Continuation.new task
+      block.call(cont)
+      return nil
+    else
+      return task.instance_variable_get :@__value__
+    end
   end
 end
