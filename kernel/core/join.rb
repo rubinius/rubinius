98e1875a07972f3129aa51bc513c71e36430b283
Mental <mental@unknown>
2007-04-26 02:00:42 +0000

    join calculus!

---
+++ kernel/core/join.rb
@@ -0,0 +1,74 @@
+class Join
+  class << self
+    alias :private_new :new
+    private :private_new
+
+    def new(&block)
+      raise ArgumentError, "No block given" unless block
+      c = Class.new self
+      c.class_eval do
+        @max = -1
+        @indices = {}
+        @chords = []
+        class << self
+          def chord(*names, &action)
+            raise ArgumentError, "No channel names specified" if names.empty?
+            raise ArgumentError, "No block given" unless action
+            names.map! { |n| n.to_sym }
+            indices = names.map { |n| @indices[n] ||= ( @max += 1 ) }
+            mask = indices.inject(0) { |acc, i| acc | ( 1 << i ) }
+            @chords.push [ mask, indices, action ]
+            names.each_with_index do |n, i|
+              eval <<-EOS
+                def #{ n }(value=nil)
+                  __channel_send__( #{ indices[i] }, value )
+                end
+              EOS
+            end
+            self
+          end
+        end
+        class_eval &block
+        @chords = @chords.sort_by { |mask, indices, action| -indices.size }
+        class << self
+          undef :chord
+          def new
+            private_new(@chords, @max)
+          end
+        end
+      end
+      c
+    end
+  end
+
+  def initialize(chords, max)
+    @join_pending = (0..max).map { Array.new }
+    @join_pending_mask = 0
+    @join_chords = chords
+    @join_lock = Channel.new
+    @join_lock.send nil
+  end
+
+  def __channel_send__(index, value)
+    @join_lock.receive
+    begin
+      @join_pending[index].push value
+      @join_pending_mask |= 1 << index
+      @join_chords.each do |mask, indices, action|
+        if mask & @join_pending_mask == mask
+          args = indices.map do |i|
+            pending = @join_pending[i]
+            value = pending.shift
+            @join_pending_mask ^= 1 << i if pending.empty?
+          end
+          Thread.new { instance_exec(*args, &action) }
+          break
+        end
+      end
+    ensure
+      @join_lock.send nil
+    end
+    self
+  end
+  private :__channel_send__
+end
