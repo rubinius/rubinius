93ef7304a6bcab13cc6464e190cf98a2241884af
Mental <mental@unknown>
2007-04-26 02:22:48 +0000

    now works under MRI (given suitable supplemental classes)

--- kernel/core/join.rb
+++ kernel/core/join.rb
@@ -2,41 +2,48 @@ class Join
   class << self
     alias :private_new :new
     private :private_new
+  end
 
-    def new(&block)
-      raise ArgumentError, "No block given" unless block
-      c = Class.new self
-      c.class_eval do
-        @max = -1
-        @indices = {}
-        @chords = []
-        def self.chord(*names, &action)
-          raise ArgumentError, "No channel names specified" if names.empty?
-          raise ArgumentError, "No block given" unless action
-          names.map! { |n| n.to_sym }
-          indices = names.map { |n| @indices[n] ||= ( @max += 1 ) }
-          mask = indices.inject(0) { |acc, i| acc | ( 1 << i ) }
-          @chords.push [ mask, indices, action ]
-          names.each_with_index do |n, i|
-            eval <<-EOS
-              def #{ n }(value=nil)
-                __channel_send__( #{ indices[i] }, value )
-              end
-            EOS
-          end
-          self
-        end
-        class_eval &block
-        @chords = @chords.sort_by { |mask, indices, action| -indices.size }
-        class << self
-          undef :chord
-          def new
-            private_new(@chords, @max)
-          end
-        end
-      end
-      c
+  def self.new(&block)
+    raise ArgumentError, "No block given" unless block
+    c = Class.new self
+
+    c.instance_eval do
+      @max = -1
+      @indices = {}
+      @chords = []
     end
+
+    def c.chord(*names, &action)
+      raise ArgumentError, "No channel names specified" if names.empty?
+      raise ArgumentError, "No block given" unless action
+      names.map! { |n| n.to_sym }
+      indices = names.map { |n| @indices[n] ||= ( @max += 1 ) }
+      mask = indices.inject(0) { |acc, i| acc | ( 1 << i ) }
+      @chords.push [ mask, indices, action ]
+      names.each_with_index do |n, i|
+        eval <<-EOS
+          def #{ n }(value=nil)
+            __channel_send__( #{ indices[i] }, value )
+          end
+        EOS
+      end
+      self
+    end
+
+    c.instance_eval &block
+    c.instance_eval do
+      @chords = @chords.sort_by { |mask, indices, action| -indices.size }
+    end
+
+    class << c
+      undef :chord
+      def new
+        private_new(@chords, @max)
+      end
+    end
+
+    c
   end
 
   def initialize(chords, max)
