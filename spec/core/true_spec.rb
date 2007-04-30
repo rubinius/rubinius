3fc864ba235c56118e1db66dbf9537d6ff8c0c5f
Brian Ford <brixen@gmail.com>
2007-04-30 22:00:12 +0000

    Let the breakage begin. Converting all specs to use mini_rspec with 100% compatible syntax with rspec proper. usage: 'USE_RSPEC=1 spec spec/core/false_spec.rb' for any specs that use example {} method. spec spec/core/true_spec.rb OR ./shotgun/rubinius spec/core/true_spec.rb for converted specs.

--- spec/core/true_spec.rb
+++ spec/core/true_spec.rb
@@ -3,33 +3,47 @@ require File.dirname(__FILE__) + '/../spec_helper'
 # &, ^, to_s, |
 
 context "TrueClass" do
+  def true_and(other)
+    true & other
+  end
+  
+  def true_or(other)
+    true | other
+  end
+  
+  def true_xor(other)
+    true ^ other
+  end
+  
   specify "& should return false if other is nil or false, otherwise true" do
-    example do
-      [true & true, true & false, true & nil, true & "", true & Object.new]
-    end.should == [true, false, false, true, true]
+    true_and(true).should == true
+    true_and(false).should == false
+    true_and(nil).should == false
+    true_and("").should == true
+    true_and(Object.new).should == true
   end
 
   specify "^ should return true if other is nil or false, otherwise false" do
-    example do
-      [true ^ true, true ^ false, true ^ nil, true ^ "", true ^ Object.new]
-    end.should == [false, true, true, false, false]
+    true_xor(true).should == false
+    true_xor(false).should == true
+    true_xor(nil).should == true
+    true_xor("").should == false
+    true_xor(Object.new).should == false
   end
 
   specify "to_s should return the string 'true'" do
-    example do
-      true.to_s
-    end.should == "true"
+    true.to_s.should == "true"
   end
 
   specify "| should return true" do
-    example do
-      [true | true, true | false, true | nil, true | "", true | Object.new]
-    end.should == [true, true, true, true, true]
+    true_or(true).should == true
+    true_or(false).should == true
+    true_or(nil).should == true
+    true_or("").should == true
+    true_or(Object.new).should == true
   end
   
   specify "inspect should return the string 'true'" do
-    example do
-      true.inspect
-    end.should == "true"
+    true.inspect.should == "true"
   end
 end
---
+++ spec/rspec_helper.rb
@@ -0,0 +1,46 @@
+class String
+  # remove indentation from the beginning of each line
+  # according to the number of blanks in the first line
+  def unindent
+    match = /^\s+/.match(self)
+    return unless match
+    self.gsub(/^\s{#{match[0].length}}/, "")
+  end
+end
+
+module ExceptionHelper
+  # try(anException, result) { ... } 
+  #   => result if block does not raise anException, else nil
+  def try(a, b=true)
+    yield
+    return nil
+  rescue a
+    return b
+  end
+end
+
+class Object
+  # all this does is assumes that the string rep of self _is_ the exception name
+  def should_raise(exc)
+    self.to_s.should == exc.to_s
+  end
+end
+  
+# Example courtesy of nicksieger, many thanks!
+class Spec::Runner::Context
+  def before_context_eval
+    @context_eval_module.include ExceptionHelper
+
+    case ENV['SPEC_TARGET']
+    when /mri/
+      require 'mri_target'
+      @context_eval_module.include MRITarget
+    when /jruby/
+      require 'jruby_target'
+      @context_eval_module.include JRubyTarget
+    else
+      require 'rubinius_target'
+      @context_eval_module.include RubiniusTarget
+    end
+  end
+end
