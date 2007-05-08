04c03e648ca83de2c2aee37f9aef9079d0493bd7
Eero Saynatkari <eero@kittensoft.org>
2007-05-08 06:24:48 +0000

    * Converted Integer and Kernel specs

--- spec/core/integer_spec.rb
+++ spec/core/integer_spec.rb
@@ -9,126 +9,115 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 context "Integer class method" do
   specify "induced_from should convert other to Integer" do
-    example do
-      [ Integer.induced_from(2.5), 
-        Integer.induced_from(-3.14), 
-        Integer.induced_from(1.233450999123389e+12)]
-    end.should == [2, -3, 1233450999123]
+    Integer.induced_from(2.5).should == 2 
+    Integer.induced_from(-3.14).should == -3 
+    Integer.induced_from(1.233450999123389e+12).should == 1233450999123
   end
 end
 
 context "Integer instance method" do
   specify "ceil should be a synonym for to_i" do
-    example do
-      @a = 1
-      [@a.ceil, @a.ceil.eql?(@a)]
-    end.should == [1, true]
+    a = 1
+    a.ceil.should == 1 
+    a.ceil.eql?(a).should == true
   end
   
   specify "chr should return a string containing the ASCII character represented by self" do
-    example do
-       [ 82.chr, 117.chr, 98.chr, 105.chr, 110.chr, 105.chr, 117.chr, 115.chr,
-         32.chr, 
-         114.chr, 111.chr, 99.chr, 107.chr, 115.chr ]
-    end.should == ["R", "u", "b", "i", "n", "i", "u", "s", " ", "r", "o", "c", "k", "s"]
+    [82.chr, 117.chr, 98.chr, 105.chr, 110.chr, 105.chr, 117.chr, 115.chr, 
+     32.chr, 
+     114.chr, 111.chr, 99.chr, 107.chr, 115.chr].should == ["R", "u", "b", "i", "n", "i", "u", "s",
+                                                            " ", 
+                                                            "r", "o", "c", "k", "s"]
   end
 
   specify "chr should return a new string" do
-    example do
-       82.chr.equal?(82.chr)
-    end.should == false
+     82.chr.equal?(82.chr).should == false
   end
   
   specify "downto should pass block decreasing values from self down to and including other Integer" do
-    example do
-      @a = []
-      3.downto(-1) { |i| @a << i }
-      -1.downto(3) { |i| @a << i }
-      @a
-    end.should == [3, 2, 1, 0, -1]
+    a = []
+    3.downto(-1) { |i| a << i }
+    -1.downto(3) { |i| a << i }
+    a.should == [3, 2, 1, 0, -1]
   end
   
   specify "floor should return the largest integer less than or equal to" do
-    example do
-      [0.floor, -1.floor, 1.floor, 0xffffffff.floor]
-    end.should == [0, -1, 1, 4294967295]
+    0.floor.should == 0 
+    -1.floor.should == -1 
+    1.floor.should == 1 
+    0xffffffff.floor.should == 4294967295
   end
   
   specify "integer? should return true" do
-    example do
-      [0.integer?, 0xffffffff.integer?, -1.integer?]
-    end.should == [true, true, true]
+    0.integer?.should == true 
+    0xffffffff.integer?.should == true
+    -1.integer?.should == true
   end
   
   specify "next should return the Integer equal to self + 1" do
-    example do
-      [0.next, -1.next, 0xffffffff.next, 20.next]
-    end.should == [1, 0, 4294967296, 21]
-  end
-  specify "succ should be an alias for next" do
-    example do
-      [0.succ, -1.succ, 0xffffffff.succ, 20.succ]
-    end.should == [1, 0, 4294967296, 21]
+    0.next.should == 1 
+    -1.next.should == 0
+    0xffffffff.next.should == 4294967296 
+    20.next.should == 21
   end
   
   specify "round should be a synonym for to_i" do
-    example do
-      @a = 1
-      [@a.round, @a.round.eql?(@a)]
-    end.should == [1, true]
+    a = 1
+    a.round.should == a.to_i
+    a.round.eql?(a).should == a.to_i.eql?(a)
   end
   
-  specify "succ should be a synonym for next" do
-    example do
-      [0.next, -1.succ, 0xffffffff.succ, 20.succ]
-    end.should == [1, 0, 4294967296, 21]
+  specify "succ should be an alias for next" do
+    0.succ.should == 0.next
+    -1.succ.should == -1.next
+    0xffffffff.succ.should == 0xffffffff.next
+    20.succ.should == 20.next
   end
-  
+
   specify "times should pass block values from 0 to self - 1" do
-    example do
-      @a = []
-      9.times { |i| @a << i }
-      -2.times { |i| @a << i }
-      @a
-    end.should == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    a = []
+    9.times { |i| a << i }
+    -2.times { |i| a << i }
+    a.should == [0, 1, 2, 3, 4, 5, 6, 7, 8]
   end
   
   specify "to_i should return self" do
-    example do
-      @a = 1
-      [@a.to_i, @a.to_i.eql?(@a)]
-    end.should == [1, true]
+    a = 1
+    a.to_i.should == 1
+    a.to_i.eql?(a).should == true
   end
   
   specify "to_int should be a synonym for to_i" do
-    example do
-      @a = 1
-      [@a.to_int, @a.to_int.eql?(@a)]
-    end.should == [1, true]
+    a = 1
+    a.to_int.should == a.to_i
+    a.to_int.eql?(a).should == a.to_i.eql?(a)
   end
   
   specify "truncate should be a synonym for to_i" do
-    example do
-      @a = 1
-      [@a.truncate, @a.truncate.eql?(@a)]
-    end.should == [1, true]
+    a = 1
+    a.truncate.should == a.to_i
+    a.truncate.eql?(a).should == a.to_i.eql?(a)
   end
   
   specify "upto should pass block values from self up to and including other Integer" do
-    example do
-      @a = []
-      9.upto(13) { |i| @a << i }
-      2.upto(-1) { |i| @a << i }
-      @a
-    end.should == [9, 10, 11, 12, 13]
+    a = []
+    9.upto(13) { |i| a << i }
+    2.upto(-1) { |i| a << i }
+    a.should == [9, 10, 11, 12, 13]
   end
 end
 
 context "Integer#bits" do
   specify "should return the minimum bits of storage needed for the number as a signed int" do
-    example do
-      [1.bits, -1.bits, -2.bits, 2.bits, 4.bits, 536870911.bits, -536870912.bits, 536870912.bits, -536870913.bits]
-    end.should == [2, 2, 2, 3, 4, 30, 30, 31, 31]
+    1.bits.should == 2
+    -1.bits.should == 2
+    -2.bits.should == 2
+    2.bits.should == 3
+    4.bits.should == 4
+    536870911.bits.should == 30
+    -536870912.bits.should ==  30
+    536870912.bits.should == 31
+    -536870913.bits.should == 31
   end
 end
 
--- spec/core/kernel_spec.rb
+++ spec/core/kernel_spec.rb
@@ -2,131 +2,112 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 context "Kernel.Float()" do
   specify "should call to_f to convert any arbitrary argument to a Float" do
-    example do
-      class Foo
-        def to_f; 1.1; end
-      end
+    class KernelSpecFloat
+      def to_f; 1.1; end
+    end
 
-      Float( Foo.new )
-    end.should == 1.1
+    Float(KernelSpecFloat.new).should == 1.1
   end
-  
-  specify "should call to_i to convert any arbitrary argument to a Float" do
-    example do
-      class Foo
-        def to_i; 7; end
-      end
 
-      Float( Foo.new )
-    end.should == 7.0
-  end
+#  This should not work
+#  specify "should call to_i to convert any arbitrary argument to a Float" do
+#    class KernelSpecFloat2
+#      def to_i; 7; end
+#    end
+#
+#    Float(KernelSpecFloat2.new).should == 7.0
+#  end
 
   specify "should give to_f precedence over to_i" do
-    example do
-      class Foo
-        def to_i; 7; end
-        def to_f; 69.9; end
-      end
+    class KernelSpecFloat3
+      def to_i; 7; end
+      def to_f; 69.9; end
+    end
 
-      Float( Foo.new )
-    end.should == 69.9
+    Float(KernelSpecFloat3.new).should == 69.9
   end
 
   specify "should raise a TypeError if there is no to_f or to_i method on an object" do
-    example do
-      class Foo; end
+    class KernelSpecFloat4; end
 
-      Float( Foo.new)
-    end.should_raise(TypeError)
+    should_raise(TypeError) { Float(KernelSpecFloat4.new) }
   end
 
   specify "should raise a TypeError if to_f doesn't return a Float" do
-    example do
-      class Foo
-        def to_f; 'har'; end
-      end
+    class KernelSpecFloat5
+      def to_f; 'har'; end
+    end
 
-      Float( Foo.new)
-    end.should_raise(TypeError)
+    should_raise(TypeError) { Float(KernelSpecFloat5.new) }  
   end
 end
 
 
 context "Kernel.Integer()" do
   specify "should call to_i to convert any arbitrary argument to an Integer" do
-    example do
-      class Foo
-        def to_i; 7; end
-      end
+    class KernelSpecInt
+      def to_i; 7; end
+    end
 
-      Integer( Foo.new )
-    end.should == 7
+    Integer(KernelSpecInt.new).should == 7
   end
 
   specify "should raise a TypeError if there is no to_i method on an object" do
-    example do
-      class Foo; end
+    class KernelSpecInt2; end
 
-      Integer( Foo.new )
-    end.should_raise(TypeError)
+    should_raise(TypeError) { Integer(KernelSpecInt2.new) }
   end
 
   specify "should raise a TypeError if to_i doesn't return an Integer" do
-    example do
-      class Foo
-        def to_i; 'har'; end
-      end
+    class KernelSpecInt3
+      def to_i; 'har'; end
+    end
 
-      Integer( Foo.new )
-    end.should_raise(TypeError)
+    should_raise(TypeError) { Integer(KernelSpecInt3.new) }
   end
 end
 
 
 context "Kernel.Array()" do
   specify "should call to_a to convert any arbitrary argument to an Array" do
-    example do
-      class Foo
-        def to_a; [:a, :b]; end
-      end
-      Array( Foo.new )
-    end.should == [:a, :b]
+    class KernelSpecArray
+      def to_a; [:a, :b]; end
+    end
+
+    Array(KernelSpecArray.new).should == [:a, :b]
   end
 
   specify "should prefer to_ary over to_a" do
-    example do
-      class Foo
-        def to_ary; [:a, :r, :y]; end
-        def to_a; [:a, :b]; end
-      end
-      Array( Foo.new )
-    end.should == [:a, :r, :y]
+    class KernelSpecArray2
+      def to_ary; [:a, :r, :y]; end
+      def to_a; [:a, :b]; end
+    end
+    Array(KernelSpecArray2.new).should == [:a, :r, :y]
   end
 
   specify "should raise a TypeError if to_a doesn't return an Array" do
-    example do
-      class Foo
-        def to_a; 'har'; end
-      end
+    class KernelSpecArray3
+      def to_a; 'har'; end
+    end
 
-      Integer( Foo.new )
-    end.should_raise(TypeError)
+    should_raise(TypeError) { Array(KernelSpecArray3.new) }
   end
+
   specify "called with nil as argument should return an empty Array" do
-    example do
-      Array(nil)
-    end.should == []
+    Array(nil).should == []
   end
 end
 
 
 context "Kernel.String()" do
   specify "should call to_s to convert any arbitrary object to an String" do
-    example do
-      class Foo
-        def to_s; "bar"; end
-      end
-      [String(3), String("foo"), String(nil), String(Foo.new)]
-    end.should == ["3", "foo", "", "bar"]
+    class KernelSpecString
+      def to_s; "bar"; end
+    end
+
+    String(3).should == '3'
+    String("foo").should == 'foo'
+    String(nil).should == ''
+    String(KernelSpecString.new).should == 'bar'
   end
 end
