8796b1f00501813c62676266508a6f89a82ec48e
Brian Ford <brixen@gmail.com>
2007-05-13 22:47:28 +0000

    minor reorganization, cleanup of spec dir.

--- spec/primitives/object_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../primitives_spec_helper'
-
-context "The object_taint primitive" do
-  specify "should return an identical string with a flag change" do
-    'this'.prim.object_taint.should == 'this'
-    # FIXME: can't verify flag change (wait for rspec _on_ rubinius?)
-  end
-end
-
