d9e8f1fd3bc70231c89a1bdc17a9af5a46fce819
Brian Ford <brixen@gmail.com>
2007-05-13 08:28:21 +0000

    converted incompatible specs.

--- spec/incompatible/bignum_spec.rb
+++ spec/incompatible/bignum_spec.rb
@@ -2,17 +2,17 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 context "Bignum instance method" do
   specify "coerce should return [Bignum, Bignum] if other is a Fixnum" do
-    example do
-      @a = 0xffffffff.coerce(1)
-      [@a, @a.collect { |i| i.class }]
-    end.should == [[1, 4294967295], [Bignum, Bignum]]
+      a = 0xffffffff.coerce(1)
+      a.should == [1, 4294967295]
+      a.collect { |i| i.class }.should == [Bignum, Bignum]
   end
   
   specify "coerce should return [Float, Float] if other is not a Bignum or Fixnum" do
-    example do
-      @a = 0xffffffff.coerce("2")
-      @b = 0xffffffff.coerce(1.5)
-      [@a, @a.collect { |i| i.class }, @b, @b.collect { |i| i.class }]
-    end.should == [[2.0, 4294967295.0], [Float, Float], [1.5, 4294967295.0], [Float, Float]]
+    a = 0xffffffff.coerce("2")
+    a.inspect.should == "[2.0, 4294967295.0]"
+    a.collect { |i| i.class }.should == [Float, Float]
+    b = 0xffffffff.coerce(1.5)
+    b.inspect.should == "[1.5, 4294967295.0]"
+    b.collect { |i| i.class }.should == [Float, Float]
   end
 end
--- spec/incompatible/string_spec.rb
+++ spec/incompatible/string_spec.rb
@@ -2,50 +2,35 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 context "String instance method" do
   specify "lstrip should return a string with all leading \\000 and whitespace characters removed" do
-    example do
-      [ "".lstrip,
-        " hello ".lstrip,
-        "\000  hello ".lstrip,
-        "\000\t \000hello ".lstrip,
-        "hello".lstrip ]
-    end.should == ["", "hello ", "hello ", "hello ", "hello"]
+    "".lstrip.should == ""
+    " hello ".lstrip.should == "hello "
+    "\000  hello ".lstrip.should == "hello "
+    "\000\t \000hello ".lstrip.should == "hello "
+    "hello".lstrip.should == "hello"
   end
 
   specify "lstrip! should modify self by removing all leading \\000 and whitespace characters" do
-    example do
-      @s = "\n\t This \000"
-      @t = "\000 another one"
-      @u = "\000  \000\t\v\000two  "
-      [@s.lstrip!, @t.lstrip!, @u.lstrip!]
-    end.should == ["This \000", "another one", "two  "]
+    "\n\t This \000".lstrip!.should == "This \000"
+    "\000 another one".lstrip!.should == "another one"
+    "\000  \000\t\v\000two  ".lstrip!.should == "two  "
   end
   
   specify "rstrip should return a string with all trailing \\000 and whitespace characters removed" do
-    example do
-      [ " \t\n ".rstrip,
-        "\t".rstrip,
-        "".rstrip,
-        " hello ".rstrip,
-        "\tgoodbye\r\n".rstrip,
-        "goodbye \000".rstrip,
-        "goodbye \000\t \f  \000".rstrip ]
-    end.should == ["", "", "", " hello", "\tgoodbye", "goodbye", "goodbye"]
+    " \t\n ".rstrip.should == ""
+    "\t".rstrip.rstrip.should == ""
+    "".rstrip.rstrip.should == ""
+    " hello ".rstrip.rstrip.should == " hello"
+    "\tgoodbye\r\n".rstrip.rstrip.should == "\tgoodbye"
+    "goodbye \000".rstrip.rstrip.should == "goodbye"
+    "goodbye \000\t \f  \000".rstrip.rstrip.should == "goodbye" 
   end
 
   specify "rstrip! should modify self by removing all trailing \\000 and whitespace characters" do
-    example do
-      @s = " hello "
-      @t = "\tgoodbye\r\n"
-      @u = "goodbye \000"
-      @v = "goodbye \000 "
-      @w = ""
-      @z = " \n \000\v\000"
-      [ @s.rstrip!,
-        @t.rstrip!,
-        @u.rstrip!,
-        @v.rstrip!,
-        @w.rstrip!,
-        @z.rstrip! ]
-    end.should == [" hello", "\tgoodbye", "goodbye", "goodbye", nil, ""]
+    " hello ".rstrip!.should == " hello"
+    "\tgoodbye\r\n".rstrip!.should == "\tgoodbye"
+    "goodbye \000".rstrip!.should == "goodbye"
+    "goodbye \000 ".rstrip!.should == "goodbye"
+    "".rstrip!.should == nil
+    " \n \000\v\000".rstrip!.should == ""
   end
 end
