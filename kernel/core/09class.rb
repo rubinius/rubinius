bc679989726784a55820f4173e592ac2422bc63f
Mental <mental@unknown>
2007-05-20 02:41:23 +0000

    implement full Class.new (subject to problems with Class.allocate still)

--- kernel/core/09class.rb
+++ kernel/core/09class.rb
@@ -4,4 +4,11 @@ class Class
     obj.initialize(*args, &block)
     obj
   end
+
+  def self.new(sclass=Object, &block)
+    obj = allocate()
+    obj.superclass = sclass
+    obj.class_eval(&block) if block
+    obj
+  end
 end
