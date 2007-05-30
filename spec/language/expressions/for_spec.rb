8a2177c7f23181909c5dcb51dd6df1e5b930fdbe
Wilson Bilkovich <wilson@supremetyrant.com>
2007-05-30 20:32:13 +0000

    * Enhanced hatefulness of 'for' specs

--- spec/language/expressions/for_spec.rb
+++ spec/language/expressions/for_spec.rb
@@ -40,4 +40,34 @@ describe "The for expression" do
     end
     j.should == 55
   end
+
+  it "should allow an instance variable as an iterator name" do
+    m = [1,2,3]
+    n = 0
+    for @var in m
+      n += 1
+    end
+    @var.should == 3
+    n.should == 3
+  end
+
+  it "should 'splat' multiple arguments together if there are fewer arguments than values" do
+    v = $VERBOSE
+    $VERBOSE = nil
+    class OFor
+      def each
+        [[1,2,3], [4,5,6]].each do |a|
+          yield(a[0],a[1],a[2])
+        end
+      end
+    end
+    o = OFor.new
+    qs = []
+    for q in o
+      qs << q
+    end
+    $VERBOSE = v
+    qs.should == [[1,2,3], [4,5,6]]
+    q.should == [4,5,6]
+  end
 end
