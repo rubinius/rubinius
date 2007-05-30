3b6dca92ea810aa7866a2c7aa0b8812e72b6630e
Hapk <hapk@unknown>
2007-05-30 18:56:00 +0000

    Updated specs for "if" statement to cover more cases.

--- spec/language/expressions/if_spec.rb
+++ spec/language/expressions/if_spec.rb
@@ -1,17 +1,49 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "The if expression" do
-  it "should evaluate the if body when the expression is true" do
+  it "should evaluate body if expression is true" do
+    a = []
     if true
-      a = true
-    else
-      a = false
+      a << 123
     end
-    
-    a.should == true
+    a.should == [123]
   end
-  
-  it "should return the last statement in the body" do
+
+  it "should not evaluate body if expression is false" do
+    a = []
+    if false
+      a << 123
+    end
+    a.should == []
+  end
+
+  it "should not evaluate else-body if expression is true" do
+    a = []
+    if true
+      a << 123
+    else
+      a << 456
+    end
+    a.should == [123]
+  end
+
+  it "should evaluate only else-body if expression is false" do
+    a = []
+    if false
+      a << 123
+    else
+      a << 456
+    end
+    a.should == [456]
+  end
+
+  it "should return result of then-body evaluation if expression is true" do
+    if true
+      123
+    end.should == 123
+  end
+
+  it "should return result of last statement in then-body if expression is true" do
     if true
       'foo'
       'bar'
@@ -19,29 +51,232 @@ describe "The if expression" do
     end.should == 'baz'
   end
 
-  it "should evaluate the else body when the expression is false" do
-    if false
-      'foo'
+  it "should return result of then-body evaluation if expression is true and else part is present" do
+    if true
+      123
     else
-      'bar'
-    end.should == 'bar'
-  end
-  
-  it "should take an optional then after the expression" do
-    if true then
-      'baz'
-    end.should == 'baz'
-  end
-  
-  it "should have no return value when the expression is false" do
-    if false; end.should == nil
+      456
+    end.should == 123
   end
 
-  it "should allow expression and body to be on one line (using ':')" do
-    if true: 'foo'; else 'bar'; end.should == 'foo'
+  it "should return result of else-body evaluation if expression is false" do
+    if false
+      123
+    else
+      456
+    end.should == 456
   end
-  
-  it "should allow expression and body to be on one line (using 'then')" do
-    if true then 'foo'; else 'bar'; end.should == 'foo'
+
+  it "should return nil if then-body is empty and expression is true" do
+    if true
+    end.should == nil
+  end
+
+  it "should return nil if then-body is empty, expression is true and else part is present" do
+    if true
+    else
+      456
+    end.should == nil
+  end
+
+  it "should return nil if then-body is empty, expression is true and else part is empty" do
+    if true
+    else
+    end.should == nil
+  end
+
+  it "should return nil if else-body is empty and expression is false" do
+    if false
+      123
+    else
+    end.should == nil
+  end
+
+  it "should return nil if else-body is empty, expression is false and then-body is empty" do
+    if false
+    else
+    end.should == nil
+  end
+
+  it "should consider expression with nil result as false" do
+    if nil
+      123
+    else
+      456
+    end.should == 456
+  end
+
+  it "should consider non-nil and non-boolean object in expression result as true" do
+    if Object.new
+      123
+    else
+      456
+    end.should == 123
+  end
+
+  it "should consider zero integer in expression result as true" do
+    if 0
+      123
+    else
+      456
+    end.should == 123
+  end
+
+  it "should allow starting then-body on the same line if colon is used" do
+    if true: 123
+    else
+      456
+    end.should == 123
+  end
+
+  it "should allow starting else-body on the same line" do
+    if false
+      123
+    else 456
+    end.should == 456
+  end
+
+  it "should allow both then- and else-bodies start on the same line (with colon after expression)" do
+    if false: 123
+    else 456
+    end.should == 456
+  end
+
+  it "should evaluate subsequent elsif statements and execute body of first matching" do
+    if false
+      123
+    elsif false
+      234
+    elsif true
+      345
+    elsif true
+      456
+    end.should == 345
+  end
+
+  it "should evaluate else-body if no if/elsif statements match" do
+    if false
+      123
+    elsif false
+      234
+    elsif false
+      345
+    else
+      456
+    end.should == 456
+  end
+
+  it "should allow ':' after expression when then-body is on the next line" do
+    if true:
+      123
+    end.should == 123
+
+    if true; 123; end.should == 123
+  end
+
+  it "should allow 'then' after expression when then-body is on the next line" do
+    if true then
+      123
+    end.should == 123
+
+    if true then ; 123; end.should == 123
+  end
+
+  it "should allow then-body on the same line separated with ':'" do
+    if true: 123
+    end.should == 123
+
+    if true: 123; end.should == 123
+  end
+
+  it "should allow then-body on the same line separated with 'then'" do
+    if true then 123
+    end.should == 123
+
+    if true then 123; end.should == 123
+  end
+
+  it "should return nil when then-body on the same line separated with ':' and expression is false" do
+    if false: 123
+    end.should == nil
+
+    if false: 123; end.should == nil
+  end
+
+  it "should return nil when then-body on the same line separated with 'then' and expression is false" do
+    if false then 123
+    end.should == nil
+
+    if false then 123; end.should == nil
+  end
+
+  it "should return nil when then-body separated by ':' is empty and expression is true" do
+    if true:
+    end.should == nil
+
+    if true: ; end.should == nil
+  end
+
+  it "should return nil when then-body separated by 'then' is empty and expression is true" do
+    if true then
+    end.should == nil
+
+    if true then ; end.should == nil
+  end
+
+  it "should return nil when then-body separated by ':', expression is false and no else part" do
+    if false:
+    end.should == nil
+
+    if false: ; end.should == nil
+  end
+
+  it "should return nil when then-body separated by 'then', expression is false and no else part" do
+    if false then
+    end.should == nil
+
+    if false then ; end.should == nil
+  end
+
+  it "should evaluate then-body when then-body separated by ':', expression is true and else part is present" do
+    if true: 123
+    else 456
+    end.should == 123
+
+    if true: 123; else 456; end.should == 123
+  end
+
+  it "should evaluate then-body when then-body separated by 'then', expression is true and else part is present" do
+    if true then 123
+    else 456
+    end.should == 123
+
+    if true then 123; else 456; end.should == 123
+  end
+
+  it "should evaluate else-body when then-body separated by ':' and expression is false" do
+    if false: 123
+    else 456
+    end.should == 456
+
+    if false: 123; else 456; end.should == 456
+  end
+
+  it "should evaluate else-body when then-body separated by 'then' and expression is false" do
+    if false then 123
+    else 456
+    end.should == 456
+
+    if false then 123; else 456; end.should == 456
+  end
+
+  it "should allow elsif-body at the same line separated by ':' or 'then'" do
+    if false then 123
+    elsif false: 234
+    elsif true then 345
+    elsif true: 456
+    end.should == 345
+
+    if false: 123; elsif false then 234; elsif true: 345; elsif true then 456; end.should == 345
   end
 end
