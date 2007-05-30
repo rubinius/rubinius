d627ab36d3839745e057d9f5de781269ba7b154a
Hapk <hapk@unknown>
2007-05-30 21:15:39 +0000

    Added specs for 'while' and 'until' statements.

--- spec/language/expressions/until_spec.rb
+++ spec/language/expressions/until_spec.rb
@@ -17,33 +17,109 @@ describe "The until expression" do
     until i > 9
       i += 1
     end
+
+    i.should == 10
+  end
+  
+  it "should optionally take a 'do' after the expression" do
+    i = 0
+    until i > 9 do
+      i += 1
+    end
     
     i.should == 10
   end
   
-  it "should optionally take a do after the expression" do
+  it "should allow body begin on the same line if do is used" do
     i = 0
-    until i > 9 do a = 0; i += 1; end
+    until i > 9 do i += 1
+    end
     
-    a.should == 0
     i.should == 10
   end
-
-  it "should have no return value" do
+  
+  it "should execute code in containing variable scope" do
     i = 0
-    val = until i > 9
+    until i == 1
+      a = 123
+      i = 1
+    end
+    
+    a.should == 123
+  end
+  
+  it "should execute code in containing variable scope with 'do'" do
+    i = 0
+    until i == 1 do
+      a = 123
+      i = 1
+    end
+    
+    a.should == 123
+  end
+  
+  it "should return nil if ended when condition became true" do
+    i = 0
+    while i > 9
       i += 1
     end.should == nil
-    val.should == nil
+  end
+  
+  it "should stop running body if interrupted by break" do
+    i = 0
+    until i > 9
+      i += 1
+      break if i > 5
+    end
+    i.should == 6
+  end
+  
+  it "should return value passed to break if interrupted by break" do
+    until false
+      break 123
+    end.should == 123
+  end
+  
+  it "should return nil if interrupted by break with no arguments" do
+    until false
+      break
+    end.should == nil
   end
 
-  it "should run it's preceding statement while the expression is false" do
+end
+  
+describe "The postfix until expression" do
+  it "should run preceding statement while the condition is false" do
     i = 0
     i += 1 until i > 9
     i.should == 10
   end
+  
+  it "should not run preceding statement if the condition is true" do
+    i = 0
+    i += 1 until true
+    i.should == 0
+  end
+  
+  it "should return nil if ended when condition became true" do
+    i = 0
+    (i += 1 until i>9).should == nil
+  end
+  
+  it "should return value passed to break if interrupted by break" do
+    (break 123 until false).should == 123
+  end
+  
+  it "should return nil if interrupted by break with no arguments" do
+    (break until false).should == nil
+  end
 
-  it "should run a preceding begin .. end block while the expression is false" do
+end
+
+
+describe "The postfix until expression with preceding begin .. end" do
+  
+  it "should run block while the expression is false" do
     i = 0
     begin
       i += 1
@@ -52,7 +128,25 @@ describe "The until expression" do
     i.should == 10
   end
   
-  it "should run a preceding begin .. end block at least once (even if the expression is true)" do
+  it "should stop running block if interrupted by break" do
+    i = 0
+    begin
+      i += 1
+      break if i > 5
+    end until i > 9
+    
+    i.should == 6
+  end
+  
+  it "should return value passed to break if interrupted by break" do
+    (begin; break 123; end until false).should == 123
+  end
+  
+  it "should return nil if interrupted by break with no arguments" do
+    (begin; break; end until false).should == nil
+  end
+  
+  it "should run block at least once (even if the expression is true)" do
     i = 0
     begin
       i += 1
--- spec/language/expressions/while_spec.rb
+++ spec/language/expressions/while_spec.rb
@@ -23,29 +23,102 @@ describe "The while expression" do
   it "should optionally take a 'do' after the expression" do
     i = 0
     while i < 3 do
-      a = 0
       i += 1
     end
     
-    a.should == 0
     i.should == 3
   end
   
-  it "should have no return value" do
+  it "should allow body begin on the same line if do is used" do
     i = 0
-    val = while i < 3
-      i += 1
-    end.should == nil
-    val.should == nil
+    while i < 3 do i += 1
+    end
+    
+    i.should == 3
   end
   
-  it "should run it's preceding statement while the expression is true" do
+  it "should execute code in containing variable scope" do
+    i = 0
+    while i != 1
+      a = 123
+      i = 1
+    end
+    
+    a.should == 123
+  end
+  
+  it "should execute code in containing variable scope with 'do'" do
+    i = 0
+    while i != 1 do
+      a = 123
+      i = 1
+    end
+    
+    a.should == 123
+  end
+  
+  it "should return nil if ended when condition became false" do
+    i = 0
+    while i < 3
+      i += 1
+    end.should == nil
+  end
+  
+  it "should stop running body if interrupted by break" do
+    i = 0
+    while i < 10
+      i += 1
+      break if i > 5
+    end
+    i.should == 6
+  end
+  
+  it "should return value passed to break if interrupted by break" do
+    while true
+      break 123
+    end.should == 123
+  end
+  
+  it "should return nil if interrupted by break with no arguments" do
+    while true
+      break
+    end.should == nil
+  end
+
+end
+  
+describe "The postfix while expression" do
+  it "should run preceding statement while the condition is true" do
     i = 0
     i += 1 while i < 3
     i.should == 3
   end
   
-  it "should run a preceding begin .. end block while the expression is true" do
+  it "should not run preceding statement if the condition is false" do
+    i = 0
+    i += 1 while false
+    i.should == 0
+  end
+  
+  it "should return nil if ended when condition became false" do
+    i = 0
+    (i += 1 while i<10).should == nil
+  end
+  
+  it "should return value passed to break if interrupted by break" do
+    (break 123 while true).should == 123
+  end
+  
+  it "should return nil if interrupted by break with no arguments" do
+    (break while true).should == nil
+  end
+
+end
+
+
+describe "The postfix while expression with preceding begin .. end" do
+  
+  it "should run block while the expression is true" do
     i = 0
     begin
       i += 1
@@ -54,7 +127,25 @@ describe "The while expression" do
     i.should == 3
   end
   
-  it "should run a preceding begin .. end block at least once (even if the expression is false)" do
+  it "should stop running block if interrupted by break" do
+    i = 0
+    begin
+      i += 1
+      break if i > 5
+    end while i < 10
+    
+    i.should == 6
+  end
+  
+  it "should return value passed to break if interrupted by break" do
+    (begin; break 123; end while true).should == 123
+  end
+  
+  it "should return nil if interrupted by break with no arguments" do
+    (begin; break; end while true).should == nil
+  end
+  
+  it "should run block at least once (even if the expression is false)" do
     i = 0
     begin
       i += 1
