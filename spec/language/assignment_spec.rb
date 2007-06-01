db4775403d57ea29165165b9cbf0110739d91e2a
Wilson Bilkovich <wilson@supremetyrant.com>
2007-06-01 02:26:30 +0000

    * Ticket #128 - Patch by Marcus Crafter to enhance assignment specs

--- spec/language/assignment_spec.rb
+++ spec/language/assignment_spec.rb
@@ -1,8 +1,164 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+describe "Basic assignment" do
+  
+  it "should allow the rhs to be assigned to the lhs" do
+    a = nil;       a.should == nil
+    a = 1;         a.should == 1
+    a = [];        a.should == []
+    a = [1];       a.should == [1]
+    a = [nil];     a.should == [nil]
+    a = [[]];      a.should == [[]]
+    a = [*[]];     a.should == []
+    a = [*[1]];    a.should == [1]
+    a = [*[1,2]];  a.should == [1, 2]
+  end
 
-context 'Single assignment' do
-  specify 'Assignment does not modify the lhs, it reassigns its reference' do
+  it "should allow the assignment of the rhs to the lhs using the rhs splat operator" do
+    a = *nil;      a.should == nil
+    a = *1;        a.should == 1
+   *a = nil;       a.should == [nil]
+    a = *[];       a.should == nil
+    a = *[1];      a.should == 1
+    a = *[nil];    a.should == nil
+    a = *[[]];     a.should == []
+    a = *[*[]];    a.should == nil
+    a = *[*[1]];   a.should == 1
+    a = *[*[1,2]]; a.should == [1,2]
+  end
+
+  it "should allow the assignment of the rhs to the lhs using the lhs splat operator" do
+    *a = 1;        a.should == [1]
+    *a = [];       a.should == [[]]
+
+    *a = [1];      a.should == [[1]]
+    *a = [nil];    a.should == [[nil]]
+    *a = [[]];     a.should == [[[]]]
+    *a = [*[]];    a.should == [[]]
+    *a = [*[1]];   a.should == [[1]]
+    *a = [*[1,2]]; a.should == [[1,2]]
+  end
+
+  it "should allow the assignment of the rhs to the lhs using the lhs and rhs splat operator simultaneously" do
+    *a = *nil;      a.should == [nil]
+    *a = *[nil];    a.should == [nil]
+    *a = *[[]];     a.should == [[]]
+    *a = *1;        a.should == [1]
+    *a = *[];       a.should == []
+    *a = *[1];      a.should == [1]
+    *a = *[*[]];    a.should == []
+    *a = *[*[1]];   a.should == [1]
+    *a = *[*[1,2]]; a.should == [1,2]
+  end
+  
+  it "should allow multiple values to be assigned" do
+    a,b,*c = nil;       [a,b,c].should == [nil, nil, []]
+    a,b,*c = 1;         [a,b,c].should == [1, nil, []]
+    a,b,*c = [];        [a,b,c].should == [nil, nil, []]
+    a,b,*c = [1];       [a,b,c].should == [1, nil, []]
+    a,b,*c = [nil];     [a,b,c].should == [nil, nil, []]
+    a,b,*c = [[]];      [a,b,c].should == [[], nil, []]
+    a,b,*c = [*[]];     [a,b,c].should == [nil, nil, []]
+    a,b,*c = [*[1]];    [a,b,c].should == [1, nil, []]
+    a,b,*c = [*[1,2]];  [a,b,c].should == [1, 2, []]
+    
+    a,b,*c = *nil;      [a,b,c].should == [nil, nil, []]
+    a,b,*c = *1;        [a,b,c].should == [1, nil, []]
+    a,b,*c = *[];       [a,b,c].should == [nil, nil, []]
+    a,b,*c = *[1];      [a,b,c].should == [1, nil, []]
+    a,b,*c = *[nil];    [a,b,c].should == [nil, nil, []]
+
+    a,b,*c = *[[]];     [a,b,c].should == [[], nil, []]
+
+    a,b,*c = *[*[]];    [a,b,c].should == [nil, nil, []]
+    a,b,*c = *[*[1]];   [a,b,c].should == [1, nil, []]
+    a,b,*c = *[*[1,2]]; [a,b,c].should == [1, 2, []]
+  end
+  
+end
+
+describe "Assignment using expansion" do
+  
+  it "should succeed without conversion" do
+    *x = (1..7).to_a
+    x.should == [[1, 2, 3, 4, 5, 6, 7]]
+  end
+  
+end
+
+describe "Assigning multiple values" do
+  
+  it "should allow parallel assignment" do
+    a, b = 1, 2
+    a.should == 1
+    b.should == 2
+
+    a, = 1,2
+    a.should == 1
+  end
+  
+  it "should allow safe parallel swapping" do
+    a, b = 1, 2
+    a, b = b, a
+    a.should == 2
+    b.should == 1
+  end
+
+  it "should bundle remaining values to an array when using the splat operator" do
+    a, *b = 1, 2, 3
+    a.should == 1
+    b.should == [2, 3]
+    
+    *a = 1, 2, 3
+    a.should == [1, 2, 3]
+    
+    *a = 4
+    a.should == [4]
+    
+    *a = nil
+    a.should == [nil]
+  end
+    
+  it "should allow complex parallel assignment" do
+    a, (b, c), d = 1, [2, 3], 4
+    a.should == 1
+    b.should == 2
+    c.should == 3
+    d.should == 4
+  end
+  
+end
+
+describe "Conditional assignment" do
+
+  it "should assign the lhs if previously unassigned" do
+    a=[]
+    a[0] ||= "bar"
+    a[0].should == "bar"
+
+    h={}
+    h["foo"] ||= "bar"
+    h["foo"].should == "bar"
+    
+    aa = 5
+    aa ||= 25
+    aa.should == 5
+
+    bb ||= 25
+    bb.should == 25
+
+    cc &&=33
+    cc.should == nil
+
+    cc = 5
+    cc &&=44
+    cc.should == 44
+  end
+  
+end
+
+describe 'Single assignment' do
+  it 'Assignment does not modify the lhs, it reassigns its reference' do
     a = 'Foobar'
     b = a
     b = 'Bazquux'
@@ -10,21 +166,21 @@ context 'Single assignment' do
     b.should == 'Bazquux'
   end
 
-  specify 'Assignment does not copy the object being assigned, just creates a new reference to it' do
+  it 'Assignment does not copy the object being assigned, just creates a new reference to it' do
     a = []
     b = a
     b << 1
     a.should == [1]
   end
 
-  specify 'If rhs has multiple arguments, lhs becomes an Array of them' do
+  it 'If rhs has multiple arguments, lhs becomes an Array of them' do
     a = 1, 2, 3
     a.should == [1, 2, 3]
   end
 end
 
-context 'Multiple assignment without grouping or splatting' do
-  specify 'An equal number of arguments on lhs and rhs assigns positionally' do
+describe 'Multiple assignment without grouping or splatting' do
+  it 'An equal number of arguments on lhs and rhs assigns positionally' do
     a, b, c, d = 1, 2, 3, 4
     a.should == 1
     b.should == 2
@@ -32,20 +188,20 @@ context 'Multiple assignment without grouping or splatting' do
     d.should == 4
   end 
 
-  specify 'If rhs has too few arguments, the missing ones on lhs are assigned nil' do
+  it 'If rhs has too few arguments, the missing ones on lhs are assigned nil' do
     a, b, c = 1, 2
     a.should == 1
     b.should == 2
     c.should == nil
   end
 
-  specify 'If rhs has too many arguments, the extra ones are silently not assigned anywhere' do
+  it 'If rhs has too many arguments, the extra ones are silently not assigned anywhere' do
     a, b = 1, 2, 3
     a.should == 1
     b.should == 2
   end
 
-  specify 'The assignments are done in parallel so that lhs and rhs are independent of eachother without copying' do
+  it 'The assignments are done in parallel so that lhs and rhs are independent of eachother without copying' do
     o_of_a, o_of_b = Object.new, Object.new
     a, b = o_of_a, o_of_b
     a, b = b, a
@@ -54,12 +210,12 @@ context 'Multiple assignment without grouping or splatting' do
   end
 end
 
-context 'Multiple assignments with splats' do
-  specify '* on the lhs has to be applied to the last parameter' do
+describe 'Multiple assignments with splats' do
+  it '* on the lhs has to be applied to the last parameter' do
     should_raise(SyntaxError) { eval 'a, *b, c = 1, 2, 3' }
   end
 
-  specify '* on the lhs collects all parameters from its position onwards as an Array or an empty Array' do
+  it '* on the lhs collects all parameters from its position onwards as an Array or an empty Array' do
     a, *b = 1, 2
     c, *d = 1
     e, *f = 1, 2, 3
@@ -82,8 +238,8 @@ context 'Multiple assignments with splats' do
   end
 end
 
-context 'Multiple assignments with grouping' do
-  specify 'A group on the lhs is considered one position and treats its corresponding rhs position like an Array' do
+describe 'Multiple assignments with grouping' do
+  it 'A group on the lhs is considered one position and treats its corresponding rhs position like an Array' do
     a, (b, c), d = 1, 2, 3, 4
     e, (f, g), h = 1, [2, 3, 4], 5
     i, (j, k), l = 1, 2, 3
@@ -101,7 +257,7 @@ context 'Multiple assignments with grouping' do
     l.should == 3
   end
 
-  specify 'rhs cannot use parameter grouping, it is a syntax error' do
+  it 'rhs cannot use parameter grouping, it is a syntax error' do
     should_raise(SyntaxError) { eval '(a, b) = (1, 2)' }
   end
 end
