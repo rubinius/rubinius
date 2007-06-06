7311fdf31481ba7a4373d5b007efb62c1f84c389
Tilman <tilman@unknown>
2007-06-06 20:03:30 +0000

    Hardcode output for Time.gm(...).inspect. This makes utctime superfluous for now.

--- spec/core/time_spec.rb
+++ spec/core/time_spec.rb
@@ -23,16 +23,6 @@ context "Time class method" do
       `date -j -f "%s" #{seconds} "+%a %b %d %H:%M:%S %z %Y"`.chomp
     end
   end
-  
-  def utctime(seconds)
-    if RUBY_PLATFORM =~ /darwin/
-      `date -u -r #{seconds} +'%a %b %d %H:%M:%S %z %Y'`.chomp
-    elsif RUBY_PLATFORM =~ /linux/
-      `date -u -d @#{seconds} +'%a %b %d %H:%M:%S %z %Y'`.chomp
-    else
-      `date -u -j -f "%s" #{seconds} "+%a %b %d %H:%M:%S %z %Y"`.chomp
-    end
-  end
 
   def with_timezone(zone)
     old = ENV["TZ"]
@@ -64,11 +54,11 @@ context "Time class method" do
   # gm/utc
   
   specify "gm should create a time based on given values, interpreted as UTC (GMT)" do
-    Time.gm(2000,"jan",1,20,15,1).inspect.should == utctime(946757701)
+    Time.gm(2000,"jan",1,20,15,1).inspect.should == "Sat Jan 01 20:15:01 UTC 2000"
   end
   
   specify "gm should create a time based on given C-style gmtime arguments, interpreted as UTC (GMT)" do
-    Time.gm(1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored).inspect.should == utctime(946757701)
+    Time.gm(1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored).inspect.should == "Sat Jan 01 20:15:01 UTC 2000"
   end
   
   specify "utc is a synonym for Time.gm" do
