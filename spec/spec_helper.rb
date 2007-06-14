27b2767cd21b5a69ee10a2a629a68de64fd8ae5a
Florian Gross <florgro@gmail.com>
2007-06-14 18:03:26 +0000

    Introducing RUBY_NAME so we can spawn a new Ruby for things that can't be tested otherwise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,12 @@ end
 def rubinius_examples
   Object.const_defined?(:RUBY_ENGINE) and RUBY_ENGINE == 'rbx'
 end
+
+if !defined?(RUBY_NAME) then
+  begin
+    require 'rbconfig'
+    RUBY_NAME = Config::CONFIG["RUBY_INSTALL_NAME"]
+  rescue Exception
+    RUBY_NAME = RUBY_ENGINE
+  end
+end
