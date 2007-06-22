ecaf1abafeb69994b05463742ca4220797f62ad3
Eero Saynatkari <eero@kittensoft.org>
2007-06-22 23:25:13 +0000

    * Array specs by Josh Susser (hasmanyjosh)

--- spec/core/array_spec.rb
+++ spec/core/array_spec.rb
@@ -102,16 +102,17 @@ context "Array instance method" do
     ([] & []).should == []
     ([1, 2] & []).should == []
     ([] & [1, 2]).should == []
-    ([ 1, 1, 3, 5 ] & [ 1, 2, 3 ]).should == [1, 3]
+    ([ 1, 3, 5 ] & [ 1, 2, 3 ]).should == [1, 3]
   end
   
   specify "& should create an array with no duplicates" do
-    ([] | []).should == []
-    ([1, 2] | []).should == [1, 2]
-    ([] | [1, 2]).should == [1, 2]
     ([ 1, 1, 3, 5 ] & [ 1, 2, 3 ]).uniq!.should == nil
   end
   
+  specify "& should create an array with elements in order they are first encountered" do
+    ([ 1, 2, 3, 2, 5 ] & [ 5, 2, 3, 4 ]).should == [2, 3, 5]
+  end
+
   specify "& does not modify the original Array" do
     a = [1, 1, 3, 5]
     a & [1, 2, 3]
@@ -141,8 +142,19 @@ context "Array instance method" do
     ([5, 6] & ToAryArray[1, 2, 5, 6]).should == [5, 6]
   end
   
-  specify "| should return an array of elements that appear in either array (union) without duplicates" do
-    ([1, 2, 3] | [1, 2, 3, 4, 5]).should == [1, 2, 3, 4, 5]
+  specify "| should return an array of elements that appear in either array (union)" do
+    ([] | []).should == []
+    ([1, 2] | []).should == [1, 2]
+    ([] | [1, 2]).should == [1, 2]
+    ([ 1, 2, 3, 4 ] | [ 3, 4, 5 ]).should == [1, 2, 3, 4, 5]
+  end
+  
+  specify "| should create an array with no duplicates" do
+    ([ 1, 2, 3, 1, 4, 5 ] | [ 1, 3, 4, 5, 3, 6 ]).should == [1, 2, 3, 4, 5, 6]
+  end
+  
+  specify "| should create an array with elements in order they are first encountered" do
+    ([ 1, 2, 3, 1 ] | [ 1, 3, 4, 5 ]).should == [1, 2, 3, 4, 5]
   end
 
   specify "| should call to_ary on its argument" do
