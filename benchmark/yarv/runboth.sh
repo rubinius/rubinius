52a2407b72a8206d5cd4212603ce22af462d51b7
Graham <graham@unknown>
2007-06-27 22:48:48 +0000

    Adding a ruby-native version of /usr/bin/time, as the version of that program that actually lets you format output is not at all portable.

--- benchmark/yarv/runboth.sh
+++ benchmark/yarv/runboth.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-TIME="%e"
 CMD1=ruby
 CMD2=rbx
 ulimit -v 262144
@@ -14,11 +13,11 @@ echo "Test	$CMD1	$CMD2"
 for FILE in $FILES
 do
 	BASE=`echo $FILE | awk -F '.' '{ print $1 }'`
-	RUBYTIME=`/usr/bin/time -f "$TIME" sh -c "ruby $FILE > $FILE.out.ruby" 2>&1`
+	RUBYTIME=`./time.rb sh -c "ruby $FILE > $FILE.out.ruby" 2>&1`
 	if [ $? != 0 ]; then
 	 RUBYTIME='Error'
 	fi
-	RBXTIME=`/usr/bin/time -f "$TIME" sh -c "rbx $FILE > $FILE.out.rbx" 2>&1`
+	RBXTIME=`./time.rb sh -c "rbx $FILE > $FILE.out.rbx" 2>&1`
 	if [ $? != 0 ]; then
 	 RBXTIME='Error'
 	fi
