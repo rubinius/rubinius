7d3dcc24cb72d6548cf44d8519691f4cd7344801
Florian Gross <florgro@gmail.com>
2007-07-01 21:00:37 +0000

    Heavily extended and refactored String#% specs; some cleanup

--- spec/core/string_spec.rb
+++ spec/core/string_spec.rb
@@ -15,43 +15,117 @@ class MyString < String; end
 class MyArray < Array; end
 
 describe "String#%(Object)" do
-  it "ignores unused arguments" do
-    ("" % [1, 2, 3]).should == ""
-    ("%s" % [1, 2, 3]).should == "1"
-    ("%2$s" % [1, 2, 3]).should == "2"
+  it "formats multiple expressions" do
+    ("%b %x %d %s" % [10, 10, 10, 10]).should == "1010 a 10 10"
   end
   
-  it "ignores trailing percent signs" do
-    ("%" % "x").should == "%"
-    ("foo%" % "x").should == "foo%"
+  it "formats expressions mid string" do
+    ("hello %s!" % "world").should == "hello world!"
+  end
+  
+  it "formats %% into %" do
+    ("%d%% %s" % [10, "of chickens!"]).should == "10% of chickens!"
   end
 
-  it "replaces trailing absolute argument specifier with percent sign" do
+  it "ignores unused arguments when $DEBUG is false" do
+    begin
+      old_debug = $DEBUG
+      $DEBUG = false
+
+      ("" % [1, 2, 3]).should == ""
+      ("%s" % [1, 2, 3]).should == "1"
+    ensure
+      $DEBUG = old_debug
+    end
+  end
+
+  it "raises ArgumentError for unused arguments when $DEBUG is true" do
+    begin
+      old_debug = $DEBUG
+      $DEBUG = true
+
+      should_raise(ArgumentError) { "" % [1, 2, 3] }
+      should_raise(ArgumentError) { "%s" % [1, 2, 3] }
+    ensure
+      $DEBUG = old_debug
+    end
+  end
+  
+  it "always allows unused arguments when positional argument style is used" do
+    begin
+      old_debug = $DEBUG
+
+      $DEBUG = false
+      ("%2$s" % [1, 2, 3]).should == "2"
+      $DEBUG = true
+      ("%2$s" % [1, 2, 3]).should == "2"
+    ensure
+      $DEBUG = old_debug
+    end
+  end
+  
+  it "ignores percent signs at end of string / before newlines" do
+    ("%" % []).should == "%"
+    ("foo%" % []).should == "foo%"
+  end
+  
+  it "replaces percent sign followed by null byte with a percent sign" do
+    ("%\0x hello" % []).should == "%x hello"
+  end
+
+  it "replaces trailing absolute argument specifier without type with percent sign" do
     ("hello %1$" % "foo").should == "hello %"
   end
   
-  it "raises when given invalid argument specifiers" do
-    should_raise(ArgumentError) { "%1" % "x" }
-    should_raise(ArgumentError) { "%+" % "x" }
-    should_raise(ArgumentError) { "%-" % "x" }
-    should_raise(ArgumentError) { "%#" % "x" }
-    should_raise(ArgumentError) { "%0" % "x" }
-    should_raise(ArgumentError) { "%*" % "x" }
-    should_raise(ArgumentError) { "%_" % "x" }
+  it "raises an ArgumentError when given invalid argument specifiers" do
+    should_raise(ArgumentError) { "%1" % [] }
+    should_raise(ArgumentError) { "%+" % [] }
+    should_raise(ArgumentError) { "%-" % [] }
+    should_raise(ArgumentError) { "%#" % [] }
+    should_raise(ArgumentError) { "%0" % [] }
+    should_raise(ArgumentError) { "%*" % [] }
+    should_raise(ArgumentError) { "%." % [] }
+    should_raise(ArgumentError) { "%_" % [] }
+    should_raise(ArgumentError) { "%0$s" % "x" }
+    should_raise(ArgumentError) { "%*0$s" % [5, "x"] }
+    should_raise(ArgumentError) { "%*1$.*0$1$s" % [1, 2, 3] }
   end
-    
-  it "raises when there are less arguments than format specifiers" do
+
+  it "raises an ArgumentError when multiple positional argument tokens are given for one format specifier" do
+    should_raise(ArgumentError) { "%1$1$s" % "foo" }
+  end
+
+  it "raises an ArgumentError when multiple width star tokens are given for one format specifier" do
+    should_raise(ArgumentError) { "%**s" % [5, 5, 5] }
+  end
+
+  it "raises an ArgumentError when a width star token is seen after a width token" do
+    should_raise(ArgumentError) { "%5*s" % [5, 5] }
+  end
+
+  it "raises an ArgumentError when multiple precision tokens are given" do
+    should_raise(ArgumentError) { "%.5.5s" % 5 }
+    should_raise(ArgumentError) { "%.5.*s" % [5, 5] }
+    should_raise(ArgumentError) { "%.*.5s" % [5, 5] }
+  end
+  
+  it "raises an ArgumentError when there are less arguments than format specifiers" do
     ("foo" % []).should == "foo"
     should_raise(ArgumentError) { "%s" % [] }
     should_raise(ArgumentError) { "%s %s" % [1] }
   end
   
-  it "raises when absolute and relative argument numbers are mixed" do
+  it "raises an ArgumentError when absolute and relative argument numbers are mixed" do
     should_raise(ArgumentError) { "%s %1$s" % "foo" }
     should_raise(ArgumentError) { "%1$s %s" % "foo" }
 
     should_raise(ArgumentError) { "%s %2$s" % ["foo", "bar"] }
     should_raise(ArgumentError) { "%2$s %s" % ["foo", "bar"] }
+
+    should_raise(ArgumentError) { "%*2$s" % [5, 5, 5] }
+    should_raise(ArgumentError) { "%*.*2$s" % [5, 5, 5] }
+    should_raise(ArgumentError) { "%*2$.*2$s" % [5, 5, 5] }
+    should_raise(ArgumentError) { "%*.*2$s" % [5, 5, 5] }
   end
   
   it "allows reuse of the one argument multiple via absolute argument numbers" do
@@ -71,6 +145,19 @@ describe "String#%(Object)" do
     ("%p %p" % MyArray[1, 2]).should == "1 2"
   end
   
+  it "allows positional arguments for width star and precision star arguments" do
+    ("%*1$.*2$3$d" % [10, 5, 1]).should == "     00001"
+  end
+  
+  it "calls to_int on width star and precision star tokens" do
+    w = Object.new
+    def w.to_int() 10 end
+    p = Object.new
+    def p.to_int() 5 end
+    
+    ("%*.*f" % [w, p, 1]).should == "   1.00000"
+  end
+  
   it "doesn't call to_ary on its argument" do
     obj = Object.new
     def obj.to_ary() [1, 2] end
@@ -78,19 +165,7 @@ describe "String#%(Object)" do
     should_raise(ArgumentError) { "%s %s" % obj }
     ("%s" % obj).should == "obj"
   end
-  
-  it "formats multiple expressions" do
-    ("%b %x %d %s" % [10, 10, 10, 10]).should == "1010 a 10 10"
-  end
-  
-  it "formats expressions mid string" do
-    ("hello %s!" % "world").should == "hello world!"
-  end
-  
-  it "formats %% into %" do
-    ("%d%% %s" % [10, "of chickens!"]).should == "10% of chickens!"
-  end
-  
+
   it "supports binary formats using %b" do
     ("%b" % 10).should == "1010"
     ("% b" % 10).should == " 1010"
@@ -100,6 +175,39 @@ describe "String#%(Object)" do
     ("%-9b" % 10).should == "1010     "
     ("%05b" % 10).should == "01010"
     ("%*b" % [10, 6]).should == "       110"
+    ("%*b" % [-10, 6]).should == "110       "
+    
+    ("%b" % -5).should == "..1011"
+    ("%0b" % -5).should == "1011"
+    ("%.1b" % -5).should == "1011"
+    ("%.7b" % -5).should == "1111011"
+    ("%.10b" % -5).should == "1111111011"
+    ("% b" % -5).should == "-101"
+    ("%+b" % -5).should == "-101"
+    ("%b" % -(2 ** 64 + 5)).should ==
+    "..101111111111111111111111111111111111111111111111111111111111111011"
+  end
+  
+  it "supports binary formats using %B with same behaviour as %b except for using 0B instead of 0b for #" do
+    ("%B" % 10).should == ("%b" % 10)
+    ("% B" % 10).should == ("% b" % 10)
+    ("%1$B" % [10, 20]).should == ("%1$b" % [10, 20])
+    ("%+B" % 10).should == ("%+b" % 10)
+    ("%-9B" % 10).should == ("%-9b" % 10)
+    ("%05B" % 10).should == ("%05b" % 10)
+    ("%*B" % [10, 6]).should == ("%*b" % [10, 6])
+    ("%*B" % [-10, 6]).should == ("%*b" % [-10, 6])
+
+    ("%B" % -5).should == ("%b" % -5)
+    ("%0B" % -5).should == ("%0b" % -5)
+    ("%.1B" % -5).should == ("%.1b" % -5)
+    ("%.7B" % -5).should == ("%.7b" % -5)
+    ("%.10B" % -5).should == ("%.10b" % -5)
+    ("% B" % -5).should == ("% b" % -5)
+    ("%+B" % -5).should == ("%+b" % -5)
+    ("%B" % -(2 ** 64 + 5)).should == ("%b" % -(2 ** 64 + 5))
+
+    ("%#B" % 10).should == "0B1010"
   end
     
   it "supports character formats using %c" do
@@ -107,6 +215,7 @@ describe "String#%(Object)" do
     ("%2$c" % [10, 11, 14]).should == "\v"
     ("%-4c" % 10).should == "\n   "
     ("%*c" % [10, 3]).should == "         \003"
+    ("%c" % (256 + 42)).should == "*"
     
     should_raise(TypeError) { "%c" % Object }
     
@@ -194,6 +303,15 @@ describe "String#%(Object)" do
     ("%-9o" % 10).should == "12       "
     ("%05o" % 10).should == "00012"
     ("%*o" % [10, 6]).should == "         6"
+
+    ("%o" % -5).should == "..73"
+    ("%0o" % -5).should == "73"
+    ("%.1o" % -5).should == "73"
+    ("%.7o" % -5).should == "7777773"
+    ("%.10o" % -5).should == "7777777773"
+    ("% o" % -26).should == "-32"
+    ("%+o" % -26).should == "-32"
+    ("%o" % -(2 ** 64 + 5)).should == "..75777777777777777777773"
   end
   
   it "supports inspect formats using %p" do
@@ -217,6 +335,15 @@ describe "String#%(Object)" do
     obj.should_receive(:to_s, :returning => "obj")
     ("%s" % obj).should == "obj"
   end
+
+  # MRI crashes on this one.
+  # See http://groups.google.com/group/ruby-core-google/t/c285c18cd94c216d
+  failure :mri do
+    it "ignores huge precisions for %s" do
+      ("%.25555555555555555555555555555555555555s" % "hello world").should ==
+      "hello world"
+    end
+  end
   
   it "supports unsigned formats using %u" do
     ("%u" % 10).should == "10"
@@ -227,18 +354,14 @@ describe "String#%(Object)" do
     ("%04u" % 10).should == "0010"
     ("%*u" % [10, 4]).should == "         4"
     
-    ("%u" % -431647880).should == "..3863319416"
-  end
-  
-  it "supports hex formats using %X" do
-    ("%X" % 10).should == "A"
-    ("% X" % 10).should == " A"
-    ("%1$X" % [10, 20]).should == "A"
-    ("%#X" % 10).should == "0XA"
-    ("%+X" % 10).should == "+A"
-    ("%-9X" % 10).should == "A        "
-    ("%05X" % 10).should == "0000A"
-    ("%*X" % [10, 6]).should == "         6"
+    ("%u" % -5).should == "..4294967291"
+    ("%0u" % -5).should == "4294967291"
+    ("%.1u" % -5).should == "4294967291"
+    ("%.7u" % -5).should == "4294967291"
+    ("%.10u" % -5).should == "4294967291"
+    ("% u" % -26).should == "-26"
+    ("%+u" % -26).should == "-26"
+    ("%u" % -(2 ** 64 + 5)).should == "-79228162495817593519834398715"
   end
   
   it "supports hex formats using %x" do
@@ -250,6 +373,35 @@ describe "String#%(Object)" do
     ("%-9x" % 10).should == "a        "
     ("%05x" % 10).should == "0000a"
     ("%*x" % [10, 6]).should == "         6"
+
+    ("%x" % -5).should == "..fb"
+    ("%0x" % -5).should == "fb"
+    ("%.1x" % -5).should == "fb"
+    ("%.7x" % -5).should == "ffffffb"
+    ("%.10x" % -5).should == "fffffffffb"
+    ("% x" % -26).should == "-1a"
+    ("%+x" % -26).should == "-1a"
+    ("%x" % -(2 ** 64 + 5)).should == "..fefffffffffffffffb"
+  end
+  
+  it "supports hex formats using %X" do
+    ("%X" % 10).should == "A"
+    ("% X" % 10).should == " A"
+    ("%1$X" % [10, 20]).should == "A"
+    ("%#X" % 10).should == "0XA"
+    ("%+X" % 10).should == "+A"
+    ("%-9X" % 10).should == "A        "
+    ("%05X" % 10).should == "0000A"
+    ("%*X" % [10, 6]).should == "         6"
+    
+    ("%X" % -5).should == "..FB"
+    ("%0X" % -5).should == "FB"
+    ("%.1X" % -5).should == "FB"
+    ("%.7X" % -5).should == "FFFFFFB"
+    ("%.10X" % -5).should == "FFFFFFFFFB"
+    ("% X" % -26).should == "-1A"
+    ("%+X" % -26).should == "-1A"
+    ("%X" % -(2 ** 64 + 5)).should == "..FEFFFFFFFFFFFFFFFB"
   end
   
   %w(b d i o u X x).each do |f|
@@ -938,6 +1090,7 @@ describe "String#downcase!" do
 end
 
 describe "String#dump" do
+  # Older versions of MRI wrongly print \b as \010
   it "should produce a version of self with all nonprinting charaters replaced by \\nnn notation" do
     ("\000".."A").to_a.to_s.dump.should == "\"\\000\\001\\002\\003\\004\\005\\006\\a\\b\\t\\n\\v\\f\\r\\016\\017\\020\\021\\022\\023\\024\\025\\026\\027\\030\\031\\032\\e\\034\\035\\036\\037 !\\\"\\\#$%&'()*+,-./0123456789\""
   end
@@ -945,13 +1098,15 @@ describe "String#dump" do
   it "should ignore the $KCODE setting" do
     old_kcode = $KCODE
 
-    $KCODE = "NONE"
-    "äöü".dump.should == "\"\\303\\244\\303\\266\\303\\274\""
+    begin
+      $KCODE = "NONE"
+      "äöü".dump.should == "\"\\303\\244\\303\\266\\303\\274\""
 
-    $KCODE = "UTF-8"
-    "äöü".dump.should == "\"\\303\\244\\303\\266\\303\\274\""
-    
-    $KCODE = old_kcode
+      $KCODE = "UTF-8"
+      "äöü".dump.should == "\"\\303\\244\\303\\266\\303\\274\""
+    ensure
+      $KCODE = old_kcode
+    end
   end
 end
 
@@ -1328,20 +1483,23 @@ describe "String#insert(index, other)" do
 end
 
 describe "String#inspect" do
+  # Older versions of MRI wrongly print \b as \010
   it "should produce a version of self with all nonprinting charaters replaced by \\nnn notation" do
-    ("\000".."A").to_a.to_s.dump.should == "\"\\000\\001\\002\\003\\004\\005\\006\\a\\b\\t\\n\\v\\f\\r\\016\\017\\020\\021\\022\\023\\024\\025\\026\\027\\030\\031\\032\\e\\034\\035\\036\\037 !\\\"\\\#$%&'()*+,-./0123456789\""
+    ("\000".."A").to_a.to_s.inspect.should == "\"\\000\\001\\002\\003\\004\\005\\006\\a\\b\\t\\n\\v\\f\\r\\016\\017\\020\\021\\022\\023\\024\\025\\026\\027\\030\\031\\032\\e\\034\\035\\036\\037 !\\\"\\\#$%&'()*+,-./0123456789\""
   end
   
   it "should produce different output based on $KCODE" do
     old_kcode = $KCODE
 
-    $KCODE = "NONE"
-    "äöü".inspect.should == "\"\\303\\244\\303\\266\\303\\274\""
+    begin
+      $KCODE = "NONE"
+      "äöü".inspect.should == "\"\\303\\244\\303\\266\\303\\274\""
 
-    $KCODE = "UTF-8"
-    "äöü".inspect.should == "\"äöü\""
-    
-    $KCODE = old_kcode
+      $KCODE = "UTF-8"
+      "äöü".inspect.should == "\"äöü\""
+    ensure
+      $KCODE = old_kcode
+    end
   end
 end
 
@@ -1777,7 +1935,6 @@ describe "String#rindex(regexp [, start_offset])" do
         str.rindex(regexp).should == str.rindex(needle)
         
         0.upto(str.size + 1) do |start|
-          p [str, regexp, needle, start]
           str.rindex(regexp, start).should == str.rindex(needle, start)
         end
         
