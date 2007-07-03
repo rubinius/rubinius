895f1abdc0bfcdb213f97067704b1bb87a7e6d17
Florian Gross <florgro@gmail.com>
2007-07-03 17:01:27 +0000

    New specs for casecmp

--- spec/core/string_spec.rb
+++ spec/core/string_spec.rb
@@ -361,7 +361,7 @@ describe "String#%(Object)" do
     ("%.10u" % -5).should == "4294967291"
     ("% u" % -26).should == "-26"
     ("%+u" % -26).should == "-26"
-    ("%u" % -(2 ** 64 + 5)).should == "-79228162495817593519834398715"
+    ("%u" % -(2 ** 64 + 5)).should == "..79228162495817593519834398715"
   end
   
   it "supports hex formats using %x" do
@@ -1095,6 +1095,12 @@ describe "String#capitalize" do
     "HELLO".capitalize.should == "Hello"
     "123ABC".capitalize.should == "123abc"
   end
+
+  it "is locale insensitive (only upcases a-z and only downcases A-Z)" do
+    "ÄÖÜ".capitalize.should == "ÄÖÜ"
+    "ärger".capitalize.should == "ärger"
+    "BÄR".capitalize.should == "BÄr"
+  end
 end
 
 describe "String#capitalize!" do
@@ -1126,6 +1132,55 @@ describe "String#casecmp" do
     "aBcDeF".casecmp("abcdef").should == 0
     "abcdef".casecmp("abcdefg").should == -1
     "abcdef".casecmp("ABCDEF").should == 0
+  end
+  
+  # Broken in MRI 1.8.4
+  it "doesn't consider non-ascii characters equal that aren't" do
+    # -- Latin-1 --
+    upper_a_tilde  = "\xC3"
+    upper_a_umlaut = "\xC4"
+    lower_a_tilde  = "\xE3"
+    lower_a_umlaut = "\xE4"
+
+    lower_a_tilde.casecmp(lower_a_umlaut).should_not == 0
+    lower_a_umlaut.casecmp(lower_a_tilde).should_not == 0
+    upper_a_tilde.casecmp(upper_a_umlaut).should_not == 0
+    upper_a_umlaut.casecmp(upper_a_tilde).should_not == 0
+    
+    # -- UTF-8 --
+    upper_a_tilde  = "\xC3\x83"
+    upper_a_umlaut = "\xC3\x84"
+    lower_a_tilde  = "\xC3\xA3"
+    lower_a_umlaut = "\xC3\xA4"
+    
+    lower_a_tilde.casecmp(lower_a_umlaut).should_not == 0
+    lower_a_umlaut.casecmp(lower_a_tilde).should_not == 0
+    upper_a_tilde.casecmp(upper_a_umlaut).should_not == 0
+    upper_a_umlaut.casecmp(upper_a_tilde).should_not == 0
+  end
+  
+  it "doesn't do case mapping for non-ascii characters" do
+    # -- Latin-1 --
+    upper_a_tilde  = "\xC3"
+    upper_a_umlaut = "\xC4"
+    lower_a_tilde  = "\xE3"
+    lower_a_umlaut = "\xE4"
+    
+    upper_a_tilde.casecmp(lower_a_tilde).should == -1
+    upper_a_umlaut.casecmp(lower_a_umlaut).should == -1
+    lower_a_tilde.casecmp(upper_a_tilde).should == 1
+    lower_a_umlaut.casecmp(upper_a_umlaut).should == 1
+
+    # -- UTF-8 --
+    upper_a_tilde  = "\xC3\x83"
+    upper_a_umlaut = "\xC3\x84"
+    lower_a_tilde  = "\xC3\xA3"
+    lower_a_umlaut = "\xC3\xA4"
+
+    upper_a_tilde.casecmp(lower_a_tilde).should == -1
+    upper_a_umlaut.casecmp(lower_a_umlaut).should == -1
+    lower_a_tilde.casecmp(upper_a_tilde).should == 1
+    lower_a_umlaut.casecmp(upper_a_umlaut).should == 1
   end
   
   it "ignores subclass differences" do
@@ -1352,7 +1407,7 @@ describe "String#delete!([other_strings])" do
 end
 
 describe "String#downcase" do
-  it "returns a copy of self with all uppercase letters downcase'd" do
+  it "returns a copy of self with all uppercase letters downcased" do
     "hELLO".downcase.should == "hello"
     "hello".downcase.should == "hello"
   end
