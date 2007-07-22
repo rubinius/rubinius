d0536ef1bdf59a4c50edb7019d16d27ed9988b95
Florian Gross <florgro@gmail.com>
2007-07-22 13:50:56 +0000

    Add dummy Module#ivar_as_index; Always load rubinius' Array implementation

--- hashi/hashi.rb
+++ hashi/hashi.rb
@@ -1,3 +1,26 @@
 $:.unshift File.dirname(__FILE__)
 require 'exception'
 require 'tuple'
+
+class Module
+  def ivar_as_index(hash)
+    # do nothing
+  end
+end
+
+# -- special logic for Array --
+
+class Array
+  alias :old_inspect :inspect
+  
+  def inspect()
+    "RUBY_LITERAL" + old_inspect
+  end
+end
+
+class Object
+  remove_const :Array
+end
+
+require 'kernel/bootstrap/array'
+require 'kernel/core/array'
