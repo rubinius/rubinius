3d960a021cb9ac2bdc2a204f94b4f024f3ef60a4
Brian Ford <brixen@gmail.com>
2007-08-04 22:06:12 +0000

    fixed completeness to pass correct spec example string for class methods. changed float specs to describe per method.

--- spec/core/float_spec.rb
+++ spec/core/float_spec.rb
@@ -52,58 +52,109 @@ describe "Float#CONSTANTS" do
   end
 end
 
-context "Float" do
-  specify "% should return self modulo other" do
-    (6543.21 % 137).should_be_close(104.21, TOLERANCE)
-    (5667.19 % 0xffffffff).should_be_close(5667.19,TOLERANCE)
-    (6543.21 % 137.24).should_be_close(92.9299999999996, TOLERANCE)
+describe "Float.induced_from" do
+  it "returns the argument when passed a Float" do
+    Float.induced_from(5.5).eql?(5.5).should == true
   end
-  
-  specify "% should NOT raise ZeroDivisionError if other is zero" do
-    (1.0 % 0).to_s.should == 'NaN'
-    (1.0 % 0.0).to_s.should == 'NaN'
+
+  it "returns a Float of the correct value when sent a Fixnum" do
+    Float.induced_from(36).should == 36.0
   end
-  
-  specify "* should return self multiplied by other" do 
+
+  it "returns a Float of the correct value when sent a Bignum" do
+    Float.induced_from(23472398472349872349872349872348972348972439423).to_s.should == '2.34723984723499e+46'
+  end
+
+  it "raises a TypeError if the argument is not a Float, Fixnum, or Bignum" do
+    class Foo
+      def to_f; 9.9; end
+    end
+    should_raise(TypeError) { Float.induced_from(Foo.new) }
+  end
+end 
+
+float_modulo = shared "Float#% | Float#modulo" do |cmd|
+  describe "Float##{cmd}" do
+    it "returns self modulo other" do
+      6543.21.send(cmd, 137).should_be_close(104.21, TOLERANCE)
+      5667.19.send(cmd, 0xffffffff).should_be_close(5667.19, TOLERANCE)
+      6543.21.send(cmd, 137.24).should_be_close(92.9299999999996, TOLERANCE)
+
+      6543.21.send(cmd, 137).should_be_close(6543.21.%(137), TOLERANCE)
+      5667.19.send(cmd, 0xffffffff).should_be_close(5667.19.%(0xffffffff), TOLERANCE)
+      6543.21.send(cmd, 137.24).should_be_close(6543.21.%(137.24), TOLERANCE)
+    end
+
+    it "does NOT raise ZeroDivisionError if other is zero" do
+      1.0.send(cmd, 0).to_s.should == 'NaN'
+      1.0.send(cmd, 0.0).to_s.should == 'NaN'
+    end
+  end
+end
+
+describe "Float#%" do
+  it_behaves_like(float_modulo, :%)
+end
+
+describe "Float#*" do
+  it "returns self multiplied by other" do 
     (4923.98221 * 2).should_be_close(9847.96442, TOLERANCE) 
     (6712.5 * 0.25).should_be_close(1678.125, TOLERANCE) 
     (256.4096 * 0xffffffff).to_s.should == 1101270846124.03.to_s
   end
-  
-  specify "** should return self raise to the other power" do
+end
+
+describe "Float#**" do
+  it "returns self raise to the other power" do
     (2.3 ** 3).should_be_close(12.167,TOLERANCE)
     (5.2 ** -1).should_be_close(0.192307692307692,TOLERANCE)
     (9.5 ** 0.5).should_be_close(3.08220700148449, TOLERANCE) 
     (9.5 ** 0xffffffff).to_s.should == 'Infinity'
   end
-  
-  specify "+ should return self plus other" do
+end
+
+describe "Float#+" do
+  it "returns self plus other" do
     (491.213 + 2).should_be_close(493.213, TOLERANCE)
     (9.99 + 0xffffffff).should_be_close(4294967304.99,TOLERANCE)
     (1001.99 + 5.219).should_be_close(1007.209, TOLERANCE)
   end
+end
 
-  specify "- should return self minus other" do
+describe "Float#+@" do 
+  it "returns the same value with same sign (twos complement)" do 
+    34.56.send(:+@).should == 34.56
+    -34.56.send(:+@).should == -34.56
+    0.0.send(:+@).should == 0.0
+  end  
+end
+
+describe "Float#-" do
+  it "returns self minus other" do
     (9_237_212.5280 - 5_280).should_be_close(9231932.528, TOLERANCE)
     (2_560_496.1691 - 0xfffffffff).should_be_close(-68716916238.8309, TOLERANCE)
     (5.5 - 5.5).should_be_close(0.0,TOLERANCE)
   end
-  
-  specify "-@ should negate self" do
+end
+
+describe "Float#-@" do
+  it "negates self" do
     (2.221.send(:-@)).should_be_close(-2.221, TOLERANCE)
     -2.01.should_be_close(-2.01,TOLERANCE)
     -2_455_999_221.5512.should_be_close(-2455999221.5512, TOLERANCE)
     (--5.5).should_be_close(5.5, TOLERANCE)
     -8.551.send(:-@).should_be_close(8.551, TOLERANCE)
   end
+end
 
-  specify "/ should return self divided by other" do
+describe "Float#/" do
+  it "returns self divided by other" do
     (5.75 / -2).should_be_close(-2.875,TOLERANCE)
     (451.0 / 9.3).should_be_close(48.494623655914,TOLERANCE)
     (91.1 / -0xffffffff).should_be_close(-2.12108716418061e-08, TOLERANCE)
   end
   
-  specify "/ should NOT raise ZeroDivisionError if other is zero" do
+  it "does NOT raise ZeroDivisionError if other is zero" do
     (1.0 / 0.0).to_s.should == 'Infinity'
     (-1.0 / 0.0).to_s.should == '-Infinity'
     (1.0 / -0.0).to_s.should == '-Infinity'
@@ -112,184 +163,88 @@ context "Float" do
     (-0.0 / 0.0).to_s.should == 'NaN'
     (-0.0 / -0.0).to_s.should == 'NaN'
   end
-  
-  specify "< should return true if self is less than other" do
+end
+
+describe "Float#<" do
+  it "returns true if self is less than other" do
     (71.3 < 91.8).should == true
     (192.6 < -500).should == false
     (-0.12 < 0x4fffffff).should == true
   end
-  
-  specify "<= should return true if self is less than or equal to other" do
+end
+
+describe "Float#<=" do
+  it "returns true if self is less than or equal to other" do
     (2.0 <= 3.14159).should == true
     (-2.7183 <= -24).should == false
     (0.0 <= 0.0).should == true
     (9_235.9 <= 0xffffffff).should == true
   end
-  
-  specify "<=> should return -1, 0, 1 when self is less than, equal, or greater than other" do
+end
+
+describe "Float#<=>" do
+  it "returns -1, 0, 1 when self is less than, equal, or greater than other" do
     (1.5 <=> 5).should == -1
     (2.45 <=> 2.45).should == 0
     ((0xffffffff*1.1) <=> 0xffffffff).should == 1
   end
-  
-  specify "== should true if self has the same value as other" do
+end
+
+describe "Float#==" do
+  it "returns true if self has the same value as other" do
     (1.0 == 1).should == true
     (2.71828 == 1.428).should == false
     (-4.2 == 4.2).should == false
   end
 
-  specify "== calls 'other == self' if coercion fails" do
+  it "calls 'other == self' if coercion fails" do
     class X; def ==(other); 2.0 == other; end; end
 
     (1.0 == X.new).should == false
     (2.0 == X.new).should == true
   end
-  
-  specify "> should return true if self is greater than other" do
+end
+
+describe "Float#>" do
+  it "returns true if self is greater than other" do
     (1.5 > 1).should == true
     (2.5 > 3).should == false
     (45.91 > 0xffffffff).should == false
   end
-  
-  specify ">= should return true if self is greater than or equal to other" do
+end
+
+describe "Float#>=" do
+  it "returns true if self is greater than or equal to other" do
     (5.2 >= 5.2).should == true
     (9.71 >= 1).should == true
     (5.55382 >= 0xfabdafbafcab).should == false
   end
+end
 
-  specify "abs should return the absolute value" do
+describe "Float#abs" do
+  it "returns the absolute value" do
     -99.1.abs.should_be_close(99.1, TOLERANCE)
     4.5.abs.should_be_close(4.5, TOLERANCE)
     0.0.abs.should_be_close(0.0, TOLERANCE)
   end
   
-  specify "ceil should return the smallest Integer greater than or equal to self" do
+end
+
+describe "Float#ceil" do
+  it "returns the smallest Integer greater than or equal to self" do
     -1.2.ceil.should == -1
     -1.0.ceil.should == -1
     0.0.ceil.should == 0
     1.3.ceil.should == 2
     3.0.ceil.should == 3
   end
-  
-  specify "coerce should [other, self] both as Floats" do
+end
+
+describe "Float#coerce" do
+  it "returns [other, self] both as Floats" do
     1.2.coerce(1).inspect.should == '[1.0, 1.2]'
     -0.0.coerce(0xffffffff).inspect.should == '[4294967295.0, -0.0]'
     5.28.coerce(1.0).inspect.should == '[1.0, 5.28]'
-  end
-  
-  specify "divmod should return an [quotient, modulus] from dividing self by other" do
-    3.14.divmod(2).inspect.should == "[1, 1.14]"
-    2.8284.divmod(3.1415).inspect.should == "[0, 2.8284]"
-    -1.0.divmod(0xffffffff).inspect.should == "[-1, 4294967294.0]"
-  end
-
-  # This failed for me on MRI. I'm assuming it is platform dependent -- flgr
-  if RUBY_PLATFORM["darwin"] && !defined?(RUBY_ENGINE) then
-    it "divmod should return [NaN, NaN] if other is zero" do
-      1.0.divmod(0).inspect.should == '[NaN, NaN]'
-      1.0.divmod(0.0).inspect.should == '[NaN, NaN]'
-    end
-  else
-    it "divmod should raise FloatDomainError if other is zero" do
-      should_raise(FloatDomainError) { 1.0.divmod(0) }
-      should_raise(FloatDomainError) { 1.0.divmod(0.0) }
-    end
-  end
-  
-  specify "eql? should return true if other is a Float equal to self" do
-    1.0.eql?(1).should == false
-    0.0.eql?(0.0).should == true
-  end
-  
-  specify "finite? should return true if a valid IEEE floating-point number" do
-    (1.5**0xffffffff).finite?.should == false
-    3.14159.finite?.should == true
-    (-1.0/0.0).finite?.should == false
-  end
-  
-  specify "floor should return the largest Integer less than or equal to self" do
-    -1.2.floor.should == -2
-    -1.0.floor.should == -1
-    0.0.floor.should == 0
-    1.0.floor.should == 1
-    5.9.floor.should == 5
-  end
-  
-  specify "hash should be provided" do
-    0.0.respond_to?(:hash).should == true
-  end
-  
-  specify "infinite? should return nil, -1, +1 when self is finite, -Infinity, +Infinity" do
-    1.0.infinite?.should == nil
-    (1.0/0.0).infinite?.should == 1
-    (1.0/-0.0).infinite?.should == -1
-  end
-  
-  specify "modulo should be a synonym for %" do
-    6543.21.modulo(137).should_be_close(104.21, TOLERANCE)
-    5667.19.modulo(0xffffffff).should_be_close(5667.19, TOLERANCE)
-    6543.21.modulo(137.24).should_be_close(92.9299999999996, TOLERANCE)
-    
-    6543.21.modulo(137).should_be_close(6543.21.%(137), TOLERANCE)
-    5667.19.modulo(0xffffffff).should_be_close(5667.19.%(0xffffffff), TOLERANCE)
-    6543.21.modulo(137.24).should_be_close(6543.21.%(137.24), TOLERANCE)
-  end
-
-  specify "modulo should NOT raise ZeroDivisionError if other is zero" do
-    (1.0 % 0).to_s.should == 'NaN'
-    (1.0 % 0.0).to_s.should == 'NaN'
-  end
-
-  specify "nan? should return true if self is not a valid IEEE floating-point number" do
-    0.0.nan?.should == false
-    -1.5.nan?.should == false
-    (0.0/0.0).nan?.should == true
-  end
-  
-  specify "round should return the nearest Integer" do
-    5.5.round.should == 6
-    0.4.round.should == 0
-    -2.8.round.should == -3
-    0.0.round.should == 0
-  end
-  
-  specify "to_f should return self" do
-    -500.3.to_f.should == -500.3
-    267.51.to_f.should == 267.51
-    1.1412.to_f.should == 1.1412
-  end
-  
-  specify "to_i should return self truncated to an Integer" do
-    899.2.to_int.should == 899
-    -1.122256e-45.to_int.should == 0
-    5_213_451.9201.to_int.should == 5213451
-  end
-  
-  specify "to_int should be a synonym for to_i" do
-    899.2.to_int.should == 899
-    -1.122256e-45.to_int.should == 0
-    5_213_451.9201.to_int.should == 5213451
-  end
-  
-  specify "to_s should return a string representation of self, possibly Nan, -Infinity, +Infinity" do
-    0.551e7.to_s.should == "5510000.0"
-    -3.14159.to_s.should == "-3.14159"
-    0.0.to_s.should == "0.0"
-  end
-  
-  specify "truncate should be a synonym for to_i" do
-    899.2.truncate.should == 899
-    -1.122256e-45.truncate.should == 0
-    5_213_451.9201.truncate.should == 5213451
-  end
-  
-  specify "zero? should return true if self is 0.0" do
-    0.0.zero?.should == true
-    1.0.zero?.should == false
-    -1.0.zero?.should == false
-  end
-  
-  specify "coerce should return [Float, Float]" do
     1.0.coerce(1).inspect.should == '[1.0, 1.0]'
     1.0.coerce(0xffffffff).inspect.should == '[4294967295.0, 1.0]'
     1.0.coerce("2.5").inspect.should == '[2.5, 1.0]'
@@ -297,31 +252,129 @@ context "Float" do
   end
 end
 
-context "Float.induced_from" do
-  specify "should return the argument when passed a Float" do
-    Float.induced_from(5.5).eql?(5.5).should == true
+describe "Float#divmod" do
+  it "returns an [quotient, modulus] from dividing self by other" do
+    3.14.divmod(2).inspect.should == "[1, 1.14]"
+    2.8284.divmod(3.1415).inspect.should == "[0, 2.8284]"
+    -1.0.divmod(0xffffffff).inspect.should == "[-1, 4294967294.0]"
   end
 
-  specify "should return a Float of the correct value when sent a Fixnum" do
-    Float.induced_from(36).should == 36.0
-  end
-
-  specify "should return a Float of the correct value when sent a Bignum" do
-    Float.induced_from(23472398472349872349872349872348972348972439423).to_s.should == '2.34723984723499e+46'
-  end
-
-  specify "should raise a TypeError if the argument is not a Float, Fixnum, or Bignum" do
-    class Foo
-      def to_f; 9.9; end
+  # This failed for me on MRI. I'm assuming it is platform dependent -- flgr
+  if RUBY_PLATFORM["darwin"] && !defined?(RUBY_ENGINE) then
+    it "returns [NaN, NaN] if other is zero" do
+      1.0.divmod(0).inspect.should == '[NaN, NaN]'
+      1.0.divmod(0.0).inspect.should == '[NaN, NaN]'
     end
-    should_raise(TypeError) { Float.induced_from(Foo.new) }
+  else
+    it "raises FloatDomainError if other is zero" do
+      should_raise(FloatDomainError) { 1.0.divmod(0) }
+      should_raise(FloatDomainError) { 1.0.divmod(0.0) }
+    end
   end
-end 
+end
 
-describe "Float#+@" do 
-  it " should return the same value with same sign (two complement)" do 
-    34.56.send(:+@).should == 34.56
-    -34.56.send(:+@).should == -34.56
-    0.0.send(:+@).should == 0.0
+describe "Float#eql?" do
+  it "returns true if other is a Float equal to self" do
+    1.0.eql?(1).should == false
+    0.0.eql?(0.0).should == true
+  end
+end
+
+describe "Float#finite?" do
+  it "returns true if a valid IEEE floating-point number" do
+    (1.5**0xffffffff).finite?.should == false
+    3.14159.finite?.should == true
+    (-1.0/0.0).finite?.should == false
+  end
+end
+
+describe "Float#floor" do
+  it "returns the largest Integer less than or equal to self" do
+    -1.2.floor.should == -2
+    -1.0.floor.should == -1
+    0.0.floor.should == 0
+    1.0.floor.should == 1
+    5.9.floor.should == 5
+  end
+end
+
+describe "Float#hash" do
+  it "is provided" do
+    0.0.respond_to?(:hash).should == true
+  end
+end
+
+describe "Float#infinite?" do
+  it "returns nil, -1, +1 when self is finite, -Infinity, +Infinity" do
+    1.0.infinite?.should == nil
+    (1.0/0.0).infinite?.should == 1
+    (1.0/-0.0).infinite?.should == -1
+  end
+end
+
+describe "Float#modulo" do
+  it_behaves_like(float_modulo, :modulo)
+end
+
+describe "Float#nan?" do
+  it "returns true if self is not a valid IEEE floating-point number" do
+    0.0.nan?.should == false
+    -1.5.nan?.should == false
+    (0.0/0.0).nan?.should == true
+  end
+end
+
+describe "Float#round" do
+  it "returns the nearest Integer" do
+    5.5.round.should == 6
+    0.4.round.should == 0
+    -2.8.round.should == -3
+    0.0.round.should == 0
+  end
+end
+
+describe "Float#to_f" do
+  it "returns self" do
+    -500.3.to_f.should == -500.3
+    267.51.to_f.should == 267.51
+    1.1412.to_f.should == 1.1412
+  end
+end
+
+float_to_i = shared "Float#to_i | Float#to_int | Float#truncate" do |cmd|
+  describe "Float##{cmd}" do
+    it "returns self truncated to an Integer" do
+      899.2.send(cmd).should == 899
+      -1.122256e-45.send(cmd).should == 0
+      5_213_451.9201.send(cmd).should == 5213451
+    end
+  end
+end
+
+describe "Float#to_i" do
+  it_behaves_like(float_to_i, :to_i)
+end
+
+describe "Float#" do
+  it_behaves_like(float_to_i, :to_int)
+end
+
+describe "Float#to_s" do
+  it "returns a string representation of self, possibly Nan, -Infinity, +Infinity" do
+    0.551e7.to_s.should == "5510000.0"
+    -3.14159.to_s.should == "-3.14159"
+    0.0.to_s.should == "0.0"
   end  
+end
+
+describe "Float#" do
+  it_behaves_like(float_to_i, :truncate)
+end
+
+describe "Float#zero?" do
+  it "returns true if self is 0.0" do
+    0.0.zero?.should == true
+    1.0.zero?.should == false
+    -1.0.zero?.should == false
+  end
 end
