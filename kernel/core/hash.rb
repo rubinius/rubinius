8fb93a09ac3d7a9d68ff01ae4f6ce658240c6786
Florian Gross <florgro@gmail.com>
2007-08-06 00:58:00 +0000

    Hash: Welcome the new minimalism

--- kernel/core/hash.rb
+++ kernel/core/hash.rb
@@ -1,123 +1,138 @@
 class Hash
   include Enumerable
-  alias :has_key?   :key?
-  alias :include?   :key?
-  alias :member?    :key?
-  alias :each_pair  :each
-  alias :length     :size
-  alias :store      :[]=
-
+  
+  # VM specific: keys, fetch, []=, delete
+  # We choose fetch instead of [] because it doesn't need Hash#default
+  # Alternative subset: keys, [], []=, delete, initialize, default, default_proc
+  
+  alias :store :[]=
+  
+  # TODO: Ought to use  default = (no_default = true; nil)
+  # Compiler currently chokes on it. -- flgr
+  def initialize(*args, &default_proc)
+    if args.size > 1 then
+      raise(ArgumentError, "wrong number of arguments (%d for 1)" % args.size)
+    end
+    
+    has_default = args.size == 1
+    default = args.first if has_default
+    
+    if default_proc and has_default then
+      raise(ArgumentError, "can't supply both default proc and default value")
+    end
+    
+    @default_proc = default_proc
+    @default = default
+  end
+  
   def self.[](*args)
     unless args.size % 2 == 0 or (args.size == 1 and args[0].is_a?(Hash))
       raise ArgumentError, "odd number of arguments for Hash" 
     end
-    hsh = {}
+    
+    hsh = self.new
+    
     while args.size >= 2
       k = args.shift
       v = args.shift
       hsh[k] = v
     end
+    
     if args.size == 1
       hsh.merge! args[0]
     end
-    hsh
-  end
-
-  def default_proc
-    @default_proc ? @default : nil
-  end
-
-  def default=(nd)
-    @default_proc = false
-    @default = nd
-  end
-
-  def value?(val)
-    values.include?(val)
-  end
-  alias :has_value? :value?
     
-  def delete(key)
-    retval = nil
-    found, val = find_unambigious(key)
-    if found
-      retval = delete_by_hash key.hash, key
-    else
-      retval = yield(key) if block_given?
-    end
-    retval    
-  end
+    return hsh
+  end  
+
+  # TODO: This really ought to work -- flgr
+  #attr_reader :default_proc
+  def default_proc() @default_proc end
   
-  def delete_if
-    # Do this in 2 steps, so we're not altering the structure
-    # while we walk it.
-    to_delete = []
-    each { |k, v| to_delete << k if yield(k, v) }
-    to_delete.each { |k| delete(k) }
-    return self
-  end
+  def default=(default)
+    @default_proc = nil
+    @default = default
+  end  
   
-  def clear
-    delete_if {true}
+  def default(key = nil)
+    default_proc ? default_proc.call(self, key) : @default
   end
+
+  def key?(key)
+    # Slightly wrong: should test bins directly
+    key_hash = key.hash
+    keys.any? { |k| key_hash == k.hash and key.eql?(k) }
+  end unless instance_methods.include?("key?")
   
+  alias :has_key? :key?
+  alias :include? :key?
+  alias :member?  :key?
+    
+  def [](key)
+    key?(key) ? fetch(key) : default(key)
+  end
+
   def each_key(&block)
+    # Slightly wrong: should be affected by modification from block
     keys.each(&block)
-    self
-  end
-  
+    return self
+  end unless instance_methods.include?("each_key")
+
   def each_value(&block)
     values.each(&block)
-    self
+    return self
   end
-  
-  def index(val)
-    each {|k, v| return k if v == val}
-    nil
-  end
-  
-  def merge!(other_hash)
-   other_hash.each do |k, v| 
-     if block_given? && self.key?(k)
-       self[k] = yield(k, self[k], v)
-     else
-       self[k] = v
-     end
-   end
-    self
-  end
-  alias :update :merge!
-  
-  def shift
-    out = nil
-    if empty?
-      out = default_proc ? default_proc.call(self, nil) : default
-    else
-      i = 0
-      tup = nil
-      while !tup
-        tup = @values.at(i)
-        i += 1
-      end
-      key = tup.at(1)
-      out = [key, self[key]]
-      delete(key)
+
+  def each_pair()
+    each_key do |key|
+      yield(key, self[key])
     end
-    out
   end
-  
-  def values_at(*args)
-    args.collect { |a| self[a] }
+
+  def each()
+    each_pair do |*pair|
+      yield(pair)
+    end
   end
-  alias :indexes :values_at
-  alias :indices :values_at
-  
-  def merge(other_hash, &block)
-    dup.merge!(other_hash, &block)
+
+  def values()
+    keys.map { |key| self[key] }
   end
+
+  def index(value)
+    keys.find { |key| self[key] == value }
+  end
+
+  def value?(value)
+    keys.any? { |key| self[key] == value }
+  end
+  alias :has_value? :value?
+
+  def clear()
+    # Non-empty hashes would however raise because of delete()
+    raise(TypeError, "can't modify frozen hash") if frozen?
+    
+    keys.each { |key| delete(key) }
+    return self
+  end
+
+  def size()
+    keys.size
+  end
+  alias :length :size
   
-  def reject(&block)
-    dup.delete_if(&block)
+  def empty?()
+    size == 0
+  end
+
+  def delete_if()
+    # Non-empty hashes would however raise because of delete()
+    raise(TypeError, "can't modify frozen hash") if frozen?
+
+    each_pair do |key, value|
+      delete(key) if yield(key, value)
+    end
+    return self
   end
   
   def reject!(&block)
@@ -125,84 +140,118 @@ class Hash
     delete_if(&block)
     old_size == size ? nil : self
   end
-  
-  def invert
-    out = {}
-    each {|k, v| out[v] = k}
-    out
+
+  def reject(&block)
+    self.dup.delete_if(&block)
   end
-  
-  def to_hash
-    self
+
+  def values_at(*keys)
+    keys.map { |key| self[key] }
   end
-  
-  def replace(other_hash)
-    unless self.equal?(other_hash)
-      clear
-      other_hash.each {|k, v| self[k] = v}
-      self.default = other_hash.default
+  alias :indexes :values_at
+  alias :indices :values_at
+
+  def invert()
+    result = {}
+    each_pair do |key, value|
+      result[value] = key
     end
-    self
-  end
-  alias :initialize_copy :replace
-  
-  def rehash
-    out = {}
-    each {|k, v| out[k] = v}
-    replace(out)
+    return result
   end
   
-  def ==(other)
-    return true if self.equal?(other)
-    unless other.kind_of?(Hash)
-      return other.respond_to?(:to_hash) ? self == other.to_hash : false
-    end
-    return false unless other.size == size
-    #pickaxe claims that defaults are compared, but MRI 1.8.4 doesn't actually do that
-    #return false unless other.default == default
-    each do |k, v| 
-      return false unless other[k] == self[k]
-    end
-    true
-  end
-  
-  def dup
-    {}.replace(self)
-  end
-  def fetch(key, *rest)
-    raise ArgumentError, "wrong number of arguments (#{rest.size + 1} for 2)" if rest.size > 1
-    found, val = find_unambigious(key)
-    unless found
-      if rest.size == 0 && !block_given?
-        raise IndexError, 'key not found'
-      elsif block_given?
-        val = yield key
+  def merge!(other)
+    other.to_hash.each do |key, value| 
+      if key?(key) and block_given?
+        self[key] = yield(key, self[key], value)
       else
-        val = rest.first
+        self[key] = value
       end
     end
     
-    val
+    return self
+  end
+  alias :update :merge!
+
+  def merge(other, &block)
+    dup.merge!(other, &block)
   end
   
-  def to_a
-    a = []
-    each {|k,v| a << [k,v] }
-	a
+  def replace(other)
+    other = other.to_hash
+    
+    unless equal?(other)
+      clear
+      
+      other.each do |key, value|
+        self[key] = value
+      end
+
+      if other.default_proc then
+        @default_proc = other.default_proc
+      else
+        self.default = other.default
+      end
+    end
+    
+    return self
+  end
+  alias :initialize_copy :replace
+  private :initialize_copy
+
+  def rehash()
+    replace(clone)
   end
 
-  def to_s
+  def shift()
+    # Non-empty hashes would however raise because of delete()
+    raise(TypeError, "can't modify frozen hash") if frozen?
+
+    return default if empty?
+    key = keys.first
+    value = delete(key)
+    return [key, value]
+  end
+
+  def to_hash()
+    self
+  end
+
+  def ==(other)
+    return true if equal?(other)
+    
+    self.size == other.size and
+    other.all? do |key, value|
+      key?(key) and
+      self[key] == value
+    end
+  end
+
+  def inspect()
+    seen = (Thread.current[:__inspect_key__] ||= [])
+    if seen.include?(object_id) then
+      "{...}"
+    else
+      begin
+        seen << object_id
+        "{%s}" % map { |pair| "%p=>%p" % pair }.join(", ")
+      ensure
+        seen.pop
+      end
+    end
+  end
+
+  # These need to be defined as new instance methods
+  def to_a() super end
+  def sort() super end
+
+  # Difference to Enumerable#select: yields key, value instead of [key, value]
+  def select()
+    super do |args|
+      yield(*args)
+    end
+  end
+
+  def to_s()
     to_a.join
   end
-
-  def sort(&block)
-    to_a.sort(&block)
-  end
-  
-  def find_all(&block)
-    a = []
-   	each {|k,v| a << [k,v] if yield(k,v) }
-    a
-  end
-  alias_method :select, :find_all
 end
