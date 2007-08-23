bff7c05ce12c79ef111422ecf4525f1a65e7a5f0
Pedro Del Gallego Vida <pedro@la-vaca-roja.(none)>
2007-08-24 01:15:41 +0200

    More specs dor enumerable     * add inject, min, grep, find, detect, find_all, select

--- spec/core/enumerable_spec.rb
+++ spec/core/enumerable_spec.rb
@@ -42,63 +42,12 @@ context "A class with Enumerable mixin, cmd" do
   
   it "detect should return the first element for which block is not false" do
     Numerous.new.detect { |i| i > 3 }.should == 5
-  end
-  
-  it "each_with_index should pass each element and its index to block" do
-    @a = []
-    Numerous.new.each_with_index { |o, i| @a << [o, i] }
-    @a.should == [[2, 0], [5, 1], [3, 2], [6, 3], [1, 4], [4, 5]]
-  end
-  
-  it "find should be a synonym for detect" do
-    Numerous.new.find { |i| i > 3 }.should == Numerous.new.detect { |i| i > 3 }
-  end
-  
-  it "find_all should be a synonym for select" do
-    Numerous.new.find_all { |i| i % 2 == 0 }.should == Numerous.new.select { |i| i % 2 == 0 }
-  end
-  
-  it "grep without a block should return an array of all elements === pattern" do
-    class EnumerableSpecGrep; def ===(obj); obj == '2'; end; end
-
-    Numerous.new('2', 'a', 'nil', '3', false).grep(EnumerableSpecGrep.new).should == ['2']
-  end
-  
-  it "grep with a block should return an array of elements === pattern passed through block" do
-    class EnumerableSpecGrep2; def ===(obj); /^ca/ =~ obj; end; end
-
-    Numerous.new("cat", "coat", "car", "cadr", "cost").grep(EnumerableSpecGrep2.new) { |i| i.upcase }.should == ["CAT", "CAR", "CADR"]
-  end
-  
-  it "include? should return true if any element == argument" do
-    class EnumerableSpecIncludeP; def ==(obj) obj == 5; end; end
-
-    Numerous.new.include?(5).should == true 
-    Numerous.new.include?(10).should == false
-    Numerous.new.include?(EnumerableSpecIncludeP.new).should == true
-  end
-  
-  it "inject with argument takes a block with an accumulator (with argument as initial value) and the current element. Value of block becomes new accumulator" do
-    a = []
-    Numerous.new.inject(0) { |memo, i| a << [memo, i]; i }
-    a.should == [[0, 2], [2, 5], [5, 3], [3, 6], [6, 1], [1, 4]]
-  end
+  end  
     
-  it "inject without argument takes a block with an accumulator (with first element as initial value) and the current element. Value of block becomes new accumulator" do
-    a = []
-    Numerous.new.inject { |memo, i| a << [memo, i]; i }
-    a.should == [[2, 5], [5, 3], [3, 6], [6, 1], [1, 4]]
-  end
-  
   it "map should be a synonym for collect" do
     Numerous.new.map { |i| i % 2 }.should == Numerous.new.collect { |i| i % 2 }
   end
-  
-  it "member? should be a synonym for include?" do
-    Numerous.new.include?(5).should ==  Numerous.new.member?(5)
-    Numerous.new.include?(10).should ==  Numerous.new.member?(10)
-  end
-  
+    
   it "partition should return two arrays, the first containing elements for which the block is true, the second containing the rest" do
     Numerous.new.partition { |i| i % 2 == 0 }.should == [[2, 6, 4], [5, 3, 1]]
   end
@@ -106,10 +55,7 @@ context "A class with Enumerable mixin, cmd" do
   it "reject should return an array of the elements for which block is false" do
     Numerous.new.reject { |i| i > 3 }.should == [2, 3, 1]
   end
-  
-  it "select should return an array of all elements for which block is not false" do
-    Numerous.new.find_all { |i| i % 2 == 0 }.should == [2, 6, 4]
-  end 
+   
   
   it "zip without a block should merge elements with corresponding indexes from each #to_a argument" do
     class EnumerableSpecZip; def to_a(); [1, 2, 3, 4, 5, 6]; end; end
@@ -143,6 +89,12 @@ end
 describe "File.each_with_index" do
   before :each do
     @b = [2, 5, 3, 6, 1, 4]  
+  end 
+  
+  it "pass each element and its index to block" do
+    @a = []
+    Numerous.new.each_with_index { |o, i| @a << [o, i] }
+    @a.should == [[2, 0], [5, 1], [3, 2], [6, 3], [1, 4], [4, 5]]
   end
   
   it "provide each element to the block" do  
@@ -153,7 +105,7 @@ describe "File.each_with_index" do
     obj.should == res
   end
   
-  it "" do
+  it "provide each element to the block and its index" do
     acc = [] 
     res = @b.each_with_index {|a,i| acc << [a,i]}
     [[2, 0], [5, 1], [3, 2], [6, 3], [1, 4], [4, 5]].should == acc
@@ -161,12 +113,7 @@ describe "File.each_with_index" do
   end
 end
 
-context "A class with Enumerable mixin and <=> defined, cmd" do 
-  
-  it "min should return the minimum element" do
-    Numerous.new.min.should == 1
-  end
-  
+context "A class with Enumerable mixin and <=> defined, cmd" do  
   it "sort without a block should should return an array of elements ordered lowest to highest" do
     Numerous.new.sort.should == [1, 2, 3, 4, 5, 6]
   end
@@ -253,12 +200,67 @@ describe "Enumerable#any?" do
   end
 end
 
-describe "Enumerable.max" do  
+describe "Enumerable#min" do  
   before :each do
     @a = EachDefiner.new( 2, 4, 6, 8, 10 )
 
     @e_strs = EachDefiner.new("333", "22", "666666", "1", "55555", "1010101010")
-    @e_ints = EachDefiner.new( 333,   22,   666666,   1,   55555,   1010101010)
+    @e_ints = EachDefiner.new( 333,   22,   666666,   55555, 1010101010)
+  end
+    
+  it "min should return the minimum element" do
+    Numerous.new.min.should == 1
+  end
+  
+  it "return the minimun (basic cases)" do  
+    EachDefiner.new(55).min.should == 55
+
+    EachDefiner.new(11,99).min.should ==  11
+    EachDefiner.new(99,11).min.should == 11
+    EachDefiner.new(2, 33, 4, 11).min.should == 2
+
+    EachDefiner.new(1,2,3,4,5).min.should == 1
+    EachDefiner.new(5,4,3,2,1).min.should == 1
+    EachDefiner.new(4,1,3,5,2).min.should == 1
+    EachDefiner.new(5,5,5,5,5).min.should == 5
+
+    EachDefiner.new("aa","tt").min.should == "aa"
+    EachDefiner.new("tt","aa").min.should == "aa"
+    EachDefiner.new("2","33","4","11").min.should == "11" 
+
+    @e_strs.min.should == "1"
+    @e_ints.min.should == 22
+  end
+  
+  it "return nil when error" do    
+    EachDefiner.new().min.should == nil
+    should_raise(NoMethodError) {EachDefiner.new(Object.new, Object.new).min }
+    should_raise( ArgumentError) {EachDefiner.new(11,"22").min }
+  end
+  
+  it "return the minimun when using a block rule" do
+    EachDefiner.new("2","33","4","11").min {|a,b| a <=> b }.should == "11"
+    EachDefiner.new( 2 , 33 , 4 , 11 ).min {|a,b| a <=> b }.should == 2
+
+    EachDefiner.new("2","33","4","11").min {|a,b| b <=> a }.should == "4"
+    EachDefiner.new( 2 , 33 , 4 , 11 ).min {|a,b| b <=> a }.should == 33
+
+    @e_strs.min {|a,b| a.length <=> b.length }.should == "1"
+
+    @e_strs.min {|a,b| a <=> b }.should == "1"
+    @e_strs.min {|a,b| a.to_i <=> b.to_i }.should == "1"
+
+    @e_ints.min {|a,b| a <=> b }.should == 22
+    @e_ints.min {|a,b| a.to_s <=> b.to_s }.should == 1010101010
+  end
+end
+
+describe "Enumerable#max" do  
+  before :each do
+    @a = EachDefiner.new( 2, 4, 6, 8, 10 )
+
+    @e_strs = EachDefiner.new("333", "22", "666666", "1", "55555", "1010101010")
+    @e_ints = EachDefiner.new( 333,   22,   666666,   55555, 1010101010)
   end
   
   it "max should return the maximum element" do
@@ -311,7 +313,7 @@ describe "Enumerable.max" do
 end
 
 generic_collect_map = shared "Range#collect" do |cmd|
-  describe "Range##{cmd}" do
+  describe "Range#{cmd}" do
     before :each do
       @a = EachDefiner.new( 2, 4, 6, 8, 10 )
     end
@@ -330,16 +332,16 @@ generic_collect_map = shared "Range#collect" do |cmd|
   end
 end
 
-describe "File.collect" do   
+describe "Enumerable#collect" do   
   it_behaves_like(generic_collect_map , :collect) 
 end
 
-describe "File.map" do   
+describe "Enumerable#map" do   
   it_behaves_like(generic_collect_map , :map) 
 end
 
-generic_entries_to_a = shared "Range#collect" do |cmd|
-  describe "Range##{cmd}" do
+generic_entries_to_a = shared "Enumerable#collect" do |cmd|
+  describe "Enumerable#{cmd}" do
     before :each do
       @a = EachDefiner.new( 2, 4, 6, 8, 10 )
     end
@@ -362,11 +364,217 @@ generic_entries_to_a = shared "Range#collect" do |cmd|
   end
 end
 
-describe "File.entries" do   
+describe "Enumerable#entries" do   
   it_behaves_like(generic_entries_to_a , :entries) 
 end
 
-describe "File.to_a" do   
+describe "Enumerable#to_a" do   
   it_behaves_like(generic_entries_to_a , :to_a) 
 end
 
+generic_entries_find = shared "Enumerable#detect" do |cmd|
+  describe "Enumerable#{cmd}" do 
+    # #detect and #find are aliases, so we only need one function 
+    before :each do
+      @fail_count = 0
+      @fail_proc = lambda { @fail_count += 1 ; "not found" }
+      @fail_proc_value = "not found"
+       
+    end
+    
+    after :each do 
+      @fail_count = 0
+    end
+    
+    it "Returns the first for which block is not false or call ifnone proc, empty array and nil" do
+      args, fail_value = [], nil      
+      EachDefiner.new().send(cmd, *args) {|a| true  }.should == fail_value
+      @a.send(cmd, *args) {|a| false }.should == fail_value
+      @a.send(cmd, *args) {|a| a > 1 }.should == 2
+      @a.send(cmd, *args) {|a| a > 5 }.should == 6
+      @a.send(cmd, *args) {|a| a > 9 }.should == 10
+      @a.send(cmd, *args) {|a| a > 10 }.should == fail_value 
+    end
+    
+    it "returns the first for which block is not false, or call ifnone proc" do
+      args, fail_value = @fail_proc,  @fail_proc_value 
+      EachDefiner.new().send(cmd, *args) {|a| true  }.should == fail_value
+      @a.send(cmd, *args) {|a| false }.should == fail_value
+      @a.send(cmd, *args) {|a| a > 1 }.should == 2
+      @a.send(cmd, *args) {|a| a > 5 }.should == 6
+      @a.send(cmd, *args) {|a| a > 9 }.should == 10
+      @a.send(cmd, *args) {|a| a > 10 }.should == fail_value 
+    end
+    
+    it "call only once if " do
+      # Make sure that the "proc" is only called once, and only if no
+      # match is found.       
+      @fail_count = 0
+      @a.send(cmd, @fail_proc) {|a| false }.should == @fail_proc_value
+      @a.send(cmd, @fail_proc) {|a| true }.should_not == @fail_proc_value
+      @fail_count.should == 1
+    end
+    
+    it "not call the ifnone proc when find a element" do
+      @fail_count = 0
+      @a.send(cmd, @fail_proc) {|a| true }.should == 2
+      @fail_count.should == 0
+    end  
+  
+    it "find should be a synonym for detect" do
+      Numerous.new.send(cmd) { |i| i > 3 }.should == Numerous.new.detect { |i| i > 3 }
+      Numerous.new.send(cmd) { |i| i > 3 }.should == Numerous.new.find { |i| i > 3 } 
+    end
+  end
+end
+
+describe "Enumerable#detect" do
+  it_behaves_like(generic_entries_find , :find) 
+end
+
+describe "Enumerable#find" do
+  it_behaves_like(generic_entries_find , :detect) 
+end
+
+generic_entries_find_all = shared "Enumerable#detect" do |cmd|
+  describe "Enumerable#{cmd}" do 
+    # #find_all and #select are aliases, so we only need one function
+    # that tests both cmds
+    
+    it "find_all and Select are synonymous" do
+      Numerous.new.send(cmd){ |i| i % 2 == 0 }.should == Numerous.new.select { |i| i % 2 == 0 }
+      Numerous.new.send(cmd){ |i| i % 2 == 0 }.should == Numerous.new.find_all { |i| i % 2 == 0 }
+    end
+  
+    it "return an array of all elements for which block is not false" do
+      Numerous.new.send(cmd) { |i| i % 2 == 0 }.should == [2, 6, 4]
+    end
+    
+    it " generic_test_find_all(cmd)" do
+      EachDefiner.new().send(cmd) {|a| true}.should == []
+      @a.send(cmd) {|a| false}.should == []
+      @a.send(cmd) {|a| a < 6}.should == [2,4]
+      @a.send(cmd) {|a| a == 4}.should == [4]
+      @a.send(cmd) {|a| true}.should == @a.arr
+    end
+  end
+end
+
+describe "Enumerable#find_all" do
+  it_behaves_like(generic_entries_find_all , :find_all) 
+end
+
+describe "Enumerable#select" do
+  it_behaves_like(generic_entries_find_all , :select) 
+end
+
+describe "Enumerable#grep" do
+  
+  it "grep without a block should return an array of all elements === pattern" do
+    class EnumerableSpecGrep; def ===(obj); obj == '2'; end; end
+
+    Numerous.new('2', 'a', 'nil', '3', false).grep(EnumerableSpecGrep.new).should == ['2']
+  end
+  
+  it "grep with a block should return an array of elements === pattern passed through block" do
+    class EnumerableSpecGrep2; def ===(obj); /^ca/ =~ obj; end; end
+
+    Numerous.new("cat", "coat", "car", "cadr", "cost").grep(EnumerableSpecGrep2.new) { |i| i.upcase }.should == ["CAT", "CAR", "CADR"]
+  end 
+  
+  it "grep the enumerable (rubycon legacy)" do 
+    EachDefiner.new().grep(1).should == []
+    @a.grep(3..7).should == [4,6]
+    @a.grep(3..7) {|a| a+1}.should == [5,7]
+  end
+end
+
+describe "Enumerable#inject" do  
+  it "inject with argument takes a block with an accumulator (with argument as initial value) and the current element. Value of block becomes new accumulator" do
+    a = []
+    Numerous.new.inject(0) { |memo, i| a << [memo, i]; i }
+    a.should == [[0, 2], [2, 5], [5, 3], [3, 6], [6, 1], [1, 4]]
+  end
+    
+  it "inject without argument takes a block with an accumulator (with first element as initial value) and the current element. Value of block becomes new accumulator" do
+    a = []
+    Numerous.new.inject { |memo, i| a << [memo, i]; i }
+    a.should == [[2, 5], [5, 3], [3, 6], [6, 1], [1, 4]]
+  end  
+  
+  it "inject with inject arguments(legacy rubycon)" do
+    # with inject argument
+    EachDefiner.new().inject(1) {|acc,x| 999 }.should == 1
+    EachDefiner.new(2).inject(1) {|acc,x| 999 }.should ==  999
+    EachDefiner.new(2).inject(1) {|acc,x| acc }.should == 1
+    EachDefiner.new(2).inject(1) {|acc,x| x }.should == 2
+
+    EachDefiner.new(1,2,3,4).inject(100) {|acc,x| acc + x }.should == 110
+    EachDefiner.new(1,2,3,4).inject(100) {|acc,x| acc * x }.should == 2400
+
+    EachDefiner.new('a','b','c').inject("z") {|result, i| i+result}.should == "cbaz"
+  end
+  
+  it "inject withou inject arguments(legacy rubycon)" do
+    # no inject argument
+    EachDefiner.new(2).inject {|acc,x| 999 } .should == 2
+    EachDefiner.new(2).inject {|acc,x| acc }.should == 2
+    EachDefiner.new(2).inject {|acc,x| x }.should == 2
+
+    EachDefiner.new(1,2,3,4).inject {|acc,x| acc + x }.should == 10
+    EachDefiner.new(1,2,3,4).inject {|acc,x| acc * x }.should == 24
+
+    EachDefiner.new('a','b','c').inject {|result, i| i+result}.should == "cba"
+    EachDefiner.new(3, 4, 5).inject {|result, i| result*i}.should == 60
+    EachDefiner.new([1], 2, 'a','b').inject{|r,i| r<<i}.should == [1, 2, 'a', 'b']
+
+  end
+  it "return nil when fails(legacy rubycon)" do
+    EachDefiner.new().inject {|acc,x| 999 }.should == nil 
+  end
+end
+
+
+generic_entries_include = shared "Enumerable#include-menber" do |cmd|
+  describe "Enumerable#{cmd}" do     
+    # #include? and #member? are aliases, so we only need one function
+    # that tests both methods
+    
+    it "include? should return true if any element == argument" do
+      class EnumerableSpecIncludeP; def ==(obj) obj == 5; end; end
+
+      Numerous.new.send(cmd,5).should == true 
+      Numerous.new.send(cmd,10).should == false
+      Numerous.new.send(cmd,EnumerableSpecIncludeP.new).should == true
+    end
+  
+    it "member? should be a synonym for include?" do
+      Numerous.new.send(cmd,5).should ==  Numerous.new.member?(5)
+      Numerous.new.send(cmd,10).should ==  Numerous.new.member?(10)
+    end
+    
+    it "Returns true if any member of enum equals obj (legacy rubycon)" do
+      EachDefiner.new().send(cmd, 1).should == false
+      EachDefiner.new().send(cmd, nil).should == false
+      EachDefiner.new(2,4,6,8,10).send(cmd, 2).should == true
+      EachDefiner.new(2,4,6,8,10).send(cmd, 3).should == false
+      EachDefiner.new(2,4,6,8,10).send(cmd, 4).should == true
+      EachDefiner.new(nil).send(cmd, nil).should == true
+    end
+    
+    it "Returns true if any member of enum equals obj when == compare different classes (legacy rubycon)" do
+      # equality is tested with ==
+      EachDefiner.new(2,4,6,8,10).send(cmd, 2.0).should == true
+      EachDefiner.new(2,4,[6,8],10).send(cmd, [6, 8]).should == true
+      EachDefiner.new(2,4,[6,8],10).send(cmd, [6.0, 8.0]).should == true
+    end
+  end
+end
+
+describe "Enumerable#include?" do
+  it_behaves_like(generic_entries_include, :include?) 
+end
+
+describe "Enumerable#member?" do
+  it_behaves_like(generic_entries_include, :member?) 
+end
