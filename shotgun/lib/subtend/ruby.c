525edf6f6a7d73fea468ef772cd52b4015d89808
Brian Ford <brixen@gmail.com>
2007-08-28 21:27:21 -0700

    Fixed call to NEW_HANDLE in rb_ary_pop.

--- shotgun/lib/subtend/ruby.c
+++ shotgun/lib/subtend/ruby.c
@@ -340,7 +340,7 @@ VALUE rb_ary_push(VALUE array, VALUE val) {
 VALUE rb_ary_pop(VALUE array) {
   CTX;
   OBJECT ary = HNDL(array);
-  return NEW_HANDLE(array_pop(ctx->state, ary));
+  return NEW_HANDLE(ctx, array_pop(ctx->state, ary));
 }
 
 VALUE rb_ary_entry(VALUE array, int offset) {
