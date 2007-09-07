e0532b3fb62089da7b7362ec2232997878a1221d
Yehuda Katz <wycats@gmail.com>
2007-09-06 22:31:30 -0700

    Added specs

---
+++ spec/rubinius/subtend/ext/subtend_object.c
@@ -0,0 +1,21 @@
+#include <ruby.h>
+
+VALUE so_instance_of(VALUE self, VALUE obj, VALUE klass) {
+  return rb_obj_is_instance_of(obj, klass);
+}
+
+VALUE so_kind_of(VALUE self, VALUE obj, VALUE klass) {
+  return rb_obj_is_kind_of(obj, klass);
+}
+
+VALUE so_respond_to(VALUE self, VALUE obj, VALUE sym) {
+  return rb_respond_to(obj, SYM2ID(sym)) == 1 ? Qtrue : Qfalse;
+}
+
+void Init_subtend_object() {
+  VALUE cls;
+  cls = rb_define_class("SubtendObject", rb_cObject);
+  rb_define_method(cls, "rb_obj_is_instance_of", so_instance_of, 2);  
+  rb_define_method(cls, "rb_obj_is_kind_of", so_kind_of, 2);  
+  rb_define_method(cls, "rb_respond_to", so_respond_to, 2);    
+}
---
+++ spec/rubinius/subtend/object_spec.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/subtend_helper'
+
+extension :rubinius do
+  compile_extension('subtend_object')
+  require File.dirname(__FILE__) + '/ext/subtend_object'
+  
+  context "SubtendObject" do
+    
+    setup do
+      @o = SubtendObject.new
+    end
+    
+    class ObjectTest
+      def foo
+      end
+    end
+    
+    class DescObjectTest < ObjectTest
+    end
+    
+    specify "rb_is_instance_of should return true if an object is an instance" do
+      @o.rb_obj_is_instance_of(ObjectTest.new, ObjectTest).should == true
+      @o.rb_obj_is_instance_of(DescObjectTest.new, ObjectTest).should == false
+    end 
+
+    specify "rb_is_kind_of should return true if an object is an instance or descendent" do
+      @o.rb_obj_is_kind_of(ObjectTest.new, ObjectTest).should == true
+      @o.rb_obj_is_kind_of(DescObjectTest.new, ObjectTest).should == true
+      @o.rb_obj_is_kind_of(Object.new, ObjectTest).should == false
+    end 
+    
+    specify "rb_respond_to should return 1 if respond_to? is true and 0 if respond_to? is false" do
+      @o.rb_respond_to(ObjectTest.new, :foo).should == true
+      @o.rb_respond_to(ObjectTest.new, :bar).should == false
+    end
+    
+  end
+end
