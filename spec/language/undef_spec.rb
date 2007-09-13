b941eceb681c57d23d35f952b11b2a2d3a1ea4dd
Wilson Bilkovich <wilson@supremetyrant.com>
2007-09-13 01:00:22 -0400

    Add a minimal spec for the 'undef' keyword

--- spec/language/undef_spec.rb
+++ spec/language/undef_spec.rb
@@ -1,1 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
+class UndefSpecClass
+  def meth=(other);nil;end
+end
+
+describe "The undef keyword" do
+  it "should undefine 'meth='" do
+    obj = UndefSpecClass.new
+    (obj.meth = 5).should == 5
+    class UndefSpecClass
+      undef meth=
+    end
+    should_raise(NoMethodError) { obj.meth = 5 }
+  end
+end
