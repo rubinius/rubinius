64f53c8b40b3a80b41a2c27b4ac7255b7aad5f4d
Wilson Bilkovich <wilson@supremetyrant.com>
2007-09-14 02:18:16 -0400

    Hopefully full StringIO coverage now

--- spec/library/stringio_spec.rb
+++ spec/library/stringio_spec.rb
@@ -11,6 +11,19 @@ def util_build_stringio
   @io = StringIO.new(str)
 end
 
+describe "StringIO.open" do
+  it "should yield the IO object to the block" do
+    sio = nil
+    StringIO.open("abc") do |io|
+      io.string.should == 'abc'
+      io.read(2).should == 'ab'
+      io.closed?.should == false
+      sio = io
+    end
+    sio.closed?.should == true
+  end
+end
+
 describe "StringIO#<<" do
   it "should append to the string when in append mode" do
     @io = StringIO.new("example", 'a')
@@ -239,6 +252,13 @@ describe "StringIO#lineno" do
     6.times { @io.gets }
     @io.lineno.should == 7
   end
+
+  it "should update the line number after readline, as well as gets" do
+    @io.readline
+    @io.readline
+    @io.readline
+    @io.lineno.should == 3
+  end
 end
 
 describe "StringIO#lineno=" do
@@ -332,10 +352,345 @@ describe "StringIO#putc" do
     @io.string.should == 'AAA'
   end
 end
-__END__
 
-describe "StringIO#" do
+describe "StringIO#puts" do
   before(:each) do
     @io = StringIO.new('')
   end
+
+  it "should write a newline after objects that do not end in newlines" do
+    @io.puts(5).should == nil
+    @io.string.should == "5\n"
+  end
+
+  it "should not write a newline after objects that end in newlines" do
+    @io.puts("5\n").should == nil
+    @io.string.should == "5\n"
+  end
 end
+
+describe "StringIO#read" do
+  before(:each) do
+    util_build_stringio
+  end
+
+  it "should read at most 'length' bytes" do
+    @io.read(666).should == @io.string
+  end
+
+  it "should read to the end of the string if length is omitted" do
+    @io.read.should == @io.string
+    @io.read.should == ""
+    @io.read(nil).should == ""
+    @io.read(50).should == nil
+  end
+
+  it "should only support String buffers" do
+    should_raise(TypeError) { @io.read(5, []) }
+    @io.pos.should == 0
+  end
+
+  it "should read data into a buffer string if provided" do
+    @io = StringIO.new('buffered')
+    buf = ""
+    @io.read(5, buf).object_id.should == buf.object_id
+    buf.should == "buffe"
+    @io.read(1, buf)
+    buf.should == 'r'
+  end
+end
+
+
+describe "StringIO#readchar" do
+  before(:each) do
+    @io = StringIO.new('abc')
+  end
+
+  it "should return character codes one at a time" do
+    @io.readchar.should == ?a
+    @io.readchar.should == ?b
+    @io.readchar.should == ?c
+  end
+
+  it "should raise EOFError at the end of the string" do
+    3.times { @io.readchar }
+    should_raise(EOFError) { @io.readchar }
+  end
+end
+
+describe "StringIO#readline" do
+  before(:each) do
+    @io = StringIO.new("r e a d")
+  end
+
+  it "should return the next 'line'" do
+    @io.readline.should == 'r e a d'
+  end
+
+  it "should raise EOFError at the end" do
+    @io.readline
+    should_raise(EOFError) { @io.readline }
+  end
+
+  it "should raise IOError when it is not open for reading" do
+    @io.close_read
+    should_raise(IOError) { @io.readline }
+  end
+
+  it "should support separator strings" do
+    @io.gets('e').should == 'r e'
+    @io.gets('e').should == ' a d'
+  end
+end
+
+describe "StringIO#readlines" do
+  before(:each) do
+    @io = StringIO.new("line1\nline2\nline3\n")
+  end
+
+  it "should return an array of lines" do
+    @io.readlines.should == ["line1\n", "line2\n", "line3\n"]
+  end
+
+  it "should raise IOError when it is not open for reading" do
+    @io.close_read
+    should_raise(IOError) { @io.readlines }
+  end
+
+  it "should return the rest of the stream when separator is nil" do
+    @io.read(4)
+    @io.readlines(nil).should == ["1\nline2\nline3\n"]
+  end
+
+  it "should optionally accept a separator string" do
+    @io.readlines('line').should == ["line", "1\nline", "2\nline", "3\n"]
+  end
+end
+
+describe "StringIO#rewind" do
+  before(:each) do
+    @io = StringIO.new("hello\nworld")
+  end
+
+  it "should reset the position" do
+    @io.gets
+    @io.pos.should == 6
+    @io.rewind
+    @io.pos.should == 0
+  end
+
+  it "should reset the line number" do
+    @io.gets
+    @io.lineno.should == 1
+    @io.rewind
+    @io.lineno.should == 0
+  end
+end
+
+describe "StringIO#seek" do
+  before(:each) do
+    @io = StringIO.new("12345678")
+  end
+
+  it "should seek to an absolute position" do
+    @io.seek(5).should == 0
+    @io.read(1).should == '6'
+  end
+
+  it "should seek from the current position" do
+    @io.read(1)
+    @io.seek(1, IO::SEEK_CUR)
+    @io.read(1).should == '3'
+  end
+
+  it "should seek from the end of the IO" do
+    @io.seek(1, IO::SEEK_END)
+    @io.read(1).should == nil
+    @io.seek(-2, IO::SEEK_END)
+    @io.read(1).should == '7'
+  end
+end
+
+describe "StringIO#string" do
+  it "should return the underlying string" do
+    str = "hello"
+    @io = StringIO.new(str)
+    @io.string.should == str
+    @io.string.object_id.should == str.object_id
+  end
+end
+
+describe "StringIO#string=" do
+  before(:each) do
+    @io = StringIO.new("example\nstring")
+  end
+
+  it "should change the underlying string" do
+    str = "hello"
+    @io.string = str
+    @io.string.should == str
+    @io.string.object_id.should == str.object_id
+  end
+
+  it "should reset the position" do
+    @io.read(1)
+    @io.pos.should == 1
+    @io.string = "other"
+    @io.pos.should == 0
+    @io.read(1).should == 'o'
+  end
+
+  it "should reset the line number" do
+    @io.gets
+    @io.lineno.should == 1
+    @io.string = "other"
+    @io.lineno.should == 0
+    @io.gets.should == "other"
+  end
+end
+
+describe "StringIO#sync" do
+  before(:each) do
+    @io = StringIO.new('')
+  end
+
+  it "should be true" do
+    @io.sync.should == true
+  end
+end
+
+describe "StringIO#sync=" do
+  before(:each) do
+    @io = StringIO.new('')
+  end
+
+  it "should not change 'sync' status" do
+    @io.sync = false
+    @io.sync.should == true
+  end
+end
+
+describe "StringIO#sysread" do
+  before(:each) do
+    util_build_stringio
+  end
+
+  it "should read at most 'length' bytes" do
+    @io.sysread(666).should == @io.string
+  end
+
+  it "should read to the end of the string if length is omitted" do
+    @io.sysread.should == @io.string
+  end
+
+  it "should raise EOFError after the end of the string" do
+    @io.sysread
+    should_raise(EOFError) { @io.sysread }
+    should_raise(EOFError) { @io.sysread(5) }
+  end
+
+  it "should only support String buffers" do
+    should_raise(TypeError) { @io.sysread(5, []) }
+    @io.pos.should == 0
+  end
+
+  it "should read data into a buffer string if provided" do
+    @io = StringIO.new('buffered')
+    buf = ""
+    @io.sysread(5, buf).object_id.should == buf.object_id
+    buf.should == "buffe"
+  end
+end
+
+describe "StringIO#write and syswrite" do
+  before(:each) do
+    @io = StringIO.new('12345')
+  end
+
+  it "should write at the current buffer position" do
+    @io.read(2)
+    @io.write('x').should == 1
+    @io.string.should == '12x45'
+    @io.syswrite(7).should == 1
+    @io.string.should == '12x75'
+  end
+
+  it "should pad with null bytes if the position is after the end" do
+    @io.pos = 8
+    @io.write('x')
+    @io.string.should == "12345\000\000\000x"
+    @io.syswrite(9)
+    @io.string.should == "12345\000\000\000x9"
+  end
+end
+
+describe "StringIO#truncate" do
+  before(:each) do
+    @io = StringIO.new('123456789')
+  end
+
+  # TODO - ri error - says truncate always returns 0
+  it "should truncate the underlying string" do
+    @io.truncate(4).should == 4
+    @io.string.should == '1234'
+  end
+
+  it "should not update the position" do
+    @io.read(5)
+    @io.truncate(3)
+    @io.pos.should == 5
+  end
+end
+
+describe "StringIO#ungetc" do
+  before(:each) do
+    @io = StringIO.new('1234')
+  end
+
+  it "should write a character before the current position" do
+    @io.read(1)
+    @io.ungetc(65)
+    @io.string.should == 'A234'
+  end
+
+  it "should rewind the current position by one" do
+    @io.read(2)
+    @io.pos.should == 2
+    @io.ungetc(65)
+    @io.pos.should == 1
+  end
+
+  it "should do nothing when pos == 0" do
+    @io.ungetc(65)
+    @io.string.should == '1234'
+  end
+
+  it "should not accept strings" do
+    should_raise(TypeError) { @io.ungetc('A') }
+  end
+end
+
+__END__
+
+# TODO - Reopen is utterly broken in MRI right now
+# File bug report
+describe "StringIO#reopen" do
+  before(:each) do
+    @io = StringIO.new('hello','a')
+  end
+
+  it "should reopen a stream in the same mode" do
+    @io.reopen('goodbye').should == @io
+    @io.string.should == 'goodbye'
+    @io << 'x'
+    @io.string.should == 'goodbyex'
+  end
+
+  it "should reopen a stream in a different mode" do
+    @io.reopen('goodbye', 'w').should == @io
+    @io.string.should == 'goodbye'
+    @io << 'x'
+    @io.string.should == 'xoodbye'
+  end
+end
+
