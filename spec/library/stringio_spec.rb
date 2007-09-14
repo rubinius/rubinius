92da0550bd32db984fbb54f105b9701867d4faf9
Wilson Bilkovich <wilson@supremetyrant.com>
2007-09-14 00:38:55 -0400

    More StringIO specs

--- spec/library/stringio_spec.rb
+++ spec/library/stringio_spec.rb
@@ -195,13 +195,61 @@ describe "StringIO#gets" do
   end
 end
 
-describe "StringIO#size" do
+describe "StringIO#size and length" do
   before(:each) do
     util_build_stringio
   end
 
   it "should return the length of the wrapped string" do
     @io.size.should == 37
+    @io.length.should == 37
+  end
+end
+
+describe "StringIO#tty? and isatty" do
+  before(:each) do
+    @io = StringIO.new('tty')
+  end
+
+  it "should be false" do
+    @io.tty?.should == false
+    @io.isatty.should == false
+  end
+end
+
+describe "StringIO#lineno" do
+  before(:each) do
+    util_build_stringio
+  end
+
+  it "should only count lines that have been read 'line-wise'" do
+    @io.read(@io.size)
+    @io.lineno.should == 0
+  end
+
+  it "should match the newline count after each_line" do
+    @io.each_line {|l| l }
+    @io.lineno.should == 4
+  end
+
+  it "should match the number of 'lines' read, even if they are not lines" do
+    @io.gets('a')
+    @io.gets('c')
+    @io.gets('h')
+    6.times { @io.gets }
+    @io.lineno.should == 7
+  end
+end
+
+describe "StringIO#lineno=" do
+  before(:each) do
+    util_build_stringio
+  end
+
+  it "should update the current line number but not advance the position" do
+    @io.lineno = 2
+    @io.gets.should == "    each\n"
+    @io.lineno.should == 3
   end
 end
 
