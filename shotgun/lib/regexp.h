352b4ff6555677533079c3eecade9e4d9062be21
Yehuda Katz <wycats@gmail.com>
2007-09-17 15:56:13 -0700

    Missing header file

--- shotgun/lib/regexp.h
+++ shotgun/lib/regexp.h
@@ -3,4 +3,5 @@ char *regexp_version(STATE);
 OBJECT regexp_new(STATE, OBJECT pattern, OBJECT options);
 void regexp_cleanup(STATE, OBJECT regexp);
 OBJECT regexp_match(STATE, OBJECT regexp, OBJECT string);
+OBJECT regexp_scan(STATE, OBJECT regexp, OBJECT string);
 OBJECT regexp_options(STATE, OBJECT regexp);
