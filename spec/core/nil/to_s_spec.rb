8661cdb78cd2c4afa0fb231aa9cc959e338e097e
Arthur Schreiber <schreiber.arthur@gmail.com>
2007-09-22 22:35:11 +0200

    Extended nil#to_s spec.

--- spec/core/nil/to_s_spec.rb
+++ spec/core/nil/to_s_spec.rb
@@ -4,4 +4,8 @@ describe "NilClass#to_s" do
   it "returns the string ''" do
     nil.to_s.should == ""
   end
+
+  it "does not cause NilClass to be coerced to Fixnum" do
+    ("" == nil).should == false
+  end
 end
