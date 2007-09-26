5d46933362b8c54cb5d0370bbf61e063459de514
Arthur Schreiber <schreiber.arthur@gmail.com>
2007-09-26 13:14:41 +0200

    Added specs for Symbol.all_symbols.

--- spec/core/symbol/all_symbols_spec.rb
+++ spec/core/symbol/all_symbols_spec.rb
@@ -1,5 +1,9 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "Symbol.all_symbols" do
+  it "returns an array containing all the Symbols in the symbol table" do
+    Symbol.all_symbols.is_a?(Array).should == true
+    Symbol.all_symbols.all? { |s| s.is_a?(Symbol) }.should == true
+  end
   # it "is a pain in the ass to test..."
 end
