b3324808584d7b4ee6af58d98eeb7c2162c31208
Brian Ford <brixen@gmail.com>
2007-09-27 10:57:46 -0700

    Commit #180, Jason Barton's specs for Module#undef_method, #remove_method.

--- spec/core/module/remove_method_spec.rb
+++ spec/core/module/remove_method_spec.rb
@@ -1,2 +1,47 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+module ModuleSpecs
+  class NoInheritance
+    def method_to_remove; 1; end
+    remove_method :method_to_remove
+  end
+  
+  class Parent
+    def method_to_remove; 1; end
+  end
+  
+  class Child < Parent
+    def method_to_remove; 2; end     
+    remove_method :method_to_remove
+  end
+  
+  class First
+    def method_to_remove; 1; end
+  end
+  
+  class Second < First
+    def method_to_remove; 2; end
+  end
+end
+
+describe "Module#remove_method" do
+  it "removes the method from a class" do
+    x = ModuleSpecs::NoInheritance.new
+    x.respond_to?(:method_to_remove).should == false
+  end
+  
+  it "removes method from subclass, but not parent" do
+    x = ModuleSpecs::Child.new
+    x.respond_to?(:method_to_remove).should == true    
+    x.method_to_remove.should == 1
+  end
+  
+  it "raises NameError when attempting to remove method further up the inheritance tree" do
+    should_raise(NameError, "method `method_to_remove' not defined in Third") do
+      class Third < ModuleSpecs::Second
+        remove_method :method_to_remove
+      end
+    end
+  end
+end
