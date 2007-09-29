aec17c172d59e3cc31e05384ed37a709eb70ad14
Brian Ford <brixen@gmail.com>
2007-09-29 14:23:18 -0700

    Added Sampler output. Also lib/profile.rb and lib/profiler.rb.

--- kernel/core/sampler.rb
+++ kernel/core/sampler.rb
@@ -5,16 +5,36 @@ class Sampler
   
   def start
     @start_clock = activate(@frequency)
+    nil
   end
     
   def stop
     @results, @last_clock, @interval = terminate()
+    nil
   end
   
-  def display
-    puts "Clocks: #{@start_clock} / #{@last_clock}, Interval: #{@interval} useconds"
+  def display(out=STDOUT)
+    # "Clocks: #{@start_clock} / #{@last_clock}, Interval: #{@interval} useconds"
+    calls = Hash.new { |h,k| h[k] = 0 }
+    total_slices = 0
+
     @results.each do |ent|
-      puts "Object: #{ent.receiver.inspect} / Method: #{ent.method_module}##{ent.name}"
+      if ent.method_module.is_a?(MetaClass)
+        name = "#{ent.method_module.attached_instance.inspect}.#{ent.name}"
+      else
+        name = "#{ent.method_module.name}##{ent.name}"
+      end
+      # FIXME: calls[name] += 1 always sets calls[name] to 1
+      calls[name] = calls[name] + 1
+      total_slices += 1
     end
+
+    # FIXME: [[:a,1],[:b,2]].each { |a, b| ... } => a == [:a, 1], b == nil
+    out.printf " %% time   slices   name\n"
+    calls.sort { |a, b| b[1] <=> a[1] }.each do |entry|
+      percent_time = 100.0 * entry[1] / total_slices
+      out.printf " %6.2f %8d   %s\n", percent_time, entry[1], entry[0]
+    end
+    nil
   end
 end
---
+++ lib/profile.rb
@@ -0,0 +1,6 @@
+require 'profiler'
+
+at_exit do
+  Profiler__::print_profile(STDERR)
+end
+Profiler__::start_profile
