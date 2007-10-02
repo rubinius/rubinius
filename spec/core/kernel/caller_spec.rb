9ad0c1428df70c9fd9e0081651e3b60cf5773267
Martin Kuehl <martin.kuehl@gmail.com>
2007-10-03 01:30:46 +0200

    Add spec for checking the argument handling of Kernel#caller.

--- spec/core/kernel/caller_spec.rb
+++ spec/core/kernel/caller_spec.rb
@@ -20,4 +20,10 @@ describe 'Kernel#caller' do
     c(0)[1][-24..-1].should == "caller_spec.rb:11:in `b'"
     c(0)[2][-24..-1].should == "caller_spec.rb:14:in `c'"
   end
+  
+  it "omits a number of frames corresponding to the parameter" do
+    c(0)[1..-1].should == c(1)
+    c(0)[2..-1].should == c(2)
+    c(0)[3..-1].should == c(3)
+  end
 end
