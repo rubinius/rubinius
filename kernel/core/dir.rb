d22b3ae9af08683ea74247e4997cc2c73ca29c75
Kevin Clark <kevin.clark@gmail.com>
2007-10-01 17:51:05 -0700

    Set dir.rb to depend on array (for FFI::Struct.layout)
    Change Dir#read to use DirEntry rather than generic FFI::Struct

--- kernel/core/dir.rb
+++ kernel/core/dir.rb
@@ -1,19 +1,22 @@
+# depends on: array.rb
+
 class Dir  
-  # class DirEntry < FFI::Struct            
-  #   # struct dirent {
-  #   #   ino_t d_ino;      /* file number of entry */
-  #   #   __uint16_t d_reclen;    /* length of this record */
-  #   #   __uint8_t  d_type;    /* file type, see below */
-  #   #   __uint8_t  d_namlen;    /* length of string in d_name */
-  #   #   char d_name[__DARWIN_MAXNAMLEN + 1];  /* name must be no longer than this */
-  #   # };
-  #   
-  #   layout :d_ino,    :uint,
-  #          :d_reclen, :ushort,
-  #          :d_type,   :uchar,
-  #          :d_namlen, :uchar,
-  #          :d_name,   :strptr    
-  # end
+  class DirEntry < FFI::Struct            
+    # struct dirent {
+    #   ino_t d_ino;      /* file number of entry */
+    #   __uint16_t d_reclen;    /* length of this record */
+    #   __uint8_t  d_type;    /* file type, see below */
+    #   __uint8_t  d_namlen;    /* length of string in d_name */
+    #   char d_name[__DARWIN_MAXNAMLEN + 1];  /* name must be no longer than this */
+    # };
+    
+    layout  :d_ino,    :uint,
+            :d_reclen, :ushort,
+            :d_type,   :uchar,
+            :d_namlen, :uchar,
+            :d_name,   :char_array
+      
+  end
   
   def self.glob(pattern, flags)
     Ruby.primitive :dir_glob
@@ -65,20 +68,9 @@ class Dir
   end
   
   def read
-    # TODO: DirEntry should subclass FFI::Struct instead of using
-    # the struct constructor. Can't do it right now because splats are
-    # broken, so FFI::Struct.layout is broken -- KC, 9/29/07
     dir_entry_ptr = Platform::POSIX.readdir(@dirptr)
-    
     return nil if dir_entry_ptr.null?
-    
-    s = FFI::Struct.new(dir_entry_ptr, 
-           :d_ino,    :uint,
-           :d_reclen, :ushort,
-           :d_type,   :uchar,
-           :d_namlen, :uchar,
-           :d_name,   :char_array)
-    s[:d_name]
+    DirEntry.new(dir_entry_ptr)[:d_name]
   end
 
   class << self
