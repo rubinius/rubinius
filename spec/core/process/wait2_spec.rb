7b79130f38925cb48712be617cef5a80c71f0ba4
Brian Ford <brixen@gmail.com>
2007-10-02 14:29:42 -0700

    Commit #5 (LH) Charles Comstock Process.wait2 spec.

--- spec/core/process/wait2_spec.rb
+++ spec/core/process/wait2_spec.rb
@@ -1,1 +1,17 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "Process.wait2" do
+  it "should return the pid and status of child process" do
+    pidf = Process.fork { exit 99 }
+    results = Process.wait2
+    results.size.should == 2
+    pidw,status = results
+    (pidf == pidw).should == true
+    status.exitstatus.should == 99
+  end
+
+  it "should raise a StandardError if no child processes exist" do
+    should_raise(Errno::ECHILD) { Process.wait2 }
+    should_raise(StandardError) { Process.wait2 }
+  end
+end
