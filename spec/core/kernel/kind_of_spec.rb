4583be7e7ed76e5843dcb396f8bae735f341de73
Martin Kuehl <martin.kuehl@gmail.com>
2007-10-03 02:03:58 +0200

    Remove superfluous whitespace from Object#kind_of? specs.

--- spec/core/kernel/kind_of_spec.rb
+++ spec/core/kernel/kind_of_spec.rb
@@ -3,42 +3,41 @@ require File.dirname(__FILE__) + '/fixtures/classes'
 
 @kernel_kind_of = shared "Kernel#kind_of" do |cmd|
   describe "Object##{cmd}" do
-    it "returns true if class is the class of obj, or if class is one of the superclasses of obj or modules included in obj (String example)" do    
+    it "returns true if class is the class of obj, or if class is one of the superclasses of obj or modules included in obj (String example)" do
       s = "hello"
       s.send(cmd, String).should == true
       s.send(cmd, Object).should == true
-      s.send(cmd, Class).should == false 
+      s.send(cmd, Class).should == false
     end
 
     it "returns true if class is the class of obj, or if class is one of the superclasses of obj or modules included in obj Array example" do
       a = []
       a.send(cmd, Array).should == true
       a.send(cmd, Enumerable).should == true
-    end    
+    end
           
-    module KernelSpecs::M;    end
+    module KernelSpecs::M; end
     class KernelSpecs::X; include KernelSpecs::M; end
     class KernelSpecs::Y < KernelSpecs::X; end
     class KernelSpecs::Z < KernelSpecs::Y; end 
   
-    it "returns true if class is the class of obj, or if class is one of the superclasses of obj or modules included in obj Custom class example" do     
+    it "returns true if class is the class of obj, or if class is one of the superclasses of obj or modules included in obj Custom class example" do
       y = KernelSpecs::Y.new
-      y.send(cmd, KernelSpecs::X).should == true       
-      y.send(cmd, KernelSpecs::Y).should == true       
-      y.send(cmd, KernelSpecs::Z).should == false       
+      y.send(cmd, KernelSpecs::X).should == true
+      y.send(cmd, KernelSpecs::Y).should == true
+      y.send(cmd, KernelSpecs::Z).should == false
       y.send(cmd, KernelSpecs::M).should == true
     end
     
-    it "nil.#{cmd} cases specs"  do 
-      (nil.send(cmd,  NilClass)).should == true
+    it "nil.#{cmd} cases specs"  do
+      (nil.send(cmd, NilClass)).should == true
       (nil.send(cmd, Object)).should == true
       (nil.send(cmd, Module)).should == false
       (nil.send(cmd, Kernel)).should == true
-      
     end
-  end 
+  end
 end
 
 describe "Kernel#kind_of?" do
-  it_behaves_like(@kernel_kind_of , :kind_of?) 
+  it_behaves_like(@kernel_kind_of , :kind_of?)
 end
