c1b17108a78a4dc5d3e224158f8f9d76232003e6
Charles Comstock <dgtized@gmail.com>
2007-10-03 17:42:06 -0500

    basic specs for Kernel#sleep

--- spec/core/kernel/sleep_spec.rb
+++ spec/core/kernel/sleep_spec.rb
@@ -1,2 +1,15 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Kernel.time" do
+  it "should wait 0.1 second" do
+    t0 = Time.now
+    sleep 0.1
+    t1 = Time.now
+    diff = (t1 - t0 - 0.1).abs
+    (diff < 0.01).should == true
+
+    # this is a base case -- it doesn't check anything concerning
+    # sleep calls in interrupted threads
+  end
+end
