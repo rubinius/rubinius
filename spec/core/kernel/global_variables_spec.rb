00417283b36dcb58da82c6fc2e9be7580de945b0
Charles Comstock <dgtized@gmail.com>
2007-10-03 14:15:25 -0500

    added specs for Kernel#global_variables

--- spec/core/kernel/global_variables_spec.rb
+++ spec/core/kernel/global_variables_spec.rb
@@ -1,2 +1,13 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Kernel.global_variables" do
+  it "should find subset starting with std" do
+    (global_variables.grep /std/).should == ["$stdout","$stdin","$stderr"]
+    a = global_variables.size
+    global_variables.include?("$foolish_global_var").should == false
+    eval("$foolish_global_var = 1")
+    global_variables.size.should == a+1
+    global_variables.include?("$foolish_global_var").should == true
+  end
+end
