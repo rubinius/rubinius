422e45f210ec9dc2438ae3b11544823bb6ffdd50
Jason Yates <jaywhy@gmail.com>
2007-10-04 16:21:27 -0400

    added specs for FileTest#exists? and FileTest#exist?

--- spec/core/filetest/exist_spec.rb
+++ spec/core/filetest/exist_spec.rb
@@ -1,1 +1,25 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+@filetest_exist = shared "FileTest.exist?" do |cmd|
+  describe "FileTest.#{cmd}" do
+    it "return true if the file exist" do
+      FileTest.exist?(__FILE__).should == true
+      FileTest.exist?('a_fake_file').should == false
+    end
+  
+    it "return true if the file exist using the alias exists?" do 
+      FileTest.exists?(__FILE__).should == true
+      FileTest.exists?('a_fake_file').should == false
+    end
+  
+    it "raise an exception if the argumnent is not from the correct type or are missing" do
+      should_raise(ArgumentError){ FileTest.exist? }
+      should_raise(ArgumentError){ FileTest.exist?(__FILE__, __FILE__) }
+      should_raise(TypeError){ FileTest.exist?(nil) }
+    end 
+  end
+end
+
+describe "FileTest.exist?" do
+  it_behaves_like(@filetest_exist, :exist?)
+end
--- spec/core/filetest/exists_spec.rb
+++ spec/core/filetest/exists_spec.rb
@@ -1,1 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/exist_spec'
+
+describe "FileTest.exists?" do
+  it_behaves_like(@filetest_exist, :exists?)
+end
