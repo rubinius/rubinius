778e11d2df647cf91a712bb30df34152c71dbc3f
Martin Kuehl <martin.kuehl@gmail.com>
2007-10-05 11:42:40 +0200

    Don't work around rbx bugs in Hash specs.

--- spec/core/hash/hash_spec.rb
+++ spec/core/hash/hash_spec.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "Hash" do
   it "includes Enumerable" do
-    Hash.ancestors.include?(Enumerable).should == true
+    Hash.include?(Enumerable).should == true
   end
 end
