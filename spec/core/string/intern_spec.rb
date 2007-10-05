ba0f4ef5405665c84af4410d70be5ef911a93195
Yehuda Katz <wycats@gmail.com>
2007-10-04 22:58:50 -0700

    Adds the intern spec from Ticket #8

--- spec/core/string/intern_spec.rb
+++ spec/core/string/intern_spec.rb
@@ -1,3 +1,9 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes.rb'
 
+describe "String#intern" do
+  it "returns a symbol of itself" do
+    "hello there".intern.should == :"hello there"
+    "hello".intern.should == :hello
+  end
+end
