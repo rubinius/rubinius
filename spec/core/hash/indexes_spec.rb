52f903938f4eacf4465f7a36cacb25aa662aa559
Martin Kuehl <martin.kuehl@gmail.com>
2007-10-09 13:44:02 +0200

    Turn Hash#values_at into a shared spec.

--- spec/core/hash/indexes_spec.rb
+++ spec/core/hash/indexes_spec.rb
@@ -1,10 +1,7 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+require File.dirname(__FILE__) + '/values_at_spec'
 
 describe "Hash#indexes" do
-  it "is a DEPRECATED synonyms for values_at" do
-    h = {:a => 9, :b => 'a', :c => -10, :d => nil}
-    h.indexes(:a, :d, :b).should == h.values_at(:a, :d, :b)
-    h.indexes().should == h.values_at()
-  end
+  it_behaves_like(@hash_values_at, :indexes)
 end
--- spec/core/hash/indices_spec.rb
+++ spec/core/hash/indices_spec.rb
@@ -1,10 +1,7 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+require File.dirname(__FILE__) + '/values_at_spec'
 
 describe "Hash#indices" do
-  it "is a DEPRECATED synonyms for values_at" do
-    h = {:a => 9, :b => 'a', :c => -10, :d => nil}
-    h.indices(:a, :d, :b).should == h.values_at(:a, :d, :b)
-    h.indices().should == h.values_at()
-  end
+  it_behaves_like(@hash_values_at, :indices)
 end
