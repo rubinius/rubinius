e9977ae9364d08ab1db2348f0822abe9e0b64f5e
Brian Ford <brixen@gmail.com>
2007-10-10 23:44:40 -0700

    Added --gdb and --valgrind options to bin/mspec.
    Removed obsolete scripts from bin.

--- bin/audit_compiler.rb
+++
@@ -1,9 +0,0 @@
-Dir["kernel/bootstrap/*.rb"].each do |file|
-  puts file
-  system("ruby bin/compare_assembly.rb #{file}")
-end
-
-Dir["kernel/core/*.rb"].each do |file|
-  puts file
-  system("ruby bin/compare_assembly.rb #{file}")
-end
