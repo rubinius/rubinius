f9177eb198003b495f485a13910808fe603030ad
Ben Curren <ben@esomnie.com>
2007-10-11 20:54:22 -0700

    Added a failing test for setting and getting constants on an instance of a module.

--- spec/core/module/initialize_spec.rb
+++ spec/core/module/initialize_spec.rb
@@ -1,2 +1,11 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Module#initialize" do
+  it "sets the constant with the given name to the given value" do
+    m = Module.new do
+      const_set :A, "A"
+    end
+    m.const_get("A").should == "A"
+  end
+end
