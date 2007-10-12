b068c8634b56cd9129f9fc7c309bfa81869209c8
Charles Comstock <dgtized@gmail.com>
2007-10-12 18:21:52 -0500

    specs for behavior of throw/catch and how they interact with ensure

--- spec/language/ensure_spec.rb
+++ spec/language/ensure_spec.rb
@@ -1,1 +1,16 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "The ensure keyword" do
+  it "should execute as a result of a throw within it's block" do
+    i = []
+    catch(:exit) do
+      begin
+        i << :begin
+        throw :exit
+      ensure
+        i << :ensure
+      end      
+    end
+    i.should == [:begin,:ensure]
+  end  
+end
--- spec/language/throw_spec.rb
+++ spec/language/throw_spec.rb
@@ -1,1 +1,60 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "The throw keyword" do
+  it "should abandon processing" do
+    i = 0
+    catch(:done) do
+      loop do
+        i += 1
+        throw :done if i > 4
+      end
+      i += 1
+    end    
+    i.should == 5
+  end
+
+  it "should support a second parameter" do
+    msg = catch(:exit) do
+      throw :exit,:msg
+    end
+    msg.should == :msg
+  end
+
+  it "should use nil as a default second parameter" do
+    msg = catch(:exit) do
+      throw :exit
+    end
+    msg.should == nil
+  end
+
+  it "should support nesting" do
+    i = []
+    catch(:exita) do
+      i << :a
+      catch(:exitb) do
+        i << :b
+        throw :exita
+      end
+      i << :b_exit
+    end
+    i << :a_exit
+
+    i.should == [:a,:b,:a_exit]
+  end
+
+  it "should unwind stack from within a method" do
+    def throw_method(handler,val)
+      throw handler,val
+    end
+
+    catch(:exit) do
+      throw_method(:exit,5)
+    end.should == 5
+  end
+
+  it "should raise a name error if outside of scope of a matching catch" do    
+    should_raise(NameError) { throw :test,5 }
+    should_raise(NameError) { catch(:different) { throw :test,5 } }
+  end
+end
+
