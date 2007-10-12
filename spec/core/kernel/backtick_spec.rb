01e27ea5fa0e0f0d170cd88f128adfbb2a2703bd
Martin Kuehl <martin.kuehl@gmail.com>
2007-10-12 22:01:43 +0200

    Add spec for backticks and their setting of $?.

--- spec/core/kernel/backtick_spec.rb
+++ spec/core/kernel/backtick_spec.rb
@@ -1,2 +1,23 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Kernel#`" do
+  it "returns the result of the executed sub-process" do
+    ip = 'world'
+    `echo disc #{ip}`.should == "disc world\n"
+  end
+
+  it "sets $? to the exit status of the executed sub-process" do
+    ip = 'world'
+    `echo disc #{ip}`
+    $?.class.should == Process::Status
+    $?.stopped?.should == false
+    $?.exited?.should == true
+    $?.exitstatus.should == 0
+    `echo disc #{ip}; exit 99`
+    $?.class.should == Process::Status
+    $?.stopped?.should == false
+    $?.exited?.should == true
+    $?.exitstatus.should == 99
+  end
+end
