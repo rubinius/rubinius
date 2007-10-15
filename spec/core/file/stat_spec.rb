ae613272bcf0c260ad3da00ffd14d4a76422ac46
Tilman Sauerbeck <tilman@code-monkey.de>
2007-10-15 08:30:11 +0200

    Added the beginnings of a File.stat spec.

--- spec/core/file/stat_spec.rb
+++ spec/core/file/stat_spec.rb
@@ -1,1 +1,16 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "File.stat" do
+  before :each do
+    @file = '/tmp/i_exist'
+    File.open(@file,'w'){|f| f.write 'rubinius'}
+  end
+
+  after :each do
+    File.delete(@file) if File.exist?(@file)
+  end
+
+  it "should return a File::Stat object if the given file exists" do
+    File.stat(@file).path.should == "/tmp/i_exist"
+  end
+end
