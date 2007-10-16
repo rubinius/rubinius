e158c3130f033a1029ae26888b8e7e541f2b388a
Charles Comstock <dgtized@gmail.com>
2007-10-15 19:46:58 -0500

    spec for __LINE__ added

--- spec/language/line_spec.rb
+++ spec/language/line_spec.rb
@@ -1,3 +1,19 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# specs for __LINE__
+describe "The __LINE__ constant" do
+  it "should increment for each line" do    
+    cline = __LINE__
+    __LINE__.should == cline + 1
+    # comment is at cline + 2
+    __LINE__.should == cline + 3
+  end
+
+  it "should be eval aware" do
+    eval("__LINE__").should == 1    
+    cmd =<<EOF
+# comment at line 1
+__LINE__
+EOF
+    eval(cmd).should == 2
+  end
+end
