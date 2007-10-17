271bc31ba814e68fb414ebf29cf9648f57fe4cb6
David Altenburg <david@gensym.org>
2007-10-17 00:39:59 -0500

    Added to fork spec: check that fork returns a nil pid for the child process

--- spec/core/process/fork_spec.rb
+++ spec/core/process/fork_spec.rb
@@ -1,1 +1,22 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "Process.fork" do
+  before :each do
+    @file = '/tmp/i_exist'
+  end
+  
+  after :each do
+    File.delete(@file) if File.exist?(@file)
+  end
+  
+  it "return nil for the child process" do
+    child_id = Process.fork
+    if child_id == nil
+      File.open(@file,'w'){|f| f.write 'rubinius'}
+      Process.exit!
+    else
+      Process.waitpid(child_id)
+    end
+    File.exist?(@file).should == true
+  end
+end
