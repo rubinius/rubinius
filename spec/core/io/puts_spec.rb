5cf41abaff9dc04cdba5fe50492d4ebfdde2a274
Jon Guymon <gnarg@kiryo.(none)>
2007-10-17 01:55:01 -0400

    normalized specs for IO#puts StringIO#puts and Kernel#puts

--- spec/core/io/puts_spec.rb
+++ spec/core/io/puts_spec.rb
@@ -1,7 +1,5 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-DEFAULT_RECORD_SEPARATOR = $/
-
 # TODO: need to find a better way to test this. Too fragile to set expectations
 # to each write call. Only care that all the characters are sent not the number
 # or write calls. Also, these tests do not make sure the ordering of the write calls
@@ -11,48 +9,67 @@ describe "IO#puts" do
     @io = IO.new(2)
   end
   
-  it "no args" do
-    @io.should_receive(:write, :with => [DEFAULT_RECORD_SEPARATOR])
+  it "writes just a newline when given no args" do
+    @io.should_receive(:write, :with => ["\n"])
     @io.puts.should == nil
   end
   
-  it "nil arg" do
+  it "writes nil with a newline when given nil as an arg" do
     @io.should_receive(:write, :with => ["nil"])
-    @io.should_receive(:write, :with => [DEFAULT_RECORD_SEPARATOR])
+    @io.should_receive(:write, :with => ["\n"])
     @io.puts(nil).should == nil
   end
   
-  it "non string args" do
+  it "calls to_s before writing non-string objects" do
     object = Object.new
     object.should_receive(:to_s, :returning => "hola")
     
     @io.should_receive(:write, :with => ["hola"])
-    @io.should_receive(:write, :with => [DEFAULT_RECORD_SEPARATOR])
+    @io.should_receive(:write, :with => ["\n"])
     @io.puts(object).should == nil
   end
   
-  it "multiple args" do
+  it "writes each arg if given several" do
     @io.should_receive(:write, :with => ["1"])
     @io.should_receive(:write, :with => ["two"])
     @io.should_receive(:write, :with => ["3"])
-    @io.should_receive(:write, :with => [DEFAULT_RECORD_SEPARATOR], :count => 3)
+    @io.should_receive(:write, :with => ["\n"], :count => 3)
     @io.puts(1, "two", 3).should == nil
   end
   
-  it "nested array arg" do
+  it "flattens a nested array before writing it" do
     @io.should_receive(:write, :with => ["1"])
     @io.should_receive(:write, :with => ["2"])
     @io.should_receive(:write, :with => ["3"])
-    @io.should_receive(:write, :with => [DEFAULT_RECORD_SEPARATOR], :count => 3)
+    @io.should_receive(:write, :with => ["\n"], :count => 3)
     @io.puts([1, 2, [3]]).should == nil
   end
   
-  it "recursive array arg" do
+  it "writes [...] for a recursive array arg" do
     x = []
     x << 2 << x
     @io.should_receive(:write, :with => ["2"])
     @io.should_receive(:write, :with => ["[...]"])
-    @io.should_receive(:write, :with => [DEFAULT_RECORD_SEPARATOR], :count => 2)
+    @io.should_receive(:write, :with => ["\n"], :count => 2)
     @io.puts(x).should == nil
+  end
+  
+  it "writes a newline after objects that do not end in newlines" do
+    @io.should_receive(:write, :with => ["5"])
+    @io.should_receive(:write, :with => ["\n"])
+    @io.puts(5).should == nil
+  end
+
+  it "does not write a newline after objects that end in newlines" do
+    @io.should_receive(:write, :with => ["5\n"])
+    @io.puts("5\n").should == nil
+  end
+  
+  it "ignores the $/ separator global" do
+    $/ = ":"
+    @io.should_receive(:write, :with => ["5"])
+    @io.should_receive(:write, :with => ["\n"])
+    @io.puts(5).should == nil
+    $/ = "\n"
   end
 end
