68965dc12ea369f6db64c208cb2ce123c1398bb8
Tilman Sauerbeck <tilman@code-monkey.de>
2007-10-23 10:20:04 +0200

    Extended the Numeric#nonzero? spec a bit.

--- spec/core/numeric/nonzero_spec.rb
+++ spec/core/numeric/nonzero_spec.rb
@@ -3,8 +3,12 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 # Returns num if num is not zero, nil otherwise. 
 # This behavior is useful when chaining comparisons:
 describe "Numeric#nonzero?" do 
-  it "return the vaule if number is different to 0" do 
+  it "should return nil if the number is zero" do
     0.nonzero?.should == nil
+  end
+
+  it "should return the value if number is different to 0" do
     1.nonzero?.should == 1
+    2.nonzero?.should == 2
   end  
 end
