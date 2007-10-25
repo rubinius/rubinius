688f03ac452f698105812c28c29dcc7162b7037c
Adam Gardiner <adam.b.gardiner@gmail.com>
2007-10-25 22:43:20 +1000

    Added specs for operator assignment, i.e. +=, *=, ||= etc

--- spec/language/variables_spec.rb
+++ spec/language/variables_spec.rb
@@ -211,6 +211,301 @@ describe "Conditional assignment" do
   
 end
 
+class OpAsgn
+  attr_accessor :a, :side_effect
+  def do_side_effect; self.side_effect = true; return @a; end
+end
+
+describe "Operator assignment 'var op= expr'" do
+  it "is equivalent to 'var = var op expr'" do
+    x = 13
+    (x += 5).should == 18
+    x.should == 18
+
+    x = 17
+    (x -= 11).should == 6
+    x.should == 6
+
+    x = 2
+    (x *= 5).should == 10
+    x.should == 10
+
+    x = 36
+    (x /= 9).should == 4
+    x.should == 4
+
+    x = 23
+    (x %= 5).should == 3
+    x.should == 3
+    (x %= 3).should == 0
+    x.should == 0
+
+    x = 2
+    (x **= 3).should == 8
+    x.should == 8
+
+    x = 4
+    (x |= 3).should == 7
+    x.should == 7
+    (x |= 4).should == 7
+    x.should == 7
+
+    x = 6
+    (x &= 3).should == 2
+    x.should == 2
+    (x &= 4).should == 0
+    x.should == 0
+
+    # XOR
+    x = 2
+    (x ^= 3).should == 1
+    x.should == 1
+    (x ^= 4).should == 5
+    x.should == 5
+
+    # Bit-shift left
+    x = 17
+    (x <<= 3).should == 136
+    x.should == 136
+
+    # Bit-shift right
+    x = 5
+    (x >>= 1).should == 2
+    x.should == 2
+
+    x = nil
+    (x ||= 17).should == 17
+    x.should == 17
+    (x ||= 2).should == 17
+    x.should == 17
+
+    x = false
+    (x &&= true).should == false
+    x.should == false
+    (x &&= false).should == false
+    x.should == false
+    x = true
+    (x &&= true).should == true
+    x.should == true
+    (x &&= false).should == false
+    x.should == false
+  end
+  
+  it "uses short-circuit arg evaluation for operators ||= and &&=" do
+    x = 8
+    y = OpAsgn.new
+    (x ||= y.do_side_effect).should == 8
+    y.side_effect.should == nil
+    
+    x = nil
+    (x &&= y.do_side_effect).should == nil
+    y.side_effect.should == nil
+
+    y.a = 5
+    (x ||= y.do_side_effect).should == 5
+    y.side_effect.should == true
+  end
+end
+
+describe "Operator assignment 'obj.meth op= expr'" do
+  it "is equivalent to 'obj.meth = obj.meth op expr'" do
+    @x = OpAsgn.new
+    @x.a = 13
+    (@x.a += 5).should == 18
+    @x.a.should == 18
+
+    @x.a = 17
+    (@x.a -= 11).should == 6
+    @x.a.should == 6
+
+    @x.a = 2
+    (@x.a *= 5).should == 10
+    @x.a.should == 10
+
+    @x.a = 36
+    (@x.a /= 9).should == 4
+    @x.a.should == 4
+
+    @x.a = 23
+    (@x.a %= 5).should == 3
+    @x.a.should == 3
+    (@x.a %= 3).should == 0
+    @x.a.should == 0
+
+    @x.a = 2
+    (@x.a **= 3).should == 8
+    @x.a.should == 8
+
+    @x.a = 4
+    (@x.a |= 3).should == 7
+    @x.a.should == 7
+    (@x.a |= 4).should == 7
+    @x.a.should == 7
+
+    @x.a = 6
+    (@x.a &= 3).should == 2
+    @x.a.should == 2
+    (@x.a &= 4).should == 0
+    @x.a.should == 0
+
+    # XOR
+    @x.a = 2
+    (@x.a ^= 3).should == 1
+    @x.a.should == 1
+    (@x.a ^= 4).should == 5
+    @x.a.should == 5
+
+    @x.a = 17
+    (@x.a <<= 3).should == 136
+    @x.a.should == 136
+
+    @x.a = 5
+    (@x.a >>= 1).should == 2
+    @x.a.should == 2
+
+    @x.a = nil
+     (@x.a ||= 17).should == 17
+    @x.a.should == 17
+    (@x.a ||= 2).should == 17
+    @x.a.should == 17
+
+    @x.a = false
+    (@x.a &&= true).should == false
+    @x.a.should == false
+    (@x.a &&= false).should == false
+    @x.a.should == false
+    @x.a = true
+    (@x.a &&= true).should == true
+    @x.a.should == true
+    (@x.a &&= false).should == false
+    @x.a.should == false
+  end
+  
+  it "uses short-circuit arg evaluation for operators ||= and &&=" do
+    x = 8
+    y = OpAsgn.new
+    (x ||= y.do_side_effect).should == 8
+    y.side_effect.should == nil
+    
+    x = nil
+    (x &&= y.do_side_effect).should == nil
+    y.side_effect.should == nil
+
+    y.a = 5
+    (x ||= y.do_side_effect).should == 5
+    y.side_effect.should == true
+  end
+end
+
+describe "Operator assignment 'obj[idx] op= expr'" do
+  it "is equivalent to 'obj[idx] = obj[idx] op expr'" do
+    x = [2,13,7]
+    (x[1] += 5).should == 18
+    x.should == [2,18,7]
+
+    x = [17,6]
+    (x[0] -= 11).should == 6
+    x.should == [6,6]
+
+    x = [nil,2,28]
+    (x[2] *= 2).should == 56
+    x.should == [nil,2,56]
+
+    x = [3,9,36]
+    (x[2] /= x[1]).should == 4
+    x.should == [3,9,4]
+
+    x = [23,4]
+    (x[0] %= 5).should == 3
+    x.should == [3,4]
+    (x[0] %= 3).should == 0
+    x.should == [0,4]
+
+    x = [1,2,3]
+    (x[1] **= 3).should == 8
+    x.should == [1,8,3]
+
+    x = [4,5,nil]
+    (x[0] |= 3).should == 7
+    x.should == [7,5,nil]
+    (x[0] |= 4).should == 7
+    x.should == [7,5,nil]
+
+    x = [3,6,9]
+    (x[1] &= 3).should == 2
+    x.should == [3,2,9]
+    (x[1] &= 4).should == 0
+    x.should == [3,0,9]
+
+    # XOR
+    x = [0,1,2]
+    (x[2] ^= 3).should == 1
+    x.should == [0,1,1]
+    (x[2] ^= 4).should == 5
+    x.should == [0,1,5]
+
+    x = [17]
+    (x[0] <<= 3).should == 136
+    x.should == [136]
+
+    x = [nil,5,8]
+    (x[1] >>= 1).should == 2
+    x.should == [nil,2,8]
+
+    x = [1,nil,12]
+    (x[1] ||= 17).should == 17
+    x.should == [1,17,12]
+    (x[1] ||= 2).should == 17
+    x.should == [1,17,12]
+  
+    x = [true, false, false]
+    (x[1] &&= true).should == false
+    x.should == [true, false, false]
+    (x[1] &&= false).should == false
+    x.should == [true, false, false]
+    (x[0] &&= true).should == true
+    x.should == [true, false, false]
+    (x[0] &&= false).should == false
+    x.should == [false, false, false]
+  end
+
+  it "uses short-circuit arg evaluation for operators ||= and &&=" do
+    x = 8
+    y = OpAsgn.new
+    (x ||= y.do_side_effect).should == 8
+    y.side_effect.should == nil
+    
+    x = nil
+    (x &&= y.do_side_effect).should == nil
+    y.side_effect.should == nil
+
+    y.a = 5
+    (x ||= y.do_side_effect).should == 5
+    y.side_effect.should == true
+  end
+
+  it "handles complex index (idx) arguments" do
+    x = [1,2,3,4]
+    (x[0,2] += [5]).should == [1,2,5]
+    x.should == [1,2,5,3,4]
+    (x[0,2] += [3,4]).should == [1,2,3,4]
+    x.should == [1,2,3,4,5,3,4]
+    
+    (x[2..3] += [8]).should == [3,4,8]
+    x.should == [1,2,3,4,8,5,3,4]
+    
+    y = OpAsgn.new
+    y.a = 1
+    (x[y.do_side_effect] *= 2).should == 4
+    x.should == [1,4,3,4,8,5,3,4]
+    
+    h = {'key1' => 23, 'key2' => 'val'}
+    (h['key1'] %= 5).should == 3
+    (h['key2'] += 'ue').should == 'value'
+    h.should == {'key1' => 3, 'key2' => 'value'}
+  end
+end
+
 describe 'Single assignment' do
   it 'Assignment does not modify the lhs, it reassigns its reference' do
     a = 'Foobar'
