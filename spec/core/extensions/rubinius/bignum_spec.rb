2371b920ca3f956213ab9e406a3b5d2afab4f18e
Arthur Schreiber <schreiber.arthur@gmail.com>
2007-10-27 16:15:33 +0200

    Remove invalid and failing Bignum#coerce spec.

--- spec/core/extensions/rubinius/bignum_spec.rb
+++ spec/core/extensions/rubinius/bignum_spec.rb
@@ -19,20 +19,11 @@ extension :rubinius do
     end
   end
 
-  context "Bignum instance method" do
-    specify "coerce should return [Bignum, Bignum] if other is a Fixnum" do
-        a = 0xffffffff.coerce(1)
-        a.should == [1, 4294967295]
-        a.collect { |i| i.class }.should == [Bignum, Bignum]
-    end
-
-    specify "coerce should return [Float, Float] if other is not a Bignum or Fixnum" do
-      a = 0xffffffff.coerce("2")
-      a.inspect.should == "[2.0, 4294967295.0]"
-      a.collect { |i| i.class }.should == [Float, Float]
-      b = 0xffffffff.coerce(1.5)
-      b.inspect.should == "[1.5, 4294967295.0]"
-      b.collect { |i| i.class }.should == [Float, Float]
+  describe "Bignum#coerce" do
+    it "returns two bignums even if the given argument is a Fixnum" do
+      a = 0xffffffff.coerce(1)
+      a.should == [1, 4294967295]
+      a.collect { |i| i.class }.should == [Bignum, Bignum]
     end
   end
 end
