f120a470a5e07a7e53b1e006173942d58956e86b
Arthur Schreiber <schreiber.arthur@gmail.com>
2007-10-28 19:33:10 +0100

    Extended Kernel#` specs.

--- spec/core/kernel/backtick_spec.rb
+++ spec/core/kernel/backtick_spec.rb
@@ -2,11 +2,21 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Kernel#`" do
-  it "returns the result of the executed sub-process" do
+  it "returns the standard output of the executed sub-process" do
     ip = 'world'
     `echo disc #{ip}`.should == "disc world\n"
   end
+  
+  it "tries to convert the given argument to String using to_str" do
+    (obj = Object.new).should_receive(:to_str, :returning => "echo test")
+    Kernel.`(obj).should == "test\n"
 
+    obj = Object.new
+    obj.should_receive(:respond_to?, :with => [:to_str], :count => :any, :returning => true)
+    obj.should_receive(:method_missing, :with => [:to_str], :returning => "echo another test")
+    Kernel.`(obj).should == "another test\n"
+  end
+  
   it "sets $? to the exit status of the executed sub-process" do
     ip = 'world'
     `echo disc #{ip}`
