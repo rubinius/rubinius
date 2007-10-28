5e52a259b91e81fe5497f44107dee6ffd613b3be
Brian Ford <brixen@gmail.com>
2007-10-28 00:22:40 -0700

    Fixed wording on Rubinius Bignum#coerce extension specs.

--- spec/core/extensions/rubinius/bignum_spec.rb
+++ spec/core/extensions/rubinius/bignum_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 
 extension :rubinius do
   describe "Bignum" do
-    specify "should have max value 2 ** 29" do
+    it "has max value 2 ** 29" do
       max = 2 ** 29
       max.class.should == Bignum
       sub = max - 1
@@ -10,7 +10,7 @@ extension :rubinius do
       sub.class.should == Fixnum
     end
     
-    specify "should have min value -(2 ** 29)" do
+    it "has min value -(2 ** 29)" do
       min = -2 ** 29
       min.class.should == Bignum
       sub = min + 1
@@ -19,14 +19,14 @@ extension :rubinius do
     end
   end
 
-  context "Bignum instance method" do
-    specify "coerce should return [Bignum, Bignum] if other is a Fixnum" do
+  context "Bignum#coerce" do
+    it "returns [Bignum, Bignum] if other is a Fixnum" do
         a = 0xffffffff.coerce(1)
         a.should == [1, 4294967295]
         a.collect { |i| i.class }.should == [Bignum, Bignum]
     end
 
-    specify "coerce should return [Float, Float] if other is not a Bignum or Fixnum" do
+    it "returns [Float, Float] if other is not a Bignum or Fixnum" do
       a = 0xffffffff.coerce("2")
       a.inspect.should == "[2.0, 4294967295.0]"
       a.collect { |i| i.class }.should == [Float, Float]
