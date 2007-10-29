754e48c223c3464c7d048452585c07b8d0b3d8c7
Brian Ford <brixen@gmail.com>
2007-10-29 00:11:41 -0700

    Specs for IO#rewind.

--- spec/core/io/rewind_spec.rb
+++ spec/core/io/rewind_spec.rb
@@ -1,1 +1,26 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO#rewind" do
+  before :each do
+    @file = File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r')
+    @io = IO.open @file.fileno
+  end
+  
+  after :each do
+    @file.close
+  end
+  
+  it "positions the instance to the beginning of input" do
+    @io.readline.should == "Voici la ligne une.\n"
+    @io.readline.should == "Qui è la linea due.\n"
+    @io.rewind
+    @io.readline.should == "Voici la ligne une.\n"
+  end
+  
+  it "sets lineno to 0" do
+    @io.readline.should == "Voici la ligne une.\n"
+    @io.lineno.should == 1
+    @io.rewind
+    @io.lineno.should == 0
+  end
+end
