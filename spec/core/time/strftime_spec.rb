1f6c50f5c77566e66cb0b842733b7f4f4b24e937
Tilman Sauerbeck <tilman@code-monkey.de>
2007-10-30 18:13:47 +0100

    Use a fixed timezone for the Time#strftime spec.

--- spec/core/time/strftime_spec.rb
+++ spec/core/time/strftime_spec.rb
@@ -3,6 +3,8 @@ require File.dirname(__FILE__) + '/fixtures/methods'
 
 describe "Time#strftime" do
   it "formats time according to the directives in the given format string" do
-    Time.at(0).strftime("There is %M minutes in epoch").should == "There is 00 minutes in epoch"
+    with_timezone("GMT", 0) do
+      Time.at(0).strftime("There is %M minutes in epoch").should == "There is 00 minutes in epoch"
+    end
   end
 end
