11f0ed51b4bfb3bea2b544a82b3158fd3daf2ad8
Victor Hugo Borja <vic.borja@gmail.com>
2007-11-02 03:02:28 -0600

    Specs for Module#remove_const
    Signed-off-by: Brian Ford <brixen@gmail.com>

--- spec/core/module/remove_const_spec.rb
+++ spec/core/module/remove_const_spec.rb
@@ -1,2 +1,21 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Module#remove_const" do
+  it "returns the value of the removed constant" do
+    ModuleSpecs::Internal::ONE = 1
+    ModuleSpecs::Internal.send(:remove_const, :ONE).should == 1
+    should_raise(NameError) do
+      ModuleSpecs::Internal.send(:remove_const, :ONE)
+    end
+  end
+
+  it "is a private method" do 
+    ModuleSpecs::Internal::TWO = 2
+    should_raise(NameError) do
+      ModuleSpecs::Internal.remove_const(:TWO)
+    end
+    ModuleSpecs::Internal.send(:remove_const, :TWO).should == 2
+  end
+end
+
