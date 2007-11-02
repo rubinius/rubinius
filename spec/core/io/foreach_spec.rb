3efc01e110473d003ffb0a1376ec179f30e600de
Charles Comstock <dgtized@gmail.com>
2007-11-02 13:30:44 -0500

    specs for IO::foreach and specs to test IO::readlines,IO.readlines with string separator

--- spec/core/io/foreach_spec.rb
+++ spec/core/io/foreach_spec.rb
@@ -1,1 +1,21 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO::foreach" do
+  it "yields a sequence of Strings that were separated by $/" do
+    lines = []
+    IO::foreach(File.dirname(__FILE__) + '/fixtures/readlines.txt') do |line|
+      lines << line
+    end
+    lines.should == ["Voici la ligne une.\n", "Qui \303\250 la linea due.\n", 
+                     "Aqu\303\255 est\303\241 la l\303\255nea tres.\n", "Ist hier Linie vier.\n", 
+                     "Est\303\241 aqui a linha cinco.\n", "Here is line six.\n"]
+  end  
+  it "yields a sequence of Strings that were separated by I" do
+    lines = []
+    IO::foreach(File.dirname(__FILE__) + '/fixtures/readlines.txt','r') do |line|
+      lines << line
+    end
+    lines.should == ["Voici la ligne une.\nQui \303\250 la linea due.\nAqu\303\255 est\303\241 la l\303\255nea tr",
+                     "es.\nIst hier"," Linie vier",".\nEst\303\241 aqui a linha cinco.\nHer","e is line six.\n"]
+  end
+end
--- spec/core/io/readlines_spec.rb
+++ spec/core/io/readlines_spec.rb
@@ -8,4 +8,24 @@ describe "IO#readlines" do
           "Est\303\241 aqui a linha cinco.\n", "Here is line six.\n"]
     end
   end
+  it "returns an Array of Strings that were separated by r" do
+    File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r') do |f|
+      f.readlines('r').should == ["Voici la ligne une.\nQui \303\250 la linea due.\nAqu\303\255 est\303\241 la l\303\255nea tr",
+                                  "es.\nIst hier"," Linie vier",".\nEst\303\241 aqui a linha cinco.\nHer","e is line six.\n"]
+    end
+  end
+end
+
+describe "IO::readlines" do
+  it "returns an Array of Strings that were separated by $/" do
+    lines = IO::readlines(File.dirname(__FILE__) + '/fixtures/readlines.txt')
+    lines.should == ["Voici la ligne une.\n", "Qui \303\250 la linea due.\n", 
+                     "Aqu\303\255 est\303\241 la l\303\255nea tres.\n", "Ist hier Linie vier.\n", 
+                     "Est\303\241 aqui a linha cinco.\n", "Here is line six.\n"]
+  end
+  it "returns an Array of Strings that were separated by r" do
+    lines = IO::readlines(File.dirname(__FILE__) + '/fixtures/readlines.txt','r')
+    lines.should == ["Voici la ligne une.\nQui \303\250 la linea due.\nAqu\303\255 est\303\241 la l\303\255nea tr",
+                     "es.\nIst hier"," Linie vier",".\nEst\303\241 aqui a linha cinco.\nHer","e is line six.\n"]
+  end
 end
