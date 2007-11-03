85d63b676e463a2bec9a322bc8eeffd2daee433b
Chen Yufei <cyfdecyf@gmail.com>
2007-11-03 23:39:23 +0000

    Added specs for IO#gets

--- spec/core/io/gets_spec.rb
+++ spec/core/io/gets_spec.rb
@@ -1,1 +1,63 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO#gets" do
+  @testfile = File.dirname(__FILE__) + '/fixtures/gets.txt'
+  @createfile = File.dirname(__FILE__) + '/fixtures/i_exit'
+
+  after :each do
+    File.delete @createfile if File.exists?(@createfile)
+  end
+
+  @lines = ["Voici la ligne une.\n", "Qui \303\250 la linea due.\n", "\n", "\n",
+        "Aqu\303\255 est\303\241 la l\303\255nea tres.\n", "Ist hier Linie vier.\n", "\n",
+        "Est\303\241 aqui a linha cinco.\n", "Here is line six.\n"]
+
+  it "returns the next line of string that were separated by $/" do
+    File.open(@testfile, 'r') do |f|
+      @lines.each {|line| line.should == f.gets}
+    end
+  end
+
+  it "assigns the returned line to $_" do
+    File.open(@testfile, 'r') do |f|
+      @lines.each do |line|
+        f.gets
+        $_.should == line
+      end
+    end
+  end
+
+  it "returns nil if called at the end of the stream" do
+    File.open(@testfile, 'r') do |f|
+      @lines.length.times { f.gets }
+      f.gets.should == nil
+    end
+  end
+
+  it "returns the entire content if the separator is nil" do
+    File.open(@testfile, 'r') do |f|
+      f.gets(nil).should == "Voici la ligne une.\nQui \303\250 la linea due.\n\n\nAqu\303\255 est\303\241 la l\303\255nea tres.\nIst hier Linie vier.\n\nEst\303\241 aqui a linha cinco.\nHere is line six.\n"
+    end
+  end
+
+  # Two successive newlines in the input separate paragraphs.
+  # When there are more than two successive newlines, only two are kept.
+  it "returns the next paragraph if the separator's length is 0" do
+    pars = ["Voici la ligne une.\nQui \303\250 la linea due.\n\n",
+          "Aqu\303\255 est\303\241 la l\303\255nea tres.\nIst hier Linie vier.\n\n",
+          "Est\303\241 aqui a linha cinco.\nHere is line six.\n"]
+    File.open(@testfile, 'r') do |f|
+      pars.each {|par| par.should == f.gets("")}
+    end
+  end
+
+  it "raises IOError if the stream is not opened for reading" do
+    should_raise(IOError) do
+      File.open(@createfile, 'a') {|f| f.gets}
+    end
+
+    should_raise(IOError) do
+      File.open(@createfile, 'w') {|f| f.gets}
+    end
+  end
+end
