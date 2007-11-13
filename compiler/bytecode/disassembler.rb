5bf68329f1ff1b00590c0181d8be48619c446919
Adam Gardiner <adam.b.gardiner@gmail.com>
2007-11-13 22:23:13 +1100

    Fixed disassembler to work with current .rbc format

--- compiler/bytecode/disassembler.rb
+++ compiler/bytecode/disassembler.rb
@@ -13,18 +13,28 @@ class MarshalEmitter
     ?f => :false,
     ?i => :int,
     ?s => :string,
+    ?d => :float,
     ?B => :bignum,
     ?x => :symbol,
     ?b => :bytes,
+    ?I => :instructions,
     ?p => :tuple,
-    ?m => :method
+    ?m => :method,
+    ?r => :object
   }
   
   NoBody = [:nil, :true, :false]
   
   def self.process_rbc(file)
-    str = File.read(file)
-    new(str, 4)
+    # Binary mode needs to be specified on Win
+    str = ""
+    File.open(file, 'rb') do |f|
+      str = f.read
+    end
+    raise "Not a Rubinius compiled file" unless 'RBIX' == str[0..3]
+    ver = str[4..7].unpack("N").first
+    raise "Unsupported .rbc version #{ver}" unless 5 == ver
+    new(str, 8)
   end
   
   def digest
@@ -38,6 +48,7 @@ class MarshalEmitter
     @index += 1
     
     name = TagNames[tag]
+    raise "Unrecognised tag '" << (tag || '\0') << "' at #{@index} (#{sprintf('%#x', @index)})" unless name
         
     if NoBody.include? name
       [name]
@@ -67,7 +78,9 @@ class MarshalEmitter
   end
   
   alias :process_bignum :process_string
+  alias :process_float  :process_string
   alias :process_symbol :process_string
+  alias :process_bytes  :process_string
   
   def process_tuple
     sz = @string[@index,4].unpack("N").first
@@ -81,19 +94,22 @@ class MarshalEmitter
   end
   
   alias :process_method :process_tuple
+  alias :process_object :process_tuple
   
-  def process_bytes
+  def process_instructions
+    endian = @string[@index]
+    @index += 1
     body = process_string()
-    dis = Bytecode::Disassembler.new(body)
-    [body, dis.process_all]
-  end
-  
+    dis = Bytecode::Disassembler.new(body, endian)
+    dis.process_all()
+  end  
 end
 
 module Bytecode
   class Disassembler
-    def initialize(string)
+    def initialize(string, endian=?b)
       @string = string
+      @endian = endian
       @index = 0
       @opcodes = Bytecode::InstructionEncoder::OpCodes
       @one = Bytecode::InstructionEncoder::IntArg
@@ -109,14 +125,24 @@ module Bytecode
       return 0
     end
     
+    def get_dword
+      if ?b == @endian
+        # Instructions are encoded big-endian
+        dw = @string[@index, 4].unpack("N").first
+      else
+        # Instructions are encoded little-endian
+        dw = (@string[@index] | (@string[@index+1] << 8) | (@string[@index+2] << 16) | (@string[@index+3] << 24))
+      end
+      @index += 4
+      dw
+    end
+
     def process
-      bc = @string[@index]
-      @index += 1
+      bc = get_dword
       kind = @opcodes[bc]
       code = [kind]
       num_args(kind).times do
-        code << @string[@index, 4].unpack("N").first
-        @index += 4
+        code << get_dword
       end
       @codes << code
     end
