06d5312c51b09faef87d2deb53f3c472eaa94100
Charles Comstock <dgtized@gmail.com>
2007-11-18 17:49:44 -0600

    basic callcc behavioral specs

--- spec/core/kernel/callcc_spec.rb
+++ spec/core/kernel/callcc_spec.rb
@@ -2,6 +2,26 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Kernel#callcc" do
+  it "should be possible to exit a loop like a break" do
+    i = 0
+    Kernel.callcc do |x|
+      loop do
+        i += 1
+        x.call() if i == 5
+      end
+    end.should == nil
+    i.should == 5
+  end
+
+  it "should be possible to call a continuation multiple times" do
+    i = 0
+    cont = nil
+    Kernel.callcc {|cont|}
+    i += 1
+    cont.call() if i < 5
+    i.should == 5    
+  end
+  
   it "raises a LocalJumpError if callcc is not given a block" do
     should_raise(LocalJumpError) { Kernel.callcc }
   end
