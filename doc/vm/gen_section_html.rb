6efca889aa25d81bad55f4b49380f38d16c4205e
Adam Gardiner <adam.b.gardiner@gmail.com>
2007-11-19 23:23:35 +1100

    Updated docs, added support for bluecloth/markdown
    RedCloth struggles with multi-line code and tables that contain lists.
    Added support for BlueCloth as an alternate markup converter, as it seems to handle this better.

--- doc/vm/gen_section_html.rb
+++ doc/vm/gen_section_html.rb
@@ -1,21 +1,27 @@
 require 'rubygems'
-require 'redcloth'
 require 'erb'
 
 section_template = ERB.new(File.read('section_template.html.erb'),nil,'<>')
 
-Dir.glob("*.textile").each do |file|
-  if ARGV.empty? or ARGV.include? file
-    begin
-      html_file = file.chomp('.textile') << '.html'
-      red = RedCloth.new File.read(file)
-      File.open(html_file, 'w') do |f|
-        html = red.to_html
-        f.puts section_template.result(binding)
-      end
-    rescue Exception => ex
-      puts "An exception occurred while processing section '#{section}'"
-      raise ex
+ARGV.each do |file|
+  begin
+    cloth = case File.extname(file)
+      when '.textile'
+        require 'redcloth'
+        RedCloth
+      when '.markdown'
+        require 'bluecloth'
+        BlueCloth
     end
+
+    html_file = file.chomp(File.extname(file)) << '.html'
+    markup = cloth.new File.read(file)
+    File.open(html_file, 'w') do |f|
+      html = markup.to_html
+      f.puts section_template.result(binding)
+    end
+  rescue Exception => ex
+    puts "An exception occurred while processing section file '#{file}'"
+    raise ex
   end
 end
