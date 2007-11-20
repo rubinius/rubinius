ab9e40600fd2522d4abce86f7b8bdc632f6e9018
Jeremy Durham <jeremydurham@gmail.com>
2007-11-19 23:42:55 -0500

    Added very basic specs for Kernel#open

--- spec/core/kernel/open_spec.rb
+++ spec/core/kernel/open_spec.rb
@@ -1,2 +1,24 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Kernel#open" do
+  before :each do
+    @file = "test.txt"
+    File.open(@file, "w"){} # touch
+  end
+  
+  after :each do
+    File.delete(@file) rescue nil
+  end
+  
+  it "should open a file when given a valid filename" do
+    @file = open("test.txt")
+    @file.class.should == File
+  end
+  
+  it "should open an io when path starts with a pipe" do
+    @io = open("|date")
+    @io.class.should == IO
+  end
+    
+end
