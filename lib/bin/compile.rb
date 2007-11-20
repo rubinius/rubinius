rubinius/rubinius

commit 29dcf877b82b3eb43ef6542c5972ed8211ee55f9

Added -fPIC to build extensions on amd64. syck builds now.

lib/bin/compile.rb
@@ -99,7 +99,8 @@ class ExtensionCompiler
   end
   
   def compile_options    
-    (@includes + @compile_flags).join(" ")
+    str = (@includes + @compile_flags).join(" ")
+    str += ' -fPIC ' if Rubinius::PLATFORM == :amd64
   end
     
   def compiler

commit 9228730d76a575aa05b12e332964eedd6e13acfd

Fixed build breakage from 29dcf877.

lib/bin/compile.rb
@@ -100,7 +100,12 @@ class ExtensionCompiler
   
   def compile_options    
     str = (@includes + @compile_flags).join(" ")
-    str += ' -fPIC ' if Rubinius::PLATFORM == :amd64
+
+    if Rubinius::PLATFORM == :amd64
+      str += ' -fPIC '
+    end
+
+    str
   end
     
   def compiler