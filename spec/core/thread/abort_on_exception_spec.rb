ed5a46e13b35d6ad48cce1d3eed96c2f78ace049
Jeremy Durham <jeremydurham@gmail.com>
2007-11-22 11:42:36 -0500

    Added basic specs for abort_on_exception
    * Added specs for Thread#abort_on_exception ($DEBUG on and off)
    * Added specs for Thread#abort_on_exception=

--- spec/core/thread/abort_on_exception_spec.rb
+++ spec/core/thread/abort_on_exception_spec.rb
@@ -1,2 +1,20 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+
+describe "Thread#abort_on_exception" do
+  it "should be false unless $DEBUG is true" do
+    Thread.abort_on_exception.should == false unless $DEBUG
+  end
+
+  it "should be true if $DEBUG is true" do
+    Thread.abort_on_exception.should == true if $DEBUG
+  end
+  
+  it "should be changeable to true or false" do
+    Thread.abort_on_exception = false
+    Thread.abort_on_exception.should == false
+    Thread.abort_on_exception = true
+    Thread.abort_on_exception.should == true
+  end
+  
+end
