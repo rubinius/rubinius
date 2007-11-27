55c7529f4c8b02eff7e0b594f33b28750877fca2
Victor Hugo Borja <vic.borja@gmail.com>
2007-11-27 14:53:02 -0600

    Specs for Module#private/public/protected

--- spec/core/module/protected_spec.rb
+++ spec/core/module/protected_spec.rb
@@ -1,2 +1,8 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
+require File.dirname(__FILE__) + '/shared/visibility'
+
+describe "Module#protected" do
+  it_behaves_like :set_method_visibility, :protected
+end
+
