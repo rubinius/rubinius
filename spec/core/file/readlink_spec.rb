e7eb6a8e1e1310c08220db0ed7979ec4c721fccb
Tilman Sauerbeck <tilman@code-monkey.de>
2007-11-27 19:34:48 +0100

    Moved the after(:each) block before the specs, so the block is actually run.

--- spec/core/file/readlink_spec.rb
+++ spec/core/file/readlink_spec.rb
@@ -12,7 +12,12 @@ describe "File.readlink" do
       File.symlink(@file1, @file3)
     end
   end 
-  
+
+  after :each do
+    File.delete(@file1) if File.exists?(@file1)
+    File.delete(@file3) if File.exists?(@file3)
+  end
+
   it "return the name of the file referenced by the given link" do
     File.readlink(@file3).should == @file1
   end
@@ -21,10 +26,5 @@ describe "File.readlink" do
     should_raise(SystemCallError) do
       File.readlink("/this/surely/doesnt/exist")
     end
-  end
-  
-  after :each do
-    File.delete(@file1) if File.exists?(@file1)
-    File.delete(@file3) if File.exists?(@file3)
   end
 end
