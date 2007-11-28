edfff4981285007ecac132f565243150a8a8bd7e
Curtis Schofield <123@noself.net>
2007-11-27 19:02:45 -0800

    Specs created for Process#gid and Process#uid
    * both are using the unix system command 'id'

--- spec/core/process/uid_spec.rb
+++ spec/core/process/uid_spec.rb
@@ -1,1 +1,8 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "Process.uid" do
+  it "returns the correct uid for the user executing this process" do
+     current_uid_according_to_unix = `id -ur`.to_i
+     Process.uid.should == current_uid_according_to_unix
+  end
+end
