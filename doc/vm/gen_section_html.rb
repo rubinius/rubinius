3068d146d294c3e8b8bcb715470d3cf3499d6534
Adam Gardiner <adam.b.gardiner@gmail.com>
2007-11-28 12:27:52 +1100

    Standardised on RedCloth for VM doc overview pages
    Turns out RedCloth includes support for the nice code
    indentation feature of BlueCloth (plus many other
    Markdown features).

--- doc/vm/gen_section_html.rb
+++ doc/vm/gen_section_html.rb
@@ -1,21 +1,13 @@
 require 'rubygems'
 require 'erb'
+require 'redcloth'
 
 section_template = ERB.new(File.read('section_template.html.erb'),nil,'<>')
 
 ARGV.each do |file|
   begin
-    cloth = case File.extname(file)
-      when '.textile'
-        require 'redcloth'
-        RedCloth
-      when '.markdown'
-        require 'bluecloth'
-        BlueCloth
-    end
-
     html_file = file.chomp(File.extname(file)) << '.html'
-    markup = cloth.new File.read(file)
+    markup = RedCloth.new File.read(file)
     File.open(html_file, 'w') do |f|
       html = markup.to_html
       f.puts section_template.result(binding)
