d08e0e119b26df8acfc032b3886cec22965f9061
Eric Hodel <drbrain@segment7.net>
2007-11-29 12:29:55 -0800

    Hack out the etc-reliant bits

--- lib/fileutils.rb
+++ lib/fileutils.rb
@@ -969,41 +969,42 @@ module FileUtils
 
   OPT_TABLE['chown_R'] = [:noop, :verbose, :force]
 
-  begin
-    require 'etc'
-
-    def fu_get_uid(user)   #:nodoc:
-      return nil unless user
-      user = user.to_s
-      if /\A\d+\z/ =~ user
-      then user.to_i
-      else Etc.getpwnam(user).uid
-      end
-    end
-    private_module_function :fu_get_uid
-
-    def fu_get_gid(group)   #:nodoc:
-      return nil unless group
-      if /\A\d+\z/ =~ group
-      then group.to_i
-      else Etc.getgrnam(group).gid
-      end
-    end
-    private_module_function :fu_get_gid
-
-  rescue LoadError
-    # need Win32 support???
-
-    def fu_get_uid(user)   #:nodoc:
-      user    # FIXME
-    end
-    private_module_function :fu_get_uid
-
-    def fu_get_gid(group)   #:nodoc:
-      group   # FIXME
-    end
-    private_module_function :fu_get_gid
-  end
+# TODO needs etc.so
+#  begin
+#    require 'etc'
+#
+#    def fu_get_uid(user)   #:nodoc:
+#      return nil unless user
+#      user = user.to_s
+#      if /\A\d+\z/ =~ user
+#      then user.to_i
+#      else Etc.getpwnam(user).uid
+#      end
+#    end
+#    private_module_function :fu_get_uid
+#
+#    def fu_get_gid(group)   #:nodoc:
+#      return nil unless group
+#      if /\A\d+\z/ =~ group
+#      then group.to_i
+#      else Etc.getgrnam(group).gid
+#      end
+#    end
+#    private_module_function :fu_get_gid
+#
+#  rescue LoadError
+#    # need Win32 support???
+#
+#    def fu_get_uid(user)   #:nodoc:
+#      user    # FIXME
+#    end
+#    private_module_function :fu_get_uid
+#
+#    def fu_get_gid(group)   #:nodoc:
+#      group   # FIXME
+#    end
+#    private_module_function :fu_get_gid
+#  end
 
   #
   # Options: noop verbose
