450778cf5f416f6b9531664d4fff2c159c93cbe7
Victor Hugo Borja <vic.borja@gmail.com>
2007-12-02 01:39:28 -0600

    Shared spec for class_eval.
    - removed method-arguments from describe string
      This was causing bin/completeness to report 0 examples for Module#class_eval/module_eval
    - examples checking for TypeErrors test the exception is raised, but don't check
      the exact message as it is not part of the interface.

--- spec/core/module/shared/class_eval.rb
+++ spec/core/module/shared/class_eval.rb
@@ -1,5 +1,5 @@
 shared :module_class_eval do |cmd|
-  describe "Module##{cmd}(string, filename, lineno, &block)" do
+  describe "Module##{cmd}" do
     it "evaluates a given string in the context of self" do
       ModuleSpecs.send(cmd, "self").should == ModuleSpecs
       ModuleSpecs.send(cmd, "1 + 1").should == 2
@@ -11,11 +11,7 @@ shared :module_class_eval do |cmd|
     end
     
     it "uses the optional filename and lineno parameters for error messages" do
-      begin
-        ModuleSpecs.send(cmd, "raise", "test", 102)
-      rescue Exception => e
-        e.backtrace.first.should == "test:102:in `send'"
-      end
+      ModuleSpecs.send(cmd, "[__FILE__, __LINE__]", "test", 102).should == ["test", 102]
     end
   end
 
@@ -26,12 +22,12 @@ shared :module_class_eval do |cmd|
 
   it "raises a TypeError when the given eval-string can't be converted to string using to_str" do
     o = Object.new
-    should_raise(TypeError, "can't convert Object into String") do
+    should_raise(TypeError) do
       ModuleSpecs.send(cmd, o)
     end
     
     (o = Object.new).should_receive(:to_str, :returning => 123)
-    should_raise(TypeError, "Object#to_str should return String") do
+    should_raise(TypeError) do
       ModuleSpecs.send(cmd, o)
     end
   end
@@ -43,13 +39,13 @@ shared :module_class_eval do |cmd|
   end
   
   it "raises an ArgumentError when more than 3 arguments are given" do
-    should_raise(ArgumentError, "wrong number of arguments: #{cmd}(src) or #{cmd}{..}") do
+    should_raise(ArgumentError) do
       ModuleSpecs.send(cmd, "1 + 1", "some file", 0, "bogus")
     end
   end
   
   it "raises an ArgumentError when a block and normal arguments are given" do
-    should_raise(ArgumentError, "wrong number of arguments (1 for 0)") do
+    should_raise(ArgumentError) do
       ModuleSpecs.send(cmd, "1 + 1") { 1 + 1 }
     end
   end
