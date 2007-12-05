0c661894b54615ee4915d61569e072ffdfa8826d
Eero Saynatkari <rubinius@projects.kittensoft.org>
2007-12-05 09:17:24 -0500

    Much-improved tiny option parser lib/options.rb.
    * The Options API is much more user-friendly now,
      size is still about 100 LoC
    * Specs for the API.

--- lib/options.rb
+++ lib/options.rb
@@ -1,69 +1,95 @@
-# A simple, independent option parser originally from the ruSH project.
+# A simple, independent option parser originally from the rs project.
 class Options
 
   # Create an instance. If a Hash of options is
   # given, it is passed to #configure.
-  def initialize(allowed_options = nil)
-    @allowed, @usage = {}, ''
-    configure allowed_options if allowed_options
-  end                                 # initialize(allowed_options)
+  def initialize(message = '', &block)
+    @allowed, @header, @optdesc = {}, message, ''
 
-  # Add a single option to the parser given a short and long
-  # name as well as a description. Arguments may be specified.
-  #   :none (default)
-  #   :one means exactly one argument 
-  #   :many means more than one 
-  #   :maybe means zero or more
-  def option(short, long, desc, args = :none)
-    raise ArgumentError.new('Option must have short, long and desc') unless short and long and desc
+    block.call(self) if block
+  end                                 # initialize
 
-    short, long = short.delete('-'), long.delete('-')
+  # Adds an option to the parser. The option must be defined as 
+  # a string of the format "-s --long Description". The first
+  # element is a dash followed by a character (the "short option"),
+  # the second is two dashes followed by two or more characters 
+  # (the "long option"). The rest of the string becomes the third 
+  # element which is used as the description of the option's 
+  # purpose. The elements are separated by any number of whitespace 
+  # characters.
+  #
+  # By default an option takes no arguments but a second argument
+  # may be given to designate a different argument count. The
+  # two last versions are greedy and will consume all arguments
+  # until the next option switch or the end of input.
+  #
+  #   :none   no arguments
+  #   :one    exactly one argument 
+  #   :many   one or more
+  #   :any    zero or more
+  def option(definition, args = :none)
+    result = definition.scan /-(\w)\s+--(\w\S+?)\s+?(\S.*)/
+    raise ArgumentError, "Option format is '-s --long Description here'" if result.empty?
+
+    short, long, desc = result.first
 
     # Store the data along with a cross-reference
     @allowed[short] = {:desc => desc, :args => args, :other => long}
     @allowed[long]  = {:desc => desc, :args => args, :other => short}
 
-    # Add to usage to maintain order
+    # Add to usage now to maintain order
     argdesc   = case args
-                  when :one; 'ARG'
-                  when :many; 'ARG[, ...]'
-                  when :maybe; '[ARG, ...]'
-                  else;         ''
+                  when :one then 'ARG'
+                  when :any then '[ARG, ...]'
+                  when :many then 'ARG1, ARG2[, ...]'
+                  else         
+                    ''
                 end
                 
-    @usage << "     #{"-#{short} --#{long} #{argdesc}".ljust(30)} #{desc}\n" 
+    @optdesc << "        Options:\n\n" if @optdesc.empty?
+    @optdesc << "        #{"-#{short} --#{long} #{argdesc}".ljust(30)} #{desc}\n" 
   end                                 # option
+
+  # Optional error handling block
+  def on_error(&block)
+    @error_block = block
+  end
+
+  # Text to show above options in usage message
+  def header(message)
+    @header = message
+  end                                 # header
 
   # Generate a usage message
   def usage()
-    @usage
+    @header + @optdesc
   end                                 # usage
 
-  # Parse arguments according to previously a given #configure.
-  # The parser only accepts an Array, Strings have to be parsed
-  # beforehand. The parser matches the given options to the ones
-  # defined in #configure, leaves non-option arguments (such as
-  # the path in 'ls -la /etc') alone. Any unknown arguments raise
-  # an ArgumentError. The results of the parse are returned as a
-  # Hash, see below. The accepted forms for options are:
+  alias :help :usage
+
+  # The accepted forms for options are:
   #
   # Short opts: -h, -ht, -h ARG, -ht ARG (same as -h -t ARG).
   # Long opts:  --help, --help ARG, (No joining.)
-  #
-  # Quoted strings are valid arguments but for Strings only.
-  # Arrays should have parsed those at an earlier point.
   #
   # The returned Hash is indexed by the names of the found
   # options. These indices point to an Array of arguments
   # to that option if any, or just true if not. The Hash
   # also contains a special index, :args, containing all
   # of the non-option arguments.
+  #
+  # Upon encountering an error, the parser will raise an
+  # ArgumentError with an explanation. This behaviour may
+  # be overridden by supplying a block through #on_error.
+  # The block will be given the Options instance and the
+  # Exception object.
   def parse(arguments)
     expecting = nil     # An option may be expecting an argument
     @opts     = {}      # Options parsed
     @nonopts  = []      # Non-option arguments
 
     arguments = arguments.split unless arguments.kind_of?(Array)
+    raise ArgumentError, "Empty input" if arguments.empty?
 
     arguments.each do |opt|
       # Option type
@@ -80,7 +106,6 @@ class Options
 
         # Parse individual flags if any
         opts.each do |o|
-          # Valid options only
           data = @allowed.fetch(o) {|x| raise ArgumentError.new("Invalid option #{x}")}
 
           # Option takes arguments?
@@ -88,7 +113,7 @@ class Options
             # Prepare for incoming data
             @opts[o] ||= []; expecting = [o, data[:args]]
             
-          elsif data[:args] == :maybe
+          elsif data[:args] == :any
             # Cheat
             @opts[o] = true; expecting = [o, :many] 
 
@@ -124,13 +149,21 @@ class Options
 
       case @allowed[key][:args]
         when :one
-          raise ArgumentError, "#{key} must have one argument" if o.nil? or o == true or o.size != 1
+          if o.nil? or o == true or o.size != 1
+            raise ArgumentError, "#{key} must have one argument" 
+          end
         when :many
-          raise ArgumentError, "Too few arguments for #{key}" if o.nil? or o == true or o.size < 2
+          if o.nil? or o == true or o.size < 2
+            raise ArgumentError, "Too few arguments for #{key}" 
+          end
       end
     end 
 
     @opts[:args] = @nonopts
     @opts
-  end                                 # parse(arguments)
+
+  rescue ArgumentError => ex
+    raise ex unless @error_block
+    @error_block.call self, ex
+  end                                 # parse
 end                                   # class Options
