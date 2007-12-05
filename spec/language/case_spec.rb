a60e3bf901b62fbbbef59acb2c6c9f164be1fbbc
Adam Gardiner <adam.b.gardiner@gmail.com>
2007-12-05 14:10:15 +1100

    Cleanup case spec, update excludes
    * converted case specs to not use should
    * separated out case specs with target expressions from those without
    * updated excludes for two failing specs under compiler1

--- spec/language/case_spec.rb
+++ spec/language/case_spec.rb
@@ -1,35 +1,35 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "The 'case'-construct" do
-  it "should evaluate the body of the when clause matching the case target expression" do
+  it "evaluates the body of the when clause matching the case target expression" do
     case 1
       when 2: false
       when 1: true
     end.should == true
   end
 
-  it "should evaluate the body of the when clause whose array expression includes the case target expression" do
+  it "evaluates the body of the when clause whose array expression includes the case target expression" do
     case 2
       when 3, 4: false
       when 1, 2: true
     end.should == true
   end
 
-  it "should evaluate the body of the when clause whose range expression includes the case target expression" do
+  it "evaluates the body of the when clause whose range expression includes the case target expression" do
     case 5
       when 21..30: false
       when 1..20: true
     end.should == true
   end
 
-  it "should return nil when no 'then'-bodies are given" do
+  it "returns nil when no 'then'-bodies are given" do
     case "a"
       when "a"
       when "b"
     end.should == nil
   end
   
-  it "should evaluate the 'else'-body when no expression matched" do
+  it "evaluates the 'else'-body when no other expression matches" do
     case "c"
       when "a": 'foo'
       when "b": 'bar'
@@ -37,15 +37,211 @@ describe "The 'case'-construct" do
     end.should == 'zzz'
   end
   
-  it "should return nil when no expression matched and 'else'-body is empty" do
+  it "returns nil when no expression matches and 'else'-body is empty" do
     case "c"
       when "a": "a"
       when "b": "b"
       else
     end.should == nil
   end
+  it "returns the statement following ':'" do
+    case "a"
+      when "a": 'foo'
+      when "b": 'bar'
+    end.should == 'foo'
+  end
+    
+  it "returns the statement following 'then'" do
+    case "a"
+      when "a" then 'foo'
+      when "b" then 'bar'
+    end.should == 'foo'
+  end
+    
+  it "allows mixing ':' and 'then'" do
+    case "b"
+      when "a": 'foo'
+      when "b" then 'bar'
+    end.should == 'bar'
+  end
+    
+  it "tests classes with case equality" do
+    case "a"
+      when String
+        'foo'
+      when Symbol
+        'bar'
+    end.should == 'foo'
+  end
   
-  it "should evaluate the body of the first when clause that is not false/nil when no case target expression is given" do
+  it "tests with matching regexps" do
+    case "hello"
+      when /abc/: false
+      when /^hell/: true
+    end.should == true
+  end
+  
+  it "does not test with equality when given classes" do
+    case :symbol.class
+      when Symbol
+        "bar"
+      when String
+        "bar"
+      else
+        "foo"
+    end.should == "foo"
+  end
+  
+  it "takes lists of values" do
+    case 'z'
+      when 'a', 'b', 'c', 'd'
+        "foo" 
+      when 'x', 'y', 'z'
+        "bar" 
+    end.should == "bar"
+
+    case 'b'
+      when 'a', 'b', 'c', 'd'
+        "foo" 
+      when 'x', 'y', 'z'
+        "bar" 
+    end.should == "foo"
+  end
+  
+  it "expands arrays to lists of values" do
+    case 'z'
+      when *['a', 'b', 'c', 'd']
+        "foo" 
+      when *['x', 'y', 'z']
+        "bar" 
+    end.should == "bar"
+  end
+
+  it "takes an expanded array in addition to a list of values" do
+    case 'f'
+      when 'f', *['a', 'b', 'c', 'd']
+        "foo" 
+      when *['x', 'y', 'z']
+        "bar" 
+    end.should == "foo"
+
+    case 'b'
+      when 'f', *['a', 'b', 'c', 'd']
+        "foo" 
+      when *['x', 'y', 'z']
+        "bar" 
+    end.should == "foo"
+  end
+
+  it "concats arrays before expanding them" do
+    a = ['a', 'b', 'c', 'd']
+    b = ['f']
+  
+    case 'f'
+      when 'f', *a|b
+        "foo" 
+      when *['x', 'y', 'z']
+        "bar" 
+    end.should == "foo"
+  end
+  
+  it "never matches when clauses with no values" do
+    case nil
+      when *[]
+        "foo"
+    end.should == nil
+  end
+  
+  it "lets you define a method after the case statement" do
+    case (def foo; 'foo'; end; 'f')
+      when 'a'
+        'foo'
+      when 'f'
+        'bar'
+    end.should == 'bar'
+  end
+  
+  it "raises a SyntaxError when 'else' is used when no 'when' is given" do
+    should_raise(SyntaxError) do
+      eval <<-CODE
+      case 4
+        else
+          true
+      end
+      CODE
+    end
+  end
+
+  it "raises a SyntaxError when 'else' is used before a 'when' was given" do
+    should_raise(SyntaxError) do
+      eval <<-CODE
+      case 4
+        else
+          true
+        when 4: false
+      end
+      CODE
+    end
+  end
+
+  it "supports nested case statements" do
+    result = false
+    case :x
+    when Symbol
+      case :y
+      when Symbol
+        result = true
+      end
+    end
+    result.should == true
+  end
+
+  it "supports nested case statements followed by a when with a splatted array" do
+    result = false
+    case :x
+    when Symbol
+      case :y
+      when Symbol
+        result = true
+      end
+    when *[Symbol]
+      result = false
+    end
+    result.should == true
+  end
+
+  it "supports nested case statements followed by a when with a splatted non-array" do
+    result = false
+    case :x
+    when Symbol
+      case :y
+      when Symbol
+        result = true
+      end
+    when *Symbol
+      result = false
+    end
+    result.should == true
+  end
+
+  it "works even if there's only one when statement" do
+    case 1
+    when 1
+      100
+    end.should == 100
+  end
+end
+
+
+describe "The 'case'-construct with no target expression" do
+  
+  it "evaluates the body of the first when clause that is not false/nil" do
+    case
+      when false: 'foo'
+      when 2: 'bar'
+      when 1 == 1: 'baz'
+    end.should == 'bar'
+
     case
       when false: 'foo'
       when nil: 'foo'
@@ -53,7 +249,7 @@ describe "The 'case'-construct" do
     end.should == 'bar'
   end
     
-  it "should evaluate the body of the first when clause that is not false/nil when no case target expression is given" do
+  it "evaluates the body of the else clause if all when clauses are false/nil" do
     case
       when false: 'foo'
       when nil: 'foo'
@@ -73,184 +269,6 @@ describe "The 'case'-construct" do
       else 'bar'
     end.should == 'foo'
   end
-    
-  it "should return the statement following ':'" do
-    case "a"
-      when "a": 'foo'
-      when "b": 'bar'
-    end.should == 'foo'
-  end
-    
-  it "should return the statement following 'then'" do
-    case "a"
-      when "a" then 'foo'
-      when "b" then 'bar'
-    end.should == 'foo'
-  end
-    
-  it "should allow mixing ':' and 'then'" do
-    case "b"
-      when "a": 'foo'
-      when "b" then 'bar'
-    end.should == 'bar'
-  end
-    
-  it "should test classes with case equality" do
-    case "a"
-      when String
-        'foo'
-      when Symbol
-        'bar'
-    end.should == 'foo'
-  end
-  
-  it "should test with matching regexps" do
-    case "hello"
-      when /abc/: false
-      when /^hell/: true
-    end.should == true
-  end
-  
-  it "should not test with equality when given classes" do
-    case :symbol.class
-      when Symbol
-        "bar"
-      when String
-        "bar"
-      else
-        "foo"
-    end.should == "foo"
-  end
-  
-  it "should take lists of values" do
-    case 'z'
-      when 'a', 'b', 'c', 'd'
-        "foo" 
-      when 'x', 'y', 'z'
-        "bar" 
-    end.should == "bar"
-
-    case 'b'
-      when 'a', 'b', 'c', 'd'
-        "foo" 
-      when 'x', 'y', 'z'
-        "bar" 
-    end.should == "foo"
-  end
-  
-  it "should expand arrays to lists of values" do
-    case 'z'
-      when *['a', 'b', 'c', 'd']
-        "foo" 
-      when *['x', 'y', 'z']
-        "bar" 
-    end.should == "bar"
-  end
-
-  it "should take an expanded array in addition to a list of values" do
-    case 'f'
-      when 'f', *['a', 'b', 'c', 'd']
-        "foo" 
-      when *['x', 'y', 'z']
-        "bar" 
-    end.should == "foo"
-  end
-
-  it "should concat arrays before expanding them" do
-    a = ['a', 'b', 'c', 'd']
-    b = ['f']
-  
-    case 'f'
-      when 'f', *a|b
-        "foo" 
-      when *['x', 'y', 'z']
-        "bar" 
-    end.should == "foo"
-  end
-  
-  it "should never match when clauses with no values" do
-    case nil
-      when *[]
-        "foo"
-    end.should == nil
-  end
-  
-  it "should let you define a method after the case statement" do
-    case (def foo; 'foo'; end; 'f')
-      when 'a'
-        'foo'
-      when 'f'
-        'bar'
-    end.should == 'bar'
-  end
-  
-  it "should raise a SyntaxError when 'else' is used when no 'when' is given" do
-    should_raise(SyntaxError) do
-      eval <<-CODE
-      case 4
-        else
-          true
-      end
-      CODE
-    end
-  end
-
-  it "should raise a SyntaxError when 'else' is used before a 'when' was given" do
-    should_raise(SyntaxError) do
-      eval <<-CODE
-      case 4
-        else
-          true
-        when 4: false
-      end
-      CODE
-    end
-  end
-
-  it "should support nested case statements" do
-    result = false
-    case :x
-    when Symbol
-      case :y
-      when Symbol
-        result = true
-      end
-    end
-    result.should == true
-  end
-
-  it "should support nested case statements followed by a when with a splatted array" do
-    result = false
-    case :x
-    when Symbol
-      case :y
-      when Symbol
-        result = true
-      end
-    when *[Symbol]
-      result = false
-    end
-    result.should == true
-  end
-
-  it "should support nested case statements followed by a when with a splatted non-array" do
-    result = false
-    case :x
-    when Symbol
-      case :y
-      when Symbol
-        result = true
-      end
-    when *Symbol
-      result = false
-    end
-    result.should == true
-  end
-
-  it "should work even if there's only one when statement" do
-    case 1
-    when 1
-      100
-    end.should == 100
-  end
 end
+    
+
