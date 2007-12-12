0d774c99254b2c5992a17ecb2a5a12dcd2cad05e
Chris Shoemaker <chris.shoemaker@cox.net>
2007-12-11 20:37:45 -0500

    Add a Hash#store spec for storing unequal keys w/ same hash.

--- spec/core/hash/shared/store.rb
+++ spec/core/hash/shared/store.rb
@@ -15,6 +15,19 @@ shared :hash_store do |cmd|
       h.keys[0].reverse.should == "oof"
     end
 
+    it "stores unequal keys that hash to the same value" do
+      h = {}
+      k1 = ["x"]
+      k2 = ["y"]
+      # So they end up in the same bucket
+      def k1.hash() 0 end
+      def k2.hash() 0 end
+
+      h[k1] = 1
+      h[k2] = 2
+      h.size.should == 2
+    end
+
     compliant :mri, :jruby do
       it "duplicates and freezes string keys" do
         key = "foo"
