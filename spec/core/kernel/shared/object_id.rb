b3aa2af4a3467b4eeb8765010286c12bd5adfbf9
Tilman Sauerbeck <tilman@code-monkey.de>
2007-12-15 22:53:35 +0100

    Sanitized Object#id spec.

--- spec/core/kernel/shared/object_id.rb
+++ spec/core/kernel/shared/object_id.rb
@@ -2,24 +2,20 @@ shared :kernel_object_id do |cmd|
   describe "Object##{cmd}" do
     # #object_id and #__id__ are aliases, so we only need one function
     # that tests both methods
-    it "return an Integer" do
+    it "returns an integer" do
       Object.new.send(cmd).class.should == Fixnum      
       nil.send(cmd).class.should == Fixnum      
     end
   
-    it "return same number will be returned on all calls to id for a given object" do
+    it "returns the same value on all calls to id for a given object" do
       o1 = Object.new
-      o2 = Object.new
       o1.send(cmd).should == o1.send(cmd)
     end
   
-    it "return same number will be returned on all calls to id for a given object" do
+    it "returns different values for different objects" do
       o1 = Object.new
       o2 = Object.new
       o1.send(cmd).should_not == o2.send(cmd)
     end
-    it "" do
-    end
-    
   end
 end
