5b1252e6b2d8f8d70343b06f3520114de2040524
Hunter Kelly <retnuh@gmail.com>
2007-12-15 17:31:25 +0000

    Minor fix to struct_spec to include fixture.

--- spec/core/struct/struct_spec.rb
+++ spec/core/struct/struct_spec.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Struct anonymous class instance methods" do
   specify "Enumerable methods should work" do
