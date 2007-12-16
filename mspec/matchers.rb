48de1861f6f433961a0829c7ccb9310f68e2ad30
Brian Ford <brixen@gmail.com>
2007-12-15 18:43:51 -0800

    Added be_kind_of matcher to mspec.

--- mspec/matchers.rb
+++ mspec/matchers.rb
@@ -1,6 +1,7 @@
 require 'mspec/matchers/base'
 require 'mspec/matchers/be_ancestor_of'
 require 'mspec/matchers/be_close'
+require 'mspec/matchers/be_kind_of'
 require 'mspec/matchers/include'
 require 'mspec/matchers/raise_error'
 require 'mspec/matchers/output'
