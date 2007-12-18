d21810882621356c35dcd101daca5ee5549f6607
Charles Comstock <dgtized@gmail.com>
2007-12-18 00:42:32 -0600

    Threads should report there status of sleep, aborting, and run

--- spec/core/thread/inspect_spec.rb
+++ spec/core/thread/inspect_spec.rb
@@ -2,14 +2,40 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Thread#inspect" do
-  it "shows the thread's status" do
-    x = nil
-    t = Thread.new { x = Thread.current.inspect; sleep 4 }
-    Thread.pass while t.status == 'run'
-    x.include?('run').should == true
-    t.inspect.include?('sleep').should == true
-    t.kill
-    Thread.pass while t.alive?
-    t.inspect.include?('dead').should == true
+  it "can check it's own status" do
+    t = Thread.new { Thread.current.inspect }
+    t.value.should include('run')
+  end
+  
+  it "should describe a sleeping thread" do
+    c = Channel.new
+    t = Thread.new do      
+      c << :s1      
+      sleep
+      c << Thread.current.inspect
+      sleep
+    end
+    
+    c.receive.should == :s1
+    t.inspect.should include('sleep')
+    t.run
+    c.receive.should include('run')
+    t.inspect.should include('sleep')
+    t.run
+    t.join
+    t.inspect.should include('dead')
+  end
+
+  compliant(:mri) do
+    it "should report aborting on a killed thread" do
+      c = Channel.new
+      t = Thread.new { c << Thread.current.inspect; Thread.stop }
+      c.receive.should include('run')
+      t.inspect.should  include('sleep')
+      Thread.critical = true
+      t.kill
+      t.inspect.should include('aborting')
+      Thread.critical = false
+    end
   end
 end
