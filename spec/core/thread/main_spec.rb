11348e25ba30199e3beb05f8c38c18820fbefc3f
Charles Comstock <dgtized@gmail.com>
2007-12-18 01:11:38 -0600

    Some minor fixes in Thread specs

--- spec/core/thread/main_spec.rb
+++ spec/core/thread/main_spec.rb
@@ -5,5 +5,6 @@ describe "Thread.main" do
   it "returns the main thread" do
     Thread.new { @main = Thread.main ; @current = Thread.current}.join
     @main.should_not == @current
+    @main.should == Thread.current
   end
 end
