634300eed40ef0ded16ab7cac7865dd783486c2d
Charles Nutter <headius@charles-nutters-computer.local>
2007-12-20 03:46:59 -0600

    Add 'sleep' checks to threadgroup spec to avoid the same race conditions seen in kernel/sleep_spec.

--- spec/core/threadgroup/list_spec.rb
+++ spec/core/threadgroup/list_spec.rb
@@ -3,11 +3,13 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe "ThreadGroup#list" do
   it "should return the list of threads in the group" do
     thread = Thread.new { sleep }
+    Thread.pass until thread.status == 'sleep'
     tg = ThreadGroup.new
     tg.add(thread)
     tg.list.should include(thread)
     
     thread2 = Thread.new { sleep }
+    Thread.pass until thread2.status == 'sleep'
     tg.add(thread2)
     
     (tg.list & [thread, thread2]).should include(thread, thread2)
