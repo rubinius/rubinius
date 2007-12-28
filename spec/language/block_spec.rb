eb561025707736ebe196eab3b4ff2bd1c98f45a4
Brian Ford <brixen@gmail.com>
2007-12-27 23:51:42 -0800

    Fixed language/block specs to guard ruby18 feature.

--- spec/language/block_spec.rb
+++ spec/language/block_spec.rb
@@ -24,10 +24,6 @@ describe "Block parameters" do
     i.should == 3
   end
 
-  compliant :ruby do
-    require 'spec/language/strange_block_args_subspec.rb'
-  end
-
   it "captures variables from the outer scope" do
     a = [1,2,3]    
     sum = 0
@@ -49,5 +45,32 @@ describe "A block whose arguments are splatted" do
     a = []
     BlockSpecs::Yield.new.two_arg_array { |*args| a << args }
     a.should == [[[1, 2]]]
+  end
+end
+
+compliant :ruby18 do
+  # This functionality is being removed from MRI and has never
+  # been used in 1.8, therefore rubinius doesn't support
+  # it already.
+  describe "Block parameters (to be removed from MRI)" do
+    it "should assign to a global variable" do
+      $global_for_block_assignment = 0
+      a = [1,2,3]
+      a.each {|$global_for_block_assignment| ;}
+      $global_for_block_assignment.should == 3
+    end
+
+    it "should call method=" do
+      class T
+        def n; return @n; end
+        def n=(val); @n = val + 1; end
+        def initialize; @n = 0; end
+      end
+      t = T.new
+      t.n.should == 0
+      a = [1,2,3]    
+      a.each {|t.n| }
+      t.n.should == 4    
+    end
   end
 end
