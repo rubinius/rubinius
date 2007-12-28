d54fb1e7c3f586a6d8ac200d6de839ebe6cb4c46
Charles Nutter <headius@charles-nutters-computer.local>
2007-12-28 14:40:50 -0600

    Move rubinius-specific spec from core/kernel/Integer_spec to rubinius/core/kernel/Integer_spec.

---
+++ spec/rubinius/core/kernel/Integer_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/fixtures/classes'
+
+extension :rubinius do
+  describe "Kernel.Integer" do
+    it "should call to_inum on strings with a base of 0 and with checks on" do
+      the_answer = "0x42"
+      the_answer.should_receive(:to_inum).with(0, true).and_return(66)
+      Integer(the_answer).should == 66
+    end
+  end
+end
