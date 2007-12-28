1b0333479bf6da2c76c8d3c1e1640dc156086d9f
Vladimir Sizikov <vsizikov@gmail.com>
2007-12-28 08:24:30 +0100

    Improved ObjectSpace#each_object spec test.
    Previously, the test was failing from time to time,
    depending on Garbage Collector behavior.

--- spec/core/objectspace/each_object_spec.rb
+++ spec/core/objectspace/each_object_spec.rb
@@ -3,9 +3,11 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 context "ObjectSpace.each_object" do
   specify "calls the block once for each living, nonimmediate object in the Ruby process" do
     class ObjectSpaceSpecEachObject; end
-    ObjectSpaceSpecEachObject.new
+    new_obj = ObjectSpaceSpecEachObject.new
 
     count = ObjectSpace.each_object(ObjectSpaceSpecEachObject) {}
     count.should == 1
+    # this is needed to prevent the new_obj from being GC'd too early
+    new_obj.should_not == nil
   end
 end
