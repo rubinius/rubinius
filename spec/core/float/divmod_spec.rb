af7bb00beeb359fd6183def039b9a1fcd0ce7c48
Charles Nutter <headius@charles-nutters-computer.local>
2007-12-28 23:39:15 -0600

    Expand Float divmod array equality comparisons to use be_close with a default tolerance.

--- spec/core/float/divmod_spec.rb
+++ spec/core/float/divmod_spec.rb
@@ -2,9 +2,15 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "Float#divmod" do
   it "returns an [quotient, modulus] from dividing self by other" do
-    3.14.divmod(2).inspect.should == "[1, 1.14]"
-    2.8284.divmod(3.1415).inspect.should == "[0, 2.8284]"
-    -1.0.divmod(0xffffffff).inspect.should == "[-1, 4294967294.0]"
+    values = 3.14.divmod(2)
+    values[0].should == 1
+    values[1].should be_close(1.14, TOLERANCE)
+    values = 2.8284.divmod(3.1415)
+    values[0].should == 0
+    values[1].should be_close(2.8284, TOLERANCE)
+    values = -1.0.divmod(0xffffffff)
+    values[0].should == -1
+    values[1].should be_close(4294967294.0, TOLERANCE)
   end
 
   platform :version => '1.8.5'..'1.8.6' do
