6f448f0dd72b5df2cc69e28db3d5593f897a9dbd
MenTaLguY <mental@rydia.net>
2007-12-29 00:58:58 -0500

    a more modest spec for Thread.pass

--- spec/core/thread/pass_spec.rb
+++ spec/core/thread/pass_spec.rb
@@ -2,22 +2,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Thread.pass" do
-  it "should invoke the scheduler to pass execution to another thread" do
-    list = []
-    a = Thread.new do
-      list << :a; Thread.pass
-      list << :c; Thread.pass
-      list << :e; Thread.pass
-      list << :g
-    end
-    b = Thread.new do
-      list << :b; Thread.pass
-      list << :d; Thread.pass
-      list << :f; Thread.pass
-      list << :h
-    end
-    a.join
-    b.join
-    list.should == [:a,:b,:c,:d,:e,:f,:g,:h]
+  it "should at least be a noop returning nil" do
+    Thread.pass.should == nil
   end
 end
