70ddfd43fd727122f56e8bdfcf3febd1ac1b5479
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2008-01-04 01:01:11 +0800

    Fix for Time#yday spec when Time.at might return yday+1 depending on tz
    * Wrapped Time.at in a with_timezone("UTC") for consistent results
    
    Signed-off-by: Brian Ford <brixen@gmail.com>

--- spec/ruby/1.8/core/time/yday_spec.rb
+++ spec/ruby/1.8/core/time/yday_spec.rb
@@ -3,6 +3,8 @@ require File.dirname(__FILE__) + '/fixtures/methods'
 
 describe "Time#yday" do
   it "returns an integer representing the day of the year, 1..366" do
-    Time.at(9999999).yday.should == 116
+    with_timezone("UTC") do
+      Time.at(9999999).yday.should == 116
+    end
   end
 end
