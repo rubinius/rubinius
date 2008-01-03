e68bd05defe5ab749110af507c86769c9a036b25
Charles Nutter <headius@charles-nutters-computer.local>
2008-01-02 19:04:48 -0600

    Removing 'Range#initialize can't be called twice' spec.
    Evan agreed that these specs aren't useful, and I don't believe they're
    within the bounds of reasonable language specification since they're
    going around visibility and testing behaviors no sane programmer would
    ever be able to see.

--- spec/ruby/1.8/core/range/initialize_spec.rb
+++ spec/ruby/1.8/core/range/initialize_spec.rb
@@ -1,9 +1,1 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
-
-describe "Range#initialize" do
-  it "can't be called twice" do
-    [0..1, 'a'..'b'].each do |range|
-      lambda { range.send(:initialize, range.begin, range.end) }.should raise_error(NameError)
-    end
-  end
-end
