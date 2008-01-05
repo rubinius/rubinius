34fa6a1e3c56ae7db3d53aba639352c0fab11a30
Evan Phoenix <evan@fallingsnow.net>
2008-01-05 13:19:11 -0800

    Fix metaclass slot => name index

--- kernel/bootstrap/metaclass.rb
+++ kernel/bootstrap/metaclass.rb
@@ -1,5 +1,5 @@
 class MetaClass
-  ivar_as_index :__ivars__ => 0, :methods => 1, :method_cache => 2, :name => 3, :constants => 4, :parent => 5, :superclass => 6, :instance_fields => 7, :instance_flags => 8, :attached_instance => 9
+  ivar_as_index :__ivars__ => 0, :methods => 1, :method_cache => 2, :name => 3, :constants => 4, :parent => 5, :superclass => 6, :instance_fields => 7, :has_ivars => 8, :needs_cleanup => 9, :object_type => 10, :attached_instance => 11
   def __ivars__        ; @__ivars__         ; end
   # Do not define #methods as an accessor for @methods, use #method_table instead
   def method_cache     ; @method_cache      ; end
