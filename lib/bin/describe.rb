094db0be5414b8dffd17d4dcc6d8b4378a371fd8
Ryan Davis <ryand-ruby@zenspider.com>
2008-01-08 16:14:07 -0800

    Cleaned out dead code. Added ability for describe to do strings of ruby

--- lib/bin/describe.rb
+++ lib/bin/describe.rb
@@ -3,24 +3,16 @@ require 'compiler1/bytecode/rubinius'
 
 file = ARGV.shift
 
-#fd = File.open(file)
-#code = fd.read
-#fd.close
-#
-#puts "Path: #{file}"
-#puts "Size: #{code.size} bytes"
-#
-#sexp = code.to_sexp
-#puts "\nSexp:\n  #{sexp.indented_inspect}"
-
-sexp = File.to_sexp file
-puts "\nSexp:\n  #{sexp.indented_inspect}"
+sexp = if File.exist? file then
+         File.to_sexp file
+       else
+         file.to_sexp
+       end
+puts "\nSexp:\n  #{sexp.inspect}"
 
 compiler = Compiler1::Bytecode::Compiler.new
 state = Compiler1::RsLocalState.new
 
-# nx = compiler.fully_normalize(sexp, state)
-# puts "\nNormalized Sexp:\n  #{nx.indented_inspect}"
 begin
   desc = compiler.compile_as_script(sexp, :__eval_script__, state)
 rescue Object => e
