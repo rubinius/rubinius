690626f43f7b4ce888de081033eaadfba543acff
Brian Ford <brixen@gmail.com>
2008-01-08 18:58:50 -0800

    Removed subtend specs from CI run. Increase File#mtime tolerance.

--- spec/ruby/1.8/core/file/mtime_spec.rb
+++ spec/ruby/1.8/core/file/mtime_spec.rb
@@ -12,7 +12,7 @@ describe "File.mtime" do
 
   it "returns the modification Time of the file" do
     File.mtime(@filename).class.should == Time
-    File.mtime(@filename).should be_close(@mtime, 1.0)
+    File.mtime(@filename).should be_close(@mtime, 2.0)
   end
 
   it "raises an Errno::ENOENT exception if the file is not found" do
