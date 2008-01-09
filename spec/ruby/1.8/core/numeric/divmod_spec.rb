d699f6605db86e6f6bc61d0f3a79fc1535816c70
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2008-01-09 10:38:06 +0800

    Add spec to illustrate Numeric#divmod bug in MRI and rubinius (excluded)

--- spec/ruby/1.8/core/numeric/divmod_spec.rb
+++ spec/ruby/1.8/core/numeric/divmod_spec.rb
@@ -6,43 +6,51 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 #    x = q*y + r
 
 # FIXME: this awful wording
-describe "Numeric#divmod" do    
-  it "divmod right integers" do 
+describe "Numeric#divmod" do
+  it "divmod right integers" do
     13.divmod(4).should == [3,1]
     4.divmod(13).should == [0,4]
   end
-  
-  it "divmod right integers and floats" do 
+
+  it "divmod right integers and floats" do
     13.divmod(4.0).should == [3,1]
     4.divmod(13).should == [0,4]
   end
-  
-  it "divmod right the integers and floats" do 
+
+  it "divmod right the integers and floats" do
     13.divmod(4.0).should == [3,1]
     4.divmod(13).should == [0,4]
   end
-  
-  it "divmod right  floats" do 
+
+  it "divmod right floats" do
     13.0.divmod(4.0).should == [3.0,1.0]
     4.0.divmod(13).should == [0.0,4.0]
+    # MRI 1.8.6 bug Numeric#divmod bug
+    # http://rubyforge.org/tracker/?func=detail&atid=1698&aid=14540&group_id=426
+    y = 1 / 12.0
+    0.58.divmod(y).first.should == 6
+    0.59.divmod(y).first.should == 7
+    0.63.divmod(y).first.should == 7
+    0.66.divmod(y).first.should == 7
+    0.67.divmod(y).first.should == 8
   end
-  
+
   it "returns self divmod other" do
     (3**33).divmod( 100).should == [55590605665555, 23]
   end
-  
+
   it "raises an ArgumentError when not passed one argument" do
     lambda { 13.divmod       }.should raise_error(ArgumentError)
     lambda { 13.divmod(1, 2) }.should raise_error(ArgumentError)
   end
-  
+
   it "raises a ZeroDivisionError when passed 0" do
     lambda { 13.divmod(0)      }.should raise_error(ZeroDivisionError)
   end
-  
+
   it "raises a TypeError when not passed a Numeric type" do
     lambda { 13.divmod(nil)    }.should raise_error(TypeError)
     lambda { 13.divmod('test') }.should raise_error(TypeError)
-    lambda { 13.divmod(true)   }.should raise_error(TypeError)   
-  end 
+    lambda { 13.divmod(true)   }.should raise_error(TypeError)
+  end
 end
