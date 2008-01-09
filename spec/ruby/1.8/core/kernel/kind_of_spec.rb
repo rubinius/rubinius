01a189cc3e52e8bcc6f22bcc5713e765bba84160
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-01-09 10:29:27 -0500

    Object#is_a?, #kind_of? and #instance_of? specs.
    * Removed obsolete kernel specs for same.

--- spec/ruby/1.8/core/kernel/kind_of_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
-require File.dirname(__FILE__) + '/fixtures/classes'
-require File.dirname(__FILE__) + '/shared/kind_of'
-
-describe "Kernel#kind_of?" do
-  it_behaves_like(:kernel_kind_of , :kind_of?)
-end
