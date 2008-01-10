4d2e53e7376080e42b84dca486debcf4f153f32f
Jeremy Roach <jero_rub@yahoo.com>
2008-01-10 15:17:00 -0600

    implement more of Marshal.dump
    negative Fixnum, Bignum, Regexp, Struct

--- kernel/core/marshal.rb
+++ kernel/core/marshal.rb
@@ -35,13 +35,16 @@ module Marshal
   TYPE_LINK = '@'
 
   def self.dump(obj, depth = -1, io = nil)
+    cls = obj.class
+    sup = get_superclass(cls)
+
     str = [MAJOR_VERSION, MINOR_VERSION].pack('CC')
-    str << serialize(obj, depth)
+    str << serialize(obj, cls, sup, depth)
     str
   end
 
-  def self.serialize(obj, depth)
-    get_class_names(obj).each do |class_name|
+  def self.serialize(obj, cls, sup, depth)
+    [cls.to_s, sup.to_s].each do |class_name|
       case class_name
       when 'NilClass'
         return serialize_nil(obj)
@@ -56,14 +59,25 @@ module Marshal
       when 'Symbol'
         return serialize_symbol(obj)
       when 'String'
-        return serialize_string(obj)
+        ivar_prefix = serialize_instance_variables_prefix(obj)
+        ivar_suffix = serialize_instance_variables_suffix(obj, cls, sup, depth)
+        return ivar_prefix + serialize_string(obj) + ivar_suffix
       when / Integer | Fixnum | Bignum /x
         if obj >= -2**30 and obj <= (2**30 - 1)
           return serialize_fixnum(obj)
+        else
+          return serialize_bignum(obj)
         end
+      when 'Regexp'
+        ivar_prefix = serialize_instance_variables_prefix(obj)
+        ivar_suffix = serialize_instance_variables_suffix(obj, cls, sup, depth)
+        return ivar_prefix + serialize_regexp(obj) + ivar_suffix
+      when 'Struct'
+        return serialize_struct(obj, depth)
       end
     end
-    ''
+    ivar_suffix = serialize_instance_variables_suffix(obj, cls, sup, depth)
+    serialize_object(obj) + ivar_suffix
   end
 
   def self.serialize_integer(n)
@@ -73,17 +87,16 @@ module Marshal
       s = to_byte(n + 5)
     elsif n < 0 and n > -124
       s = to_byte(256 + (n - 5))
-    elsif n < 0
-      s = "\x00"
     else
       s = "\x00"
       cnt = 0
-      while n != 0
+      4.times do
         s << to_byte(n & 0xFF)
         n >>= 8
         cnt += 1
+        break if n == 0 or n == -1
       end
-      s[0] = to_byte(cnt)
+      s[0] = to_byte(n < 0 ? 256 - cnt : cnt)
     end
     s
   end
@@ -113,6 +126,28 @@ module Marshal
     TYPE_MODULE + serialize_integer(obj.name.length) + obj.name
   end
 
+  def self.serialize_instance_variables_prefix(obj)
+    if obj.instance_variables.length > 0
+      TYPE_IVAR
+    else
+      ''
+    end
+  end
+
+  def self.serialize_instance_variables_suffix(obj, cls, sup, depth)
+    if cls == Object or obj.instance_variables.length > 0
+      str = serialize_integer(obj.instance_variables.length)
+      obj.instance_variables.each do |ivar|
+        o = obj.instance_variable_get(ivar.to_sym)
+        str << serialize_symbol(ivar) +
+               serialize(o, o.class, get_superclass(o.class), depth)
+      end
+      str
+    else
+      ''
+    end
+  end
+
   def self.serialize_extended_object(obj)
     str = ''
     get_module_names(obj).each do |mod_name|
@@ -139,10 +174,41 @@ module Marshal
     TYPE_FIXNUM + serialize_integer(obj)
   end
 
-  def self.get_class_names(obj)
-    cls = obj.class
-    sup = get_superclass(cls)
-    [cls.to_s, sup.to_s]
+  def self.serialize_bignum(obj)
+    str = TYPE_BIGNUM + (obj < 0 ? '-' : '+') + "\0"
+    obj = obj.abs
+    while obj != 0
+      str << to_byte(obj & 0xFF)
+      obj >>= 8
+    end
+    str << (str.length % 2 == 0 ? "\0" : '')
+    str[2] = to_byte(((str.length - 7) / 2) + 7)
+    str
+  end
+
+  def self.serialize_regexp(obj)
+    str = obj.source
+    serialize_extended_object(obj) +
+    serialize_user_class(obj, Regexp) +
+    TYPE_REGEXP + serialize_integer(str.length) +
+    str + to_byte(obj.options & 0x7)
+  end
+
+  def self.serialize_object(obj)
+    serialize_extended_object(obj) +
+    TYPE_OBJECT + serialize_symbol(obj.class.name)
+  end
+
+  def self.serialize_struct(obj, depth)
+    str = serialize_extended_object(obj) +
+    TYPE_STRUCT + serialize_symbol(obj.class.name) +
+    serialize_integer(obj.length)
+
+    obj.each_pair do |sym, val|
+      str << serialize_symbol(sym) +
+             serialize(val, val.class, get_superclass(val.class), depth)
+    end
+    str
   end
 
   def self.get_superclass(cls)
@@ -160,7 +226,7 @@ module Marshal
       names << sup.name
       sup = sup.superclass
     end
-    names.reverse
+    names
   end
 
   def self.to_byte(n)
