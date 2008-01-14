adad84f7a26bf40809366f2f7b6acfc61dcfefc2
Charles Nutter <headius@charles-nutters-computer.local>
2008-01-14 00:31:12 -0600

    Class.inherited gets invoked regardless of visibility

--- spec/ruby/1.8/core/class/inherited_spec.rb
+++ spec/ruby/1.8/core/class/inherited_spec.rb
@@ -18,4 +18,18 @@ describe "Class.inherited" do
       $child_class = nil
     end
   end
+
+  it "will be invoked by child class regardless of visibility" do
+    top = Class.new do
+      class << self
+        def inherited(cls); end
+      end
+    end
+
+    class << top; private :inherited; end
+    lambda { Class.new(top) }.should_not raise_error
+
+    class << top; protected :inherited; end
+    lambda { Class.new(top) }.should_not raise_error
+  end
 end
