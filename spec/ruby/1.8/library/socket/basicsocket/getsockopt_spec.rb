1ea4f82183190c4c87da48c381f1db417c7403ac
Caleb Tennis <caleb@tarknology.com>
2008-01-14 09:26:36 -0500

    Updated some socket specs

--- spec/ruby/1.8/library/socket/basicsocket/getsockopt_spec.rb
+++ spec/ruby/1.8/library/socket/basicsocket/getsockopt_spec.rb
@@ -7,9 +7,13 @@ describe "BasicSocket#getsockopt" do
     @sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
   end
   
-  it "identifies the socket type" do
+  it "identifies the SOCK_STREAM socket type" do
     n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE)
-    n.should == [Socket::SOCK_STREAM].pack("i")  
+    n.should == [Socket::SOCK_STREAM].pack("i")
+  end
+
+  it "identifies the SOCK_DGRAM socket type" do
+    UDPSocket.new.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE).should == [Socket::SOCK_DGRAM].pack("i")
   end
   
   it "identifies the socket error" do
---
+++ spec/ruby/1.8/library/socket/shared/pack_sockaddr.rb
@@ -0,0 +1,38 @@
+shared :pack_sockaddr_in do |cmd|
+  describe "Socket##{cmd}" do
+
+    it "properly encodes the sin_family portion of sockaddr_in struct" do
+      Socket.sockaddr_in(80, "").unpack("sslc")[0].should == 2
+    end
+
+    it "properly encodes the sin_port portion of sockaddr_in struct" do
+      Socket.sockaddr_in(0, "").unpack("sslc")[1].should == 0
+      Socket.sockaddr_in(1, "").unpack("sslc")[1].should == 256    # possibly different on big endian machines
+      Socket.sockaddr_in(80, "").unpack("sslc")[1].should == 20480 # possibly different on big endian machines
+    end 
+
+    it "properly encodes the sin_addr portion of sockaddr_in struct" do
+      Socket.sockaddr_in(0, "127.0.0.1").unpack("sslc")[2].should == 16777343
+      Socket.sockaddr_in(0, "0.0.0.0").unpack("sslc")[2].should == 0
+    end
+   
+    not_compliant_on :rubinius do
+      it "encodes an empty sin_addr in sockaddr_in struct as 0" do
+        Socket.sockaddr_in(80, "").unpack("sslc")[2].should == 0
+      end
+    end
+  end
+end
+
+
+shared :pack_sockaddr_un do |cmd|
+  describe "Socket##{cmd}" do
+
+    not_compliant_on :rubinius do
+      it "encodes the unix socket type into the string" do
+        Socket.sockaddr_un("/tmp/s").unpack("ccA104")[2].should == "/tmp/s"
+      end
+    end
+
+  end
+end
