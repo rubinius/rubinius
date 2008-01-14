c21636d6b2502db344049e7dc62d42ff8c18b040
Caleb Tennis <caleb@tarknology.com>
2008-01-13 19:34:50 -0500

    Add specs for BasicSocket#getsockopt

--- spec/ruby/1.8/library/socket/basicsocket/getsockopt_spec.rb
+++ spec/ruby/1.8/library/socket/basicsocket/getsockopt_spec.rb
@@ -1,2 +1,21 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/classes'
+
+describe "BasicSocket#getsockopt" do
+  
+  before(:each) do
+    @sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+  end
+  
+  it "identifies the socket type" do
+    n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE)
+    n.should == [Socket::SOCK_STREAM].pack("i")  
+  end
+  
+  it "identifies the socket error" do
+    n = @sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR)
+    n.should == [0].pack("i")  
+  end 
+  
+end
+
