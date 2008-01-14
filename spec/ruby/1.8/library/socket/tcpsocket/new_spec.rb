c561368c03c605de41746fac2ce5a6386fcf4f54
Charles Nutter <headius@charles-nutters-computer.local>
2008-01-14 03:17:15 -0600

    Quarantine a suspicious TCPSocket.new speck failing on MRI on OS X.

--- spec/ruby/1.8/library/socket/tcpsocket/new_spec.rb
+++ spec/ruby/1.8/library/socket/tcpsocket/new_spec.rb
@@ -10,9 +10,12 @@ describe "TCPSocket.new" do
     lambda { TCPSocket.new('127.0.0.1', SocketSpecs.port) }.should raise_error(Errno::ECONNREFUSED)
   end
   
+# This isn't passing on MRI on at least OS X for me (headius)
+quarantine! do
   it "connects to a listening server" do
     @server = TCPServer.new(SocketSpecs.port)
     @server.accept_nonblock
     lambda { TCPSocket.new('127.0.0.1', SocketSpecs.port) }.should_not raise_error(Errno::ECONNREFUSED)
   end
+end
 end
