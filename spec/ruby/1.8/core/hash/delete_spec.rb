a2b8b5511e79b47fa7777e716ee16511fdec3fd4
Charles Nutter <headius@charles-nutters-computer.local>
2008-01-14 16:47:21 -0600

    Remove the goofy Hash#delete spec and replace with two others.
    The old version of this example depended on individual hash buckets having a
    specific ordering, which overreaches a bit. The new version, while a little
    cumbersome, should work correctly regardless of hash implementation or hash
    and bucket ordering.
    
    I also moved out a few lines that were unrelated to this example into a
    separate one.

--- spec/ruby/1.8/core/hash/delete_spec.rb
+++ spec/ruby/1.8/core/hash/delete_spec.rb
@@ -2,27 +2,45 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Hash#delete" do
-  it "removes the first entry (#keys order) whose key is == key and return the deleted value" do
+  it "removes the entry and returns the deleted value" do
     h = {:a => 5, :b => 2}
     h.delete(:b).should == 2
     h.should == {:a => 5}
+  end
 
+  it "calls eql? on colliding keys it finds" do
     h = Hash.new
-    k1 = ["x"]
-    k2 = ["y"]
-    # So they end up in the same bucket
-    def k1.hash() 0 end
-    def k2.hash() 0 end
 
-    h[k1] = 1
-    h[k2] = 2
-    h.size.should == 2
-    k1.replace(k2)
-    h.size.should == 2
+    # a class that tracks how many times eql? is called
+    eql_counter = Class.new do
+      def eql_count; @eql_count ||= 0; end
+      def eql_count=(count); @eql_count = count; end
+      # defer to super, so both get inserted
+      def eql?(obj); self.eql_count += 1; super; end
+    end
 
-    first_value = h.values.first
-    h.delete(k2).should == first_value
-    h.size.should == 1
+    # two equals_counter objects with the same hash
+    obj1 = eql_counter.new
+    obj2 = eql_counter.new
+    def obj1.hash; 0; end
+    def obj2.hash; 0; end
+
+    h[obj1] = :a
+    h[obj2] = :b
+
+    # obj2 has the same hash, so eql? should have been called once
+    obj2.eql_count.should == 1
+
+    # now always return true, so first key encountered is deleted
+    def obj1.eql?(obj); self.eql_count += 1; true; end
+    def obj2.eql?(obj); self.eql_count += 1; true; end
+
+    # delete one of them
+    h.delete(obj1)
+
+    # assert that between the two objects, eql? was called twice total
+    # we can't assert specific counts since we shouldn't specify bucket ordering
+    (obj1.eql_count + obj2.eql_count).should == 2
   end
 
   it "calls supplied block if the key is not found" do
