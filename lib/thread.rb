2b1547cff38ea43e0ceaadbca9295e12f23257d7
Charles Comstock <dgtized@gmail.com>
2008-01-15 15:12:23 -0600

    copied Thread.exclusive from stdlib/thread.rb into lib/thread.rb

--- lib/thread.rb
+++ lib/thread.rb
@@ -21,6 +21,22 @@ if $DEBUG
   Thread.abort_on_exception = true
 end
 
+class Thread
+  #
+  # Wraps a block in Thread.critical, restoring the original value upon exit
+  # from the critical section.
+  #
+  def Thread.exclusive
+    _old = Thread.critical
+    begin
+      Thread.critical = true
+      return yield
+    ensure
+      Thread.critical = _old
+    end
+  end
+end
+
 class Mutex
   def initialize
     @lock = Channel.new
