a48cbbd3f5da3c971a215423b3e27b058de04196
Dirkjan Bussink <d.bussink@gmail.com>
2008-01-15 21:15:09 +0100

    Add more Date specs

--- spec/ruby/1.8/library/date/minus_spec.rb
+++ spec/ruby/1.8/library/date/minus_spec.rb
@@ -1,20 +1,14 @@
 require 'date' 
 require File.dirname(__FILE__) + '/../../spec_helper'
-require File.dirname(__FILE__) + '/shared/parse'
-require File.dirname(__FILE__) + '/shared/parse_us'
-require File.dirname(__FILE__) + '/shared/parse_eu'
 
 describe "Date#-" do
 
-
-  # The space separator is also different, doesn't work for only numbers
-  it "substract a number of days from a Date" do
+  it "should substract a number of days from a Date" do
     d = Date.civil(2007, 5 ,2) - 13
     d.should == Date.civil(2007, 4, 19)
   end
   
-  # The space separator is also different, doesn't work for only numbers
-  it "substract a negative number of days from a Date" do
+  it "should substract a negative number of days from a Date" do
     d = Date.civil(2007, 4, 19).-(-13)
     d.should == Date.civil(2007, 5 ,2) 
   end
@@ -27,7 +21,7 @@ describe "Date#-" do
     
   end
 
-  it "shoud raise an error on non numeric parameters" do
+  it "should raise an error on non numeric parameters" do
     lambda { Date.civil(2007,2,27) - :hello }.should raise_error(TypeError)
     lambda { Date.civil(2007,2,27) - "hello" }.should raise_error(TypeError)
     lambda { Date.civil(2007,2,27) - Object.new }.should raise_error(TypeError)
