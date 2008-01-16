4e8a0d264dc7d5a4866a1a1b83238bebb47e4ab6
Jonas Pfenniger <zimbatm@oree.ch>
2008-01-16 21:51:52 +0100

    Removed platform-specific specs in spec/ruby/1.8/core/signal/list_spec.rb
    There is not direct way to know if a signal exists or not, since it all
    depends on <signal.h>. In practice, there is no real risk for rubinius
    to miss a signal.

--- spec/ruby/1.8/core/signal/list_spec.rb
+++ spec/ruby/1.8/core/signal/list_spec.rb
@@ -48,33 +48,6 @@ describe "Signal.list" do
     INFO
   }
 
-  LINUX_SIGNALS = {"PWR"=>30, "USR1"=>10, "BUS"=>7, "USR2"=>12, "TERM"=>15, 
-    "SEGV"=>11, "KILL"=>9, "EXIT"=>0, "POLL"=>29, "STOP"=>19, "SYS"=>31,
-    "TRAP"=>5, "IOT"=>6, "HUP"=>1, "INT"=>2, "WINCH"=>28, "XCPU"=>24, 
-    "TTIN"=>21, "CLD"=>17, "TSTP"=>20, "FPE"=>8, "IO"=>29, "TTOU"=>22, 
-    "PROF"=>27, "CHLD"=>17, "CONT"=>18, "PIPE"=>13, "ABRT"=>6, "VTALRM"=>26, 
-    "QUIT"=>3, "ILL"=>4, "XFSZ"=>25, "URG"=>23, "ALRM"=>14}
-
-  DARWIN_SIGNALS = {"USR1"=>30, "BUS"=>10, "USR2"=>31, "TERM"=>15, "SEGV"=>11,
-    "KILL"=>9, "EMT"=>7, "EXIT"=>0, "STOP"=>17, "SYS"=>12, "TRAP"=>5, 
-    "INFO"=>29, "IOT"=>6, "HUP"=>1, "INT"=>2, "WINCH"=>28, "XCPU"=>24, 
-    "TTIN"=>21, "CLD"=>20, "TSTP"=>18, "FPE"=>8, "IO"=>23, "TTOU"=>22, 
-    "PROF"=>27, "CHLD"=>20, "CONT"=>19, "PIPE"=>13, "ABRT"=>6, "VTALRM"=>26, 
-    "QUIT"=>3, "ILL"=>4, "XFSZ"=>25, "URG"=>16, "ALRM"=>14}
-
-  it "returns a hash of signal names mapped to the corresponding signal numbers" do
-    # FIXME: values may change trough the arches too
-    platform_is :linux do
-      Signal.list.should == LINUX_SIGNALS
-    end
-    platform_is :darwin do
-      Signal.list.should == DARWIN_SIGNALS
-    end
-    platform_is_not :linux, :darwin do
-      raise "FIXME, please add platform support"
-    end
-  end
-
   it "doesn't contain other signals than in 1.8" do
     (Signal.list.keys - RUBY18_SIGNALS).should == []
   end
