c47b473b99b59074673adb7e8d50a250e34436e7
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2008-01-18 17:37:29 +0800

    Use a regexp to match the inspect output instead of deviating on rbx

--- spec/ruby/1.8/core/method/shared/to_s.rb
+++ spec/ruby/1.8/core/method/shared/to_s.rb
@@ -1,13 +1,7 @@
 shared :method_to_s do |cmd|
   describe "Method##{cmd}" do
     it "returns a string representing the method" do
-      compliant_on :ruby, :jruby do
-        11.method("+").send(cmd).should == "#<Method: Fixnum#+>"
-      end
-
-      deviates_on :rubinius do
-        11.method("+").send(cmd).should == "#<Method Fixnum(Fixnum)#+>"
-      end
+      11.method("+").send(cmd).should =~ /#<Method(:)? Fixnum(\(Fixnum\))?#\+>/
     end
   end
 end
