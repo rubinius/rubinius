fc8c2c5584305b3e0b2a74ba8250a0b7072a372f
Ben Hughes <ben@pixelmachine.org>
2008-01-19 16:20:55 -0500

    Added specs for File::Stat#ftype. #264.
    * Tests "file" and "directory"
    
    Signed-off-by: Eero Saynatkari <rubinius@projects.kittensoft.org>

--- spec/ruby/1.8/core/file/stat/ftype_spec.rb
+++ spec/ruby/1.8/core/file/stat/ftype_spec.rb
@@ -1,1 +1,16 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "File::Stat.ftype" do
+  it "returns file for a file" do
+    file = "/tmp/i_am_file"
+    File.open(file, "w") {}
+    File.stat(file).ftype.should == "file"
+  end
+
+  it "returns directory for a dir" do
+    File.stat('/tmp').ftype.should == "directory"
+  end
+  #add in tests for characterSpecial, blockSpecial, fifo, link, and socket
+end
+
+
