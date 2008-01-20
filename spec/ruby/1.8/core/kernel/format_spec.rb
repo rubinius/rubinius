6f08d5e21473d0f2adff66a32acd46ddd8945fa0
Brian Ford <brixen@gmail.com>
2008-01-20 12:47:11 -0800

    Added spec for Kernel.format.

--- spec/ruby/1.8/core/kernel/format_spec.rb
+++ spec/ruby/1.8/core/kernel/format_spec.rb
@@ -6,3 +6,9 @@ describe "Kernel#format" do
     Kernel.private_instance_methods.should include("format")
   end
 end
+
+describe "Kernel.format" do
+  it "is accessible as a module function" do
+    Kernel.format("%s", "hello").should == "hello"
+  end
+end
