7e3474a16ec20094630e865594405ea7f1658c58
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2008-01-21 10:08:06 +0800

    Module#instance_method raises TypeError/ArgumentError on invalid arg
    * Fixed spec to expect TypeError when passed nil
    * Fixed spec to expect ArgumentError when passed non-symbol/string

--- spec/ruby/1.8/core/module/instance_method_spec.rb
+++ spec/ruby/1.8/core/module/instance_method_spec.rb
@@ -20,6 +20,7 @@ describe "Module#instance_method" do
   end
   
   it "raises a TypeError if the given name is not a string/symbol" do
-    lambda { Object.instance_method(mock('x')) }.should raise_error(TypeError)
+    lambda { Object.instance_method(nil)       }.should raise_error(TypeError)
+    lambda { Object.instance_method(mock('x')) }.should raise_error(ArgumentError)
   end
 end
