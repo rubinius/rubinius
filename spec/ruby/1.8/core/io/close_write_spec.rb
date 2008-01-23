c78091236495f4a16aa874de97cce3ec485c1f5b
Vladimir Sizikov <vsizikov@gmail.com>
2008-01-23 12:54:25 +0100

    Added IO#close_write and IO#close_read specs.
    And all of them fail in rubinius. (not implemented)
    And most of them fail in JRuby. (recent bugs)
    They pass just fine on MRI 1.8.6 (p111 and latest)

--- spec/ruby/1.8/core/io/close_write_spec.rb
+++ spec/ruby/1.8/core/io/close_write_spec.rb
@@ -1,1 +1,38 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO#close_write" do
+  before :each do
+    @io = IO.popen(RUBY_NAME, "r+")
+  end
+  after :each do
+    @io.close unless @io.closed?
+  end
+
+  it "closes the write end of a duplex I/O stream" do
+    @io.close_write
+    lambda { @io.print("attempt to write") }.should raise_error(IOError)
+    lambda { @io.syswrite("attempt to write") }.should raise_error(IOError)
+  end
+
+  it "raises an IOError on subsequent invocations" do
+    @io.close_write
+    lambda { @io.close_write }.should raise_error(IOError)
+    lambda { @io.close_write }.should raise_error(IOError)
+  end
+
+  it "allows subsequent invocation of close" do
+    @io.close_write
+    lambda { @io.close }.should_not raise_error
+  end
+
+  it "raises an IOError if the stream is not duplexed." do
+     @io = IO.popen("ls", "r")
+     lambda { @io.close_write }.should raise_error(IOError)
+  end
+
+  it "flushes and closes the write stream" do
+    @io.print("p 12345")
+    @io.close_write
+    @io.read.should == "12345\n"
+  end
+end
