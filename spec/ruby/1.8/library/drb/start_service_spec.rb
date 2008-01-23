fd5fb764ee21b354b75b84f34906663874a24639
Charles Comstock <dgtized@gmail.com>
2008-01-22 22:28:02 -0600

    spec for DRb method call using a block

--- spec/ruby/1.8/library/drb/start_service_spec.rb
+++ spec/ruby/1.8/library/drb/start_service_spec.rb
@@ -15,5 +15,22 @@ describe "DRb.start_server" do
     obj.add(1,2,3).should == 6
     DRb.stop_service
   end
+
+  it "should run a basic remote call passing a block" do
+    class TestServer
+      def add1(x)        
+        return (yield x)+1
+      end
+    end
+    
+    DRb.start_service('druby://localhost:9001', TestServer.new)
+    
+    obj = DRbObject.new(nil, 'druby://localhost:9001')
+    obj.add1(2) do |i|
+      i.should == 2
+      i+1
+    end.should == 4
+    DRb.stop_service
+  end
 end
 
