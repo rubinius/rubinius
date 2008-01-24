13dbdf62e802028cb61f9375196712f0b789ff37
Vladimir Sizikov <vsizikov@gmail.com>
2008-01-24 21:30:02 +0100

    Added some SystemCallError specs.
    And exclusions for rubinius too.

--- spec/ruby/1.8/core/exception/system_call_error_spec.rb
+++ spec/ruby/1.8/core/exception/system_call_error_spec.rb
@@ -8,6 +8,12 @@ describe "SystemCallError.new" do
   it "takes an optional errno argument" do
     SystemCallError.should be_ancestor_of(SystemCallError.new("message",1).class)
   end
+
+  it "accepts single Fixnum argument as errno" do
+    SystemCallError.new(-2**24).errno.should == -2**24
+    SystemCallError.new(42).errno.should == 42
+    SystemCallError.new(2**24).errno.should == 2**24
+  end
 end
 
 describe "SystemCallError#errno" do
@@ -16,6 +22,24 @@ describe "SystemCallError#errno" do
   end  
   
   it "returns the errno given as optional argument to new" do
+    SystemCallError.new("message", -2**30).errno.should == -2**30
+    SystemCallError.new("message", -1).errno.should == -1
+    SystemCallError.new("message", 0).errno.should == 0
+    SystemCallError.new("message", 1).errno.should == 1
     SystemCallError.new("message", 42).errno.should == 42
+    SystemCallError.new("message", 2**30).errno.should == 2**30
   end
 end
+
+describe "SystemCallError#message" do
+  it "should return default message when no message given" do
+    SystemCallError.new(2**28).message.should =~ /Unknown error/
+  end
+
+  it "returns the message given as an argument to new" do
+    SystemCallError.new("message", 1).message.should  =~ /message/
+    SystemCallError.new("XXX").message.should =~ /XXX/
+  end
+end
+
+
