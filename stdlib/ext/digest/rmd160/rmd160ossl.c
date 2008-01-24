17a1fc286f2f5ea09a590a303866a049a39614e5
Eric Hodel <drbrain@segment7.net>
2008-01-23 21:42:23 -0800

    Digest is now implemented in lib, remove stdlib cruft.

--- stdlib/ext/digest/rmd160/rmd160ossl.c
+++
@@ -1,8 +0,0 @@
-/* $Id: rmd160ossl.c 11908 2007-02-27 11:51:55Z knu $ */
-
-#include "defs.h"
-#include "rmd160ossl.h"
-
-void RMD160_Finish(RMD160_CTX *ctx, char *buf) {
-	RIPEMD160_Final((unsigned char *)buf, ctx);
-}
