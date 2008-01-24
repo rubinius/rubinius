704c2df9dd10c3dc511fa7ef1ea2443ad624f186
Eric Hodel <drbrain@segment7.net>
2008-01-23 18:14:51 -0800

    Fix whitespace, RDoc, extra empty parens

--- lib/digest.rb
+++ lib/digest.rb
@@ -1,17 +1,17 @@
 module Digest
 
- # Generates a hex-encoded version of a given _string_
+ # Generates a hex-encoded version of a given +string+
   def Digest.hexencode(string)
     hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-    
+
     result = ' '*(string.length * 2)
-    
+
     string.split('').to_a.each_with_index do |byte,i|
       byte = byte[0]
       result[i + i]     = hex[byte >> 4];
       result[i + i + 1] = hex[byte & 0x0f];
     end
-    
+
     result
   end
 
@@ -23,17 +23,16 @@ module Digest
     #     digest_obj.update(string) -> digest_obj
     #     digest_obj << string -> digest_obj
     #
-    # Updates the digest using a given _string_ and returns self.
+    # Updates the digest using a given +string+ and returns self.
     #
-    # The update() method and the left-shift operator are overridden by
+    # The update method and the left-shift operator are overridden by
     # each implementation subclass. (One should be an alias for the
     # other)
     def update(string)
-      raise RuntimeError, "#{self.class} does not implement update()"
+      raise RuntimeError, "#{self.class} does not implement update"
     end
     alias :<< :update
 
- 
     # call-seq:
     #     digest_obj.instance_eval { finish } -> digest_obj
     #
@@ -42,13 +41,12 @@ module Digest
     # This method is overridden by each implementation subclass and often
     # made private, because some of those subclasses may leave internal
     # data uninitialized.  Do not call this method from outside.  Use
-    # #digest!() instead, which ensures that internal data be reset for
+    # #digest! instead, which ensures that internal data be reset for
     # security reasons.
     def finish(string)
-      raise RuntimeError, "#{self.class} does not implement finish()"
+      raise RuntimeError, "#{self.class} does not implement finish"
     end
 
-    #
     # call-seq:
     #     digest_obj.reset -> digest_obj
     #
@@ -56,14 +54,14 @@ module Digest
     #
     # This method is overridden by each implementation subclass.
     def reset(string)
-      raise RuntimeError, "#{self.class} does not implement reset()"
+      raise RuntimeError, "#{self.class} does not implement reset"
     end
 
     # call-seq:
     #     digest_obj.new -> another_digest_obj
     #
     # Returns a new, initialized copy of the digest object.  Equivalent
-    # to digest_obj.clone().reset().
+    # to digest_obj.clone.reset.
     def new
       self.clone.reset
     end
@@ -75,11 +73,11 @@ module Digest
     # If none is given, returns the resulting hash value of the digest,
     # keeping the digest's state.
     #
-    # If a _string_ is given, returns the hash value for the given
-    # _string_, resetting the digest to the initial state before and
+    # If a +string+ is given, returns the hash value for the given
+    # +string+, resetting the digest to the initial state before and
     # after the process.
     def digest(string=nil)
-      if (string) 
+      if (string)
         reset
         update(string)
         value = finish
@@ -89,7 +87,7 @@ module Digest
         value = clone.finish
         clone.reset
       end
-      
+
       value
     end
 
@@ -111,8 +109,8 @@ module Digest
     # If none is given, returns the resulting hash value of the digest in
     # a hex-encoded form, keeping the digest's state.
     #
-    # If a _string_ is given, returns the hash value for the given
-    # _string_ in a hex-encoded form, resetting the digest to the initial
+    # If a +string+ is given, returns the hash value for the given
+    # +string+ in a hex-encoded form, resetting the digest to the initial
     # state before and after the process.
     def hexdigest(data=nil)
       Digest.hexencode(digest(data))
@@ -124,15 +122,15 @@ module Digest
     # Returns the resulting hash value and resets the digest to the
     # initial state.
     def hexdigest!
-      result = hexdigest()
-      reset()
+      result = hexdigest
+      reset
       result
     end
 
     # call-seq:
     #     digest_obj.to_s -> string
     #
-    # Returns digest_obj.hexdigest().
+    # Returns digest_obj.hexdigest.
     def to_s
       hexdigest
     end
@@ -157,14 +155,14 @@ module Digest
       return hexdigest == other.hexdigest if other.is_a? Digest::Instance
       to_s == other.to_str
     end
-    
+
     # call-seq:
     #     digest_obj.digest_length -> integer
     #
     # Returns the length of the hash value of the digest.
     #
     # This method should be overridden by each implementation subclass.
-    # If not, digest_obj.digest().length() is returned.
+    # If not, digest_obj.digest.length is returned.
     def digest_length
       digest.length
     end
@@ -173,10 +171,10 @@ module Digest
     #     digest_obj.length -> integer
     #     digest_obj.size -> integer
     #
-    # Returns digest_obj.digest_length().
+    # Returns digest_obj.digest_length.
     alias :length :digest_length
     alias :size   :digest_length
-    
+
     # call-seq:
     #     digest_obj.block_length -> integer
     #
@@ -184,7 +182,7 @@ module Digest
     #
     # This method is overridden by each implementation subclass.
     def block_length
-      raise RuntimeError, "#{self.class} does not implement block_length()"
+      raise RuntimeError, "#{self.class} does not implement block_length"
     end
 
   end
@@ -193,19 +191,19 @@ module Digest
     # call-seq:
     #     Digest::Class.digest(string, #parameters) -> hash_string
     #
-    # Returns the hash value of a given _string_.  This is equivalent to
+    # Returns the hash value of a given +string+.  This is equivalent to
     # Digest::Class.new(#parameters).digest(string), where extra
-    # _parameters_, if any, are passed through to the constructor and the
-    # _string_ is passed to #digest().
+    # +parameters+, if any, are passed through to the constructor and the
+    # +string+ is passed to #digest.
     def digest(data)
       raise ArgumentError, "no data given" unless data
       self.new.digest(data)
     end
-    
+
     # call-seq:
     #     Digest::Class.hexdigest(string[, ...]) -> hash_string
     #
-    # Returns the hex-encoded hash value of a given _string_.  This is
+    # Returns the hex-encoded hash value of a given +string+.  This is
     # almost equivalent to
     # Digest.hexencode(Digest::Class.new(*parameters).digest(string)).
     def hexdigest(string = nil)
@@ -224,7 +222,8 @@ module Digest
 
       Digest.hexencode data
     end
-   
+
   end
-  
+
 end
+
