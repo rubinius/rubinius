6735df441af2489d47674b0cc500dab37dd4319e
Jonathan Younger <jonathan@daikini.com>
2008-01-24 17:09:49 -0700

    Simplify Dir#open spec dependencies.
    The "takes a block which yields the Dir instance and closes it after"
    expectation was failing because it depended on File.for_fd working
    properly with closed file descriptors which it does not.
    
    This revision removes the dependency on File.for_fd as well as
    IO.sysopen (which is not yet implemented in jruby) such that
    the spec now passes on ruby, rbx and jruby.
    
    Signed-off-by: Eero Saynatkari <rubinius@projects.kittensoft.org>

--- spec/ruby/1.8/core/dir/shared/open.rb
+++ spec/ruby/1.8/core/dir/shared/open.rb
@@ -11,18 +11,12 @@ shared :dir_open do |cmd|
     end
   
     it "takes a block which yields the Dir instance and closes it after" do
-      # This is a bit convoluted but we are trying to ensure the file gets closed.
-      # To do that, we peek to see what the next FD number is and then probe that
-      # to see whether it has been closed.
-      peek = IO.sysopen DirSpecs.mock_dir
-      File.for_fd(peek).close
-
-      Dir.send(cmd, DirSpecs.mock_dir) { }
-      lambda { File.for_fd peek }.should raise_error(SystemCallError)
+      closed_dir = Dir.send(cmd, DirSpecs.mock_dir) { |dir| dir }
+      lambda { closed_dir.close }.should raise_error(IOError, "closed directory")
     end
   
     it "returns the value of the block if a block is passed" do
-      Dir.open(DirSpecs.mock_dir) {|dir| dir.class}.should == Dir
+      Dir.open(DirSpecs.mock_dir) {|dir| dir.class }.should == Dir
     end
   end
 end
