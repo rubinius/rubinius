015a0d023e8c649160800ddb8a269aa789266d51
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-01-26 13:31:00 -0500

    Added/changed the Dir open specs after previous simplification.

--- spec/ruby/1.8/core/dir/shared/open.rb
+++ spec/ruby/1.8/core/dir/shared/open.rb
@@ -5,18 +5,31 @@ shared :dir_open do |cmd|
       dir.class.should == Dir
       dir.close
     end
-  
+
     it "raises a SystemCallError if the directory does not exist" do
       lambda {  Dir.send cmd, DirSpecs.nonexistent }.should raise_error(SystemCallError)
     end
-  
-    it "takes a block which yields the Dir instance and closes it after" do
+
+    it "may take a block which is yielded to with the Dir instance" do
+      Dir.send(cmd, DirSpecs.mock_dir) {|dir| dir.class.should == Dir }
+    end
+
+    it "returns the value of the block if a block is given" do
+      Dir.open(DirSpecs.mock_dir) {|dir| :value }.should == :value
+    end
+
+    it "closes the Dir instance when the block exits if given a block" do
       closed_dir = Dir.send(cmd, DirSpecs.mock_dir) { |dir| dir }
       lambda { closed_dir.close }.should raise_error(IOError, "closed directory")
     end
-  
-    it "returns the value of the block if a block is passed" do
-      Dir.open(DirSpecs.mock_dir) {|dir| dir.class }.should == Dir
+
+    it "closes the Dir instance when the block exits the block even due to an exception" do
+      @closed_dir = nil
+
+      l = lambda { Dir.send(cmd, DirSpecs.mock_dir) { |dir| @closed_dir = dir; raise } }
+      l.should raise_error
+
+      lambda { @closed_dir.close }.should raise_error(IOError, "closed directory")
     end
   end
 end
