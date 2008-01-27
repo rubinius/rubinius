bca6aef9b81166f9c5f4aeaafc673a54710d4d35
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-01-26 19:57:33 -0500

    Corrected semantics of spec statements for Module#const_get.
    * Specs were correct but the description was inaccurate.
    * Prettified just a little.

--- spec/ruby/1.8/core/module/const_get_spec.rb
+++ spec/ruby/1.8/core/module/const_get_spec.rb
@@ -2,28 +2,28 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Module#const_get" do
-  extended_on :rubinius do
-    it "returns the value of the constant with the given String is defined in its parent" do
-      ModuleSpecs.const_get("Super::SuperChild").should == ModuleSpecs::Super::SuperChild
-      lambda {
-        ModuleSpecs.const_get("Super::Something")
-      }.should raise_error(NameError)
-    end
-  end
 
-  it "returns the value of its own constant with the given name" do
+  it "returns the constant with the given name" do
     ModuleSpecs.const_get(:Basic).should == ModuleSpecs::Basic
     ModuleSpecs.const_get("Child").should == ModuleSpecs::Child
   end
 
-  it "returns the value of the top level constant with the given name" do
+  extended_on :rubinius do
+    it "returns the value of the constant when a scoped constant name" do
+      ModuleSpecs.const_get("Super::SuperChild").should == ModuleSpecs::Super::SuperChild
+
+      lambda {
+        ModuleSpecs.const_get("Super::NonExistingConstantName")
+      }.should raise_error(NameError)
+    end
+  end
+
+  it "returns the top-level constant with the given name" do
     ModuleSpecs.const_get(:TopLevelConst).should == TopLevelConst
   end
 
   it "raises a NameError when there is no constant with the given name" do
-    lambda {
-      ModuleSpecs.const_get("NotExistant").should == nil
-    }.should raise_error(NameError)
+    lambda { ModuleSpecs.const_get("NotExistant") }.should raise_error(NameError)
   end
 
   it "tries to convert the given name to a string using to_str" do
