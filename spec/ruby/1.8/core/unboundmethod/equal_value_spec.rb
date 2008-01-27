57bddb7b38dbb762b2469c51eb961e01f03c8518
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-01-27 14:50:38 -0500

    Updated UnboundMethod#== spec for Rubinius/MRI difference on Modules.
    * Rubinius' UnboundMethod#== is true for methods from included Modules also.

--- spec/ruby/1.8/core/unboundmethod/equal_value_spec.rb
+++ spec/ruby/1.8/core/unboundmethod/equal_value_spec.rb
@@ -65,14 +65,19 @@ describe "UnboundMethod#==" do
 
   # See below for MRI
   deviates_on :rubinius do
-    it "returns true if same method but one extracted from a subclass" do
+    it "returns true if same method extracted from super- and subclass" do
       (@parent == @child1).should == true
       (@child1 == @parent).should == true
     end
 
-    it "returns true if same method but extracted from two different subclasses" do
+    it "returns true if same method extracted from two different subclasses" do
       (@child2 == @child1).should == true
       (@child1 == @child2).should == true
+    end
+
+    it "returns true if same method and the method is defined in an included Module" do
+      (@includee == @includer).should == true
+      (@includer == @includee).should == true
     end
   end
 
@@ -86,6 +91,7 @@ describe "UnboundMethod#==" do
     (@identical_body == @original_name).should == false
   end
 
+  # See above for Rubinius
   not_compliant_on :rubinius do
     it "returns false if same method but one extracted from a subclass" do
       (@parent == @child1).should == false
@@ -96,11 +102,11 @@ describe "UnboundMethod#==" do
       (@child2 == @child1).should == false
       (@child1 == @child2).should == false
     end
-  end
 
-  it "returns false if methods are the same but added from an included Module" do
-    (@includee == @includer).should == false
-    (@includer == @includee).should == false
+    it "returns false if methods are the same but added from an included Module" do
+      (@includee == @includer).should == false
+      (@includer == @includee).should == false
+    end
   end
 
   it "returns false if both have same Module, same name, identical body but not the same" do
