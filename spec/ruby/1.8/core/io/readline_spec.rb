30116d672d950687646c1668eac4d9f5b10f4df7
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-01-27 18:53:19 -0500

    IO#readline EOFError spec modified from Chen Yufei's patch.
    * Patch was out-of-date, applied by hand.

--- spec/ruby/1.8/core/io/readline_spec.rb
+++ spec/ruby/1.8/core/io/readline_spec.rb
@@ -1,1 +1,12 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO#readline" do
+  @testfile = File.dirname(__FILE__) + '/fixtures/gets.txt'
+
+  it "raises EOFError on end of stream" do
+    File.open(@testfile, 'r') do |f|
+      lambda { loop { f.readline } }.should raise_error(EOFError)
+    end
+  end
+end
+
