faf4af851ad531a242eb0e2ee63089e94dd754de
Caleb Tennis <ctennis@engineyard.com>
2008-01-29 14:52:21 -0500

    Use %w notation

--- lib/fcntl.rb
+++ lib/fcntl.rb
@@ -1,6 +1,8 @@
 module Fcntl
-  Constants = ["F_DUPFD","F_GETFD","F_GETLK","F_SETFD","F_GETFL","F_SETFL","F_SETLK","F_SETLKW","FD_CLOEXEC","F_RDLCK","F_UNLCK",
-    "F_WRLCK","O_CREAT","O_EXCL","O_NOCTTY","O_TRUNC","O_APPEND","O_NONBLOCK","O_NDELAY","O_RDONLY","O_RDWR","O_WRONLY","O_ACCMODE" ]
+  Constants = 
+    %w[ F_DUPFD F_GETFD F_GETLK F_SETFD F_GETFL F_SETFL F_SETLK F_SETLKW 
+        FD_CLOEXEC F_RDLCK F_UNLCK F_WRLCK O_CREAT O_EXCL O_NOCTTY O_TRUNC 
+        O_APPEND O_NONBLOCK O_NDELAY O_RDONLY O_RDWR O_WRONLY O_ACCMODE ]
 
   Constants.each do |constant|
     value = Rubinius::RUBY_CONFIG["rbx.platform.fcntl.#{constant}"]
