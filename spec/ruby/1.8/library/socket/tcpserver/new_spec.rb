8fb2eb68858a1ee1dafb06b833f43d6da817756f
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-01-30 19:29:25 -0500

    Slightly more permissive TCPServer.new spec for hostname string.

--- spec/ruby/1.8/library/socket/tcpserver/new_spec.rb
+++ spec/ruby/1.8/library/socket/tcpserver/new_spec.rb
@@ -5,7 +5,7 @@ describe "TCPServer.new" do
   after(:each) do
     @server.close if @server && !@server.closed?
   end
-  
+
   it "binds to a host and a port" do
     @server = TCPServer.new('127.0.0.1', SocketSpecs.port)
     addr = @server.addr
@@ -13,22 +13,21 @@ describe "TCPServer.new" do
     addr[1].be_kind_of Fixnum
     # on some platforms (Mac), MRI
     # returns comma at the end.
-    addr[2].should =~ /^localhost,?$/
+    addr[2].should =~ /^localhost\b/
     addr[3].should == '127.0.0.1'
   end
-  
+
   it "binds to localhost and a port with either IPv4 or IPv6" do
     @server = TCPServer.new('localhost', SocketSpecs.port)
     addr = @server.addr
     if addr[0] == 'AF_INET'
       addr[1].be_kind_of Fixnum
-      addr[2].should == 'localhost'
+      addr[2].should =~ /^localhost\b/
       addr[3].should == '127.0.0.1'
     else
       addr[1].be_kind_of Fixnum
-      addr[2].should == 'localhost'
+      addr[2].should =~ /^localhost\b/
       addr[3].should == '::1'
     end
   end
-  
 end
