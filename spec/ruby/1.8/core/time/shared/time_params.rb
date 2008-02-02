a5081ca646e99ec94fedfabf03b7eb0a8d37afc3
Dirkjan Bussink <d.bussink@gmail.com>
2008-02-02 23:53:52 +0100

    Fixed Time specs for 64 bit archs

--- spec/ruby/1.8/core/time/shared/time_params.rb
+++ spec/ruby/1.8/core/time/shared/time_params.rb
@@ -23,9 +23,31 @@ shared :time_params do |cmd|
       Time.send(cmd, 1.0, 15.0, 20.0, 1.0, 1.0, 2000.0, :ignored, :ignored, :ignored, :ignored).should == Time.send(cmd, 1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored)
     end
 
+    it "should accept various year ranges" do
+      lambda { Time.send(cmd, 1901, 12, 31, 23, 59, 59, 0) }.should_not raise_error(ArgumentError) # mon
+      lambda { Time.send(cmd, 2037, 12, 31, 23, 59, 59, 0) }.should_not raise_error(ArgumentError) # mon
+      
+      lambda { Time.send(cmd, 1901, 12, 31, 23, 59, 59, 0) }.should_not raise_error(ArgumentError) # mon
+      lambda { Time.send(cmd, 1901, 12, 31, 23, 59, 59, 0) }.should_not raise_error(ArgumentError) # mon
+      
+      if Rubinius::WORDSIZE == 32
+        lambda { Time.send(cmd, 1900, 12, 31, 23, 59, 59, 0) }.should raise_error(ArgumentError) # mon
+        lambda { Time.send(cmd, 2038, 12, 31, 23, 59, 59, 0) }.should raise_error(ArgumentError) # mon
+      end
+      
+      if Rubinius::WORDSIZE == 64
+        lambda { Time.send(cmd, 1900, 12, 31, 23, 59, 59, 0) }.should_not raise_error(ArgumentError) # mon
+        lambda { Time.send(cmd, 2038, 12, 31, 23, 59, 59, 0) }.should_not raise_error(ArgumentError) # mon
+      end
+      
+    end
+
     it "throws ArgumentError for out of range values" do
       # year-based Time.local(year (, month, day, hour, min, sec, usec))
-      lambda { Time.send(cmd, 1111, 12, 31, 23, 59, 59, 0) }.should raise_error(ArgumentError) # year
+      # Year range only fails on 32 bit archs
+      if Rubinius::WORDSIZE == 32
+        lambda { Time.send(cmd, 1111, 12, 31, 23, 59, 59, 0) }.should raise_error(ArgumentError) # year
+      end
       lambda { Time.send(cmd, 2008, 13, 31, 23, 59, 59, 0) }.should raise_error(ArgumentError) # mon
       lambda { Time.send(cmd, 2008, 12, 32, 23, 59, 59, 0) }.should raise_error(ArgumentError) # day
       lambda { Time.send(cmd, 2008, 12, 31, 25, 59, 59, 0) }.should raise_error(ArgumentError) # hour
@@ -38,7 +60,10 @@ shared :time_params do |cmd|
       lambda { Time.send(cmd, 59, 59, 25, 31, 12, 2008, :ignored, :ignored, :ignored, :ignored) }.should raise_error(ArgumentError) # hour
       lambda { Time.send(cmd, 59, 59, 23, 32, 12, 2008, :ignored, :ignored, :ignored, :ignored) }.should raise_error(ArgumentError) # day
       lambda { Time.send(cmd, 59, 59, 23, 31, 13, 2008, :ignored, :ignored, :ignored, :ignored) }.should raise_error(ArgumentError) # month
-      lambda { Time.send(cmd, 59, 59, 23, 31, 12, 1111, :ignored, :ignored, :ignored, :ignored) }.should raise_error(ArgumentError) # year
+      # Year range only fails on 32 bit archs
+      if Rubinius::WORDSIZE == 32
+        lambda { Time.send(cmd, 59, 59, 23, 31, 12, 1111, :ignored, :ignored, :ignored, :ignored) }.should raise_error(ArgumentError) # year
+      end
     end
 
     it "throws ArgumentError for invalid number of arguments" do
