983d053e8111ca4f46906b0ba3ba6e8ed64d4577
Eric Hodel <ehodel@engineyard.com>
2008-02-01 16:18:33 -0800

    Remove unused Compression::ZLib

--- kernel/platform/compression.rb
+++
@@ -1,8 +0,0 @@
-# depends on: ffi.rb
-
-module Compression
-  class ZLib
-    attach_function "compress_inflate", :inflate, [:state, :string], :object
-    attach_function "compress_deflate", :deflate, [:state, :string], :object
-  end
-end
