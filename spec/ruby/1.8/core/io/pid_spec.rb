a1d7b67942aed8d1b185476dee6f2d99403ed227
Vladimir Sizikov <vsizikov@gmail.com>
2008-02-04 17:07:00 +0100

    Added rubyspecs for IO#pid.

--- spec/ruby/1.8/core/io/pid_spec.rb
+++ spec/ruby/1.8/core/io/pid_spec.rb
@@ -1,1 +1,27 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO#pid" do
+  before :each do
+    @file = File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r')
+    @io = IO.open @file.fileno, 'r'
+  end
+
+  after :each do
+    @file.close unless @file.closed?
+  end
+
+  it "returns nil for IO not associated with a process" do
+    @io.pid.should == nil
+  end
+
+  it "returns the ID of a process associated with stream" do
+    IO.popen(RUBY_NAME, "r+") { |io|
+      io.pid.should_not == nil
+    }
+  end
+
+  it "raises IOError on closed stream" do
+    process_io = IO.popen(RUBY_NAME, "r+") { |io| io }
+    lambda { process_io.pid }.should raise_error(IOError)
+  end
+end
