3ae3cafcb10725953c8e595641af277f36c88677
Vladimir Sizikov <vsizikov@gmail.com>
2008-02-04 18:39:34 +0100

    New rubyspecs for IO#to_io.

--- spec/ruby/1.8/core/io/to_io_spec.rb
+++ spec/ruby/1.8/core/io/to_io_spec.rb
@@ -1,1 +1,19 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "IO#to_io" do
+  it "returns self for open stream" do
+    io = IO.new(2, 'w')
+    io.to_io.should == io
+    io.close
+
+    File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r') { |io|
+      io.to_io.should == io
+    }
+  end
+
+  it "returns self for closed stream" do
+    io = File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r')
+    io.close
+    io.to_io.should == io
+  end
+end
