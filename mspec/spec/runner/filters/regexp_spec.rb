8d58e7f5ba23c0c384132b5c0ab32a99c7aabf5e
Brian Ford <bford@engineyard.com>
2008-02-03 16:28:59 -0800

    Added RegexpFilter that does not escape the match string.

---
+++ mspec/spec/runner/filters/regexp_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require File.dirname(__FILE__) + '/../../../runner/mspec'
+require File.dirname(__FILE__) + '/../../../runner/filters/regexp'
+
+describe RegexpFilter, "#to_regexp" do
+  before :each do
+    @filter = RegexpFilter.new nil
+  end
+  
+  it "converts its arguments to Regexp instances" do
+    @filter.to_regexp('a(b|c)', 'b[^ab]', 'cc?').should == [/a(b|c)/, /b[^ab]/, /cc?/]
+  end
+end
