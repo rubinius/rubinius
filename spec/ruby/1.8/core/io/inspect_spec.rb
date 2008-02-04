e33350eddc3441b2ebe06336500e6445406285d1
Vladimir Sizikov <vsizikov@gmail.com>
2008-02-04 22:12:14 +0100

    Moved one IO#inspect spec to File#inspect specs.
    Since the behavior is File-specific.

--- spec/ruby/1.8/core/io/inspect_spec.rb
+++ spec/ruby/1.8/core/io/inspect_spec.rb
@@ -5,8 +5,4 @@ describe "IO#inspect" do
   it "does not raise anything when invoked on closed stream" do
     lambda { IOSpecs.closed_file.inspect }.should_not raise_error
   end
-
-  it "returns string that contains 'closed' when invoked on closed stream" do
-    IOSpecs.closed_file.inspect.should =~ /closed/
-  end
 end
