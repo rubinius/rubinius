c5f9381ee74ed2d9c91cca1dd2ce9719b6f51bd8
Vladimir Sizikov <vsizikov@gmail.com>
2008-02-05 10:07:57 +0100

    Corrected IO test to not interfere with Kernel#puts tests.

--- spec/ruby/1.8/core/io/to_io_spec.rb
+++ spec/ruby/1.8/core/io/to_io_spec.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "IO#to_io" do
   it "returns self for open stream" do
     io = IO.new(2, 'w')
     io.to_io.should == io
-    io.close
 
     File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r') { |io|
       io.to_io.should == io
@@ -12,8 +12,7 @@ describe "IO#to_io" do
   end
 
   it "returns self for closed stream" do
-    io = File.open(File.dirname(__FILE__) + '/fixtures/readlines.txt', 'r')
-    io.close
+    io = IOSpecs.closed_file
     io.to_io.should == io
   end
 end
