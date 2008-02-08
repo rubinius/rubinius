fddaa684bd7e8c403ff96179ca71a5837f609b63
Vladimir Sizikov <vsizikov@gmail.com>
2008-02-08 16:30:20 +0100

    New File#chmod rubyspecs.

--- spec/ruby/1.8/core/file/chmod_spec.rb
+++ spec/ruby/1.8/core/file/chmod_spec.rb
@@ -13,8 +13,51 @@ describe "File#chmod" do
   
   it "returns 0 if successful" do
     @file.chmod(0755).should == 0
-  end 
-    
+  end
+
+  it "always succeeds with any numeric values" do
+    vals = [-2**30, -2**16, -2**8, -2, -1,
+      -0.5, 0, 1, 2, 5.555575, 16, 32, 64, 2**8, 2**16, 2**30]
+    vals.each { |v|
+      lambda { @file.chmod(v) }.should_not raise_error
+    }
+  end
+
+  it "invokes to_int on non-integer argument" do
+    mode = File.stat(@filename).mode
+    (obj = mock('mode')).should_receive(:to_int).and_return(mode)
+    @file.chmod(obj)
+    File.stat(@filename).mode.should == mode
+  end
+
+  it "with '0222' makes file writable but not readable or executable" do
+    @file.chmod(0222)
+    File.readable?(@filename).should == false
+    File.writable?(@filename).should == true
+    File.executable?(@filename).should == false
+  end
+
+  it "with '0444' makes file readable but not writable or executable" do
+    @file.chmod(0444)
+    File.readable?(@filename).should == true
+    File.writable?(@filename).should == false
+    File.executable?(@filename).should == false
+  end
+
+  it "with '0666' makes file readable and writable but not executable" do
+    @file.chmod(0666)
+    File.readable?(@filename).should == true
+    File.writable?(@filename).should == true
+    File.executable?(@filename).should == false
+  end
+
+  it "with '0111' makes file executable but not readable or writable" do
+    @file.chmod(0111)
+    File.readable?(@filename).should == false
+    File.writable?(@filename).should == false
+    File.executable?(@filename).should == true
+  end
+
   platform_is_not :mswin do
     it "modifies the permission bits of the files specified" do
       @file.chmod(0755)
@@ -33,11 +76,54 @@ describe "File.chmod" do
   after :each do
     File.delete(@file) if File.exist?(@file)
   end  
-  
+
   it "returns the number of files modified" do
     @count.should == 1
   end
-  
+
+  it "always succeeds with any numeric values" do
+    vals = [-2**30, -2**16, -2**8, -2, -1,
+      -0.5, 0, 1, 2, 5.555575, 16, 32, 64, 2**8, 2**16, 2**30]
+    vals.each { |v|
+      lambda { File.chmod(v, @file) }.should_not raise_error
+    }
+  end
+
+  it "invokes to_int on non-integer argument" do
+    mode = File.stat(@file).mode
+    (obj = mock('mode')).should_receive(:to_int).and_return(mode)
+    File.chmod(obj, @file)
+    File.stat(@file).mode.should == mode
+  end
+
+  it "with '0222' makes file writable but not readable or executable" do
+    File.chmod(0222, @file)
+    File.readable?(@file).should == false
+    File.writable?(@file).should == true
+    File.executable?(@file).should == false
+  end
+
+  it "with '0444' makes file readable but not writable or executable" do
+    File.chmod(0444, @file)
+    File.readable?(@file).should == true
+    File.writable?(@file).should == false
+    File.executable?(@file).should == false
+  end
+
+  it "with '0666' makes file readable and writable but not executable" do
+    File.chmod(0666, @file)
+    File.readable?(@file).should == true
+    File.writable?(@file).should == true
+    File.executable?(@file).should == false
+  end
+
+  it "with '0111' makes file executable but not readable or writable" do
+    File.chmod(0111, @file)
+    File.readable?(@file).should == false
+    File.writable?(@file).should == false
+    File.executable?(@file).should == true
+  end
+
   platform_is_not :mswin do
     it "modifies the permission bits of the files specified" do
       File.stat(@file).mode.should == 33261
