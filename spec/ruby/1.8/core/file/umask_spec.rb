5f6ac709500cb64df110a44d31e0c0b89dd68aec
Vladimir Sizikov <vsizikov@gmail.com>
2008-02-08 16:39:47 +0100

    New rubyspecs for File#umask.

--- spec/ruby/1.8/core/file/umask_spec.rb
+++ spec/ruby/1.8/core/file/umask_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "File.umask" do
   before :each do
+    @orig_umask = File.umask
     @file = 'test.txt'
     File.open(@file, 'w') {}
   end
@@ -9,6 +10,7 @@ describe "File.umask" do
   after :each do
     File.delete(@file) if File.exists?(@file)
     @file = nil
+    File.umask(@orig_umask)
   end
 
   it "return a Fixnum" do
@@ -21,6 +23,24 @@ describe "File.umask" do
     File.umask.should == 006
   end
 
+  it "invokes to_int on non-integer argument" do
+    (obj = mock(022)).should_receive(:to_int).any_number_of_times.and_return(022)
+    File.umask(obj)
+    File.umask(obj).should == 022
+  end
+
+  it "always succeeds with any integer values" do
+    vals = [-2**30, -2**16, -2**8, -2,
+      -1.5, -1, 0.5, 0, 1, 2, 7.77777, 16, 32, 64, 2**8, 2**16, 2**30]
+    vals.each { |v|
+      lambda { File.umask(v) }.should_not raise_error
+    }
+  end
+
+  it "raises ArgumentError when more than one argument is provided" do
+    lambda { File.umask(022, 022) }.should raise_error(ArgumentError)
+  end
+  
   platform_is :mswin do
     it "Returns the current umask value for this process. (basic)" do   
       File.umask.should == 0
