5c75721d5a78e25a77e9f068bf4c95e729604959
oleg dashevskii <be9@be9.ru>
2008-02-13 11:26:34 +0600

    Remove tests that have been superseded by precedence_spec.

--- spec/ruby/1.8/language/and_spec.rb
+++ spec/ruby/1.8/language/and_spec.rb
@@ -1,15 +1,1 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-
-describe "The && keyword" do
-  it "binds more tightly than =" do
-    i = 1 && 10
-    i.should == 10
-  end
-end
-
-describe "The and keyword" do
-  it "binds less tightly than =" do
-    i = 1 and 10
-    i.should == 1
-  end
-end
