38ab306c72f1ecb94f7bfa1c213df473cc6a06f3
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-02-17 14:37:52 -0500

    Small stupid heuristic to slash large reverse-sorted Array#sort times.

--- kernel/core/array.rb
+++ kernel/core/array.rb
@@ -1581,6 +1581,15 @@ class Array
       middle = left_end + ((right_end - left_end) / 2)
       low, mid, hi = @tuple.at(left_end), @tuple.at(middle), @tuple.at(right_end)
 
+      # "Heuristic" for reverse-sorted
+      if @total > 1000 and (low <=> mid) == 1 and (mid <=> hi) == 1
+        semi_left = @tuple.at(left_end + ((middle - left_end) / 2))
+        semi_right = @tuple.at(middle + ((right_end - middle) / 2))
+        reverse! if (low <=> semi_left) == 1 and (semi_left <=> middle) == 1 and
+                    (middle <=> semi_right) == 1 and (semi_right <=> hi) == 1
+        middle += (right_end - middle) / 2
+      end
+
       # These can be reordered which may help with sorting randomly
       # distributed elements at the cost of presorted. Be sure to
       # mark down the correct order though..
@@ -1691,6 +1700,15 @@ class Array
       # CAUTION: This may differ from how non-block version works
       middle = left_end + ((right_end - left_end) / 2)
       low, mid, hi = @tuple.at(left_end), @tuple.at(middle), @tuple.at(right_end)
+
+      # "Heuristic" for reverse-sorted
+      if @total > 1000 and (low <=> mid) == 1 and (mid <=> hi) == 1
+        semi_left = @tuple.at(left_end + ((middle - left_end) / 2))
+        semi_right = @tuple.at(middle + ((right_end - middle) / 2))
+        reverse! if (low <=> semi_left) == 1 and (semi_left <=> middle) == 1 and
+                    (middle <=> semi_right) == 1 and (semi_right <=> hi) == 1
+        middle += (right_end - middle) / 2
+      end
 
       # These can be reordered which may help with sorting randomly
       # distributed elements at the cost of presorted. Be sure to
