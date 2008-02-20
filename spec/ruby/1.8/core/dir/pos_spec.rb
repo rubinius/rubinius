22e01d1914db92d159ee15d3cf73c9d6e9d0a24b
Brian Ford <bford@engineyard.com>
2008-02-19 23:20:27 -0800

    Fix Dir#pos=/#seek specs. We shouldn't spec undefined platform behavior.

--- spec/ruby/1.8/core/dir/pos_spec.rb
+++ spec/ruby/1.8/core/dir/pos_spec.rb
@@ -12,39 +12,5 @@ describe "Dir#pos" do
 end
 
 describe "Dir#pos=" do
-  before(:each) do
-    @dir = Dir.open DirSpecs.mock_dir
-  end
-
-  after(:each) do
-    @dir.close
-  end
-
-  it "seeks to a certain previous position and returns an Integer position number" do
-    pos = @dir.pos
-    a   = @dir.read
-    b   = @dir.read
-    ret = @dir.pos = pos
-    c   = @dir.read
-
-    a.should_not == b
-    b.should_not == c
-    c.should     == a
-
-    pos.should  be_kind_of(Integer)
-    ret.should  be_kind_of(Integer)
-  end
-
-  it "does nothing if given a position that was not previously returned by #pos/#tell" do
-    a = @dir.read
-    b = @dir.read
-
-    @dir.rewind
-    @dir.pos = 5
-
-    c = @dir.read
-
-    a.should == c
-    a.should_not == b
-  end
+  it_behaves_like :dir_pos_set, :pos=
 end
--- spec/ruby/1.8/core/dir/seek_spec.rb
+++ spec/ruby/1.8/core/dir/seek_spec.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/common'
+require File.dirname(__FILE__) + '/shared/pos'
 
 describe "Dir#seek" do
   before(:each) do
@@ -10,32 +11,9 @@ describe "Dir#seek" do
     @dir.close
   end
 
-  it "moves the read position to a previously obtained position" do
-    pos = @dir.pos
-    a   = @dir.read
-    b   = @dir.read
-    ret = @dir.pos = pos
-    c   = @dir.read
-
-    a.should_not == b
-    b.should_not == c
-    c.should     == a
-  end
-
-  it "does nothing if given a position that was not previously returned by #pos/#tell" do
-    a = @dir.read
-    b = @dir.read
-
-    @dir.rewind
-    @dir.seek 5
-
-    c = @dir.read
-
-    a.should == c
-    a.should_not == b
-  end
-
   it "returns the Dir instance" do
     @dir.seek(@dir.pos).should == @dir
   end
+
+  it_behaves_like :dir_pos_set, :seek
 end
