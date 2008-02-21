f396bd718572d9402d0d7eeb8da02474914396a8
Brian Ford <bford@engineyard.com>
2008-02-20 17:59:29 -0800

    Use File.delete in YAML specs instead of rm.

--- spec/ruby/1.8/library/yaml/load_spec.rb
+++ spec/ruby/1.8/library/yaml/load_spec.rb
@@ -2,12 +2,15 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/common'
 
 describe "YAML.load" do
+  after :each do
+    File.delete $test_file if File.exist? $test_file
+  end
+  
   it "returns a document from current io stream when io provided" do
     File.open($test_file, 'w') do |io|
       YAML.dump( ['badger', 'elephant', 'tiger'], io )
     end
     File.open($test_file) { |yf| YAML.load( yf ) }.should == ['badger', 'elephant', 'tiger']
-    system "rm #{$test_file}"
   end
   
   it "accepts a string argument" do
