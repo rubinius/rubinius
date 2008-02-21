fd0ff43d2d384e221ff8de611843f3406d192657
Brian Ford <bford@engineyard.com>
2008-02-20 17:04:46 -0800

    Fixed YAML spec to pass MRI. Added fails tag for rbx.

--- spec/ruby/1.8/library/yaml/tagged_classes_spec.rb
+++ spec/ruby/1.8/library/yaml/tagged_classes_spec.rb
@@ -3,6 +3,6 @@ require File.dirname(__FILE__) + '/fixtures/common'
 
 describe "YAML.tagged_classes" do
   it "returns a complete dictionary of taguris paired with classes" do
-    YAML.tagged_classes["tag:yaml.org,2002:int"].should === Integer
+    YAML.tagged_classes["tag:yaml.org,2002:int"].should == Integer
   end  
 end
