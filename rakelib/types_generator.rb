a8556ff651292bdc88f1680c8017ced7fa9be795
Dirkjan Bussink <d.bussink@gmail.com>
2008-02-22 18:09:08 +0100

    Revert "Move _XOPEN_SOURCE flag so it is enabled on all platforms"
    This reverts commit c7f507f461a4f8bafba05f4723c31bfe881e5250.

--- rakelib/types_generator.rb
+++ rakelib/types_generator.rb
@@ -32,7 +32,7 @@ class TypesGenerator
       "void *" => :pointer
     }
     
-    typedefs = `echo "#include <sys/types.h>\n#include <sys/socket.h>\n#include <netdb.h>\n#include <sys/resource.h>" | cpp -D_XOPEN_SOURCE=500 -D_DARWIN_USE_64_BIT_INODE -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64`
+    typedefs = `echo "#include <sys/types.h>\n#include <sys/socket.h>\n#include <sys/resource.h>" | cpp -D_DARWIN_USE_64_BIT_INODE -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64`
     code = ""
     typedefs.each do |type|
       # We only care about single line typedef
