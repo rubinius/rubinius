e50ec6470dfc905198065a98b65b33a99da60e15
Wilson Bilkovich <wilson@supremetyrant.com>
2008-02-21 20:20:41 -0500

    Some compiler specs for 'defined?' handling

---
+++ spec/compiler/defined_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe Compiler do
+  it "compiles defined? instance_exec" do
+    gen [:defined, [:vcall, :instance_exec]] do |g|
+      g.push_literal :instance_exec
+      g.push :self
+      g.send :"respond_to?", 1
+    end
+  end
+  it "compiles defined? instance_exec()" do
+    gen [:defined, [:fcall, :instance_exec]] do |g|
+      g.push_literal :instance_exec
+      g.push :self
+      g.send :"respond_to?", 1
+    end
+  end
+end
