57b0e54d7659d183869a679abab967832031f9bf
oleg dashevskii <be9@be9.ru>
2008-02-26 23:30:57 +0600

    Converted libbstring, libcchash, libev & libltdl to CMake.

---
+++ shotgun/external_libs/libev/config.h.cmake
@@ -0,0 +1,16 @@
+#cmakedefine HAVE_SYS_INOTIFY_H 1
+#cmakedefine HAVE_SYS_EPOLL_H 1
+#cmakedefine HAVE_SYS_EVENT_H 1
+#cmakedefine HAVE_SYS_QUEUE_H 1
+#cmakedefine HAVE_PORT_H 1
+#cmakedefine HAVE_POLL_H 1
+#cmakedefine HAVE_SYS_SELECT_H 1
+#cmakedefine HAVE_INOTIFY_INIT 1
+#cmakedefine HAVE_EPOLL_CTL 1
+#cmakedefine HAVE_KQUEUE 1
+#cmakedefine HAVE_PORT_CREATE 1
+#cmakedefine HAVE_POLL 1
+#cmakedefine HAVE_SELECT 1
+#cmakedefine HAVE_CLOCK_GETTIME 1
+#cmakedefine HAVE_NANOSLEEP 1
+#cmakedefine HAVE_LIBM 1
