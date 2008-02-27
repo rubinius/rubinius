8c51eb0d42dc9c2e4c73bd027d574fddbdc01ceb
oleg dashevskii <be9@be9.ru>
2008-02-27 12:22:48 +0600

    Cmakified lightning & onig.

---
+++ shotgun/external_libs/lightning/config.h.cmake
@@ -0,0 +1,5 @@
+#cmakedefine LIGHTNING_CROSS 1
+#cmakedefine LIGHTNING_DISASSEMBLE 1
+#cmakedefine _ASM_SAFETY 1
+
+#define LIGHTNING_TARGET LIGHTNING_@target@
---
+++ shotgun/external_libs/onig/config.h.cmake
@@ -0,0 +1,16 @@
+#cmakedefine HAVE_ALLOCA_H 1
+#cmakedefine HAVE_PROTOTYPES 1
+#cmakedefine HAVE_STDARG_PROTOTYPES 1
+#cmakedefine HAVE_STDLIB_H 1
+#cmakedefine HAVE_STRING_H 1
+#cmakedefine HAVE_SYS_TIMES_H 1
+#cmakedefine HAVE_SYS_TIME_H 1
+#cmakedefine HAVE_SYS_TYPES_H 1
+#cmakedefine HAVE_UNISTD_H 1
+
+#define SIZEOF_INT @SIZEOF_INT@
+#define SIZEOF_LONG @SIZEOF_LONG@
+#define SIZEOF_SHORT @SIZEOF_SHORT@
+
+#cmakedefine USE_COMBINATION_EXPLOSION_CHECK 1
+#cmakedefine USE_CRNL_AS_LINE_TERMINATOR 1
