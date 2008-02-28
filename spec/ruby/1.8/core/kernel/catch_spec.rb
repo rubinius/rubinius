0c89dc90fdcb7933169e23462197d59f9627f510
Ryan Davis <rdavis@engineyard.com>
2008-02-28 14:31:05 -0800

    Added basic throw/catch specs. Fixed raised NameError to contain the name

--- spec/ruby/1.8/core/kernel/catch_spec.rb
+++ spec/ruby/1.8/core/kernel/catch_spec.rb
@@ -2,6 +2,24 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Kernel#catch" do
+
+  it "throws the given name and is caught by matching catch block" do
+    bad = false
+    catch :blah do
+      throw :blah
+      bad = true
+    end
+    bad.should == false
+  end
+
+  it "raises unless the thrown name is known" do
+    proc {
+      throw :blah
+    }.should raise_error(NameError, "uncaught throw `blah'") { |error|
+      error.name.should == :blah
+    }
+  end
+
   it "is a private method" do
     Kernel.private_instance_methods.should include("catch")
   end
