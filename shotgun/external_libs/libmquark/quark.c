0f0cce1ad7031bbca0d160a5f2cb0d79e51a2f43
oleg dashevskii <be9@be9.ru>
2008-03-02 13:25:10 +0600

    Fixes for libev & libmquark to compile on Mingw/Windows.
    Also replaced hash function in quark with a better one (djb2).

--- shotgun/external_libs/libmquark/quark.c
+++ shotgun/external_libs/libmquark/quark.c
@@ -1,4 +1,9 @@
-#include <pthread.h>
+#ifdef _WIN32
+#  include <windows.h>
+#else
+#  include <pthread.h>
+#endif
+
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -9,26 +14,59 @@
 
 static struct hashtable *quark_string_hash = NULL; /* const char * -> int */
 static ptr_array quark_string_index = NULL;
+
+#ifdef _WIN32
+static CRITICAL_SECTION quark_static_cs;
+static bool cs_initialized = false;
+
+static void mutex_lock()
+{
+  if (!cs_initialized) {
+    InitializeCriticalSection(&quark_static_cs);
+
+    cs_initialized = true;	  
+  }
+
+  EnterCriticalSection(&quark_static_cs);
+}
+
+static void mutex_unlock()
+{
+  LeaveCriticalSection(&quark_static_cs);
+}
+#else
 static pthread_mutex_t quark_static_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static void mutex_lock()
+{
+  pthread_mutex_lock(&quark_static_lock);
+}
+
+static void mutex_unlock()
+{
+  pthread_mutex_unlock(&quark_static_lock);
+}
+#endif
 
 enum {QUARK_NOT_FOUND = ~0L};
 
+/* djb2 hash function */
 static unsigned int string_hash(const void *val)
 {
-  unsigned int retval = 0;
+  unsigned int hash = 5381;
   const char *str = (const char *)val;
-  while (*str)
-  {
-    retval += 5* (*str);
-    str++;
-  }
-  return retval;
+  int c;
+
+  while (c = *str++)
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+  return hash;
 }
+
 static int string_eq(const void* a1, const void* a2)
 {
   return strcmp((const char*) a1, (const char*) a2) == 0;
 }
-
 
 /* assumes static lock is held */
 static inline bool quark_init_if_needed()
@@ -46,49 +84,49 @@ static inline bool quark_init_if_needed()
 quark quark_from_string(const char *string)
 {
   if (string == NULL) return 0;
-  pthread_mutex_lock(&quark_static_lock);
+  mutex_lock();
   if (!quark_init_if_needed())
   {
     uintptr_t value = (uintptr_t)hashtable_search(quark_string_hash, string);
     if (value != 0)
     {
-      pthread_mutex_unlock(&quark_static_lock);
+      mutex_unlock();
       return value;
     }
   }
   string = strdup(string);
   if (!ptr_array_append(quark_string_index, string))
   { 
-    pthread_mutex_unlock(&quark_static_lock);
+    mutex_unlock();
     return QUARK_NOT_FOUND; 
   }
   size_t index = ptr_array_length(quark_string_index) - 1;
   hashtable_insert(quark_string_hash, (char*) string, (void*) index);
-  pthread_mutex_unlock(&quark_static_lock);
+  mutex_unlock();
   return index;
 }
 
 quark quark_from_static_string(const char *string)
 {
   if (string == NULL) return 0;
-  pthread_mutex_lock(&quark_static_lock);
+  mutex_lock();
   if (!quark_init_if_needed())
   {
     uintptr_t value = (uintptr_t)hashtable_search(quark_string_hash, string);
     if (value != 0)
     {
-      pthread_mutex_unlock(&quark_static_lock);
+      mutex_unlock();
       return value;
     }
   }
   if (!ptr_array_append(quark_string_index, string))
   { 
-    pthread_mutex_unlock(&quark_static_lock);
+    mutex_unlock();
     return QUARK_NOT_FOUND;
   }
   size_t index = ptr_array_length(quark_string_index) - 1;
   hashtable_insert(quark_string_hash, (char *)string, (void*) index);
-  pthread_mutex_unlock(&quark_static_lock);
+  mutex_unlock();
   return index;
 }
 
@@ -96,13 +134,13 @@ const char * quark_to_string(quark q)
 {
   const char *retval;
   if (q == 0) return NULL;
-  pthread_mutex_lock(&quark_static_lock);
+  mutex_lock();
   if (quark_init_if_needed() || q >= ptr_array_length(quark_string_index))
   {
-    pthread_mutex_unlock(&quark_static_lock);
+    mutex_unlock();
     return NULL;
   }
   retval = ptr_array_get_index(quark_string_index, q);
-  pthread_mutex_unlock(&quark_static_lock);
+  mutex_unlock();
   return retval;
 }
