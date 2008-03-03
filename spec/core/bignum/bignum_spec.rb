605bdc53e9dd4fb95dae6557d9ee6f9e2b8ceb80
David Whittington <djwhitt@gmail.com>
2008-03-03 08:44:33 +0000

    Modified Bignum threshold specs to take into account platform wordsize

--- spec/core/bignum/bignum_spec.rb
+++ spec/core/bignum/bignum_spec.rb
@@ -1,19 +1,43 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "Bignum" do
-  it "has positive threshold value 2 ** 29" do
-    max = 2 ** 29
-    max.class.should == Bignum
-    sub = max - 1
-    sub.should == 536870911
-    sub.class.should == Fixnum
+  platform_is :wordsize => 32 do
+    it "has positive threshold value 2 ** 29" do
+      max = 2 ** 29
+      max.class.should == Bignum
+      sub = max - 1
+      sub.should == 536870911
+      sub.class.should == Fixnum
+    end
+  end
+
+  platform_is :wordsize => 64 do
+    it "has positive threshold value 2 ** 61" do
+      max = 2 ** 61
+      max.class.should == Bignum
+      sub = max - 1
+      sub.should == 2305843009213693951
+      sub.class.should == Fixnum
+    end
   end
   
-  it "has negative threshold value -(2 ** 29)" do
-    min = -2 ** 29
-    min.class.should == Bignum
-    sub = min + 1
-    sub.should == -536870911
-    sub.class.should == Fixnum
+  platform_is :wordsize => 32 do
+    it "has negative threshold value -(2 ** 29)" do
+      min = -2 ** 29
+      min.class.should == Bignum
+      sub = min + 1
+      sub.should == -536870911
+      sub.class.should == Fixnum
+    end
+  end
+
+  platform_is :wordsize => 64 do
+    it "has negative threshold value -(2 ** 61)" do
+      min = -2 ** 61
+      min.class.should == Bignum
+      sub = min + 1
+      sub.should == -2305843009213693951
+      sub.class.should == Fixnum
+    end
   end
 end
