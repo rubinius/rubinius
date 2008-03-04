16253b78e10d688a04b6033387d6ff1f698b94cf
Marcus Crafter <crafterm@redartisan.com>
2008-03-04 19:06:21 +1100

    Added entry-counts.d dtrace script for probe analysis.
    Updated .gitignore to allow .d files outside of shotgun.

---
+++ tools/dtrace/entry-counts.d
@@ -0,0 +1,19 @@
+/*
+ * entry/exit counts
+ *
+ * This script manages a counter for every method invocation. Upon function-entry the probe increments
+ * the counter, upon function-return the counter decrements the counter. At the end of the run
+ * dtrace should print all methods run, with a count of 0 for all of them - indicating that the probes
+ * have fired correctly.
+ *
+ * Example invocation:
+ *
+ * $> sudo dtrace -s entry-counts.d -c 'rbx ~/hello.rb'
+ */
+
+#pragma D option dynvarsize=250m
+#pragma D option bufsize=64m
+
+rubinius$target:::function-entry { @[copyinstr(arg0), copyinstr(arg1)] = sum(1); } 
+rubinius$target:::function-return { @[copyinstr(arg0), copyinstr(arg1)] = sum(-1); } 
+
