36f91c5da132f309fbf6d047fd74ebd8aa7cbf22
Matt Palmer <mpalmer@hezmatt.org>
2008-03-08 17:26:49 +1100

    Rearrange the UDPSocket test cases for better separation
    * open_specs now only contains a spec that calls UDPSocket.open;
    * send_specs now has separate tests for ad-hoc and connection-oriented
      sends.

--- spec/ruby/1.8/library/socket/udpsocket/open_spec.rb
+++ spec/ruby/1.8/library/socket/udpsocket/open_spec.rb
@@ -7,38 +7,7 @@ describe "UDPSocket.open" do
     @server.close if @server && !@server.closed?
   end
 
-  it "returns a socket that can be written to and read from" do
-    @ready = false
-    server_thread = Thread.new do
-      @server = UDPSocket.open
-      begin
-        @server.bind(nil,SocketSpecs.port)
-      rescue Object => e
-        puts "unable to bind."
-        raise e
-      end
-      @ready = true
-      msg1 = @server.recvfrom(64)
-      msg1[0].should == "ad hoc"
-      msg1[1][0].should == "AF_INET"
-      msg1[1][1].should be_kind_of(Fixnum)
-      msg1[1][3].should == "127.0.0.1"
-
-      msg2 = @server.recvfrom(64)
-      msg2[0].should == "connection-based"
-      msg2[1][0].should == "AF_INET"
-      msg2[1][1].should be_kind_of(Fixnum)
-      msg2[1][3].should == "127.0.0.1"
-    end
-
-    Thread.pass while server_thread.status and !@ready
-
-    UDPSocket.open.send("ad hoc", 0, 'localhost',SocketSpecs.port)
-
+  it "allows calls to open without arguments" do
     @socket = UDPSocket.open
-    @socket.connect('localhost',SocketSpecs.port)
-    @socket.send("connection-based", 0)
-
-    server_thread.join
   end
 end
