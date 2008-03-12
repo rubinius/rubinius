2ff775cbcf2ade4315fbdbb37fa78ee84a1e645a
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2008-03-12 11:59:26 +0800

    Add String#to_a specs

---
+++ spec/ruby/1.8/core/string/to_a_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "String#to_a" do
+  it "returns an empty array for empty strings" do
+    "".to_a.should == []
+  end
+  
+  it "returns an array containing the string for non-empty strings" do
+    "hello".to_a.should == ["hello"]
+  end
+end
