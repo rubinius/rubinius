90eb74998e132373e6b96e3c66bfa909854e3ef0
Ryan Davis <rdavis@engineyard.com>
2008-03-18 17:41:50 -0700

    Added spec for 'a [ 42 ]'

--- spec/parser/fixtures/sexp_expectations.rb
+++ spec/parser/fixtures/sexp_expectations.rb
@@ -1146,6 +1146,12 @@ end",
                       [:lit, :c]],
   },
 
+  "fcall_index_space" => {
+    "Ruby"        => "a [ 42 ]",
+    "ParseTree"   => [:fcall, :a, [:array, [:array, [:lit, 42]]]],
+    "RubyToRuby"  => "a[42]",
+  },
+
   "fcall_keyword"  => {
     "Ruby"        => "42 if block_given?",
     "ParseTree"   => [:if, [:fcall, :block_given?], [:lit, 42], nil],
