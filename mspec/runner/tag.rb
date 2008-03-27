1da73a01541f07bd18d3d64ac88950990ffadecb
Brian Ford <bford@engineyard.com>
2008-03-27 14:22:55 -0700

    Allow ':' in MSpec tag comments: tag(bug:555):Some#method.

--- mspec/runner/tag.rb
+++ mspec/runner/tag.rb
@@ -6,7 +6,7 @@ class SpecTag
   end
   
   def parse(string)
-    m = /^([^()#:]+)(\(([^):]+)?\))?:(.*)/.match string
+    m = /^([^()#:]+)(\(([^)]+)?\))?:(.*)/.match string
     @tag, @comment, @description = m.values_at(1, 3, 4) if m
   end
   
