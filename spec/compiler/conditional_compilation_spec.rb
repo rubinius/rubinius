4f78ee2b0bebb9170a483927af9c7520ca67f912
Eero Saynatkari <rubinius@projects.kittensoft.org>
2008-04-01 09:58:53 -0400

    Specs to verify conditional compilation in the compiler.

---
+++ spec/compiler/conditional_compilation_spec.rb
@@ -0,0 +1,84 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+require 'compiler/graph'
+
+# This is just:
+#   Rubinius.compile_if($CompilerSpecsConditional) { $stderr.puts "Conditional" }
+
+  
+describe Compiler, "conditional compilation with Rubinius.compile_if given an argument evaluating to false at compile time" do
+  it "completely omits the call block and its contained code from the bytecode output as if the block did not exist" do
+    full_sexp =  [ :newline, 1, "../some/file", 
+                   [:iter, 
+                     [:call, [:const, :Rubinius], :compile_if, 
+                       [:array, [:gvar, :$CompilerSpecsConditional]]
+                     ], nil, 
+                     [:newline, 1, "../some/file", 
+                       [:call, [:gvar, :$stderr], :puts, 
+                         [:array, [:str, "Conditional"]]
+                       ]
+                     ]
+                   ]
+                 ]
+
+    omitted_sexp = [:nil]
+
+    $CompilerSpecsConditional = false
+    
+    c = Compiler.new Compiler::Generator
+    c.activate :conditional_compilation
+
+    a = c.into_script full_sexp
+    b = c.into_script omitted_sexp
+
+    a.class.should == Compiler::Node::Script
+    b.class.should == Compiler::Node::Script
+    a.body.class.should == Compiler::Node::Nil
+    b.body.class.should == Compiler::Node::Nil
+  end
+end
+
+describe Compiler, "conditional compilation with Rubinius.compile_if given an argument evaluating to true at compile time" do
+  it "omits the Rubinius.compile_if block artifacts from the produced bytecode" do
+    full_sexp =  [ :newline, 1, "../some/file", 
+                   [:iter, 
+                     [:call, [:const, :Rubinius], :compile_if, 
+                       [:array, [:gvar, :$CompilerSpecsConditional]]
+                     ], nil, 
+                     [:newline, 1, "../some/file", 
+                       [:call, [:gvar, :$stderr], :puts, 
+                         [:array, [:str, "Conditional"]]
+                       ]
+                     ]
+                   ]
+                 ]
+
+    included_sexp =  [:newline, 1, "../some/file", 
+                       [:call, [:gvar, :$stderr], :puts, 
+                         [:array, [:str, "Conditional"]]
+                       ]
+                     ]
+
+    $CompilerSpecsConditional = true
+    
+    c = Compiler.new Compiler::Generator
+    c.activate :conditional_compilation
+
+    a = c.into_script full_sexp
+    b = c.into_script included_sexp
+
+    a.class.should == b.class
+    a.body.class.should == b.body.class
+    a.body.file.should == b.body.file
+    a.body.child.method.should == b.body.child.method
+    a.body.child.object.name.should == b.body.child.object.name
+  end
+
+#  it "produces the code contained within its block normally in the bytecode output" do
+#    $CompilerSpecsConditional = true
+#    
+#    c = Compiler.new Compiler::Generator
+#    c.into_script(@full_sexp.dup).should == c.convert_sexp(@included_sexp.dup)
+#  end
+end
+
