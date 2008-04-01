d6dfbd3b0bab57453e67991c3320744b08346979
Charles Comstock <dgtized@gmail.com>
2008-04-01 15:04:18 -0500

    DRb specs now attempt to check if server is up/down prior to each call to start_server
    note that there is something wrong with the way stop_server works in rubinius as it appears that the TCPServer is still binding the port.  Spec is tagged to deal with this but technically it's probably a bug in TCPServer

--- spec/ruby/1.8/library/drb/start_service_spec.rb
+++ spec/ruby/1.8/library/drb/start_service_spec.rb
@@ -1,37 +1,28 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/fixtures/test_server'
 require 'drb'
 
 describe "DRb.start_service" do
-  after :each do
-    DRb.stop_service
-  end
-
-  it "should run a basic remote call" do
-    class TestServer
-      def add(*args)
-        args.inject {|n,v| n+v}
-      end
-    end
-    
-    DRb.start_service('druby://localhost:9001', TestServer.new)
-    
+  it "should run a basic remote call" do    
+    lambda { DRb.current_server }.should raise_error(DRb::DRbServerNotFound)
+    server = DRb.start_service('druby://localhost:9001', TestServer.new)
+    DRb.current_server.should == server
     obj = DRbObject.new(nil, 'druby://localhost:9001')
     obj.add(1,2,3).should == 6
+    DRb.stop_service
+    lambda { DRb.current_server }.should raise_error(DRb::DRbServerNotFound)
   end
 
   it "should run a basic remote call passing a block" do
-    class TestServer
-      def add1(x)        
-        return (yield x)+1
-      end
-    end
-    
-    DRb.start_service('druby://localhost:9001', TestServer.new)
-    
+    lambda { DRb.current_server }.should raise_error(DRb::DRbServerNotFound)
+    server = DRb.start_service('druby://localhost:9001', TestServer.new)
+    DRb.current_server.should == server
     obj = DRbObject.new(nil, 'druby://localhost:9001')
-    obj.add1(2) do |i|
+    obj.add_yield(2) do |i|
       i.should == 2
       i+1
     end.should == 4
+    DRb.stop_service
+    lambda { DRb.current_server }.should raise_error(DRb::DRbServerNotFound)
   end
 end
