d250939060a4a91a6fee59bd4bfa4e86eb271373
Paul Thornthwaite <tokengeek@gmail.com>
2008-04-03 14:36:42 +0100

    Specs for Set#superset and Set#proper_superset added
    Signed-off-by: Eero Saynatkari <rubinius@projects.kittensoft.org>

--- spec/ruby/1.8/library/set/proper_superset_spec.rb
+++ spec/ruby/1.8/library/set/proper_superset_spec.rb
@@ -1,2 +1,19 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'set'
+
+describe "Set#superset" do
+
+  before :each do
+    @set = Set[1, 2, 3, 4]
+    @superset = Set[1, 2, 3, 4, 5]
+  end
+
+  it "returns true if a proper superset" do
+    @superset.proper_superset?(@set).should == true
+  end
+  
+  it "returns false when compared to itself" do
+    @set.proper_superset?(@set).should == false
+  end
+  
+end
--- spec/ruby/1.8/library/set/superset_spec.rb
+++ spec/ruby/1.8/library/set/superset_spec.rb
@@ -1,2 +1,19 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'set'
+
+describe "Set#superset" do
+
+  before :each do
+    @set = Set[1, 2, 3, 4]
+    @superset = Set[1, 2, 3, 4, 5]
+  end
+
+  it "returns true if a superset" do
+    @superset.superset?(@set).should == true
+  end
+  
+  it "returns true when compared to itself" do
+    @set.superset?(@set).should == true
+  end
+  
+end
