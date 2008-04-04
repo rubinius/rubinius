3a547c2b82434c64b72967ebd917fc063ff1317d
Brian Ford <bford@engineyard.com>
2008-04-03 23:16:18 -0700

    Fixed GetoptLong specs to not depend on value of ARGV.

--- spec/ruby/1.8/library/getoptlong/error_message_spec.rb
+++ spec/ruby/1.8/library/getoptlong/error_message_spec.rb
@@ -2,22 +2,31 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require 'getoptlong'
 
 describe "GetoptLong#error_message" do
+  before :each do
+    @stderr = $stderr
+    @argv = ARGV
+  end
+
+  after :each do
+    $stderr = @stderr
+    ARGV = @argv
+  end
+
   it "returns nil if no error occurred" do
     opts = GetoptLong.new
     opts.error_message.should == nil
   end
-  
+
   it "returns the error message of the last error that occurred" do
     begin
-      s = $stderr
       $stderr = CaptureOutput.new
+      ARGV = []
+
       opts = GetoptLong.new
       opts.get
       opts.ordering = GetoptLong::PERMUTE
     rescue ArgumentError
       opts.error_message.should == "argument error"
-    ensure
-      $stderr = s
     end
   end
 end
