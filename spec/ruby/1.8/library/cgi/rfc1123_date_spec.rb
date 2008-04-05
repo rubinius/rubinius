7b9f5a213c971636b663e992fcb8578888d27f52
makoto kuwata <kwa@kuwata-lab.com>
2008-04-05 13:03:59 +0900

    Add spec file for CGI::rfc1123_date().
    Signed-off-by: Eero Saynatkari <rubinius@projects.kittensoft.org>

--- spec/ruby/1.8/library/cgi/rfc1123_date_spec.rb
+++ spec/ruby/1.8/library/cgi/rfc1123_date_spec.rb
@@ -1,7 +1,19 @@
+require 'cgi'
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe 'CGI#rfc1123_date' do
-  it 'should spec something' do
-    raise 'not yet'
+
+describe "CGI::rfc1123_date" do
+
+  it "convert Time object into RFC1123 format such as 'Sat, 01 Dec 2007 15:56:42 GMT'" do
+    sec = 1196524602
+    input = Time.at(sec)
+    expected = 'Sat, 01 Dec 2007 15:56:42 GMT'
+    CGI.rfc1123_date(input).should == expected
   end
+
+  it "raise error when argument is not Time object" do
+    str = 'Sat, 01 Dec 2007 15:56:42 GMT'
+    lambda { CGI.rfc1123_date(str) }.should raise_error(Exception)  # NoMethodError
+  end
+
 end
