fc321869d73f58dcfbb55ba374646c1568528004
makoto kuwata <kwa@kuwata-lab.com>
2008-04-05 13:01:33 +0900

    Add spec files for CGI::escape() and CGI::unescape().
    Signed-off-by: Eero Saynatkari <rubinius@projects.kittensoft.org>

--- spec/ruby/1.8/library/cgi/unescape_spec.rb
+++ spec/ruby/1.8/library/cgi/unescape_spec.rb
@@ -1,7 +1,25 @@
+require 'cgi'
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe 'CGI#unescape' do
-  it 'should spec something' do
-    raise 'not yet'
+
+describe "CGI::unescape" do
+
+  it "decode '%' format string" do
+    input = '%a4%Aa'
+    expected = "\244\252"
+    CGI::unescape(input).should == expected
   end
+
+  it "leave invalid format string as is" do
+    input = "%XX%5%%%"
+    expected = input
+    CGI::unescape(input).should == expected
+  end
+
+  it "raise error when non-string is passed" do
+    lambda { CGI::unescape(nil)  }.should raise_error(Exception)  # NoMethodError
+    lambda { CGI::unescape(123)  }.should raise_error(Exception)  # NoMethodError
+    lambda { CGI::unescape(true) }.should raise_error(Exception)  # NoMethodError
+  end
+
 end
