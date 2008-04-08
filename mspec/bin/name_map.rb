a37b0dde518f645a2a1172c3105747a519a2b7e7
Brian Ford <bford@engineyard.com>
2008-04-08 15:33:46 -0700

    Specs for and rework of MSpec NameMap.

--- mspec/bin/name_map.rb
+++ mspec/bin/name_map.rb
@@ -55,36 +55,47 @@ class NameMap
     'initialize' => 'new'
   }
 
-  EXCLUDED_CLASSES = %w[DTracer NameMap OptionParser SystemExit]
+  EXCLUDED = %w[
+    MkSpec
+    DTracer
+    NameMap
+    OptionParser
+    SystemExit
+  ]
 
-  def self.const_lookup(c)
+  def const_lookup(c)
     c.split('::').inject(Object) { |k,n| k.const_get n }
   end
 
-  def self.get_class_or_module(c)
+  def exception?(c)
+    c == Errno or c.ancestors.include? Exception
+  end
+
+  def get_class_or_module(c)
     const = const_lookup(c)
 
-    return const if Module === const and
-                    not EXCLUDED_CLASSES.include?(const.name)
+    if Module === const and not EXCLUDED.include? const.name
+      return const 
+    end
   rescue NameError
   end
 
-  def self.get_dir_name(c, base = nil)
-    base ||= 'spec/ruby/1.8/core/'
-
+  def get_dir_name(c, base)
     name = c.name.split('::').last
 
-    if (name.include?('Error') or name == 'Errno') and
-       c.ancestors.include?(Exception) then
-      File.join base, 'exception'
-    elsif c == Class
-      File.join base, name.downcase
+    case
+    when exception?(c)
+      dir = 'exception'
+    when c == Class
+      dir = name.downcase
     else
-      File.join base, name.gsub(/Class/, '').downcase
+      dir = name.gsub(/Class/, '').downcase
     end
+
+    File.join base, dir
   end
 
-  def self.get_spec_name(m, c)
+  def get_spec_name(m, c)
     if MAP.key?(m)
       name = MAP[m].is_a?(Hash) ? MAP[m][c] || MAP[m][:default] : MAP[m]
     else
--- mspec/spec/bin/name_map_spec.rb
+++ mspec/spec/bin/name_map_spec.rb
@@ -1,4 +1,129 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/../../bin/name_map'
 
-# TODO: specs
+module NameMapSpecs
+  class A
+    class B
+    end
+  end
+
+  class Error
+  end
+end
+
+describe NameMap, "#const_lookup" do
+  before :each do
+    @map = NameMap.new
+  end
+
+  it "returns the constant specified by 'A::B'" do
+    @map.const_lookup("NameMapSpecs::A::B").should == NameMapSpecs::A::B
+  end
+
+  it "returns a regular constant specified without scoping" do
+    @map.const_lookup("NameMapSpecs").should == NameMapSpecs
+  end
+end
+
+describe NameMap, "#exception?" do
+  before :each do
+    @map = NameMap.new
+  end
+
+  it "returns true if the constant is Errno" do
+    @map.exception?(Errno).should == true
+  end
+
+  it "returns true if the constant is a kind of Exception" do
+    @map.exception?(Errno::EBADF).should == true
+    @map.exception?(LoadError).should == true
+    @map.exception?(SystemExit).should == true
+  end
+
+  it "returns false if the constant is not a kind of Exception" do
+    @map.exception?(NameMapSpecs::Error).should == false
+    @map.exception?(NameMapSpecs).should == false
+  end
+end
+
+describe NameMap, "#get_class_or_module" do
+  before :each do
+    @map = NameMap.new
+  end
+
+  it "returns the constant specified by the string" do
+    @map.get_class_or_module("NameMapSpecs").should == NameMapSpecs
+  end
+
+  it "returns the constant specified by the 'A::B' string" do
+    @map.get_class_or_module("NameMapSpecs::A").should == NameMapSpecs::A
+  end
+
+  it "returns nil if the constant is not a class or module" do
+    @map.get_class_or_module("Float::MAX").should == nil
+  end
+
+  it "returns nil if the constant is in the set of excluded constants" do
+    @map.get_class_or_module("MkSpec").should == nil
+    @map.get_class_or_module("DTracer").should == nil
+    @map.get_class_or_module("NameMap").should == nil
+    @map.get_class_or_module("OptionParser").should == nil
+    @map.get_class_or_module("SystemExit").should == nil
+  end
+
+  it "returns nil if the constant does not exist" do
+    @map.get_class_or_module("Heaven").should == nil
+    @map.get_class_or_module("Hell").should == nil
+    @map.get_class_or_module("Bush::Brain").should == nil
+  end
+end
+
+describe NameMap, "#get_dir_name" do
+  before :each do
+    @map = NameMap.new
+  end
+
+  it "returns a directory name from the base name and constant" do
+    @map.get_dir_name(NameMapSpecs, 'spec/core').should == 'spec/core/namemapspecs'
+  end
+
+  it "returns a directory name from the last component in the constants name" do
+    @map.get_dir_name(NameMapSpecs::A, 'spec').should == 'spec/a'
+  end
+
+  it "returns a directory name without 'class' for constants like TrueClass" do
+    @map.get_dir_name(TrueClass, 'spec').should == 'spec/true'
+    @map.get_dir_name(FalseClass, 'spec').should == 'spec/false'
+  end
+
+  it "returns 'exception' for the directory name of any Exception subclass" do
+    @map.get_dir_name(SystemExit, 'spec').should == 'spec/exception'
+    @map.get_dir_name(Errno::EBADF, 'spec').should == 'spec/exception'
+  end
+
+  it "returns 'class' for Class" do
+    @map.get_dir_name(Class, 'spec').should == 'spec/class'
+  end
+end
+
+# These specs do not cover all the mappings, but only describe how the
+# name is derived when the hash item maps to a single value, a hash with
+# a specific item, or a hash with a :default item.
+describe NameMap, "#get_spec_name" do
+  before :each do
+    @map = NameMap.new
+  end
+
+  it "returns the name of the spec file based on the constant and method" do
+    @map.get_spec_name("[]=", "Array").should == "element_set_spec.rb"
+  end
+
+  it "returns the name of the spec file based on the special entry for the method" do
+    @map.get_spec_name("~", "Regexp").should == "match_spec.rb"
+    @map.get_spec_name("~", "Fixnum").should == "complement_spec.rb"
+  end
+
+  it "returns the name of the spec file based on the default entry for the method" do
+    @map.get_spec_name("<<", "NameMapSpecs").should == "append_spec.rb"
+  end
+end
