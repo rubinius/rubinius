c281add79d621f6327740109895c624dd25a2e1b
Federico Builes <federico.builes@gmail.com>
2008-04-09 09:13:29 -0500

    Cleaning up UPSocket#send specs
    * Got rid of the weird exception catching
    * DRY things up a bit with before :each

--- spec/ruby/1.8/library/socket/udpsocket/send_spec.rb
+++ spec/ruby/1.8/library/socket/udpsocket/send_spec.rb
@@ -2,62 +2,40 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/classes'
 
 describe "UDPSocket.send" do
-  it "sends data in ad hoc mode" do
+  before :each do
     @ready = false
-    server_thread = Thread.new do
+    @server_thread = Thread.new do
       @server = UDPSocket.open
-      begin
-        @server.bind(nil,SocketSpecs.port)
-      rescue Object => e
-        puts "unable to bind."
-        raise e
-      end
+      @server.bind(nil, SocketSpecs.port)
       @ready = true
-      msg1 = @server.recvfrom(64)
-      msg1[0].should == "ad hoc"
-      msg1[1][0].should == "AF_INET"
-      msg1[1][1].should be_kind_of(Fixnum)
-      msg1[1][3].should == "127.0.0.1"
-
+      @msg = @server.recvfrom(64)
       @server.close
     end
-
-    Thread.pass while server_thread.status and !@ready
-
+    Thread.pass while @server_thread.status and !@ready
+  end
+  
+  it "sends data in ad hoc mode" do
     @socket = UDPSocket.open
     @socket.send("ad hoc", 0, 'localhost',SocketSpecs.port)
     @socket.close
-
-    server_thread.join
+    @server_thread.join
+      
+    @msg[0].should == "ad hoc"
+    @msg[1][0].should == "AF_INET"
+    @msg[1][1].should be_kind_of(Fixnum)
+    @msg[1][3].should == "127.0.0.1"
   end
 
   it "sends data in connection mode" do
-    @ready = false
-    server_thread = Thread.new do
-      @server = UDPSocket.open
-      begin
-        @server.bind(nil,SocketSpecs.port)
-      rescue Object => e
-        puts "unable to bind."
-        raise e
-      end
-      @ready = true
-      msg1 = @server.recvfrom(64)
-      msg1[0].should == "connection-based"
-      msg1[1][0].should == "AF_INET"
-      msg1[1][1].should be_kind_of(Fixnum)
-      msg1[1][3].should == "127.0.0.1"
-
-      @server.close
-    end
-
-    Thread.pass while server_thread.status and !@ready
-
     @socket = UDPSocket.open
     @socket.connect('localhost',SocketSpecs.port)
     @socket.send("connection-based", 0)
     @socket.close
-
-    server_thread.join
+    @server_thread.join
+    
+    @msg[0].should == "connection-based"
+    @msg[1][0].should == "AF_INET"
+    @msg[1][1].should be_kind_of(Fixnum)
+    @msg[1][3].should == "127.0.0.1"
   end
 end
