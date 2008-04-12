0596b1aca45a85de5f3d727632585da924fd3eb0
Matthias Reitinger <m.reitinger@mytum.de>
2008-04-12 10:11:13 +0200

    New specs for BigDecimal.new.
    Signed-off-by: Marius Nuennerich <marius@nuenneri.ch>

--- spec/ruby/1.8/library/bigdecimal/new_spec.rb
+++ spec/ruby/1.8/library/bigdecimal/new_spec.rb
@@ -17,4 +17,66 @@ describe "BigDecimal.new" do
     BigDecimal.new(pi_string).precs[1].should >= pi_string.size
   end
 
+  it "ignores leading whitespace" do
+    BigDecimal.new("  \t\n \r1234").should == BigDecimal.new("1234")
+  end
+
+  it "ignores trailing garbage" do
+    BigDecimal.new("123E45ruby").should == BigDecimal.new("123E45")
+    BigDecimal.new("123x45").should == BigDecimal.new("123")
+    BigDecimal.new("123.4%E5").should == BigDecimal.new("123.4")
+  end
+
+  it "treats invalid strings as 0.0" do
+    BigDecimal.new("ruby").should == BigDecimal.new("0.0")
+  end
+
+  it "allows omitting the integer part" do
+    BigDecimal.new(".123").should == BigDecimal.new("0.123")
+    BigDecimal.new("-.123").should == BigDecimal.new("-0.123")
+  end
+
+  it "allows for underscores in all parts" do
+    reference = BigDecimal.new("12345.67E89")
+
+    BigDecimal.new("12_345.67E89").should == reference
+    BigDecimal.new("1_2_3_4_5_._6____7_E89").should == reference
+    BigDecimal.new("12345_.67E_8__9_").should == reference
+  end
+
+  it "accepts NaN and [+-]Infinity" do
+    BigDecimal.new("NaN").nan?.should == true
+
+    pos_inf = BigDecimal.new("Infinity")
+    pos_inf.finite?.should == false
+    pos_inf.should > 0
+    pos_inf.should == BigDecimal.new("+Infinity")
+
+    neg_inf = BigDecimal.new("-Infinity")
+    neg_inf.finite?.should == false
+    neg_inf.should < 0
+  end
+
+  it "allows for [eEdD] as exponent separator" do
+    reference = BigDecimal.new("12345.67E89")
+
+    BigDecimal.new("12345.67e89").should == reference
+    BigDecimal.new("12345.67E89").should == reference
+    BigDecimal.new("12345.67d89").should == reference
+    BigDecimal.new("12345.67D89").should == reference
+  end
+
+  it "allows for varying signs" do
+    reference = BigDecimal.new("123.456E1")
+
+    BigDecimal.new("+123.456E1").should == reference
+    BigDecimal.new("-123.456E1").should == -reference
+    BigDecimal.new("123.456E+1").should == reference
+    BigDecimal.new("12345.6E-1").should == reference
+    BigDecimal.new("+123.456E+1").should == reference
+    BigDecimal.new("+12345.6E-1").should == reference
+    BigDecimal.new("-123.456E+1").should == -reference
+    BigDecimal.new("-12345.6E-1").should == -reference
+  end
+
 end
