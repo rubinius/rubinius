eacb4f8a4d0ba606458a5756ddd6f2ce723a3dfa
Dirkjan Bussink <d.bussink@gmail.com>
2008-04-12 17:45:55 +0200

    Specced different behavior between File.stat and File.lstat

--- spec/ruby/1.8/core/file/lstat_spec.rb
+++ spec/ruby/1.8/core/file/lstat_spec.rb
@@ -5,7 +5,29 @@ describe "File.lstat" do
   it_behaves_like :file_stat, :lstat
 end
 
-describe "File#lstat" do
+describe "File.lstat" do
+  
+  before :each do
+    @file = '/tmp/i_exist'
+    @link = '/tmp/i_am_a_symlink'
+    File.open(@file,'w'){|f| f.write 'rubinius'}
+    File.symlink(@file, @link)
+  end
+
+  after :each do
+    File.delete(@link) if File.exist?(@link)
+    File.delete(@file) if File.exist?(@file)
+  end
+  
+  it "returns a File::Stat object with symlink properties for a symlink" do
+    st = File.lstat(@link)
+
+    st.symlink?.should == true
+    st.file?.should == false
+  end
+end
+
+describe "File#lstat"
   it "needs to be reviewed for spec completeness" do
   end
 end
