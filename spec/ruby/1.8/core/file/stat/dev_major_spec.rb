c411b15b9f94fec21b02a9208cbae4b42452431d
Dirkjan Bussink <d.bussink@gmail.com>
2008-04-15 20:19:24 +0200

    Properly rename File::Stat#dev_major and File::Stat#dev_minor specs

--- spec/ruby/1.8/core/file/stat/dev_major_spec.rb
+++ spec/ruby/1.8/core/file/stat/dev_major_spec.rb
@@ -1,12 +1,7 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 
-describe "File::Stat.dev_major" do
+describe "File::Stat#dev_major" do
   it "returns the major part of File::Stat#dev" do
     File.stat('/dev/null').dev_major.should be_kind_of(Integer)
-  end
-end
-
-describe "File::Stat#dev_major" do
-  it "needs to be reviewed for spec completeness" do
   end
 end
--- spec/ruby/1.8/core/file/stat/dev_minor_spec.rb
+++ spec/ruby/1.8/core/file/stat/dev_minor_spec.rb
@@ -1,12 +1,7 @@
 require File.dirname(__FILE__) + '/../../../spec_helper'
 
-describe "File::Stat.dev_minor" do
+describe "File::Stat#dev_minor" do
   it "returns the minor part of File::Stat#dev" do
     File.stat('/dev/null').dev_minor.should be_kind_of(Integer)
-  end
-end
-
-describe "File::Stat#dev_minor" do
-  it "needs to be reviewed for spec completeness" do
   end
 end
