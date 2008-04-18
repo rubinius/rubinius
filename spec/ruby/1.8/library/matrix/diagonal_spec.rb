b6bc5b224ade56ab96f3585b6b1c25e6dd5e1ad5
Marnen Laibow-Koser <marnen@marnen.org>
2008-04-18 01:19:27 -0400

    Write specs for Matrix.diagonal.
    Signed-off-by: Federico Builes <federico.builes@gmail.com>

--- spec/ruby/1.8/library/matrix/diagonal_spec.rb
+++ spec/ruby/1.8/library/matrix/diagonal_spec.rb
@@ -4,4 +4,28 @@ require 'matrix'
 describe "Matrix.diagonal" do
   it "needs to be reviewed for spec completeness" do
   end
+  
+  before(:each) do
+    @m = Matrix.diagonal(10, 11, 12, 13, 14)
+  end
+  
+  it "returns an object of type Matrix" do
+    @m.class.should == Matrix
+  end
+  
+  it "puts its arguments on the diagonal of a matrix" do
+    (0..4).each do |i|
+      @m[i, i].should == i + 10
+    end
+  end
+  
+  it "fills all non-diagonal cells with 0" do
+    (0..4).each do |i|
+      (0..4).each do |j|
+        if i != j
+          @m[i, j].should == 0
+        end
+      end
+    end
+  end
 end
