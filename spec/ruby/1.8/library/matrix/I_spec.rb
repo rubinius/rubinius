2c84f77535d677a42bee93759c77f79c2cdd4d93
Marnen Laibow-Koser <marnen@marnen.org>
2008-04-18 12:25:55 -0400

    Name spec correctly.

--- spec/ruby/1.8/library/matrix/I_spec.rb
+++ spec/ruby/1.8/library/matrix/I_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/shared/identity'
 
-describe "Matrix.identity" do
+describe "Matrix.I" do
   it_behaves_like(:matrix_identity, :I)
 end
