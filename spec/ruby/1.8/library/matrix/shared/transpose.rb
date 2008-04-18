958ca1faa1dc60ce591b4b2f768f22ac7f6cb56f
Marnen Laibow-Koser <marnen@marnen.org>
2008-04-18 12:22:09 -0400

    Move the "needs to be reviewed" indicator to the right place.
    Signed-off-by: Federico Builes <federico.builes@gmail.com>

--- spec/ruby/1.8/library/matrix/shared/transpose.rb
+++ spec/ruby/1.8/library/matrix/shared/transpose.rb
@@ -2,6 +2,9 @@ require 'matrix'
 
 shared :matrix_transpose do |cmd|
   describe "Matrix##{cmd}" do
+    it "needs to be reviewed for spec completeness" do
+    end
+  
     it "transposes rows and columns" do
       Matrix[[1, 2], [3, 4], [5, 6]].transpose.should == Matrix[[1, 3, 5], [2, 4, 6]]
     end
--- spec/ruby/1.8/library/matrix/t_spec.rb
+++ spec/ruby/1.8/library/matrix/t_spec.rb
@@ -2,8 +2,5 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/shared/transpose'
 
 describe "Matrix#transpose" do
-  it "needs to be reviewed for spec completeness" do
-  end
-  
   it_behaves_like(:matrix_transpose, :t)
 end
