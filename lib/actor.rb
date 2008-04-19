f5fe367f12970e4a59dbcc9ffb939155446eefa4
MenTaLguY <mental@rydia.net>
2008-04-19 04:20:37 -0400

    remove stale require

--- lib/actor.rb
+++ lib/actor.rb
@@ -28,8 +28,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-require 'mailbox'
-
 class Actor
   class DeadActorError < RuntimeError
     attr_reader :actor
