a992fec185dc2e2ccef7646adae322ef56d33b56
Brian Ford <bford@engineyard.com>
2008-04-24 15:48:12 -0700

    Removed reliance on YAML lib from YamlFormatter.

--- mspec/runner/formatters/yaml.rb
+++ mspec/runner/formatters/yaml.rb
@@ -20,24 +20,22 @@ class YamlFormatter < DottedFormatter
   def finish
     switch
 
-    result = { "exceptions" => [] }
+    print "---\n"
+    print "exceptions:\n"
     @states.each do |state|
       state.exceptions.each do |msg, exc|
         outcome = failure?(state) ? "FAILED" : "ERROR"
         str =  "#{state.description} #{outcome}\n"
         str << "Exception occurred during: #{msg}\n" if msg
         str << backtrace(exc)
-        result["exceptions"] << str
+        print "- ", str.inspect, "\n"
       end
     end
 
-    result["time"]         = @timer.elapsed
-    result["examples"]     = @tally.examples
-    result["expectations"] = @tally.expectations
-    result["failures"]     = @tally.failures
-    result["errors"]       = @tally.errors
-
-    require 'yaml'
-    print result.to_yaml
+    print "time: ",         @timer.elapsed,      "\n"
+    print "examples: ",     @tally.examples,     "\n"
+    print "expectations: ", @tally.expectations, "\n"
+    print "failures: ",     @tally.failures,     "\n"
+    print "errors: ",       @tally.errors,       "\n"
   end
 end
