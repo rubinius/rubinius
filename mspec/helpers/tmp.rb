561ce228e01cb7827eb3407864ca946d696d1493
Brian Ford <bford@engineyard.com>
2008-04-24 23:15:40 -0700

    Make #tmp helper consider stupidly named symlink on OS X.

--- mspec/helpers/tmp.rb
+++ mspec/helpers/tmp.rb
@@ -6,11 +6,17 @@
 # Usage in a spec:
 #
 #   File.open(tmp("tags.txt"), "w") { |f| f.puts "" }
+#
+# The order of directories below with "/private/tmp"
+# preceding "/tmp" is significant. On OS X, the directory
+# "/tmp" is a symlink to "private/tmp" with no leading
+# "/". Rather than futzing with what constitutes an
+# absolute path, we just look for "/private/tmp" first.
 
 class Object
   def tmp(name)
     unless @spec_temp_directory
-      [ "/tmp", ENV["TMPDIR"], ENV["TMP"],
+      [ "/private/tmp", "/tmp", ENV["TMPDIR"], ENV["TMP"],
         ENV["TEMP"], ENV["USERPROFILE"] ].each do |dir|
         if dir and File.directory?(dir) and File.writable?(dir)
           temp = File.expand_path dir
