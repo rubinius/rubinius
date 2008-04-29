08969735750e699e1405d4eebe192a540ea91089
MenTaLguY <mental@rydia.net>
2008-04-28 21:52:31 -0400

    split out Actor::Filter to a separate file

--- lib/actor.rb
+++ lib/actor.rb
@@ -420,40 +420,6 @@ class Actor
       @lock << nil
     end
   end
-
-  class Filter
-    attr_reader :timeout
-    attr_reader :timeout_action
-
-    def initialize
-      @pairs = []
-      @timeout = nil
-      @timeout_action = nil
-    end
-
-    def timeout?
-      not @timeout.nil?
-    end
-
-    def when(pattern, &action)
-      raise ArgumentError, "no block given" unless action
-      @pairs.push [pattern, action]
-      self
-    end
-
-    def after(seconds, &action)
-      raise ArgumentError, "no timeout given" unless seconds
-      seconds = seconds.to_f
-      if seconds < @timeout
-        @timeout = seconds
-        @timeout_action = action
-      end
-      self
-    end
-
-    def action_for(value)
-      pair = @pairs.find { |pattern, action| pattern === value }
-      pair ? pair.last : nil
-    end
-  end
 end
+
+require 'actor/filter'
