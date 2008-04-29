4a846f807fe2c4c12d8719bc5c9ccb4ab696aff9
Federico Builes <federico.builes@gmail.com>
2008-04-29 15:29:34 -0500

    Fixes REXML::Element#namespaces specs
    * Use sort on the arrays to make sure the specs pass on JRuby too.

--- spec/ruby/1.8/library/rexml/element/namespaces_spec.rb
+++ spec/ruby/1.8/library/rexml/element/namespaces_spec.rb
@@ -1,7 +1,7 @@
 require 'rexml/document'
 require File.dirname(__FILE__) + '/../../../spec_helper'
 
-describe "REXML::Element#namespace" do
+describe "REXML::Element#namespaces" do
   before :each do
     doc = REXML::Document.new("<a xmlns='1' xmlns:y='2'><b/><c xmlns:z='3'/></a>")
     @elem = doc.elements["//c"]
@@ -9,8 +9,8 @@ describe "REXML::Element#namespace" do
 
   it "returns a hash of the namespaces" do
     ns = {"y"=>"2", "z"=>"3", "xmlns"=>"1"}
-    @elem.namespaces.keys.should == ns.keys
-    @elem.namespaces.values.should == ns.values
+    @elem.namespaces.keys.sort.should == ns.keys.sort
+    @elem.namespaces.values.sort.should == ns.values.sort
   end
 
   it "returns an empty hash if no namespaces exist" do
@@ -21,12 +21,12 @@ describe "REXML::Element#namespace" do
 
   it "uses namespace prefixes as keys" do
     prefixes = ["y", "z", "xmlns"]
-    @elem.namespaces.keys.should == prefixes
+    @elem.namespaces.keys.sort.should == prefixes.sort
   end
 
   it "uses namespace values as the hash values" do
     values = ["2", "3", "1"]
-    @elem.namespaces.values.should == values
+    @elem.namespaces.values.sort.should == values.sort
   end
   
 end
