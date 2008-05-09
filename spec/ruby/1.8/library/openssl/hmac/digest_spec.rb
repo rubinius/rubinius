f8bd3e34014a7351470685676b6b168abd787794
Phil Hagelberg <technomancy@gmail.com>
2008-05-09 12:53:00 -0700

    Added specs for OpenSSL::HMAC.hexdigest and .digest

---
+++ spec/ruby/1.8/library/openssl/hmac/digest_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require File.dirname(__FILE__) + '/../shared/constants'
+require 'openssl'
+
+describe "OpenSSL::HMAC.digest" do
+  include HMACConstants
+  it 'returns an SHA1 digest' do
+    cur_digest = OpenSSL::Digest::Digest.new('SHA1')
+    cur_digest.digest.should == HMACConstants::BlankSHA1Digest
+    digest = OpenSSL::HMAC.digest(cur_digest,
+                                        HMACConstants::Key,
+                                        HMACConstants::Contents)
+    digest.should == HMACConstants::SHA1Digest
+  end
+end
+
+# Should add in similar specs for MD5, RIPEMD160, and SHA256
