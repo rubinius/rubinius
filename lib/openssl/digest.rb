916dccb5ea71a176b1479ae228430a1a577ed7bb
Wilson Bilkovich <wilson@supremetyrant.com>
2008-05-09 17:03:13 -0400

    Implement OpenSSL::Digest's update method

--- lib/openssl/digest.rb
+++ lib/openssl/digest.rb
@@ -1,28 +1,32 @@
 module OpenSSL
   module Digest
+    # Native OpenSSL functions and structs
     module Foreign
       set_ffi_lib "libssl"
+      # const EVP_MD *EVP_get_digestbyname(const char *name);
+      attach_function "EVP_get_digestbyname", "ossl_digest_getbyname", [:string], :pointer
+
+      #void OpenSSL_add_all_digests(void);
+      attach_function "OpenSSL_add_all_digests", "ossl_add_digests", [], :void
+
       # int EVP_DigestInit_ex(EVP_MD_CTX *ctx, const EVP_MD *type, ENGINE *impl);
       attach_function "EVP_DigestInit_ex", "ossl_digest_init_ex", [:pointer, :pointer, :pointer], :int
 
-      # const EVP_MD *EVP_get_digestbyname(const char *name);
-      attach_function "EVP_get_digestbyname", "ossl_digest_getbyname", [:string], :pointer
-      
+      #int EVP_DigestUpdate(EVP_MD_CTX *ctx, const void *d, size_t cnt);
+      attach_function "EVP_DigestUpdate", "ossl_digest_update", [:pointer, :string, :size_t], :int
+
       #EVP_MD_CTX *EVP_MD_CTX_create(void);
       attach_function "EVP_MD_CTX_create", "ossl_digest_ctx_create", [], :pointer
-      
-      #void OpenSSL_add_all_digests(void);
-      attach_function "OpenSSL_add_all_digests", "ossl_add_digests", [], :void
-      
+
       #int EVP_MD_CTX_copy_ex(EVP_MD_CTX *out,const EVP_MD_CTX *in);
       attach_function "EVP_MD_CTX_copy_ex", "ossl_digest_ctx_copy", [:pointer, :pointer], :int
-      
-      #int EVP_DigestFinal_ex(EVP_MD_CTX *ctx, unsigned char *md, unsigned int *s);
-      attach_function "EVP_DigestFinal_ex", "ossl_digest_final_ex", [:pointer, :pointer, :pointer], :int
 
       #int EVP_MD_CTX_cleanup(EVP_MD_CTX *ctx);
       attach_function "EVP_MD_CTX_cleanup", "ossl_digest_ctx_cleanup", [:pointer], :int
-      
+
+      #int EVP_DigestFinal_ex(EVP_MD_CTX *ctx, unsigned char *md, unsigned int *s);
+      attach_function "EVP_DigestFinal_ex", "ossl_digest_final_ex", [:pointer, :pointer, :pointer], :int
+
       class EVP_MD < FFI::Struct
         config("rbx.platform.evp_md", :md_size)
       end
@@ -34,10 +38,8 @@ module OpenSSL
           evp_md[:md_size]
         end
       end
-    end
+    end # Foreign
 
-    DIGEST_TYPES = ["MD2", "MDC2", "DSS1", "SHA1", "MD5",
-                      "DSS", "SHA", "MD4", "RIPEMD160"]
     class Digest
       # Populate OpenSSL's digest type table
       Foreign.ossl_add_digests
@@ -55,25 +57,34 @@ module OpenSSL
           end
           return digest_class.new
         end
-      end
+      end # metaclass
 
-      # TODO - call update if passed data
       def initialize(data = nil)
-        @context, @digest, @buffer = nil, nil, nil
+        @context, @digest = nil, nil
 
         @context = Foreign.ossl_digest_ctx_create
 
         @digest = Foreign.ossl_digest_getbyname self.class.const_get(:DigestName)
         Errno.handle if @digest.nil?
         Foreign.ossl_digest_init_ex(@context, @digest, nil)
+        update(data) if data
+      end
+
+      def update(data)
+        data = StringValue(data)
+        err = Foreign.ossl_digest_update @context, data, data.size
+        Errno.handle if err.zero?
+        return self
       end
 
       def hexdigest
-        finalize
+        buffer = finalize
 
         out = ""
         # Convert the binary string into lowercase hex
-        @buffer.read_string(@buffer.total).each_byte do |byte|
+        buffer.read_string(buffer.total).each_byte do |byte|
+          # TODO - Implement a lightweight helper for hex values
+          # with leading zeroes. sprintf is slow.
           out << sprintf("%02x", byte)
         end
         out
@@ -84,6 +95,7 @@ module OpenSSL
       # Copy the digest context and then finalize it
       def finalize
         # Create a placeholder to copy into
+        # TODO - ctx_create may be overkill here. Just use ctx_init?
         final = Foreign.ossl_digest_ctx_create
 
         err = Foreign.ossl_digest_ctx_copy(final, @context)
@@ -91,22 +103,26 @@ module OpenSSL
 
         final_ctx = Foreign::EVP_MD_CTX.new(final)
         # Create a buffer to hold the finalized output
-        @buffer = MemoryPointer.new(final_ctx.digest_size)
+        buffer = MemoryPointer.new(final_ctx.digest_size)
         buffer_size = MemoryPointer.new(:uint)
-        buffer_size.write_int(@buffer.total)
+        buffer_size.write_int(buffer.total)
 
-        err = Foreign.ossl_digest_final_ex(final, @buffer, buffer_size)
+        err = Foreign.ossl_digest_final_ex(final, buffer, buffer_size)
         Foreign.ossl_digest_ctx_cleanup(final)
-        Errno.handle if err.zero?
+        if err.zero? then
+          Errno.handle
+        else
+          buffer
+        end
       end
       private :finalize
-    end
+    end # Digest
 
     class SHA1 < Digest
       DigestName = "SHA1"
-      def self.new
-        original_new
+      def self.new(*args)
+        original_new(*args)
       end
-    end
-  end
-end
+    end # SHA1
+  end # OpenSSL::Digest
+end # OpenSSL
