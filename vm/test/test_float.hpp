1bf040486756f00e8658e16cabe24b09f60b9b16
Joachim Mårtensson <joachimm@c-33e2e055.129-2-64736c10.cust.bredbandsbolaget.se>
2008-05-11 10:24:31 +0200

    Added tests for Float's coerce, compare, and into_string in the c++ vm
    Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

--- vm/test/test_float.hpp
+++ vm/test/test_float.hpp
@@ -24,4 +24,34 @@ class TestFloat : public CxxTest::TestSuite {
     Float* flt = Float::create(state, 1.0);
     TS_ASSERT_EQUALS(flt->val, 1.0);
   }
+  
+  void test_compare() {
+      Float* fltBig = Float::create(state, 4.0);
+      Float* fltSmall = Float::create(state, 9.0);
+      TS_ASSERT_EQUALS(fltBig->compare(state, fltSmall), Object::i2n(-1));
+      TS_ASSERT_EQUALS(fltSmall->compare(state, fltBig), Object::i2n(1));
+      TS_ASSERT_EQUALS(fltSmall->compare(state, fltSmall), Object::i2n(0));
+  }
+  
+  void test_coerce() {
+      OBJECT o = Object::i2n(5432);
+      Float* coerced = Float::coerce(state, o);
+      TS_ASSERT(kind_of<Float>(coerced));
+      
+      Bignum* bn = Bignum::create(state, (native_int)2147483647);
+      Float* coercedBn = Float::coerce(state, bn);
+      TS_ASSERT(kind_of<Float>(coercedBn));
+      
+      String* str = String::create(state, "blah");
+      Float* coercedStr = Float::coerce(state, str);
+      TS_ASSERT(kind_of<Float>(coercedStr));
+      TS_ASSERT_EQUALS(coercedStr->val, 0.0);
+  }
+  
+  void test_into_string() {
+      Float* flt = Float::create(state, 1.0);
+      char buf[10];
+      flt->into_string(state, buf,10);
+      TS_ASSERT(strcmp(buf, "+1.000000") == 0);
+  }
 };
