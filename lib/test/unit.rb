5673309bf8e058a500e0c9ced6fc99eda19e6e01
Ryan Davis <ryand-ruby@zenspider.com>
2008-05-21 17:24:36 -0700

    More rails hacks:   Moved build_message to Assertions.   Now bitch loudly if someone defines one of my classes instead of requiring.

--- lib/test/unit.rb
+++ lib/test/unit.rb
@@ -2,20 +2,16 @@ require 'mini/test'
 
 module Test
   module Unit # was ::Mini::Test, but rails' horrid code forced my hand
-    remove_const :TestCase if defined? TestCase # nope... fuck you
+    if defined? TestCase then
+      warn "ARGH! someone defined Test::Unit::TestCase rather than requiring"
+      remove_const :TestCase
+    end
 
     TestCase             = ::Mini::Test::TestCase
     AssertionFailedError = ::Mini::Assertion
 
     class TestCase
       alias :method_name :name # so lame
-
-      def build_message(user_message, template_message, *args)
-        user_message ||= ''
-        user_message += ' ' unless user_message.empty?
-        msg = template_message.split(/<\?>/).zip(args.map { |o| o.inspect })
-        user_message + msg.join
-      end
     end
   end
 end
--- lib/test/unit/assertions.rb
+++ lib/test/unit/assertions.rb
@@ -3,12 +3,13 @@ require 'test/unit'
 
 module Test::Unit
   if defined? Assertions then
+    warn "ARGH! someone defined Test::Unit::Assertions rather than requiring"
     CRAP_ASSERTIONS = Assertions
     remove_const :Assertions
 
     ObjectSpace.each_object(Module) do |offender|
       offender.send :include, ::Mini::Assertions if offender < CRAP_ASSERTIONS
-    end
+    end rescue :fuck_you_then!
 
     Test::Unit::TestCase.send :include, CRAP_ASSERTIONS
   end
@@ -17,8 +18,14 @@ module Test::Unit
 
   module Assertions
     def self.included x
-      x.send :include, Test::Unit::CRAP_ASSERTIONS if
-        defined? Test::Unit::CRAP_ASSERTIONS
+      x.send :include, Test::Unit::CRAP_ASSERTIONS
+    end if defined? Test::Unit::CRAP_ASSERTIONS
+
+    def build_message(user_message, template_message, *args)
+      user_message ||= ''
+      user_message += ' ' unless user_message.empty?
+      msg = template_message.split(/<\?>/).zip(args.map { |o| o.inspect })
+      user_message + msg.join
     end
   end
 end
