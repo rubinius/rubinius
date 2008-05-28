38884702cb5be7127bfa77769559d9fe85c1627e
Cezar Sa Espinola <cezarsa@gmail.com>
2008-05-28 00:50:08 -0300

    Allowing subtend specs to be executed under MRI (bin/mspec -truby).

--- spec/subtend/subtend_helper.rb
+++ spec/subtend/subtend_helper.rb
@@ -1,9 +1,15 @@
 # Compiles the extension used in the subtend specs
+require 'rbconfig'
 def compile_extension(name)
   path = File.dirname(__FILE__) + '/ext/'
-  ext = "#{path}#{name}.#{Rubinius::LIBSUFFIX}"
+  ext = "#{path}#{name}.#{Config::CONFIG['DLEXT']}"
   ext_source = "#{path}#{name}.c"
   if !File.exists?(ext) or File.mtime(ext) < File.mtime(ext_source)
-    system "./shotgun/rubinius compile #{ext_source} -g > /dev/null"
+    if Object.constants.include? 'Rubinius'
+      system "./shotgun/rubinius compile #{ext_source} -g > /dev/null"
+    else
+      c = Config::CONFIG
+      system "#{c['LDSHARED']} -g #{ext_source} -I #{c['archdir']} #{c['SOLIBS']} #{c['LIBRUBYARG_SHARED']} -o #{ext} > /dev/null"
+    end
   end
 end
