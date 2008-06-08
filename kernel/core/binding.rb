d8b8078eacd59088de07dfe51660d690fdfc58ca
Wilson Bilkovich <wilson@supremetyrant.com>
2008-06-08 17:25:47 -0400

    Move class Binding from eval.rb to binding.rb

---
+++ kernel/core/binding.rb
@@ -0,0 +1,55 @@
+# depends on: eval.rb
+
+##
+# Objects of class Binding encapsulate the execution context at some
+# particular place in the code and retain this context for future use. The
+# variables, methods, value of self, and possibly an iterator block that can
+# be accessed in this context are all retained. Binding objects can be created
+# using Kernel#binding, and are made available to the callback of
+# Kernel#set_trace_func.
+#
+# These binding objects can be passed as the second argument of the
+# Kernel#eval method, establishing an environment for the evaluation.
+#
+#   class Demo
+#     def initialize(n)
+#       @secret = n
+#     end
+#     def getBinding
+#       return binding()
+#     end
+#   end
+#   
+#   k1 = Demo.new(99)
+#   b1 = k1.getBinding
+#   k2 = Demo.new(-3)
+#   b2 = k2.getBinding
+#   
+#   eval("@secret", b1)   #=> 99
+#   eval("@secret", b2)   #=> -3
+#   eval("@secret")       #=> nil
+#
+# Binding objects have no class-specific methods.
+
+class Binding
+  attr_accessor :context
+  attr_accessor :caller_env
+
+  def self.setup(ctx)
+    bind = allocate()
+    while ctx.from_eval?
+      ctx = ctx.next_frame
+    end
+    
+    bind.context = ctx
+    return bind
+  end
+
+  def self.from_env(env)
+    bind = allocate()
+
+    bind.context = env.home_block
+    bind.caller_env = env
+    return bind
+  end
+end
