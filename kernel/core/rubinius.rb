d78d4f193b35f98bd85bd6b821b67e0af624d435
Ryan Davis <ryand-ruby@zenspider.com>
2008-06-09 16:44:55 -0700

    Added Rubinius.warn

---
+++ kernel/core/rubinius.rb
@@ -0,0 +1,11 @@
+module Rubinius
+  AtExit = []
+
+  def warn(warning)
+    unless $VERBOSE.nil? then
+      $stderr.write "#{MethodContext.current.sender.location}: warning: #{warning}\n"
+    end
+    nil
+  end
+  module_function :warn
+end
