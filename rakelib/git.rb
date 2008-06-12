fe4384be0d1ed24c50c624004e72fa5e5a85afab
Ryan Davis <ryand-ruby@zenspider.com>
2008-06-11 17:18:15 -0700

    Rough draft of release recipes. Includes git.rb refactorings

--- rakelib/git.rb
+++ rakelib/git.rb
@@ -1,3 +1,6 @@
+
+MINIMUM_VERSION = [1, 5, 3]
+
 def is_git_dir(dir)
   File.directory?(dir) and File.directory?(File.join(dir, ".git"))
 end
@@ -7,51 +10,59 @@ def git_branch
 end
 
 def compare_git_ver
-  m = /git version (\d+).(\d+).(\d+)/.match(`git version`.strip)
-  return true  if m[1].to_i > 1
-  return false if m[1].to_i < 1
-  return true  if m[2].to_i > 5
-  return false if m[2].to_i < 5
-  return true  if m[3].to_i >= 3
-  return false
+  v = `git version`.scan(/version (\d+).(\d+).(\d+)/).flatten.map { |s| s.to_i }
+  m = [1, 5, 3]
+
+  (v <=> MINIMUM_VERSION) >= 0
 end
 
 def check_git_ver
-  raise "Invalid git version, use at least 1.5.3" unless compare_git_ver
+  raise "Invalid git version, use at least #{MINIMUM_VERSION.join(".")}" unless
+    compare_git_ver
 end
 
-def git_update
-  check_git_ver
-  `git diff-files --quiet`
-  if $?.exitstatus == 1
-    stash = true
-    clear = `git stash list`.scan("\n").size == 0
-    puts "* Saving changes..."
-    `git stash save`
-  else
-    stash = false
-  end
-
+def on_master
   branch = git_branch()
-  if branch != "master"
-    switch = true
-    `git checkout master`
-    puts "* Switching back to master..."
-  else
-    switch = false
-  end
+  switch = if branch != "master" then
+             `git checkout master`
+             puts "* Switching back to master..."
+             true
+           end
 
-  puts "* Pulling in new commits..."
-  sh "git fetch"
-  sh "git rebase origin"
+  yield
 
   if switch
     puts "* Porting changes into #{branch}..."
     `git checkout #{branch}`
     sh "git rebase master"
   end
+end
 
-  if stash
+def with_git_changes
+  `git diff-files --quiet`
+  if $?.exitstatus == 1 then
+    yield
+    true
+  end
+end
+
+def git_update
+  check_git_ver
+
+  stash = with_git_changes do
+    clear = `git stash list`.scan("\n").size == 0
+    puts "* Saving changes..."
+    `git stash save`
+    true
+  end
+
+  on_master do
+    puts "* Pulling in new commits..."
+    sh "git fetch"
+    sh "git rebase origin"
+  end
+
+  if stash then
     puts "* Applying changes..."
     sh "git stash apply"
     `git stash clear` if clear
@@ -59,27 +70,15 @@ def git_update
 end
 
 def git_push
-  branch = git_branch()
-  if branch != "master"
-    `git diff-files --quiet`
-    if $?.exitstatus == 1
-      puts "You have outstanding changes. Please commit them first."
-      exit 1
-    end
+  branch = git_branch
 
+  with_git_changes do
+    abort "You have outstanding changes. Please commit them first."
+  end if branch != "master"
+
+  on_master do
     puts "* Merging topic '#{branch}' back into master..."
-    `git checkout master`
-    sh "git merge #{branch}"
-    switch = true
-  else
-    switch = false
-  end
-
-  puts "* Pushing changes..."
-  sh "git push"
-
-  if switch
-    puts "* Switching back to #{branch}..."
-    `git checkout #{branch}`
+    puts "* Pushing changes..."
+    sh "git push"
   end
 end
