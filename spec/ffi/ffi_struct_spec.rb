630caef70f140b72c1f458c2cb8c5bf8e4a93535
pluskid <pluskid@gmail.com>
2008-06-16 15:40:11 -0400

    Added a spec for FFI::Struct.new

---
+++ spec/ffi/ffi_struct_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+module FFIStructSpecs
+  DummyLayout = ['a', :int, 0]
+  
+  class DummyStruct < FFI::Struct
+    layout(*DummyLayout)
+  end
+end
+
+describe "FFI::Struct.new" do
+  it "creates pre-configured struct with proper size" do
+    dummy = FFIStructSpecs::DummyStruct.new
+    dummy.size.should == FFI.type_size(:int)
+  end
+
+  it "creates struct configured on the fly with proper size" do
+    dummy = FFI::Struct.new(nil, *FFIStructSpecs::DummyLayout)
+    dummy.size.should == FFI.type_size(:int)
+  end
+end
