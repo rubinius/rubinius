65341f2ebf6a8aace09201d9d69307ecc2f641b4
Brian Ford <bford@engineyard.com>
2008-07-28 16:51:20 -0700

    Added annotations for LookupTable primitives.

--- vm/builtin_lookuptable.hpp
+++ vm/builtin_lookuptable.hpp
@@ -16,23 +16,31 @@ namespace rubinius {
     /* Prototypes */
     static LookupTable* create(STATE, size_t sz = LOOKUPTABLE_MIN_SIZE);
     void setup(STATE, size_t sz);
+    // Ruby.primitive :lookuptable_store
     OBJECT store(STATE, OBJECT key, OBJECT val);
+    // Ruby.primitive :lookuptable_fetch
     OBJECT fetch(STATE, OBJECT key);
     OBJECT fetch(STATE, OBJECT key, bool* found);
+    // Ruby.primitive :lookuptable_dup
     LookupTable* dup(STATE);
     static OBJECT entry_new(STATE, OBJECT key, OBJECT val);
     static OBJECT entry_append(STATE, Tuple* top, OBJECT nxt);
     void   redistribute(STATE, size_t size);
     Tuple* find_entry(STATE, OBJECT key);
     OBJECT find(STATE, OBJECT key);
+    // Ruby.primitive :lookuptable_delete
     OBJECT remove(STATE, OBJECT key);
+    // Ruby.primitive :lookuptable_has_key
     OBJECT has_key(STATE, OBJECT key);
     static Array* collect(STATE, LookupTable* tbl, OBJECT (*action)(STATE, Tuple*));
     static OBJECT get_key(STATE, Tuple* entry);
+    // Ruby.primitive :lookuptable_keys
     Array* all_keys(STATE);
     static OBJECT get_value(STATE, Tuple* entry);
+    // Ruby.primitive :lookuptable_values
     Array* all_values(STATE);
     static OBJECT get_entry(STATE, Tuple* entry);
+    // Ruby.primitive :lookuptable_entries
     Array* all_entries(STATE);
 
     class Info : public TypeInfo {
