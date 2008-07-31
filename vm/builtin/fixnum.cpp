c7f39c7b316326dbce0973ac9ac974fb4bebb31b
Ryan Davis <rdavis@engineyard.com>
2008-07-31 16:16:46 -0700

    Removed all (non-template) code from fixnum header

--- vm/builtin/fixnum.cpp
+++ vm/builtin/fixnum.cpp
@@ -3,6 +3,313 @@
 #include "builtin/fixnum.hpp"
 
 namespace rubinius {
+
+  INTEGER Fixnum::add(STATE, FIXNUM other) {
+    native_int r = to_nint() + other->to_nint();
+    if(r > FIXNUM_MAX || r < FIXNUM_MIN) {
+      return Bignum::create(state, r);
+    } else {
+      return Object::i2n(r);
+    }
+  }
+
+  INTEGER Fixnum::add(STATE, Bignum* other) {
+    return other->add(state, this);
+  }
+
+  Float* Fixnum::add(STATE, Float* other) {
+    return other->add(state, this);
+  }
+
+  INTEGER Fixnum::sub(STATE, FIXNUM other) {
+    native_int r = n2i() - other->n2i();
+    if(r > FIXNUM_MAX || r < FIXNUM_MIN) {
+      return Bignum::create(state, r);
+    } else {
+      return Object::i2n(state, r);
+    }
+  }
+    
+  INTEGER Fixnum::sub(STATE, Bignum* other) {
+    return as<Bignum>(other->neg(state))->add(state, this);
+  }
+
+  Float* Fixnum::sub(STATE, Float* other) {
+    return Float::coerce(state, this)->sub(state, other);
+  }
+
+  INTEGER Fixnum::mul(STATE, FIXNUM other) {
+    native_int a  = n2i();
+    native_int b  = other->n2i();
+      
+    if(a == 0 || b == 0) return Object::i2n(0);
+      
+    if(a > 0) {
+      if(b > 0) {
+        if(a > (FIXNUM_MAX / b)) {
+          return Bignum::create(state, a)->mul(state, other);
+        } 
+      } else {
+        if (b < (FIXNUM_MIN / a)) {
+          return Bignum::create(state, a)->mul(state, other);
+        }
+      }
+    } else {
+      if(b > 0){
+        if(a < (FIXNUM_MIN / b)) {
+          return Bignum::create(state, a)->mul(state, other);
+        }
+      } else {
+        if(b < (FIXNUM_MAX / a)) {
+          return Bignum::create(state, a)->mul(state, other);
+        }
+      }
+    }
+      
+    return Object::i2n(state, n2i() * other->n2i());
+  }
+
+  INTEGER Fixnum::mul(STATE, Bignum* other) {
+    return other->mul(state, this);
+  }
+
+  Float* Fixnum::mul(STATE, Float* other) {
+    return other->mul(state, this);
+  }
+
+  INTEGER Fixnum::div(STATE, FIXNUM other) {
+    if(other->n2i() == 0) {
+      throw ZeroDivisionError(other, "divided by 0");
+    }
+    native_int numerator = n2i();
+    native_int denominator = other->n2i();
+    native_int quotient = numerator / denominator;
+    if(quotient < 0 && quotient * denominator != numerator) --quotient;
+    return Object::i2n(state, quotient);
+  }
+
+  INTEGER Fixnum::div(STATE, Bignum* other) {
+    return Bignum::create(state, n2i())->div(state, other);
+  }
+
+  Float* Fixnum::div(STATE, Float* other) {
+    return Float::coerce(state, this)->div(state, other);
+  }
+
+  INTEGER Fixnum::mod(STATE, FIXNUM other) {
+    native_int numerator = n2i();
+    native_int denominator = other->n2i();
+    native_int quotient = div(state, other)->n2i();
+    return Object::i2n(state, numerator - denominator * quotient);
+  }
+
+  INTEGER Fixnum::mod(STATE, Bignum* other) {
+    return Bignum::create(state, n2i())->mod(state, other);
+  }
+
+  Float* Fixnum::mod(STATE, Float* other) {
+    return Float::create(state, n2i())->mod(state, other);
+  }
+
+  Array* Fixnum::divmod(STATE, FIXNUM other) {
+    if(other->n2i() == 0) {
+      throw ZeroDivisionError(other, "divided by 0");
+    }
+    native_int numerator = n2i();
+    native_int denominator = other->n2i();
+    native_int fraction = div(state, other)->n2i();
+    Array* ary = Array::create(state, 2);
+    ary->set(state, 0, Object::i2n(fraction));
+    ary->set(state, 1, Object::i2n(numerator - denominator * fraction));
+    return ary;
+  }
+
+  Array* Fixnum::divmod(STATE, Bignum* other) {
+    return Bignum::create(state, n2i())->divmod(state, other);
+  }
+
+  Array* Fixnum::divmod(STATE, Float* other) {
+    return Float::create(state, n2i())->divmod(state, other);
+  }
+
+  INTEGER Fixnum::neg(STATE) {
+    return Object::i2n(-n2i());
+  }
+
+  OBJECT Fixnum::equal(STATE, FIXNUM other) {
+    return n2i() == other->n2i() ? Qtrue : Qfalse;
+  }
+
+  OBJECT Fixnum::equal(STATE, Bignum* other) {
+    return other->equal(state, this);
+  }
+
+  OBJECT Fixnum::equal(STATE, Float* other) {
+    return (double)n2i() == other->val ? Qtrue : Qfalse;
+  }
+
+  FIXNUM Fixnum::compare(STATE, FIXNUM other) {
+    native_int left  = n2i();
+    native_int right = other->n2i();
+    if(left == right) {
+      return Object::i2n(0);
+    } else if(left < right) {
+      return Object::i2n(-1);
+    } else {
+      return Object::i2n(1);
+    }
+  }
+
+  FIXNUM Fixnum::compare(STATE, Bignum* other) {
+    native_int res = other->compare(state, this)->n2i();
+    if(res == 0) {
+      return Object::i2n(0);
+    } else if(res < 0) {
+      return Object::i2n(1);
+    } else {
+      return Object::i2n(-1);
+    }
+  }
+
+  FIXNUM Fixnum::compare(STATE, Float* other) {
+    double left  = (double)n2i();
+    double right = other->val;
+    if(left == right) {
+      return Object::i2n(0);
+    } else if(left < right) {
+      return Object::i2n(-1);
+    } else {
+      return Object::i2n(1);
+    }
+  }
+
+  OBJECT Fixnum::gt(STATE, FIXNUM other) {
+    return n2i() > other->n2i() ? Qtrue : Qfalse;
+  }
+
+  OBJECT Fixnum::gt(STATE, Bignum* other) {
+    return other->lt(state, this);
+  }
+
+  OBJECT Fixnum::gt(STATE, Float* other) {
+    return (double) n2i() > other->val ? Qtrue : Qfalse;
+  }
+
+  OBJECT Fixnum::ge(STATE, FIXNUM other) {
+    return n2i() >= other->n2i() ? Qtrue : Qfalse;      
+  }
+
+  OBJECT Fixnum::ge(STATE, Bignum* other) {
+    return other->le(state, this);
+  }
+
+  OBJECT Fixnum::ge(STATE, Float* other) {
+    return (double) n2i() >= other->val ? Qtrue : Qfalse;
+  }
+
+  OBJECT Fixnum::lt(STATE, FIXNUM other) {
+    return n2i() < other->n2i() ? Qtrue : Qfalse;      
+  }
+
+  OBJECT Fixnum::lt(STATE, Bignum* other) {
+    return other->gt(state, this);
+  }
+
+  OBJECT Fixnum::lt(STATE, Float* other) {
+    return (double) n2i() < other->val ? Qtrue : Qfalse;
+  }
+
+  OBJECT Fixnum::le(STATE, FIXNUM other) {
+    return n2i() <= other->n2i() ? Qtrue : Qfalse;      
+  }
+
+  OBJECT Fixnum::le(STATE, Bignum* other) {
+    return other->ge(state, this);
+  }
+
+  OBJECT Fixnum::le(STATE, Float* other) {
+    return (double) n2i() <= other->val ? Qtrue : Qfalse;
+  }
+
+  INTEGER Fixnum::left_shift(STATE, INTEGER bits) {
+    native_int shift = bits->n2i();
+    if(shift < 0) {
+      return right_shift(state, Object::i2n(-shift));
+    }
+      
+    native_int self = n2i();
+      
+    if(shift > (native_int)FIXNUM_WIDTH || self >> ((native_int)FIXNUM_WIDTH - shift) > 0) {
+      return Bignum::create(state, self)->left_shift(state, bits);
+    }
+      
+    return Object::i2n(self << shift);
+  }
+
+  INTEGER Fixnum::right_shift(STATE, INTEGER bits) {
+    native_int shift = bits->n2i();
+    if(shift < 0) {
+      return left_shift(state, Object::i2n(-shift));
+    }
+      
+    native_int self = n2i();
+      
+    native_int a = self >> shift;
+    return Object::i2n(a); 
+  }
+
+  native_int Fixnum::to_nint() {
+    return STRIP_TAG(this);
+  }
+
+  INTEGER Fixnum::size(STATE) {
+    return Object::i2n(sizeof(native_int));
+  }
+
+  INTEGER Fixnum::bit_and(STATE, FIXNUM other) {
+    return Object::i2n(n2i() & other->n2i());
+  }
+
+  INTEGER Fixnum::bit_and(STATE, Bignum* other) {
+    return other->bit_and(state, this);
+  }
+
+  INTEGER Fixnum::bit_and(STATE, Float* other) {
+    return Object::i2n(n2i() & (native_int)other->val);
+  }
+
+  INTEGER Fixnum::bit_or(STATE, FIXNUM other) {
+    return Object::i2n(n2i() | other->n2i());
+  }
+
+  INTEGER Fixnum::bit_or(STATE, Bignum* other) {
+    return other->bit_or(state, this);
+  }
+
+  INTEGER Fixnum::bit_or(STATE, Float* other) {
+    return Object::i2n(n2i() | (native_int)other->val);
+  }
+
+  INTEGER Fixnum::bit_xor(STATE, FIXNUM other) {
+    return Object::i2n(n2i() ^ other->n2i());
+  }
+
+  INTEGER Fixnum::bit_xor(STATE, Bignum* other) {
+    return other->bit_xor(state, this);
+  }
+
+  INTEGER Fixnum::bit_xor(STATE, Float* other) {
+    return Object::i2n(n2i() ^ (native_int)other->val);
+  }
+
+  INTEGER Fixnum::invert(STATE) {
+    return Object::i2n(~n2i());
+  }
+
+  Float* Fixnum::to_f(STATE) {
+    return Float::create(state, (double)n2i());
+  }
+
   String* Fixnum::to_s(STATE) {
     std::stringstream sout;
     sout << n2i();
--- vm/builtin/fixnum.hpp
+++ vm/builtin/fixnum.hpp
@@ -11,368 +11,162 @@ namespace rubinius {
     const static object_type type = FixnumType;
 
     // Ruby.primitive! :fixnum_add
-    INTEGER add(STATE, FIXNUM other) {
-      native_int r = to_nint() + other->to_nint();
-      if(r > FIXNUM_MAX || r < FIXNUM_MIN) {
-        return Bignum::create(state, r);
-      } else {
-        return Object::i2n(r);
-      }
-    }
+    INTEGER add(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_add
-    INTEGER add(STATE, Bignum* other) {
-      return other->add(state, this);
-    }
+    INTEGER add(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_add
-    Float* add(STATE, Float* other) {
-      return other->add(state, this);
-    }
+    Float* add(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_sub
-    INTEGER sub(STATE, FIXNUM other) {
-      native_int r = n2i() - other->n2i();
-      if(r > FIXNUM_MAX || r < FIXNUM_MIN) {
-        return Bignum::create(state, r);
-      } else {
-        return Object::i2n(state, r);
-      }
-    }
+    INTEGER sub(STATE, FIXNUM other);
     
     // Ruby.primitive! :fixnum_sub
-    INTEGER sub(STATE, Bignum* other) {
-      return as<Bignum>(other->neg(state))->add(state, this);
-    }
+    INTEGER sub(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_sub
-    Float* sub(STATE, Float* other) {
-      return Float::coerce(state, this)->sub(state, other);
-    }
+    Float* sub(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_mul
-    INTEGER mul(STATE, FIXNUM other) {
-      native_int a  = n2i();
-      native_int b  = other->n2i();
-      
-      if(a == 0 || b == 0) return Object::i2n(0);
-      
-      if(a > 0) {
-        if(b > 0) {
-          if(a > (FIXNUM_MAX / b)) {
-            return Bignum::create(state, a)->mul(state, other);
-          } 
-        } else {
-          if (b < (FIXNUM_MIN / a)) {
-            return Bignum::create(state, a)->mul(state, other);
-          }
-        }
-      } else {
-        if(b > 0){
-          if(a < (FIXNUM_MIN / b)) {
-            return Bignum::create(state, a)->mul(state, other);
-          }
-        } else {
-          if(b < (FIXNUM_MAX / a)) {
-            return Bignum::create(state, a)->mul(state, other);
-          }
-        }
-      }
-      
-      return Object::i2n(state, n2i() * other->n2i());
-    }
+    INTEGER mul(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_mul
-    INTEGER mul(STATE, Bignum* other) {
-      return other->mul(state, this);
-    }
+    INTEGER mul(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_mul
-    Float* mul(STATE, Float* other) {
-      return other->mul(state, this);
-    }
+    Float* mul(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_div
-    INTEGER div(STATE, FIXNUM other) {
-      if(other->n2i() == 0) {
-        throw ZeroDivisionError(other, "divided by 0");
-      }
-      native_int numerator = n2i();
-      native_int denominator = other->n2i();
-      native_int quotient = numerator / denominator;
-      if(quotient < 0 && quotient * denominator != numerator) --quotient;
-      return Object::i2n(state, quotient);
-    }
+    INTEGER div(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_div
-    INTEGER div(STATE, Bignum* other) {
-      return Bignum::create(state, n2i())->div(state, other);
-    }
+    INTEGER div(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_div
-    Float* div(STATE, Float* other) {
-      return Float::coerce(state, this)->div(state, other);
-    }
+    Float* div(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_mod
-    INTEGER mod(STATE, FIXNUM other) {
-      native_int numerator = n2i();
-      native_int denominator = other->n2i();
-      native_int quotient = div(state, other)->n2i();
-      return Object::i2n(state, numerator - denominator * quotient);
-    }
+    INTEGER mod(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_mod
-    INTEGER mod(STATE, Bignum* other) {
-      return Bignum::create(state, n2i())->mod(state, other);
-    }
+    INTEGER mod(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_mod
-    Float* mod(STATE, Float* other) {
-      return Float::create(state, n2i())->mod(state, other);
-    }
+    Float* mod(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_divmod
-    Array* divmod(STATE, FIXNUM other) {
-      if(other->n2i() == 0) {
-        throw ZeroDivisionError(other, "divided by 0");
-      }
-      native_int numerator = n2i();
-      native_int denominator = other->n2i();
-      native_int fraction = div(state, other)->n2i();
-      Array* ary = Array::create(state, 2);
-      ary->set(state, 0, Object::i2n(fraction));
-      ary->set(state, 1, Object::i2n(numerator - denominator * fraction));
-      return ary;
-    }
+    Array* divmod(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_divmod
-    Array* divmod(STATE, Bignum* other) {
-      return Bignum::create(state, n2i())->divmod(state, other);
-    }
+    Array* divmod(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_divmod
-    Array* divmod(STATE, Float* other) {
-      return Float::create(state, n2i())->divmod(state, other);
-    }
+    Array* divmod(STATE, Float* other);
 
     // Ruby.primitive :fixnum_neg
-    INTEGER neg(STATE) {
-      return Object::i2n(-n2i());
-    }
+    INTEGER neg(STATE);
 
     // Ruby.primitive! :fixnum_equal
-    OBJECT equal(STATE, FIXNUM other) {
-      return n2i() == other->n2i() ? Qtrue : Qfalse;
-    }
+    OBJECT equal(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_equal
-    OBJECT equal(STATE, Bignum* other) {
-      return other->equal(state, this);
-    }
+    OBJECT equal(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_equal
-    OBJECT equal(STATE, Float* other) {
-      return (double)n2i() == other->val ? Qtrue : Qfalse;
-    }
+    OBJECT equal(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_compare
-    FIXNUM compare(STATE, FIXNUM other) {
-      native_int left  = n2i();
-      native_int right = other->n2i();
-      if(left == right) {
-        return Object::i2n(0);
-      } else if(left < right) {
-        return Object::i2n(-1);
-      } else {
-        return Object::i2n(1);
-      }
-    }
+    FIXNUM compare(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_compare
-    FIXNUM compare(STATE, Bignum* other) {
-      native_int res = other->compare(state, this)->n2i();
-      if(res == 0) {
-        return Object::i2n(0);
-      } else if(res < 0) {
-        return Object::i2n(1);
-      } else {
-        return Object::i2n(-1);
-      }
-    }
+    FIXNUM compare(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_compare
-    FIXNUM compare(STATE, Float* other) {
-      double left  = (double)n2i();
-      double right = other->val;
-      if(left == right) {
-        return Object::i2n(0);
-      } else if(left < right) {
-        return Object::i2n(-1);
-      } else {
-        return Object::i2n(1);
-      }
-    }
+    FIXNUM compare(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_gt
-    OBJECT gt(STATE, FIXNUM other) {
-      return n2i() > other->n2i() ? Qtrue : Qfalse;
-    }
+    OBJECT gt(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_gt
-    OBJECT gt(STATE, Bignum* other) {
-      return other->lt(state, this);
-    }
+    OBJECT gt(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_gt
-    OBJECT gt(STATE, Float* other) {
-      return (double) n2i() > other->val ? Qtrue : Qfalse;
-    }
+    OBJECT gt(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_ge
-    OBJECT ge(STATE, FIXNUM other) {
-      return n2i() >= other->n2i() ? Qtrue : Qfalse;      
-    }
+    OBJECT ge(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_ge
-    OBJECT ge(STATE, Bignum* other) {
-      return other->le(state, this);
-    }
+    OBJECT ge(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_ge
-    OBJECT ge(STATE, Float* other) {
-      return (double) n2i() >= other->val ? Qtrue : Qfalse;
-    }
+    OBJECT ge(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_lt
-    OBJECT lt(STATE, FIXNUM other) {
-      return n2i() < other->n2i() ? Qtrue : Qfalse;      
-    }
+    OBJECT lt(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_lt
-    OBJECT lt(STATE, Bignum* other) {
-      return other->gt(state, this);
-    }
+    OBJECT lt(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_lt
-    OBJECT lt(STATE, Float* other) {
-      return (double) n2i() < other->val ? Qtrue : Qfalse;
-    }
+    OBJECT lt(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_le
-    OBJECT le(STATE, FIXNUM other) {
-      return n2i() <= other->n2i() ? Qtrue : Qfalse;      
-    }
+    OBJECT le(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_le
-    OBJECT le(STATE, Bignum* other) {
-      return other->ge(state, this);
-    }
+    OBJECT le(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_le
-    OBJECT le(STATE, Float* other) {
-      return (double) n2i() <= other->val ? Qtrue : Qfalse;
-    }
+    OBJECT le(STATE, Float* other);
 
     // Ruby.primitive :fixnum_left_shift
-    INTEGER left_shift(STATE, INTEGER bits) {
-      native_int shift = bits->n2i();
-      if(shift < 0) {
-        return right_shift(state, Object::i2n(-shift));
-      }
-      
-      native_int self = n2i();
-      
-      if(shift > (native_int)FIXNUM_WIDTH || self >> ((native_int)FIXNUM_WIDTH - shift) > 0) {
-        return Bignum::create(state, self)->left_shift(state, bits);
-      }
-      
-      return Object::i2n(self << shift);
-    }
+    INTEGER left_shift(STATE, INTEGER bits);
 
     // Ruby.primitive :fixnum_right_shift
-    INTEGER right_shift(STATE, INTEGER bits) {
-      native_int shift = bits->n2i();
-      if(shift < 0) {
-        return left_shift(state, Object::i2n(-shift));
-      }
-      
-      native_int self = n2i();
-      
-      native_int a = self >> shift;
-      return Object::i2n(a); 
-    }
+    INTEGER right_shift(STATE, INTEGER bits);
 
-    native_int to_nint() {
-      return STRIP_TAG(this);
-    }
+    native_int to_nint();
 
     // Ruby.primitive :fixnum_size
-    INTEGER size(STATE) {
-      return Object::i2n(sizeof(native_int));
-    }
+    INTEGER size(STATE);
 
     // Ruby.primitive! :fixnum_and
-    INTEGER bit_and(STATE, FIXNUM other) {
-      return Object::i2n(n2i() & other->n2i());
-    }
+    INTEGER bit_and(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_and
-    INTEGER bit_and(STATE, Bignum* other) {
-      return other->bit_and(state, this);
-    }
+    INTEGER bit_and(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_and
-    INTEGER bit_and(STATE, Float* other) {
-      return Object::i2n(n2i() & (native_int)other->val);
-    }
+    INTEGER bit_and(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_or
-    INTEGER bit_or(STATE, FIXNUM other) {
-      return Object::i2n(n2i() | other->n2i());
-    }
+    INTEGER bit_or(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_or
-    INTEGER bit_or(STATE, Bignum* other) {
-      return other->bit_or(state, this);
-    }
+    INTEGER bit_or(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_or
-    INTEGER bit_or(STATE, Float* other) {
-      return Object::i2n(n2i() | (native_int)other->val);
-    }
+    INTEGER bit_or(STATE, Float* other);
 
     // Ruby.primitive! :fixnum_xor
-    INTEGER bit_xor(STATE, FIXNUM other) {
-      return Object::i2n(n2i() ^ other->n2i());
-    }
+    INTEGER bit_xor(STATE, FIXNUM other);
 
     // Ruby.primitive! :fixnum_xor
-    INTEGER bit_xor(STATE, Bignum* other) {
-      return other->bit_xor(state, this);
-    }
+    INTEGER bit_xor(STATE, Bignum* other);
 
     // Ruby.primitive! :fixnum_xor
-    INTEGER bit_xor(STATE, Float* other) {
-      return Object::i2n(n2i() ^ (native_int)other->val);
-    }
+    INTEGER bit_xor(STATE, Float* other);
 
     // Ruby.primitive :fixnum_invert
-    INTEGER invert(STATE) {
-      return Object::i2n(~n2i());
-    }
+    INTEGER invert(STATE);
 
     // Ruby.primitive :fixnum_to_f
-    Float* to_f(STATE) {
-      return Float::create(state, (double)n2i());
-    }
+    Float* to_f(STATE);
 
     // Ruby.primitive :fixnum_to_s
-    String* to_s(STATE); // {
-//       std::stringstream sout;
-//       sout << n2i();
-//       return String::create(state, sout.str().c_str());
-//     }
+    String* to_s(STATE);
   };
 
   typedef Fixnum* FIXNUM;
