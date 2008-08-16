952a9298c1a7b35763d68f2b3694819386ca9f94
Dirkjan Bussink <d.bussink@gmail.com>
2008-08-16 23:36:07 +0200

    Implement Integer tests

--- vm/test/test_integer.hpp
+++ vm/test/test_integer.hpp
@@ -21,26 +21,62 @@ class TestInteger : public CxxTest::TestSuite {
   }
 
   void test_from_native_int() {
-    // TODO
+    INTEGER obj = Integer::from(state, (native_int)13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int) 13);
+
+    obj = Integer::from(state, (native_int)-13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)-13);
+
+    obj = Integer::from(state, (native_int)2147483647);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)2147483647);
   }
 
   void test_from_int() {
-    // TODO
+    INTEGER obj = Integer::from(state, (int)13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int) 13);
+
+    obj = Integer::from(state, (int)-13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)-13);
+
+    obj = Integer::from(state, (int)2147483647);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)2147483647);
   }
 
   void test_from_unsigned_int() {
-    // TODO
+    INTEGER obj = Integer::from(state, (unsigned int)13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int) 13);
+
+    obj = Integer::from(state, (unsigned int)2147483647);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)2147483647);
   }
 
   void test_from_unsigned_long() {
-    // TODO
+    INTEGER obj = Integer::from(state, (unsigned long)13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int) 13);
+
+    obj = Integer::from(state, (unsigned long)2147483647);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)2147483647);
   }
 
   void test_from_long_long() {
-    // TODO
+    INTEGER obj = Integer::from(state, (long long)13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int) 13);
+
+    obj = Integer::from(state, (long long)-13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)-13);
+
+    obj = Integer::from(state, 214748364712331LL);
+    TS_ASSERT_EQUALS(as<Bignum>(obj)->to_long_long(), 214748364712331LL);
   }
 
   void test_from_unsigned_long_long() {
-    // TODO
+    INTEGER obj = Integer::from(state, (unsigned long long)13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int) 13);
+
+    obj = Integer::from(state, (unsigned long long)-13);
+    TS_ASSERT_EQUALS(obj->to_native(), (native_int)-13);
+
+    obj = Integer::from(state, 214748364712331ULL);
+    TS_ASSERT_EQUALS(as<Bignum>(obj)->to_ulong_long(), 214748364712331ULL);
   }
 };
