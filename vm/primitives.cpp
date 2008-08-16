59922203e4e6f299d2add6a88b33dce1896cc163
Eric Hodel <ehodel@engineyard.com>
2008-08-15 17:45:31 -0700

    Improve error message about primitive failures

--- vm/primitives.cpp
+++ vm/primitives.cpp
@@ -6,7 +6,7 @@ namespace rubinius {
   bool Primitives::unknown_primitive(STATE, VMExecutable* exec, Task* task, Message& msg) {
     std::cout << "\n";
     state->print_backtrace();
-    std::cout << "Called unbound/invalid primitive: " << *msg.name->to_str(state) <<"\n";
+    std::cout << "Called unbound or invalid primitive from: " << *msg.name->to_str(state) <<"\n";
     abort();
   }
 
