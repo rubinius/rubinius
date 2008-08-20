b54e225a10343a30b89656e2f6047eaf03f70b52
Ryan Davis <rdavis@engineyard.com>
2008-08-20 15:34:50 -0700

    YEEE HAW! nuked grammar

--- shotgun/lib/grammar.h
+++
@@ -1,5 +0,0 @@
-#include "shotgun/lib/grammar_internal.h"
-
-OBJECT syd_compile_string(STATE, const char *f, bstring s, int line, int newlines);
-OBJECT syd_compile_file(STATE, const char *f, FILE *file, int start, int newlines);
-quark id_to_quark(ID id);
--- shotgun/lib/grammar_runtime.h
+++
@@ -1,9 +0,0 @@
-#ifndef RBS_GRAMMAR_RUNTIME_H
-#define RBS_GRAMMAR_RUNTIME_H
-
-rb_parse_state *alloc_parse_state();
-void *pt_allocate(rb_parse_state *st, int size);
-void pt_free(rb_parse_state *st);
-void create_error(rb_parse_state *parse_state, char *msg);
-
-#endif /* RBS_GRAMMAR_RUNTIME_H */
