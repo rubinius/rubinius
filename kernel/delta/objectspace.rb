804c9d3006947d71077e3eb188d17e1d37d8c269
Eric Hodel <ehodel@engineyard.com>
2008-08-25 15:00:28 -0700

    Disable finalizers because threads don't work yet

--- kernel/delta/objectspace.rb
+++ kernel/delta/objectspace.rb
@@ -1,8 +1,9 @@
 module ObjectSpace
-  @thread = Thread.new do
-    loop do
-      Rubinius::ON_GC.receive
-      ObjectSpace.run_finalizers
-    end
-  end
+  # HACK disable finalizers
+  #@thread = Thread.new do
+  #  loop do
+  #    Rubinius::ON_GC.receive
+  #    ObjectSpace.run_finalizers
+  #  end
+  #end
 end
