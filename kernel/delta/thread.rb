899a37f8cba9c5e0e356f889bc1cccfe5a07804f
Eric Hodel <ehodel@engineyard.com>
2008-08-25 15:20:56 -0700

    HACK around create_weakref primitive not existing

--- kernel/delta/thread.rb
+++ kernel/delta/thread.rb
@@ -2,6 +2,6 @@ class Thread
   Thread.current.setup(true)
   Thread.initialize_main_thread(Thread.current)
   dg = ThreadGroup.new
-  ThreadGroup.const_set :Default, dg
-  dg.add Thread.current
+  Default = dg
+  #dg.add Thread.current
 end
