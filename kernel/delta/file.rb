6642717ee7f6acf10128fa22aa76bacdbeca5bdd
Eric Hodel <ehodel@engineyard.com>
2008-08-25 11:43:55 -0700

    No need for const_set now

--- kernel/delta/file.rb
+++ kernel/delta/file.rb
@@ -2,7 +2,7 @@ class File
   private_class_method :dirsep?, :next_path, :range, :name_match
 
   # these will be necessary when we run on Windows
-  const_set :DOSISH, RUBY_PLATFORM.match("mswin")
-  const_set :CASEFOLD_FILESYSTEM, DOSISH
-  const_set :FNM_SYSCASE, CASEFOLD_FILESYSTEM ? FNM_CASEFOLD : false
+  DOSISH = !!(RUBY_PLATFORM =~ /mswin/)
+  CASEFOLD_FILESYSTEM = DOSISH
+  FNM_SYSCASE = CASEFOLD_FILESYSTEM ? FNM_CASEFOLD : false
 end
