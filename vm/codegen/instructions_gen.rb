d345d2b10ab7c3e42b2df6ff3a66e8bcb13f65db
Wilson Bilkovich <wbilkovich@engineyard.com>
2008-08-28 13:09:44 -0400

    Zero out the stream of opcodes when setting up an instruction test

--- vm/codegen/instructions_gen.rb
+++ vm/codegen/instructions_gen.rb
@@ -199,6 +199,7 @@ void #{meth}() {
   // The += 0 disable unused variable warnings.
 
   opcode stream[100];
+  memset(stream, 0, sizeof(opcode) * 100);
   stream[0] = InstructionSequence::insn_#{ins.opcode};
 
 #define run(val) task->execute_stream(stream)
