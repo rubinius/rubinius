5f86f2c1d99294677294b2efc043e5a2460c1ade
Evan Phoenix <ephoenix@engineyard.com>
2008-08-28 01:22:17 -0700

    Load compiled_file.rb from the kernel

--- lib/compiler/mri_shim.rb
+++ lib/compiler/mri_shim.rb
@@ -11,7 +11,7 @@ $:.unshift File.dirname(__FILE__) + "/.."
 require 'compiler/compiler'
 require 'compiler/text'
 require 'compiler/marshal'
-require 'compiler/compiled_file'
+require 'kernel/delta/compiled_file'
 
 class SendSite
   def initialize(name)
