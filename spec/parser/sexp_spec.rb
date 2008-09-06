62c6036e6acba96a0d95bf94176973615dfda0d5
Ryan Davis <rdavis@engineyard.com>
2008-09-06 00:24:00 -0700

    Minor cleanup and doco

--- spec/parser/sexp_spec.rb
+++ spec/parser/sexp_spec.rb
@@ -3,9 +3,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 $: << 'lib'
 require 'pt_testcase'
 
-SEXP_EXPECTATIONS = ParseTreeTestCase.testcases
-
 class String
+
+  ##
+  # This form doesn't do the rubinius specific rewriting...  That's
+  # fine for now, we don't need to test that here as the compiler is
+  # all over that.
   def to_sexp
     require 'ruby_parser'
 
@@ -14,7 +17,7 @@ class String
 end
 
 describe "String#to_sexp" do
-  SEXP_EXPECTATIONS.each do |node, hash|
+  ParseTreeTestCase.testcases.each do |node, hash|
     next if Array === hash['Ruby']
 
     it "parses :#{node}" do
