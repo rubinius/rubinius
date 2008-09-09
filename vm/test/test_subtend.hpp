0e45966248f094c22224a9dd25dc2624c4c1461c
Eero Saynatkari <esaynatkari@engineyard.com>
2008-09-09 21:25:13 +0300

    Subtend spec uses Handles.

--- vm/test/test_subtend.hpp
+++ vm/test/test_subtend.hpp
@@ -7,6 +7,7 @@
 #include "vm.hpp"
 #include "message.hpp"
 #include "builtin/task.hpp"
+#include "builtin/fixnum.hpp"
 
 /* Testee */
 #include "vmnativemethod.hpp"
@@ -15,21 +16,22 @@
 static NativeMethodContext* hidden_context = NULL;
 static int hidden_given_int = 0;
 
+using namespace rubinius;
+
 extern "C" {
-  int dummy_function(int i)
+  HandleTo dummy_function(HandleTo obj)
   {
     hidden_context = NativeMethodContext::current();
-    hidden_given_int = i;
+    hidden_given_int = as<Fixnum>(obj->to_object())->to_native();
 
-    return i + i;
+    return new Handle(hidden_context->handles, Fixnum::from(hidden_given_int * 2));
   }
 }
 
-using namespace rubinius;
 
 class TestSubtend : public CxxTest::TestSuite
 {
-public:
+  public:
 
   VM* my_state;
 
@@ -50,16 +52,15 @@ public:
     Task* task = Task::create(my_state);
     Message message(my_state);
 
-    NativeMethod* method = NativeMethod::create(reinterpret_cast<NativeMethod::PointerTo>(&dummy_function));
+    NativeMethod* method = NativeMethod::create(&dummy_function);
 
     bool ret = method->execute(my_state, task, message);
 
     TS_ASSERT(true == ret);
 
     TS_ASSERT(NULL != hidden_context);
-    TS_ASSERT(0 != hidden_given_int);
 
     TS_ASSERT(5 == hidden_given_int);
-    TS_ASSERT(10 == hidden_context->return_value);
+    TS_ASSERT(10 == as<Fixnum>(hidden_context->return_value)->to_native());
   }
 };
