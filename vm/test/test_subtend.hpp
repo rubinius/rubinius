cf87e0d4d7818c4b952beb2f4211263f5bbf9bbe
Eero Saynatkari <esaynatkari@engineyard.com>
2008-09-09 19:07:21 +0300

    Subtend test updated to use a NativeMethod object.

--- vm/test/test_subtend.hpp
+++ vm/test/test_subtend.hpp
@@ -10,15 +10,18 @@
 
 /* Testee */
 #include "vmnativemethod.hpp"
+#include "builtin/nativemethod.hpp"
 
 static NativeMethodContext* hidden_context = NULL;
+static int hidden_given_int = 0;
 
 extern "C" {
-  void dummy_function()
+  int dummy_function(int i)
   {
     hidden_context = NativeMethodContext::current();
-    hidden_context->action = NativeMethodContext::RETURN_FROM_C;
-    return;
+    hidden_given_int = i;
+
+    return i + i;
   }
 }
 
@@ -35,20 +38,28 @@ public:
   }
 
   void tearDown() {
+    hidden_context = NULL;
+    hidden_given_int = 0;
     delete my_state;
   }
 
   /* Tests */
 
-  void test_ruby_to_c()
+  void test_ruby_to_c_call()
   {
     Task* task = Task::create(my_state);
     Message message(my_state);
 
-    bool ret = VMNativeMethod::executor(my_state, &dummy_function, task, &message);
+    NativeMethod* method = NativeMethod::create(reinterpret_cast<NativeMethod::PointerTo>(&dummy_function));
 
-    std::cout << ret << std::endl;
+    bool ret = method->execute(my_state, task, message);
+
     TS_ASSERT(true == ret);
+
     TS_ASSERT(NULL != hidden_context);
+    TS_ASSERT(0 != hidden_given_int);
+
+    TS_ASSERT(5 == hidden_given_int);
+    TS_ASSERT(10 == hidden_context->return_value);
   }
 };
