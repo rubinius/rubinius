6f9d2b4ac493315b91329582cc3da3394127edc7
Brian Ford <bford@engineyard.com>
2008-09-10 11:19:11 -0700

    Removed obsolete/never worked gc_collect_references primitive.

--- kernel/bootstrap/gc.rb
+++ kernel/bootstrap/gc.rb
@@ -2,14 +2,9 @@ class GC
   def self.start
     run(false)
   end
-    
+
   def self.run(tenure)
     Ruby.primitive :gc_start
-    raise PrimitiveFailure, "primitive failed"
-  end
-  
-  def self.collect_references(obj)
-    Ruby.primitive :gc_collect_references
     raise PrimitiveFailure, "primitive failed"
   end
 end
