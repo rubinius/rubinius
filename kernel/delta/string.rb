58d56b7e381a7741b8adf5643687fe545d72d975
Ryan Davis <rdavis@engineyard.com>
2008-09-09 17:34:18 -0700

    Moved LitRewriter to its own file in lib/compiler

--- kernel/delta/string.rb
+++ kernel/delta/string.rb
@@ -1,43 +1,3 @@
-# depends on: sexp_processor.rb sexp.rb
-
-module Rubinius; end
-class Rubinius::LitRewriter < SexpProcessor # TODO: move
-  # HACK: until we figger out how PT should be
-  def rewrite_dregx(exp)
-    exp << 0 unless Integer === exp.last
-    exp
-  end
-
-  # HACK: until we figger out how PT should be
-  def rewrite_dregx_once(exp)
-    exp << 0 unless Integer === exp.last
-    exp
-  end
-
-  def rewrite_lit(exp)
-    raise "wtf: #{exp.inspect}" unless exp.size == 2
-    exp.shift
-    value = exp.shift
-    case value
-    when Float then
-      s(:float, value)
-    when Integer then
-      s(:fixnum, value) # TODO: rename :int
-    when Range then
-      l = rewrite_lit(s(:lit, value.begin))
-      r = rewrite_lit(s(:lit, value.end))
-      t = value.exclude_end? ? :dot3 : :dot2
-      s(t, l, r)
-    when Regexp then
-      s(:regex, value.source, value.options)
-    when Symbol then
-      s(:lit, value)
-    else
-      raise "unhandled :lit type #{value.inspect}"
-    end
-  end
-end
-
 class String
   BASE_64_A2B = {}
   (?A..?Z).each {|x| BASE_64_A2B[x] = x - ?A}
@@ -49,6 +9,7 @@ class String
 
   def to_sexp(name="(eval)") # TODO: maybe move into lib/compiler and after_load
     require 'ruby_parser'
+    require 'compiler/lit_rewriter'
 
     sexp = RubyParser.new.process(self, name)
     sexp = Rubinius::LitRewriter.new.process(sexp)
