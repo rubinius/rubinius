8a19d1f471b3f458b33aa2b6b34dcbe434ac72a9
Eero Saynatkari <esaynatkari@engineyard.com>
2008-09-12 02:44:36 +0300

    NativeMethod handles different underlying function signatures. Arities.

--- vm/builtin/nativemethod.hpp
+++ vm/builtin/nativemethod.hpp
@@ -14,6 +14,32 @@
 namespace rubinius {
 
   /**
+   *  The various special method arities from a
+   *  C method. If not one of these, then the
+   *  number denotes the exact number of args
+   *  instead.
+   */
+  enum Arity {
+    ARGS_IN_RUBY_ARRAY = -3,
+    RECEIVER_PLUS_ARGS_IN_RUBY_ARRAY = -2,
+    ARG_COUNT_ARGS_IN_C_ARRAY_PLUS_RECEIVER = -1
+  };
+
+
+  /* The various function signatures needed since C++ requires strict typing here. */
+
+  /** Generic function pointer used to store any type of functor. */
+  typedef     void (*GenericFunctor)(void);
+
+  /* Actual functor types. */
+
+  typedef HandleTo (*ArgcFunctor)(int, HandleTo*, HandleTo);
+  typedef HandleTo (*OneArgFunctor)(HandleTo);
+  typedef HandleTo (*TwoArgFunctor)(HandleTo, HandleTo);
+  typedef HandleTo (*ThreeArgFunctor)(HandleTo, HandleTo, HandleTo);
+
+
+  /**
    *  Ruby side of a NativeMethod, i.e. a C-implemented method.
    *
    *  The possible signatures for the C function are:
@@ -28,35 +54,36 @@ namespace rubinius {
    */
   class NativeMethod : public Executable
   {
-    public:   /* Types */
-
-      typedef HandleTo (*PointerTo)(HandleTo);
-
-
     public:   /* Ctors */
 
-      static NativeMethod* create(PointerTo method)
+      template <typename FunctorType>
+      static NativeMethod* create(FunctorType functor, int arity)
       {
         NativeMethod* obj = new NativeMethod();
-        obj->my_method = method;
+        obj->my_functor = reinterpret_cast<GenericFunctor>(functor);
+        obj->my_arity = Fixnum::from(arity);
 
         return obj;
       }
 
     public:   /* Interface */
 
-      bool execute(VM* state, Task* task, Message& message)
-      {
-        return VMNativeMethod::execute(state, task, &message, this);
-      }
+             bool   execute(VM* state, Task* task, Message& message) {
+                      return VMNativeMethod::execute(state, task, &message, this);
+                    }
 
-      PointerTo actual_function_object() const { return my_method; }
+                    template <typename FunctorType>
+      FunctorType   functor_as() const { return reinterpret_cast<FunctorType>(my_functor); }
+                    /** Arity of the method. @see Arity. */
+     const Fixnum*  arity() const { return my_arity; }
 
     public:   /* Ruby slots and bookkeeping */
 
+
     private:  /* Instance vars */
 
-      PointerTo  my_method;     /**< Function object that implements this method. */
+             Fixnum*  my_arity;         /**< Arity of the method. @see Arity */
+     GenericFunctor   my_functor;       /**< Function object that implements this method. */
   };
 
 }
