8a87bf598c00e5b8cc406226c8afd9dc1ab5026d
Eero Saynatkari <esaynatkari@engineyard.com>
2008-09-13 16:33:08 +0300

    Remove the old .c and .h files that were converted to .cpp and .hpp.

--- vm/strlcat.h
+++
@@ -1,8 +0,0 @@
-#ifndef RBS_STRLCAT_H
-#define RBS_STRLCAT_H
-
-#ifndef HAVE_STRLCAT
-size_t strlcat (char *dst, const char *src, size_t siz);
-#endif
-
-#endif
--- vm/strlcpy.h
+++
@@ -1,8 +0,0 @@
-#ifndef RBS_STRLCPY_H
-#define RBS_STRLCPY_H
-
-#ifndef HAVE_STRLCPY
-size_t strlcpy (char *dst, const char *src, size_t siz);
-#endif
-
-#endif
