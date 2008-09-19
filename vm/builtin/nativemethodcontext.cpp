c7d40aeb63854941d8d87362a79987e33338be6d
Eero Saynatkari <esaynatkari@engineyard.com>
2008-09-19 19:56:28 +0300

    NativeMethodContext updated for form and style.

--- vm/builtin/nativemethodcontext.cpp
+++ vm/builtin/nativemethodcontext.cpp
@@ -9,6 +9,29 @@
 
 namespace rubinius {
 
+
+/* Handle */
+
+
+  /* Instance methods */
+
+  Handle::Handle() : my_index(0), my_storage(NULL)
+  {
+    /* Nada */
+  }
+
+  Handle::Handle(const Handle& other) : my_index(other.my_index), my_storage(other.my_storage) {}
+
+  Object* Handle::object()
+  {
+    return (*my_storage)[my_index];
+  }
+
+
+
+/* NativeMethodContext */
+
+
   /**
    *  Currently active NativeMethodContext access.
    *
@@ -16,54 +39,43 @@ namespace rubinius {
    */
   static NativeMethodContext* hidden_current_native_context = NULL;
 
-  /**
-   *  Initialise a new context.
-   *
-   *  This constructor is always hidden behind NativeMethodContext.create().
-   */
-  NativeMethodContext::NativeMethodContext(VM* state, Message* message, Task* task, NativeMethod* method)
-    : MethodContext()
-    , my_action(ORIGINAL_CALL)
-    , my_c_call_point()
-    , my_dispatch_point()
-    , my_message(message)
-    , my_method(method)
-    , my_handles()
-    , my_return_value(Qnil)
-    , my_state(state)
-    , my_task(task)
+
+  /* Class methods */
+
+  void NativeMethodContext::register_class_with(VM* state)
   {
-    /* Setting up the inherited stuff that we will actually use and others will need. */
-    this->sender(my_state, my_task->active());
-    this->home(my_state, const_cast<NativeMethodContext*>(this));
-
-    this->self(my_state, my_message->recv);
-
-    this->module(my_state, my_message->module);
-
-    this->name(my_state, my_message->name);
-
-//    my_stack_size = DEFAULT_STACK_SIZE;
-//    my_stack = new char[my_stack_size];
-    stack_size = DEFAULT_STACK_SIZE;
+    state->globals.nativectx.set(state->new_class("NativeMethodContext", state->globals.methctx.get()));
+    state->globals.nativectx.get()->set_object_type(state, NContextType);
   }
 
-  /**
-   *  Brand new context for a brand new call.
-   *
-   *  TODO:   Cache these.
-   */
-  NativeMethodContext* NativeMethodContext::create(VM* state, Message* msg, Task* task, NativeMethod* method)
+  NativeMethodContext* NativeMethodContext::create(VM* state,
+                                                   Message* msg = NULL,
+                                                   Task* task = as<Task>(Qnil),
+                                                   NativeMethod* method = reinterpret_cast<NativeMethod*>(Qnil)) /* TODO: wtf? */
   {
-    /* Using placement new here as an experiment.
-     *
-     * TODO: OM should be changed to call destructor
-     * when using placement new.
-     *
-     * TODO: Why new_struct? What struct?
-     */
-    void* object = state->new_struct( G(nativectx), static_cast<std::size_t>(sizeof(NativeMethodContext) + DEFAULT_STACK_SIZE) );
-    NativeMethodContext* nmc = new (object) NativeMethodContext(state, msg, task, method);
+    static std::size_t context_size = sizeof(NativeMethodContext) + DEFAULT_STACK_SIZE;
+
+    NativeMethodContext* nmc = static_cast<NativeMethodContext*>(state->new_struct(G(nativectx), context_size));
+
+    /* MethodContext stuff. */
+    nmc->sender(state, task->active());
+    nmc->home(state, const_cast<NativeMethodContext*>(nmc));
+    nmc->self(state, msg->recv);
+    nmc->module(state, msg->module);
+    nmc->name(state, as<Object>(msg->name));
+
+    /* Instead of storing the memory within as MethodContexts, we heap-allocate. */
+    nmc->stack_size = DEFAULT_STACK_SIZE;
+
+    nmc->my_stack = static_cast<void*>(new char[nmc->stack_size]);
+
+    nmc->my_action = ORIGINAL_CALL;
+    nmc->my_message = msg;
+    nmc->my_method = method;
+    nmc->my_handles = new HandleStorage();
+    nmc->my_return_value = as<Object>(Qnil);
+    nmc->my_state = state;
+    nmc->my_task = task;
 
     /* TODO: Make active here. */
 
@@ -71,33 +83,23 @@ namespace rubinius {
     return nmc;
   }
 
-  /**
-   *  Set the currently active NativeMethodContext.
-   */
+  NativeMethodContext* NativeMethodContext::allocate(VM* state)
+  {
+    return create(state);
+  }
+
   void NativeMethodContext::current_context_is(NativeMethodContext* context)
   {
     hidden_current_native_context = context;
   }
 
-  /**
-   *  Access the currently active NativeMethodContext.
-   *
-   *  TODO:   Exception handling.
-   *  TODO:   Use reference.
-   */
   NativeMethodContext* NativeMethodContext::current()
   {
      return hidden_current_native_context;
   }
 
-  /**
-   *  Register this class.
-   *
-   *  Only ever called in bootstrapping.
-   */
-  void NativeMethodContext::register_class_with(VM* state) {
-    state->globals.nativectx.set(state->new_class("NativeMethodContext", state->globals.methctx.get()));
-    state->globals.nativectx.get()->set_object_type(state, NativeMethodContextType);
-  }
+
+  /* Instance methods */
+
 }
 
--- vm/builtin/nativemethodcontext.hpp
+++ vm/builtin/nativemethodcontext.hpp
@@ -4,12 +4,11 @@
 /* Std */
 #include <vector>
 
-/* Sys */
-#include <ucontext.h>
-
 /* Project */
 #include "type_info.hpp"
 #include "builtin/contexts.hpp"
+
+#include "vmnativemethod.hpp"
 
 
 namespace rubinius {
@@ -39,68 +38,91 @@ namespace rubinius {
    *  TODO: Add tests.
    *  TODO: Avoid extra work on Qnil/Qtrue/Qfalse.
    */
-  class Handle
-  {
-    public:   /* Ctors */
+  class Handle {
+  public:   /* Ctors */
 
-      Handle() : my_index(0), my_storage(NULL) {}
+    /** Blank creation needed by HandleStorages. */
+    Handle();
 
-      template <typename T>
-        Handle(HandleStorage& storage, T* obj)
-          : my_index(0)
-          , my_storage(&storage)
-        {
-          my_storage->push_back(obj);
-          my_index = my_storage->size() - 1;
-        }
+    /**
+     *  Normal creation to associate any Object subclass.
+     *
+     *  Objects always stored as Objects, so user is resposible
+     *  for casting to the correct type when needed.
+     */
+    template <typename T>
+      Handle::Handle(HandleStorage& storage, T* obj)
+        : my_index(0)
+        , my_storage(&storage)
+      {
+        my_storage->push_back(as<Object>(obj));
+        my_index = my_storage->size() - 1;
+      }
 
-      Handle(const Handle& other)
-        : my_index(other.my_index)
-        , my_storage(other.my_storage)
-      {}
-
-
-    public:   /* Interface */
-
-                /** Explicitly retrieve the contained object. */
-       Object*  object() { return (*my_storage)[my_index]; }
+    /** Copy construction semantics for Handle locals. */
+    Handle(const Handle& other);
 
 
-    private:  /* Instance vars */
+  public:   /* Interface */
 
-      std::size_t     my_index;     /**< Index into storage to retrieve Object. */
-      HandleStorage*  my_storage;   /**< Storage of actual object information. */
+    /** Explicitly retrieve the contained object. */
+    Object*  object();
+
+
+  private:  /* Instance vars */
+
+    std::size_t     my_index;     /**< Index into storage to retrieve Object. */
+    HandleStorage*  my_storage;   /**< Storage of actual object information. */
   };
 
+  /** More prosaic name for Handles. */
   typedef Handle HandleTo;
 
+
   /**
-   *  Specialised version of as() to access a Handle's actual object.
-   *
-   *  @see  vm/object.hpp for normal version.
-   */
+  *  Specialised version of as() to access a Handle's actual object.
+  *
+  *  @see  vm/object.hpp for normal version.
+  */
   template <typename T>
-    T* as(HandleTo handle) { return as<T>(handle.object()); }
+    T* as(HandleTo handle)
+    {
+      return as<T>(handle.object());
+    }
+
 
   /**
-   *  Specialised version of try_as() to access a Handle's actual object.
-   *
-   *  @see  vm/object.hpp for normal version.
-   */
+  *  Specialised version of try_as() to access a Handle's actual object.
+  *
+  *  @see  vm/object.hpp for normal version.
+  */
   template <typename T>
-    T* try_as(HandleTo handle) { return try_as<T>(handle.object()); }
+    T* try_as(HandleTo handle)
+    {
+      return try_as<T>(handle.object());
+    }
 
 
   /**
-   *  Method context for C-implemented methods.
-   *
-   *  TODO: Subclassing MethodContext is fucking ugly.
-   *        Create a Context superclass for both.
-   *
-   *  TODO: Add tests.
-   */
+  *  Method context for C-implemented methods.
+  *
+  *  TODO: Subclassing MethodContext is fucking ugly.
+  *        Create a Context superclass for both.
+  *
+  *  TODO: Add tests.
+  */
   class NativeMethodContext : public MethodContext {
-    public:   /* Types */
+  public:   /* Slots and bookkeeping. */
+
+    /** Dynamically calculated size. (TODO) */
+    const static std::size_t fields = 0;
+    const static object_type type = NContextType;
+
+    /** Add the class object to the object space. */
+    static void register_class_with(VM* state);
+
+
+  public:   /* Types */
 
     /**
      *  Control flow requests.
@@ -108,10 +130,8 @@ namespace rubinius {
      *  Due to the unusual program flow here, the context will
      *  explicitly contain the action that the dispatch code
      *  should perform for this particular context/call.
-     *
      */
-    enum Action
-    {
+    enum Action {
       UNDEFINED,
       ORIGINAL_CALL,
       CALL_FROM_C,
@@ -119,144 +139,81 @@ namespace rubinius {
     };
 
 
-    public:   /* Constants */
+  public:   /* Constants */
 
-                                  /** 64KiB -- Do NOT decrease this, ucontext will fail. */
-       static const std::size_t   DEFAULT_STACK_SIZE = 1024 * 64;
-
-
-    public:   /* Ctors */
-
-                                  /**
-                                   *  Brand new context for a brand new call.
-                                   *
-                                   *  The C stack is allocated *inside* the context.
-                                   */
-    static NativeMethodContext*   create(VM* state, Message* message, Task* task, NativeMethod* method);
+    /** 64KiB -- Do NOT decrease this, ucontext will fail. */
+    static const std::size_t DEFAULT_STACK_SIZE = 1024 * 64;
 
 
-    public:   /* Interface */
+  public:   /* Ctors */
 
-                                  /** Record a NativeMethodContext as the currently active one. */
-                   static void    current_context_is(NativeMethodContext* context);
-                                  /** Access currently active NativeMethodContext. */
-    static NativeMethodContext*   current();
+    /** Create and populate context for a new C call. */
+    static NativeMethodContext* create(VM* state, Message* message, Task* task, NativeMethod* method);
 
-
-    public:   /* Accessors */
-
-      Action action() { return my_action; }
-      NativeMethod* method() { return my_method; }
-      HandleStorage& handles() { return my_handles; }
-      Message* message() { return my_message; }
-
-      VM* state() { return my_state; }
-
-      void action(Action action) { my_action = action; }
-      void return_value(Object* obj) { my_return_value = obj; }
-      Object* return_value() { return my_return_value; }
-
-      std::size_t stacksize() { return stack_size; }
-      void* stack() { return static_cast<void*>(stk); }
-
-      ucontext_t* c_call_point() { return &my_c_call_point; }
-      ucontext_t* dispatch_point() { return &my_dispatch_point; }
-
-    private:  /* Hidden stuff. */
-
-                                  /** Constructor allocates the NMC, hidden behind ::create(). */
-                                  NativeMethodContext(VM* state, Message* message, Task* task, NativeMethod* method);
+    /** Allocate a functional but empty context. */
+    static NativeMethodContext* allocate(VM* state);
 
 
+  public:   /* Accessors */
 
-    public:   /* Bootstrapping */
+    VM* state() { return my_state; }
 
-                                  /** Add the class object to the object space. */
-                   static void    register_class_with(VM* state);
+    Action action() { return my_action; }
+    void action(Action action) { my_action = action; }
 
+    NativeMethod* method() { return my_method; }
+    HandleStorage& handles() { return *my_handles; }
+    Message* message() { return my_message; }
 
-    public:   /* Slots and bookkeeping. */
+    void return_value(Object* obj) { my_return_value = obj; }
+    Object* return_value() { return my_return_value; }
 
-        const static size_t   fields  = 0;      // TODO: Check this
-    const static object_type  type    = NativeMethodContextType;
+    std::size_t stacksize() { return stack_size; }
+    void* stack() { return my_stack; }
 
-
-    /*  OK, these are the slots we get from MethodContext (and all we really need.)
-
-     *  MethodContext* sender_; // slot
-     *  MethodContext* home_;   // slot
-     *  OBJECT self_;           // slot
-
-     *  CompiledMethod* cm_;    // slot
-
-     *  Module* module_;        // slot
-     *  OBJECT block_;          // slot
-     *  OBJECT name_;           // slot
-     */
+    ExecutionPoint c_call_point() { return &my_c_call_point; }
+    ExecutionPoint dispatch_point() { return &my_dispatch_point; }
 
 
-    public:  /* Instance vars */
+  public:   /* Interface */
 
-    /*  These are the inherited ivars, of which the two last are
-     *  useful for our purposes. We are going to abuse the hell
-     *  out of the "array": the object is allocated the extra
-     *  memory at the end, so the variable just works as a pointer
-     *  to that region.
-     *
-     *  VMMethod* vmm;
+    /** Record a NativeMethodContext as the currently active one. */
+    static void current_context_is(NativeMethodContext* context);
 
-     *  struct jit_state js;
-     *  int    ip;
-     *  size_t args;
+    /** Access currently active NativeMethodContext. */
+    static NativeMethodContext* current();
 
-     *  size_t stack_size;
-     *
-     *  OBJECT stk[];
-     */
 
-    /*
-     *  OS X 10.5's ucontext implementation is broken and requires
-     *  that we manually set up the additional mcontext fields so
-     *  that the ucontext_t's are sized up correctly. Otherwise they
-     *  get written over :/
-     */
+  public:  /* Instance vars */
 
-    /* TODO: Make this an actual conditional and stuff. */
+    /* We are ignoring MethodContext's stk */
 
-#ifndef HAS_UCONTEXT
-  #define HAS_UCONTEXT
-#endif
+    /** Execution point where actual function call made (Subtend stack.) */
+    DECLARE_POINT_VARIABLE(my_c_call_point);
 
-    Action          my_action;            /**< Action requested to be performed. */
-    ucontext_t      my_c_call_point;      /**< Point to execute actual C dispatch (subtend stack) */
-#if defined(__APPLE__) && defined(HAS_UCONTEXT)
-  _STRUCT_MCONTEXT  __my_c_call_point_mc;
-#endif
-    ucontext_t      my_dispatch_point;    /**< Point of return to dispatch code (vm stack) */
-#if defined(__APPLE__) && defined(HAS_UCONTEXT)
-  _STRUCT_MCONTEXT  __my_dispatch_point_mc;
-#endif
+    /** Execution point to return to for further handling (VM stack.) */
+    DECLARE_POINT_VARIABLE(my_dispatch_point);
+
+    Action          my_action;            /**< Action for the VMNativeMethod to perform. */
     Message*        my_message;           /**< Message representing this call. */
     NativeMethod*   my_method;            /**< Function-like object that actually implements the method. */
-    HandleStorage   my_handles;           /**< Object handles for this call. */
+    HandleStorage*  my_handles;           /**< Object handles for this call. */
     Object*         my_return_value;      /**< Return value from the call. */
-//    char*           my_stack;
-//    std::size_t     my_stack_size;
+    void*           my_stack;             /**< Stack for executing the C method. */
     VM*             my_state;             /**< VM state for this invocation. */
     Task*           my_task;              /**< Task in which we are running. */
 
 
   public:
 
-    /**
-     *  Generated type information.
-     */
+    /** Generated type information. */
     class Info : public MethodContext::Info
     {
-      public:
+    public:
       BASIC_TYPEINFO(MethodContext::Info)
     };
-  };
+
+  };    /* NativeMethodContext */
 
 }
 
