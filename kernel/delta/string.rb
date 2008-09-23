e31ecf5c5d166ce815a48deeb58c9d9796fd2fdc
Ryan Davis <rdavis@engineyard.com>
2008-09-23 15:10:40 -0700

    Added lit_writer as an option

--- kernel/delta/string.rb
+++ kernel/delta/string.rb
@@ -8,7 +8,8 @@ class String
   BASE_64_A2B[?=]  = 0
 
   # TODO - Pass the starting line info into RubyParser
-  def to_sexp(name="(eval)", line = 1) # TODO: maybe move into lib/compiler and after_load
+  # TODO: maybe move into lib/compiler and after_load
+  def to_sexp(name="(eval)", line = 1, lit_rewriter=true)
     # TODO: for Ryan - fix the paths in compiler/ruby_parser and friends,
     # e.g. require 'sexp' should be require 'compiler/sexp'
     $: << "lib/compiler"
@@ -16,7 +17,7 @@ class String
     require 'compiler/lit_rewriter'
 
     sexp = RubyParser.new.process(self, name)
-    sexp = Rubinius::LitRewriter.new.process(sexp)
+    sexp = Rubinius::LitRewriter.new.process(sexp) if lit_rewriter
     sexp
   end
 end
