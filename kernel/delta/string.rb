c919f2f2e9938e9dbbfcb8442dde413a92ec6d77
Eric Hodel <ehodel@engineyard.com>
2008-09-30 13:52:33 -0700

    Don't add lib/compiler to load path repeatedly

--- kernel/delta/string.rb
+++ kernel/delta/string.rb
@@ -15,9 +15,11 @@ class String
     $: << "lib/compiler"
     require 'compiler/ruby_parser'
     require 'compiler/lit_rewriter'
+    $:.pop
 
     sexp = RubyParser.new.process(self, name)
     sexp = Rubinius::LitRewriter.new.process(sexp) if lit_rewriter
     sexp
   end
+
 end
