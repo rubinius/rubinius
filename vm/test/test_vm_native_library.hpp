036626b32ea5cb753c34bd54e1918829e3b93343
Eero Saynatkari <esaynatkari@engineyard.com>
2008-10-12 19:40:48 +0300

    Update NativeLibrary tests to reflect better behaviour.

--- vm/test/test_vm_native_library.hpp
+++ vm/test/test_vm_native_library.hpp
@@ -1,5 +1,7 @@
 #include "native_libraries.hpp"
 
+#include "builtin/exception.hpp"
+
 #include <cxxtest/TestSuite.h>
 
 using namespace rubinius;
@@ -8,40 +10,42 @@ using namespace rubinius;
 class TestNativeLibrary : public CxxTest::TestSuite {
   public:
 
-  VM *state;
+  VM *state_;
 
   void setUp() {
-    state = new VM();
+    state_ = new VM();
   }
 
   void tearDown() {
-    delete state;
+    delete state_;
   }
 
   void test_find_symbol_default() {
     void* ep;
-    String* name1 = String::create(state, "_blah_not_here");
-    String* name2 = String::create(state, "strlen");
+    String* name1 = String::create(state_, "_blah_not_here");
+    String* name2 = String::create(state_, "strlen");
 
-    ep = NativeLibrary::find_symbol(name1, Qnil);
-    TS_ASSERT(!ep);
+    TS_ASSERT_THROWS(NativeLibrary::find_symbol(state_, name1, Qnil),
+                     RubyException);
 
-    ep = NativeLibrary::find_symbol(name2, Qnil);
+    ep = NativeLibrary::find_symbol(state_, name2, Qnil);
     TS_ASSERT(ep);
   }
 
   void test_find_symbol_in_library() {
     void* ep;
-    String* lib1 = String::create(state, "blah");
-    String* lib2 = String::create(state, "libc");
-    String* name = String::create(state, "strlen");
+    String* lib1 = String::create(state_, "blah");
+    String* lib2 = String::create(state_, "libc");
+    String* name = String::create(state_, "strlen");
 
-    ep = NativeLibrary::find_symbol(name, lib1);
-    TS_ASSERT(!ep);
+
+    TS_ASSERT_THROWS(NativeLibrary::find_symbol(state_, name, lib1),
+                     RubyException);
 
     // TODO:  This is probably failing on Linux because libc is already loaded
     //        so dlopen is returning NULL.
-    ep = NativeLibrary::find_symbol(name, lib2);
+    ep = NativeLibrary::find_symbol(state_, name, lib2);
     TS_ASSERT(ep);
   }
+
 };
