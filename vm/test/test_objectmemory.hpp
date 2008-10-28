e4c7d3a74ea8b6ed39529fc3b5affa988495cd32
Evan Phoenix <ephoenix@engineyard.com>
2008-10-28 12:12:23 -0700

    Add test for XMALLOC/GC behavior

--- vm/test/test_objectmemory.hpp
+++ vm/test/test_objectmemory.hpp
@@ -560,5 +560,16 @@ class TestObjectMemory : public CxxTest::TestSuite {
     TS_ASSERT(state->om->contexts.scan <= state->om->contexts.current);
   }
 
+  void test_xmalloc_causes_gc() {
+    // Knows that the threshold is 10M
+    state->interrupts.check = false;
+    void* ptr = XMALLOC(1000000);
+    XFREE(ptr);
+    TS_ASSERT_EQUALS(state->interrupts.check, false);
+    ptr = XMALLOC(9000001);
+    TS_ASSERT_EQUALS(state->interrupts.check, true);
+    XFREE(ptr);
+  }
+
 };
 
