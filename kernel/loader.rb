e43f3c8a0af416c0a3b76f7baf712e3f25de5122
Eric Hodel <ehodel@engineyard.com>
2008-10-27 17:15:00 -0700

    Expand 'lib' in case the current directory changes

--- kernel/loader.rb
+++ kernel/loader.rb
@@ -37,7 +37,7 @@ end
 # Setup $LOAD_PATH.
 
 additions = []
-additions << "lib"
+additions << File.expand_path("lib")
 
 # The main stdlib location
 # HACK todo remove this comment when we're setting this constant in the VM
