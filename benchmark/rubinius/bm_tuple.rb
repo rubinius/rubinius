43a8ff03f3850ddaf9e1f8215e85541a71b2a040
Charles Comstock <dgtized@gmail.com>
2008-10-29 02:34:00 -0500

    added Tuple#to_a benchmark to bm_tuple

--- benchmark/rubinius/bm_tuple.rb
+++ benchmark/rubinius/bm_tuple.rb
@@ -2,6 +2,8 @@ require 'benchmark'
  
 total = (ENV['TOTAL'] || 5_000).to_i
 
+Tuple = Array unless defined?(Tuple)
+
 Benchmark.bmbm do |x|
   x.report 'empty' do
     total.times {|i| Tuple.new(1) }
@@ -13,6 +15,12 @@ Benchmark.bmbm do |x|
     end
   end
 
+  x.report 'Tuple#to_a' do
+    total.times do |i|
+      Tuple.new(i).to_a
+    end
+  end
+  
   x.report 'Tuple#+' do
     total.times do |i|
       Tuple.new(i) + Tuple.new(i)
