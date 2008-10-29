33c41313796420b96f0ad08ea7cfb8059e28ef6c
Charles Comstock <dgtized@gmail.com>
2008-10-29 01:17:30 -0500

    some basic array method benchmarks

---
+++ benchmark/rubinius/bm_array.rb
@@ -0,0 +1,47 @@
+require 'benchmark'
+ 
+total = (ENV['TOTAL'] || 5_000).to_i
+
+array = Array.new(total) {|i| i}
+
+Benchmark.bmbm do |x|
+  x.report 'empty' do
+    total.times {|i| i }
+  end
+
+  x.report 'Array#initialize' do
+    total.times do |i|
+      Array.new(i,0)
+    end
+  end
+
+  x.report 'Array#initialize {|i| i}' do
+    total.times do |i|
+      Array.new(i) {|i| i}
+    end
+  end
+  
+  x.report 'Array#include?' do
+    total.times do |i|
+      array.include?(i)
+    end
+  end
+
+  x.report 'Array#index' do
+    total.times do |i|
+      array.index(i)
+    end
+  end
+
+  x.report 'Array#rindex' do
+    total.times do |i|
+      array.rindex(i)
+    end
+  end
+  
+  x.report 'Array#reverse' do
+    total.times do |i|
+      array.reverse
+    end
+  end
+end
