62323291bdb9c758651932e726b4c79480ff29e4
Charles Comstock <dgtized@gmail.com>
2008-10-29 00:09:23 -0500

    renamed bm_array_each to bm_compare_each and added ByteArray and string benchmarks

---
+++ benchmark/rubinius/bm_compare_each.rb
@@ -0,0 +1,28 @@
+require 'benchmark'
+
+total = (ENV['TOTAL'] || 1_000).to_i
+size = 5_000
+
+ByteArray = Array unless defined?(ByteArray)
+
+array = Array.new(size) { |i| i }
+byte_array = ByteArray.new(size)
+string = array.join
+
+Benchmark.bmbm do |x|
+  x.report("empty loop") do
+    total.times { |i| i }
+  end
+
+  x.report("ByteArray#each") do
+    total.times { |i| byte_array.each { |a| a } }
+  end
+
+  x.report("String#each") do
+    total.times { |i| string.each {|a| a} }
+  end
+  
+  x.report("Array#each") do
+    total.times { |i| array.each { |a| a } }
+  end
+end
