fe521b1d0a93117e0ca6431e343503eb021d9481
Eero Saynatkari <esaynatkari@engineyard.com>
2008-10-30 02:04:39 +0200

    Add old timeout.rb back.

--- lib/timeout.rb
+++ lib/timeout.rb
@@ -1,89 +1,188 @@
+#--
+# = timeout.rb
 #
-# = Timeout, timeout.rb
+# execution timeout
 #
-# Rubinius' implementation of the Ruby Stdlib Timeout API.
+# = Copyright
 #
-
-
-
+# Copyright - (C) 2008  Evan Phoenix
+# Copyright:: (C) 2000  Network Applied Communication Laboratory, Inc.
+# Copyright:: (C) 2000  Information-technology Promotion Agency, Japan
 #
-# Hard time limit for an arbitrary operation.
+#++
 #
-# The provided block of code is executed in a new Thread. If the
-# block has not returned within the given number of seconds, the
-# Thread it runs in is terminated by killing it.
+# = Description
 #
-# On success, true is returned. Otherwise, an error is raised in
-# the Thread in which the call to Timeout was made. The error is
-# either of the type supplied or, by default, Timeout::Error.
+# A way of performing a potentially long-running operation in a thread, and
+# terminating it's execution if it hasn't finished within fixed amount of
+# time.
 #
-# The timeout may either be called directly through the module:
+# Previous versions of timeout didn't use a module for namespace. This version
+# provides both Timeout.timeout, and a backwards-compatible #timeout.
 #
+# = Synopsis
 #
-#   Timeout.timeout(60 * 30) {
-#     Fun.start
+#   require 'timeout'
+#   status = Timeout::timeout(5) {
+#     # Something that should be interrupted if it takes too much time...
 #   }
 #
-#
-# ...or through an includable instance method:
-#
-#
-#   module Fun
-#     include Timeout
-#
-#     def self.play_games(game)
-#       timeout(60 * 30) { start }
-#
-#     rescue Timeout::Error
-#       $stdout.puts i
-#     end
-#
-#   end
-#
+
+require 'thread'
+
 module Timeout
 
-  #
-  # Simple error class with distinct type.
-  #
-  class Error < Interrupt; end
+  ##
+  # Raised by Timeout#timeout when the block times out.
 
+  class Error<Interrupt
+  end
 
-  #
-  # Hard time limit for an arbitrary operation.
-  #
-  # Time in seconds after which the block is killed, and an
-  # optional error class argument (defaults to Timeout::Error.)
-  #
-  # @todo   This is currently a blocking call in the calling
-  #         thread. --rue
-  #
-  def timeout(in_this_many_seconds, error = Timeout::Error, &block)
-    raise ThreadError, "Attempted during critical!" if Thread.critical
+  # A mutex to protect @requests
+  @mutex = Mutex.new
 
-    t = Thread.new &block
+  # All the outstanding TimeoutRequests
+  @requests = []
 
-    unless t.join in_this_many_seconds
-      t.kill
-      raise error, "Task #{t} not completed within #{in_this_many_seconds}s!"
+  # Represents +thr+ asking for it to be timeout at in +secs+
+  # seconds. At timeout, raise +exc+.
+  class TimeoutRequest
+    def initialize(secs, thr, exc)
+      @left = secs
+      @thread = thr
+      @exception = exc
     end
 
-    t.value
+    attr_reader :thread, :left
+
+    # Called because +time+ seconds have gone by. Returns
+    # true if the request has no more time left to run.
+    def elapsed(time)
+      @left -= time
+      @left <= 0
+    end
+
+    # Raise @exception if @thread.
+    def cancel
+      if @thread and @thread.alive?
+        @thread.raise @exception, "execution expired"
+      end
+
+      @left = 0
+    end
+
+    # Abort this request, ie, we don't care about tracking
+    # the thread anymore.
+    def abort
+      @thread = nil
+      @left = 0
+    end
+  end
+
+  def self.add_timeout(time, exc)
+
+    @controller ||= Thread.new do
+      while true
+        if @requests.empty?
+          sleep
+          next
+        end
+
+        min = nil
+
+        @mutex.synchronize do
+          min = @requests.min { |a,b| a.left <=> b.left }
+        end
+
+        slept_for = sleep(min.left)
+
+        @mutex.synchronize do
+          @requests.delete_if do |r|
+            if r.elapsed(slept_for)
+              r.cancel
+              true
+            else
+              false
+            end
+          end
+        end
+
+      end
+    end
+
+    req = TimeoutRequest.new(time, Thread.current, exc)
+
+    @mutex.synchronize do
+      @requests << req
+    end
+
+    @controller.run
+
+    return req
+  end
+
+  ##
+  # Executes the method's block. If the block execution terminates before +sec+
+  # seconds has passed, it returns true. If not, it terminates the execution
+  # and raises +exception+ (which defaults to Timeout::Error).
+  #
+  # Note that this is both a method of module Timeout, so you can 'include
+  # Timeout' into your classes so they have a #timeout method, as well as a
+  # module method, so you can call it directly as Timeout.timeout().
+  
+  def timeout(sec, exception=Error)
+    return yield if sec == nil or sec.zero?
+    raise ThreadError, "timeout within critical session" if Thread.critical
+
+    req = Timeout.add_timeout sec, exception
+    
+    begin
+      yield sec
+    ensure
+      req.abort
+    end
   end
 
   module_function :timeout
+
 end
 
-
+##
+# Identical to:
 #
-# Legacy API
+#   Timeout::timeout(n, e, &block).
 #
+# Defined for backwards compatibility with earlier versions of timeout.rb, see
+# Timeout#timeout.
 
+def timeout(n, e=Timeout::Error, &block) # :nodoc:
+  Timeout::timeout(n, e, &block)
+end
 
-class TimeoutError < Timeout::Error; end
+##
+# Another name for Timeout::Error, defined for backwards compatibility with
+# earlier versions of timeout.rb.
 
-module Kernel
-  def timeout(seconds, error = Timeout::Error, &block)
-    Timeout.timeout seconds, error, &block
-  end
+TimeoutError = Timeout::Error # :nodoc:
+
+if __FILE__ == $0
+  p timeout(5) {
+    45
+  }
+  p timeout(5, TimeoutError) {
+    45
+  }
+  p timeout(nil) {
+    54
+  }
+  p timeout(0) {
+    54
+  }
+  p timeout(5) {
+    loop {
+      p 10
+      sleep 1
+    }
+  }
 end
 
