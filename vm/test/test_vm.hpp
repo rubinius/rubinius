ff607789a062325bc0ca344bdb99d4d7a6965b7d
Eric Hodel <ehodel@engineyard.com>
2008-11-04 13:21:17 -0800

    Added a new root

--- vm/test/test_vm.hpp
+++ vm/test/test_vm.hpp
@@ -62,7 +62,7 @@ class TestVM : public CxxTest::TestSuite {
   }
 
   void test_globals() {
-    TS_ASSERT_EQUALS(state->globals.roots.size(), 120U);
+    TS_ASSERT_EQUALS(state->globals.roots.size(), 121U);
   }
 
   void test_collection() {
