bb772e82d605e2a3e1e20041df130da6f4cc919e
Charles Comstock <dgtized@gmail.com>
2008-12-02 00:31:38 -0600

    greatly improved the string append benchmark

--- benchmark/rubinius/bm_string_concat.rb
+++ benchmark/rubinius/bm_string_concat.rb
@@ -1,63 +1,114 @@
 require 'benchmark'
  
-total = (ENV['TOTAL'] || 5_000).to_i
+total = (ENV['TOTAL'] || 1_500).to_i
+inside = total / 10
 
-array = Array.new(total) {|i| i}
+unless "".respond_to? :append
+  class String
+    alias :append :<<
+  end
+end
 
 Benchmark.bmbm do |x|
   x.report 'empty' do
-    total.times {|i| i }
+    total.times {|i| inside.times {|j| j } }
   end
 
-  x.report 'String#<<(fixed)' do
-    str1 = "helluva"
-    str2 = "fool"
+  x.report 'String#append(fixed)' do
     total.times do |i|
-      str1 << str2
+      str1 = "helluva"
+      str2 = "fool"
+      inside.times do 
+        str1.append str2
+      end
     end
   end
 
-  x.report 'String#<<(increasing)' do
+  x.report 'String#append(increasing)' do
     total.times do |i|
-      str1 = "foo" * i % 100
-      str2 = "a" * i % 100
-      25.times do
+      str1 = "foo" * (i % 100)
+      str2 = "a" * (i % 100)
+      inside.times do
+        str1.append str2
+      end
+    end
+  end
+  
+  x.report 'String#<<(fixed)' do
+    total.times do |i|
+      str1 = "helluva"
+      str2 = "fool"
+      inside.times do 
         str1 << str2
       end
     end
   end
 
-  x.report 'String#+(fixed)' do
-    str1 = "helluva"
-    str2 = "fool"
+  x.report 'String#<<(increasing)' do
     total.times do |i|
-      str1 = str1 + str2
+      str1 = "foo" * (i % 100)
+      str2 = "a" * (i % 100)
+      inside.times do
+        str1 << str2
+      end
+    end
+  end
+
+  x.report 'String#concat(fixed)' do
+    total.times do |i|
+      str1 = "helluva"
+      str2 = "fool"
+      inside.times do
+        str1.concat str2
+      end
+    end
+  end
+
+  x.report 'String#concat(increasing)' do
+    total.times do |i|
+      str1 = "foo" * (i % 100)
+      str2 = "a" * (i % 100)
+      inside.times do
+        str1.concat str2
+      end
+    end
+  end
+
+  x.report 'String#+(fixed)' do
+    total.times do |i|
+      str1 = "helluva"
+      str2 = "fool"
+      inside.times do
+        str1 = str1 + str2
+      end
     end
   end
 
   x.report 'String#+(increasing)' do
     total.times do |i|
-      str1 = "foo" * i % 100
-      str2 = "a" * i % 100
-      25.times do
+      str1 = "foo" * (i % 100)
+      str2 = "a" * (i % 100)
+      inside.times do
         str1 = str1 + str2
       end
     end
   end
   
   x.report 'String#+=(fixed)' do
-    str1 = "helluva"
-    str2 = "fool"
     total.times do |i|
-      str1 += str2
+      str1 = "helluva"
+      str2 = "fool"
+      inside.times do 
+        str1 += str2
+      end
     end
   end
 
   x.report 'String#+=(increasing)' do
     total.times do |i|
-      str1 = "foo" * i % 100
-      str2 = "a" * i % 100
-      25.times do
+      str1 = "foo" * (i % 100)
+      str2 = "a" * (i % 100)
+      inside.times do
         str1 += str2
       end
     end
