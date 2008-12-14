0e7d8ff2b94213f94ce9528b88e39a7c35f61ae9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2008-12-15 00:04:17 +0100

    ObjectSpace.each_object works correct for Class, Module, Fixnum, TrueClass, FalseClass, NilClass, and some others

--- kernel/common/objectspace.rb
+++ kernel/common/objectspace.rb
@@ -1,28 +1,94 @@
 # depends on: module.rb
 
 module ObjectSpace
-  def self.each_object(what=nil, &block)
-    unless what == Class
+
+  # HACK: Tryes to handle as much as it can.
+  # Any way to get a list of all instances? Or of all objects?
+  def self.each_object(what = nil, &block)
+    # Now this will become ugly.
+    case
+
+    when (what and not what.is_a? Module)
+      raise TypeError, "class or module required"
+
+    when what == Class
+      recursive_loop(Object, block) { |a_class| a_class.__subclasses__ }
+
+    when what == Module
+      recursive_loop(Object, block) do |a_module|
+        (a_module.constants.inject([]) do |list, const|
+          begin
+            const = a_module.const_get const
+            list << const if const.is_a? Module
+            list += const.__subclasses__ if const.is_a? Class
+          rescue NameError, LoadError # Handles autoloading.
+          end
+          list
+        end).uniq
+      end
+
+    when what == Fixnum
+      (Platform::Fixnum.MIN .. Platform::Fixnum.MAX).each(&block)
+
+    when [TrueClass, FalseClass, NilClass].include?(what)
+      block.call what.new
+      return 1
+
+    when what == GlobalVariables
+      block.call(Globals)
+      return 1
+
+    when (defined?(Singleton) and what.ancestors.include?(Singleton))
+      return 0 unless what.instance_eval { _instantiate? }
+      block.call(what.instance)
+      return 1
+
+    when what.is_a?(MetaClass)
+      block.call(what.attached_instance)
+      return 1
+
+    when what.respond_to?(:each_instance)
+      return what.each_instance(&block)
+
+    when what.is_a?(Enumerable)
+      return what.inject(0) { |o| block.call(o) }
+
+    when nil
+      raise ArgumentError, "ObjectSpace cannot loop through all objects yet"
+
+    else
+      # Remove the following line when each_object(nil) is implemented.
       raise ArgumentError, "ObjectSpace doesn't support '#{what}' yet"
+      count = 0
+      each_object do |obj|
+        if obj.is_a? what
+          count += 1
+          block.call(obj)
+        end
+      end
+      count
+
     end
-    
-    emit_subclasses(Object, block)
   end
   
-  def self.emit_subclasses(start, prc, skip_list = [])
-    subs = start.__subclasses__ - skip_list
-    skip_list += subs
-    return 0 unless subs
-    count = 0
-    
-    subs.each do |c|
-      count += 1
-      prc.call(c)
-      count += emit_subclasses(c, prc, skip_list)
+  protected
+
+  def self.recursive_loop(start, each_block, skip = [], &grepper)
+    list = grepper.call(start)
+    list -= skip
+    list.inject(0) do |count, element|
+      unless skip.include? element
+        each_block.call(element)
+        skip << element
+        count += 1 + recursive_loop(element, each_block, skip, &grepper)
+      end
+      count
     end
-    
-    return count
   end
+
+  public
+
+
 
   # Finalizer support. Uses WeakRef to detect object death.
   # WeakRef uses the GC to do all the real work.
