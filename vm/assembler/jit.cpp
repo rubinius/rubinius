6f41a59b0aa6881b9ed4616b5062ab3e2a60890c
Evan Phoenix <ephoenix@engineyard.com>
2008-12-17 11:12:33 -0800

    Some 64bit fixes, a bit more specialization in the assembler

--- vm/assembler/jit.cpp
+++ vm/assembler/jit.cpp
@@ -11,6 +11,7 @@
 
 #include "instructions.hpp"
 #include "assembler/jit.hpp"
+#include "event.hpp"
 
 using namespace assembler;
 using namespace assembler_x86;
@@ -45,12 +46,23 @@ namespace rubinius {
     ops.save_stack_pointer();
   }
 
+  ExecuteStatus JITCompiler::check_interrupts(VMMethod* const vmm, Task* const task,
+      MethodContext* const ctx) {
+    task->state->events->poll();
+    return task->state->interrupts.check_events ? cExecuteRestart : cExecuteContinue;
+  }
+
   ExecuteStatus JITCompiler::slow_plus_path(VMMethod* const vmm, Task* const task,
       MethodContext* const ctx) {
     return send_slowly(vmm, task, ctx, task->state->globals.sym_plus.get(), 1);
   }
 
-  void JITCompiler::maybe_return(int i, uint32_t **last_imm, AssemblerX86::NearJumpLocation &fin) {
+  ExecuteStatus JITCompiler::slow_minus_path(VMMethod* const vmm, Task* const task,
+      MethodContext* const ctx) {
+    return send_slowly(vmm, task, ctx, task->state->globals.sym_minus.get(), 1);
+  }
+
+  void JITCompiler::maybe_return(int i, uintptr_t **last_imm, AssemblerX86::NearJumpLocation &fin) {
 
     // EDX will contain the native ip, to be stored
     // back into the MethodContext in the epilogue.
@@ -72,7 +84,7 @@ namespace rubinius {
 
   void JITCompiler::compile(VMMethod* vmm) {
     // Used for fixups
-    uint32_t* last_imm = NULL;
+    uintptr_t* last_imm = NULL;
 
     // A label pointing to the code for each virtual ip
     std::vector<AssemblerX86::NearJumpLocation> labels(vmm->total);
@@ -120,7 +132,7 @@ namespace rubinius {
         // stack caching.
         uncache_stack();
 
-        *last_imm = (uint32_t)a.pc();
+        *last_imm = (uintptr_t)a.pc();
         Relocation* rel = new Relocation(Relocation::LocalAbsolute,
             last_imm, a.pc(), 0);
         a.add_relocation(last_imm, rel);
@@ -136,7 +148,8 @@ namespace rubinius {
       case InstructionSequence::insn_noop:
         break;
       case InstructionSequence::insn_goto:
-        a.jump(labels[vmm->opcodes[i + 1]]);
+        opcode dest = vmm->opcodes[i + 1];
+        a.jump(labels[dest]);
         break;
       case InstructionSequence::insn_goto_if_false:
         cache_stack();
@@ -239,17 +252,18 @@ namespace rubinius {
         s.push(eax);
         break;
 
+      case InstructionSequence::insn_meta_send_op_minus:
       case InstructionSequence::insn_meta_send_op_plus: {
         cache_stack();
-        AssemblerX86::NearJumpLocation slow_plus;
+        AssemblerX86::NearJumpLocation slow_path;
         AssemblerX86::NearJumpLocation done;
 
         // This code is HIGHLY aware that the tag bit for fixnum
         // is a 1 in the low position only.
 
         // Pull in the top 2 entries on the stack into registers
-        s.load_nth(eax, 0);
-        s.load_nth(ecx, 1);
+        s.load_nth(ecx, 0);
+        s.load_nth(eax, 1);
 
         // Perform the bit and to find out if they're both fixnums
         a.mov(edx, eax);
@@ -258,18 +272,30 @@ namespace rubinius {
 
         // This seems odd, like the condition is backwards, but thats
         // how test works.
-        a.jump_if_equal(slow_plus);
+        a.jump_if_equal(slow_path);
 
         // Ok, they're are both fixnums...
-        // And add them together directly
-        a.add(eax, ecx);
+        if(op == InstructionSequence::insn_meta_send_op_plus) {
+          // And add them together directly
+          a.add(eax, ecx);
 
-        // Check the x86 overflow bit, and if so, run the slow path
-        a.jump_if_overflow(slow_plus);
+          // Check the x86 overflow bit, and if so, run the slow path
+          a.jump_if_overflow(slow_path);
 
-        // Everything was good, so subtract 1 because the tag adds an
-        // extra 1 to the result
-        a.sub(eax, 1);
+          // Everything was good, so subtract 1 because the tag adds an
+          // extra 1 to the result
+          a.sub(eax, 1);
+        } else {
+          // And subtract them together directly
+          a.sub(eax, ecx);
+
+          // Check the x86 overflow bit, and if so, run the slow path
+          a.jump_if_overflow(slow_path);
+
+          // Everything was good, so add 1 because the tag subtracts an
+          // extra 1 to the result
+          a.add(eax, 1);
+        }
 
         // Remove one from the stack
         s.pop();
@@ -282,9 +308,13 @@ namespace rubinius {
         uncache_stack();
         a.jump(done);
 
-        a.set_label(slow_plus);
+        a.set_label(slow_path);
         uncache_stack(true);
-        ops.call_via_symbol((void*)JITCompiler::slow_plus_path);
+        if(op == InstructionSequence::insn_meta_send_op_plus) {
+          ops.call_via_symbol((void*)JITCompiler::slow_plus_path);
+        } else {
+          ops.call_via_symbol((void*)JITCompiler::slow_minus_path);
+        }
         maybe_return(i, &last_imm, fin);
 
         // This is a phi point, where the fast path and slow path merge.
