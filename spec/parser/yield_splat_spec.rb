f0998bb0b91ce19de2e517d51aad46bc8127f41b
Brian Ford <bford@engineyard.com>
2009-01-09 11:58:12 -0800

    Updated parser specs for splat changes.

--- spec/parser/yield_splat_spec.rb
+++
@@ -1,6 +0,0 @@
-def test_case
-{"RawParseTree"=>[:yield, [:splat, [:vcall, :ary]], true],
- "Ruby"=>"yield(*ary)",
- "RubyParser"=>
-  s(:yield, s(:array, s(:splat, s(:call, nil, :ary, s(:arglist)))))}
-end
