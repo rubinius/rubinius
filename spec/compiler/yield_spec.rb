9a93449b490acf596608ec06499234cac2d9c0be
Brian Ford <bford@engineyard.com>
2009-01-30 22:25:41 -0800

    Additional splat compiler specs for yield.

--- spec/compiler/yield_spec.rb
+++ spec/compiler/yield_spec.rb
@@ -89,6 +89,22 @@ describe "A Yield node" do
     end
   end
 
+  relates "yield([*[1, 2]])" do
+    parse do
+      [:yield, [:array, [:splat, [:array, [:lit, 1], [:lit, 2]]]]]
+    end
+
+    compile do |g|
+      g.push_block
+      g.array_of_splatted_array 2 do
+        g.push 1
+        g.push 2
+      end
+
+      g.meta_send_call 1
+    end
+  end
+
   relates "yield(*[1])" do
     parse do
       [:yield, [:splat, [:array, [:lit, 1]]]]
@@ -99,6 +115,25 @@ describe "A Yield node" do
 
       g.push 1
       g.make_array 1
+      g.cast_array
+
+      g.push :nil
+
+      g.send_with_splat :call, 0, false, false
+    end
+  end
+
+  relates "yield(*[1, 2])" do
+    parse do
+      [:yield, [:splat, [:array, [:lit, 1], [:lit, 2]]]]
+    end
+
+    compile do |g|
+      g.push_block
+
+      g.push 1
+      g.push 2
+      g.make_array 2
       g.cast_array
 
       g.push :nil
