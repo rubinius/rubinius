179781391b0cd9ca5545037d9d2f84214cbbe71a
Brian Ford <bford@engineyard.com>
2009-03-24 10:29:29 -0700

    Get C-API specs compiling under rbx.

--- spec/capi/spec_helper.rb
+++ spec/capi/spec_helper.rb
@@ -2,16 +2,28 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 require 'rbconfig'
 
-def compile_extension(name)
-  path = File.dirname(__FILE__) + '/ext/'
+def compile_extension(path, ext, ext_source)
+  signature = "#{path}compiled_by"
 
-  ext = "#{path}#{name}.#{Config::CONFIG['DLEXT']}"
-  ext_source = "#{path}#{name}.c"
+  return if File.exists?(signature) and
+            IO.read(signature).chomp == RUBY_NAME and
+            File.exists?(ext) and File.mtime(ext) > File.mtime(ext_source)
 
-  if !File.exists?(ext) or File.mtime(ext) < File.mtime(ext_source)
-    system "rake build:debug_flags extension:specs:#{name}"
+  # TODO use Tap; write a common ext build task
+  if RUBY_NAME == 'rbx'
+    `./bin/rbx compile -I#{Rubinius::HDR_PATH} #{ext_source}`
+  else
+    # TODO add compile commands for MRI
   end
+
+  File.open(signature, "w") { |f| f.puts RUBY_NAME }
 end
 
 def load_extension(name)
+  path = File.dirname(__FILE__) + '/ext/'
+  ext = "#{path}#{name}_spec.#{Config::CONFIG['DLEXT']}"
+  ext_source = "#{path}#{name}_spec.c"
+
+  compile_extension path, ext, ext_source
+  require ext
 end
