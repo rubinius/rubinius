3a066562bf4b699558e2cc66525c63ab279331a5
Evan Phoenix <ephoenix@engineyard.com>
2009-03-25 21:51:43 -0700

    Simple driver to compile and run one test file

---
+++ vm/test/one.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export VERBOSE=1
+
+if test "x$PROFILE" != "x"; then
+  FLAGS="-fprofile-arcs -ftest-coverage"
+else
+  FLAGS=""
+fi
+
+vm/test/cxxtest/cxxtestgen.pl --error-printer --have-eh --abort-on-fail --include=string.h -o vm/test/one.cpp "$*"
+
+g++ $FLAGS -ggdb3 -g -Wall -Werror -Ivm/external_libs/libtommath -Ivm/test/cxxtest -I. -Ivm -o vm/test/one vm/test/one.cpp && ./vm/test/one
