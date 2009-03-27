093f08b8520f0ddb995aeaac7cbe540e5511fe4c
Brian Ford <bford@engineyard.com>
2009-03-26 17:45:47 -0700

    Cleaned up capi string specs.

--- spec/capi/string_spec.rb
+++ spec/capi/string_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 load_extension('string')
 
-describe "CApiString" do
+describe "C-API String function" do
   before :each do
     @s = CApiStringSpecs.new
   end
@@ -19,104 +19,122 @@ describe "CApiString" do
     end
   end
 
-  it "rb_str_new should return a new string object" do
-    # Hardcoded to pass const char * = "hello"
-    @s.say_hello.should == "hello"
-  end
-
-  it "rb_str_new2 should return a new string object, figuring out the length itself" do
-    # Hardcoded to pass const char * = "hello\0invisible"
-    @s.say_hello2.should == "hello"
-  end
-
-  it "rb_str_new2 should raise ArgumentError if passed NULL" do
-    lambda { @s.rb_str_new2_with_null }.should raise_error(ArgumentError)
-  end
-
-  it "rb_str_dup should return a copy of the string" do
-    str1 = "hi"
-    str2 = @s.dup_string str1
-    str1.should == str2
-    str1.object_id.should_not == str2.object_id
-  end
-
-  it "rb_str_append should append a string to another string" do
-    @s.rb_str_append("Hello", " Goodbye").should == "Hello Goodbye"
-  end
-
-  it "rb_str_plus should return a new string from two other strings" do
-    @s.rb_str_plus("Hello", " Goodbye").should == "Hello Goodbye"
-  end
-
-  it "rb_str_buf_cat should concat C strings to ruby strings" do
-    @s.phrase_as_question("Your house is on fire").should == "Your house is on fire?"
-  end
-
-  it "rb_str_cat should concat C strings to ruby strings" do
-    @s.cat_as_question("Your house is on fire").should == "Your house is on fire?"
-  end
-
-  it "rb_str_cat2 should concat C strings to ruby strings" do
-    @s.cat2_as_question("Your house is on fire").should == "Your house is on fire?"
-  end
-
-  it "rb_str_cmp should compare two strings" do
-    @s.rb_str_cmp("xxx", "xxxx").should == -1
-    @s.rb_str_cmp("xxxx", "xxx").should == 1
-    @s.rb_str_cmp("xxx", "yyy").should == -1
-    @s.rb_str_cmp("yyy", "xxx").should == 1
-    @s.rb_str_cmp("ppp", "ppp").should == 0
-  end
-
-  it "rb_str_split should split strings over a splitter" do
-    @s.rb_str_split("Hello,Goodbye").should == ["Hello", "Goodbye"]
-  end
-
-  it "rb_str2inum should return a string representation as a number" do
-    @s.rb_str2inum("10", 10).should == 10
-    @s.rb_str2inum("A", 16).should == 10
-  end
-
-  it "rb_str2inum should return a string representation as a number" do
-    @s.rb_cstr2inum(10).should == 10
-    @s.rb_cstr2inum(16).should == 16
-  end
-
-  it "rb_str_substr should return a substring" do
-    "hello".length.times do |time|
-      @s.rb_str_substr("hello", 0, time + 1).should == "hello"[0..time]
+  describe "rb_str_new" do
+    it "returns a new string object" do
+      # Hardcoded to pass const char * = "hello"
+      @s.rb_str_new.should == "hello"
     end
   end
 
-  it "RSTRING(str)->ptr should return the string on the object" do
-    @s.rb_rstring_see("foo").should == "foo"
+  describe "rb_str_new2" do
+    it "returns a new string object calling strlen on the passed C string" do
+      # Hardcoded to pass const char * = "hello\0invisible"
+      @s.rb_str_new2.should == "hello"
+    end
+
+    it "rb_str_new2 should raise ArgumentError if passed NULL" do
+      lambda { @s.rb_str_new2_with_null }.should raise_error(ArgumentError)
+    end
   end
 
-  it "Changing RSTRING(str)->ptr should change the string" do
-    t = "a"
-    @s.rb_rstring_assign_foo(t)
-    t.should == "foo"
-    $global_rstring_test = "b"
-    @s.rb_rstring_assign_global_foobar()
-    $global_rstring_test.should == "foobar"
+  describe "rb_str_dup" do
+    it "returns a copy of the string" do
+      str1 = "hi"
+      str2 = @s.rb_str_dup str1
+      str1.should == str2
+      str1.object_id.should_not == str2.object_id
+    end
   end
 
-  it "Reducing RSTRING(str)->len should cut the string" do
-    t = "12345"
-    @s.rb_rstring_set_len(t, 3)
-    t.should == "123"
+  describe "rb_str_append" do
+    it "appends a string to another string" do
+      @s.rb_str_append("Hello", " Goodbye").should == "Hello Goodbye"
+    end
   end
 
-  it "Changing RSTRING(str)->ptr and calling upcase! should change the string and upcase it" do
-    t = ""
-    @s.rb_rstring_assign_foo_and_upcase(t)
-    t.should == "FOO"
+  describe "rb_str_plus" do
+    it "returns a new string from concatenating two other strings" do
+      @s.rb_str_plus("Hello", " Goodbye").should == "Hello Goodbye"
+    end
   end
 
-  it "rb_str_to_str should try to coerce to String, otherwise raise a TypeError" do
-    @s.rb_str_to_str("foo").should == "foo"
-    @s.rb_str_to_str(ValidTostrTest.new).should == "ruby"
-    lambda { @s.rb_str_to_str(0) }.should raise_error(TypeError)
-    lambda { @s.rb_str_to_str(InvalidTostrTest.new) }.should raise_error(TypeError)
+  describe "rb_str_buf_cat" do
+    it "concatenates a C string to a ruby string" do
+      @s.rb_str_buf_cat("Your house is on fire").should == "Your house is on fire?"
+    end
+  end
+
+  describe "rb_str_cat" do
+    it "concatenates a C string to ruby string" do
+      @s.rb_str_cat("Your house is on fire").should == "Your house is on fire?"
+    end
+  end
+
+  describe "rb_str_cat2" do
+    it "concatenates a C string to a ruby string" do
+      @s.rb_str_cat2("Your house is on fire").should == "Your house is on fire?"
+    end
+  end
+
+  describe "rb_str_cmp" do
+    it "returns 0 if two strings are identical" do
+      @s.rb_str_cmp("ppp", "ppp").should == 0
+    end
+
+    it "returns -1 if the first string is shorter than the second" do
+      @s.rb_str_cmp("xxx", "xxxx").should == -1
+    end
+
+    it "returns -1 if the first string is lexically less than the second" do
+      @s.rb_str_cmp("xxx", "yyy").should == -1
+    end
+
+    it "returns 1 if the first string is longer than the second" do
+      @s.rb_str_cmp("xxxx", "xxx").should == 1
+    end
+
+    it "returns 1 if the first string is lexically greater than the second" do
+      @s.rb_str_cmp("yyy", "xxx").should == 1
+    end
+  end
+
+  describe "rb_str_split" do
+    it "splits strings over a splitter" do
+      @s.rb_str_split("Hello,Goodbye").should == ["Hello", "Goodbye"]
+    end
+  end
+
+  describe "rb_str2inum" do
+    it "converts a string to a number given a base" do
+      @s.rb_str2inum("10", 10).should == 10
+      @s.rb_str2inum("A", 16).should == 10
+    end
+  end
+
+  describe "rb_cstr2inum" do
+    it "converts a C string to a number given a base" do
+      @s.rb_cstr2inum("10", 10).should == 10
+      @s.rb_cstr2inum("10", 16).should == 16
+    end
+  end
+
+  describe "rb_str_substr" do
+    it "returns a substring" do
+      "hello".length.times do |time|
+        @s.rb_str_substr("hello", 0, time + 1).should == "hello"[0..time]
+      end
+    end
+  end
+
+  describe "rb_str_to_str" do
+    it "calls #to_str to coerce the value to a String" do
+      @s.rb_str_to_str("foo").should == "foo"
+      @s.rb_str_to_str(ValidTostrTest.new).should == "ruby"
+    end
+
+    it "raises a TypeError if coercion fails" do
+      lambda { @s.rb_str_to_str(0) }.should raise_error(TypeError)
+      lambda { @s.rb_str_to_str(InvalidTostrTest.new) }.should raise_error(TypeError)
+    end
   end
 end
