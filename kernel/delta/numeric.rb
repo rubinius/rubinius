89423a1f81d7304ab48d1d8eda71327332e3bfb6
Brian Ford <bford@engineyard.com>
2009-04-16 00:34:50 -0700

    Correctly mimic crazy MRI coerce functions.

--- kernel/delta/numeric.rb
+++ kernel/delta/numeric.rb
@@ -1,15 +1,19 @@
 class Numeric
   # C-API method corresponding to rb_num_coerce_bin
   def self.num_coerce_bin(x, y, op)
-    b, a = x.coerce y
+    begin
+      a, b = y.coerce x
+    rescue
+      raise TypeError, "#{y.inspect} can't be coerced into #{x.class}"
+    end
     a.send op, b
   end
 
   # C-API method corresponding to rb_num_coerce_cmp
   def self.num_coerce_cmp(x, y, op)
     begin
-      b, a = x.coerce y
-    rescue TypeError
+      a, b = y.coerce x
+    rescue
       return nil
     end
     a.send op, b
