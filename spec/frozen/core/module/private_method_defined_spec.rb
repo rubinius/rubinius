3ac2da4c52f94479dcbb0017901990f4267b7348
Brian Ford <bford@engineyard.com>
2009-04-25 15:09:00 -0700

    Fixed specs for Module#private_method_defined?

--- spec/frozen/core/module/private_method_defined_spec.rb
+++ spec/frozen/core/module/private_method_defined_spec.rb
@@ -31,24 +31,32 @@ describe "Module#private_method_defined?" do
     ModuleSpecs::CountsMixin.private_method_defined?(:private_3).should == true
   end
 
+  it "raises an ArgumentError if passed a Fixnum" do
+    lambda {
+      ModuleSpecs::CountsMixin.private_method_defined?(1)
+    }.should raise_error(ArgumentError)
+  end
+
+  it "raises a TypeError if passed nil" do
+    lambda {
+      ModuleSpecs::CountsMixin.private_method_defined?(nil)
+    }.should raise_error(TypeError)
+  end
+
+  it "raises a TypeError if passed false" do
+    lambda {
+      ModuleSpecs::CountsMixin.private_method_defined?(false)
+    }.should raise_error(TypeError)
+  end
+
+  it "raises a TypeError if passed an object that does not defined #to_str" do
+    lambda {
+      ModuleSpecs::CountsMixin.private_method_defined?(mock('x'))
+    }.should raise_error(TypeError)
+  end
+
   not_compliant_on :rubinius do
-    it "raises an ArgumentError if passed a Fixnum" do
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(1)
-      }.should raise_error(ArgumentError)
-    end
-
-    it "raises a TypeError if not passed a Symbol" do
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(nil)
-      }.should raise_error(TypeError)
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(false)
-      }.should raise_error(TypeError)
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(mock('x'))
-      }.should raise_error(TypeError)
-
+    it "raises a TypeError if passed an object that defines #to_sym" do
       sym = mock('symbol')
       def sym.to_sym() :private_3 end
       lambda {
@@ -57,26 +65,18 @@ describe "Module#private_method_defined?" do
     end
   end
 
-  it "accepts any argument that is a String type" do
+  deviates_on :rubinius do
+    it "calls #to_sym to coerce the passed object to a Symbol" do
+      sym = mock('symbol')
+      def sym.to_sym() :private_3 end
+
+      ModuleSpecs::CountsMixin.private_method_defined?(sym).should be_true
+    end
+  end
+
+  it "calls #to_str to coerce the passed object to a String" do
     str = mock('string')
     def str.to_str() 'private_3' end
     ModuleSpecs::CountsMixin.private_method_defined?(str).should == true
-  end
-
-  deviates_on :rubinius do
-    it "raises a TypeError if not passed a String type" do
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(1)
-      }.should raise_error(TypeError)
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(nil)
-      }.should raise_error(TypeError)
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(false)
-      }.should raise_error(TypeError)
-      lambda {
-        ModuleSpecs::CountsMixin.private_method_defined?(mock('x'))
-      }.should raise_error(TypeError)
-    end
   end
 end
