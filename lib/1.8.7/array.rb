2ee7a5f31e1ca733340ca4a47ec875ad93b2664a
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-05 18:58:22 -0400

    1.8.7: Array#index, #rindex

--- lib/1.8.7/array.rb
+++ lib/1.8.7/array.rb
@@ -1,11 +1,13 @@
 class Array
   # Recursively flatten any contained Arrays into an one-dimensional result.
+  # The optional level argument determines the level of recursion to flatten
   def flatten(level=Undefined)
     dup.flatten!(level) || self
   end
 
   # Flattens self in place as #flatten. If no changes are
   # made, returns nil, otherwise self.
+  # The optional level argument determines the level of recursion to flatten
   def flatten!(level=Undefined)
     if level == Undefined
       level = -1
@@ -17,4 +19,45 @@ class Array
     replace(out) if ret
     ret
   end
+  
+  # Returns the index of the first element in the Array
+  # for which elem == obj is true or nil. If a block is
+  # given instead of an argument, returns first object
+  # for which block is true. Returns nil if no match is found.
+  def index(obj = Undefined)
+    i = 0
+    if obj == Undefined
+      while i < @total do
+        return i if yield at(i)
+        i += 1
+      end
+    else
+      while i < @total do
+        return i if at(i) == obj
+        i += 1
+      end
+    end
+    nil
+  end
+
+  # Returns the index of the last element in the Array
+  # for which elem == obj is true. If a block is given
+  # instead of an argument, returns last object for
+  # which block is true
+  def rindex(obj = Undefined)
+    i = @total - 1
+    if obj == Undefined
+      while i >= 0 do
+        return i if yield at(i)
+        i -= 1
+      end
+    else
+      while i >= 0 do
+        return i if at(i) == obj
+        i -= 1
+      end
+    end
+    nil
+  end
+  
 end
---
+++ lib/1.9.rb
@@ -0,0 +1,3 @@
+# All libraries for Ruby 1.9+ should be located here
+require '1.8.7'
+RUBY_VERSION.replace "1.9.1" if RUBY_VERSION == "1.8.7"
