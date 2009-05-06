1ca6b50fb72839ab0032683ab895893b1c2f8733
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-06 14:23:02 -0400

    1.8.7: Array#pop, Array#slice!

--- lib/1.8.7/array.rb
+++ lib/1.8.7/array.rb
@@ -4,7 +4,7 @@ class Array
   # no guarantees about the order in which the combinations are yielded, we copy MRI.
   # When invoked without a block, returns an enumerator object instead.
   def combination(num)
-    num = num.to_i
+    num = Type.coerce_to num, Fixnum, :to_int
     return to_enum(:combination, num) unless block_given?
     return self unless (0..size).include? num
     # Implementation note: slightly tricky.
@@ -29,7 +29,8 @@ class Array
     if n.nil?
       loop(&block)
     else
-      n.to_i.times{each(&block)}
+      n = Type.coerce_to n, Fixnum, :to_int
+      n.times{each(&block)}
     end
     nil
   end
@@ -44,7 +45,7 @@ class Array
   # made, returns nil, otherwise self.
   # The optional level argument determines the level of recursion to flatten
   def flatten!(level=Undefined)
-    if level == Undefined
+    if level.equal? Undefined
       level = -1
     else
       level = Type.coerce_to(level, Integer, :to_int)
@@ -61,7 +62,7 @@ class Array
   # for which block is true. Returns nil if no match is found.
   def index(obj = Undefined)
     i = 0
-    if obj == Undefined
+    if obj.equal? Undefined
       while i < @total do
         return i if yield at(i)
         i += 1
@@ -75,13 +76,30 @@ class Array
     nil
   end
 
+  # Removes and returns the last element from the Array.
+  # If a number n is given, returns an array of the last n elements (or less)
+  # just like array.slice!(-n, n) does.
+  alias_method :single_pop, :pop
+  private :single_pop
+  def pop(n = Undefined)
+    if n.equal? Undefined
+      single_pop
+    else
+      n = Type.coerce_to(n, Fixnum, :to_int)
+      raise ArgumentError, "negative array size" if n < 0
+      first = size - n
+      first = 0 if first < 0
+      Array.new slice!(first..size)
+    end
+  end
+
   # Returns the index of the last element in the Array
   # for which elem == obj is true. If a block is given
   # instead of an argument, returns last object for
   # which block is true
   def rindex(obj = Undefined)
     i = @total - 1
-    if obj == Undefined
+    if obj.equal? Undefined
       while i >= 0 do
         return i if yield at(i)
         i -= 1
@@ -93,6 +111,27 @@ class Array
       end
     end
     nil
+  end
+  
+  # Deletes the element(s) given by an index (optionally with a length)
+  # or by a range. Returns the deleted object, subarray, or nil if the
+  # index is out of range. Equivalent to:
+  def slice!(*args)
+    out = self[*args]
+    if !(Range === args[0])
+      # make sure that negative values are not passed through to the
+      # []= assignment
+      args[0] = Type.coerce_to args[0], Integer, :to_int
+      args[0] += self.length if args[0] < 0
+      # Don't extend the array for out of bounds ranges
+      return out if args[0] < 0 || args[0] > self.length
+      args << 1 if args.size == 1
+    else
+      # Don't extend the array for out of bounds ranges
+      return out if args[0].begin.to_int > self.length
+    end
+    self[*args] = []
+    out
   end
   
 end
