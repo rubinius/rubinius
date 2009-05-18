4d5a9e8d4adfce29958c195f7c090f6d2705e423
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-18 15:02:50 -0400

    1.9: Kernel#define_singleton_method  (implementation of new method)

--- lib/1.9/kernel.rb
+++ lib/1.9/kernel.rb
@@ -1,3 +1,9 @@
 module Kernel
   alias_method :__callee__, :__method__
+  
+  def define_singleton_method(*args, &block)
+    class << self
+      self
+    end.send(:define_method, *args, &block)
+  end
 end
