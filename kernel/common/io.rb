bcb33f34934d40a7c35d7b6e3a35e37ee4c7d90e
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-18 23:46:02 -0400

    Moving some io stuff around

--- kernel/common/io.rb
+++ kernel/common/io.rb
@@ -1,36 +1,4 @@
 class IO
-  ##
-  # Executes the block for every line in ios, where
-  # lines are separated by sep_string. ios must be
-  # opened for reading or an IOError will be raised.
-  #
-  #  f = File.new("testfile")
-  #  f.each {|line| puts "#{f.lineno}: #{line}" }
-  # produces:
-  #
-  #  1: This is line one
-  #  2: This is line two
-  #  3: This is line three
-  #  4: And so on...
-  def each(sep=$/)
-    ensure_open_and_readable
-
-    sep = sep.to_str if sep
-    while line = read_to_separator(sep)
-      yield line
-    end
-
-    self
-  end
-
-  alias_method :each_line, :each
-
-  def each_byte
-    yield getc until eof?
-
-    self
-  end
-
   # Import platform constants
 
   SEEK_SET = Rubinius::RUBY_CONFIG['rbx.platform.io.SEEK_SET']
--- kernel/common/io_186.rb
+++ kernel/common/io_186.rb
@@ -1,3 +1,34 @@
 # These methods are overriden by lib/1.8.7/... or lib/1.9/...
 class IO
+  ##
+  # Executes the block for every line in ios, where
+  # lines are separated by sep_string. ios must be
+  # opened for reading or an IOError will be raised.
+  #
+  #  f = File.new("testfile")
+  #  f.each {|line| puts "#{f.lineno}: #{line}" }
+  # produces:
+  #
+  #  1: This is line one
+  #  2: This is line two
+  #  3: This is line three
+  #  4: And so on...
+  def each(sep=$/)
+    ensure_open_and_readable
+
+    sep = sep.to_str if sep
+    while line = read_to_separator(sep)
+      yield line
+    end
+
+    self
+  end
+
+  alias_method :each_line, :each
+
+  def each_byte
+    yield getc until eof?
+
+    self
+  end
 end
