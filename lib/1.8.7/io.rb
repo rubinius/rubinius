39aa2f60bdffb7d2a4ffffc5b4b6288231ae4d46
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-25 14:58:27 -0400

    1.8.7: IO#each_char  (implementation of new method)

--- lib/1.8.7/io.rb
+++ lib/1.8.7/io.rb
@@ -2,7 +2,7 @@ class IO
   def bytes
     to_enum :each_byte
   end
-  
+
   ##
   # Executes the block for every line in ios, where
   # lines are separated by sep_string. ios must be
@@ -33,6 +33,34 @@ class IO
   def each_byte
     return to_enum :each_byte unless block_given?
     yield getbyte until eof?
+
+    self
+  end
+
+  def each_char
+    return to_enum :each_char unless block_given?
+    ensure_open_and_readable
+    if $KCODE == "UTF-8"
+      lookup = 7.downto(4)
+      while c = read(1) do
+        n = c[0]
+        leftmost_zero_bit = lookup.find{|i| n[i].zero? }
+        case leftmost_zero_bit
+        when 7 # ASCII
+          yield c
+        when 6 # UTF 8 complementary characters
+          next # Encoding error, ignore
+        else
+          more = read(6-leftmost_zero_bit)
+          break unless more
+          yield c+more
+        end
+      end
+    else
+      while s = read(1)
+        yield s
+      end
+    end
 
     self
   end
