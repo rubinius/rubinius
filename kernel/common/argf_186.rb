c810fec184b3250340cdde108a9ef8da9ab393cf
Marc-Andre Lafortune <github@marc-andre.ca>
2009-05-25 15:38:47 -0400

    1.8.7: ARGF#bytes, chars, each_char, getbyte, lines, readbyte  (implemented new methods)
    Also fixed each* (now returning self)

--- kernel/common/argf_186.rb
+++ kernel/common/argf_186.rb
@@ -4,16 +4,19 @@ module Rubinius
       while stream = next_stream
         stream.each_line(&block)
       end
+      self
     end
 
     def each(&block)
       each_line &block
+      self
     end
 
     def each_byte(&block)
       while stream = next_stream
         stream.each_byte(&block)
       end
+      self
     end
   end
 end
--- lib/1.8.7/argf.rb
+++ lib/1.8.7/argf.rb
@@ -1,15 +1,25 @@
 module Rubinius
   class ARGFClass
+    def bytes
+      to_enum :each_byte
+    end
+
+    def chars
+      to_enum :each_char
+    end
+
     def each_line(&block)
       return to_enum :each_line unless block_given?
       while stream = next_stream
         stream.each_line(&block)
       end
+      self
     end
 
     def each(&block)
       return to_enum :each unless block_given?
       each_line &block
+      self
     end
 
     def each_byte(&block)
@@ -17,6 +27,31 @@ module Rubinius
       while stream = next_stream
         stream.each_byte(&block)
       end
+      self
     end
+
+    def each_char(&block)
+      return to_enum :each_char unless block_given?
+      while stream = next_stream
+        stream.each_char(&block)
+      end
+      self
+    end
+
+    def getbyte
+      stream = next_stream
+      stream.getbyte if stream
+    end
+
+    def lines(*args)
+      to_enum :each_line, *args
+    end
+
+    def readbyte
+      stream = next_stream
+      raise EOFError unless stream
+      stream.readbyte
+    end
+
   end
 end
