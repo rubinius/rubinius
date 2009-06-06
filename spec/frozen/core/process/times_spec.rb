d2a79ef2445716a1f01e61eed080be92f24bc97a
Brian Ford <bford@engineyard.com>
2009-06-05 19:27:13 -0700

    Use shared specs for Process.times and Time.times.
    Also, removes deprecation warning spec because RubySpec does
    not spec warnings except under very rare circumstances. Also,
    removes 1.9 spec that Time.times no longer exists because
    RubySpec only specs the behavior of existing methods.

--- spec/frozen/core/process/times_spec.rb
+++ spec/frozen/core/process/times_spec.rb
@@ -1,29 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/../../shared/process/times'
 
 describe "Process.times" do
-  it "returns a Struct::Tms" do
-    Process::times.class.should == Struct::Tms
-  end
-
-  it "returns current cpu times" do
-
-    t = Process::times
-
-    # Stall for 1 second, but do work instead of sleeping
-    # because process times won't increase.
-    start = Time.now
-    1 until (Time.now - start) > 1.2
-
-    # Ensure times is larger. NOTE that there is no 
-    # guarantee of an upper bound since anything may be 
-    # happening at the OS level, so we ONLY check that at 
-    # least an interval has elapsed. Also, we are assuming 
-    # there is a correlation between wall clock time and 
-    # process time. In practice, there is an observed 
-    # discrepancy often 10% or greater. In other words, 
-    # this is a very fuzzy test.
-    t2 = Process::times
-    diff = (t2.utime + t2.stime) - (t.utime + t.stime)
-    diff.should > 0
-  end
+  it_behaves_like :process_times, :times, Process
 end
--- spec/frozen/core/time/times_spec.rb
+++ spec/frozen/core/time/times_spec.rb
@@ -1,16 +1,8 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
-require File.dirname(__FILE__) + '/fixtures/methods'
+require File.dirname(__FILE__) + '/../../shared/process/times'
 
 describe 'Time.times' do
   ruby_version_is "" ... "1.9" do
-    it "is an obsolete method" do
-      lambda { Time.times }.should complain(/obsolete.*use.*Process::times/)
-    end
-  end
-
-  ruby_version_is "1.9" do
-    it "is no longer exist" do
-      lambda { Time.times }.should raise_error(NoMethodError)
-    end
+    it_behaves_like :process_times, :times, Time
   end
 end
