a4d4d458cb62bbe8cc242f7fd30e7fba60a55e2e
Brian Ford <bford@engineyard.com>
2009-06-12 16:25:08 -0700

    Added Class#new spec for block passed to #initialize.

--- spec/frozen/core/class/initialize_spec.rb
+++ spec/frozen/core/class/initialize_spec.rb
@@ -5,24 +5,34 @@ describe "Class#new" do
     klass = Class.new
     klass.new.is_a?(klass).should == true
   end
-  
+
   it "invokes #initialize on the new instance with the given args" do
     klass = Class.new do
       def initialize(*args)
         @initialized = true
         @args = args
       end
-      
+
       def args
         @args
       end
-      
+
       def initialized?
         @initialized || false
       end
     end
-    
+
     klass.new.initialized?.should == true
     klass.new(1, 2, 3).args.should == [1, 2, 3]
+  end
+
+  it "passes the block to #initialize" do
+    klass = Class.new do
+      def initialize
+        yield
+      end
+    end
+
+    klass.new { break 42 }.should == 42
   end
 end
