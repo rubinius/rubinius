794b460e8f7e1a9900d2e229a993c3d46e06097f
Brian Ford <bford@engineyard.com>
2009-06-17 02:03:48 -0700

    Include __STDC_LIMIT_MACROS stuff if needed.

--- vm/llvm/jit.hpp
+++ vm/llvm/jit.hpp
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <unistd.h>
 
+#ifndef __STDC_LIMIT_MACROS
+#include <tr1/cstdint>
+#endif
+
 #include <llvm/Module.h>
 #include <llvm/DerivedTypes.h>
 #include <llvm/Function.h>
