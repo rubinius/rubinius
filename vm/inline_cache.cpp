362c3958b38c4df9460dcc0425a8a5956887f539
Evan Phoenix <ephoenix@engineyard.com>
2009-06-22 15:43:08 -0700

    Add klass tracking in ICs, add ability to disable the IC

--- vm/inline_cache.cpp
+++ vm/inline_cache.cpp
@@ -1,6 +1,7 @@
 #include "inline_cache.hpp"
 #include "arguments.hpp"
 #include "call_frame.hpp"
+#include "global_cache.hpp"
 
 #include "builtin/symbol.hpp"
 #include "builtin/lookuptable.hpp"
@@ -14,6 +15,19 @@ namespace rubinius {
     MethodVisibility* vis;
 
     Module* module = klass_;
+
+    // Check the global cache first!
+    GlobalCache::cache_entry* global_entry =
+      state->global_cache->lookup(module, name);
+
+    if(global_entry &&
+        global_entry->is_public &&
+        !global_entry->method_missing) {
+      this->method = global_entry->method;
+      this->module = global_entry->module;
+
+      return true;
+    }
 
     bool skip_vis_check = false;
 
@@ -32,6 +46,9 @@ namespace rubinius {
         if(!vis) {
           this->method = as<Executable>(entry);
           this->module = module;
+
+          state->global_cache->retain(state, klass_, name, this->module,
+                                      this->method, false, false);
           return true;
         }
 
@@ -60,6 +77,10 @@ namespace rubinius {
         } else {
           this->method = as<Executable>(vis->method());
           this->module = module;
+
+          state->global_cache->retain(state, klass_, name, this->module,
+                                      this->method, false,
+                                      !vis->public_p(state));
           return true;
         }
       }
@@ -70,12 +91,26 @@ namespace rubinius {
       if(module->nil_p()) return false;
     } while(1);
 
-    return true;
+    // Shouldn't be here!
+    rubinius::abort();
   }
 
-  bool InlineCache::fill_private(STATE, Symbol* name, Module* module) {
+  bool InlineCache::fill_private(STATE, Symbol* name, Module* start) {
     Object* entry;
     MethodVisibility* vis;
+
+    Module* module = start;
+
+    // Check the global cache first!
+    GlobalCache::cache_entry* global_entry =
+      state->global_cache->lookup(module, name);
+
+    if(global_entry && !global_entry->method_missing) {
+      this->method = global_entry->method;
+      this->module = global_entry->module;
+
+      return true;
+    }
 
     do {
       entry = module->method_table()->fetch(state, name);
@@ -100,11 +135,19 @@ namespace rubinius {
           if(!vis->method()->nil_p()) {
             this->method = as<Executable>(vis->method());
             this->module = module;
+            state->global_cache->retain(state, start, name, this->module,
+                                        this->method, false,
+                                        !vis->public_p(state));
+
             return true;
           }
         } else {
           this->method = as<Executable>(entry);
           this->module = module;
+
+          state->global_cache->retain(state, start, name, this->module,
+                                      this->method, false, false);
+
           return true;
         }
       }
@@ -115,10 +158,63 @@ namespace rubinius {
       if(module->nil_p()) return false;
     } while(1);
 
-    return true;
+    // Shouldn't be here!
+    rubinius::abort();
+  }
+
+  bool InlineCache::fill_method_missing(STATE, Module* module) {
+    Object* entry;
+    MethodVisibility* vis;
+
+    Symbol* name = G(sym_method_missing);
+
+    do {
+      entry = module->method_table()->fetch(state, name);
+
+      /* Nothing, there? Ok, keep looking. */
+      if(!entry->nil_p()) {
+
+        /* A 'false' method means to terminate method lookup.
+         * (eg. undef_method) */
+        if(entry == Qfalse) return false;
+
+        vis = try_as<MethodVisibility>(entry);
+
+        if(vis) {
+          /* The method was callable, but we need to keep looking
+           * for the implementation, so make the invocation bypass all further
+           * visibility checks.
+           *
+           * This is pretty much always where a subclass marks a superclass
+           * method as public. We don't move the method, we just put this
+           * marker into the method table. */
+          if(!vis->method()->nil_p()) {
+            this->method = as<Executable>(vis->method());
+            this->module = module;
+
+            return true;
+          }
+        } else {
+          this->method = as<Executable>(entry);
+          this->module = module;
+
+          return true;
+        }
+      }
+
+      module = module->superclass();
+
+      /* No more places to look, we couldn't find it. */
+      if(module->nil_p()) return false;
+    } while(1);
+
+    // Shouldn't be here!
+    rubinius::abort();
   }
 
   void InlineCache::run_wb(STATE, CompiledMethod* exec) {
+    update_seen_classes();
+
     exec->write_barrier(state, module);
     exec->write_barrier(state, method);
     exec->write_barrier(state, klass_);
@@ -131,7 +227,7 @@ namespace rubinius {
     cache->set_klass(recv->lookup_begin(state));
 
     if(!cache->fill_public(state, call_frame->self(), cache->name)) {
-      assert(cache->fill_private(state, G(sym_method_missing), cache->klass())
+      assert(cache->fill_method_missing(state, cache->klass())
              && "no method_missing!");
 
       args.unshift(state, cache->name);
@@ -141,6 +237,11 @@ namespace rubinius {
     }
 
     cache->run_wb(state, call_frame->cm);
+
+    // If we overflow, disable the cache.
+    if(cache->seen_classes_overflow() > 0) {
+      cache->execute_backend_ = disabled_cache;
+    }
 
     return cache->method->execute(state, call_frame, *cache, args);
   }
@@ -152,7 +253,7 @@ namespace rubinius {
     cache->set_klass(recv->lookup_begin(state));
 
     if(!cache->fill_private(state, cache->name, cache->klass())) {
-      assert(cache->fill_private(state, G(sym_method_missing), cache->klass())
+      assert(cache->fill_method_missing(state, cache->klass())
              && "no method_missing!");
 
       args.unshift(state, cache->name);
@@ -162,6 +263,11 @@ namespace rubinius {
     }
 
     cache->run_wb(state, call_frame->cm);
+
+    // If we overflow, disable the cache.
+    if(cache->seen_classes_overflow() > 0) {
+      cache->execute_backend_ = disabled_cache;
+    }
 
     return cache->method->execute(state, call_frame, *cache, args);
   }
@@ -175,7 +281,7 @@ namespace rubinius {
     Module* const start = call_frame->module()->superclass();
 
     if(!cache->fill_private(state, cache->name, start)) {
-      assert(cache->fill_private(state, G(sym_method_missing), start)
+      assert(cache->fill_method_missing(state,  start)
              && "no method_missing!");
 
       args.unshift(state, cache->name);
@@ -186,6 +292,11 @@ namespace rubinius {
 
     cache->run_wb(state, call_frame->cm);
 
+    // If we overflow, disable the cache.
+    if(cache->seen_classes_overflow() > 0) {
+      cache->execute_backend_ = disabled_cache;
+    }
+
     return cache->method->execute(state, call_frame, *cache, args);
   }
 
@@ -193,6 +304,7 @@ namespace rubinius {
                                    Arguments& args)
   {
     if(likely(cache->valid_p(state, args.recv()))) {
+      *cache->hits_++;
       return cache->method->execute(state, call_frame, *cache, args);
     }
 
@@ -203,11 +315,18 @@ namespace rubinius {
                                    Arguments& args)
   {
     if(likely(cache->valid_p(state, args.recv()))) {
+      *cache->hits_++;
       args.unshift(state, cache->name);
       return cache->method->execute(state, call_frame, *cache, args);
     }
 
     return cache->initialize(state, call_frame, args);
+  }
+
+  Object* InlineCache::disabled_cache(STATE, InlineCache* cache, CallFrame* call_frame,
+                                      Arguments& args)
+  {
+    return cache->send(state, call_frame, args);
   }
 
   void InlineCacheRegistry::add_cache(Symbol* sym, InlineCache* cache) {
--- vm/inline_cache.hpp
+++ vm/inline_cache.hpp
@@ -12,6 +12,31 @@ namespace rubinius {
   class CallFrame;
   class Arguments;
 
+  // How many receiver class have been seen to keep track of inside an IC
+  const static int cTrackedICHits = 3;
+
+  class InlineCacheHit {
+    Module* module_;
+    int hits_;
+
+  public:
+
+    InlineCacheHit()
+      : module_(0)
+      , hits_(0)
+    {}
+
+    Module* module() {
+      return module_;
+    }
+
+    int hits() {
+      return hits_;
+    }
+
+    friend class InlineCache;
+  };
+
   class InlineCache : public Dispatch {
     Module* klass_;
 
@@ -19,6 +44,10 @@ namespace rubinius {
 
     CacheExecutor initial_backend_;
     CacheExecutor execute_backend_;
+
+    int* hits_;
+    int seen_classes_overflow_;
+    InlineCacheHit seen_classes_[cTrackedICHits];
 
   public:
 
@@ -37,10 +66,14 @@ namespace rubinius {
                                Arguments& args);
 
     static Object* check_cache_mm(STATE, InlineCache* cache, CallFrame* call_frame,
-                               Arguments& args);
+                                  Arguments& args);
+
+    static Object* disabled_cache(STATE, InlineCache* cache, CallFrame* call_frame,
+                                  Arguments& args);
 
     bool fill_public(STATE, Object* self, Symbol* name);
     bool fill_private(STATE, Symbol* name, Module* start);
+    bool fill_method_missing(STATE, Module* start);
 
     void run_wb(STATE, CompiledMethod* exec);
 
@@ -49,6 +82,8 @@ namespace rubinius {
       , klass_(0)
       , initial_backend_(empty_cache)
       , execute_backend_(empty_cache)
+      , hits_(0)
+      , seen_classes_overflow_(0)
     {}
 
     void set_name(Symbol* sym) {
@@ -87,6 +122,33 @@ namespace rubinius {
 
     void clear() {
       klass_ = 0;
+    }
+
+    void update_seen_classes() {
+      for(int i = 0; i < cTrackedICHits; i++) {
+        Module* mod = seen_classes_[i].module_;
+        if(mod == klass_) {
+          hits_ = &seen_classes_[i].hits_;
+          return;
+        } else if(!mod) {
+          // An empty space, record it.
+          seen_classes_[i].module_ = mod;
+          hits_ = &seen_classes_[i].hits_;
+          return;
+        }
+      }
+
+      // Hmmm, what do we do when this is full? Just ignore them?
+      // For now, just keep track of how many times we overflow.
+      seen_classes_overflow_++;
+    }
+
+    int seen_classes_overflow() {
+      return seen_classes_overflow_;
+    }
+
+    int hits() {
+      return *hits_;
     }
 
   };
