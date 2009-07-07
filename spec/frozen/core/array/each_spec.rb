406a6651c3331254302359089eb7df3d834bd4e0
Brian Ford <bford@engineyard.com>
2009-07-07 16:54:48 -0700

    Fix Array#each specs according to [ruby-core:23633].

--- spec/frozen/core/array/each_spec.rb
+++ spec/frozen/core/array/each_spec.rb
@@ -1,33 +1,16 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
+# Modifying a collection while the contents are being iterated
+# gives undefined behavior. See
+# http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/23633
+
 describe "Array#each" do
   it "yields each element to the block" do
     a = []
     x = [1, 2, 3]
     x.each { |item| a << item }.should equal(x)
     a.should == [1, 2, 3]
-  end
-
-  it "yields each element to the block even if the array is changed during iteration" do
-    a = [1, 2, 3, 4, 5]
-    b = []
-    a.each {|x| b << x; a << x+5 if (x%2).zero? }
-    b.should == [1, 2, 3, 4, 5, 7, 9]
-  end
-
-  it "yields only elements that are still in the array" do
-    a = [0, 1, 2, 3, 4]
-    b = []
-    a.each {|x| b << x; a.pop if (x%2).zero? }
-    b.should == [0, 1, 2]
-  end
-
-  it "yields elements based on an internal index" do
-    a = [0, 1, 2, 3, 4]
-    b = []
-    a.each {|x| b << x; a.shift if (x%2).zero? }
-    b.should == [0, 2, 4]
   end
 
   it "yields each element to a block that takes multiple arguments" do
@@ -47,6 +30,7 @@ describe "Array#each" do
       lambda{ [1,2].each }.should raise_error(LocalJumpError)
     end
   end
+
   ruby_version_is '1.8.7' do
     it 'returns an Enumerator if no block given' do
       [1,2].each.should be_kind_of(enumerator_class)
