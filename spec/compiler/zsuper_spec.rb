b8566ca9d759a02aa548d7d0f87ce85f8245d1c4
Brian Ford <bford@engineyard.com>
2009-07-10 16:02:32 -0700

    Some more ZSuper compiler specs.

--- spec/compiler/zsuper_spec.rb
+++ spec/compiler/zsuper_spec.rb
@@ -18,4 +18,77 @@ describe "A Zsuper node" do
       end
     end
   end
+
+  relates <<-ruby do
+      def x(a)
+        super
+      end
+    ruby
+
+    parse do
+      [:defn, :x, [:args, :a], [:scope, [:block, [:zsuper]]]]
+    end
+
+    compile do |g|
+      in_method :x do |d|
+        d.push_local 0
+        d.push_block
+        d.send_super :x, 1
+      end
+    end
+  end
+
+  relates <<-ruby do
+      def x(&block)
+        super
+      end
+    ruby
+
+    parse do
+      [:defn, :x, [:args, :"&block"], [:scope, [:block, [:zsuper]]]]
+    end
+
+    compile do |g|
+      in_method :x do |d|
+        proc_lbl = d.new_label
+
+        d.push_block
+        d.dup
+        d.is_nil
+        d.git proc_lbl
+
+        d.push_const :Proc
+        d.swap
+        d.send :__from_block__, 1
+
+        proc_lbl.set!
+        d.set_local 0
+        d.pop
+
+        d.push_block
+        d.send_super :x, 0
+      end
+    end
+  end
+
+  relates <<-ruby do
+      def x(a, *as)
+        super
+      end
+    ruby
+
+    parse do
+      [:defn, :x, [:args, :a, :"*as"], [:scope, [:block, [:zsuper]]]]
+    end
+
+    compile do |g|
+      in_method :x do |d|
+        d.push_local 0
+        d.push_local 1
+        d.cast_array
+        d.push_block
+        d.send_super :x, 1, true
+      end
+    end
+  end
 end
