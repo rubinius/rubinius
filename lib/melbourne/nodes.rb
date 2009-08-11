4a1cef9032f1498f385f489b7b012c7f813a2cf9
Brian Ford <bford@engineyard.com>
2009-08-10 21:42:58 -0700

    Simplistic ascii node grapher.

--- lib/melbourne/nodes.rb
+++ lib/melbourne/nodes.rb
@@ -76,6 +76,76 @@ class Compiler
     end
   end
 
+  class AsciiGrapher
+    def initialize(ast)
+      @ast = ast
+    end
+
+    def print
+      graph_node @ast
+    end
+
+    def indented_print(level, value)
+      puts "#{" " * level}#{value}"
+    end
+
+    def print_node(node, level)
+      name = node.class.to_s.split("::").last
+      indented_print level, name
+    end
+
+    def graph_node(node, level=0)
+      print_node node, level
+      level += 2
+
+      nodes = []
+      node.instance_variables.each do |v|
+        next if v == "@compiler"
+
+        value = node.instance_variable_get v
+
+        # lame, yes. remove when Node doesn't have @body by default
+        next if v == "@body" and value.nil? and not v.respond_to? :body=
+
+        if value.kind_of? Node
+          nodes << value
+        else
+          graph_value v, value, level
+        end
+      end
+
+      nodes.each { |n| graph_node n, level }
+    end
+
+    def graph_simple(name, value, level)
+      puts "#{" " * level}#{name}: #{value}"
+    end
+
+    def graph_value(name, value, level)
+      case value
+      when NilClass
+        graph_simple name, value.inspect, level
+      when TrueClass, FalseClass, Symbol, Fixnum, String
+        graph_simple name, value, level
+      when Array
+        puts "#{" " * level}#{name}:"
+        nodes = []
+        value.each do |v|
+          if v.kind_of? Node
+            nodes << v
+          else
+            graph_value "-", v, level + 2
+          end
+        end
+
+        nodes.each { |n| graph_node n, level + 2 }
+      else
+        graph_simple name, value.class, level
+      end
+    end
+  end
+
+
   class Node
     attr_accessor :parent
 
@@ -99,6 +169,11 @@ class Compiler
         child.visit(ch_arg, &block) if child
       end
     end
+
+    def ascii_graph
+      AsciiGrapher.new(self).print
+    end
+
 
     class Alias < Node
       attr_accessor :to, :from
