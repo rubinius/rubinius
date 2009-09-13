7548459c05b1eab71548742cc2816b079799a091
Brian Ford <bford@engineyard.com>
2009-09-12 21:07:27 -0700

    Avoid using RSTRING in melbourne.
    *Any* cached string will significantly slow down melbourne because
    it is implemented as C-API calls to ruby code. That means every
    transition involving a C-API call triggers flushing and updating
    a cached RString structure.

--- lib/ext/melbourne/melbourne.cpp
+++ lib/ext/melbourne/melbourne.cpp
@@ -7,16 +7,18 @@ extern "C" {
 #endif
 
 VALUE melbourne_string_to_ast(VALUE self, VALUE source, VALUE name, VALUE line) {
-  bstring b_str = blk2bstr(RSTRING_PTR(source), RSTRING_LEN(source));
-  VALUE result = melbourne::string_to_ast(self, RSTRING_PTR(name), b_str, FIX2INT(line));
+  bstring b_str = blk2bstr(rb_str_ptr_readonly(source), rb_str_len(source));
+  VALUE result = melbourne::string_to_ast(self,
+      rb_str_ptr_readonly(name), b_str, FIX2INT(line));
   bdestroy(b_str);
 
   return result;
 }
 
 VALUE melbourne_file_to_ast(VALUE self, VALUE fname, VALUE start) {
-  FILE *file = fopen(RSTRING_PTR(fname), "r");
-  VALUE result = melbourne::file_to_ast(self, RSTRING_PTR(fname), file, FIX2INT(start));
+  FILE *file = fopen(rb_str_ptr_readonly(fname), "r");
+  VALUE result = melbourne::file_to_ast(self,
+      rb_str_ptr_readonly(fname), file, FIX2INT(start));
   fclose(file);
 
   return result;
