55993e95c9f2ab5aee46b7ed25c2358da9de52fb
Brian Ford <bford@engineyard.com>
2009-09-25 15:48:08 -0700

    Added more compiler specs for defined?.
    TODO: a lot more of these are needed but the current compiler
    has lame output in many cases.

--- spec/compiler/defined_spec.rb
+++ spec/compiler/defined_spec.rb
@@ -25,4 +25,68 @@ describe "A Defined node" do
       f.set!
     end
   end
+
+  relates "defined? a" do
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push :self
+      g.push_literal :a
+      g.push :true
+      g.send :__respond_to_eh__, 2
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "method"
+
+      f.set!
+    end
+  end
+
+  relates <<-ruby do
+      a = 1
+      defined? a
+    ruby
+
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push 1
+      g.set_local 0
+      g.pop
+
+      g.push :true
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "local-variable"
+
+      f.set!
+    end
+  end
+
+  relates "defined? X" do
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push_scope
+      g.push_literal :X
+      g.send :const_defined?, 1
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "constant"
+
+      f.set!
+    end
+  end
 end
