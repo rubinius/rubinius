ca023e759a58565eb0fdd73591df7be867beb0fd
Evan Phoenix <ephoenix@engineyard.com>
2009-09-29 00:33:27 -0700

    Update Struct to 1.8.7

--- kernel/common/struct.rb
+++ kernel/common/struct.rb
@@ -226,6 +226,16 @@ class Struct
     to_a.eql? other
   end
 
+  def each(&block)
+    return to_enum :each unless block_given?
+    values.each(&block)
+  end
+
+  def each_pair
+    return to_enum :each_pair unless block_given?
+    _attrs.map { |var| yield var, instance_variable_get("@#{var}") }
+  end
+
   ##
   # call-seq:
   #   struct.hash   => fixnum
