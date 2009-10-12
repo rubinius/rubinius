9c59a0aea245db516489512e24d0243e980fb6b0
Brian Ford <bford@engineyard.com>
2009-10-11 18:37:30 -0700

    More compiler defined specs.
    The tags for the current compiler are because the bytecode
    is wacky in these cases.

--- spec/compiler/defined_spec.rb
+++ spec/compiler/defined_spec.rb
@@ -52,22 +52,17 @@ describe "A Defined node" do
     ruby
 
     compile do |g|
-      t = g.new_label
-      f = g.new_label
-
       g.push 1
       g.set_local 0
       g.pop
 
-      g.push :true
-      g.git t
-      g.push :nil
-      g.goto f
-
-      t.set!
       g.push_literal "local-variable"
+    end
+  end
 
-      f.set!
+  relates "defined? x = 1" do
+    compile do |g|
+      g.push_literal "assignment"
     end
   end
 
@@ -142,6 +137,122 @@ describe "A Defined node" do
 
       t.set!
       g.push_literal "constant"
+
+      f.set!
+    end
+  end
+
+  relates "defined? self::A" do
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push_scope
+      g.push_literal "self::A"
+      g.send :const_path_defined?, 1
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "constant"
+
+      f.set!
+    end
+  end
+
+  relates "defined? self" do
+    compile do |g|
+      g.push_literal "self"
+    end
+  end
+
+  relates "defined? true" do
+    compile do |g|
+      g.push_literal "true"
+    end
+  end
+
+  relates "defined? false" do
+    compile do |g|
+      g.push_literal "false"
+    end
+  end
+
+  relates "defined? nil" do
+    compile do |g|
+      g.push_literal "nil"
+    end
+  end
+
+  relates "defined? @var" do
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push :self
+      g.push_literal :@var
+      g.send :instance_variable_defined?, 1
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "instance-variable"
+
+      f.set!
+    end
+  end
+
+  relates "defined? @@var" do
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push_scope
+      g.push_literal :@@var
+      g.send :class_variable_defined?, 1
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "class variable"
+
+      f.set!
+    end
+  end
+
+  relates "defined? :a" do
+    compile do |g|
+      g.push_literal "expression"
+    end
+  end
+
+  relates "defined? 'a'" do
+    compile do |g|
+      g.push_literal "expression"
+    end
+  end
+
+  relates "defined? 0" do
+    compile do |g|
+      g.push_literal "expression"
+    end
+  end
+
+  relates "defined? yield" do
+    compile do |g|
+      t = g.new_label
+      f = g.new_label
+
+      g.push_block
+      g.git t
+      g.push :nil
+      g.goto f
+
+      t.set!
+      g.push_literal "yield"
 
       f.set!
     end
