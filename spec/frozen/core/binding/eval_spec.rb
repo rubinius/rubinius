95e35c4a95d8782c6391f2aa6c9d37f60123e497
Evan Phoenix <ephoenix@engineyard.com>
2009-11-10 16:21:00 -0800

    Cleanup Binding#eval spec

--- spec/frozen/core/binding/eval_spec.rb
+++ spec/frozen/core/binding/eval_spec.rb
@@ -5,20 +5,13 @@ describe "Binding#eval" do
   ruby_version_is '1.8.7' do
     it "behaves like Kernel.eval(..., self)" do
       obj = BindingSpecs::Demo.new(1)
-      bind1 = obj.get_binding
-      bind2 = obj.dup.get_binding
-      { "@secret += square(3)" => 10,
-        "a" => true,
-        "class Inside ; end" => nil,
-        "Inside.name" => "BindingSpecs::Demo::Inside"
-      }.each do |test, result|
-        r1 = bind1.eval(test)
-        r2 = Kernel.eval(test, bind2)
-        r1.should == r2
-        r1.should == result
-      end
+      bind = obj.get_binding
+
+      bind.eval("@secret += square(3)").should == 10
+      bind.eval("a").should be_true
+
+      bind.eval("class Inside; end")
+      bind.eval("Inside.name").should == "BindingSpecs::Demo::Inside"
     end
-  
-    it "needs to be completed"
   end
 end
