db0630d5f2b22c21b4c07ccc83c79b959cab38ae
Evan Phoenix <ephoenix@engineyard.com>
2009-11-10 19:13:03 -0800

    Fix ENV.clear spec

--- spec/frozen/core/env/clear_spec.rb
+++ spec/frozen/core/env/clear_spec.rb
@@ -5,7 +5,13 @@ describe "ENV.clear" do
     orig = ENV.to_hash
     begin
       ENV.clear
-      env.should == {}
+
+      # This used 'env' the helper before. That shells out to 'env' which
+      # itself sets up certain environment variables before it runs, because
+      # the shell sets them up before it runs any command.
+      #
+      # Thusly, you can ONLY test this by asking through ENV itself.
+      ENV.size.should == 0
     ensure
       ENV.replace orig
     end
