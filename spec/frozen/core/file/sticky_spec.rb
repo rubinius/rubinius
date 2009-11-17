d7175ac3412e6dfce13d6574b8755a7949562dcf
Mikko Perttunen <cyndis@lyseo.edu.ouka.fi>
2009-11-17 13:36:19 +0200

    Updated specs for File.sticky? and FileTest.sticky?

--- spec/frozen/core/file/sticky_spec.rb
+++ spec/frozen/core/file/sticky_spec.rb
@@ -3,6 +3,7 @@ require File.dirname(__FILE__) + '/../../shared/file/sticky'
 
 describe "File.sticky?" do
   it_behaves_like :file_sticky, :sticky?, File
+  it_behaves_like :file_sticky_missing, :sticky?, File
 end
 
 describe "File.sticky?" do
--- spec/frozen/core/filetest/sticky_spec.rb
+++ spec/frozen/core/filetest/sticky_spec.rb
@@ -3,4 +3,5 @@ require File.dirname(__FILE__) + '/../../shared/file/sticky'
 
 describe "FileTest.sticky?" do
   it_behaves_like :file_sticky, :sticky?, FileTest
+  it_behaves_like :file_sticky_missing, :sticky?, FileTest
 end
