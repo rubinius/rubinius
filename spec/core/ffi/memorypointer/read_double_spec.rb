62059fdce9dc9df1ece9dc4d31dabf1d6fe7fec7
Mikko Perttunen <cyndis@lyseo.edu.ouka.fi>
2009-11-21 14:07:58 +0200

    Add specs for MemoryPointer#read_double, write_double

---
+++ spec/core/ffi/memorypointer/read_double_spec.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "FFI::MemoryPointer#read_double" do
+  it "needs to be reviewed for spec completeness"
+end
---
+++ spec/core/ffi/memorypointer/write_double_spec.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "FFI::MemoryPointer#write_double" do
+  it "needs to be reviewed for spec completeness"
+end
