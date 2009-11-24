95b93840f9ed52a52af8c876a13ec3a31807f7ae
Evan Phoenix <ephoenix@engineyard.com>
2009-11-24 15:30:11 -0800

    Flesh out IO#read_nonblock spec

--- spec/frozen/core/io/read_nonblock_spec.rb
+++ spec/frozen/core/io/read_nonblock_spec.rb
@@ -2,7 +2,51 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "IO#read_nonblock" do
+  before(:each) do
+    @read, @write = IO.pipe
+  end
+
+  after(:each) do
+    @read.close rescue nil
+    @write.close rescue nil
+  end
+
+  it "raises EAGAIN when there is no data" do
+    lambda { @read.read_nonblock(5) }.should raise_error(Errno::EAGAIN)
+  end
+
+  it "returns at most the number of bytes requested" do
+    @write << "hello"
+    @read.read_nonblock(4).should == "hell"
+  end
+
+  it "returns less data if that is all that is available" do
+    @write << "hello"
+    @read.read_nonblock(10).should == "hello"
+  end
+
+  not_compliant_on :rubinius do
+    it "changes the behavior of #read to nonblocking" do
+      @write << "hello"
+      @read.read_nonblock(5)
+
+      # Yes, use normal IO#read here. #read_nonblock has changed the internal
+      # flags of @read to be nonblocking, so now any normal read calls raise
+      # EAGAIN if there is no data.
+      lambda { @read.read(5) }.should raise_error(Errno::EAGAIN)
+    end
+  end
+
   it "raises IOError on closed stream" do
     lambda { IOSpecs.closed_file.read_nonblock(5) }.should raise_error(IOError)
+  end
+
+  it "raises EOFError when the end is reached" do
+    @write << "hello"
+    @write.close
+
+    @read.read_nonblock(5)
+
+    lambda { @read.read_nonblock(5) }.should raise_error(EOFError)
   end
 end
