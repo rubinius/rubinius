fdd68693481052b8b463beb7403f3ae235c2a581
Brian Ford <bford@engineyard.com>
2009-11-25 13:16:10 -0800

    Bump version to magic one dot oh rc one.

--- kernel/delta/ruby_constants.rb
+++ kernel/delta/ruby_constants.rb
@@ -4,7 +4,7 @@ RUBY_ENGINE       = "rbx"
 RUBY_PLATFORM     = Rubinius::HOST.dup
 RUBY_VERSION      = "1.8.7"
 RUBY_PATCHLEVEL   = 174
-RUBY_RELEASE_DATE = "2009-11-06"
+RUBY_RELEASE_DATE = "2009-11-25"
 RUBY_COPYRIGHT    = "rubinius - Copyright (C) 2006-2009 Evan Phoenix"
 
 # Deprecated Ruby constants
