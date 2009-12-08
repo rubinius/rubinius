306c17d48c274acf6ec7d3144142f94a4cde3e5d
Evan Phoenix <ephoenix@engineyard.com>
2009-12-07 18:08:47 -0800

    Whitespace fix

--- spec/frozen/core/string/split_spec.rb
+++ spec/frozen/core/string/split_spec.rb
@@ -5,7 +5,7 @@ describe "String#split with String" do
   it "returns an array of substrings based on splitting on the given string" do
     "mellow yellow".split("ello").should == ["m", "w y", "w"]
   end
-  
+
   it "suppresses trailing empty fields when limit isn't given or 0" do
     "1,2,,3,4,,".split(',').should == ["1", "2", "", "3", "4"]
     "1,2,,3,4,,".split(',', 0).should == ["1", "2", "", "3", "4"]
@@ -38,24 +38,24 @@ describe "String#split with String" do
     "xxx".split('x', 3).should == ["", "", "x"]
     "xxx".split('x', 4).should == ["", "", "", ""]
   end
-  
+
   it "doesn't suppress or limit fields when limit is negative" do
     "1,2,,3,4,,".split(',', -1).should == ["1", "2", "", "3", "4", "", ""]
     "1,2,,3,4,,".split(',', -5).should == ["1", "2", "", "3", "4", "", ""]
     "  a  b  c\nd  ".split("  ", -1).should == ["", "a", "b", "c\nd", ""]
     ",".split(",", -1).should == ["", ""]
   end
-  
+
   it "defaults to $; when string isn't given or nil" do
     begin
       old_fs = $;
-    
+
       [",", ":", "", "XY", nil].each do |fs|
         $; = fs
-        
+
         ["x,y,z,,,", "1:2:", "aXYbXYcXY", ""].each do |str|
           expected = str.split(fs || " ")
-          
+
           str.split(nil).should == expected
           str.split.should == expected
 
@@ -66,9 +66,9 @@ describe "String#split with String" do
       end
     ensure
       $; = old_fs
-    end    
+    end
   end
-    
+
   it "ignores leading and continuous whitespace when string is a single space" do
     " now's  the time  ".split(' ').should == ["now's", "the", "time"]
     " now's  the time  ".split(' ', -1).should == ["now's", "the", "time", ""]
@@ -76,33 +76,33 @@ describe "String#split with String" do
     "\t\n a\t\tb \n\r\r\nc\v\vd\v ".split(' ').should == ["a", "b", "c", "d"]
     "a\x00a b".split(' ').should == ["a\x00a", "b"]
   end
-  
+
   it "splits between characters when its argument is an empty string" do
     "hi!".split("").should == ["h", "i", "!"]
     "hi!".split("", -1).should == ["h", "i", "!", ""]
     "hi!".split("", 2).should == ["h", "i!"]
   end
-  
+
   it "tries converting its pattern argument to a string via to_str" do
     obj = mock('::')
     obj.should_receive(:to_str).and_return("::")
 
     "hello::world".split(obj).should == ["hello", "world"]
   end
-  
+
   it "tries converting limit to an integer via to_int" do
     obj = mock('2')
     obj.should_receive(:to_int).and_return(2)
 
     "1.2.3.4".split(".", obj).should == ["1", "2.3.4"]
   end
-  
+
   it "doesn't set $~" do
     $~ = nil
     "x.y.z".split(".")
     $~.should == nil
   end
-  
+
   it "returns subclass instances based on self" do
     ["", "x.y.z.", "  x  y  "].each do |str|
       ["", ".", " "].each do |pat|
@@ -110,23 +110,23 @@ describe "String#split with String" do
           StringSpecs::MyString.new(str).split(pat, limit).each do |x|
             x.class.should == StringSpecs::MyString
           end
-          
+
           str.split(StringSpecs::MyString.new(pat), limit).each do |x|
-            x.class.should == String 
+            x.class.should == String
           end
         end
       end
     end
   end
-  
+
   it "does not call constructor on created subclass instances" do
     # can't call should_not_receive on an object that doesn't yet exist
     # so failure here is signalled by exception, not expectation failure
-    
+
     s = StringSpecs::StringWithRaisingConstructor.new('silly:string')
     s.split(':').first.should == 'silly'
   end
-    
+
   it "taints the resulting strings if self is tainted" do
     ["", "x.y.z.", "  x  y  "].each do |str|
       ["", ".", " "].each do |pat|
@@ -134,13 +134,13 @@ describe "String#split with String" do
           str.dup.taint.split(pat).each do |x|
             x.tainted?.should == true
           end
-          
+
           str.split(pat.dup.taint).each do |x|
             x.tainted?.should == false
           end
         end
       end
-    end    
+    end
   end
 end
 
@@ -155,7 +155,7 @@ describe "String#split with Regexp" do
   it "treats negative limits as no limit" do
     "".split(%r!/+!, -1).should == []
   end
-  
+
   it "suppresses trailing empty fields when limit isn't given or 0" do
     "1,2,,3,4,,".split(/,/).should == ["1", "2", "", "3", "4"]
     "1,2,,3,4,,".split(/,/, 0).should == ["1", "2", "", "3", "4"]
@@ -188,24 +188,24 @@ describe "String#split with Regexp" do
     "xxx".split(/x/, 3).should == ["", "", "x"]
     "xxx".split(/x/, 4).should == ["", "", "", ""]
   end
-  
+
   it "doesn't suppress or limit fields when limit is negative" do
     "1,2,,3,4,,".split(/,/, -1).should == ["1", "2", "", "3", "4", "", ""]
     "1,2,,3,4,,".split(/,/, -5).should == ["1", "2", "", "3", "4", "", ""]
     "  a  b  c\nd  ".split(/\s+/, -1).should == ["", "a", "b", "c", "d", ""]
     ",".split(/,/, -1).should == ["", ""]
   end
-  
+
   it "defaults to $; when regexp isn't given or nil" do
     begin
       old_fs = $;
-    
+
       [/,/, /:/, //, /XY/, /./].each do |fs|
         $; = fs
-        
+
         ["x,y,z,,,", "1:2:", "aXYbXYcXY", ""].each do |str|
           expected = str.split(fs)
-          
+
           str.split(nil).should == expected
           str.split.should == expected
 
@@ -216,17 +216,17 @@ describe "String#split with Regexp" do
       end
     ensure
       $; = old_fs
-    end    
+    end
   end
-  
+
   it "splits between characters when regexp matches a zero-length string" do
     "hello".split(//).should == ["h", "e", "l", "l", "o"]
     "hello".split(//, -1).should == ["h", "e", "l", "l", "o", ""]
     "hello".split(//, 2).should == ["h", "ello"]
-    
+
     "hi mom".split(/\s*/).should == ["h", "i", "m", "o", "m"]
   end
-  
+
   it "includes all captures in the result array" do
     "hello".split(/(el)/).should == ["h", "el", "lo"]
     "hi!".split(/()/).should == ["h", "", "i", "", "!"]
@@ -250,17 +250,17 @@ describe "String#split with Regexp" do
     lambda {"1.2.3.4".split(".", "three")}.should raise_error(TypeError)
     lambda {"1.2.3.4".split(".", nil)    }.should raise_error(TypeError)
   end
-  
+
   it "doesn't set $~" do
     $~ = nil
     "x:y:z".split(/:/)
     $~.should == nil
   end
-  
+
   it "returns the original string if no matches are found" do
     "foo".split("\n").should == ["foo"]
   end
-  
+
   it "returns subclass instances based on self" do
     ["", "x:y:z:", "  x  y  "].each do |str|
       [//, /:/, /\s+/].each do |pat|
@@ -276,11 +276,11 @@ describe "String#split with Regexp" do
   it "does not call constructor on created subclass instances" do
     # can't call should_not_receive on an object that doesn't yet exist
     # so failure here is signalled by exception, not expectation failure
-    
+
     s = StringSpecs::StringWithRaisingConstructor.new('silly:string')
     s.split(/:/).first.should == 'silly'
   end
-  
+
   it "taints the resulting strings if self is tainted" do
     ["", "x:y:z:", "  x  y  "].each do |str|
       [//, /:/, /\s+/].each do |pat|
@@ -288,12 +288,12 @@ describe "String#split with Regexp" do
           str.dup.taint.split(pat).each do |x|
             x.tainted?.should == true
           end
-          
+
           str.split(pat.dup.taint).each do |x|
             x.tainted?.should == false
           end
         end
       end
-    end    
-  end  
+    end
+  end
 end
