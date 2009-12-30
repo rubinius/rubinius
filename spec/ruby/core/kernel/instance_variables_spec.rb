63366b7e7f512e0fa25cb804e4a8643b1cd4f61f
Dirkjan Bussink <d.bussink@gmail.com>
2009-12-30 23:16:18 +0100

    Add initial version of Kernel#instance_variables spec

--- spec/ruby/core/kernel/instance_variables_spec.rb
+++ spec/ruby/core/kernel/instance_variables_spec.rb
@@ -2,5 +2,32 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Kernel#instance_variables" do
-  it "needs to be reviewed for spec completeness"
+
+  describe "immediate values" do
+
+    it "returns an empty array if no instance variables are defined" do
+      0.instance_variables.should == []
+    end
+
+    it "returns the correct array if an instance variable is added" do
+      a = 0
+      a.instance_variable_set("@test", 1)
+      a.instance_variables.should == ["@test"]
+    end
+
+  end
+
+  describe "regular objects" do
+
+    it "returns an empty array if no instance variables are defined" do
+      Object.new.instance_variables.should == []
+    end
+
+    it "returns the correct array if an instance variable is added" do
+      a = Object.new
+      a.instance_variable_set("@test", 1)
+      a.instance_variables.should == ["@test"]
+    end
+
+  end
 end
