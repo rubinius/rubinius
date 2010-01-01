4a62bfb3f3af341ae30a71270ff671b7b64208d9
Evan Phoenix <ephoenix@engineyard.com>
2009-12-31 17:28:36 -0800

    Add testcase for Regexp#match and $~

--- spec/ruby/core/regexp/match_spec.rb
+++ spec/ruby/core/regexp/match_spec.rb
@@ -26,4 +26,13 @@ describe "Regexp#match on a successful match" do
   it "returns a MatchData object" do
     (/(.)(.)(.)/.match "abc").class.should == MatchData
   end
+
+  it "resets $~ if passed nil" do
+    # set $~
+    /./.match("a")
+    $~.should be_kind_of(MatchData)
+
+    /1/.match(nil)
+    $~.should be_nil
+  end
 end
