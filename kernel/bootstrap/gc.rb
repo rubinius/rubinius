abee1a28fe7301629210d0bcdca50988c1e65d3a
Evan Phoenix <ephoenix@engineyard.com>
2010-01-26 13:42:45 -0800

    Stub out GC methods, pass all specs.

--- kernel/bootstrap/gc.rb
+++ kernel/bootstrap/gc.rb
@@ -8,11 +8,37 @@ class GC
     raise PrimitiveFailure, "GC.run primitive failed"
   end
 
+  # Totally fake.
   def self.stress
-    false
+    @stress_level ||= false
   end
 
+  # Totally fake.
   def self.stress=(flag)
-    raise NotImplementedError
+    @stress_level = !!flag
+  end
+
+  # Totally fake.
+  @enabled = true
+
+  def self.enable
+    # We don't support disable, so sure! enabled!
+    ret = !@enabled
+    @enabled = true
+
+    return ret
+  end
+
+  # Totally fake.
+  def self.disable
+    # Treat this like a request that we don't honor.
+    ret = !@enabled
+    @enabled = false
+
+    return ret
+  end
+
+  def garbage_collect
+    GC.start
   end
 end
