1f21ac366196675d4ecd9a2b07edc49985ea453f
Evan Phoenix <ephoenix@engineyard.com>
2010-03-01 15:19:37 -0800

    Lame rails ivar conflict fix

--- spec/core/hash/allocate_spec.rb
+++ spec/core/hash/allocate_spec.rb
@@ -8,7 +8,7 @@ describe "Hash.allocate" do
     h.instance_variable_get(:@size).should == 0
     h.instance_variable_get(:@capacity).should == Hash::MIN_SIZE
     h.instance_variable_get(:@mask).should == Hash::MIN_SIZE - 1
-    h.instance_variable_get(:@max).should == Hash::MAX_ENTRIES
+    h.instance_variable_get(:@max_entries).should == Hash::MAX_ENTRIES
     h.instance_variable_get(:@entries).should be_kind_of(Rubinius::Tuple)
   end
 end
