51e0b6417f21e14135fa5b92abd2da2b602ba4a3
Evan Phoenix <ephoenix@engineyard.com>
2010-03-04 19:28:26 -0800

    More Regexp#inspect cases

--- spec/ruby/core/regexp/inspect_spec.rb
+++ spec/ruby/core/regexp/inspect_spec.rb
@@ -5,12 +5,16 @@ describe "Regexp#inspect" do
     /ab+c/ix.inspect.should == "/ab+c/ix"
     /a(.)+s/n.inspect.should =~ %r|/a(.)+s/n?|  # Default 'n' may not appear
     # 1.9 doesn't round-trip the encoding flags, such as 'u'. This is
-    # seemingly by design.  
+    # seemingly by design.
     /a(.)+s/m.inspect.should == "/a(.)+s/m"     # But a specified one does
   end
-  
+
   it "correctly escapes forward slashes /" do
     Regexp.new("/foo/bar").inspect.should == "/\\/foo\\/bar/"
     Regexp.new("/foo/bar[/]").inspect.should == "/\\/foo\\/bar[\\/]/"
-  end  
+  end
+
+  it "doesn't over escape forward slashes" do
+    /\/foo\/bar/.inspect.should == '/\/foo\/bar/'
+  end
 end
