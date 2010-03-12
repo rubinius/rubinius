998db1bdec1caf3c4bb552ed98459995b125c2d4
Brian Ford <bford@engineyard.com>
2010-03-11 23:06:49 -0800

    Added #new_io and #new_fd mspec helpers.

--- mspec/lib/mspec/helpers/io.rb
+++ mspec/lib/mspec/helpers/io.rb
@@ -19,3 +19,18 @@ class IOStub < String
     self
   end
 end
+
+class Object
+  # Creates a "bare" file descriptor (i.e. one that is not associated
+  # with any Ruby object). The file descriptor can safely be passed
+  # to IO.new without creating a Ruby object alias to the fd.
+  def new_fd(name, mode="w:utf-8")
+    IO.sysopen name, fmode(mode)
+  end
+
+  # Creates an IO instance for a temporary file name. The file
+  # must be deleted.
+  def new_io(name, mode="w:utf-8")
+    IO.new new_fd(name, fmode(mode))
+  end
+end
--- mspec/spec/helpers/io_spec.rb
+++ mspec/spec/helpers/io_spec.rb
@@ -1,4 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+require 'mspec/helpers/io'
+require 'mspec/helpers/fs'
+require 'mspec/helpers/tmp'
 
 describe IOStub do
   before :each do
@@ -39,5 +42,58 @@ describe IOStub do
 
   it "provides a flush method that does nothing and returns self" do
     @out.flush.should == @out
+  end
+end
+
+describe Object, "#new_fd" do
+  before :each do
+    @name = tmp("io_specs")
+  end
+
+  after :each do
+    @io.close unless @io.closed?
+    rm_r @name
+  end
+
+  it "returns a Fixnum that can be used to create an IO instance" do
+    fd = new_fd @name
+    fd.should be_an_instance_of(Fixnum)
+
+    @io = IO.new fd
+    @io.sync = true
+    @io.print "io data"
+
+    IO.read(@name).should == "io data"
+  end
+end
+
+describe Object, "#new_io" do
+  before :each do
+    @name = tmp("io_specs.txt")
+  end
+
+  after :each do
+    @io.close if @io and !@io.closed?
+    rm_r @name
+  end
+
+  it "returns an IO instance" do
+    @io = new_io @name
+    @io.should be_an_instance_of(IO)
+  end
+
+  it "opens the IO for reading if passed 'r'" do
+    touch(@name) { |f| f.print "io data" }
+    @io = new_io @name, "r"
+    @io.read.should == "io data"
+    lambda { @io.puts "more data" }.should raise_error(IOError)
+  end
+
+  it "opens the IO for writing if passed 'w'" do
+    @io = new_io @name, "w"
+    @io.sync = true
+
+    @io.print "io data"
+    IO.read(@name).should == "io data"
   end
 end
