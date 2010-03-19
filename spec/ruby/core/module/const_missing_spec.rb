1cfc0e55bdee5127783cc75ebcf28e1456b4de86
Evan Phoenix <ephoenix@engineyard.com>
2010-03-19 16:04:28 -0700

    Add spec for #const_missing raising NameError

--- spec/ruby/core/module/const_missing_spec.rb
+++ spec/ruby/core/module/const_missing_spec.rb
@@ -9,4 +9,10 @@ describe "Module#const_missing" do
   it "is called when an undefined constant is referenced via #const_get" do
     ConstantSpecs::ClassA.const_get(:CS_CONSTX).should == :CS_CONSTX
   end
+
+  it "raises NameError and includes the name of the value that wasn't found" do
+    lambda {
+      ConstantSpecs.const_missing("HelloMissing")
+    }.should raise_error(NameError, /ConstantSpecs::HelloMissing/)
+  end
 end
