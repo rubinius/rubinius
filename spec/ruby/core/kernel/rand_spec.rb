4fd66101107365179b572a1303b7a49bfe296ea9
Matthew Draper <matthew@trebex.net>
2010-04-14 23:04:46 +0930

    Remove the last test that over-specifies randomness

--- spec/ruby/core/kernel/rand_spec.rb
+++ spec/ruby/core/kernel/rand_spec.rb
@@ -30,33 +30,6 @@ describe "Kernel.rand" do
     [0, 1, 2, 3].should include(rand(-4))
   end
 
-  it "produces a vaguely even distribution" do
-    # Like several other rand specs, this one is based on probabilities;
-    # with a sufficiently high quality RNG, this spec should fail
-    # occassionally.
-
-    # Probability of random failure: 1 in 10**20
-
-    counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    5000.times do |i|
-      n = rand(10)
-      counts[n] += 1
-    end
-
-    # Under a perfect distribution (which obviously wouldn't be random),
-    # all values in 'counts' are 500.
-    # We ignore the highest and lowest value, then apply a +/-30%
-    # tolerance on the remaining 8 -- this is a trade-off between
-    # strictness, and likelihood of random failure.  For comparison, a
-    # +/-20% tolerance would increase probability of random failure to
-    # about 1 in 10**10, and +/-10% would make the probability
-    # approximately 1 in 100.
-
-    counts.sort!
-    counts[1].should > 350
-    counts[-2].should < 650
-  end
-
   it "never returns a value greater or equal to 1.0 with no arguments" do
     1000.times do
       (0...1.0).should include(rand)
