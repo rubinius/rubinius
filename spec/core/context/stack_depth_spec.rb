f976513ddc45466df16ffe4ac2007f77e3a10cfa
Brian Ford <bford@engineyard.com>
2010-05-05 18:07:14 -0700

    Cleaning up some specs and CI tags.

--- spec/core/context/stack_depth_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
-
-describe "MethodContext#stack_depth" do
-  it "returns the depth of the bytecode operand stack" do
-    ctx = Rubinius::Task.current.current_context
-    ctx.stack_depth.should == 1
-  end
-end
