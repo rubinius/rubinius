437efdd12e24c443e1be07bbb345498a9e88700d
Brian Ford <bford@engineyard.com>
2010-05-05 18:38:12 -0700

    Quarantine Ping spec.

--- spec/ruby/library/ping/pingecho_spec.rb
+++ spec/ruby/library/ping/pingecho_spec.rb
@@ -1,15 +1,9 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is ""..."1.9" do
-  
   require 'ping'
 
   describe "Ping.pingecho" do
-
-    it "responds to pingecho method" do
-      Ping.should respond_to(:pingecho)
-    end
-
     it "pings a host using the correct number of arguments" do
       Ping.pingecho('127.0.0.1', 10, 7).should be_true
       Ping.pingecho('127.0.0.1', 10).should be_true
@@ -24,8 +18,12 @@ ruby_version_is ""..."1.9" do
       Ping.pingecho('127.0.0.1', 'invalid timeout').should be_false
     end
 
-    it "returns false if the host is invalid" do
-      Ping.pingecho(0).should be_false
+    # Is it ever possible to spec invalid hosts? I think the consensus
+    # is building towards "no".
+    quarantine! do
+      it "returns false if the host is invalid" do
+        Ping.pingecho(0).should be_false
+      end
     end
   end
 end
