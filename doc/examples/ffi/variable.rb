63553ff68dc64227babb7145b54c2920bef1ce83
Brian Ford <bford@engineyard.com>
2010-05-12 18:09:05 -0700

    Updates for most files under doc directory.

--- doc/examples/ffi/variable.rb
+++
@@ -1,8 +0,0 @@
-pr = FFI.create_function nil, "printf", [:string, :int], :int
-p pr
-pr.call("%d rocks\n", 47)
-
-pf = NativeFunction::Variable.new nil, "printf", [:string], :int
-p pf
-pf.call [:int], "%d really rocks\n", 42
-pf[:int].call("%d really rocks\n", 42)
