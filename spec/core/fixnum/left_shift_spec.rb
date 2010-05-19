b2acbcd0be271f1f04911c8aea65094c6c3e544a
Brian Ford <bford@engineyard.com>
2010-05-19 11:07:22 -0700

    Added rbx specific Fixnum#<<, #>> specs.

---
+++ spec/core/fixnum/left_shift_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Fixnum#<< with n << m" do
+  it "raises a RangeError when m > 0 and the Integer argument is not in Fixnum range" do
+    lambda { 3 << (fixnum_max() + 1) }.should raise_error(RangeError)
+  end
+
+  it "raises a RangeError when m > 0 the Float argument is not in Fixnum range" do
+    lambda { 3 << (fixnum_max() + 1).to_f }.should raise_error(RangeError)
+  end
+
+  it "raises a RangeError when m < 0 and the Integer argument is not in Fixnum range" do
+    lambda { 3 << -(fixnum_max() + 1) }.should raise_error(RangeError)
+  end
+
+  it "raises a RangeError when m < 0 the Float argument is not in Fixnum range" do
+    lambda { 3 << -(fixnum_max() + 1).to_f }.should raise_error(RangeError)
+  end
+end
