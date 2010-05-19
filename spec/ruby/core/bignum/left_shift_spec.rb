15d14c6ed4ce6431d28f70d61d61ff80512be869
Brian Ford <bford@engineyard.com>
2010-05-19 14:01:59 -0700

    Fixed Bignum#<<, #>> specs.

--- spec/ruby/core/bignum/left_shift_spec.rb
+++ spec/ruby/core/bignum/left_shift_spec.rb
@@ -1,57 +1,81 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe "Bignum#<<" do
+describe "Bignum#<< with n << m" do
   before(:each) do
-    @bignum = bignum_value(9)
-  end
-  
-  it "returns self shifted the given amount of bits to the left" do
-    (@bignum << 4).should == 147573952589676413072
-    (@bignum << 9).should == 4722366482869645218304
+    @bignum = bignum_value() * 16
   end
 
-  it "performs a right-shift if given a negative value" do
-    (@bignum << -2).should == (@bignum >> 2)
-    (@bignum << -4).should == (@bignum >> 4)
+  it "returns n shifted left m bits when n > 0, m > 0" do
+    (@bignum << 4).should == 2361183241434822606848
   end
 
-  it "tries to convert the given argument to an Integer using to_int" do
-    (@bignum << 4.5).should == 147573952589676413072
-    
-    (obj = mock('4')).should_receive(:to_int).and_return(4)
-    (@bignum << obj).should == 147573952589676413072
+  it "returns n shifted left m bits when n < 0, m > 0" do
+    (-@bignum << 9).should == -75557863725914323419136
   end
 
-  it "raises a TypeError when the given argument can't be converted to Integer" do
-    obj = mock("Converted to Integer")
-    lambda { @bignum << obj }.should raise_error(TypeError)
-    
+  it "returns n shifted right m bits when n > 0, m < 0" do
+    (@bignum << -1).should == 73786976294838206464
+  end
+
+  it "returns n shifted right m bits when n < 0, m < 0" do
+    (-@bignum << -2).should == -36893488147419103232
+  end
+
+  it "returns n when n > 0, m == 0" do
+    (@bignum << 0).should == @bignum
+  end
+
+  it "returns n when n < 0, m == 0" do
+    (-@bignum << 0).should == -@bignum
+  end
+
+  it "returns 0 when m < 0 and m == p where 2**p > n >= 2**(p-1)" do
+    (@bignum << -68).should == 0
+  end
+
+  not_compliant_on :rubinius do
+    it "returns 0 when m < 0 and m is a Bignum" do
+      (@bignum << -bignum_value()).should == 0
+    end
+  end
+
+  deviates_on :rubinius do
+    it "raises a RangeError when m < 0 and m is a Bignum" do
+      lambda { @bignum << -bignum_value() }.should raise_error(RangeError)
+    end
+  end
+
+  it "returns a Fixnum == fixnum_max() when (fixnum_max() * 2) << -1 and n > 0" do
+    result = (fixnum_max() * 2) << -1
+    result.should be_an_instance_of(Fixnum)
+    result.should == fixnum_max()
+  end
+
+  it "returns a Fixnum == fixnum_min() when (fixnum_min() * 2) << -1 and n < 0" do
+    result = (fixnum_min() * 2) << -1
+    result.should be_an_instance_of(Fixnum)
+    result.should == fixnum_min()
+  end
+
+  it "calls #to_int to convert the argument to an Integer" do
+    obj = mock("4")
+    obj.should_receive(:to_int).and_return(4)
+
+    (@bignum << obj).should == 2361183241434822606848
+  end
+
+  it "raises a TypeError when #to_int does not return an Integer" do
+    obj = mock("a string")
     obj.should_receive(:to_int).and_return("asdf")
+
     lambda { @bignum << obj }.should raise_error(TypeError)
   end
 
-  platform_is :wordsize => 32 do
-    it "raises a RangeError when the given argument is a Bignum" do
-      lambda { @bignum << bignum_value }.should raise_error(RangeError)
-      lambda { -@bignum << bignum_value }.should raise_error(RangeError)
-      
-      obj = mock("Converted to Integer")
-      obj.should_receive(:to_int).exactly(2).times.and_return(bignum_value)
-      lambda { @bignum << obj }.should raise_error(RangeError)
-      lambda { -@bignum << obj }.should raise_error(RangeError)
-    end
+  it "raises a TypeError when passed nil" do
+    lambda { @bignum << nil }.should raise_error(TypeError)
   end
 
-  platform_is :wordsize => 64 do
-    it "raises a RangeError when the given argument is a Bignum" do
-      # check against 2**64 for 64-bit machines.
-      lambda { @bignum << (bignum_value << 1) }.should raise_error(RangeError)
-      lambda { -@bignum << (bignum_value << 1) }.should raise_error(RangeError)
-      
-      obj = mock("Converted to Integer")
-      obj.should_receive(:to_int).exactly(2).times.and_return(bignum_value << 1)
-      lambda { @bignum << obj }.should raise_error(RangeError)
-      lambda { -@bignum << obj }.should raise_error(RangeError)
-    end
+  it "raises a TypeError when passed a String" do
+    lambda { @bignum << "4" }.should raise_error(TypeError)
   end
 end
--- spec/ruby/core/bignum/right_shift_spec.rb
+++ spec/ruby/core/bignum/right_shift_spec.rb
@@ -1,48 +1,81 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe "Bignum#>>" do
-  before(:each) do 
-    @bignum = bignum_value(90812)
+describe "Bignum#>> with n >> m" do
+  before(:each) do
+    @bignum = bignum_value() * 16
   end
 
-  it "returns self shifted the given amount of bits to the right" do
-    (@bignum >> 1).should == 4611686018427433310
-    (@bignum >> 3).should == 1152921504606858327
+  it "returns n shifted right m bits when n > 0, m > 0" do
+    (@bignum >> 1).should == 73786976294838206464
   end
 
-  it "performs a left-shift if given a negative value" do
-    (@bignum >> -1).should == (@bignum << 1)
-    (@bignum >> -3).should == (@bignum << 3)
+  it "returns n shifted right m bits when n < 0, m > 0" do
+    (-@bignum >> 2).should == -36893488147419103232
   end
-  
-  it "tries to convert the given argument to an Integer using to_int" do
-    (@bignum >> 1.3).should == 4611686018427433310
-    
-    (obj = mock('1')).should_receive(:to_int).and_return(1)
-    (@bignum >> obj).should == 4611686018427433310
+
+  it "returns n shifted left m bits when  n > 0, m < 0" do
+    (@bignum >> -2).should == 590295810358705651712
   end
-  
-  it "raises a TypeError when the given argument can't be converted to Integer" do
-    obj = mock('asdf')
-    lambda { @bignum >> obj }.should raise_error(TypeError)
-    
+
+  it "returns n shifted left m bits when  n < 0, m < 0" do
+    (-@bignum >> -3).should == -1180591620717411303424
+  end
+
+  it "returns n when n > 0, m == 0" do
+    (@bignum >> 0).should == @bignum
+  end
+
+  it "returns n when n < 0, m == 0" do
+    (-@bignum >> 0).should == -@bignum
+  end
+
+  it "returns 0 when m > 0 and m == p where 2**p > n >= 2**(p-1)" do
+    (@bignum >> 68).should == 0
+  end
+
+  not_compliant_on :rubinius do
+    it "returns 0 when m is a Bignum" do
+      (@bignum >> bignum_value()).should == 0
+    end
+  end
+
+  deviates_on :rubinius do
+    it "raises a RangeError when m is a Bignum" do
+      lambda { @bignum >> bignum_value() }.should raise_error(RangeError)
+    end
+  end
+
+  it "returns a Fixnum == fixnum_max() when (fixnum_max() * 2) >> 1 and n > 0" do
+    result = (fixnum_max() * 2) >> 1
+    result.should be_an_instance_of(Fixnum)
+    result.should == fixnum_max()
+  end
+
+  it "returns a Fixnum == fixnum_min() when (fixnum_min() * 2) >> 1 and n < 0" do
+    result = (fixnum_min() * 2) >> 1
+    result.should be_an_instance_of(Fixnum)
+    result.should == fixnum_min()
+  end
+
+  it "calls #to_int to convert the argument to an Integer" do
+    obj = mock("2")
+    obj.should_receive(:to_int).and_return(2)
+
+    (@bignum >> obj).should == 36893488147419103232
+  end
+
+  it "raises a TypeError when #to_int does not return an Integer" do
+    obj = mock("a string")
     obj.should_receive(:to_int).and_return("asdf")
+
     lambda { @bignum >> obj }.should raise_error(TypeError)
   end
 
-  it "returns 0 when the given argument is a Bignum and self is positive" do
-    (@bignum >> bignum_value).should == 0
-    
-    obj = mock("Converted to Integer")
-    obj.should_receive(:to_int).and_return(bignum_value)
-    (@bignum >> obj).should == 0
+  it "raises a TypeError when passed nil" do
+    lambda { @bignum >> nil }.should raise_error(TypeError)
   end
-  
-  it "returns -1 when the given argument is a Bignum and self is negative" do
-    (-@bignum >> bignum_value).should == -1
 
-    obj = mock("Converted to Integer")
-    obj.should_receive(:to_int).and_return(bignum_value)
-    (-@bignum >> obj).should == -1
+  it "raises a TypeError when passed a String" do
+    lambda { @bignum >> "4" }.should raise_error(TypeError)
   end
 end
