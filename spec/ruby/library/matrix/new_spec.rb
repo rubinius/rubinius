19957da5eab49d536911ecff7f690fdfdcd4c22f
Brian Ford <bford@engineyard.com>
2010-05-20 11:56:10 -0700

    Fixed Matrix.new spec. @bugfix

--- spec/ruby/library/matrix/new_spec.rb
+++ spec/ruby/library/matrix/new_spec.rb
@@ -1,10 +1,8 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require 'matrix'
 
-describe "Matrix#new" do
-  
-  it "is not accessible" do
-    lambda { Matrix.new }.should raise_error(NoMethodError)
+describe "Matrix.new" do
+  it "is private" do
+    Matrix.should have_private_method(:new)
   end
-  
 end
