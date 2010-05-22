cc9d10f14ee89c46499a8565ce1a2ae56fa5b43e
Matthew Draper <matthew@trebex.net>
2010-05-23 03:32:39 +0930

    Spec Time#eql?(non_time_value).

--- spec/ruby/core/time/eql_spec.rb
+++ spec/ruby/core/time/eql_spec.rb
@@ -7,4 +7,10 @@ describe "Time#eql?" do
     Time.at(100, 100).should_not eql(Time.at(100, 99))
     Time.at(100, 100).should_not eql(Time.at(99, 100))
   end  
+
+  it "returns false when given a non-time value" do
+    Time.at(100, 100).should_not eql("100")
+    Time.at(100, 100).should_not eql(100)
+    Time.at(100, 100).should_not eql(100.1)
+  end
 end
