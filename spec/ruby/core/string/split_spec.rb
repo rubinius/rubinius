be4643a9f3c45fe4f34dd72cd3ea85d84682d2a9
Evan Phoenix <ephoenix@engineyard.com>
2010-05-25 12:05:33 -0700

    Add spec for String#split using the encoding of the Regexp

--- spec/ruby/core/string/split_spec.rb
+++ spec/ruby/core/string/split_spec.rb
@@ -247,6 +247,16 @@ describe "String#split with Regexp" do
     ary.should == ["こ", "に", "ち", "わ"]
   end
 
+  it "respects the encoding of the regexp when splitting between characters" do
+    str = "\303\202"
+
+    $KCODE = "a"
+
+    ary = str.split(//u)
+    ary.size.should == 1
+    ary.should == ["\303\202"]
+  end
+
   it "includes all captures in the result array" do
     "hello".split(/(el)/).should == ["h", "el", "lo"]
     "hi!".split(/()/).should == ["h", "", "i", "", "!"]
