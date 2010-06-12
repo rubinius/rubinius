746cc91db0d2d93a7ca17c2a9435b55bb56a2432
Brian Ford <bford@engineyard.com>
2010-06-11 17:19:15 -0700

    More specs for Numeric#<=>.

--- spec/ruby/core/numeric/comparison_spec.rb
+++ spec/ruby/core/numeric/comparison_spec.rb
@@ -9,11 +9,40 @@ describe "Numeric#<=>" do
   it "returns 0 if self equals other" do
     (@obj <=> @obj).should == 0
   end
-  
+
   it "returns nil if self does not equal other" do
     (@obj <=> NumericSub.new).should == nil
     (@obj <=> 10).should == nil
     (@obj <=> -3.5).should == nil
     (@obj <=> bignum_value).should == nil
+  end
+
+  describe "with subclasses of Numeric" do
+    before :each do
+      @a = NumericSpecs::Comparison.new
+      @b = NumericSpecs::Comparison.new
+
+      ScratchPad.clear
+    end
+
+    it "is called when instances are compared with #<" do
+      (@a < @b).should be_false
+      ScratchPad.recorded.should == :numeric_comparison
+    end
+
+    it "is called when instances are compared with #<=" do
+      (@a <= @b).should be_false
+      ScratchPad.recorded.should == :numeric_comparison
+    end
+
+    it "is called when instances are compared with #>" do
+      (@a > @b).should be_true
+      ScratchPad.recorded.should == :numeric_comparison
+    end
+
+    it "is called when instances are compared with #>=" do
+      (@a >= @b).should be_true
+      ScratchPad.recorded.should == :numeric_comparison
+    end
   end
 end
--- spec/ruby/core/numeric/fixtures/classes.rb
+++ spec/ruby/core/numeric/fixtures/classes.rb
@@ -1,3 +1,15 @@
+module NumericSpecs
+  class Comparison < Numeric
+    # This method is used because we cannot define
+    # singleton methods on subclasses of Numeric,
+    # which is needed for a.should_receive to work.
+    def <=>(other)
+      ScratchPad.record :numeric_comparison
+      1
+    end
+  end
+end
+
 class NumericSub < Numeric
   # Allow methods to be mocked
   def singleton_method_added(val)
