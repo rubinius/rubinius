d4a4a56bb9ce857474780bfc694a4b4a070c426a
Evan Phoenix <ephoenix@engineyard.com>
2010-06-25 16:38:44 -0700

    Use Kernel.rand rather than rand to avoid AS conflict

--- kernel/common/array.rb
+++ kernel/common/array.rb
@@ -1295,7 +1295,7 @@ class Array
   # If the array is empty, the first form returns nil, and the second
   # form returns an empty array.
   def choice(n=undefined)
-    return at(rand(size)) if n.equal? undefined
+    return at(Kernel.rand(size)) if n.equal? undefined
 
     n = Type.coerce_to(n, Fixnum, :to_int)
     raise ArgumentError, "negative array size" if n < 0
@@ -1304,7 +1304,7 @@ class Array
     result = Array.new(self)
 
     n.times do |i|
-      r = i + rand(size - i)
+      r = i + Kernel.rand(size - i)
       result.tuple.swap(i,r)
     end
 
@@ -1385,7 +1385,7 @@ class Array
   # Shuffles elements in self in place.
   def shuffle!
     size.times do |i|
-      r = i + rand(size - i)
+      r = i + Kernel.rand(size - i)
       @tuple.swap(i,r)
     end
     self
