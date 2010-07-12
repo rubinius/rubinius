ef67d3296a167fa914e6e062e174480138e7072c
Evan Phoenix <ephoenix@engineyard.com>
2010-07-12 00:34:22 -0700

    Fix Struct.specialize_initialize for subclasses of Struct

--- kernel/delta/struct.rb
+++ kernel/delta/struct.rb
@@ -11,6 +11,23 @@ class Struct
   end
 
   def self.specialize_initialize
+    # Because people are crazy, they subclass Struct directly, ie.
+    #  class Craptastic < Struct
+    #
+    # NOT
+    #
+    #  class Fine < Struct.new(:x, :y)
+    #
+    # When they do this craptastic act, they'll sometimes define their
+    # own #initialize and super into Struct#initialize.
+    #
+    # When they do this and then do Craptastic.new(:x, :y), this code
+    # will accidentally shadow they're #initialize. So for now, only run
+    # the specialize if we're trying new Struct's directly from Struct itself,
+    # not a craptastic Struct subclass.
+
+    return unless self.equal? Struct
+
     attrs = self::STRUCT_ATTRS
 
     args = []
