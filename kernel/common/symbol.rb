b3d1019b0fe45911c64485031733d2936ee0cdc8
Evan Phoenix <ephoenix@engineyard.com>
2010-07-13 17:46:59 -0700

    Don't use self in a block that might be instance_evald. Fixes #399.

--- kernel/common/symbol.rb
+++ kernel/common/symbol.rb
@@ -40,7 +40,13 @@ class Symbol
 
   # Returns a Proc object which respond to the given method by sym.
   def to_proc
-    Proc.new { |*args| args.shift.__send__(self, *args) }
+    # Put sym in the outer enclosure so that this proc can be instance_eval'd.
+    # If we used self in the block and the block is passed to instance_eval, then
+    # self becomes the object instance_eval was called on. So to get around this,
+    # we leave the symbol in sym and use it in the block.
+    #
+    sym = self
+    Proc.new { |*args| args.shift.__send__(sym, *args) }
   end
 
 end
