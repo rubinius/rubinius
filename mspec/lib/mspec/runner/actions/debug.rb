57405cc016dea7dc87413277bff0497861457cf6
Evan Phoenix <ephoenix@engineyard.com>
2010-07-26 10:53:20 -0700

    Have mspec --spec-debug use the rbx reference debugger

--- mspec/lib/mspec/runner/actions/debug.rb
+++ mspec/lib/mspec/runner/actions/debug.rb
@@ -1,8 +1,25 @@
 require 'mspec/runner/actions/filter'
 
 class DebugAction < ActionFilter
+  def initialize(tags=nil, descs=nil)
+    super
+
+    if defined?(RUBY_ENGINE) and RUBY_ENGINE == "rbx"
+      require 'debugger'
+      @use_rubinius = true
+    else
+      @use_rubinius = false
+    end
+  end
+
   def before(state)
-    Kernel.debugger if self === state.description
+    if self === state.description
+      if @use_rubinius
+        Debugger.here
+      else
+        Kernel.debugger
+      end
+    end
   end
 
   def register
