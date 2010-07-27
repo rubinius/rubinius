feb133d5c18fd098d3a9901dcfbf0529276617bd
Evan Phoenix <ephoenix@engineyard.com>
2010-07-27 15:44:09 -0700

    Add missing file

---
+++ kernel/bootstrap/iseq.rb
@@ -0,0 +1,29 @@
+module Rubinius
+  class InstructionSequence
+    def initialize(size)
+      if size.kind_of? Tuple
+        @opcodes = size
+      else
+        @opcodes = Tuple.new(size)
+      end
+    end
+
+    attr_reader :opcodes
+
+    def ==(other)
+      other.kind_of?(InstructionSequence) and @opcodes == other.opcodes
+    end
+
+    def []=(idx, val)
+      @opcodes[idx] = val
+    end
+
+    def [](idx)
+      @opcodes[idx]
+    end
+
+    def size
+      @opcodes.size
+    end
+  end
+end
