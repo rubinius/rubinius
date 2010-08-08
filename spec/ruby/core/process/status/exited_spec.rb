e955f4f21a7851f8494086a596451192f5da5cff
Dirkjan Bussink <d.bussink@gmail.com>
2010-08-08 23:22:18 +0200

    Add initial specs for Process::Status

--- spec/ruby/core/process/status/exited_spec.rb
+++ spec/ruby/core/process/status/exited_spec.rb
@@ -1,5 +1,40 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 
 describe "Process::Status#exited?" do
-  it "needs to be reviewed for spec completeness"
+
+  platform_is_not :windows do
+
+    with_tty do
+
+      describe "a cleanly exited child" do
+
+        before :each do
+          @pid = Process.fork {
+            Process.exit! 0
+          }
+          Process.waitpid(@pid)
+        end
+
+        it "returns that the process exited cleanly" do
+          $?.exited?.should be_true
+        end
+      end
+
+      describe "a terminated child" do
+
+        before :each do
+          @pid = Process.fork {
+            sleep 2
+            Process.exit! 42
+          }
+          Process.kill("SIGKILL", @pid)
+          Process.waitpid(@pid)
+        end
+
+        it "returns that the process didn't exit cleanly" do
+          $?.exited?.should be_false
+        end
+      end
+    end
+  end
 end
--- spec/ruby/core/process/status/exitstatus_spec.rb
+++ spec/ruby/core/process/status/exitstatus_spec.rb
@@ -1,5 +1,21 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 
 describe "Process::Status#exitstatus" do
-  it "needs to be reviewed for spec completeness"
+
+  platform_is_not :windows do
+
+    with_tty do
+
+      before :each do
+        @pid = Process.fork {
+          Process.exit! 42
+        }
+        Process.waitpid(@pid)
+      end
+
+      it "returns the process exit code" do
+        $?.exitstatus.should == 42
+      end
+    end
+  end
 end
