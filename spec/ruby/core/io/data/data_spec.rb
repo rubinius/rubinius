88f64480ae9ec8bab4c38839ffb2a64967152a81
Brian Ford <bford@engineyard.com>
2010-08-11 10:50:48 -0700

    Spec for empty script with __END__ data.

--- spec/ruby/core/io/data/data_spec.rb
+++ spec/ruby/core/io/data/data_spec.rb
@@ -1,12 +1,25 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 
 describe "DATA" do
+  it "presents $0 file data after __END__ as a File object" do
+    data = ruby_exe(fixture(__FILE__, "data1.rb"))
+    data.should == "[File, \"hello\"]"
+  end
+end
+
+describe "DATA" do
   before :each do
-    @data1_name = fixture __FILE__, "data1.rb"
+    @stdout = tmp("data_empty_stdout.txt")
+    @stderr = tmp("data_empty_stderr.txt")
   end
 
-  it "presents $0 file data after __END__ as a File object" do
-    data = ruby_exe(@data1_name)
-    data.should == "[File, \"hello\"]"
+  after :each do
+    rm_r @stdout, @stderr
+  end
+
+  it "may be included in an empty script" do
+    ruby_exe(fixture(__FILE__, "data3.rb"), :args => "> #{@stdout} 2> #{@stderr}")
+    File.size?(@stdout).should be_nil
+    File.size?(@stderr).should be_nil
   end
 end
