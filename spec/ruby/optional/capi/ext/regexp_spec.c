cafdc5992a8aef3f63738bcb062cf44ded945396
Brian Ford <bford@engineyard.com>
2010-08-12 14:25:18 -0700

    A couple spec style cleanups.

--- spec/ruby/optional/capi/ext/regexp_spec.c
+++ spec/ruby/optional/capi/ext/regexp_spec.c
@@ -8,13 +8,13 @@ extern "C" {
 #endif
 
 #ifdef HAVE_RB_REG_NEW
-VALUE sp_a_re(VALUE self) {
+VALUE regexp_spec_re(VALUE self) {
   return rb_reg_new("a", 1, 0);
 }
 #endif
 
 #ifdef HAVE_RB_REG_NTH_MATCH
-VALUE sp_a_reg_1st_match(VALUE self, VALUE md) {
+VALUE regexp_spec_reg_1st_match(VALUE self, VALUE md) {
   return rb_reg_nth_match(1, md);
 }
 #endif
@@ -24,11 +24,11 @@ void Init_regexp_spec() {
   cls = rb_define_class("CApiRegexpSpecs", rb_cObject);
 
 #ifdef HAVE_RB_REG_NEW
-  rb_define_method(cls, "a_re", sp_a_re, 0);
+  rb_define_method(cls, "a_re", regexp_spec_re, 0);
 #endif
 
 #ifdef HAVE_RB_REG_NTH_MATCH
-  rb_define_method(cls, "a_re_1st_match", sp_a_reg_1st_match, 1);
+  rb_define_method(cls, "a_re_1st_match", regexp_spec_reg_1st_match, 1);
 #endif
 }
 
--- spec/ruby/optional/capi/regexp_spec.rb
+++ spec/ruby/optional/capi/regexp_spec.rb
@@ -2,23 +2,27 @@ require File.expand_path('../spec_helper', __FILE__)
 
 load_extension("regexp")
 
-describe "CApiRegexp" do
+describe "C-API Regexp function" do
   before :each do
     @p = CApiRegexpSpecs.new
   end
 
-  it "rb_reg_new should return a new valid Regexp" do
-    my_re = @p.a_re
-    my_re.kind_of?(Regexp).should == true
-    ('1a' =~ my_re).should == 1
-    ('1b' =~ my_re).should == nil
-    my_re.source.should == 'a'
+  describe "rb_reg_new" do
+    it "returns a new valid Regexp" do
+      my_re = @p.a_re
+      my_re.kind_of?(Regexp).should == true
+      ('1a' =~ my_re).should == 1
+      ('1b' =~ my_re).should == nil
+      my_re.source.should == 'a'
+    end
   end
 
-  it "rb_reg_nth_match should return a the appropriate match data entry" do
-    @p.a_re_1st_match(/([ab])/.match("a")).should == 'a'
-    @p.a_re_1st_match(/([ab])/.match("b")).should == 'b'
-    @p.a_re_1st_match(/[ab]/.match("a")).should == nil
-    @p.a_re_1st_match(/[ab]/.match("c")).should == nil
+  describe "rb_reg_nth_match" do
+    it "returns a the appropriate match data entry" do
+      @p.a_re_1st_match(/([ab])/.match("a")).should == 'a'
+      @p.a_re_1st_match(/([ab])/.match("b")).should == 'b'
+      @p.a_re_1st_match(/[ab]/.match("a")).should == nil
+      @p.a_re_1st_match(/[ab]/.match("c")).should == nil
+    end
   end
 end
