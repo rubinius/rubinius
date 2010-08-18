699f47be573b6f615769a658f55e04cfe7e12e98
Evan Phoenix <ephoenix@engineyard.com>
2010-08-17 18:10:55 -0700

    Add specs for block arg Array unwrapping behavior

--- spec/ruby/language/fixtures/block.rb
+++ spec/ruby/language/fixtures/block.rb
@@ -3,11 +3,11 @@ module BlockSpecs
     def splat(*args)
       yield *args
     end
-    
+
     def two_args
       yield 1, 2
     end
-    
+
     def two_arg_array
       yield [1, 2]
     end
@@ -15,18 +15,9 @@ module BlockSpecs
     def yield_splat_inside_block
       [1, 2].send(:each_with_index) {|*args| yield(*args)}
     end
+
+    def yield_this(obj)
+      yield obj
+    end
   end
 end
-
-# def block_spec_method(*args)
-#   yield(*args)
-# end
-# 
-# def block_spec_method2
-#   yield 1, 2
-# end
-# 
-# def block_spec_method3
-#   yield [1, 2]
-# end
-# 
