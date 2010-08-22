c365af755890a6e1e872e8795d7db7a5e4bd76a6
Brian Ford <bford@engineyard.com>
2010-08-22 11:20:28 -0700

    Changed 'localhost' literal to be_valid_DNS_name matcher in Socket.getnameinfo specs.
    Closes #439.

--- spec/ruby/library/socket/socket/getnameinfo_spec.rb
+++ spec/ruby/library/socket/socket/getnameinfo_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../fixtures/classes', __FILE__)
 
 require 'socket'
 
-describe "Socket#getnameinfo" do
+describe "Socket.getnameinfo" do
   before :all do
     @reverse_lookup = BasicSocket.do_not_reverse_lookup
     BasicSocket.do_not_reverse_lookup = true
@@ -22,14 +22,15 @@ describe "Socket#getnameinfo" do
   it "gets the name information and resolve the host" do
     sockaddr = Socket.sockaddr_in SocketSpecs.port, '127.0.0.1'
     name_info = Socket.getnameinfo(sockaddr, Socket::NI_NUMERICSERV)
-    name_info.should == ['localhost', "#{SocketSpecs.port}"]
+    name_info[0].should be_valid_DNS_name
+    name_info[1].should == SocketSpecs.port.to_s
   end
 
   it "gets the name information and resolves the service" do
     sockaddr = Socket.sockaddr_in 80, '127.0.0.1'
     name_info = Socket.getnameinfo(sockaddr)
     name_info.size.should == 2
-    name_info[0].should == "localhost"
+    name_info[0].should be_valid_DNS_name
     # see http://www.iana.org/assignments/port-numbers
     name_info[1].should =~ /^(www|http|www-http)$/
   end
