09863f5e943764b8e474f75131a0c08ad6ec1b02
Dirkjan Bussink <d.bussink@gmail.com>
2010-08-24 20:31:03 +0200

    Rework Process::Status specs to be simplified and run them on Windows

--- spec/ruby/core/process/status/exitstatus_spec.rb
+++ spec/ruby/core/process/status/exitstatus_spec.rb
@@ -2,20 +2,12 @@ require File.expand_path('../../../../spec_helper', __FILE__)
 
 describe "Process::Status#exitstatus" do
 
-  platform_is_not :windows do
-
-    with_tty do
-
-      before :each do
-        @pid = Process.fork {
-          Process.exit! 42
-        }
-        Process.waitpid(@pid)
-      end
-
-      it "returns the process exit code" do
-        $?.exitstatus.should == 42
-      end
-    end
+  before :each do
+    ruby_exe("exit(42)")
   end
+
+  it "returns the process exit code" do
+    $?.exitstatus.should == 42
+  end
+
 end
