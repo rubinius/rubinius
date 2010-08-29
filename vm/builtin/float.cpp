0141e58c2b93d42104f1fd9208f59131bcffe05b
Ivan Evtukhovich <evtuhovich@gmail.com>
2010-08-29 23:46:40 +0400

    Fixes Float#divmod bug. Closes #451.
    Signed-off-by: Brian Ford <bford@engineyard.com>

--- vm/builtin/float.cpp
+++ vm/builtin/float.cpp
@@ -104,8 +104,8 @@ namespace rubinius {
 
   Float* Float::mod(STATE, Float* other) {
     double res = fmod(this->val, other->val);
-    if((other->val < 0.0 && this->val > 0.0) ||
-       (other->val > 0.0 && this->val < 0.0)) {
+    if((other->val < 0.0 && this->val > 0.0 && res != 0) ||
+       (other->val > 0.0 && this->val < 0.0 && res != 0)) {
       res += other->val;
     }
     return Float::create(state, res);
