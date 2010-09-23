89b5a60fe3bf4b8e7469372de717bfa92b1e588d
Brian Ford <bford@engineyard.com>
2010-09-23 11:26:12 -0700

    Converted more documentation.

--- doc-src/lib/default.rb
+++ doc-src/lib/default.rb
@@ -6,11 +6,48 @@ include Nanoc3::Helpers::LinkTo
 def table_of_contents
   toc = "<ul>\n"
 
-  [ ["What is Rubinius", "/"],
-    ["Getting Started", "/getting_started/"],
-    ["Contributing",    "/contributing/"]
+  [ ["What is Rubinius",  "/"],
+    ["Getting Started",   "/getting_started/"],
+    ["Common Problems",   "/common_problems/"],
+    ["Contributing",      "/contributing/"]
   ].each do |name, link|
-    toc << "<li>#{link_to_unless_current(name, link)}</li>"
+    toc << "<li>#{link_to_unless_current(name, link)}</li>\n"
+  end
+
+  toc << "</ul>\n"
+  toc << "<h3>Tools</h3>\n"
+  toc << "<ul>\n"
+
+  [ ["Profiler", "/tools/profiler/"],
+    ["Debugger", "/tools/debugger/"],
+  ].each do |name, link|
+    toc << "<li>#{link_to_unless_current(name, link)}</li>\n"
+  end
+
+  toc << "</ul>\n"
+  toc << "<h3>Developing</h3>\n"
+  toc << "<ul>\n"
+
+  [ ["Specs",             "/dev/specs/"],
+    ["Build System",      "/dev/build_system/"],
+    ["Bootstrapping",     "/dev/bootstrapping/"],
+    ["Bytecode Compiler", "/dev/compiler/"],
+    ["Style Guide",       "/dev/style_guide/"],
+    ["Reading List",      "/dev/reading_list/"],
+    ["Glossary",          "/glossary/"]
+  ].each do |name, link|
+    toc << "<li>#{link_to_unless_current(name, link)}</li>\n"
+  end
+
+  toc << "</ul>\n"
+  toc << "<h3>Howto</h3>\n"
+  toc << "<ul>\n"
+
+  [ ["Write a Ticket",      "/howto/write_a_ticket/"],
+    ["Write a Ruby spec",   "/howto/write_a_ruby_spec/"],
+    ["Fix a Failing Spec",  "/howto/fix_a_failing_spec/"],
+  ].each do |name, link|
+    toc << "<li>#{link_to_unless_current(name, link)}</li>\n"
   end
 
   toc << "</ul>\n"
