634fd8e988c528c3771e1414e3c7525b67b7b102
Brian Ford <bford@engineyard.com>
2010-09-23 01:22:37 -0700

    Some initial content for documentation site.

--- doc-src/lib/default.rb
+++ doc-src/lib/default.rb
@@ -1,2 +1,17 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+include Nanoc3::Helpers::LinkTo
+
+def table_of_contents
+  toc = "<ul>\n"
+
+  [ ["What is Rubinius", "/"],
+    ["Getting Started", "/getting_started/"],
+    ["Contributing",    "/contributing/"]
+  ].each do |name, link|
+    toc << "<li>#{link_to_unless_current(name, link)}</li>"
+  end
+
+  toc << "</ul>\n"
+end
