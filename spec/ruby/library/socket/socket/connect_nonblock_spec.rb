b71e3b63a20dd73f6214f9f495a2c22a5c9421cf
Evan Phoenix <ephoenix@engineyard.com>
2010-09-27 12:46:28 -0700

    Add spec for Socket#connect_nonblock

--- spec/ruby/library/socket/socket/connect_nonblock_spec.rb
+++ spec/ruby/library/socket/socket/connect_nonblock_spec.rb
@@ -1,2 +1,48 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 require File.expand_path('../../fixtures/classes', __FILE__)
+
+require 'socket'
+
+describe "Socket#connect_nonblock" do
+  before :each do
+    @addr = Socket.sockaddr_in(SocketSpecs.port, "127.0.0.1")
+    @socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+  end
+
+  after :each do
+    @socket.close
+  end
+
+  it "takes an encoded socket address and starts the connection to it" do
+    lambda {
+      @socket.connect_nonblock(@addr)
+    }.should raise_error(Errno::EINPROGRESS)
+  end
+
+  it "raises Errno::ISCONN when the socket has connected" do
+    ready = false
+    thread = Thread.new do
+      server = TCPServer.new(SocketSpecs.port)
+      ready = true
+      conn = server.accept
+      conn << "hello!"
+      conn.close
+      server.close
+    end
+
+    Thread.pass while (thread.status and thread.status != 'sleep') or !ready
+
+    begin
+      @socket.connect_nonblock(@addr)
+    rescue Errno::EINPROGRESS
+    end
+
+    IO.select nil, [@socket]
+
+    lambda {
+      @socket.connect_nonblock(@addr)
+    }.should raise_error(Errno::EISCONN)
+
+    @socket.read(6).should == "hello!"
+  end
+end
