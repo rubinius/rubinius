02fbd733928e9a303794a9582df1d41ab9bcf842
Brian Ford <bford@engineyard.com>
2010-09-30 16:49:47 -0700

    Moved safe_write into conditional to avoid defined-not-used warning.

--- vm/environment.cpp
+++ vm/environment.cpp
@@ -108,12 +108,12 @@ namespace rubinius {
 
   static void null_func(int sig) {}
 
+#ifdef USE_EXECINFO
+
   static void safe_write(int fd, const char* str, int len=0) {
     if(!len) len = strlen(str);
     if(write(fd, str, len) == 0) exit(101);
   }
-
-#ifdef USE_EXECINFO
 
   static void write_sig(int fd, int sig) {
     switch(sig) {
