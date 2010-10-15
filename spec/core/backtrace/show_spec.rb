e1a0ed9ed91fb459cc4e3a083898193102fe6c68
Jari Bakken <jari.bakken@gmail.com>
2010-10-15 21:27:26 +0200

    Add spec for Backtrace#show.
    Also adding width/min_width args to Backtrace#initialize for testability.

--- spec/core/backtrace/show_spec.rb
+++ spec/core/backtrace/show_spec.rb
@@ -1,1 +1,16 @@
 require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "Backtrace" do
+  it "returns a string if file paths exceed terminal width and show_color is false" do
+    locs = Rubinius::VM.backtrace(1)
+
+    # don't depend on the location of this file
+    locs.each_with_index do |loc, idx|
+      loc.stub!(:position).and_return(idx.to_s + "a"*90 + ".rb")
+    end
+
+    bt = Backtrace.new(locs, 65, 20)
+
+    bt.show("\n", false).should be_kind_of(String)
+  end
+end
