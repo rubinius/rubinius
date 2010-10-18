179f7ea2f36a4123f54fd90263455ee5d220bcef
Brian Ford <bford@engineyard.com>
2010-10-18 16:31:21 -0700

    Omit drive letter on Windows for dependencies.

--- rakelib/dependency_grapher.rb
+++ rakelib/dependency_grapher.rb
@@ -114,9 +114,8 @@ class DependencyGrapher
 
       width = str.size
       @dependencies.each do |name|
-        # Fix for Windows drive letters. This string may
-        # be frozen so we do not modify it inline.
-        name = "/#{name[0, 1]}#{name[2..-1]}" if name[1] == ?:
+        # Omit drive letter on Windows.
+        name = name[2..-1] if name[1] == ?:
 
         size = name.size + 1
         if width + size > max
