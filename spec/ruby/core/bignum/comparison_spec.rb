4018ebda2515288ad224f9b7508f867f28d396c7
Brian Ford <bford@engineyard.com>
2010-11-19 12:10:52 -0800

    Updated Bignum#<=> specs.
    Notes:
    * Use nested describes to group related specs.
    * Separate specs for data with different classes (ie. don't
      put specs for Fixnum and Bignum values together). This is
      based on the principle that failing specs should not mask
      other passing specs. In other words, if specs are together
      in an example block, all should fail or pass. This is a
      relaxation of the one spec per example rule.
    * Use the simplest data in the spec that will work.
    * Use bignum_value and infinity_value helpers.

--- spec/ruby/core/bignum/comparison_spec.rb
+++ spec/ruby/core/bignum/comparison_spec.rb
@@ -1,71 +1,114 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Bignum#<=>" do
-  before(:each) do
-    @bignum = bignum_value(96)
-    @inf = 1/0.0
+  describe "with a Fixnum" do
+    it "returns -1 when other is larger" do
+      (-bignum_value <=> 2).should == -1
+    end
+
+    it "returns 1 when other is smaller" do
+      (bignum_value <=> 2).should == 1
+    end
   end
 
-  it "returns -1 when self is less than the given argument" do
-    (-@bignum <=> @bignum).should == -1
-    (-@bignum <=> -1).should == -1
-    (-@bignum <=> -4.5).should == -1
+  describe "with a Bignum" do
+    describe "when other is negative" do
+      it "returns -1 when self is negative and other is larger" do
+        (-bignum_value(42) <=> -bignum_value).should == -1
+      end
+
+      it "returns 0 when other is equal" do
+        (-bignum_value <=> -bignum_value).should == 0
+      end
+
+      it "returns 1 when self is negative and other is smaller" do
+        (-bignum_value <=> -bignum_value(94)).should == 1
+      end
+
+      it "returns 1 when self is positive" do
+        (bignum_value <=> -bignum_value).should == 1
+      end
+    end
+
+    describe "when other is positive" do
+      it "returns -1 when self is negative" do
+        (-bignum_value <=> bignum_value).should == -1
+      end
+
+      it "returns -1 when self is positive and other is larger" do
+        (bignum_value <=> bignum_value(38)).should == -1
+      end
+
+      it "returns 0 when other is equal" do
+        (bignum_value <=> bignum_value).should == 0
+      end
+
+      it "returns 1 when other is smaller" do
+        (bignum_value(56) <=> bignum_value).should == 1
+      end
+    end
   end
 
-  it "returns 0 when self is equal to the given argument" do
-    (@bignum <=> @bignum).should == 0
-    (-@bignum <=> -@bignum).should == 0
+  describe "with a Float" do
+    describe "when other is negative" do
+      it "returns -1 when self is negative and other is larger" do
+        (-bignum_value(0xffff) <=> -bignum_value.to_f).should == -1
+      end
+
+      it "returns 0 when other is equal" do
+        (-bignum_value <=> -bignum_value.to_f).should == 0
+      end
+
+      it "returns 1 when self is negative and other is smaller" do
+        (-bignum_value <=> -bignum_value(0xffef).to_f).should == 1
+      end
+
+      it "returns 1 when self is positive" do
+        (bignum_value <=> -bignum_value.to_f).should == 1
+      end
+    end
+
+    describe "when other is positive" do
+      it "returns -1 when self is negative" do
+        (-bignum_value <=> bignum_value.to_f).should == -1
+      end
+
+      it "returns -1 when self is positive and other is larger" do
+        (bignum_value <=> bignum_value(0xfffe).to_f).should == -1
+      end
+
+      it "returns 0 when other is equal" do
+        (bignum_value <=> bignum_value.to_f).should == 0
+      end
+
+      it "returns 1 when other is smaller" do
+        (bignum_value(0xfeff) <=> bignum_value.to_f).should == 1
+      end
+    end
   end
 
-  it "returns 1 when self is greater than the given argument" do
-    (@bignum <=> -@bignum).should == 1
-    (@bignum <=> 1).should == 1
-    (@bignum <=> 4.5).should == 1
+  describe "with an Object" do
+    # TODO for srawlins
   end
 
-  it "returns nil when the given argument is not an Integer" do
-    (@bignum <=> mock('str')).should == nil
-    (@bignum <=> 'test').should == nil
+  # The tests below are taken from matz's revision 23730 for Ruby trunk
+  ruby_bug "[ruby-dev:38672] [Bug #1645]", "1.8.7.302" do
+    it "returns 1 when self is Infinity and other is a Bignum" do
+      (infinity_value <=> Float::MAX.to_i*2).should == 1
+    end
+
+    it "returns -1 when self is negative and other is Infinty" do
+      (-Float::MAX.to_i*2 <=> infinity_value).should == -1
+    end
   end
 
-  # TODO: Remove duplicate bug guards when ruby_bug is updated.
   ruby_bug "[ruby-dev:38672] [Bug #1645]", "1.8.7.248" do
-    # The 4 tests below are taken from matz's revision 23730 for Ruby trunk
-    #
-    it "returns 1 when self is Infinity and other is a Bignum" do
-      (@inf <=> Float::MAX.to_i*2).should == 1
-    end
-
     it "returns 1 when self is negative and other is -Infinity" do
-      (-Float::MAX.to_i*2 <=> -@inf).should == 1
+      (-Float::MAX.to_i*2 <=> -infinity_value).should == 1
     end
 
     it "returns -1 when self is -Infinity and other is negative" do
-      (-@inf <=> -Float::MAX.to_i*2).should == -1
-    end
-
-    it "returns -1 when self is negative and other is Infinty" do
-      (-Float::MAX.to_i*2 <=> @inf).should == -1
-    end
-  end
-
-  ruby_bug "[ruby-dev:38672] [Bug #1645]", "1.8.7" do
-    # The 4 tests below are taken from matz's revision 23730 for Ruby trunk
-    #
-    it "returns 1 when self is Infinity and other is a Bignum" do
-      (@inf <=> Float::MAX.to_i*2).should == 1
-    end
-
-    it "returns 1 when self is negative and other is -Infinity" do
-      (-Float::MAX.to_i*2 <=> -@inf).should == 1
-    end
-
-    it "returns -1 when self is -Infinity and other is negative" do
-      (-@inf <=> -Float::MAX.to_i*2).should == -1
-    end
-
-    it "returns -1 when self is negative and other is Infinty" do
-      (-Float::MAX.to_i*2 <=> @inf).should == -1
+      (-infinity_value <=> -Float::MAX.to_i*2).should == -1
     end
   end
 end
