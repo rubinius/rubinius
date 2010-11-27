83cd8d26b5446b705e0b8a0561bc3e13bd5424cd
Brian Ford <bford@engineyard.com>
2010-11-26 19:47:24 -0800

    Some cleanup of C-API proc_spec.

--- spec/ruby/optional/capi/ext/proc_spec.c
+++ spec/ruby/optional/capi/ext/proc_spec.c
@@ -8,11 +8,11 @@ extern "C" {
 #endif
 
 #ifdef HAVE_RB_PROC_NEW
-VALUE concat_func(VALUE args) {
+VALUE proc_spec_concat_func(VALUE args) {
   int i;
   char buffer[500] = {0};
-  for(i = 0; i < RARRAY(args)->len; ++i) {
-    VALUE v = RARRAY(args)->ptr[i];
+  for(i = 0; i < RARRAY_LEN(args); ++i) {
+    VALUE v = RARRAY_PTR(args)[i];
     strcat(buffer, StringValuePtr(v));
     strcat(buffer, "_");
   }
@@ -21,8 +21,8 @@ VALUE concat_func(VALUE args) {
 
 }
 
-VALUE sp_underline_concat_proc(VALUE self) {
-  return rb_proc_new(concat_func, Qnil);
+VALUE proc_spec_underline_concat_proc(VALUE self) {
+  return rb_proc_new(proc_spec_concat_func, Qnil);
 }
 #endif
 
@@ -43,7 +43,7 @@ void Init_proc_spec() {
   cls = rb_define_class("CApiProcSpecs", rb_cObject);
 
 #ifdef HAVE_RB_PROC_NEW
-  rb_define_method(cls, "underline_concat_proc", sp_underline_concat_proc, 0);
+  rb_define_method(cls, "underline_concat_proc", proc_spec_underline_concat_proc, 0);
 #endif
 
   rb_define_method(cls, "rb_Proc_new", proc_spec_rb_Proc_new, 1);
