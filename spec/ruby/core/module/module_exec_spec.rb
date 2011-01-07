70094dbf931a823dfadb7204f3bc4788f7d53818
Brian Ford <brixen@gmail.com>
2011-01-07 15:53:39 -0800

    Updated CI specs to RubySpec a15b26ca.

--- spec/ruby/core/module/module_exec_spec.rb
+++ spec/ruby/core/module/module_exec_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/class_exec', __FILE__)
 
-describe "Module#class_exec" do
+describe "Module#module_exec" do
   it_behaves_like :module_class_exec, :module_exec
 end
