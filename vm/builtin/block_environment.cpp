8aaeba07c8dffd96bb3ca8769c293b8e4de01ffb
Evan Phoenix <ephoenix@engineyard.com>
2011-01-19 15:57:00 -0800

    Use proper width to OnStack<>

--- vm/builtin/block_environment.cpp
+++ vm/builtin/block_environment.cpp
@@ -218,7 +218,7 @@ namespace rubinius {
   BlockEnvironment* BlockEnvironment::under_call_frame(STATE, CompiledMethod* cm,
       VMMethod* caller, CallFrame* call_frame, size_t index)
   {
-    OnStack<2> os(state, cm);
+    OnStack<1> os(state, cm);
 
     state->set_call_frame(call_frame);
 
