dfcf2193a87df9d8d205ef0de39f1b473218da14
Brian Ford <bford@engineyard.com>
2011-01-26 21:58:42 -0800

    Fixed Array#inspect specs for tainted Array or elements.

--- spec/ruby/core/array/inspect_spec.rb
+++ spec/ruby/core/array/inspect_spec.rb
@@ -17,7 +17,7 @@ describe "Array#inspect" do
     str.should include('items[1]')
     str.should include('items[2]')
   end
-  
+
   it "handles recursive arrays" do
     x = [1, 2]
     x << x << 4
@@ -30,25 +30,30 @@ describe "Array#inspect" do
     lambda{ x.inspect }.should_not raise_error
     lambda{ y.inspect }.should_not raise_error
   end
-  
-  it "propagates taint from itself or elements" do
-    x = [1, 2]
-    x.taint
-    s = x.inspect
-    s.tainted?.should == true
-    
-    x = [1, s]
-    s = x.inspect
-    s.tainted?.should == true
+
+  it "taints the result String if the Array is tainted" do
+    a = [1, 2].taint
+    a.inspect.tainted?.should be_true
   end
-  
+
+  it "taints the result String if an element is tainted" do
+    a = [1, "str".taint]
+    a.inspect.tainted?.should be_true
+  end
+
+  it "does not taint the result String if the Array is empty" do
+    a = []
+    a.taint
+    a.inspect.tainted?.should be_false
+  end
+
   ruby_version_is "1.9" do
     it "propagates untrust from itself or elements" do
       x = [1, 2]
       x.untrust
       s = x.inspect
       s.untrusted?.should == true
-      
+
       x = [1, s]
       x.untrust
       s = x.inspect
