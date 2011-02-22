e5250ce63c6701bfb4168806bbbd782ad5235d73
Evan Phoenix <ephoenix@engineyard.com>
2011-02-21 16:41:11 -0800

    Flesh out string coerce specs in StringScanner

--- spec/ruby/library/stringscanner/dup_spec.rb
+++ spec/ruby/library/stringscanner/dup_spec.rb
@@ -2,17 +2,38 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require 'strscan'
 
 describe "StringScanner#dup" do
-  before :each do
-    @s = StringScanner.new("This is a test")
-    @d = @s.dup
+  before(:each) do
+    @string = "this is a test"
+    @orig_s = StringScanner.new(@string)
   end
 
-  it "returns a new StringScanner duplicated from the original" do    
-    @d.should be_kind_of(StringScanner)
-    @d.inspect.should == @s.inspect
-    @d.string.should == @s.string
-    @d.eos?.should == @s.eos?
-    @d.matched?.should == @s.matched?
-    @d.pos.should == @s.pos
+  it "copies the passed StringScanner's content to self" do
+    s = @orig_s.dup
+    s.string.should == @string
+  end
+
+  it "copies the passed StringSCanner's position to self" do
+    @orig_s.pos = 5
+    s = @orig_s.dup
+    s.pos.should eql(5)
+  end
+
+  it "copies previous match state" do
+    @orig_s.scan(/\w+/)
+    @orig_s.scan(/\s/)
+
+    @orig_s.pre_match.should == "this"
+
+    s = @orig_s.dup
+    s.pre_match.should == "this"
+
+    s.unscan
+    s.scan(/\s/).should == " "
+  end
+
+  it "copies the passed StringScanner scan pointer to self" do
+    @orig_s.terminate
+    s = @orig_s.dup
+    s.eos?.should be_true
   end
 end 
--- spec/ruby/library/stringscanner/string_spec.rb
+++ spec/ruby/library/stringscanner/string_spec.rb
@@ -3,13 +3,18 @@ require 'strscan'
 
 describe "StringScanner#string" do
   before :each do
-    @s = StringScanner.new("This is a test")
+    @string = "This is a test"
+    @s = StringScanner.new(@string)
   end
 
   it "returns the string being scanned" do
     @s.string.should == "This is a test"
     @s << " case"
     @s.string.should == "This is a test case"
+  end
+
+  it "returns the identical object passed in" do
+    @s.string.equal?(@string).should be_true
   end
 end
 
@@ -21,5 +26,15 @@ describe "StringScanner#string=" do
   it "changes the string being scanned to the argument and resets the scanner" do
     @s.string = "Hello world"
     @s.string.should == "Hello world"
+  end
+
+  it "converts the argument into a string using #to_str" do
+    m = mock(:str)
+
+    s = "test"
+    m.should_receive(:to_str).and_return(s)
+
+    @s.string = m
+    @s.string.should == s
   end
 end
