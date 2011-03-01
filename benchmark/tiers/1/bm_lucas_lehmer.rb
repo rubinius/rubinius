f4a6f4b085ab3862703bde3dd4c859884b8d5199
Evan Phoenix <ephoenix@engineyard.com>
2011-02-28 16:20:44 -0800

    More benchmark conversions

--- benchmark/tiers/1/bm_lucas_lehmer.rb
+++ benchmark/tiers/1/bm_lucas_lehmer.rb
@@ -1,16 +1,23 @@
-# Lucas–Lehmer primality test for Mersenne numbers (2**p - 1)
+require 'benchmark'
+require 'benchmark/ips'
 
-def is_prime?(p)
-  s = 4
-  m = 2**p - 1
-  (p-2).times do
-    s = (s**2 - 2) % m
-  end
-  s == 0 ? true : false
-end
+# Lucas–Lehmer primality test for Mersenne numbers (2**p - 1)
 
 # [9689, 9941, 11213, 19937].each { |n| is_prime?(n) }
 # 4 Mersenne's exponents
-def Bench.run
-  is_prime?(9689)
+#
+# MEASUREMENT NOTE:
+# 
+# Because of the 2**p, this becomes mostly a Bignum benchmark.
+
+Benchmark.ips do |x|
+  x.report "prime test" do
+    p = 1001
+    s = 4
+    m = 2**p - 1
+    (p-2).times do
+      s = (s**2 - 2) % m
+    end
+    s == 0 ? true : false
+  end
 end
