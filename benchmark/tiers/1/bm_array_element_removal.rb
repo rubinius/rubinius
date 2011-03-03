1bc3341fd116f82c0877daf51c809167d222f5ac
Chuck Remes <cremes@mac.com>
2011-03-03 13:44:02 -0600

    modify benchmark to be more concise

--- benchmark/tiers/1/bm_array_element_removal.rb
+++ benchmark/tiers/1/bm_array_element_removal.rb
@@ -1,53 +1,29 @@
 require 'benchmark'
 require 'benchmark/ips'
 
-# had to size this so that array allocation didn't swamp
-# the other numbers; it needs to be small enough to keep
-# allocation fast yet large enough that the tests don't
-# need to allocate too many of them
-BaseArray = (0...10_000).to_a
-
 
 Benchmark.ips do |x|
   
-  x.report "array delete_at" do |times|
-    scratch_array = BaseArray.dup
-    i = 0
-    while i < times
-      scratch_array.delete_at(0)
-      scratch_array = BaseArray.dup if scratch_array.empty?
-      i += 1
-    end
+  base_array = (0...5_000).to_a
+  
+  x.report "array delete_at" do
+    scratch_array = base_array.dup
+    scratch_array.delete_at(0) until scratch_array.empty?
   end
   
-  x.report "array slice!" do |times|
-    scratch_array = BaseArray.dup
-    i = 0
-    while i < times
-      scratch_array.slice!(0)
-      scratch_array = BaseArray.dup if scratch_array.empty?
-      i += 1
-    end
+  x.report "array slice!" do
+    scratch_array = base_array.dup
+    scratch_array.slice!(0) until scratch_array.empty?
   end
 
-  x.report "array drop" do |times|
-    scratch_array = BaseArray.dup
-    i = 0
-    while i < times
-      scratch_array.drop(1)
-      scratch_array = BaseArray.dup if scratch_array.empty?
-      i += 1
-    end
+  x.report "array drop" do
+    scratch_array = base_array.dup
+    scratch_array = scratch_array.drop(1) until scratch_array.empty?
   end
   
-  x.report "array shift" do |times|
-    scratch_array = BaseArray.dup
-    i = 0
-    while i < times
-      scratch_array.shift
-      scratch_array = BaseArray.dup if scratch_array.empty?
-      i += 1
-    end    
+  x.report "array shift" do
+    scratch_array = base_array.dup
+    scratch_array.shift until scratch_array.empty?
   end
 
 end
