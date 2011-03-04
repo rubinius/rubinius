48dcd3d4bee41371d018064d1ae4f5b9cd86129d
Chuck Remes <cremes@mac.com>
2011-03-03 18:07:55 -0600

    fix string concat benchmark

--- benchmark/tiers/1/bm_app_strconcat.rb
+++ benchmark/tiers/1/bm_app_strconcat.rb
@@ -4,10 +4,19 @@ require 'benchmark/ips'
 Benchmark.ips do |x|
   part = "aaaa"
 
-  x.report "string concat" do |times|
+  x.report "string interpolation" do |times|
     i = 0
     while i < times
       "#{part} #{part} #{part}"
+      i += 1
+    end
+  end
+
+  x.report "string concat" do |times|
+    i = 0
+    while i < times
+      part + part + part
+      i += 1
     end
   end
 end
