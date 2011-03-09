9e6b6f8d786c589cbf60c14b22ea2f13a191c996
Chuck Remes <cremes@mac.com>
2011-03-09 11:16:43 -0600

    update gsub benchmark

--- benchmark/core/string/bench_sub.rb
+++ benchmark/core/string/bench_sub.rb
@@ -4,6 +4,14 @@ require 'benchmark/ips'
 Benchmark.ips do |x|
   string = "2011-Mar-08T10:09:08.467"
 
+  x.report "gsub" do |times|
+    i = 0
+    while i < times
+      string.gsub(/[^-+',.\/:@[:alnum:]\[\]\x80-\xff]+/n, ' ')
+      i += 1
+    end
+  end
+
   x.report "gsub!" do |times|
     i = 0
     while i < times
