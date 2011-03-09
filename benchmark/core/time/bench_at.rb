b425851bee3220e1ac62d10a4bf02d6ce9793f29
Chuck Remes <cremes@mac.com>
2011-03-09 12:18:19 -0600

    clarify bench report description

--- benchmark/core/time/bench_at.rb
+++ benchmark/core/time/bench_at.rb
@@ -3,11 +3,11 @@ require 'benchmark/ips'
 require 'time'
 
 Benchmark.ips do |x|
-  nearest_second = Time.parse("2011-03-08T10:09:08.467").to_i
-  fractional_second = Time.parse("2011-03-08T10:09:08.467").to_f
+  int_second = Time.parse("2011-03-08T10:09:08.467").to_i
+  float_second = Time.parse("2011-03-08T10:09:08.467").to_f
   
 
-  x.report "#at to nearest second" do |times|
+  x.report "#at with integer" do |times|
     i = 0
     while i < times
       Time.at(nearest_second)
@@ -15,7 +15,7 @@ Benchmark.ips do |x|
     end
   end
 
-  x.report "#at for fractional second" do |times|
+  x.report "#at with float" do |times|
     i = 0
     while i < times
       Time.at(fractional_second)
