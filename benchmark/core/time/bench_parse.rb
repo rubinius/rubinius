e5e26a4bc50ad8d8f2f3e516eccd56322a9595b1
Chuck Remes <cremes@mac.com>
2011-03-16 15:27:32 -0500

    add another time format to Time#parse benchmark

--- benchmark/core/time/bench_parse.rb
+++ benchmark/core/time/bench_parse.rb
@@ -16,6 +16,8 @@ Benchmark.ips do |x|
   rfc2822_date = "08 Mar 11"
   rfc2822_date_time = "08 Mar 11 10:09:08 GMT"
   rfc2822_date_time_cst = "08 Mar 11 10:09:08 CST"
+
+  format_c = "Fri Jan 02 08:10:00 -0600 2004"
   
 
   x.report "parse '#{iso_8601_date_only_locale_neutral}'" do |times|
@@ -94,6 +96,14 @@ Benchmark.ips do |x|
     i = 0
     while i < times
       Time.parse(rfc2822_date_time_cst)
+      i += 1
+    end
+  end
+
+  x.report "parse '#{format_c}'" do |times|
+    i = 0
+    while i < times
+      Time.parse(format_c)
       i += 1
     end
   end
