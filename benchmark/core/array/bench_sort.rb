11862279b8b32640bdaec29cf34b908058cda120
Chuck Remes <cremes@mac.com>
2011-03-25 13:08:55 -0500

    added nested fixnum arrays

--- benchmark/core/array/bench_sort.rb
+++ benchmark/core/array/bench_sort.rb
@@ -2,10 +2,20 @@ require 'benchmark'
 require 'benchmark/ips'
 require File.expand_path('../shared_array.rb', __FILE__)
 
+def make_nested(kind)
+  ary = []
+  source = eval("$small_#{kind}_array")
+  source.each_with_index do |element, index|
+    ary << [element, source[index + 1]] # yes, the last pair will be [<element>, nil]
+  end
+  ary
+end
+
 Benchmark.ips do |x|
   string_array = $small_string_array.dup
   fixnum_array = $small_fixnum_array.dup
   time_array = $small_time_array.dup
+  nested_fixnum_array = make_nested(:fixnum)
 
   x.report "sort strings ascending" do |times|
     i = 0
@@ -104,6 +114,40 @@ Benchmark.ips do |x|
     i = 0
     while i < times
       array2 = time_array.dup
+      array2.sort! { |a,b| b <=> a }
+      i += 1
+    end
+  end
+
+  x.report "sort nested fixnum arrays ascending" do |times|
+    i = 0
+    while i < times
+      nested_fixnum_array.sort
+      i += 1
+    end
+  end
+
+  x.report "array sort! nested fixnum arrays ascending" do |times|
+    i = 0
+    while i < times
+      array2 = nested_fixnum_array.dup
+      array2.sort!
+      i += 1
+    end
+  end
+
+  x.report "sort nested fixnum arrays descending with block" do |times|
+    i = 0
+    while i < times
+      nested_fixnum_array.sort { |a,b| b <=> a }
+      i += 1
+    end
+  end
+
+  x.report "sort! nested fixnum arrays descending with block" do |times|
+    i = 0
+    while i < times
+      array2 = nested_fixnum_array.dup
       array2.sort! { |a,b| b <=> a }
       i += 1
     end
