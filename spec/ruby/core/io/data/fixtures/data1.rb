a23be12ccd6665388e586df2dfe0b50eade1c6a4
Evan Phoenix <ephoenix@engineyard.com>
2011-03-25 16:58:05 -0700

    Move all DATA specs to one place

--- spec/ruby/core/io/data/fixtures/data1.rb
+++
@@ -1,3 +0,0 @@
-print [DATA.class, DATA.read].inspect
-__END__
-hello
--- spec/ruby/language/predefined/data_spec.rb
+++ spec/ruby/language/predefined/data_spec.rb
@@ -16,4 +16,28 @@ describe "The DATA constant" do
   it "does not change when an included files also has a __END__" do
     ruby_exe(fixture(__FILE__, "data3.rb")).chomp.should == "data 3"
   end
+
+  it "is included in an otherwise empty file" do
+    ap = fixture(__FILE__, "print_data.rb")
+    str = ruby_exe(fixture(__FILE__, "data_only.rb"), :options => "-r#{ap}")
+    str.chomp.should == "data only"
+  end
+
+  platform_is_not :windows do
+    it "succeeds in locking the file DATA came from" do
+      path = fixture(__FILE__, "data_flock.rb")
+
+      data = ruby_exe(path)
+      data.should == "0"
+
+      begin
+        file = File.open(path)
+        file.flock(File::LOCK_EX)
+        data = ruby_exe(path)
+        data.should == "false"
+      ensure
+        file.flock(File::LOCK_UN) if file
+      end
+    end
+  end
 end
