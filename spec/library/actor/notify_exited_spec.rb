3c689edf0cf6bfe4bec8d520cb23f3b4ff805230
MenTaLguY <mental@rydia.net>
2011-04-08 17:39:13 -0700

    spec for existing behavior

--- spec/library/actor/notify_exited_spec.rb
+++ spec/library/actor/notify_exited_spec.rb
@@ -2,5 +2,20 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require 'actor'
 
 describe "Actor#notify_exited" do
-  it "needs to be reviewed for spec completeness"
+  it "should kill actors not trapping exits" do
+    sync = Rubinius::Channel.new
+    actor = Actor.spawn do
+      begin
+        Actor.receive
+      rescue Exception => exc
+        sync << exc
+      ensure
+        sync << nil
+      end
+    end
+    actor.notify_exited(:actor, :reason)
+    ex = sync.receive
+    ex.actor.should == :actor
+    ex.reason.should == :reason
+  end
 end
