b513d136c194db230ece0a9cf3a2f7afac9790e5
Evan Phoenix <ephoenix@engineyard.com>
2011-04-29 17:24:10 -0700

    Struct - spun and twirled

--- kernel/common/symbol.rb
+++ kernel/common/symbol.rb
@@ -22,12 +22,12 @@ class Symbol
   # Returns a pretty version of the symbol, fit for viewing
   #  :symbol.inspect #=> ":symbol"
   def inspect
-    str = self.to_s
+    str = to_s
 
     case str
-    when /^(\$|@@?)[a-z_][a-z_\d]*$/i,                     # Variable names
+    when /^(\$|@@?)[a-z_][a-z_\d]*$/i,                      # Variable names
          /^[a-z_][a-z_\d]*[=?!]?$/i,                        # Method names
-         /^\$(-[a-z_\d]|[+~:?<_\/'"$.,`!;\\=*>&@]|\d+)$/i, # Special global variables
+         /^\$(-[a-z_\d]|[+~:?<_\/'"$.,`!;\\=*>&@]|\d+)$/i,  # Special global variables
          /^([|^&\/%~`]|<<|>>|<=>|===?|=~|[<>]=?|[+-]@?|\*\*?|\[\]=?)$/ # Operators
       ":#{str}"
     else
