1c9884c6834e8768f409f7c7b0a8cb9b37d7dbf3
Brian Ford <bford@engineyard.com>
2011-05-04 12:44:06 -0700

    Updated MSpec source to d9912dbc.

--- mspec/lib/mspec/helpers.rb
+++ mspec/lib/mspec/helpers.rb
@@ -1,5 +1,6 @@
 require 'mspec/helpers/argv'
 require 'mspec/helpers/const_lookup'
+require 'mspec/helpers/datetime'
 require 'mspec/helpers/ducktype'
 require 'mspec/helpers/encode'
 require 'mspec/helpers/enumerator_class'
