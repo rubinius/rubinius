185efbfa8edad043fec4e0b8a6fcece948bd9689
Evan Phoenix <ephoenix@engineyard.com>
2011-05-11 13:24:31 -0700

    Fix Object.const_defined? wrt include modules. Fixes #865

--- kernel/common/module.rb
+++ kernel/common/module.rb
@@ -481,7 +481,19 @@ class Module
   end
 
   def const_defined?(name)
-    @constant_table.has_key? normalize_const_name(name)
+    name = normalize_const_name(name)
+    return true if @constant_table.has_key? name
+
+    # a silly special case
+    if self.equal? Object
+      mod = direct_superclass
+      while mod.kind_of? Rubinius::IncludedModule
+        return true if mod.constant_table.has_key? name
+        mod = mod.direct_superclass
+      end
+    end
+
+    return false
   end
 
   def const_get(name)
