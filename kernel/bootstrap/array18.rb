657833a65624aec1f623aa738d441919dfa22c39
Evan Phoenix <evan@fallingsnow.net>
2011-06-16 17:44:34 -0700

    Fix Array#map perf fix messed up in the 2.0 merge

--- kernel/bootstrap/array18.rb
+++ kernel/bootstrap/array18.rb
@@ -3,10 +3,22 @@ class Array
   # each element in self to the supplied block.
   def map
     return dup unless block_given?
-    array = Array.new size
-    i = -1
-    each { |x| array[i+=1] = yield(x) }
-    array
+    out = Array.new size
+
+    i = @start
+    total = i + @total
+    tuple = @tuple
+
+    out_tuple = out.tuple
+
+    j = 0
+    while i < total
+      out_tuple[j] = yield tuple.at(i)
+      i += 1
+      j += 1
+    end
+
+    out
   end
 
   def self.coerce_into_array(obj)
