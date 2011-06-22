251b9f5b38aec5d09467210c2c62095f55f21d2e
Flavio Castelli <fcastelli@novell.com>
2011-06-22 22:31:56 +0200

    Core refactoring: follow rubyspec coding guidelines
    Put utility class into a special module in order to avoid name clashes.

--- spec/ruby/core/kernel/instance_variable_defined_spec.rb
+++ spec/ruby/core/kernel/instance_variable_defined_spec.rb
@@ -1,20 +1,22 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-class Foo
-  def initialize
-    @bar = 'this is an instance variable'
+module KernelSpec
+  class Foo
+    def initialize
+      @bar = 'this is an instance variable'
+    end
   end
 end
 
 describe "Kernel#instance_variable_defined?" do
   it 'returns false if the instance variable is not defined' do
-    foo = Foo.new
+    foo = KernelSpec::Foo.new
     foo.instance_variable_defined?("@foo").should == false
     foo.instance_variable_defined?(:@foo).should == false
   end
 
   it 'returns true if the instance variable is defined' do
-    foo = Foo.new
+    foo = KernelSpec::Foo.new
     foo.instance_variable_defined?("@bar").should == true
     foo.instance_variable_defined?(:@bar).should == true
   end
