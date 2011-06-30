608059e51569e2e920f841749ad8f3ab11a2589b
Brian Ford <bford@engineyard.com>
2011-06-30 00:04:50 -0700

    Some more Enumerable specs for composing Enumerators.

--- spec/ruby/core/enumerable/shared/find_all.rb
+++ spec/ruby/core/enumerable/shared/find_all.rb
@@ -1,5 +1,6 @@
 describe :enumerable_find_all, :shared => true do
   before :each do
+    ScratchPad.record []
     @elements = (1..10).to_a
     @numerous = EnumerableSpecs::Numerous.new(*@elements)
   end
@@ -10,15 +11,20 @@ describe :enumerable_find_all, :shared => true do
     @numerous.send(@method) {|i| false }.should == []
   end
 
+  it "passes through the values yielded by #each_with_index" do
+    [:a, :b].each_with_index.send(@method) { |x, i| ScratchPad << [x, i] }
+    ScratchPad.recorded.should == [[:a, 0], [:b, 1]]
+  end
+
   ruby_version_is "" ... "1.8.7" do
     it "raises a LocalJumpError if no block given" do
       lambda { @numerous.send(@method) }.should raise_error(LocalJumpError)
     end
   end
+
   ruby_version_is "1.8.7" do
     it "returns an enumerator when no block given" do
       @numerous.send(@method).should be_an_instance_of(enumerator_class)
     end
   end
-
 end
