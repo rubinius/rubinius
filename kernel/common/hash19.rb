72502da288b770550cea9e129b901e1bafae7eb2
Brian Ford <bford@engineyard.com>
2011-07-03 16:40:27 -0700

    A few fixes for Hash in 1.9 mode.

--- kernel/common/hash19.rb
+++ kernel/common/hash19.rb
@@ -12,4 +12,31 @@ class Hash
   end
 
   alias_method :to_s, :inspect
+
+  alias_method :each_pair, :each
+
+  def merge!(other)
+    Rubinius.check_frozen
+
+    other = Rubinius::Type.coerce_to other, Hash, :to_hash
+
+    if block_given?
+      other.each_entry do |entry|
+        key = entry.key
+        if key? key
+          __store__ key, yield(key, self[key], entry.value)
+        else
+          __store__ key, entry.value
+        end
+      end
+    else
+      other.each_entry do |entry|
+        key = entry.key
+        __store__ key, entry.value
+      end
+    end
+    self
+  end
+  alias_method :update, :merge!
+
 end
