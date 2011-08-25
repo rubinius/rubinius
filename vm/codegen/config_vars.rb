bd3322f0ca2054ea659571491dbe49acf8ca6d03
Brian Ford <bford@engineyard.com>
2011-08-24 17:43:58 -0700

    Windows.

--- vm/codegen/config_vars.rb
+++ vm/codegen/config_vars.rb
@@ -4,6 +4,6 @@ require 'rubinius/configuration'
 
 c = Rubinius::ConfigurationVariables.instance
 
-File.open ARGV.shift, "w" do |f|
+File.open ARGV.shift, "wb" do |f|
   c.write_vm_variables(f)
 end
