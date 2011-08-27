ffe8f7f18c2dc6363e65e0e6f43256a5fcc99656
Brian Ford <bford@engineyard.com>
2011-08-26 21:03:00 -0700

    Added spec passing IO.copy_stream.
    Caveat, the specs are not complete yet. See
      http://redmine.ruby-lang.org/issues/5237

--- kernel/common/io19.rb
+++ kernel/common/io19.rb
@@ -8,4 +8,83 @@ class IO
       f.read(*arg)
     end
   end
+
+  class StreamCopier
+    def initialize(from, to, length, offset)
+      @length = length
+      @offset = offset
+
+      @from_io, @from = to_io(from, "rb")
+      @to_io, @to = to_io(to, "wb")
+
+      @method = read_method @from
+    end
+
+    def to_io(obj, mode)
+      if obj.kind_of? IO
+        flag = true
+        io = obj
+      else
+        flag = false
+
+        if obj.kind_of? String
+          io = File.open obj, mode
+        elsif obj.respond_to? :to_path
+          path = Rubinius::Type.coerce_to obj, String, :to_path
+          io = File.open path, mode
+        else
+          io = obj
+        end
+      end
+
+      return flag, io
+    end
+
+    def read_method(obj)
+      if obj.respond_to? :readpartial
+        :readpartial
+      else
+        :read
+      end
+    end
+
+    def run
+      @from.ensure_open_and_readable
+      @to.ensure_open_and_writable
+
+      saved_pos = @from.pos if @from_io
+
+      @from.seek @offset, IO::SEEK_CUR if @offset
+
+      size = rest = @length ? @length : 16384
+      bytes = 0
+
+      begin
+        while data = @from.send(@method, size, "")
+          @to.write data
+          bytes += data.size
+          rest -= data.size
+
+          break unless rest > 0
+        end
+      rescue EOFError
+        # done reading
+      end
+
+      @to.flush
+      return bytes
+    ensure
+      if @from_io
+        @from.pos = saved_pos if @offset
+      else
+        @from.close
+      end
+
+      @to.close unless @to_io
+    end
+  end
+
+  def self.copy_stream(from, to, max_length=nil, offset=nil)
+    StreamCopier.new(from, to, max_length, offset).run
+  end
 end
