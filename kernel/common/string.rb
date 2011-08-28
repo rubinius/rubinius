9649b36ab42a23f21482ca35fc41a432f924ee93
Brian Ford <bford@engineyard.com>
2011-08-27 19:27:49 -0700

    Fixed String#get|setbyte for negative indexes.

--- kernel/common/string.rb
+++ kernel/common/string.rb
@@ -892,6 +892,7 @@ class String
   # This method is specifically part of 1.9 but we enable it in 1.8 also
   # because we need it internally.
   def getbyte(index)
+    index = size + index if index < 0
     @data[index]
   end
 
@@ -1594,6 +1595,7 @@ class String
   # This method is specifically part of 1.9 but we enable it in 1.8 also
   # because we need it internally.
   def setbyte(index, byte)
+    index = size + index if index < 0
     @data[index] = byte
   end
 
