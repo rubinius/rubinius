a0688e1fc5ca886f07aa9e39aee73035760f99b9
Chris White <cwprogram@live.com>
2011-09-11 08:51:42 -0700

    Initial commit of the implementation of Base64.strict_decode64() written entirely in Ruby.

--- lib/base64.rb
+++ lib/base64.rb
@@ -38,6 +38,8 @@
 # RFC 2045 (http://www.faqs.org/rfcs/rfc2045.html).
 
 module Base64
+  ENCODE_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+  DECODE_TABLE = 0.upto(ENCODE_TABLE.length - 1).inject({}) { | holder,i | holder[ENCODE_TABLE[i..i]] = i; holder }
   module_function
 
   # Returns the Base64-decoded version of +str+.
@@ -114,4 +116,110 @@ module Base64
       print $&, "\n"
     end
   end 
+
+  # Returns the Base64-encoded version of +bin+.
+  # This method complies with RFC 4648.
+  # No line feeds are added.
+  
+  # This does not currently work as base64 strict requires that
+  # no newlines are inserted, and the current implementation of
+  # pack("m0") in rubinius does match with MRI's logic
+  #def strict_encode64(bin)
+  #  [bin].pack("m0").chomp
+  #end
+
+  # Returns the Base64-decoded version of +str+.
+  # This method complies with RFC 4648.
+  # ArgumentError is raised if +str+ is incorrectly padded or contains
+  # non-alphabet characters.  Note that CR or LF are also rejected.
+  def strict_decode64(str)
+    # no sense in doing all the magic below if it's just an empty string
+    return str if str == ""
+    # the base64 decoding algorithm operates in units of 4
+    raise ArgumentError, "invalid base64" unless (str.length % 4) == 0
+    decoded = ""
+
+    # Operate on the string in units of 4
+    0.step(str.length - 1, 4) do | i |
+      # This holds values for byte mangling to produce the original string
+      byte_buffer = Array.new
+      # This will hold the result of the byte mangling on the above array values
+      decode_buffer = Array.new
+
+      # In Rubinius, str[i] will return the byte value while str[i..i]
+      # gives the actual character, which is what we want
+      first_character = str[i..i]; second_character = str[i+1..i+1];
+      third_character = str[i+2..i+2]; fourth_character = str[i+3..i+3];
+
+      # For the first two characters, verify they are in the decoding table
+      # and append to the byte buffer accordingly
+      raise ArgumentError, "character not in base64 alphabet" if !DECODE_TABLE.key?(first_character)
+      byte_buffer << DECODE_TABLE[first_character]
+
+      raise ArgumentError, "character not in base64 alphabet" if !DECODE_TABLE.key?(second_character)
+      byte_buffer << DECODE_TABLE[second_character]
+
+      # check for padding
+      if third_character == "="
+        # the padding is invalid if it's
+        raise ArgumentError, "invalid base64 padding" if 
+          # not at the end of the string
+          (i + 4) != str.length or 
+          # the last character is also not an = sign
+          fourth_character != "=" or 
+          # not a correct placeholder for a 0 byte
+          (byte_buffer[1] & 0xf) != 0
+
+        decode_buffer << (byte_buffer[0] << 2 | byte_buffer[1] >> 4)
+        decoded << decode_buffer.pack('c*')
+        return decoded
+      end
+
+      raise ArgumentError, "character not in base64 alphabet" if !DECODE_TABLE.key?(third_character)
+      byte_buffer << DECODE_TABLE[third_character]
+
+      if fourth_character == "="
+        # same check logic as above
+        raise ArgumentError, "invalid base64 padding" if 
+          (i + 4) != str.length or 
+          (byte_buffer[2] & 0x3) != 0
+
+        decode_buffer << (byte_buffer[0] << 2 | byte_buffer[1] >> 4)
+        decode_buffer << (byte_buffer[1] << 4 | byte_buffer[2] >> 2)
+        decoded << decode_buffer.pack('c*')
+        return decoded
+      end
+
+      raise ArgumentError, "character not in base64 alphabet" if !DECODE_TABLE.key?(fourth_character)
+      byte_buffer << DECODE_TABLE[fourth_character]
+
+      # here some bit operations happen which are then packed
+      # to the original string value
+      decode_buffer << (byte_buffer[0] << 2 | byte_buffer[1] >> 4)
+      decode_buffer << (byte_buffer[1] << 4 | byte_buffer[2] >> 2)
+      decode_buffer << (byte_buffer[2] << 6 | byte_buffer[3])
+      decoded << decode_buffer.pack('c*')
+    end
+
+    decoded
+  end
+
+  # Returns the Base64-encoded version of +bin+.
+  # This method complies with ``Base 64 Encoding with URL and Filename Safe
+  # Alphabet'' in RFC 4648.
+  # The alphabet uses '-' instead of '+' and '_' instead of '/'.
+  
+  # This will be fixed when strict_encode64 is properly implemented
+  #def urlsafe_encode64(bin)
+  #  strict_encode64(bin).tr("+/", "-_")
+  #end
+
+  # Returns the Base64-decoded version of +str+.
+  # This method complies with ``Base 64 Encoding with URL and Filename Safe
+  # Alphabet'' in RFC 4648.
+  # The alphabet uses '-' instead of '+' and '_' instead of '/'.
+  def urlsafe_decode64(str)
+    strict_decode64(str.tr("-_", "+/"))
+  end
+
 end
