553ead307f4d48eb8409303a7143fb9d9a3577d9
jc00ke <jesse@jc00ke.com>
2011-10-07 00:23:35 -0700

    NoMethodError spec for BasicObject#method_missing
    In regards to rubinius/rubinius#1194

--- spec/ruby/core/basicobject/method_missing_spec.rb
+++ spec/ruby/core/basicobject/method_missing_spec.rb
@@ -3,6 +3,10 @@ require File.expand_path('../../../shared/kernel/method_missing', __FILE__)
 ruby_version_is "1.9" do
   describe "BasicObject#method_missing" do
     it_behaves_like(:method_missing, :method_missing, BasicObject)
+
+    it "should raise NoMethodError if undefined method is called" do
+      lambda { BasicObject.new.unknown }.should raise_error(NoMethodError)
+    end
   end
 
   describe "BasicObject#method_missing is undefined" do
