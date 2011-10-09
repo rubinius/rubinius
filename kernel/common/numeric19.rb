1d44cae48d3e80c7dd9853195a197889af5d4bb1
Ricardo Mendes <rokusu@gmail.com>
2011-10-09 07:29:29 +0100

    Numeric#img and Numeric#imaginary
    1.9 support

--- kernel/common/numeric19.rb
+++ kernel/common/numeric19.rb
@@ -2,6 +2,11 @@ class Numeric
   def i
     Complex(0, self)
   end
+
+  def imag
+    0
+  end
+  alias_method :imaginary, :imag
   
   def rationalize
     Rational(self,1)
@@ -11,5 +16,4 @@ class Numeric
     [self, 0]
   end
   alias_method :rectangular, :rect
-
 end
