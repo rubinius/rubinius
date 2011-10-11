fcdfdc43808a90311e77353ad911a41aa1df8264
Ricardo Mendes <rokusu@gmail.com>
2011-10-12 00:00:26 +0100

    fixed Comparable#== 1.9 behaviour
    - extracted method to comparable18 and comparable19
    - added files to respective load_order
    - fixed behavior of comparable19

--- kernel/common/comparable.rb
+++ kernel/common/comparable.rb
@@ -35,23 +35,6 @@
 
 module Comparable
 
-  # Compares two objects based on the receiver's <code><=></code>
-  # method, returning true if it returns 0. Also returns true if
-  # _obj_ and _other_ are the same object.
-  def ==(other)
-    return true if equal?(other)
-
-    begin
-      unless comp = (self <=> other)
-        return nil
-      end
-
-      return Comparable.compare_int(comp) == 0
-    rescue StandardError
-      return nil
-    end
-  end
-
   #  Compares two objects based on the receiver's <code><=></code>
   #  method, returning true if it returns 1.
   def >(other)
