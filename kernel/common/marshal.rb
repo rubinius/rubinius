73024491ada03a77e7c7332b7868cb6b4cb53717
Christopher Bertels <bakkdoor@flasht.de>
2011-10-10 23:24:46 -0700

    Added 1.8 and 1.9 files for marshall.rb and fixed bugs related to Marshal##dump 1.9 path committed earlier.

--- kernel/common/marshal.rb
+++ kernel/common/marshal.rb
@@ -32,25 +32,6 @@ class FalseClass
   end
 end
 
-class Class
-  def __marshal__(ms)
-    if Rubinius::Type.singleton_class_object(self)
-      raise TypeError, "singleton class can't be dumped"
-    elsif name.empty?
-      raise TypeError, "can't dump anonymous module #{self}"
-    end
-
-    "c#{ms.serialize_integer(name.length)}#{name}"
-  end
-end
-
-class Module
-  def __marshal__(ms)
-    raise TypeError, "can't dump anonymous module #{self}" if name.empty?
-    "m#{ms.serialize_integer(name.length)}#{name}"
-  end
-end
-
 class Symbol
   def __marshal__(ms)
     if idx = ms.find_symlink(self)
