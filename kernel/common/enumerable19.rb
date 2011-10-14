725707070128fc6f1f6d61981ef365c6ae57def6
Eero Saynatkari <projects@kittensoft.org>
2011-10-14 11:54:21 +0300

    Documented conversion protocol for 1.9 Enumerable#zip.

--- kernel/common/enumerable19.rb
+++ kernel/common/enumerable19.rb
@@ -373,12 +373,13 @@ module Enumerable
   #    enum.zip(arg, ...)                   => array
   #    enum.zip(arg, ...) { |arr| block }   => nil
   #
-  # Converts any arguments to arrays, then merges elements of +enum+ with
-  # corresponding elements from each argument. This generates a sequence of
-  # enum#size +n+-element arrays, where +n+ is one more that the count of
-  # arguments. If the size of any argument is less than enum#size, nil values
-  # are supplied. If a block given, it is invoked for each output array,
-  # otherwise an array of arrays is returned.
+  # Converts any arguments by using +#to_ary+, or +#each+, then
+  # merges elements of +enum+ with corresponding elements from each
+  # argument. This generates a sequence of enum#size +n+-element
+  # arrays, where +n+ is one more that the count of arguments. If the
+  # size of any argument is less than enum#size, nil values are supplied.
+  # If a block given, it is invoked for each output array, otherwise
+  # an array of arrays is returned.
   #
   #   a = [ 4, 5, 6 ]
   #   b = [ 7, 8, 9 ]
@@ -386,7 +387,7 @@ module Enumerable
   #   (1..3).zip(a, b)      #=> [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
   #   "cat\ndog".zip([1])   #=> [["cat\n", 1], ["dog", nil]]
   #   (1..3).zip            #=> [[1], [2], [3]]
-
+  #
   def zip(*args)
     args.map! do |a|
       if a.respond_to? :to_ary
