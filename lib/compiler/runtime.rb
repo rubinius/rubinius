78067c0256103e4ce5c0b26091c14abafcdc822c
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-10-15 14:20:17 -0700

    implement flip-flops, fixes #1257

--- lib/compiler/runtime.rb
+++ lib/compiler/runtime.rb
@@ -39,6 +39,20 @@ module Rubinius
 
     module_function :find_constant_for_op_asign_or
 
+    def get_flip_flop(scope, index)
+      scope.flip_flops ||= {}
+      scope.flip_flops[index]
+    end
+
+    module_function :get_flip_flop
+
+    def set_flip_flop(scope, index, value)
+      scope.flip_flops ||= {}
+      scope.flip_flops[index] = value
+    end
+
+    module_function :set_flip_flop
+
     def self.rbx_marshal_constant
       name
     end
