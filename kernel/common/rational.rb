74f2e6dbc2b213bd1c9b916fb660a2c33638ff98
burningTyger <b6tyger@gmail.com>
2011-10-17 00:09:43 +0200

    yet another fix for c63d832a1a

--- kernel/common/rational.rb
+++ kernel/common/rational.rb
@@ -246,6 +246,24 @@ class Rational < Numeric
   end
 
   #
+  # Returns the remainder when this value is divided by +other+.
+  #
+  # Examples:
+  #   r = Rational(7,4)    # -> Rational(7,4)
+  #   r % Rational(1,2)    # -> Rational(1,4)
+  #   r % 1                # -> Rational(3,4)
+  #   r % Rational(1,7)    # -> Rational(1,28)
+  #   r % 0.26             # -> 0.19
+  #
+  def % (other)
+    if other == 0.0
+      raise ZeroDivisionError, "division by zero"
+    end
+    value = (self / other).floor
+    return self - other * value
+  end
+  
+  #
   # Returns the quotient _and_ remainder.
   #
   # Examples:
