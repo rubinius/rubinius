282ab51dd415d24aad336b76226ff8d9334cc041
Shane Emmons <semmons99@gmail.com>
2011-10-16 21:48:29 -0400

    patch Fixnum#div for 1.9

--- kernel/common/fixnum18.rb
+++ kernel/common/fixnum18.rb
@@ -1,21 +1,8 @@
 class Fixnum < Integer
-  def imaginary
-    0
-  end
-
-  def real?
-    true
-  end
-
   # Must be it's own method, so that super calls the correct method
   # on Numeric
   def div(o)
     Rubinius.primitive :fixnum_div
-
-    if o.is_a?(Float) && o == 0.0
-      raise ZeroDivisionError, "division by zero"
-    end
-
     redo_coerced :div, o
   end
 end
