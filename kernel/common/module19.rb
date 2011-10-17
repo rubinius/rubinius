e473360a925baa32003c0a00ff93f50da8ab8f63
Max Gonzih <gonzih@gmail.com>
2011-10-17 20:09:37 +0300

    fixes spec for Module#attr

--- kernel/common/module19.rb
+++ kernel/common/module19.rb
@@ -18,4 +18,21 @@ class Module
   def name
     @module_name ? @module_name.to_s : nil
   end
+
+  def attr(*attributes)
+    vis = Rubinius::VariableScope.of_sender.method_visibility
+
+    if attributes.size == 2 && (attributes.last.is_a?(TrueClass) || attributes.last.is_a?(FalseClass))
+      name = attributes.first
+      Rubinius.add_reader name, self, vis
+      Rubinius.add_writer name, self, vis if attributes.last
+    else
+      attributes.each do |name|
+        raise TypeError, "#{name} is not a symbol" unless name.respond_to(:to_str)
+        Rubinius.add_reader(name, self, vis)
+      end
+    end
+
+    return nil
+  end
 end
