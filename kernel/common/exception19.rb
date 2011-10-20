eabc546ffb43541e39e264753252391deef82ab7
Marjan Krekoten' (Мар'ян Крекотень) <krekoten@gmail.com>
2011-10-20 21:54:15 +0300

    Implements Exception#== behaviour for 1.9 mode

--- kernel/common/exception19.rb
+++ kernel/common/exception19.rb
@@ -1,2 +1,7 @@
 class Exception
+  def ==(other)
+    (self.class == other.class || other.class.ancestors.include?(Exception)) &&
+      message == other.message &&
+      backtrace == other.backtrace
+  end
 end
