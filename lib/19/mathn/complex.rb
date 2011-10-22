a0870611678afb8fe0749be8608732e94deca51c
Leo Cassarani <leonardo.cassarani@gmail.com>
2011-10-23 00:44:08 +0100

    Fix Kernel#Complex failing spec under 1.9

--- lib/19/mathn/complex.rb
+++ lib/19/mathn/complex.rb
@@ -1,1 +1,11 @@
-# TODO: port MRI code
+#
+# Creates a Complex number.  +a+ and +b+ should be Numeric.  The result will be
+# <tt>a+bi</tt>.
+#
+def Complex(a, b = 0)
+  if b == 0
+    a
+  else
+    Complex.new(a.real - b.imag, a.imag + b.real)
+  end
+end
