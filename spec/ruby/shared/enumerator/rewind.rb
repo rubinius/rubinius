6345f327463de0b271996bff9a43dcb681751117
John Downey <jdowney@gmail.com>
2011-10-23 11:14:36 -0400

    Added spec for Enumerator#rewind to ensure implementation is clearing #peek lookahead value

--- spec/ruby/shared/enumerator/rewind.rb
+++ spec/ruby/shared/enumerator/rewind.rb
@@ -29,4 +29,13 @@ describe :enum_rewind, :shared => true do
     @enum.next.should == 1
   end
 
+  ruby_version_is "1.9" do
+    it "works with peek to reset the position" do
+      @enum.next
+      @enum.next
+      @enum.rewind
+      @enum.next
+      @enum.peek.should == 2
+    end
+  end
 end
