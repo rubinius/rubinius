67ac56fd9ec68ef59f7813124cd10b110605997d
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-10-22 20:55:16 -0500

    Revert "Fix Kernel#Complex failing spec under 1.9"
    This reverts commit a0870611678afb8fe0749be8608732e94deca51c.

--- lib/19/mathn/complex.rb
+++ lib/19/mathn/complex.rb
@@ -1,11 +1,1 @@
-#
-# Creates a Complex number.  +a+ and +b+ should be Numeric.  The result will be
-# <tt>a+bi</tt>.
-#
-def Complex(a, b = 0)
-  if b == 0
-    a
-  else
-    Complex.new(a.real - b.imag, a.imag + b.real)
-  end
-end
+# TODO: port MRI code
