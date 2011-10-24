0ee533b57c619b688ef40e50f9e15f1d38255a29
Brian Ford <brixen@gmail.com>
2011-10-24 15:22:12 -0700

    Rational(String [, String]) specs.

--- spec/ruby/core/rational/Rational_spec.rb
+++ spec/ruby/core/rational/Rational_spec.rb
@@ -14,6 +14,31 @@ ruby_version_is "1.9" do
       it_behaves_like(:rational_rational_int_int, :Rational)
     end
 
+    describe "when passed a String" do
+      it "converts the String to a Rational using the same method as String#to_r" do
+        r = Rational(13, 25)
+        s_r = ".52".to_r
+        r_s = Rational(".52")
+
+        r_s.should == r
+        r_s.should == s_r
+      end
+
+      it "scales the Rational value of the first argument by the Rational value of the second" do
+        Rational(".52", ".6").should == Rational(13, 15)
+        Rational(".52", "1.6").should == Rational(13, 40)
+      end
+
+      it "does not use the same method as Float#to_r" do
+        r = Rational(3, 5)
+        f_r = 0.6.to_r
+        r_s = Rational("0.6")
+
+        r_s.should == r
+        r_s.should_not == f_r
+      end
+    end
+
     it "raises a TypeError if the arguments are not Integers" do
       lambda {
         Rational(nil)
