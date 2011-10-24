2e8c69a9611bd6a5cee1beb0d613b42e0285a0b8
Xavier Shay <xavier@rhnh.net>
2011-10-23 20:35:02 -0700

    Naive implementation of Proc#source_location.
    It is mostly right, but doesn't pass rubyspec for multi-line procs.

--- kernel/common/proc19.rb
+++ kernel/common/proc19.rb
@@ -17,6 +17,10 @@ class Proc
     }
   end
 
+  def source_location
+    [@block.file.to_s, @block.line]
+  end
+
   def to_s
     l = " (lambda)" if lambda?
     "#<#{self.class}:0x#{self.object_id.to_s(16)}@#{@block.file}:#{@block.line}#{l}>"
