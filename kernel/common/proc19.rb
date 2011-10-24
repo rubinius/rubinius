9bea7a43460723a5fb531e6c9423d7a075820c9a
Xavier Shay <xavier@rhnh.net>
2011-10-23 20:34:36 -0700

    Implement Proc#to_s for 1.9 (with lambda tag).

--- kernel/common/proc19.rb
+++ kernel/common/proc19.rb
@@ -5,4 +5,9 @@ class Proc
   def source_location
     [@block.file.to_s, @block.line]
   end
+
+  def to_s
+    l = " (lambda)" if lambda?
+    "#<#{self.class}:0x#{self.object_id.to_s(16)}@#{@block.file}:#{@block.line}#{l}>"
+  end
 end
