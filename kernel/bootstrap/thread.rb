cc7cd77be26b9273f1f18b49ecbcdb2bcb18adac
obrok <pawel.obrok@gmail.com>
2011-10-24 22:03:29 +0200

    Fixed Thread.start throws ArgumentError without block in 1.9

--- kernel/bootstrap/thread.rb
+++ kernel/bootstrap/thread.rb
@@ -114,31 +114,6 @@ class Thread
     return thr
   end
 
-  def self.start(*args)
-    thr = Rubinius.invoke_primitive :thread_allocate, self
-
-    Rubinius.asm(args, thr) do |args, obj|
-      run obj
-      dup
-
-      push_false
-      send :setup, 1, true
-      pop
-
-      run args
-      push_block
-      send_with_splat :__thread_initialize__, 0, true
-      # no pop here, as .asm blocks imply a pop as they're not
-      # allowed to leak a stack value
-    end
-
-    return thr
-  end
-
-  class << self
-    alias_method :fork, :start
-  end
-
   def initialize(*args, &block)
     unless block
       Kernel.raise ThreadError, "no block passed to Thread#initialize"
