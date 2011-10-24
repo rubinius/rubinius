86dc15b2ee84c18e65ca883d7a4357bd9db81167
Xavier Shay <xavier@rhnh.net>
2011-10-23 20:27:52 -0700

    Implement Proc#curry.

--- kernel/common/proc19.rb
+++ kernel/common/proc19.rb
@@ -2,6 +2,46 @@ class Proc
 
   alias_method :===, :call
 
+  def curry(curried_arity = nil)
+    if lambda? && curried_arity
+      if arity > 0 && curried_arity != arity
+        raise ArgumentError, "Wrong number of arguments (%i for %i)" % [
+          curried_arity,
+          arity
+        ]
+      end
+
+      if arity < 0 && curried_arity < (-arity - 1)
+        raise ArgumentError, "Wrong number of arguments (%i for %i)" % [
+          curried_arity,
+          -arity - 1
+        ]
+      end
+    end
+
+    args = []
+
+    m = lambda? ? :lambda : :proc
+    f = send(m) {|*x|
+      args += x
+      if args.length >= arity
+        self[*args]
+      else
+        f
+      end
+    }
+    f.singleton_class.send(:define_method, :binding) {
+      raise ArgumentError, "cannot create binding from f proc"
+    }
+    f.singleton_class.send(:define_method, :parameters) {
+      [[:rest]]
+    }
+    f.singleton_class.send(:define_method, :source_location) {
+      nil
+    }
+    f
+  end
+
   def source_location
     [@block.file.to_s, @block.line]
   end
