7769afa2d6d33a14b37cf32b91a6d55155e00006
Marjan Krekoten' (Мар'ян Крекотень) <krekoten@gmail.com>
2011-10-26 15:46:09 +0300

    Class#allocate raises TypeError on #superclass

--- kernel/common/class.rb
+++ kernel/common/class.rb
@@ -81,21 +81,6 @@ class Class
     super
   end
 
-  ##
-  # Returns the Class object that this Class inherits from. Included Modules
-  # are not considered for this purpose.
-
-  def superclass
-    cls = direct_superclass
-    return nil unless cls
-
-    while cls and cls.kind_of? Rubinius::IncludedModule
-      cls = cls.direct_superclass
-    end
-
-    return cls
-  end
-
   def inherited(name)
   end
   private :inherited
