c19c199e1e41aa611f662b6581cf427b4d603072
John Firebaugh <john.firebaugh@gmail.com>
2011-10-26 10:11:30 -0700

    Spec Complex#quo, an alias for #/ on 1.9

--- spec/ruby/shared/complex/divide.rb
+++ spec/ruby/shared/complex/divide.rb
@@ -5,44 +5,44 @@ describe :complex_divide, :shared => true do
     it "divides according to the usual rule for complex numbers" do
       a = Complex((1 * 10) - (2 * 20), (1 * 20) + (2 * 10))
       b = Complex(1, 2)
-      (a / b).should == Complex(10, 20)
+      a.send(@method, b).should == Complex(10, 20)
 
       c = Complex((1.5 * 100.2) - (2.1 * -30.3), (1.5 * -30.3) + (2.1 * 100.2))
       d = Complex(1.5, 2.1)
       # remember the floating-point arithmetic
-      (c / d).should be_close(Complex(100.2, -30.3), TOLERANCE)
+      c.send(@method, d).should be_close(Complex(100.2, -30.3), TOLERANCE)
     end
   end
 
   describe "with Fixnum" do
     it "divides both parts of the Complex number" do
-      (Complex(20, 40) / 2).should == Complex(10, 20)
-      (Complex(30, 30) / 10).should == Complex(3, 3)
+      Complex(20, 40).send(@method, 2).should == Complex(10, 20)
+      Complex(30, 30).send(@method, 10).should == Complex(3, 3)
     end
 
     it "raises a ZeroDivisionError when given zero" do
-      lambda { Complex(20, 40) / 0 }.should raise_error(ZeroDivisionError)
+      lambda { Complex(20, 40).send(@method, 0) }.should raise_error(ZeroDivisionError)
     end
   end
 
   describe "with Bignum" do
     it "divides both parts of the Complex number" do
-      (Complex(20, 40) / 2).should == Complex(10, 20)
-      (Complex(15, 16) / 2.0).should be_close(Complex(7.5, 8), TOLERANCE)
+      Complex(20, 40).send(@method, 2).should == Complex(10, 20)
+      Complex(15, 16).send(@method, 2.0).should be_close(Complex(7.5, 8), TOLERANCE)
     end
   end
 
   describe "with Float" do
     it "divides both parts of the Complex number" do
-      (Complex(3, 9) / 1.5).should == Complex(2, 6)
-      (Complex(15, 16) / 2.0).should be_close(Complex(7.5, 8), TOLERANCE)
+      Complex(3, 9).send(@method, 1.5).should == Complex(2, 6)
+      Complex(15, 16).send(@method, 2.0).should be_close(Complex(7.5, 8), TOLERANCE)
     end
 
     it "returns Complex(Infinity, Infinity) when given zero" do
-      (Complex(20, 40) / 0.0).real.infinite?.should == 1
-      (Complex(20, 40) / 0.0).imag.infinite?.should == 1
-      (Complex(-20, 40) / 0.0).real.infinite?.should == -1
-      (Complex(-20, 40) / 0.0).imag.infinite?.should == 1
+      Complex(20, 40).send(@method, 0.0).real.infinite?.should == 1
+      Complex(20, 40).send(@method, 0.0).imag.infinite?.should == 1
+      Complex(-20, 40).send(@method, 0.0).real.infinite?.should == -1
+      Complex(-20, 40).send(@method, 0.0).imag.infinite?.should == 1
     end
   end
 
@@ -52,7 +52,7 @@ describe :complex_divide, :shared => true do
 
       obj = mock("Object")
       obj.should_receive(:coerce).with(value).and_return([4, 2])
-      (value / obj).should == 2
+      value.send(@method, obj).should == 2
     end
   end
 end
