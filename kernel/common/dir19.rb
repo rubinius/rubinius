fe90f75c00cc681ddd52722c2503dc3f841556ba
Brian Ford <brixen@gmail.com>
2011-10-27 00:16:46 -0700

    Stub 1.9 options Hash for Dir.open, Dir.entries.
    Once we have better encoding support we can honor these options.

--- kernel/common/dir19.rb
+++ kernel/common/dir19.rb
@@ -4,6 +4,33 @@ class Dir
   # screwing with ::File later (ie, fakefs)
   PrivateFile = ::File
 
+  def self.open(path, options=undefined)
+    dir = new path
+    if block_given?
+      begin
+        value = yield dir
+      ensure
+        dir.close
+      end
+
+      return value
+    else
+      return dir
+    end
+  end
+
+  def self.entries(path, options=undefined)
+    ret = []
+
+    open(path) do |dir|
+      while s = dir.read
+        ret << s
+      end
+    end
+
+    ret
+  end
+
   def self.exists?(path)
     PrivateFile.directory?(path)
   end
