fd2f6fdd42dec254e2dcf270207ecab77c77eca0
Cameron Dykes <yellow5c@gmail.com>
2011-10-26 21:52:22 -0500

    Implement Float#round under 1.9

--- kernel/common/float19.rb
+++ kernel/common/float19.rb
@@ -33,4 +33,45 @@ class Float
 
     (f * (RADIX ** e)).to_r
   end
+
+  def round(precision=0)
+    Integer.induced_from(precision)
+    if precision == 0
+      round_up? ? ceil : floor
+    else
+      multiple_of_ten = 10.0 ** Integer(precision).abs
+      if precision > 0
+        if finite?
+          number = self * multiple_of_ten
+          if number.infinite?
+            self
+          else
+            if round_up?(number)
+              number.ceil / multiple_of_ten
+            else
+              number.floor / multiple_of_ten
+            end
+          end
+        else
+          self
+        end
+      else
+        if multiple_of_ten.infinite?
+          0
+        else
+          number = self / multiple_of_ten
+          if round_up?(number)
+            (1..Integer(precision).abs).inject(number.ceil) { |num| num * 10 }
+          else
+            (1..Integer(precision).abs).inject(number.floor) { |num| num * 10 }
+          end
+        end
+      end
+    end
+  end
+
+  def round_up?(value=self)
+    (value > 0 && (value - value.to_i).abs >= 0.5) || (value < 0 && (value - value.to_i).abs < 0.5)
+  end
+  private :round_up?
 end
