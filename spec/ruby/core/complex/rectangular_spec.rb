1fd9a942d112894f2ed09ab42ae2af65e6e042ad
John Firebaugh <john.firebaugh@gmail.com>
2011-10-30 14:27:53 -0700

    Spec Complex.rect/rectangular

--- spec/ruby/core/complex/rectangular_spec.rb
+++ spec/ruby/core/complex/rectangular_spec.rb
@@ -1,14 +1,11 @@
-require File.expand_path('../../../spec_helper', __FILE__)
+require File.expand_path('../../../shared/complex/rect', __FILE__)
 
 ruby_version_is "1.9" do
-
-  require File.expand_path('../../../shared/complex/rect', __FILE__)
-
   describe "Complex#rectangular" do
     it_behaves_like(:complex_rect, :rectangular)
   end
 
   describe "Complex.rectangular" do
-    it "needs specs"
+    it_behaves_like(:complex_rect_class, :rectangular)
   end
 end
