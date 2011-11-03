418eedc3b1a0ca32a293dce93e576982403b69a9
John Firebaugh <john.firebaugh@gmail.com>
2011-11-03 11:13:43 -0700

    Add Complex#coerce specs

--- spec/ruby/shared/complex/coerce.rb
+++ spec/ruby/shared/complex/coerce.rb
@@ -33,9 +33,40 @@ describe :complex_coerce, :shared => true do
     result.last.should be_kind_of(Complex)
   end
 
+  it "returns an array containing other and self when other is a Complex" do
+    other = Complex(2)
+    result = @one.coerce(other)
+    result.should == [other, @one]
+    result.first.should equal(other)
+    result.last.should equal(@one)
+  end
+
+  ruby_version_is "1.9" do
+    it "returns an array containing other as Complex and self when other is a Numeric which responds to #real? with true" do
+      other = mock_numeric('other')
+      other.should_receive(:real?).any_number_of_times.and_return(true)
+      result = @one.coerce(other)
+      result.should == [other, @one]
+      result.first.should eql(Complex(other))
+      result.last.should equal(@one)
+    end
+
+    it "raises TypeError when other is a Numeric which responds to #real? with false" do
+      other = mock_numeric('other')
+      other.should_receive(:real?).any_number_of_times.and_return(false)
+      lambda { @one.coerce(other) }.should raise_error(TypeError)
+    end
+  end
+
   it "raises a TypeError when other is a String" do
     lambda { @one.coerce("20") }.should raise_error(TypeError)
-    lambda { @one.coerce(nil)   }.should raise_error(TypeError)
+  end
+
+  it "raises a TypeError when other is nil" do
+    lambda { @one.coerce(nil) }.should raise_error(TypeError)
+  end
+
+  it "raises a TypeError when other is false" do
     lambda { @one.coerce(false) }.should raise_error(TypeError)
   end
 end
