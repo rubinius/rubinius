ec14505218a1aa15a04b86afd47a72555b52d808
Brian Ford <brixen@gmail.com>
2011-11-03 20:05:13 -0700

    Added internal, external encodings to IO.

--- kernel/common/io19.rb
+++ kernel/common/io19.rb
@@ -1,4 +1,7 @@
 class IO
+  attr_accessor :external
+  attr_accessor :internal
+
   def self.binread(file, *arg)
     unless arg.size < 3
       raise ArgumentError, "wrong number of arguments (#{1+arg.size} for 1..3)"
