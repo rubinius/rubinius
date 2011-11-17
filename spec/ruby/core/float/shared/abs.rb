7f3dd2587d19839ec20005bd09b49e2eed6ba1e8
John Firebaugh <john.firebaugh@gmail.com>
2011-11-17 11:14:08 -0800

    Test correct method in float_abs shared specs

--- spec/ruby/core/float/shared/abs.rb
+++ spec/ruby/core/float/shared/abs.rb
@@ -2,8 +2,8 @@ require File.expand_path('../../../../spec_helper', __FILE__)
 
 describe :float_abs, :shared => true do
   it "returns the absolute value" do
-    -99.1.abs.should be_close(99.1, TOLERANCE)
-    4.5.abs.should be_close(4.5, TOLERANCE)
-    0.0.abs.should be_close(0.0, TOLERANCE)
+    -99.1.send(@method).should be_close(99.1, TOLERANCE)
+    4.5.send(@method).should be_close(4.5, TOLERANCE)
+    0.0.send(@method).should be_close(0.0, TOLERANCE)
   end
 end
