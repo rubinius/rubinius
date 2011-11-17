ba0fce4dcb0033a455bf0dcbc5f651e756c8f90a
John Firebaugh <john.firebaugh@gmail.com>
2011-11-17 12:10:36 -0800

    Add Float#abs specs

--- spec/ruby/core/float/shared/abs.rb
+++ spec/ruby/core/float/shared/abs.rb
@@ -6,4 +6,16 @@ describe :float_abs, :shared => true do
     4.5.send(@method).should be_close(4.5, TOLERANCE)
     0.0.send(@method).should be_close(0.0, TOLERANCE)
   end
+
+  it "returns 0.0 if -0.0" do
+    (-0.0).send(@method).should be_positive_zero
+  end
+
+  it "returns Infinity if -Infinity" do
+    (-infinity_value).send(@method).infinite?.should == 1
+  end
+
+  it "returns NaN if NaN" do
+    nan_value.send(@method).nan?.should be_true
+  end
 end
