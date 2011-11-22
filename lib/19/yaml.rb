783eb0bd552a9bb6462aa44c2e1f0b7c9cf7136f
Brian Ford <brixen@gmail.com>
2011-11-21 21:52:52 -0800

    Updated yaml to 1.9.3p0.

--- lib/19/yaml.rb
+++ lib/19/yaml.rb
@@ -1,3 +1,18 @@
+##
+# The YAML module allows you to use one of the two YAML engines that ship with
+# ruby.  By default Psych is used but the old and unmaintained Syck may be
+# chosen.
+#
+# See Psych or Syck for usage and documentation.
+#
+# To set the YAML engine to syck:
+#
+#   YAML::ENGINE.yamler = 'syck'
+#
+# To set the YAML engine back to psych:
+#
+#   YAML::ENGINE.yamler = 'psych'
+
 module YAML
   class EngineManager # :nodoc:
     attr_reader :yamler
@@ -13,7 +28,7 @@ module YAML
     def yamler= engine
       raise(ArgumentError, "bad engine") unless %w{syck psych}.include?(engine)
 
-      require engine
+      require engine unless (engine == 'syck' ? Syck : Psych).const_defined?(:VERSION)
 
       Object.class_eval <<-eorb, __FILE__, __LINE__ + 1
         remove_const 'YAML'
@@ -27,10 +42,28 @@ module YAML
     end
   end
 
+  ##
+  # Allows changing the current YAML engine.  See YAML for details.
+
   ENGINE = YAML::EngineManager.new
 end
 
-engine = (!defined?(Syck) && defined?(Psych) ? 'psych' : 'syck')
+if defined?(Psych)
+  engine = 'psych'
+elsif defined?(Syck)
+  engine = 'syck'
+else
+  begin
+    require 'psych'
+    engine = 'psych'
+  rescue LoadError
+    warn "#{caller[0]}:"
+    warn "It seems your ruby installation is missing psych (for YAML output)."
+    warn "To eliminate this warning, please install libyaml and reinstall your ruby."
+    require 'syck'
+    engine = 'syck'
+  end
+end
 
 module Syck
   ENGINE = YAML::ENGINE
