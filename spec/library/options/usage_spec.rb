fcd681c0c9616ae9da0e47897c965e2512e25fe4
Brian Ford <brixen@gmail.com>
2011-11-21 21:31:21 -0800

    options specs.

--- spec/library/options/usage_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path('../../../spec_helper', __FILE__)
-require File.expand_path('../shared/help', __FILE__)
-
-describe "Options#usage" do
-  it_behaves_like :lib_options_usage_help, :usage
-end
