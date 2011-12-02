048131541ebf9212c1eacc112be2a58dda20defd
John Firebaugh <john.firebaugh@gmail.com>
2011-12-01 19:15:46 -0800

    Fix 1.9 #instance_variable_{get,set} when passed Fixnum
    In 1.9, both methods raise TypeError when passed a Fixnum, and Rubinius
    no longer needs to deviate.

--- kernel/common/type19.rb
+++ kernel/common/type19.rb
@@ -29,5 +29,22 @@ module Rubinius
       obj = obj.to_str if obj.respond_to?(:to_str)
       coerce_to(obj, Symbol, :to_sym)
     end
+
+    def self.ivar_validate(name)
+      # adapted from rb_to_id
+      case name
+      when String
+        return name.to_sym if name[0] == ?@
+      when Symbol
+        return name if name.is_ivar?
+      when Fixnum
+        raise TypeError, "#{name.inspect} is not a symbol"
+      else
+        name = Rubinius::Type.coerce_to(name, String, :to_str)
+        return name.to_sym if name[0] == ?@
+      end
+
+      raise NameError, "`#{name}' is not allowed as an instance variable name"
+    end
   end
 end
