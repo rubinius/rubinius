ec180b08dd60b90db66d7b7bf8e9b53e841a60bb
John Firebaugh <john.firebaugh@gmail.com>
2011-12-02 14:27:07 -0800

    The Fixnum case is unnecessary in 1.9 mode
    As pointed out by dbussink.

--- kernel/common/type19.rb
+++ kernel/common/type19.rb
@@ -37,8 +37,6 @@ module Rubinius
         return name.to_sym if name[0] == ?@
       when Symbol
         return name if name.is_ivar?
-      when Fixnum
-        raise TypeError, "#{name.inspect} is not a symbol"
       else
         name = Rubinius::Type.coerce_to(name, String, :to_str)
         return name.to_sym if name[0] == ?@
