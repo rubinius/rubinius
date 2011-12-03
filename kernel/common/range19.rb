56ce206466cb863abc195e217fa492dbf2eb00ef
Sergey Nartimov <just.lest@gmail.com>
2011-12-03 13:30:21 +0300

    fix Range#min and Range#max when range excludes end

--- kernel/common/range19.rb
+++ kernel/common/range19.rb
@@ -3,13 +3,13 @@ class Range
 
   def max(&block)
     return super(&block) if block_given?
-    return nil if @end < @begin
+    return nil if @end < @begin || (@excl && @end == @begin)
     @end
   end
 
   def min(&block)
     return super(&block) if block_given?
-    return nil if @end < @begin
+    return nil if @end < @begin || (@excl && @end == @begin)
     @begin
   end
 end
