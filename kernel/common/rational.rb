ef635286179775bde271fe0e415f130dce06a919
Sergey Nartimov <just.lest@gmail.com>
2011-12-06 20:35:22 +0300

    remove unnecessary rational19.rb, move to rational.rb

--- kernel/common/rational.rb
+++ kernel/common/rational.rb
@@ -460,6 +460,50 @@ class Rational < Numeric
   attr_reader :denominator
 
   private :initialize
+
+  #
+  # Returns a simpler approximation of the value if an optional
+  # argument eps is given (rat-|eps| <= result <= rat+|eps|), self
+  # otherwise.
+  #
+  # For example:
+  #
+  #    r = Rational(5033165, 16777216)
+  #    r.rationalize                    #=> (5033165/16777216)
+  #    r.rationalize(Rational('0.01'))  #=> (3/10)
+  #    r.rationalize(Rational('0.1'))   #=> (1/3)
+  #
+  def rationalize(eps=undefined)
+    if eps.equal?(undefined)
+      self
+    else
+      e = eps.abs
+      a = self - e
+      b = self + e
+
+      p0 = 0
+      p1 = 1
+      q0 = 1
+      q1 = 0
+
+      while true
+        c = a.ceil
+        break if c < b
+        k = c - 1
+        p2 = k * p1 + p0
+        q2 = k * q1 + q0
+        t = 1 / (b - k)
+        b = 1 / (a - k)
+        a = t
+        p0 = p1
+        q0 = q1
+        p1 = p2
+        q1 = q2
+      end
+
+      Rational(c * p1 + p0, c * q1 + q0)
+    end
+  end
 end
 
 class Fixnum
