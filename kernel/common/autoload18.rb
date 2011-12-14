120828faa44fe0f9073be7884cc2ecf3f22b4b8d
Evan Phoenix <evan@fallingsnow.net>
2011-12-14 11:29:49 -0800

    Make autoload concurrent safe

--- kernel/common/autoload18.rb
+++ kernel/common/autoload18.rb
@@ -3,14 +3,64 @@ class Autoload
   # When any code that finds a constant sees an instance of Autoload as its match,
   # it calls this method on us
   def call(honor_require=false)
-    # Remove the autoload object from the constant table it was in, so
-    # we don't recurse back into ourself.
-    scope.constant_table.delete @name
+    # We leave the Autoload object in the constant table so that if another
+    # thread hits this while we're mid require they'll be come in here and
+    # be held by require until @path is available, at which time they'll
+    # attempt the lookup again.
+    #
+    worked = false
+    attempted = false
+
+    begin
+      worked = Rubinius::CodeLoader.require @path
+    else
+      attempted = true
+    ensure
+      unless attempted
+        # Because it didn't work out, we remove it now
+        # rather than before.
+        scope.constant_table.delete @name
+        Rubinius.inc_global_serial
+      end
+    end
+
+    if !honor_require or worked
+      find_const
+    end
+  end
+
+  def find_const
+    current, constant = @scope, undefined
+
+    while current
+      constant = current.constant_table.fetch name, undefined
+      unless constant.equal?(undefined)
+        if constant.equal? self
+          @scope.constant_table.delete @name
+          Rubinius.inc_global_serial
+          return scope.const_missing(name)
+        end
+        return constant
+      end
+
+      current = current.direct_superclass
+    end
+
+    if instance_of?(Module)
+      constant = Object.constant_table.fetch name, undefined
+      unless constant.equal?(undefined)
+        if constant.equal? self
+          @scope.constant_table.delete @name
+          Rubinius.inc_global_serial
+          return scope.const_missing(name)
+        end
+        return constant
+      end
+    end
+
+    @scope.constant_table.delete @name
     Rubinius.inc_global_serial
 
-    worked = Rubinius::CodeLoader.require @path
-    if !honor_require or worked
-      scope.const_get @name
-    end
+    scope.const_missing(name)
   end
 end
--- kernel/common/autoload19.rb
+++ kernel/common/autoload19.rb
@@ -3,19 +3,43 @@ class Autoload
   # When any code that finds a constant sees an instance of Autoload as its match,
   # it calls this method on us
   def call(honor_require=false)
-    # Remove the autoload object from the constant table it was in, so
-    # we don't recurse back into ourself.
-    scope.constant_table.delete @name
-    Rubinius.inc_global_serial
-
+    # We leave the Autoload object in the constant table so that if another
+    # thread hits this while we're mid require they'll be come in here and
+    # be held by require until @path is available, at which time they'll
+    # attempt the lookup again.
+    #
     worked = Rubinius::CodeLoader.require @path
+
     if !honor_require or worked
-      scope.const_get @name
+      find_const
     end
-  rescue NameError, LoadError => e
-    scope.constant_table[@name] = self
-    Rubinius.inc_global_serial
-    
-    raise e
+  end
+
+  def find_const
+    current, constant = @scope, undefined
+
+    while current
+      constant = current.constant_table.fetch name, undefined
+      unless constant.equal?(undefined)
+        if constant.equal? self
+          return scope.const_missing(name)
+        end
+        return constant
+      end
+
+      current = current.direct_superclass
+    end
+
+    if instance_of?(Module)
+      constant = Object.constant_table.fetch name, undefined
+      unless constant.equal?(undefined)
+        if constant.equal? self
+          return scope.const_missing(name)
+        end
+        return constant
+      end
+    end
+
+    scope.const_missing(name)
   end
 end
