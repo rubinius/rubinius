32c6d3f04048f078318e3807aedced9086abca78
Tom Counsell <tom@counsell.org>
2011-12-18 22:29:59 +0000

    Spec to test issue #1457, Enumerable#inject fails in certain cases in 1.9 mode

--- spec/ruby/core/enumerable/inject_spec.rb
+++ spec/ruby/core/enumerable/inject_spec.rb
@@ -4,4 +4,14 @@ require File.expand_path('../shared/inject', __FILE__)
 
 describe "Enumerable#inject" do
   it_behaves_like :enumerable_inject, :inject
+  
+  it "should work with enumerators that return arrays" do
+    passed_values = []
+    [:a].each_with_index.inject(0) do |accumulator,value|
+      passed_values << value
+      accumulator + 1
+    end.should == 1
+    passed_values.should == [[:a,0]]
+  end
+  
 end
