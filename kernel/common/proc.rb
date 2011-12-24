2156730bff3df489d0c5071251f5bab6a01425e1
Rob Gleeson <rob@flowof.info>
2011-12-24 05:10:25 +0000

    Proc::Method#lambda? returns true in 1.9 mode.

--- kernel/common/proc.rb
+++ kernel/common/proc.rb
@@ -116,13 +116,6 @@ class Proc
   class Method < Proc
     attr_accessor :bound_method
 
-    def self.__from_method__(meth)
-      obj = allocate()
-      obj.bound_method = meth
-
-      return obj
-    end
-
     def __yield__(*args, &block)
       # do a block style unwrap..
       if args.size == 1 and args.first.kind_of? Array
