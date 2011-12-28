07c88507fb3422a205c5f76397dcd55bfa18730d
Dirkjan Bussink <d.bussink@gmail.com>
2011-12-28 21:52:15 +0100

    Remove added trailing whitespace

--- spec/ruby/core/enumerable/inject_spec.rb
+++ spec/ruby/core/enumerable/inject_spec.rb
@@ -3,5 +3,5 @@ require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/inject', __FILE__)
 
 describe "Enumerable#inject" do
-  it_behaves_like :enumerable_inject, :inject  
+  it_behaves_like :enumerable_inject, :inject
 end
