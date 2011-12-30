c3184c670375f7d9b6aeaa38241629db87d075e6
John Firebaugh <john.firebaugh@gmail.com>
2011-12-30 10:13:46 -0800

    Updated MSpec source to 9622c7e8.

---
+++ mspec/spec/matchers/be_nan_spec.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require 'mspec/expectations/expectations'
+require 'mspec/matchers/be_nan'
+
+describe BeNaNMatcher do
+  it "matches when actual is NaN" do
+    BeNaNMatcher.new.matches?(nan_value).should == true
+  end
+
+  it "does not match when actual is not NaN" do
+    BeNaNMatcher.new.matches?(1.0).should == false
+    BeNaNMatcher.new.matches?(0).should == false
+  end
+
+  it "provides a useful failure message" do
+    matcher = BeNaNMatcher.new
+    matcher.matches?(0)
+    matcher.failure_message.should == ["Expected 0", "to be NaN"]
+  end
+
+  it "provides a useful negative failure message" do
+    matcher = BeNaNMatcher.new
+    matcher.matches?(nan_value)
+    matcher.negative_failure_message.should == ["Expected NaN", "not to be NaN"]
+  end
+end
