eb86e5381ce892a7964da535ca7f229f557ad163
Brian Ford <brixen@gmail.com>
2011-12-30 16:01:44 -0800

    Revert "Remove incorrect version guards"
    This reverts commit 4aa0454d3eef39d073af5abe38a42ec73084fcfe.
    
    The guards mean 1.8.7+ and these behaviors are not in 1.8.6.

--- spec/ruby/core/enumerable/reduce_spec.rb
+++ spec/ruby/core/enumerable/reduce_spec.rb
@@ -3,5 +3,7 @@ require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/inject', __FILE__)
 
 describe "Enumerable#reduce" do
-  it_behaves_like :enumerable_inject, :reduce
+  ruby_version_is '1.8.7' do
+    it_behaves_like :enumerable_inject, :reduce
+  end
 end
