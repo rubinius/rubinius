1ab4bc7d744a4c7f8a2f2e791cfdad9eb090bb28
Brian Ford <brixen@gmail.com>
2012-01-08 02:07:04 -0800

    Fixed BEGIN specs.

--- spec/ruby/language/BEGIN_spec.rb
+++ spec/ruby/language/BEGIN_spec.rb
@@ -1,57 +1,49 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 describe "The BEGIN keyword" do
-  ruby_version_is "" .. "1.9" do
-    it "runs in a new isolated scope" do
-      lambda {
-        eval "BEGIN { var_in_begin = 'foo' }; var_in_begin"
-      }.should raise_error NameError
+  before :each do
+    ScratchPad.record []
+  end
 
-      lambda {
+  ruby_version_is "".."1.9" do
+    it "runs in a new isolated scope" do
+      lambda do
+        eval "BEGIN { var_in_begin = 'foo' }; var_in_begin"
+      end.should raise_error NameError
+    end
+
+    it "does not access variables outside the eval scope" do
+      lambda do
         outside_var = 'foo'
         eval "BEGIN { outside_var }"
-      }.should raise_error NameError
+      end.should raise_error NameError
     end
   end
 
   ruby_version_is "1.9" do
     it "runs in a shared scope" do
-      lambda {
-        eval "BEGIN { var_in_begin = 'foo' }; var_in_begin"
-      }.should_not raise_error NameError
+      eval("BEGIN { var_in_begin = 'foo' }; var_in_begin").should == "foo"
+    end
 
-      lambda {
-        outside_var = 'foo'
-        eval "BEGIN { outside_var }"
-      }.should_not raise_error NameError
+    it "accesses variables outside the eval scope" do
+      outside_var = 'foo'
+      eval("BEGIN { var_in_begin = outside_var }; var_in_begin").should == "foo"
     end
 
     it "must appear in a top-level context" do
-      lambda do
-        eval "1.times{ BEGIN{ :begun }}"
-      end.should raise_error(SyntaxError)
+      lambda { eval "1.times { BEGIN { 1 } }" }.should raise_error(SyntaxError)
     end
   end
 
   it "runs first in a given code unit" do
-    o = Object.new
-    class << o
-      ARY = []
-      eval "ARY << 'foo'; BEGIN { ARY << 'bar' }"
-      def ary; ARY; end
-    end
+    eval "ScratchPad << 'foo'; BEGIN { ScratchPad << 'bar' }"
 
-    o.ary.should == ['bar', 'foo']
+    ScratchPad.recorded.should == ['bar', 'foo']
   end
 
   it "runs multiple begins in FIFO order" do
-    o = Object.new
-    class << o
-      ARY = []
-      eval "BEGIN { ARY << 'foo' }; BEGIN { ARY << 'bar' }"
-      def ary; ARY; end
-    end
+    eval "BEGIN { ScratchPad << 'foo' }; BEGIN { ScratchPad << 'bar' }"
 
-    o.ary.should == ['foo', 'bar']
+    ScratchPad.recorded.should == ['foo', 'bar']
   end
 end
