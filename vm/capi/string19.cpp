44e97d6966a912517de5bb6b475cfe925fcd9d31
John Firebaugh <john.firebaugh@gmail.com>
2012-01-14 21:14:27 -0800

    Implement rb_external_str_new_with_enc
    Reimplement rb_external_str_new and rb_external_str_new_cstr
    in terms of this function. This introduces some spec failures
    due to a latent bug in rb_to_encoding which I will spec in a
    subsequent commit.

--- vm/capi/string19.cpp
+++ vm/capi/string19.cpp
@@ -32,13 +32,13 @@ extern "C" {
     return rb_enc_str_new(ptr, strlen(ptr), rb_usascii_encoding());
   }
 
-  VALUE rb_external_str_new(const char* string, long size) {
+  VALUE rb_external_str_new_with_enc(const char* string, long size, rb_encoding* encoding) {
     NativeMethodEnvironment* env = NativeMethodEnvironment::get();
 
     String* str = String::create(env->state(), string, size);
     str->taint(env->state());
 
-    Encoding* enc = Encoding::default_external(env->state());
+    Encoding* enc = Encoding::find(env->state(), encoding->name);
     if(enc == Encoding::usascii_encoding(env->state())
        && !CBOOL(str->ascii_only_p(env->state()))) {
       str->encoding(env->state(), Encoding::ascii8bit_encoding(env->state()));
@@ -50,8 +50,12 @@ extern "C" {
     return env->get_handle(str);
   }
 
+  VALUE rb_external_str_new(const char* string, long size) {
+    return rb_external_str_new_with_enc(string, size, rb_default_external_encoding());
+  }
+
   VALUE rb_external_str_new_cstr(const char* string) {
-    return rb_external_str_new(string, strlen(string));
+    return rb_external_str_new_with_enc(string, strlen(string), rb_default_external_encoding());
   }
 
   int rb_enc_str_coderange(VALUE string) {
