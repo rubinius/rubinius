74cb4d6d53493f642e5824d3837ad40426150eb6
Jari Bakken <jari.bakken@gmail.com>
2012-01-18 18:40:36 +0100

    Add FFI::Pointer#address spec for large values.

--- spec/core/ffi/pointer_spec.rb
+++ spec/core/ffi/pointer_spec.rb
@@ -4,4 +4,16 @@ describe "FFI::Pointer#size" do
   it "matches 1.size" do
     FFI::Pointer.size.should == 1.size
   end
+
+  platform_is :wordsize => 32 do
+    it "returns its address for large values" do
+      FFI::Pointer.new(0xdeadbeef).address.should == 0xdeadbeef
+    end
+  end
+
+  platform_is :wordsize => 64 do
+    it "returns its address for large values" do
+      FFI::Pointer.new(0x8000000000000000).address.should == 0x8000000000000000
+    end
+  end
 end
