a20d321d845334e90c90f715283d450c12a65cc0
Dirkjan Bussink <d.bussink@gmail.com>
2012-01-22 17:18:10 +0100

    Add version specific lib path later
    Fixes #1525

--- kernel/loader.rb
+++ kernel/loader.rb
@@ -102,11 +102,11 @@ containing the Rubinius standard library files.
       # This conforms more closely to MRI. It is necessary to support
       # paths that mkmf adds when compiling and installing native exts.
       additions = []
-      additions << "#{@main_lib}/#{Rubinius::RUBY_LIB_VERSION}"
       additions << Rubinius::SITE_PATH
       additions << "#{Rubinius::SITE_PATH}/#{Rubinius::CPU}-#{Rubinius::OS}"
       additions << Rubinius::VENDOR_PATH
       additions << "#{Rubinius::VENDOR_PATH}/#{Rubinius::CPU}-#{Rubinius::OS}"
+      additions << "#{@main_lib}/#{Rubinius::RUBY_LIB_VERSION}"
       additions << @main_lib
       additions.uniq!
 
