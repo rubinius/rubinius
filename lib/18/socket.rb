1c51586d9e06d880b29a7fdfe42147459494c196
Evan Phoenix <evan@phx.io>
2012-01-31 09:11:27 -0800

    Quick hack: accept_nonblock2 returns nil on EAGAIN

--- lib/18/socket.rb
+++ lib/18/socket.rb
@@ -478,6 +478,22 @@ class Socket < BasicSocket
       socket
     end
 
+    def accept_nonblock2
+      return if closed?
+
+      fcntl(Fcntl::F_SETFL, Fcntl::O_NONBLOCK)
+
+      fd = Socket::Foreign.accept descriptor, nil, nil
+      if fd < 0
+        return nil if FFI::Platform::POSIX.errno == Errno::EAGAIN::Errno
+        Errno.handle 'accept(2)'
+      end
+
+      # TCPServer -> TCPSocket etc. *sigh*
+      socket = self.class.superclass.allocate
+      IO.setup socket, fd, nil, true
+      socket
+    end
   end
 
   include Socket::ListenAndAccept
