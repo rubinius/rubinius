c1061d7ccf09e768a20a5ea753abeda80e157de9
Daniel Vartanov <dan@vartanov.net>
2012-02-19 13:30:08 +0600

    Fix for FFI::Struct.layout; Fails with exception if module name is nil

--- spec/core/ffi/struct/layout_spec.rb
+++ spec/core/ffi/struct/layout_spec.rb
@@ -1,5 +1,15 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 
 describe "FFI::Struct.layout" do
+  context "when class name is not set" do
+    it "should not raise a error" do
+      lambda {
+        klass = Class.new(FFI::Struct)
+        klass.layout :any, :int
+      }.should_not raise_error
+    end
+  end
+  
+  
   it "needs to be reviewed for spec completeness"
 end
