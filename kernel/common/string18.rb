edf3e28720753c2da937d70ab713d373a8c72d23
Michael Macias <mamacias@olemiss.edu>
2012-03-09 00:21:40 -0600

    No need for language-specific implementations of String#hex since the check is in the primitive

--- kernel/common/string18.rb
+++ kernel/common/string18.rb
@@ -5,18 +5,6 @@ class String
 
   alias_method :bytesize, :size
 
-  # Treats leading characters from <i>self</i> as a string of hexadecimal digits
-  # (with an optional sign and an optional <code>0x</code>) and returns the
-  # corresponding number. Zero is returned on error.
-  #
-  #    "0x0a".hex     #=> 10
-  #    "-1234".hex    #=> -4660
-  #    "0".hex        #=> 0
-  #    "wombat".hex   #=> 0
-  def hex
-    to_inum(16, false)
-  end
-
   def upto(stop, exclusive=false)
     stop = StringValue(stop)
     return self if self > stop
