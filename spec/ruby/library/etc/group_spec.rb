9fca1afc7d1b1944f1a1d1415c94ff296faaf7aa
Dirkjan Bussink <d.bussink@gmail.com>
2012-03-09 16:04:53 +0100

    Add spec for parallel group iteration
    Spec is shamelessly taken from #1149 by @cwgem. This way it's ensured
    it is run and don't have to wait for a Rubyspec pull request.

--- spec/ruby/library/etc/group_spec.rb
+++ spec/ruby/library/etc/group_spec.rb
@@ -4,4 +4,14 @@ require 'etc'
 
 describe "Etc.group" do
   it_behaves_like(:etc_on_windows, :group)
+
+  it "raises a RuntimeError for parallel iteration" do
+    proc {
+      Etc.group do | group |
+        Etc.group do | group2 |
+        end
+      end
+    }.should raise_error(RuntimeError)
+  end
+
 end
