9ed44c7a9f2e2f66586ff71033fd707b39ab9e59
John Firebaugh <john.firebaugh@gmail.com>
2012-03-09 20:50:25 -0800

    Make taint/trust/freeze specs consistent

--- spec/ruby/core/kernel/taint_spec.rb
+++ spec/ruby/core/kernel/taint_spec.rb
@@ -2,17 +2,40 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 
 describe "Kernel#taint" do
-  it "sets self to be tainted" do
-    mock('tainted?').taint.tainted?.should == true
+  it "returns self" do
+    o = Object.new
+    o.taint.should equal(o)
   end
 
-  it "returns the immediate when called on an immediate" do
-    imediate_values = [nil, true, false, 1, :sym]
-    imediate_values.each{ |v| v.taint.should == v }
+  it "sets the tainted bit" do
+    o = Object.new
+    o.taint
+    o.tainted?.should == true
+  end
+
+  ruby_version_is ""..."1.9" do
+    it "raises TypeError on an untainted, frozen object" do
+      o = Object.new.freeze
+      lambda { o.taint }.should raise_error(TypeError)
+    end
+  end
+
+  ruby_version_is "1.9" do
+    it "raises RuntimeError on an untainted, frozen object" do
+      o = Object.new.freeze
+      lambda { o.taint }.should raise_error(RuntimeError)
+    end
+  end
+
+  it "does not raise an error on a tainted, frozen object" do
+    o = Object.new.taint.freeze
+    o.taint.should equal(o)
   end
 
   it "has no effect on immediate values" do
-    imediate_values = [nil, true, false, 1, :sym]
-    imediate_values.each{ |v| v.taint; v.tainted?.should be_false}
+    [nil, true, false, 1, :sym].each do |v|
+      v.taint
+      v.tainted?.should == false
+    end
   end
 end
