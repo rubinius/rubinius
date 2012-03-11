cc71351810d0587c2e2aaab1b1be8a44e533a29e
Ian Leitch <port001@gmail.com>
2012-03-11 19:13:55 +0000

    Move some Method#eql? logic into Rubinius::MethodEquality

--- kernel/common/method19.rb
+++ kernel/common/method19.rb
@@ -10,30 +10,13 @@ class Method
   # same object.
 
   def ==(other)
-    other.class == Method and
-      @receiver.equal?(other.receiver) and
-      (@executable == other.executable or
-       equal_to_delegated_method_receiver?(other))
+    other.class == Method &&
+      @receiver.equal?(other.receiver) &&
+      (@executable == other.executable ||
+       Rubinius::MethodEquality.method_equal_to_delegated_method_receiver?(self, other))
   end
 
   alias_method :eql?, :==
-
-  protected
-
-  def equal_to_delegated_method_receiver?(other)
-    self_receiver  = delegated_receiver_for(self)  || unbind
-    other_receiver = delegated_receiver_for(other) || other.unbind
-    self_or_other_is_delegated?(other) && (self_receiver == other_receiver)
-  end
-
-  def self_or_other_is_delegated?(other)
-    (@executable.kind_of?(Rubinius::CompiledMethod) && other.executable.kind_of?(Rubinius::DelegatedMethod)) ||
-    (@executable.kind_of?(Rubinius::DelegatedMethod) && other.executable.kind_of?(Rubinius::CompiledMethod))
-  end
-
-  def delegated_receiver_for(meth)
-    meth.executable.receiver if meth.executable.kind_of?(Rubinius::DelegatedMethod)
-  end
 end
 
 class UnboundMethod
---
+++ kernel/common/method_equality.rb
@@ -0,0 +1,18 @@
+module Rubinius
+  module MethodEquality
+    def self.method_equal_to_delegated_method_receiver?(meth, other)
+      self_receiver  = delegated_receiver_for(meth)  || meth.unbind
+      other_receiver = delegated_receiver_for(other) || other.unbind
+      either_method_is_delegated?(meth, other) && (self_receiver == other_receiver)
+    end
+
+    def self.either_method_is_delegated?(meth, other)
+      (meth.executable.kind_of?(Rubinius::CompiledMethod) && other.executable.kind_of?(Rubinius::DelegatedMethod)) ||
+      (meth.executable.kind_of?(Rubinius::DelegatedMethod) && other.executable.kind_of?(Rubinius::CompiledMethod))
+    end
+
+    def self.delegated_receiver_for(meth)
+      meth.executable.receiver if meth.executable.kind_of?(Rubinius::DelegatedMethod)
+    end
+  end
+end
