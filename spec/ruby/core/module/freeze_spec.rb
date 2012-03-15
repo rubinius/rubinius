77944f3b8d5075c3dd779b6eec78b9efcef8f9b5
John Firebaugh <john.firebaugh@gmail.com>
2012-03-15 14:40:26 -0700

    Move Module#freeze specs
    These are actually testing `def` for a singleton class.

--- spec/ruby/core/module/freeze_spec.rb
+++ spec/ruby/core/module/freeze_spec.rb
@@ -2,39 +2,5 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 
 describe "Module#freeze" do
-  ruby_version_is ""..."1.9" do
-    it "prevents further modifications to self" do
-      m = Module.new.freeze
-      m.frozen?.should == true
-
-      # Does not raise
-      class << m; end
-
-      lambda {
-        class << m
-          def test() "test" end
-        end
-      }.should raise_error(TypeError)
-
-      lambda { def m.test() "test" end }.should raise_error(TypeError)
-    end
-  end
-
-  ruby_version_is "1.9" do
-    it "prevents further modifications to self" do
-      m = Module.new.freeze
-      m.frozen?.should == true
-
-      # Does not raise
-      class << m; end
-
-      lambda {
-        class << m
-          def test() "test" end
-        end
-      }.should raise_error(RuntimeError)
-
-      lambda { def m.test() "test" end }.should raise_error(RuntimeError)
-    end
-  end
+  it "needs to be reviewed for spec completeness"
 end
