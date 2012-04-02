cc73302b462c7d92c6d0bbf915dcd65a3f7524ea
John Firebaugh <john.firebaugh@gmail.com>
2012-04-02 14:55:17 -0700

    Clean up and expand Process.exec specs

--- spec/ruby/shared/process/exec.rb
+++ spec/ruby/shared/process/exec.rb
@@ -1,35 +1,52 @@
 describe :process_exec, :shared => true do
-  it "raises a SystemCallError if cmd cannot execute" do
-    lambda { @object.exec "" }.should raise_error(SystemCallError)
+  it "raises Errno::ENOENT for an empty string" do
+    lambda { @object.exec "" }.should raise_error(Errno::ENOENT)
   end
 
-  it "raises a SystemCallError if cmd cannot execute and contains '-'" do
-    lambda { @object.exec 'cmd-plugin' }.should raise_error(SystemCallError)
-  end
-
-  it "raises an ArgumentError if the cmd includes a null byte" do
-    lambda { @object.exec "\000" }.should raise_error(ArgumentError)
-  end
-
-  it "raises Errno::ENOENT if the script does not exist" do
+  it "raises Errno::ENOENT for a command which does not exist" do
     lambda { @object.exec "bogus-noent-script.sh" }.should raise_error(Errno::ENOENT)
   end
 
-  it "runs the specified command, replacing current process" do
-    ruby_exe('exec "echo hello"; puts "fail"', :escape => true).should == "hello\n"
-  end
-
-  ruby_version_is "1.9.2" do
-    it "passes environment vars to the child environment" do
-      ruby_exe('exec({"FOO" => "BAR"}, "echo $FOO")', :escape => true).should == "BAR\n"
-    end
+  it "raises an ArgumentError if the command includes a null byte" do
+    lambda { @object.exec "\000" }.should raise_error(ArgumentError)
   end
 
   it "raises Errno::EACCES when the file does not have execute permissions" do
     lambda { @object.exec __FILE__ }.should raise_error(Errno::EACCES)
   end
 
-  it "raises Errno::ACCES when passed a directory" do
+  it "raises Errno::EACCES when passed a directory" do
     lambda { @object.exec File.dirname(__FILE__) }.should raise_error(Errno::EACCES)
+  end
+
+  it "runs the specified command, replacing current process" do
+    ruby_exe('exec "echo hello"; puts "fail"', :escape => true).should == "hello\n"
+  end
+
+  describe "with a single argument" do
+    before(:each) do
+      @path = tmp("tmp")
+      touch @path
+    end
+
+    after(:each) do
+      rm_r @path
+    end
+
+    it "subjects the specified command to shell expansion" do
+      ruby_exe('exec "echo *"', :escape => true, :dir => tmp("")).should == "#{File.basename(@path)}\n"
+    end
+  end
+
+  describe "with multiple arguments" do
+    it "does not subject the arguments to shell expansion" do
+      ruby_exe('exec "echo", "*"', :escape => true).should == "*\n"
+    end
+  end
+
+  ruby_version_is "1.9.2" do
+    it "passes environment vars to the child environment" do
+      ruby_exe('exec({"FOO" => "BAR"}, "echo $FOO")', :escape => true).should == "BAR\n"
+    end
   end
 end
