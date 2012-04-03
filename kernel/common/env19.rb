e2ea4558bf183d7b93bbaf0f8e2b8406d91db59e
John Firebaugh <john.firebaugh@gmail.com>
2012-04-03 11:05:05 -0700

    Implement ENV.keep_if

--- kernel/common/env19.rb
+++ kernel/common/env19.rb
@@ -1,5 +1,11 @@
 module Rubinius
   class EnvironmentVariables
+    def keep_if(&block)
+      return to_enum(:keep_if) unless block_given?
+      reject! {|k, v| !block.call(k, v) }
+      self
+    end
+
     def set_encoding(value)
       return unless value.kind_of? String
       value.force_encoding Encoding.find("locale")
