09f24321e79f720d6b8685805f7906b42f807732
John Firebaugh <john.firebaugh@gmail.com>
2012-04-03 15:10:39 -0700

    Fix Process.exec spec failures

--- kernel/common/process19.rb
+++ kernel/common/process19.rb
@@ -32,10 +32,11 @@ module Process
 
     if args.empty? and cmd.kind_of? String
       raise Errno::ENOENT if cmd.empty?
-      if /([*?{}\[\]<>()~&|$;'`"\n\s]|[^\w-])/o.match(cmd)
+      if /([*?{}\[\]<>()~&|$;'`"\n])/o.match(cmd)
         Process.perform_exec "/bin/sh", ["sh", "-c", cmd]
       else
-        Process.perform_exec cmd, [cmd]
+        args = cmd.split(' ')
+        Process.perform_exec args.first, args
       end
     else
       if cmd.kind_of? Array
