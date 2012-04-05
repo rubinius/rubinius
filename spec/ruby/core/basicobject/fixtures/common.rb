adf96700d238d0f2ee6b5deb275683123f0d5e96
Michal Bugno <michal.bugno@gmail.com>
2012-04-05 10:56:04 +0200

    BasicObject fixtures should not load in 1.8 Fix to :! parser error in BasicObject specs

--- spec/ruby/core/basicobject/fixtures/common.rb
+++ spec/ruby/core/basicobject/fixtures/common.rb
@@ -1,9 +1,13 @@
-module BasicObjectSpecs
-  class BOSubclass < BasicObject
-    def self.kernel_defined?
-      defined?(Kernel)
-    end
+ruby_version_is "1.9" do
 
-    include ::Kernel
+  module BasicObjectSpecs
+    class BOSubclass < BasicObject
+      def self.kernel_defined?
+        defined?(Kernel)
+      end
+
+      include ::Kernel
+    end
   end
+
 end
