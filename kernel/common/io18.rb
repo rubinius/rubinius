a451532929079faabb4161896c3fabf2b0fa2b86
Michal Bugno <michal.bugno@gmail.com>
2012-04-08 15:44:07 +0200

    IO#each_char in 1.9 handles utf-8

--- kernel/common/io18.rb
+++ kernel/common/io18.rb
@@ -153,6 +153,38 @@ class IO
 
   alias_method :each_line, :each
 
+  def each_char
+    return to_enum(:each_char) unless block_given?
+
+    ensure_open_and_readable
+    if Rubinius.kcode == :UTF8
+      # TODO zoinks. This is the slowest way possible to do this.
+      # We'll have to rewrite it.
+      lookup = 7.downto(4)
+      while c = read(1) do
+        n = c[0]
+        leftmost_zero_bit = lookup.find { |i| n[i] == 0 }
+
+        case leftmost_zero_bit
+        when 7 # ASCII
+          yield c
+        when 6 # UTF 8 complementary characters
+          next # Encoding error, ignore
+        else
+          more = read(6 - leftmost_zero_bit)
+          break unless more
+          yield c + more
+        end
+      end
+    else
+      while s = read(1)
+        yield s
+      end
+    end
+
+    self
+  end
+
   ##
   # Reads the next ``line’’ from the I/O stream;
   # lines are separated by sep_string. A separator
