5010f16fa55a136996f49a6a05a01e4442c61ffe
John Firebaugh <john.firebaugh@gmail.com>
2012-04-09 16:04:54 -0700

    Clean up Kernel.system specs

--- spec/ruby/core/kernel/fixtures/classes.rb
+++ spec/ruby/core/kernel/fixtures/classes.rb
@@ -18,7 +18,7 @@ module KernelSpecs
   end
 
   class Method
-    public :abort, :exec, :exit, :exit!, :fork
+    public :abort, :exec, :exit, :exit!, :fork, :system
     public :spawn if respond_to?(:spawn, true)
   end
 
@@ -237,10 +237,6 @@ module KernelSpecs
     def self.call_yield
       yield
     end
-  end
-
-  def self.helper_script
-    File.dirname(__FILE__) + '/check_expansion.rb'
   end
 
   module DuplicateM
