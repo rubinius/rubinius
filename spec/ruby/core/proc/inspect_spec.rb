592ad015e9af750158947126ef5d549078b18f91
John Firebaugh <john.firebaugh@gmail.com>
2012-04-17 08:51:59 -0700

    Spec Proc#inspect
    Due to MRI internals, it acts as as alias for #to_s, though
    it is not defined that way directly. (It may be a true alias
    in 2.0; see https://bugs.ruby-lang.org/issues/6130.)

--- spec/ruby/core/proc/inspect_spec.rb
+++ spec/ruby/core/proc/inspect_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/to_s', __FILE__)
 
-describe "Proc#to_s" do
-  it_behaves_like :proc_to_s, :to_s
+describe "Proc#inspect" do
+  it_behaves_like :proc_to_s, :inspect
 end
