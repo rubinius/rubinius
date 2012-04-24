ecf8e5f6d3058883a1079e1a80020776794e6bd0
r-stu31 <r.stu3.1@googlemail.com>
2012-04-25 01:21:51 +0200

    Add specs for Symbol#length and Symbol#size on 1.9.

--- spec/ruby/core/symbol/size_spec.rb
+++ spec/ruby/core/symbol/size_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is "1.9" do
   describe "Symbol#size" do
-    it "needs to be reviewed for spec completeness"
+    it_behaves_like(:symbol_length, :size)
   end
 end
