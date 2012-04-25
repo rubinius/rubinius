e58c7a471f081467ed70c5bdb3f4c178290202e1
r-stu31 <r.stu3.1@googlemail.com>
2012-04-25 14:31:28 +0200

    Split into more cases with appropriate descriptions.

--- spec/ruby/core/symbol/shared/length.rb
+++ spec/ruby/core/symbol/shared/length.rb
@@ -1,11 +1,21 @@
 describe :symbol_length, :shared => true do
-  it "returns the length of self" do
-    :"".send(@method).should == 0
+  it "returns 0 for empty name" do
+    :''.send(@method).should == 0
+  end
+
+  it "returns 1 for name formed by a NUL character" do
     :"\x00".send(@method).should == 1
+  end
+
+  it "returns 3 for name formed by 3 ASCII characters" do
     :one.send(@method).should == 3
-    :two.send(@method).should == 3
-    :three.send(@method).should == 5
+  end
+
+  it "returns 4 for name formed by 4 ASCII characters" do
     :four.send(@method).should == 4
+  end
+
+  it "returns 4 for name formed by 1 multibyte and 3 ASCII characters" do
     :"\xC3\x9Cber".send(@method).should == 4
   end
 end
