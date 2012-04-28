caa57c20f574f24c9edb2a8a37a3645c15697477
Ian Leitch <port001@gmail.com>
2012-04-27 17:20:15 -0700

    Revert "Revert "String#inspect prints unprintable chars as hex in 1.9""
    This reverts commit 832ba3076b3e9037f0d78b3b5835f92cab85992a.

--- kernel/delta/ctype.rb
+++ kernel/delta/ctype.rb
@@ -18,11 +18,7 @@ module Rubinius::CType
     when 92; '\\\\'
     else
       if num < 32 || num > 126
-        str = "\\000"
-        str.modify!
-
-        c = num.to_s 8
-        str.copy_from c, 0, c.size, 4-c.size
+        unprintable_chr(num)
       else
         num.chr
       end
