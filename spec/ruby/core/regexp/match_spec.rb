aa673566c10397b6dd35ee8946515dfe9d3c4620
John Firebaugh <john.firebaugh@gmail.com>
2012-05-01 16:35:18 -0700

    Consolidate describes

--- spec/ruby/core/regexp/match_spec.rb
+++ spec/ruby/core/regexp/match_spec.rb
@@ -12,10 +12,27 @@ end
 
 describe "Regexp#=~" do
   it_behaves_like(:regexp_match, :=~)
+
+  it "returns the index of the first character of the matching region" do
+    (/(.)(.)(.)/ =~ "abc").should == 0
+  end
 end
 
 describe "Regexp#match" do
   it_behaves_like(:regexp_match, :match)
+
+  it "returns a MatchData object" do
+    /(.)(.)(.)/.match("abc").should be_kind_of(MatchData)
+  end
+
+  it "resets $~ if passed nil" do
+    # set $~
+    /./.match("a")
+    $~.should be_kind_of(MatchData)
+
+    /1/.match(nil)
+    $~.should be_nil
+  end
 
   it "raises TypeError when the given argument cannot be coarce to String" do
     f = 1
@@ -41,26 +58,5 @@ describe "Regexp#~" do
   it "matches against the contents of $_" do
     $_ = "input data"
     (~ /at/).should == 7
-  end
-end
-
-describe "Regexp#=~ on a successful match" do
-  it "returns the index of the first character of the matching region" do
-    (/(.)(.)(.)/ =~ "abc").should == 0
-  end
-end
-
-describe "Regexp#match on a successful match" do
-  it "returns a MatchData object" do
-    /(.)(.)(.)/.match("abc").should be_kind_of(MatchData)
-  end
-
-  it "resets $~ if passed nil" do
-    # set $~
-    /./.match("a")
-    $~.should be_kind_of(MatchData)
-
-    /1/.match(nil)
-    $~.should be_nil
   end
 end
