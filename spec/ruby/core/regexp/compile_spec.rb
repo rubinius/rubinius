0f18149add0560df0f60df179886bec4eab8507e
John Firebaugh <john.firebaugh@gmail.com>
2012-05-08 12:36:40 -0700

    Fix style and description errors in specs

--- spec/ruby/core/regexp/compile_spec.rb
+++ spec/ruby/core/regexp/compile_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/new', __FILE__)
 
-describe "Range.compile" do
+describe "Regexp.compile" do
   it_behaves_like :regexp_new, :compile
 end
 
--- spec/ruby/core/regexp/quote_spec.rb
+++ spec/ruby/core/regexp/quote_spec.rb
@@ -2,5 +2,5 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../shared/quote', __FILE__)
 
 describe "Regexp.quote" do
-  it_behaves_like(:regexp_quote, :quote)
+  it_behaves_like :regexp_quote, :quote
 end
