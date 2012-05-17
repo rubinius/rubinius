dbaaf563b8d26955a92862a478bcfe025d4e738d
Ian Leitch <port001@gmail.com>
2012-05-16 17:06:43 -0700

    Improve spec coverage of IO#advise.

--- spec/ruby/core/io/advise_spec.rb
+++ spec/ruby/core/io/advise_spec.rb
@@ -14,12 +14,34 @@ ruby_version_is '1.9.3' do
       $KCODE = @kcode
     end
 
-    it "needs to be reviewed for spec completeness"
-
     it "raises a TypeError if advise is not a Symbol" do
-      lambda{
+      lambda {
         @io.advise("normal")
       }.should raise_error(TypeError)
+    end
+
+    it "raises a TypeError if offsert cannot be coerced to an Integer" do
+      lambda {
+        @io.advise(:normal, "wat")
+      }.should raise_error(TypeError)
+    end
+
+    it "raises a TypeError if len cannot be coerced to an Integer" do
+      lambda {
+        @io.advise(:normal, 0, "wat")
+      }.should raise_error(TypeError)
+    end
+
+    it "raises a RangeError if offset is too big" do
+      lambda {
+        @io.advise(:normal, 10 ** 32)
+      }.should raise_error(RangeError)
+    end
+
+    it "raises a RangeError if len is too big" do
+      lambda {
+        @io.advise(:normal, 0, 10 ** 32)
+      }.should raise_error(RangeError)
     end
 
     it "raises a NotImplementedError if advise is not recognized" do
@@ -28,8 +50,33 @@ ruby_version_is '1.9.3' do
       }.should raise_error(NotImplementedError)
     end
 
-    it "returns nil when invoked with correct arguments" do
-      @io.advise(:normal).should == nil
+    it "supports the normal advice type" do
+      @io.advise(:normal).should be_nil
+    end
+
+    it "supports the sequential advice type" do
+      @io.advise(:sequential).should be_nil
+    end
+
+    it "supports the random advice type" do
+      @io.advise(:random).should be_nil
+    end
+
+    it "supports the dontneed advice type" do
+      @io.advise(:dontneed).should be_nil
+    end
+
+    it "supports the noreuse advice type" do
+      @io.advise(:noreuse).should be_nil
+    end
+
+    it "supports the willneed advice type" do
+      @io.advise(:willneed).should be_nil
+    end
+
+    it "raises an IOError if the stream is closed" do
+      @io.close
+      lambda { @io.advise(:normal) }.should raise_error(IOError)
     end
   end
 end
