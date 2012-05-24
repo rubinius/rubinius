865ff3168c140b0d53cc55812dc007c41163a3be
Ryo Onodera <ryoqun@gmail.com>
2012-05-25 01:01:08 +0900

    Add a spec for String#to_ast on 1.9

--- spec/core/string/to_ast_spec.rb
+++ spec/core/string/to_ast_spec.rb
@@ -8,4 +8,10 @@ describe "String#to_ast" do
   it "raises SyntaxError with invalid Ruby code " do
     lambda { "{".to_ast }.should raise_error(SyntaxError)
   end
+
+  ruby_version_is "1.9" do
+    it "converts to AST nodes from Ruby 1.9 code" do
+      "->{}".to_ast.should be_kind_of(Rubinius::AST::Send)
+    end
+  end
 end
