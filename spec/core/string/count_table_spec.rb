2f7500f0c984bc367265aa9e8a85a07b73ee1a9d
Dirkjan Bussink <d.bussink@gmail.com>
2012-05-26 19:09:54 +0200

    Fix String#count_table specs for 1.8 / 1.9 mode difference

--- spec/core/string/count_table_spec.rb
+++ spec/core/string/count_table_spec.rb
@@ -20,11 +20,19 @@ describe "String#count_table" do
     256.times { |i| t.getbyte(i).should == ("-|*\030 abcde".include?(i.chr) ? 1 : 0) }
   end
 
-  it "returns a String with entries corresponding to sequences set to 1" do
-    t = "".count_table "-ab-g\\d-abhi--A\\"
-    256.times { |i|
-      t.getbyte(i).should == ("-abcdefgbhA\\".include?(i.chr) ? 1 : 0)
-    }
+  ruby_version_is "" ... "1.9" do
+    it "returns a String with entries corresponding to sequences set to 1 ignoring an invalid range" do
+      t = "".count_table "-ab-g\\d-abhi--A\\"
+      256.times { |i|
+        t.getbyte(i).should == ("-abcdefgbhA\\".include?(i.chr) ? 1 : 0)
+      }
+    end
+  end
+
+  ruby_version_is "1.9" do
+    it "raises an exception for input provided with invalid range" do
+      lambda { "".count_table "-ab-g\\d-abhi--A\\" }.should raise_error(ArgumentError)
+    end
   end
 
   it "returns a String with entries corresponding included and excluded characters" do
@@ -33,9 +41,24 @@ describe "String#count_table" do
   end
 
   it "returns a String with entries set to false for '^abc-e' inverse match strings" do
-    t = "".count_table "^ab-g\\d-abhi--A\\"
+    t = "".count_table "^abc-e"
     256.times { |i|
-      t.getbyte(i).should == ("abcdefgbhA\\".include?(i.chr) ? 0 : 1)
+      t.getbyte(i).should == ("abcde".include?(i.chr) ? 0 : 1)
     }
+  end
+
+  ruby_version_is "" ... "1.9" do
+    it "returns a String with entries set to false for '^abc-e' inverse match strings ignoring the invalid range" do
+      t = "".count_table "^ab-g\\d-abhi--A\\"
+      256.times { |i|
+        t.getbyte(i).should == ("abcdefgbhA\\".include?(i.chr) ? 0 : 1)
+      }
+    end
+  end
+
+  ruby_version_is "1.9" do
+    it "raises an exception for '^abc-e' inverse match strings with an invalid range" do
+      lambda { "".count_table "^ab-g\\d-abhi--A\\" }.should raise_error(ArgumentError)
+    end
   end
 end
