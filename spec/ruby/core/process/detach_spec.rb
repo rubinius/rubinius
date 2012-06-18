5bb60b65a9b0b8d3fa911b0c12dbced6a3634f1a
Dirkjan Bussink <d.bussink@gmail.com>
2012-06-18 20:40:50 +0200

    Make sure we join the returned thread before continuing

--- spec/ruby/core/process/detach_spec.rb
+++ spec/ruby/core/process/detach_spec.rb
@@ -5,12 +5,17 @@ describe "Process.detach" do
   platform_is_not :windows do
     it "returns a thread" do
       pid = Process.fork { Process.exit! }
-      Process.detach(pid).should be_kind_of(Thread)
+      thr = Process.detach(pid)
+      thr.should be_kind_of(Thread)
+      timeout(3) { thr.join }
     end
 
     it "produces the exit Process::Status as the thread value" do
       pid = Process.fork { Process.exit! }
-      status = Process.detach(pid).value
+      thr  = Process.detach(pid)
+      timeout(3) { thr.join }
+
+      status = thr.value
       status.should be_kind_of(Process::Status)
       status.pid.should == pid
     end
@@ -18,8 +23,8 @@ describe "Process.detach" do
     platform_is_not :openbsd do
       it "reaps the child process's status automatically" do
         pid = Process.fork { Process.exit! }
-        th = Process.detach(pid)
-        timeout(3) { th.join }
+        thr = Process.detach(pid)
+        timeout(3) { thr.join }
         lambda { Process.waitpid(pid) }.should raise_error(Errno::ECHILD)
       end
     end
@@ -27,12 +32,18 @@ describe "Process.detach" do
     ruby_version_is "1.9" do
       it "sets the :pid thread-local to the PID" do
         pid = Process.fork { Process.exit! }
-        Process.detach(pid)[:pid].should == pid
+        thr = Process.detach(pid)
+        timeout(3) { thr.join }
+
+        thr[:pid].should == pid
       end
 
       it "provides a #pid method on the returned thread which returns the PID" do
         pid = Process.fork { Process.exit! }
-        Process.detach(pid).pid.should == pid
+        thr = Process.detach(pid)
+        timeout(3) { thr.join }
+
+        thr.pid.should == pid
       end
     end
   end
