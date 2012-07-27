452a4bfe96124e4e47f58e6b183ba98369cde59e
Dirkjan Bussink <d.bussink@gmail.com>
2012-07-27 13:36:35 +0200

    Fix handling of String#find_character for encoded strings

--- vm/builtin/string.hpp
+++ vm/builtin/string.hpp
@@ -228,6 +228,7 @@ namespace rubinius {
     String* byte_substring(STATE, native_int index, native_int length);
     String* char_substring(STATE, native_int index, native_int length);
 
+    OnigEncodingType* get_encoding_kcode_fallback(STATE);
     native_int find_character_byte_index(STATE, native_int index, native_int start = 0);
 
     // Rubinius.primitive :string_index
