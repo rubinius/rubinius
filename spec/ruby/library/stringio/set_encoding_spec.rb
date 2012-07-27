e9a4da17834cf43e8b1ecb7f9d3c3dcd2e7b3f44
John Firebaugh <john.firebaugh@gmail.com>
2012-07-27 15:32:45 -0700

    Rename files and fix specs so they actually run

--- spec/ruby/library/stringio/set_encoding_spec.rb
+++ spec/ruby/library/stringio/set_encoding_spec.rb
@@ -5,8 +5,8 @@ ruby_version_is "1.9.2" do
   describe "StringIO#set_encoding" do
     it "sets the encoding of the underlying String" do
       io = StringIO.new
-      io.set_encoding Encoding::UTF8
-      io.string.encoding.should == Encoding::UTF8
+      io.set_encoding Encoding::UTF_8
+      io.string.encoding.should == Encoding::UTF_8
     end
   end
 end
