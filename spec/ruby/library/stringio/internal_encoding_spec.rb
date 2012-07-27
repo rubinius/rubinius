f2da52119456b893a8285af9f95b283abf4f1455
John Firebaugh <john.firebaugh@gmail.com>
2012-07-27 15:32:45 -0700

    Rename files and fix specs so they actually run

--- spec/ruby/library/stringio/internal_encoding_spec.rb
+++ spec/ruby/library/stringio/internal_encoding_spec.rb
@@ -5,7 +5,7 @@ ruby_version_is "1.9.2" do
   describe "StringIO#internal_encoding" do
     it "returns nil" do
       io = StringIO.new
-      io.set_encoding Encoding::UTF8
+      io.set_encoding Encoding::UTF_8
       io.internal_encoding.should == nil
     end
   end
