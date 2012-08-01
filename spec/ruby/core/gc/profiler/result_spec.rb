852c1cd9767ddaef9944a0f05df27cba72a1b78f
Dirkjan Bussink <d.bussink@gmail.com>
2012-08-01 23:56:15 +0200

    Add some basic specs for additional GC stuff in 1.9

--- spec/ruby/core/gc/profiler/result_spec.rb
+++ spec/ruby/core/gc/profiler/result_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../../../spec_helper', __FILE__)
 
 ruby_version_is "1.9" do
   describe "GC::Profiler.result" do
-    it "needs to be reviewed for spec completeness"
+    it "returns a string" do
+      GC::Profiler.result.should be_kind_of(String)
+    end
   end
 end
--- spec/ruby/core/gc/profiler/total_time_spec.rb
+++ spec/ruby/core/gc/profiler/total_time_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../../../spec_helper', __FILE__)
 
 ruby_version_is "1.9" do
   describe "GC::Profiler.total_time" do
-    it "needs to be reviewed for spec completeness"
+    it "returns an float" do
+      GC::Profiler.total_time.should be_kind_of(Float)
+    end
   end
 end
