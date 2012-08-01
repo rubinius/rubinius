fc7e4616f79a5fec0aedf8f028eabe15bfd3533f
Dirkjan Bussink <d.bussink@gmail.com>
2012-08-01 23:56:15 +0200

    Add some basic specs for additional GC stuff in 1.9

--- spec/ruby/core/gc/count_spec.rb
+++ spec/ruby/core/gc/count_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is "1.9" do
   describe "GC.count" do
-    it "needs to be reviewed for spec completeness"
+    it "returns an integer" do
+      GC.count.should be_kind_of(Integer)
+    end
   end
 end
