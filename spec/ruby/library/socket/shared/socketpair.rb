e32240e3728822947687c9afc9498f13167cb271
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-11-19 17:19:53 -0500

    Add spec for Socket#socketpair

--- spec/ruby/library/socket/shared/socketpair.rb
+++ spec/ruby/library/socket/shared/socketpair.rb
@@ -9,6 +9,12 @@ describe :socket_socketpair, :shared => true do
     end
   end
 
+  it "only allows Socket constants as strings" do
+    [ :DGRAM, :RAW, :RDM, :SEQPACKET, :STREAM ].each do |socket_type|
+      lambda { Socket.socketpair(Socket::AF_UNIX, "SOCK_#{socket_type}", 0) }.should_not raise_error(SocketError)
+    end
+  end
+
   ruby_version_is "1.9" do
     it "raises SocketError if given symbol is not a Socket constants reference" do
       lambda { Socket.socketpair(Socket::AF_UNIX, :NO_EXIST, 0) }.should raise_error(SocketError)
