5e1a69ea0116b0682fb7bafadf1b739f07def69c
Brian Ford <brixen@gmail.com>
2012-11-28 11:39:05 -0800

    Updated String#splice! internal specs.

--- spec/core/string/splice_spec.rb
+++ spec/core/string/splice_spec.rb
@@ -1,38 +1,62 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
+# The String#splice! method is a helper method. Because of the semantics of
+# Ruby methods that use this method, bounds checking and data conversions need
+# to happen before this method is called. Hence, to improve the performance of
+# this method, its API expects valid in-range values and its behavior is
+# UNDEFINED for values out-of-range. The underlying primitive that manipulates
+# vectors of bytes is boundary safe (as all such primitives must be).
+
 describe "String#splice!" do
-  it "replaces count characters starting at index with the replacement string" do
-    "aaa".splice!(1, 1, "bbb").should == "abbba"
-    "a".splice!(0, 1, "xxx").should == "xxx"
-    "aaa".splice!(3, 0, "xxx").should == "aaaxxx"
-    "aaa".splice!(3, 1, "xxx").should == "aaaxxx"
-    "aaa".splice!(3, 2, "xxx").should == "aaaxxx"
-    "aaaa".splice!(2, 0, "bb").should == "aabbaa"
+  it "replaces zero characters an the beginning of the String" do
+    "abc".splice!(0, 0, "").should == "abc"
   end
 
-  it "removes a section of string when passed an empty replacement string" do
-    "aaa".splice!(1, 1, "").should == "aa"
-    "aaa".splice!(0, 2, "").should == "a"
-    "aaa".splice!(0, 3, "").should == ""
+  it "replaces zero characters within the String" do
+    "abc".splice!(1, 0, "").should == "abc"
   end
 
-  it "accepts a negative start index" do
-    "aaaa".splice!(-2, 2, "yy").should == "aayy"
-    "aaa".splice!(-3, 2, "yy").should == "yya"
-    "aaa".splice!(-3, 3, "xx").should == "xx"
+  it "replaces zero characters at the end of the String" do
+    "abc".splice!(3, 0, "").should == "abc"
   end
 
-  it "raises IndexError if start is outside the bounds of the string" do
-    lambda { "aaa".splice!(-4, 2, "yy") }.should raise_error(IndexError)
-    lambda { "aaa".splice!(4, 1, "yy") }.should raise_error(IndexError)
+  it "replaces characters at the beginning of the String" do
+    "abc".splice!(0, 2, "xyz").should == "xyzc"
   end
 
-  it "replaces to the end of string if start + count is greater than the size of string" do
-    "aaa".splice!(1, 5, "xx").should == "axx"
-    "aaa".splice!(0, 5, "").should == ""
+  it "replaces characters within the String" do
+    "abcde".splice!(1, 2, "xyz").should == "axyzde"
   end
 
-  it "raises IndexError if count is negative" do
-    lambda { "aaa".splice!(1, -3, "bb") }.should raise_error(IndexError)
+  it "replaces characters at the end of the String" do
+    "abc".splice!(2, 1, "xyz").should == "abxyz"
+  end
+
+  it "inserts characters an the beginning of the String" do
+    "abc".splice!(0, 0, "x").should == "xabc"
+  end
+
+  it "inserts characters within the String" do
+    "abc".splice!(1, 0, "x").should == "axbc"
+  end
+
+  it "inserts characters at the end of the String" do
+    "abc".splice!(3, 0, "x").should == "abcx"
+  end
+
+  it "inserts characters after the end of the String" do
+    "abc".splice!(3, 0, "xyz").should == "abcxyz"
+  end
+
+  it "removes characters at the beginning of the String" do
+    "abc".splice!(0, 1, "").should == "bc"
+  end
+
+  it "removes characters within the String" do
+    "abcde".splice!(2, 2, "").should == "abe"
+  end
+
+  it "removes characters at the end of the String" do
+    "abcde".splice!(3, 2, "").should == "abc"
   end
 end
