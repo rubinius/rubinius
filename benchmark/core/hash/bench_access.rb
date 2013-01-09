8be92cf72f236f503e478bfd59951d1b03de3f3c
Dirkjan Bussink <d.bussink@gmail.com>
2013-01-09 21:04:59 +0100

    Convert tabs to spaces for Hash#[]/fetch benchmarks

--- benchmark/core/hash/bench_access.rb
+++ benchmark/core/hash/bench_access.rb
@@ -3,9 +3,9 @@ require 'benchmark/ips'
 require File.expand_path('../shared_hash.rb', __FILE__)
 
 Benchmark.ips do |x|
-	small_hash = $small_hash
+  small_hash = $small_hash
 
-	x.report "hsh[i] (exists)" do |times|
+  x.report "hsh[i] (exists)" do |times|
     i = 0
     while i < times
       x = small_hash[:SlBkyplxcZ]
@@ -13,7 +13,7 @@ Benchmark.ips do |x|
     end
   end
 
-	x.report "hsh[i] (doesn't exist)" do |times|
+  x.report "hsh[i] (doesn't exist)" do |times|
     i = 0
     while i < times
       x = small_hash[:SlBkdfadsfs]
@@ -21,7 +21,7 @@ Benchmark.ips do |x|
     end
   end
 
-	x.report "hsh.fetch (exists)" do |times|
+  x.report "hsh.fetch (exists)" do |times|
     i = 0
     while i < times
       x = small_hash.fetch(:SlBkyplxcZ)
@@ -30,29 +30,29 @@ Benchmark.ips do |x|
   end
 
   x.report "values_at" do |times|
-  	i = 0
-  	while i < times
-  		x = small_hash.values_at(700648627, 639030613, 471761289)
-  		i += 1
-  	end
+    i = 0
+    while i < times
+      x = small_hash.values_at(700648627, 639030613, 471761289)
+      i += 1
+    end
   end
 
   default_small_hash = small_hash.dup
   default_small_hash.default = "boom"
   x.report "hsh[i] (default value)" do |times|
-  	i = 0
-  	while i < times
-  		x = default_small_hash[:nonexistent]
-  		i += 1
-  	end
+    i = 0
+    while i < times
+      x = default_small_hash[:nonexistent]
+      i += 1
+    end
   end
 
   default_proc_hash = Hash.new {|h,k| 1 }
   x.report "hsh[i] (default proc)" do |times|
-  	i = 0
-  	while i < times
-  		x = default_proc_hash[:nonexistent]
-  		i += 1
-  	end
+    i = 0
+    while i < times
+      x = default_proc_hash[:nonexistent]
+      i += 1
+    end
   end
 end
