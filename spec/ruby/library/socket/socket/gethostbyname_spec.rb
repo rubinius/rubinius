de72b4a1be1f7dd8d28ca70a565ef473ef55215b
Sylvain Daubert <sylvain.daubert@laposte.net>
2013-01-14 20:34:02 +0100

    Update spec library/socket/socket/gethostbyname.
    Add two new specs to verify that addresses in address list returned
    by Socket.gethosbyname are in 'pack' format (one spec for IPv4
    addresses an the other for IPv6 ones).

--- spec/ruby/library/socket/socket/gethostbyname_spec.rb
+++ spec/ruby/library/socket/socket/gethostbyname_spec.rb
@@ -13,4 +13,14 @@ describe "Socket#gethostbyname" do
     addr = Socket.gethostbyname('<any>').first;
     addr.should == "0.0.0.0"
   end
+
+  it "returns address list in pack format (IPv4)" do
+    laddr = Socket.gethostbyname('127.0.0.1')[3..-1];
+    laddr.should == ["\x7f\x00\x00\x01"]
+  end
+
+  it "returns address list in pack format (IPv6)" do
+    laddr = Socket.gethostbyname('::1')[3..-1]
+    laddr.should == ["\x00" * 15 + "\x01"]
+  end
 end
