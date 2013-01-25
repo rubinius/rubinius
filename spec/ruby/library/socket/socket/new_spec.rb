7e9a4937301d399541156076566af7dab1eb58da
Brian Shirai <brixen@gmail.com>
2013-01-24 17:44:52 -0800

    Fixed Socket.new specs.

--- spec/ruby/library/socket/socket/new_spec.rb
+++ spec/ruby/library/socket/socket/new_spec.rb
@@ -1,32 +1,87 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
 require File.expand_path('../../fixtures/classes', __FILE__)
 
-describe "Socket#new" do
-  it "only allows Socket constants as strings" do
-    [ :DGRAM, :RAW, :RDM, :SEQPACKET, :STREAM ].each do |socket_type|
-      lambda { Socket.new(Socket::PF_UNIX, "SOCK_#{socket_type}", 0) }.should_not raise_error(SocketError)
+describe "Socket.new" do
+  before :each do
+    @socket = nil
+  end
+
+  after :each do
+    @socket.close if @socket and not @socket.closed?
+  end
+
+  describe "with a Socket::PF_INET family" do
+    it "creates a socket with a :DGRAM type specifier" do
+      @socket = Socket.new Socket::PF_INET, "SOCK_DGRAM", 0
+      @socket.should be_an_instance_of(Socket)
+    end
+
+    it "creates a socket with a Socket::PF_STREAM type specifier" do
+      @socket = Socket.new Socket::PF_INET, "SOCK_STREAM", 0
+      @socket.should be_an_instance_of(Socket)
+    end
+  end
+
+  describe "with a :UNIX family" do
+    it "creates a socket with a :DGRAM type specifier" do
+      @socket = Socket.new Socket::PF_UNIX, "SOCK_DGRAM", 0
+      @socket.should be_an_instance_of(Socket)
+    end
+
+    it "creates a socket with a :STREAM type specifier" do
+      @socket = Socket.new Socket::PF_UNIX, "SOCK_STREAM", 0
+      @socket.should be_an_instance_of(Socket)
     end
   end
 
   ruby_version_is ""..."1.9" do
-    it "raises Errno::EPROTONOSUPPORT if socket type is not a string or integer" do
+    it "raises an Errno::EPROTONOSUPPORT if socket type is not a string or integer" do
       lambda { Socket.new(Socket::PF_UNIX, :DGRAM, 0) }.should raise_error(Errno::EPROTONOSUPPORT)
     end
   end
 
   ruby_version_is "1.9" do
-    it "raises SocketError if given symbol is not a Socket constants reference" do
+    it "raises a SocketError if given symbol is not a Socket constants reference" do
       lambda { Socket.new(Socket::PF_UNIX, :NO_EXIST, 0) }.should raise_error(SocketError)
     end
 
-    it "only allows Socket constants as symbols" do
-      [ :DGRAM, :RAW, :RDM, :SEQPACKET, :STREAM ].each do |socket_type|
-        lambda { Socket.new(Socket::PF_UNIX, socket_type, 0) }.should_not raise_error(SocketError)
+    it "creates a socket with an :INET family specifier" do
+      @socket = Socket.new :INET, :STREAM
+      @socket.should be_an_instance_of(Socket)
+    end
+
+    it "creates a socket with a :UNIX family specifier" do
+      @socket = Socket.new :UNIX, :STREAM
+      @socket.should be_an_instance_of(Socket)
+    end
+
+    describe "with an :INET family" do
+      it "creates a socket with a :DGRAM type specifier" do
+        @socket = Socket.new :INET, :DGRAM
+        @socket.should be_an_instance_of(Socket)
+      end
+
+      it "creates a socket with a :STREAM type specifier" do
+        @socket = Socket.new :INET, :STREAM
+        @socket.should be_an_instance_of(Socket)
       end
     end
 
-    it "protocol parameter should be optional" do
-      lambda { Socket.new(Socket::PF_UNIX, :DGRAM) }.should_not raise_error(ArgumentError)
+    describe "with a :UNIX family" do
+      it "creates a socket with a :DGRAM type specifier" do
+        @socket = Socket.new :UNIX, :DGRAM
+        @socket.should be_an_instance_of(Socket)
+      end
+
+      it "creates a socket with a :STREAM type specifier" do
+        @socket = Socket.new :UNIX, :STREAM
+        @socket.should be_an_instance_of(Socket)
+      end
+    end
+
+    it "accepts an optional protocol parameter" do
+      @socket = Socket.new Socket::PF_UNIX, Socket::SOCK_DGRAM, 0
+      @socket.should be_an_instance_of(Socket)
     end
   end
 end
