32a17b16284bfbaaf1624b5e9e434d951b58b321
Ryo Onodera <ryoqun@gmail.com>
2013-02-07 21:22:55 +0900

    Use String#size to avoid confusion

--- spec/ruby/core/proc/new_spec.rb
+++ spec/ruby/core/proc/new_spec.rb
@@ -122,28 +122,28 @@ end
 
 describe "Proc.new with a block argument" do
   it "returns the passed proc created from a block" do
-    passed_prc = Proc.new { "hello".class }
+    passed_prc = Proc.new { "hello".size }
     prc = Proc.new(&passed_prc)
 
     prc.should equal(passed_prc)
-    prc.call.should == String
+    prc.call.should == 5
   end
 
   it "returns the passed proc created from a method" do
-    method = "hello".method(:class)
+    method = "hello".method(:size)
     passed_prc = Proc.new(&method)
     prc = Proc.new(&passed_prc)
 
     prc.should equal(passed_prc)
-    prc.call.should == String
+    prc.call.should == 5
   end
 
   it "returns the passed proc created from a symbol" do
-    passed_prc = Proc.new(&:class)
+    passed_prc = Proc.new(&:size)
     prc = Proc.new(&passed_prc)
 
     prc.should equal(passed_prc)
-    prc.call("hello").should == String
+    prc.call("hello").should == 5
   end
 end
 
