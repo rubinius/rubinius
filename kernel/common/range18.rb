80babf7d61205d2546463c50e086935333648aca
Kenichi Kamiya <kachick1@gmail.com>
2013-02-11 15:21:19 +0900

    Adjust Range#each for X18
    Ruby 1.8.7 can not generate Range objects from Symbol elements
    
    :a..:b #=> ArgumentError: bad value for range
    
    This commit removes verbose dealing for Symbol on X18

--- kernel/common/range18.rb
+++ kernel/common/range18.rb
@@ -3,6 +3,42 @@
 class Range
   alias_method :===, :include?
 
+  def each(&block)
+    return to_enum unless block_given?
+    first, last = @begin, @end
+
+    raise TypeError, "can't iterate from #{first.class}" unless can_iterate_from?(first)
+
+    case first
+    when Fixnum
+      last -= 1 if @excl
+
+      i = first
+      while i <= last
+        yield i
+        i += 1
+      end
+
+    when String
+      first.upto(last, @excl, &block)
+    else
+      current = first
+      if @excl
+        while (current <=> last) < 0
+          yield current
+          current = current.succ
+        end
+      else
+        while (c = current <=> last) && c <= 0
+          yield current
+          break if c == 0
+          current = current.succ
+        end
+      end
+    end
+    return self
+  end
+
   alias_method :first, :begin
 
   protected
