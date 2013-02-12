7fdad60a19f952398825c3126d741a942d152c81
Ryo Onodera <ryoqun@gmail.com>
2013-02-13 08:52:48 +0900

    Add various specs for Proc#{inspect,source_location}

--- spec/ruby/optional/capi/proc_spec.rb
+++ spec/ruby/optional/capi/proc_spec.rb
@@ -54,8 +54,18 @@ describe "C-API Proc function" do
       @prc.arity.should == -1
     end
 
+    it "returns a Proc instance correctly described in #ispect" do
+      @prc.inspect.should =~ /^#<Proc:([^ ]*?)>$/
+    end
+
     it "shouldn't be equal to another one" do
       @prc.should_not == @p.rb_proc_new
+    end
+
+    ruby_version_is "1.9" do
+      it "returns a Proc instance with #source_location == nil" do
+        @prc.source_location.should == nil
+      end
     end
   end
 end
