e2b872876ee5a8e451c91c335cb7934f238925b1
Dirkjan Bussink <d.bussink@gmail.com>
2013-02-14 09:09:16 +0100

    Add specs for comparing Etc::Passwd and Etc::Group

--- spec/ruby/library/etc/struct_group_spec.rb
+++ spec/ruby/library/etc/struct_group_spec.rb
@@ -7,21 +7,25 @@ describe "Struct::Group" do
       @g = Etc.getgrgid(`id -g`.strip.to_i)
     end
 
-    it "returns group name" do 
+    it "returns group name" do
       @g.name.should == `id -gn`.strip
     end
-    
-    it "returns group password" do 
+
+    it "returns group password" do
       @g.passwd.is_a?(String).should == true
     end
-    
-    it "returns group id" do 
+
+    it "returns group id" do
       @g.gid.should == `id -g`.strip.to_i
     end
-    
+
     it "returns an array of users belonging to the group" do 
       @g.mem.is_a?(Array).should == true
     end
-    
+
+    it "can be compared to another object" do
+      (@g == nil).should == false
+      (@g == Object.new).should == false
+    end
   end
 end
--- spec/ruby/library/etc/struct_passwd_spec.rb
+++ spec/ruby/library/etc/struct_passwd_spec.rb
@@ -7,32 +7,37 @@ describe "Struct::Passwd" do
       @pw = Etc.getpwuid(`id -u`.strip.to_i)
     end
 
-    it "returns user name" do 
+    it "returns user name" do
       @pw.name.should == `id -un`.strip
     end
-    
-    it "returns user password" do 
+
+    it "returns user password" do
       @pw.passwd.is_a?(String).should == true
     end
-    
-    it "returns user id" do 
+
+    it "returns user id" do
       @pw.uid.should == `id -u`.strip.to_i
     end
-    
+
     it "returns user group id" do
       @pw.gid.should == `id -g`.strip.to_i
     end
-    
-    it "returns user personal information (gecos field)" do 
+
+    it "returns user personal information (gecos field)" do
       @pw.gecos.is_a?(String).should == true
     end
-    
-    it "returns user home directory" do 
+
+    it "returns user home directory" do
       @pw.dir.is_a?(String).should == true
     end
-    
-    it "returns user shell" do 
+
+    it "returns user shell" do
       @pw.shell.is_a?(String).should == true
+    end
+
+    it "can be compared to another object" do
+      (@pw == nil).should == false
+      (@pw == Object.new).should == false
     end
   end
 end
