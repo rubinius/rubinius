rubinius/rubinius

commit 4bfc088e4c8760de98840d5ef71a32f6d0782c5a

Require date when time is required

lib/20/date.rb
@@ -193,7 +193,7 @@
 #
 #     puts secs_to_new_year()
 
-require 'date/format'
+require 'date'
 
 # Class representing a date.
 #

lib/19/time.rb
@@ -36,7 +36,7 @@
 #   but it is not portable.
 #
 
-require 'date/format'
+require 'date'
 
 #
 # Implements the extensions to the Time class that are described in the

commit ca65281506bc56b06894cdd29763653b4a921893

Fixed error introduced in 4bfc088e4c.

lib/20/date.rb
@@ -193,7 +193,7 @@
 #
 #     puts secs_to_new_year()
 
-require 'date'
+require 'date/format'
 
 # Class representing a date.
 #

lib/20/time.rb
@@ -36,7 +36,7 @@
 #   but it is not portable.
 #
 
-require 'date/format'
+require 'date'
 
 #
 # Implements the extensions to the Time class that are described in the