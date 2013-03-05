e695e8ab4eb1a9b04be56cca9f0219d75c2bd155
Brian Shirai <brixen@gmail.com>
2013-03-05 15:17:45 -0800

    Fixed Symbol#to_proc for 1.9.

--- kernel/common/symbol.rb
+++ kernel/common/symbol.rb
@@ -9,16 +9,4 @@ class Symbol
     Rubinius.primitive :symbol_all_symbols
     raise PrimitiveFailure, "Symbol.all_symbols primitive failed."
   end
-
-  # Returns a Proc object which respond to the given method by sym.
-  def to_proc
-    # Put sym in the outer enclosure so that this proc can be instance_eval'd.
-    # If we used self in the block and the block is passed to instance_eval, then
-    # self becomes the object instance_eval was called on. So to get around this,
-    # we leave the symbol in sym and use it in the block.
-    #
-    sym = self
-    Proc.new { |*args| args.shift.__send__(sym, *args) }
-  end
-
 end
