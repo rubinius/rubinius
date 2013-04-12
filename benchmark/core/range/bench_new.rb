8232560c4f68ebef564503571e99a8635129b9da
Dirkjan Bussink <d.bussink@gmail.com>
2013-04-12 10:55:18 +0200

    Add benchmarks for Range literals and regular constructors

--- benchmark/core/range/bench_new.rb
+++ benchmark/core/range/bench_new.rb
@@ -3,9 +3,16 @@ require 'benchmark/ips'
 #require File.expand_path('../shared_string.rb', __FILE__)
 
 Benchmark.ips do |x|
-  #short_sentence = $short_sentence.dup
 
   x.report "0..10" do |times|
+    i = 0
+    while i < times
+      0..10
+      i += 1
+    end
+  end
+
+  x.report "Range.new(0, 10)" do |times|
     i = 0
     while i < times
       Range.new(0, 10)
@@ -16,12 +23,28 @@ Benchmark.ips do |x|
   x.report "0...10" do |times|
     i = 0
     while i < times
+      0...10
+      i += 1
+    end
+  end
+
+  x.report "Range.new(0, 10, true)" do |times|
+    i = 0
+    while i < times
       Range.new(0, 10, true)
       i += 1
     end
   end
 
   x.report "0xffff..0xfffff" do |times|
+    i = 0
+    while i < times
+      0xffff..0xfffff
+      i += 1
+    end
+  end
+
+  x.report "Range.new(0xffff, 0xfffff)" do |times|
     i = 0
     while i < times
       Range.new(0xffff, 0xfffff)
@@ -32,6 +55,14 @@ Benchmark.ips do |x|
   x.report "0xffff...0xfffff" do |times|
     i = 0
     while i < times
+      0xffff...0xfffff
+      i += 1
+    end
+  end
+
+  x.report "Range.new(0xffff, 0xfffff, true)" do |times|
+    i = 0
+    while i < times
       Range.new(0xffff, 0xfffff, true)
       i += 1
     end
@@ -40,7 +71,15 @@ Benchmark.ips do |x|
   x.report "0.5..2.4" do |times|
     i = 0
     while i < times
-      Range.new(0.5,2.4)
+      0.5..2.4
+      i += 1
+    end
+  end
+
+  x.report "Range.new(0.5, 2.4)" do |times|
+    i = 0
+    while i < times
+      Range.new(0.5, 2.4)
       i += 1
     end
   end
@@ -48,12 +87,28 @@ Benchmark.ips do |x|
   x.report "0.5...2.4" do |times|
     i = 0
     while i < times
-      Range.new(0.5,2.4,true)
+      0.5...2.4
+      i += 1
+    end
+  end
+
+  x.report "Range.new(0.5, 2.4, true)" do |times|
+    i = 0
+    while i < times
+      Range.new(0.5, 2.4, true)
       i += 1
     end
   end
 
   x.report "'a'..'j'" do |times|
+    i = 0
+    while i < times
+      'a'..'j'
+      i += 1
+    end
+  end
+
+  x.report "Range.new('a', 'j')" do |times|
     i = 0
     while i < times
       Range.new('a', 'j')
@@ -62,6 +117,14 @@ Benchmark.ips do |x|
   end
 
   x.report "'a'...'j'" do |times|
+    i = 0
+    while i < times
+      'a'...'j'
+      i += 1
+    end
+  end
+
+  x.report "Range.new('a', 'j', true)" do |times|
     i = 0
     while i < times
       Range.new('a', 'j', true)
