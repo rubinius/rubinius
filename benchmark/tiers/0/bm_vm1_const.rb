588bc379144eb8fae93a7d677dcb96b9f78bef8c
Dirkjan Bussink <d.bussink@gmail.com>
2013-04-14 18:30:55 +0200

    Use loop style for const benchmark

--- benchmark/tiers/0/bm_vm1_const.rb
+++ benchmark/tiers/0/bm_vm1_const.rb
@@ -4,7 +4,11 @@ require 'benchmark/ips'
 Const = 1
 
 Benchmark.ips do |x|
-  x.report "const read", <<-CODE
-    Const
-  CODE
+  x.report "const read" do |times|
+    i = 0
+    while i < times
+      Const
+      i += 1
+    end
+  end
 end
