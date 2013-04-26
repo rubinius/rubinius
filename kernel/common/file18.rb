c46615b6b1fda1ab4f98b8763f9f516757c9777a
Brian Shirai <brixen@gmail.com>
2013-04-25 21:23:40 -0700

    Fixed File#open with file descriptor.

--- kernel/common/file18.rb
+++ kernel/common/file18.rb
@@ -1,11 +1,12 @@
 class File
-  def initialize(path_or_fd, mode = "r", perm = 0666)
+  def initialize(path_or_fd, mode=nil, perm=0666)
     if path_or_fd.kind_of? Integer
       super(path_or_fd, mode)
       @path = nil
     else
       path = Rubinius::Type.coerce_to_path path_or_fd
 
+      mode ||= "r"
       fd = IO.sysopen(path, mode, perm)
       if fd < 0
         begin
