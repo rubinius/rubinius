f31d281bd344d725986fcfb5c55c3d7d80402978
Brian Shirai <brixen@gmail.com>
2013-04-27 21:39:30 -0700

    Fixed Array#sample for 1.9.

--- kernel/common/array19.rb
+++ kernel/common/array19.rb
@@ -486,31 +486,50 @@ class Array
     replace ary
   end
 
-  def sample(*args)
-    random_generator = Kernel
-    n = nil
+  def sample(count=undefined, options=undefined)
+    return at Kernel.rand(size) if count.equal? undefined
 
-    args.each do |a|
-      if options = Rubinius::Type.check_convert_type(a, Hash, :to_hash)
-        random_generator = options[:random] if options[:random].respond_to?(:rand)
+    if options.equal? undefined
+      if o = Rubinius::Type.check_convert_type(count, Hash, :to_hash)
+        options = o
+        count = nil
       else
-        n = Rubinius::Type.coerce_to(a, Fixnum, :to_int)
-        raise ArgumentError, "negative array size" if n < 0
+        options = nil
+        count = Rubinius::Type.coerce_to count, Fixnum, :to_int
       end
+    else
+      count = Rubinius::Type.coerce_to count, Fixnum, :to_int
+      options = Rubinius::Type.coerce_to options, Hash, :to_hash
     end
 
-    return at(random_generator.rand(size)) unless n
-
-    n = size if n > size
-    result = Array.new(self)
-
-    n.times do |i|
-      r = i + random_generator.rand(size - i)
-      result.tuple.swap(i, r)
+    if count and count < 0
+      raise ArgumentError, "count must be greater than 0"
     end
 
-    result[n..size] = []
-    result
+    rng = options[:random] if options
+    rng = Kernel unless rng and rng.respond_to? :rand
+
+    unless count
+      random = Rubinius::Type.coerce_to rng.rand, Float, :to_f
+      raise RangeError, "random value must be >= 0.0" if random < 0
+      raise RangeError, "random value must be < 1.0" if random >= 1.0
+
+      return at random * size
+    end
+
+    count = size if count > size
+    result = Array.new self
+
+    count.times do |i|
+      random = Rubinius::Type.coerce_to rng.rand, Float, :to_f
+      raise RangeError, "random value must be >= 0.0" if random < 0
+      raise RangeError, "random value must be < 1.0" if random >= 1.0
+
+      r = random * size
+      result.tuple.swap(i, r.to_i)
+    end
+
+    return count == size ? result : result[0, count]
   end
 
   def select!(&block)
