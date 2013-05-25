8aabd43ca0fa5ddb5449f7c8caf100d64c13e3f8
Dirkjan Bussink <d.bussink@gmail.com>
2013-05-25 11:28:25 +0200

    Use the object write barrier instead of going through ObjectMemory

--- vm/oop.cpp
+++ vm/oop.cpp
@@ -789,8 +789,8 @@ step2:
 
     copy_body(state, other);
 
-    state->om->write_barrier(reinterpret_cast<Object*>(this), ivars_);
-    state->om->write_barrier(reinterpret_cast<Object*>(this), klass_);
+    reinterpret_cast<Object*>(this)->write_barrier(state, ivars_);
+    reinterpret_cast<Object*>(this)->write_barrier(state, klass_);
 
     // This method is only used by the GC to move an object, so must retain
     // the settings flags.
