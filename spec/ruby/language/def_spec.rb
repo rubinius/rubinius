270fa92c4bccafd5a895c73fa8d354c7d3a0eb27
Kenichi Kamiya <kachick1@gmail.com>
2013-06-23 03:06:17 +0900

    Add a spec for defining `initialize_dup` from 2.0

--- spec/ruby/language/def_spec.rb
+++ spec/ruby/language/def_spec.rb
@@ -31,6 +31,18 @@ describe "Defining an 'initialize_copy' method" do
   end
 end
 
+ruby_version_is "2.0" do
+  describe "Defining an 'initialize_dup' method" do
+    it "sets the method's visibility to private" do
+      class DefInitializeDupSpec
+        def initialize_dup
+        end
+      end
+      DefInitializeDupSpec.should have_private_instance_method(:initialize_dup, false)
+    end
+  end
+end
+
 describe "An instance method definition with a splat" do
   it "accepts an unnamed '*' argument" do
     def foo(*); end;
