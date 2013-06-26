94521c141dffc42821db41db2fc457b84c866c6c
Dirkjan Bussink <d.bussink@gmail.com>
2013-06-26 14:33:00 +0200

    Add first specs for Addrinfo object

--- spec/ruby/library/socket/addrinfo/canonname_spec.rb
+++ spec/ruby/library/socket/addrinfo/canonname_spec.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../../../../spec_helper', __FILE__)
+require File.expand_path('../../fixtures/classes', __FILE__)
 require 'socket'
 
 describe "Addrinfo#canonname" do
-  it "needs to be reviewed for spec completeness"
+
+  before :each do
+    @addrinfos = Addrinfo.getaddrinfo("localhost", 80, :INET, :STREAM, nil, Socket::AI_CANONNAME)
+  end
+
+  it "returns the canonical name for a host" do
+    @addrinfos.map {|a| a.canonname }.should include("localhost")
+  end
+
 end
--- spec/ruby/library/socket/addrinfo/unix_spec.rb
+++ spec/ruby/library/socket/addrinfo/unix_spec.rb
@@ -6,5 +6,37 @@ describe "Addrinfo.unix" do
 end
 
 describe "Addrinfo#unix?" do
-  it "needs to be reviewed for spec completeness"
+  describe "for an ipv4 socket" do
+
+    before :each do
+      @addrinfo = Addrinfo.tcp("127.0.0.1", 80)
+    end
+
+    it "returns false" do
+      @addrinfo.unix?.should be_false
+    end
+
+  end
+
+  describe "for an ipv6 socket" do
+    before :each do
+      @addrinfo = Addrinfo.tcp("::1", 80)
+    end
+
+    it "returns false" do
+      @addrinfo.unix?.should be_false
+    end
+  end
+
+  platform_is_not :windows do
+    describe "for a unix socket" do
+      before :each do
+        @addrinfo = Addrinfo.unix("/tmp/sock")
+      end
+
+      it "returns true" do
+        @addrinfo.unix?.should be_true
+      end
+    end
+  end
 end
