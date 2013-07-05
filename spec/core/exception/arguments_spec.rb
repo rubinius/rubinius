1899720ef8b813c57c358b26962431ea5a99e71b
Federico Ravasio <ravasio.federico@gmail.com>
2013-07-05 00:59:30 -0700

    Fixed a syntax mistake regarding blocks in specs.
    The block wasn't passed to `raise_error`, due to missing parenthesis
    around it and the block.
    Adding them would've looked a bit awkward, so I put the second
    expectation in a lambda and passed it as a block to `raise_error`.

--- spec/core/exception/arguments_spec.rb
+++ spec/core/exception/arguments_spec.rb
@@ -2,8 +2,7 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "ArgumentError" do
   it "uses its internal message in #inspect" do
-    lambda { 1.+(2, 3) }.should raise_error(ArgumentError) do |e|
-      e.inspect.should == "#<ArgumentError: method '+': given 2, expected 1>"
-    end
+    error_inspection = lambda { |e| e.inspect.should == "#<ArgumentError: method '+': given 2, expected 1>" }
+    lambda { 1.+(2, 3) }.should raise_error(ArgumentError, &error_inspection)
   end
 end
