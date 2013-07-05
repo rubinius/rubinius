49d83dec8f0981b07cdd488a143626d6f68c4ab7
Kenichi Kamiya <kachick1@gmail.com>
2013-07-05 15:36:41 +0900

    Add a spec for Module#private when given an undefined name

--- spec/ruby/core/module/private_spec.rb
+++ spec/ruby/core/module/private_spec.rb
@@ -42,4 +42,12 @@ describe "Module#private" do
       private.should equal(self)
     end
   end
+
+  it "raises a NameError when given an undefined name" do
+    lambda {Module.new.send(:private, :undefined)}.should(
+      raise_error(NameError) do |err|
+        err.should be_an_instance_of(NameError)
+      end
+    )
+  end
 end
