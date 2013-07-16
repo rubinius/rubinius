6a91bc129165671889ba68eec5f9cf11ab847e95
Kenichi Kamiya <kachick1@gmail.com>
2013-07-16 12:40:58 +0900

    Fix Hash#default_proc= for 2.0

--- kernel/common/hash20.rb
+++ kernel/common/hash20.rb
@@ -4,4 +4,19 @@ class Hash
   def to_h
     instance_of?(Hash) ? self : Hash[self]
   end
+
+  # Sets the default proc to be executed on each key lookup
+  def default_proc=(prc)
+    Rubinius.check_frozen
+    unless prc.nil?
+      prc = Rubinius::Type.coerce_to prc, Proc, :to_proc
+
+      if prc.lambda? and prc.arity != 2
+        raise TypeError, "default proc must have arity 2"
+      end
+    end
+
+    @default = nil
+    @default_proc = prc
+  end
 end
