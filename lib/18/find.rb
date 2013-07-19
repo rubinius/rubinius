8a3bae07693106c2d1e1cb394740c8d333569cda
Federico Ravasio <ravasio.federico@gmail.com>
2013-07-19 02:18:49 -0700

    Move existing Find implementation to 1.8 stdlib.

--- lib/18/find.rb
+++ lib/18/find.rb
@@ -36,29 +36,29 @@ module Find
     paths.collect!{|d| d.dup}
     while file = paths.shift
       catch(:prune) do
-	yield file.dup.taint
+        yield file.dup.taint
         next unless File.exist? file
-	begin
-	  if File.lstat(file).directory? then
-	    d = Dir.open(file)
-	    begin
-	      for f in d
-		next if f == "." or f == ".."
-		if File::ALT_SEPARATOR and file =~ /^(?:[\/\\]|[A-Za-z]:[\/\\]?)$/ then
-		  f = file + f
-		elsif file == "/" then
-		  f = "/" + f
-		else
-		  f = File.join(file, f)
-		end
-		paths.unshift f.untaint
-	      end
-	    ensure
-	      d.close
-	    end
-	  end
+        begin
+          if File.lstat(file).directory? then
+            d = Dir.open(file)
+            begin
+              for f in d
+                next if f == "." or f == ".."
+                if File::ALT_SEPARATOR and file =~ /^(?:[\/\\]|[A-Za-z]:[\/\\]?)$/ then
+                  f = file + f
+                elsif file == "/" then
+                  f = "/" + f
+                else
+                  f = File.join(file, f)
+                end
+                paths.unshift f.untaint
+              end
+            ensure
+              d.close
+            end
+          end
         rescue Errno::ENOENT, Errno::EACCES
-	end
+        end
       end
     end
   end
