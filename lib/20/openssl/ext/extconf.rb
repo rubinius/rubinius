1d4182cd22ac0e754f59d957520d7738104b478c
Dirkjan Bussink <d.bussink@gmail.com>
2013-07-23 21:43:07 +0200

    Remove Apple OpenSSL check for now

--- lib/20/openssl/ext/extconf.rb
+++ lib/20/openssl/ext/extconf.rb
@@ -58,9 +58,6 @@ end
 unless have_header("openssl/conf_api.h")
   raise "OpenSSL 0.9.6 or later required."
 end
-unless OpenSSL.check_func("SSL_library_init()", "openssl/ssl.h")
-  raise "Ignore OpenSSL broken by Apple.\nPlease use another openssl. (e.g. using `configure --with-openssl-dir=/path/to/openssl')"
-end
 
 Logging::message "=== Checking for OpenSSL features... ===\n"
 have_func("ERR_peek_last_error")
