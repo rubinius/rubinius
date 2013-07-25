c3eecbee349b8622df9b4b360c433c8f203655f6
Brian Shirai <brixen@gmail.com>
2013-07-25 16:19:50 -0700

    Use simpler digest methods. Thanks Terrence. [ci skip]
    No idea why the other code was written like that.

--- rakelib/package.rb
+++ rakelib/package.rb
@@ -1,17 +1,10 @@
 
 def write_md5_digest_file(filename)
   require 'digest/md5'
-  digest = Digest::MD5.new
-
-  File.open(filename, "rb") do |f|
-    until f.eof?
-      digest << f.read(1024)
-    end
-  end
 
   digest_file = "#{filename}.md5"
   File.open(digest_file, "w") do |f|
-    f.puts digest.hexdigest
+    f.puts Digest::MD5.file(filename).hexdigest
   end
 
   puts "Computed MD5 to #{digest_file}"
@@ -19,17 +12,10 @@ end
 
 def write_sha1_digest_file(filename)
   require 'digest/sha1'
-  digest = Digest::SHA1.new
-
-  File.open(filename, "rb") do |f|
-    until f.eof?
-      digest << f.read(1024)
-    end
-  end
 
   digest_file = "#{filename}.sha1"
   File.open(digest_file, "w") do |f|
-    f.puts digest.hexdigest
+    f.puts Digest::SHA1.file(filename).hexdigest
   end
 
   puts "Computed SHA1 to #{digest_file}"
