ae9601c24ab3a8445c3a20efa89653b18e125a8b
Kenichi Kamiya <kachick1@gmail.com>
2013-07-31 04:35:39 +0900

    Fix Enumerator#initialize for 2.0

--- kernel/common/enumerator20.rb
+++ kernel/common/enumerator20.rb
@@ -3,6 +3,34 @@
 module Enumerable
   class Enumerator
 
+    def initialize(object_or_size=undefined, iter=:each, *args, &block)
+      if block_given?
+        if undefined.equal? object_or_size
+          size = nil
+        else
+          size = object_or_size
+        end
+
+        object = Generator.new(&block)
+      else
+        if undefined.equal? object_or_size
+          raise ArgumentError, "Enumerator#initialize requires a block when called without arguments"
+        end
+
+        object = object_or_size
+      end
+
+      @object = object
+      @size = size
+      @iter = Rubinius::Type.coerce_to iter, Symbol, :to_sym
+      @args = args
+      @generator = nil
+      @lookahead = []
+
+      self
+    end
+    private :initialize
+
     class Lazy < self
       def initialize(receiver, size=nil)
         raise ArgumentError, "Lazy#initialize requires a block" unless block_given?
