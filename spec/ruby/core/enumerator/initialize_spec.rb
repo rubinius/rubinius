78a22e1134ff8cfbed16c2f44da96a8602be5bd3
Kenichi Kamiya <kachick1@gmail.com>
2013-08-01 01:47:26 +0900

    Adjust specs for Enumerable::Enumerator#initialize

--- spec/ruby/core/enumerator/initialize_spec.rb
+++ spec/ruby/core/enumerator/initialize_spec.rb
@@ -20,4 +20,29 @@ describe "Enumerator#initialize" do
       @uninitialized.send(:initialize) {}.should equal(@uninitialized)
     end
   end
+
+  ruby_version_is "1.9.1" do
+    # Maybe spec should be broken up?
+    it "accepts a block" do
+      @uninitialized.send(:initialize) do |yielder|
+        r = yielder.yield 3
+        yielder << r << 2 << 1
+      end
+      @uninitialized.should be_an_instance_of(enumerator_class)
+      r = []
+      @uninitialized.each{|x| r << x; x * 2}
+      r.should == [3, 6, 2, 1]
+    end
+
+    ruby_version_is "" ... "2.0" do
+      it "ignores block if arg given" do
+        @uninitialized.send(:initialize, [1,2,3]){|y| y.yield 4}
+        @uninitialized.to_a.should == [1,2,3]
+      end
+    end
+
+    ruby_version_is "2.0" do
+      it "needs to be reviewed for spec completeness"
+    end
+  end
 end
