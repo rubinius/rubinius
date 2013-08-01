61cf343fe25f49290fd4dc1f8f1a5c626de576b2
Kenichi Kamiya <kachick1@gmail.com>
2013-08-01 18:07:17 +0900

    Adjust and Add specs for Enumerator::Generator#each

--- spec/ruby/core/enumerator/generator/each_spec.rb
+++ spec/ruby/core/enumerator/generator/each_spec.rb
@@ -2,22 +2,49 @@ require File.expand_path('../../../../spec_helper', __FILE__)
 
 ruby_version_is "1.9" do
   describe "Enumerator::Generator#each" do
-    it "is an enumerable" do
-      g = Enumerator::Generator.new { |y| y << 3 << 2 << 1 }
-      g.should be_kind_of(Enumerable)
+    before(:each) do
+      @generator = enumerator_class::Generator.new do |y, *args|
+        y << 3 << 2 << 1
+        y << args unless args.empty?
+        :block_returned
+      end
     end
 
-    it "Supports enumeration with a block" do
-      g = Enumerator::Generator.new { |y| y << 3 << 2 << 1 }
+    it "is an enumerable" do
+      @generator.should be_kind_of(Enumerable)
+    end
+
+    it "supports enumeration with a block" do
       r = []
-      g.each { |v| r << v }
+      @generator.each { |v| r << v }
 
       r.should == [3, 2, 1]
     end
 
-    it "Raises a LocalJumpError if no block given" do
-      g = Enumerator::Generator.new { |y| y << 3 << 2 << 1 }
-      lambda { g.each }.should raise_error(LocalJumpError)
+    it "raises a LocalJumpError if no block given" do
+      lambda { @generator.each }.should raise_error(LocalJumpError)
+    end
+
+    it "returns the block returned value" do
+      @generator.each {}.should equal(:block_returned)
+    end
+
+    ruby_version_is "" ... "2.0" do
+      it "does not require arguments" do
+        enumerator_class::Generator.instance_method(:each).arity.should == 0
+      end
+    end
+
+    ruby_version_is "2.0" do
+      it "requires multiple arguments" do
+        enumerator_class::Generator.instance_method(:each).arity.should < 0
+      end
+
+      it "appends given arguments to receiver.each" do
+        yields = []
+        @generator.each(:each0, :each1) { |yielded| yields << yielded }
+        yields.should == [3, 2, 1, [:each0, :each1]]
+      end
     end
   end
 end
