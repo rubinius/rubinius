6fc9b5b5900de4880789c07f61c9922f09ccc786
Kenichi Kamiya <kachick1@gmail.com>
2013-08-02 03:01:06 +0900

    Write a spec for Enumerator::Lazy#lazy

--- spec/ruby/core/enumerator/lazy/lazy_spec.rb
+++ spec/ruby/core/enumerator/lazy/lazy_spec.rb
@@ -8,4 +8,11 @@ ruby_version_is "2.0" do
       enumerator_class::Lazy.superclass.should equal(enumerator_class)
     end
   end
+
+  describe "Enumerator::Lazy#lazy" do
+    it "returns self" do
+      lazy = (1..3).to_enum.lazy
+      lazy.lazy.should equal(lazy)
+    end
+  end
 end
