ecc49d9de49a9f7c4917f56b9d2e0a762ca99f9a
Kenichi Kamiya <kachick1@gmail.com>
2013-08-10 02:19:26 +0900

    Fix sharing Time.{new,now} specs
    * Fix sharing
    * Fix instance~class matcher
    * Use `send` instead of `__send__` for adjusting style

--- spec/ruby/core/time/shared/now.rb
+++ spec/ruby/core/time/shared/now.rb
@@ -4,12 +4,12 @@ describe :time_now, :shared => true do
   platform_is_not :windows do
     it "creates a time based on the current system time" do
       unless `which date` == ""
-        Time.__send__(@method).to_i.should be_close(`date +%s`.to_i, 2)
+        Time.send(@method).to_i.should be_close(`date +%s`.to_i, 2)
       end
     end
   end
 
   it "creates a subclass instance if called on a subclass" do
-    TimeSpecs::SubTime.now.should be_kind_of(TimeSpecs::SubTime)
+    TimeSpecs::SubTime.send(@method).should be_an_instance_of(TimeSpecs::SubTime)
   end
 end
