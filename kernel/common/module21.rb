0b8700a6195f36be10567395fcbd4ad308eb7f96
Brian Shirai <brixen@gmail.com>
2013-08-09 21:51:43 -0700

    Cleanup from merge.

--- kernel/common/module21.rb
+++ kernel/common/module21.rb
@@ -182,7 +182,10 @@ class Module
   def visibility_for_aliased_method(new_name, current_name_visibility)
     special_methods = [
       :initialize,
-      :initialize_copy
+      :initialize_copy,
+      :initialize_clone,
+      :initialize_dup,
+      :respond_to_missing?
     ]
 
     if special_methods.include?(new_name)
