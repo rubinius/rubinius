39342741be3a06d748273de58997a7fec81d750c
Brian Shirai <brixen@gmail.com>
2013-08-14 15:41:56 -0700

    Fixed reference to AST.

--- spec/core/string/to_ast_spec.rb
+++ spec/core/string/to_ast_spec.rb
@@ -1,8 +1,12 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "String#to_ast" do
+  before :all do
+    @ast_class = Rubinius::ToolSet::Runtime::AST
+  end
+
   it "converts to AST nodes from Ruby code" do
-    "true".to_ast.should be_kind_of(Rubinius::AST::TrueLiteral)
+    "true".to_ast.should be_kind_of(@ast_class::TrueLiteral)
   end
 
   it "raises SyntaxError with invalid Ruby code " do
@@ -11,7 +15,7 @@ describe "String#to_ast" do
 
   ruby_version_is "1.9" do
     it "converts to AST nodes from Ruby 1.9 code" do
-      "->{}".to_ast.should be_kind_of(Rubinius::AST::Send)
+      "->{}".to_ast.should be_kind_of(@ast_class::Send)
     end
   end
 end
