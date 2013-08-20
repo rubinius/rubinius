491dd1770d9d2c9aff6a70a043e7974499594e85
Dirkjan Bussink <d.bussink@gmail.com>
2013-08-20 21:27:51 +0200

    Fix dump spec for 1.8 mode

--- spec/ruby/core/marshal/dump_spec.rb
+++ spec/ruby/core/marshal/dump_spec.rb
@@ -206,10 +206,20 @@ describe "Marshal.dump" do
       Marshal.dump(encode(UserString.new.extend(Meths), "binary")).should == "\004\be:\nMethsC:\017UserString\"\000"
     end
 
-    it "dumps a String with instance variables" do
-      str = ""
-      str.instance_variable_set("@foo", "bar")
-      Marshal.dump(encode(str, "binary")).should == "\x04\bI\"\x00\x06:\t@fooI\"\bbar\x06:\x06EF"
+    ruby_version_is ""..."1.9" do
+      it "dumps a String with instance variables" do
+        str = ""
+        str.instance_variable_set("@foo", "bar")
+        Marshal.dump(encode(str, "binary")).should == "\004\bI\"\000\006:\t@foo\"\bbar"
+      end
+    end
+
+    ruby_version_is "1.9" do
+      it "dumps a String with instance variables" do
+        str = ""
+        str.instance_variable_set("@foo", "bar")
+        Marshal.dump(encode(str, "binary")).should == "\x04\bI\"\x00\x06:\t@fooI\"\bbar\x06:\x06EF"
+      end
     end
 
     with_feature :encoding do
