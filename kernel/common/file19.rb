e0a8916d2a6b6fb1cafa508c175b4f4ddbb52d2a
Kenichi Kamiya <kachick1@gmail.com>
2013-09-14 05:40:26 +0900

    Refactor File.expand_path

--- kernel/common/file19.rb
+++ kernel/common/file19.rb
@@ -135,80 +135,10 @@ class File
     dirname(realpath(path, basedir))
   end
 
-  def self.expand_path(path, dir=nil)
-    path = Rubinius::Type.coerce_to_path(path)
-    str = Rubinius::Type.external_string(path.to_s.dup).clear
-    first = path[0]
-    if first == ?~
-      case path[1]
-      when ?/
-        unless home = ENV["HOME"]
-          raise ArgumentError, "couldn't find HOME environment variable when expanding '~'"
-        end
-
-        path = ENV["HOME"] + path.byteslice(1, path.bytesize - 1)
-      when nil
-        unless home = ENV["HOME"]
-          raise ArgumentError, "couldn't find HOME environment variable when expanding '~'"
-        end
-
-        if home.empty?
-          raise ArgumentError, "HOME environment variable is empty expanding '~'"
-        end
-
-        return home.dup
-      else
-        unless length = path.find_string("/", 1)
-          length = path.bytesize
-        end
-
-        name = path.byteslice 1, length - 1
-        unless dir = Rubinius.get_user_home(name)
-          raise ArgumentError, "user #{name} does not exist"
-        end
-
-        path = dir + path.byteslice(length, path.bytesize - length)
-      end
-    elsif first != ?/
-      if dir
-        dir = expand_path dir
-      else
-        dir = PrivateDir.pwd
-      end
-
-      path = "#{dir}/#{path}"
-    end
-
-    items = []
-    start = 0
-    size = path.bytesize
-
-    while index = path.find_string("/", start) or (start < size and index = size)
-      length = index - start
-
-      if length > 0
-        item = path.byteslice start, length
-
-        if item == ".."
-          items.pop
-        elsif item != "."
-          items << item
-        end
-      end
-
-      start = index + 1
-    end
-
-    if items.empty?
-      str << "/"
-    else
-      items.each { |x| str.append "/#{x}" }
-    end
-
-    str
+  def self.expand_path_buffer(path)
+    Rubinius::Type.external_string(path.to_s.dup).clear
   end
-
-
+  private_class_method :expand_path_buffer
 end
 
 class File::Stat
