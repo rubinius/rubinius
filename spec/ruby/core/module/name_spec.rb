80d223de5772fa222dc2599380c86bf6d9e84229
Kenichi Kamiya <kachick1@gmail.com>
2013-09-14 07:19:50 +0900

    Add Module#name specs for mainly about anonymous modules
    `name.to_s.should == ""` is keeping compatibility for 1.8 with 1.9+

--- spec/ruby/core/module/name_spec.rb
+++ spec/ruby/core/module/name_spec.rb
@@ -2,6 +2,73 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/module', __FILE__)
 
 describe "Module#name" do
+  describe "on named modules" do
+    it "is set with nested names and it will not be changed by after definitions" do
+      foo = TemporaryTopConstantForModuleName1 = Module.new
+
+      begin
+        bar = TemporaryTopConstantForModuleName1::Bar = Module.new
+        baz = TemporaryTopConstantForModuleName1::Bar::Baz = Module.new
+        foo.name.should == "TemporaryTopConstantForModuleName1"
+        bar.name.should == "TemporaryTopConstantForModuleName1::Bar"
+        baz.name.should == "TemporaryTopConstantForModuleName1::Bar::Baz"
+
+        TemporaryTopConstantForModuleName1::Bar.send :remove_const, :Baz
+        TemporaryTopConstantForModuleName1::Bar::Baz2 = baz
+        baz.name.should == "TemporaryTopConstantForModuleName1::Bar::Baz"
+
+        TemporaryTopConstantForModuleName1.send :remove_const, :Bar
+        TemporaryTopConstantForModuleName1::Bar2 = bar
+        bar.name.should == "TemporaryTopConstantForModuleName1::Bar"
+        baz.name.should == "TemporaryTopConstantForModuleName1::Bar::Baz"
+      ensure
+        Object.send :remove_const, :TemporaryTopConstantForModuleName1
+      end
+
+      TemporaryTopConstantForModuleName2 = foo
+
+      begin
+        foo.name.should == "TemporaryTopConstantForModuleName1"
+        bar.name.should == "TemporaryTopConstantForModuleName1::Bar"
+        baz.name.should == "TemporaryTopConstantForModuleName1::Bar::Baz"
+      ensure
+        Object.send :remove_const, :TemporaryTopConstantForModuleName2
+      end
+    end
+  end
+
+  describe "on anonymous modulues" do
+    it "will be set with nested names only once when top constant is defined" do
+      foo, bar, baz = Module.new, Module.new, Module.new
+      foo::Bar = bar
+      foo::Bar::Baz = baz
+
+      foo.name.to_s.should == ""
+      bar.name.to_s.should == ""
+      baz.name.to_s.should == ""
+
+      TemporaryTopConstantForModuleName1 = foo
+
+      begin
+        foo.name.should == "TemporaryTopConstantForModuleName1"
+        bar.name.should == "TemporaryTopConstantForModuleName1::Bar"
+        baz.name.should == "TemporaryTopConstantForModuleName1::Bar::Baz"
+      ensure
+        Object.send :remove_const, :TemporaryTopConstantForModuleName1
+      end
+
+      TemporaryTopConstantForModuleName2 = foo
+
+      begin
+        foo.name.should == "TemporaryTopConstantForModuleName1"
+        bar.name.should == "TemporaryTopConstantForModuleName1::Bar"
+        baz.name.should == "TemporaryTopConstantForModuleName1::Bar::Baz"
+      ensure
+        Object.send :remove_const, :TemporaryTopConstantForModuleName2
+      end
+    end
+  end
+
   ruby_version_is ""..."1.9" do
     it "is an empty string for an anonymous module" do
       Module.new.name.should == ""
