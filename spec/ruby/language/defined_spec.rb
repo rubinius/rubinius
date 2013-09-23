1c490cff46a1f5679b77464883da87649a9b8a96
Brian Shirai <brixen@gmail.com>
2013-09-22 21:52:11 -0700

    Fixed some defined? specs for super.

--- spec/ruby/language/defined_spec.rb
+++ spec/ruby/language/defined_spec.rb
@@ -1250,20 +1250,16 @@ describe "The defined? keyword for super" do
       DefinedSpecs::Super.new.method_no_args.should == "super"
     end
 
-    ruby_bug '#6644', '1.9.3' do
-      it "returns 'super' from a block when a superclass method exists" do
-        DefinedSpecs::Super.new.method_block_no_args.should == nil
-      end
+    it "returns 'super' from a block when a superclass method exists" do
+      DefinedSpecs::Super.new.method_block_no_args.should == "super"
     end
 
     it "returns 'super' from a #define_method when a superclass method exists" do
       DefinedSpecs::Super.new.define_method_no_args.should == "super"
     end
 
-    ruby_bug '#6644', '1.9.3' do
-      it "returns 'super' from a block in a #define_method when a superclass method exists" do
-        DefinedSpecs::Super.new.define_method_block_no_args.should == nil
-      end
+    it "returns 'super' from a block in a #define_method when a superclass method exists" do
+      DefinedSpecs::Super.new.define_method_block_no_args.should == "super"
     end
 
     it "returns 'super' when the method exists in a supermodule" do
@@ -1292,20 +1288,16 @@ describe "The defined? keyword for super" do
       DefinedSpecs::Super.new.method_args.should == "super"
     end
 
-    ruby_bug '#6644', '1.9.3' do
-      it "returns 'super' from a block when a superclass method exists" do
-        DefinedSpecs::Super.new.method_block_args.should == nil
-      end
+    it "returns 'super' from a block when a superclass method exists" do
+      DefinedSpecs::Super.new.method_block_args.should == "super"
     end
 
     it "returns 'super' from a #define_method when a superclass method exists" do
       DefinedSpecs::Super.new.define_method_args.should == "super"
     end
 
-    ruby_bug '#6644', '1.9.3' do
-      it "returns 'super' from a block in a #define_method when a superclass method exists" do
-        DefinedSpecs::Super.new.define_method_block_args.should == nil
-      end
+    it "returns 'super' from a block in a #define_method when a superclass method exists" do
+      DefinedSpecs::Super.new.define_method_block_args.should == "super"
     end
   end
 
@@ -1467,9 +1459,20 @@ describe "The defined? keyword for exception expressions" do
     end
   end
 
-  ruby_version_is "1.9" do
+  ruby_version_is "1.9"..."2.1" do
     it "returns 'expression'" do
       defined?(begin end).should == "expression"
+    end
+  end
+
+  ruby_version_is "2.1" do
+    it "returns 'nil' for an empty expression" do
+      defined?(begin end).should == "nil"
+    end
+
+    it "returns the value of 'defined?' for the contained expression" do
+      defined?(begin retry end).should == "expression"
+      defined?(begin x end).should be_nil
     end
   end
 end
