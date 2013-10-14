e5ab1e81a86a82f91e2cb11b15c9b72d0a497e2a
Federico Ravasio <ravasio.federico@gmail.com>
2013-10-14 11:11:38 +0200

    Implement rb_fix2int and rb_fix2uint.

---
+++ vm/capi/fixnum.cpp
@@ -0,0 +1,27 @@
+#include "capi/ruby.h"
+
+extern "C" {
+  unsigned long rb_fix2uint(VALUE obj) {
+    unsigned long num = rb_num2ulong(obj);
+
+    if((int)num < 0) {
+      rb_raise(rb_eRangeError, "integer too small to convert into unsigned int");
+    }
+
+    if((unsigned int)num != num) {
+      rb_raise(rb_eRangeError, "integer too big to convert into unsigned int");
+    }
+
+    return num;
+  }
+
+  long rb_fix2int(VALUE obj) {
+    long num = rb_num2long(obj);
+
+    if((int)num != num) {
+      rb_raise(rb_eRangeError, "integer too big to convert into unsigned int");
+    }
+
+    return num;
+  }
+}
