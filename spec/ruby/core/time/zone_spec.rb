369bca16a0e7686f182369917579b7535cc8688b
Federico Ravasio <ravasio.federico@gmail.com>
2013-10-14 15:41:32 +0200

    Add specs on Time#zone after calling Time#getlocal.

--- spec/ruby/core/time/zone_spec.rb
+++ spec/ruby/core/time/zone_spec.rb
@@ -13,6 +13,22 @@ describe "Time#zone" do
     it "returns nil for a Time with a fixed offset" do
       Time.new(2001, 1, 1, 0, 0, 0, "+05:00").zone.should == nil
     end
+
+    it "returns the correct timezone for a local time" do
+      t = Time.new(2005, 2, 27, 22, 50, 0, -3600)
+
+      with_timezone("US/Eastern") do
+        t.getlocal.zone.should == "EST"
+      end
+    end
+
+    it "returns nil when getting the local time with a fixed offset" do
+      t = Time.new(2005, 2, 27, 22, 50, 0, -3600)
+
+      with_timezone("US/Eastern") do
+        t.getlocal("+05:00").zone.should be_nil
+      end
+    end
   end
 
   it "returns UTC when called on a UTC time" do
