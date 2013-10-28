1c063142608c284ad30b15c0c2eb690fe8661958
Dirkjan Bussink <d.bussink@gmail.com>
2013-10-28 15:43:15 +0100

    Use more specific symbol name for ceil macro

--- vm/include/capi/ruby/ruby.h
+++ vm/include/capi/ruby/ruby.h
@@ -967,8 +967,8 @@ struct RTypedData {
 # define BDIGIT_DBL_SIGNED long
 #endif
 
-#define CEIL(x,y) (((x) + (y) - 1) / (y))
-#define RBIGNUM_LEN(obj) CEIL(rb_big_bytes_used(obj), SIZEOF_BDIGITS)
+#define RBX_CAPI_CEIL(x,y) (((x) + (y) - 1) / (y))
+#define RBIGNUM_LEN(obj) RBX_CAPI_CEIL(rb_big_bytes_used(obj), SIZEOF_BDIGITS)
   /** Calls this method in a superclass. */
   VALUE rb_call_super(int argc, const VALUE *argv);
 
