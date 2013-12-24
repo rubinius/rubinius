d8aba3ebf338440be60a8d469804de48553f26ac
Brian Shirai <brixen@gmail.com>
2013-12-23 21:33:09 -0800

    Updated version.

--- kernel/delta/ruby_constants.rb
+++ kernel/delta/ruby_constants.rb
@@ -2,12 +2,12 @@
 #
 
 RUBY_ENGINE       = "rbx"
+RUBY_VERSION      = Rubinius::RUBY_VERSION
+RUBY_PATCHLEVEL   = 0
 RUBY_PLATFORM     = "#{Rubinius::CPU}-#{Rubinius::OS}"
 RUBY_RELEASE_DATE = Rubinius::RELEASE_DATE.dup
 RUBY_COPYRIGHT    = "rubinius - Copyright (C) 2006-2011 Evan Phoenix"
 RUBY_REVISION     = 0
-RUBY_VERSION      = "1.8.7"
-RUBY_PATCHLEVEL   = 0
 # Must be last, it reads the above constants
 RUBY_DESCRIPTION  = Rubinius.version
 
