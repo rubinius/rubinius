acae1fafb0cd7445c66f3f83a4bc27422b1f989f
Brian Shirai <brixen@gmail.com>
2014-01-08 12:05:00 -0800

    Removed problematic spec.
    We need a better mechanism for obtaining the comparison value.

--- spec/ruby/core/time/shared/now.rb
+++ spec/ruby/core/time/shared/now.rb
@@ -1,14 +1,6 @@
 require File.expand_path('../../fixtures/classes', __FILE__)
 
 describe :time_now, :shared => true do
-  platform_is_not :windows do
-    it "creates a time based on the current system time" do
-      unless `which date` == ""
-        Time.send(@method).to_i.should be_close(`date +%s`.to_i, 2)
-      end
-    end
-  end
-
   it "creates a subclass instance if called on a subclass" do
     TimeSpecs::SubTime.send(@method).should be_an_instance_of(TimeSpecs::SubTime)
   end
