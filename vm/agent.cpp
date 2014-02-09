aa45fc7d0fb60adec90568c47b06470cfe411a36
Dirkjan Bussink <d.bussink@gmail.com>
2014-02-09 19:01:53 +0100

    Move allocation to always allocate in constructor

--- vm/agent.cpp
+++ vm/agent.cpp
@@ -69,6 +69,8 @@ namespace rubinius {
     shared_.auxiliary_threads()->register_thread(this);
     shared_.set_query_agent(this);
 
+    vars_ = new agent::VariableAccess(state);
+
     initialize(state);
     start_thread(state);
   }
@@ -80,8 +82,6 @@ namespace rubinius {
 
   void QueryAgent::initialize(STATE) {
     FD_ZERO(&fds_);
-
-    vars_ = new agent::VariableAccess(state);
 
     tmp_path[0] = 0;
 
