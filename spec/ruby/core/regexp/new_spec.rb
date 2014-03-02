7981f2c05671722dcf12f0d4f6bceb46cef29ff5
jc00ke <jesse@jc00ke.com>
2014-03-02 09:35:26 -0800

    Add number arg specs for Regexp.new
    For #2959

--- spec/ruby/core/regexp/new_spec.rb
+++ spec/ruby/core/regexp/new_spec.rb
@@ -12,3 +12,16 @@ end
 describe "Regexp.new given a Regexp" do
   it_behaves_like :regexp_new_regexp, :new
 end
+
+describe "Regexp.new given a Fixnum" do
+  it "raises a TypeError" do
+    lambda { Regexp.new(1) }.should raise_error(TypeError)
+  end
+end
+
+describe "Regexp.new given a Float" do
+  it "raises a TypeError" do
+    lambda { Regexp.new(1.0) }.should raise_error(TypeError)
+  end
+end
+
