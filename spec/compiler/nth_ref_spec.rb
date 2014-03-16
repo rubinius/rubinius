b73211c1f3a46126f2cf3a43c9507c83a1b624af
Brian Shirai <brixen@gmail.com>
2014-03-15 22:36:52 -0500

    Removed compiler specs.

--- spec/compiler/nth_ref_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../spec_helper', __FILE__)
-
-describe "An Nth_ref node" do
-  relates "$1" do
-    compile do |g|
-      g.last_match 5, 0
-    end
-  end
-end
--- spec/compiler/true_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../spec_helper', __FILE__)
-
-describe "A True node" do
-  relates "true" do
-    compile do |g|
-      g.push :true
-    end
-  end
-end
