b728166b7dc4f7b1f9fd9261a2ca5028a9ad3cbd
Brian Shirai <brixen@gmail.com>
2014-03-15 22:37:28 -0500

    Removed spec/ruby/library.

--- spec/ruby/library/rational/exponent_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../../shared/rational/exponent', __FILE__)
-require 'rational'
-
-ruby_version_is ""..."1.9" do
-  describe "Rational#**" do
-    it_behaves_like(:rational_exponent, :**)
-  end
-end
--- spec/ruby/library/rational/to_f_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../../shared/rational/to_f', __FILE__)
-require 'rational'
-
-ruby_version_is ""..."1.9" do
-  describe "Rational#to_f" do
-    it_behaves_like(:rational_to_f, :to_f)
-  end
-end
