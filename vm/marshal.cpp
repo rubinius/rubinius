155a3ea613e121dbdc8cbe726b4d8a16ea46d578
Brian Shirai <brixen@gmail.com>
2014-06-14 20:14:15 -0500

    Unmarshal CompiledCode keywords, arity fields.

--- vm/marshal.cpp
+++ vm/marshal.cpp
@@ -248,6 +248,8 @@ namespace rubinius {
     code->post_args(state, force_as<Fixnum>(unmarshal()));
     code->total_args(state, force_as<Fixnum>(unmarshal()));
     code->splat(state, force_as<Fixnum>(unmarshal()));
+    code->keywords(state, force_as<Tuple>(unmarshal()));
+    code->arity(state, force_as<Fixnum>(unmarshal()));
     code->literals(state, force_as<Tuple>(unmarshal()));
     code->lines(state, force_as<Tuple>(unmarshal()));
     code->file(state, force_as<Symbol>(unmarshal()));
