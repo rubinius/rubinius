67cb983b33d03294d46750ebc9085679ddd0230d
Brian Shirai <brixen@gmail.com>
2014-08-11 14:31:12 -0500

    Fixed DebugAction registering for :before events.

--- spec/custom/runner/actions/debug.rb
+++ spec/custom/runner/actions/debug.rb
@@ -7,4 +7,12 @@ class DebugAction < ActionFilter
   def before(state)
     Rubinius::Debugger.start if self === state.description
   end
+
+  def register
+    MSpec.register :before, self
+  end
+
+  def unregister
+    MSpec.unregister :before, self
+  end
 end
