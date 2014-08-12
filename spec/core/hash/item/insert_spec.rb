3e41f72d0720eaf34e8570e7b21be4299a2e0ada
Sophia Shao <sshao@users.noreply.github.com>
2014-08-12 17:12:11 -0500

    Updated spec/core/hash with specs from master

--- spec/core/hash/item/insert_spec.rb
+++ spec/core/hash/item/insert_spec.rb
@@ -4,23 +4,20 @@ with_feature :hash_hamt do
   describe "Hash::Item#insert" do
     before :each do
       @state = Hash::State.new
-      @entry = Hash::Item.new 1, 2, @state
+      @entry = Hash::Item.new 1, 1.hash, 2, @state
     end
 
     it "updates the value if the keys match" do
-      @state.should_receive(:match?).with(1, 1).and_return(true)
-      @entry.insert(1, 0, :value)
+      @entry.insert(1, 1.hash, :value)
       @entry.value.should == :value
     end
 
     it "returns itself if the keys match" do
-      @state.should_receive(:match?).with(1, 1).and_return(true)
-      @entry.insert(1, 0, :value).should equal(@entry)
+      @entry.insert(1, 1.hash, :value).should equal(@entry)
     end
 
     it "returns nil if the keys do not match" do
-      @state.should_receive(:match?).with(1, 1).and_return(false)
-      @entry.insert(1, 0, :value).should be_nil
+      @entry.insert(2, 2.hash, :value).should be_nil
     end
 
   end
--- spec/core/hash/trie/add_spec.rb
+++ spec/core/hash/trie/add_spec.rb
@@ -11,37 +11,27 @@ with_feature :hash_hamt do
       key_hash = 0b1000000
 
       k1 = mock("Hash key1")
-      k1.should_receive(:hash).and_return(key_hash)
-
       k2 = mock("Hash key2")
 
-      entry = Hash::Item.new k1, 1, @state
+      entry = Hash::Item.new k1, key_hash, 1, @state
       @trie.add entry, k2, key_hash, 2
       @trie.entries[0].should be_an_instance_of(Hash::List)
     end
 
     it "inserts a Trie if the key hash values collide at this level" do
-      k1_hash = 0b1_000001_001000
-
       k1 = mock("Hash key1")
-      k1.should_receive(:hash).twice.and_return(k1_hash)
-
       k2 = mock("Hash key2")
 
-      entry = Hash::Item.new k1, 1, @state
+      entry = Hash::Item.new k1, 0b1_000001_001000, 1, @state
       @trie.add entry, k2, 0b10_000001_000000, 2
       @trie.entries[0].should be_an_instance_of(Hash::Trie)
     end
 
     it "inserts two Item instances if the key hash values do not collide" do
-      k1_hash = 0b1_000001_001000
-
       k1 = mock("Hash key1")
-      k1.should_receive(:hash).and_return(k1_hash)
-
       k2 = mock("Hash key2")
 
-      entry = Hash::Item.new k1, 1, @state
+      entry = Hash::Item.new k1, 0b1_000001_001000, 1, @state
       @trie.add entry, k2, 0b10_000010_000000, 2
       @trie.entries[0].should be_an_instance_of(Hash::Item)
       @trie.entries[1].should be_an_instance_of(Hash::Item)
