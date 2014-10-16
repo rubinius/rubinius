c8dde0e111930c38eccba23be6ff8faa47f562eb
Brian Shirai <brixen@gmail.com>
2014-10-15 22:02:11 -0500

    Removed debugging.

--- vm/environment.cpp
+++ vm/environment.cpp
@@ -503,8 +503,6 @@ namespace rubinius {
 
   void Environment::remove_fsapi(STATE) {
     if(rmdir(shared->fsapi_path.c_str()) < 0) {
-      std::cerr << "environment: remove_fsapi: error" << std::endl;
-      sleep(60);
       utilities::logger::error("%s: unable to remove FSAPI path", strerror(errno));
     }
   }
