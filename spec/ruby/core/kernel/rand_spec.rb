79b581ca63916e95f03e35eb6dc53121521c866e
fengb <contact@fengb.info>
2014-11-05 01:14:12 -0600

    Fixed edge case where small BigDecimal ranges were erroring

--- spec/ruby/core/kernel/rand_spec.rb
+++ spec/ruby/core/kernel/rand_spec.rb
@@ -53,6 +53,11 @@ describe "Kernel.rand" do
     rand(0.25..0.75).should be_kind_of(Float)
   end
 
+  it "returns a numeric for an range argument where max is < 1" do
+    require 'bigdecimal'
+    rand(BigDecimal(0.25, 2)..BigDecimal(0.75, 2)).should be_kind_of(Numeric)
+  end
+
   it "returns nil when range is backwards" do
     rand(1..0).should be_nil
   end
