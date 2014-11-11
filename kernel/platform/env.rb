26e037385ae89bf913d806af7771e40e469f22e8
David Boot <kodnin@gmail.com>
2014-11-11 15:42:33 +0100

    clean up env.rb

--- kernel/platform/env.rb
+++ kernel/platform/env.rb
@@ -2,10 +2,10 @@ module Rubinius
   module EnvironmentAccess
     extend FFI::Library
 
-    attach_function :getenv,   [:string], :string
-    attach_function :putenv,   [:string], :int
-    attach_function :setenv,   [:string,  :string, :int], :int
-    attach_function :unsetenv, [:string], :int
-    attach_function :environ,  'ffi_environ', [], :pointer
+    attach_function(:getenv, [:string], :string)
+    attach_function(:putenv, [:string], :int)
+    attach_function(:setenv, [:string, :string, :int], :int)
+    attach_function(:unsetenv, [:string], :int)
+    attach_function(:environ, 'ffi_environ', [], :pointer)
   end
 end
