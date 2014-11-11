1b5dc87012780f5a5d14336d7eb8b590434fafcd
David Boot <kodnin@gmail.com>
2014-11-11 15:45:03 +0100

    clean up file.rb

--- kernel/platform/file.rb
+++ kernel/platform/file.rb
@@ -1,10 +1,8 @@
-##
 # Platform specific behavior for the File class.
-
 module Rubinius
-module FFI::Platform::File
-  SEPARATOR = '/'
-  ALT_SEPARATOR = nil
-  PATH_SEPARATOR = ':'
-end
+  module FFI::Platform::File
+    SEPARATOR = '/'
+    ALT_SEPARATOR = nil
+    PATH_SEPARATOR = ':'
+  end
 end
