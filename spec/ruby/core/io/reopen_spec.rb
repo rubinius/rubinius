7bd48c735ccb4e561af38054f01fbd82e7aca0da
Brian Shirai <brixen@gmail.com>
2014-12-16 21:58:01 -0600

    Added spec for reopen with path closing fd.

--- spec/ruby/core/io/reopen_spec.rb
+++ spec/ruby/core/io/reopen_spec.rb
@@ -119,12 +119,14 @@ describe "IO#reopen with a String" do
   before :each do
     @name = tmp("io_reopen.txt")
     @other_name = tmp("io_reopen_other.txt")
+    @other_io = nil
 
     rm_r @other_name
   end
 
   after :each do
     @io.close unless @io.closed?
+    @other_io.close if @other_io and not @other_io.closed?
     rm_r @name, @other_name
   end
 
@@ -138,6 +140,19 @@ describe "IO#reopen with a String" do
 
     @name.should have_data("original data")
     @other_name.should have_data("new data")
+  end
+
+  it "closes the file descriptor obtained by opening the new file" do
+    @io = new_io @name, "w"
+
+    @other_io = File.open @other_name, "w"
+    max = @other_io.fileno
+    @other_io.close
+
+    @io.reopen @other_name
+
+    @other_io = File.open @other_name, "w"
+    @other_io.fileno.should == max
   end
 
   it "creates the file if it doesn't exist if the IO is opened in write mode" do
