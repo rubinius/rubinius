cd2fadb7942848f5bbf9f6da6fcd5054c5f11cd0
Chuck Remes <git@chuckremes.com>
2015-01-16 15:04:25 -0600

    add back dup2 function definition

--- kernel/platform/posix.rb
+++ kernel/platform/posix.rb
@@ -44,6 +44,7 @@ module FFI::Platform::POSIX
   attach_function :ioctl,       [:int, :ulong, :long], :int
   attach_function :fsync,       [:int], :int
   attach_function :dup,         [:int], :int
+  attach_function :dup2,        [:int, :int], :int
   attach_function :open,        [:string, :int, :mode_t], :int
   attach_function :close,       [:int], :int
   attach_function :lseek,       [:int, :off_t, :int], :off_t
