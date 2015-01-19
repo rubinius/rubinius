bbd1acfe494c145da1424ecd0676b8d2e3a90a5f
Chuck Remes <git@chuckremes.com>
2015-01-19 08:25:12 -0600

    add convenience methods for errno handling

--- kernel/common/errno.rb
+++ kernel/common/errno.rb
@@ -11,9 +11,17 @@ module Errno
   # Unlike rb_sys_fail(), handle does not raise an exception if errno is 0.
 
   def self.handle(additional = nil)
-    err = FFI::Platform::POSIX.errno
+    err = errno
     return if err == 0
 
     raise SystemCallError.new(additional, err)
+  end
+  
+  def self.errno
+    FFI::Platform::POSIX.errno
+  end
+  
+  def self.eql?(code)
+    FFI::Platform::POSIX.errno == code
   end
 end
