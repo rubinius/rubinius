9144e79dae4bc1c0058389a3e049d7df575f47b8
Filipe Dias <fmfdias@gmail.com>
2015-02-22 16:02:15 +0000

    Reorganizes and adds new Enumerator#size calculation tests for the Array class.
    It also removes the old falling tests tags.

--- spec/ruby/core/array/reverse_each_spec.rb
+++ spec/ruby/core/array/reverse_each_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes', __FILE__)
 require File.expand_path('../shared/enumeratorize', __FILE__)
+require File.expand_path('../../enumerable/shared/enumeratorized', __FILE__)
 
 # Modifying a collection while the contents are being iterated
 # gives undefined behavior. See
@@ -38,4 +39,5 @@ describe "Array#reverse_each" do
   end
 
   it_behaves_like :enumeratorize, :reverse_each
+  it_behaves_like :enumeratorized_with_origin_size, :reverse_each, [1,2,3]
 end
