64d3c5305276d87f2f126e3ff5f785bad0507e55
Jason Yeo <jasonyeo88@gmail.com>
2015-03-08 22:12:35 +0800

    Add File.birthtime and File#birthtime specs

---
+++ spec/ruby/core/file/birthtime_spec.rb
@@ -0,0 +1,40 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe "File.birthtime" do
+  before :each do
+    @file = __FILE__
+  end
+
+  after :each do
+    @file = nil
+  end
+
+  it "returns the birth time for the named file as a Time object" do
+    File.birthtime(@file)
+    File.birthtime(@file).should be_kind_of(Time)
+  end
+
+  it "accepts an object that has a #to_path method" do
+    File.birthtime(mock_to_path(@file))
+  end
+
+  it "raises an Errno::ENOENT exception if the file is not found" do
+    lambda { File.birthtime('bogus') }.should raise_error(Errno::ENOENT)
+  end
+end
+
+describe "File#birthtime" do
+  before :each do
+    @file = File.open(__FILE__)
+  end
+
+  after :each do
+    @file.close
+    @file = nil
+  end
+
+  it "returns the birth time for self" do
+    @file.birthtime
+    @file.birthtime.should be_kind_of(Time)
+  end
+end
