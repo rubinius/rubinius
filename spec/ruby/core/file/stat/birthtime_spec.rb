be72e9d72c637936589653940887f42060599bb4
Jason Yeo <jasonyeo88@gmail.com>
2015-03-08 21:53:01 +0800

    Add File::Stat#birthtime specs

---
+++ spec/ruby/core/file/stat/birthtime_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../../../../spec_helper', __FILE__)
+
+describe "File::Stat#birthtime" do
+  before :each do
+    @file = tmp('i_exist')
+    touch(@file) { |f| f.write "rubinius" }
+  end
+
+  after :each do
+    rm_r @file
+  end
+
+  it "returns the birthtime of a File::Stat object" do
+    st = File.stat(@file)
+    st.birthtime.should be_kind_of(Time)
+    st.birthtime.should <= Time.now
+  end
+end
