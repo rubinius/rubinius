d30a8207f9b6cb8237c506e6a5c9a4cb27c89068
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-04-29 18:30:17 -0700

    Add specs for prepended module method ownership (one failing).

--- spec/ruby/core/module/prepend_spec.rb
+++ spec/ruby/core/module/prepend_spec.rb
@@ -80,6 +80,18 @@ describe "Module#prepend" do
     Class.new { prepend(m) }.ancestors.should include(m)
   end
 
+  it "reports the prepended module as the method owner" do
+    m = Module.new { def meth; end }
+    c = Class.new { def meth; end; prepend m }
+    c.new.method(:meth).owner.should == m
+  end
+
+  it "reports the prepended module as the unbound method owner" do
+    m = Module.new { def meth; end }
+    c = Class.new { def meth; end; prepend m }
+    c.instance_method(:meth).owner.should == m
+  end
+
   it "sees an instance of a prepended class as kind of the prepended module" do
     m = Module.new
     c = Class.new { prepend(m) }
