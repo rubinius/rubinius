771ea7f6b2833658ee5d61c5e6f3020526443c58
Brian Shirai <brixen@gmail.com>
2015-12-16 23:18:57 -0800

    Fix location of LLVM include files.

--- scripts/llvm.sh
+++ scripts/llvm.sh
@@ -4,4 +4,5 @@ LLVM_TAR=llvm-3.4.2-x86_64-unknown-linux-gnu-4.7.tar.bz2
 
 curl -o $LLVM_TAR https://s3.amazonaws.com/asset.rubini.us/prebuilt/$LLVM_TAR \
   && mkdir -p vendor/llvm \
-  && tar -C vendor/llvm -xjf $LLVM_TAR
+  && tar -C vendor/llvm -xjf $LLVM_TAR \
+  && mv vendor/llvm/include vendor/llvm/Release/
