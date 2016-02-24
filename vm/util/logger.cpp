99579cbf99b018c683446f18681209d29a5033d1
Ahmad Sherif <me@ahmadsherif.com>
2016-02-24 05:09:53 +0200

    Use a clearer condition in utilities::logger::append_newline
    Credits to @brixen, follow-up on 0b4b712.
    
    [ci skip]

--- vm/util/logger.cpp
+++ vm/util/logger.cpp
@@ -54,7 +54,7 @@ namespace rubinius {
         char* end = (char*)memchr(message, '\0', LOGGER_MSG_SIZE);
         size_t bytes = end ? end - message : LOGGER_MSG_SIZE;
 
-        if(LOGGER_MSG_SIZE - bytes > 1) {
+        if(bytes + 1 < LOGGER_MSG_SIZE) {
           if(message[bytes-1] != '\n') {
             message[bytes] = '\n';
             message[bytes += 1] = '\0';
