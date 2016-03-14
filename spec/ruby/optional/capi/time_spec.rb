2bf206d50c0fb1916e17d936de2e7a4c2a42145b
Ahmad Sherif <me@ahmadsherif.com>
2016-03-14 01:57:43 -0700

    Fix an rb_time_timespec_new spec
    For time zones with daylight saving time in effect, `Time.now` has a
    DST offset, while the extension function returns a time object in
    standard time offset.

--- spec/ruby/optional/capi/time_spec.rb
+++ spec/ruby/optional/capi/time_spec.rb
@@ -274,7 +274,7 @@ describe "CApiTimeSpecs" do
 
     it "returns time object in localtime if offset given equals INT_MAX" do
       @s.rb_time_timespec_new(1447087832, 476451125, 0x7fffffff).should == Time.at(1447087832, 476451.125).localtime
-      @s.rb_time_timespec_new(1447087832, 476451125, 0x7fffffff).gmtoff.should == Time.now.gmtoff
+      @s.rb_time_timespec_new(1447087832, 476451125, 0x7fffffff).gmtoff.should == Time.local(2016).gmtoff
     end
 
     it "raises an ArgumentError if offset passed is not within range of -86400 and 86400 (exclusive)" do
