938a43458dc48063235f545ea76a7d00c1ffbc91
Brian Shirai <brixen@gmail.com>
2016-04-27 12:56:39 -0700

    Added missing CallSite#reset spec file.

---
+++ spec/core/callsite/reset_spec.rb
@@ -0,0 +1,37 @@
+require File.expand_path('../../fixtures/call_site.rb', __FILE__)
+
+describe "Rubinius::CallSite#reset" do
+  before :each do
+    @klass = Class.new do
+      def m(a)
+        a.to_s
+      end
+    end
+
+    @call_site = @klass.instance_method(:m).executable.call_sites.first
+  end
+
+  it "clears the caches" do
+    obj = @klass.new
+
+    obj.m 1
+    obj.m :a
+
+    @call_site.depth.should == 1
+    @call_site.reset
+
+    @call_site.depth.should == 0
+  end
+
+  it "sets the invokes to 0" do
+    obj = @klass.new
+
+    obj.m 1
+    obj.m :a
+
+    @call_site.invokes.should == 2
+    @call_site.reset
+
+    @call_site.invokes.should == 0
+  end
+end
