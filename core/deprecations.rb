76c6124ce52f7d7cebb9bfbac9d993d36208c52c
Brian Shirai <brixen@gmail.com>
2016-07-16 20:00:44 -0700

    Updated deprecations for Ruby 2.2.

--- core/deprecations.rb
+++ core/deprecations.rb
@@ -1,7 +1,7 @@
 module Rubinius
   DEPRECATIONS = {
     # "Description" => "Alternative"
-    "Ruby 2.1 features that are incompatible with Ruby 2.2 are deprecated." =>
-      "Use Ruby 2.2 features if they are available.",
+    "Ruby 2.1/2.2 features that are incompatible with Ruby 2.3 are deprecated." =>
+      "Use Ruby 2.3 features if they are available.",
   }
 end
