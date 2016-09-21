8918fe5726a4d8ce522d6510f1543bc401a1ddd5
Brian Shirai <brixen@gmail.com>
2016-09-20 20:19:47 -0700

    Apple 10.12 deprecates syscall(2).

--- machine/missing/gettid.cpp
+++ machine/missing/gettid.cpp
@@ -1,11 +1,18 @@
 #include "machine/missing/gettid.h"
 
 #ifndef HAVE_GETTID
+#if defined(__APPLE__)
+#define _DARWIN_C_SOURCE
+#include <pthread.h>
+#else
 #include <sys/syscall.h>
+#endif
 
 pid_t gettid(void) {
 #if defined(__APPLE__)
-  return (pid_t)syscall(SYS_thread_selfid);
+  uint64_t tid;
+  pthread_threadid_np(NULL, &tid);
+  return (pid_t)tid;
 #elif defined(__FreeBSD__)
   long tid;
   syscall(SYS_thr_self, &tid);
