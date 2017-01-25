8b030e0cee06fc3bba93afc728c1bc3fd6cf36a3
Chuck Remes <git@chuckremes.com>
2017-01-25 15:31:57 -0600

    clean up stack before executing method

--- machine/instructions/send_stack_with_block.hpp
+++ machine/instructions/send_stack_with_block.hpp
@@ -12,9 +12,9 @@ namespace rubinius {
       Arguments args(call_site->name(), recv, block, count,
                      stack_back_position(count));
 
-      Object* ret = call_site->execute(state, args);
-
       stack_clear(count + 1);
+
+      Object* ret = call_site->execute(state, args);
 
       state->vm()->checkpoint(state);
 
