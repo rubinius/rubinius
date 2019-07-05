6fc345fad445c3090a869b052fb7a82b39c5df82
Brian Shirai <brixen@gmail.com>
2019-07-04 20:56:07 -0700

    Use goto_past.

--- machine/interpreter/goto.cpp
+++ machine/interpreter/goto.cpp
@@ -5,11 +5,6 @@ namespace rubinius {
     intptr_t goto_(STATE, CallFrame* call_frame, intptr_t const opcodes[]) {
       intptr_t location = argument(0);
 
-      // Temporary until goto_past is added
-      if(location < call_frame->ip()) {
-        state->vm()->checkpoint(state);
-      }
-
       instructions::goto_(call_frame);
       call_frame->set_ip(location);
 
