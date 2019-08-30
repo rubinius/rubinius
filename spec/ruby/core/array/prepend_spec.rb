f7a5afe6b86d29b9bdf4885b40d83630acadaeaf
Christian Bruckmayer <christian@bruckmayer.net>
2019-08-30 16:10:09 +0100

    Implement specs for Array#prepend and Array#append
    See https://bugs.ruby-lang.org/issues/12746

--- spec/ruby/core/array/prepend_spec.rb
+++ spec/ruby/core/array/prepend_spec.rb
@@ -2,6 +2,8 @@ require_relative '../../spec_helper'
 require_relative 'fixtures/classes'
 require_relative 'shared/unshift'
 
-describe "Array#unshift" do
-  it_behaves_like :array_unshift, :unshift
+ruby_version_is "2.5" do
+  describe "Array#prepend" do
+    it_behaves_like :array_unshift, :prepend
+  end
 end
