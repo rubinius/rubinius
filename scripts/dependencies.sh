c01f5be6bdfc1c584c78e0b108b50e35cccf2141
Brian Shirai <brixen@gmail.com>
2020-01-09 20:32:08 -0800

    Install gdbm on Linux for Travis 18.04 bionic.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y libc++-dev libc++abi-dev bsdtar libedit-dev gdbm
+sudo apt-get install -y libc++-dev libc++abi-dev bsdtar libedit-dev libgdbm-dev
