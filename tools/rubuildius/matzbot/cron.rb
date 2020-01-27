7364233a0cba9e68a820095af83a46d9a120bcaf
Brian Shirai <brixen@gmail.com>
2020-01-26 19:17:03 -0800

    Clean up some old stuff.

--- tools/rubuildius/matzbot/cron.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-matzdir = "~/matzbot/"
-
-if `ps -A | grep \`cat #{matzdir}matzbot.pid\` | wc`.to_i.zero?
-  system("cd #{matzdir}; rm matzbot.pid; ./launch.rb -d -w `cat the_password`")
-end
